dd78d8e940d34f0af0cdebd5a64b1d0404223386
Hung-Te Lin <hungte@chromium.org>
2011-10-27 20:30:38 +0800

    gooftool: support curl for uploading reports.
    To allow more uploading protocols, curl should be supported.
    This CL enables ftps://, cpfe://URL, and curl:CURL_COMMAND upload methods.
    
    BUG=chrome-os-partner:6651
    TEST=./gft_upload.py some_file ftp://anonymous@localhost/ # success
         ./gft_upload.py some_file ftps://anonymous@localhost/ # success
         ./gft_upload.py some_file cpfe:http://localhost/ # run a local HTTP server and success
    
    Change-Id: I4fd0feb41db55bca3a9e1f8a483aeeb2c0040112
    Reviewed-on: https://gerrit.chromium.org/gerrit/10777
    Tested-by: Hung-Te Lin <hungte@chromium.org>
    Reviewed-by: Tammo Spalink <tammo@chromium.org>
    Reviewed-by: Nick Sanders <nsanders@chromium.org>
    Commit-Ready: Hung-Te Lin <hungte@chromium.org>

--- gft_upload.py
+++ gft_upload.py
@@ -20,6 +20,37 @@ import gft_common
 from gft_common import DebugMsg, ErrorDie, ErrorMsg, VerboseMsg, WarningMsg
 
 
+# Constants
+DEFAULT_RETRY_INTERVAL = 60
+DEFAULT_RETRY_TIMEOUT = 30
+
+
+def RetryCommand(callback, message_prefix, interval):
+  """Retries running some commands until success.
+
+  Args:
+    callback: A callback function to execute specified command, and return
+              if the result is success. Callback accepts a param to hold session
+              states, including two special values:
+                'message' to be logged, and 'abort' to return immediately.
+    message_prefix: Prefix string to be displayed.
+    interval: Duration (in seconds) between each retry (0 to disable).
+  """
+  results = {}
+  # Currently we do endless retry, if interval is assigned.
+  while not callback(results):
+    message = results.get('message', 'unknown')
+    abort = results.get('abort', False)
+    if (not interval) or abort:
+      ErrorDie('%s: %s' % (message_prefix, message))
+    ErrorMsg('%s: %s' % (message_prefix, message))
+    for i in range(interval, 0, -1):
+      if i % 10 == 0:
+        sys.stderr.write(" Retry in %d seconds...\n" % i)
+      time.sleep(1)
+  return True
+
+
 def CustomUpload(source_path, custom_command):
   """Uploads the source file by a customized shell command.
 
@@ -37,14 +68,83 @@ def CustomUpload(source_path, custom_command):
   return True
 
 
-def FtpUpload(source_path, ftp_url, retry=0, timeout=10):
-  """Uploads the source file to a FTP url.
+def CurlCommand(curl_command, success_string=None, abort_string=None,
+                retry_interval=DEFAULT_RETRY_INTERVAL):
+  """Performs arbitrary curl command with retrying.
+
+  Args:
+    curl_command: Parameters to be invoked with curl.
+    success_string: String to be recognized as "uploaded successfully".
+  """
+  if not curl_command:
+    ErrorDie('CurlCommand: need parameters for curl.')
+
+  cmd = 'curl -s -S %s' % curl_command
+  DebugMsg('CurlCommand: %s' % cmd)
+
+  # man curl(1) for EXIT CODES not related to temporary network failure.
+  curl_abort_exit_codes = [1, 2, 3, 27, 37, 43, 45, 53, 54, 58, 59, 63]
+
+  def CurlCallback(result):
+    (exit_code, stdout, stderr) = (
+        gft_common.ShellExecution(cmd, ignore_status=True))
+    abort = False
+    message = None
+    if exit_code == 0:
+      if abort_string and stdout.find(abort_string) >= 0:
+        message = "Abort: Found abort pattern: %s" % abort_string
+      elif (not success_string) or (stdout.find(success_string) >= 0):
+        return True
+      else:
+        message = "Retry: No valid pattern (%s) in response." % success_string
+      DebugMsg("CurlCallback: original response: %s" %
+               ' '.join(stdout.splitlines()))
+    else:
+      message = '#%d %s' % (exit_code, stderr if stderr else stdout)
+      if exit_code in curl_abort_exit_codes:
+        abort = True
+    result['abort'] = abort
+    result['message'] = message
+
+  RetryCommand(CurlCallback, 'CurlCommand', interval=retry_interval)
+  VerboseMsg('CurlCommand: successfully executed: %s' % cmd)
+  return True
+
+
+def CurlUrlUpload(source_path, params, **kargs):
+  """Uploads the source file with URL-like protocols by curl.
 
   Args:
     source_path: File to upload.
+    params: Parameters to be invoked with curl.
+    retry: Duration (in secnods) for retry.
+  """
+  return CurlCommand('--ftp-ssl -T "%s" %s' % (source_path, params), **kargs)
+
+
+def CpfeUpload(source_path, cpfe_url, **kargs):
+  """Uploads the source file to ChromeOS Partner Front End site.
+
+  Args:
+    source_path: File to upload.
+    cpfe_url: URL to CPFE.
+    retry: Duration (in secnods) for retry.
+  """
+  curl_command = '--form "report_file=@%s" %s' % (source_path, cpfe_url)
+  CPFE_SUCCESS = '[CPFE UPLOAD: OK]'
+  CPFE_ABORT = '[CPFE UPLOAD: INVALID]'
+  return CurlCommand(curl_command, success_string=CPFE_SUCCESS,
+                     abort_string=CPFE_ABORT, **kargs)
+
+
+def FtpUpload(source_path, ftp_url, retry_interval=DEFAULT_RETRY_INTERVAL,
+              retry_timeout=DEFAULT_RETRY_TIMEOUT):
+  """Uploads the source file to a FTP url.
+
+    source_path: File to upload.
     ftp_url: A ftp url in ftp://user@pass:host:port/path format.
-    retry: A number in seconds for retry duration. 0 to prevent retry.
-    timeout: A number in seconds for connection timeout.
+    retry_interval: A number in seconds for retry duration. 0 to prevent retry.
+    retry_timeout: A number in seconds for connection timeout.
 
   Raises:
     GFTError: When input url is invalid, or if network issue without retry.
@@ -81,21 +181,17 @@ def FtpUpload(source_path, ftp_url, retry=0, timeout=10):
     path = os.path.join(path, source_name)
 
   ftp = ftplib.FTP()
-  VerboseMsg('FtpUpload: target is ftp://%s:%s@%s:%s/ %s' %
-             (userid, passwd, host, port, path))
-  while True:
+  url = 'ftp://%s:%s@%s:%s/ %s' % (userid, passwd, host, port, path)
+  VerboseMsg('FtpUpload: target is %s' % url)
+
+  def FtpCallback(result):
     try:
-      ftp.connect(host=host, port=port, timeout=timeout)
-      break
+      ftp.connect(host=host, port=port, timeout=retry_timeout)
+      return True
     except Exception, e:
-      if not retry:
-        ErrorDie('Cannot connect to: %s:%s [timeout=%s]' %
-                 (host, port, timeout))
-      ErrorMsg("\n FTP ERROR: %s" % e)
-      for i in range(retry, 0, -1):
-        if i % 10 == 0:
-          WarningMsg(" Retry FTP after %d seconds ( %s )..." % (i, ftp_url))
-        time.sleep(1)
+      result['message'] = '%s' % e
+
+  RetryCommand(FtpCallback, 'FtpUpload', interval=retry_interval)
 
   # Ready for copying files
   DebugMsg('FtpUpload: connected, uploading to %s...' % path)
@@ -108,30 +204,33 @@ def FtpUpload(source_path, ftp_url, retry=0, timeout=10):
   return True
 
 
-def NoneUpload(source_path):
+def NoneUpload(source_path, **kargs):
   """ Dummy function for bypassing uploads """
-  WarningMsg('NoneUpload: skipped uploading %s' % source_path)
+  WarningMsg('NoneUpload%s: skipped uploading %s' % (kargs, source_path))
   return True
 
 
-def Upload(path, method, network_retry=60):
+def Upload(path, method, **kargs):
   """Uploads a file by given method.
 
   Args:
     path: File path to be uploaded.
     method: A string to specify the method to upload files.
-    network_retry: A number in seconds for retrying network connection.
   """
   args = method.split(':', 1)
   method = args[0]
   param = args[1] if len(args) > 1 else None
 
   if method == 'none':
-    return NoneUpload(path)
+    return NoneUpload(path, **kargs)
   elif method == 'custom':
-    return CustomUpload(path, param)
+    return CustomUpload(path, param, **kargs)
   elif method == 'ftp':
-    return FtpUpload(path, 'ftp:' + param, retry=network_retry)
+    return FtpUpload(path, 'ftp:' + param, **kargs)
+  elif method == 'ftps':
+    return CurlUrlUpload(path, '--ftp-ssl-reqd ftp:%s' % param, **kargs)
+  elif method == 'cpfe':
+    return CpfeUpload(path, param, **kargs)
   else:
     ErrorDie('Upload: unknown method: %s' % method)
   return False
@@ -152,7 +251,13 @@ def main():
         Do nothing.
 
     ftp://userid:passwd@host:port/path
-        Upload to a ftp site
+        Upload to a FTP site.
+
+    ftps://userid:passwd@host:port/path [curl_options]
+        Upload by FTP-SSL protocol using curl.
+
+    cpfe:cpfe_url [curl_options]
+        Upload to Google ChromeOS Partner Front End.
 
     custom:shell_command
         Invoke a shell command to upload the file.
