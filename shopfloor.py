5319ec821df1cf105c10e35493085e95697c9b40
Hung-Te Lin <hungte@chromium.org>
2012-02-23 12:04:57 +0800

    factory: Prevent un-necessary caching in shop floor system.
    The shop floor client module should not cache remote data (HWID, VPD, ...).
    Only session data (server address, serial number) can be kept.
    
    BUG=chrome-os-partner:6911
    TEST=Manually performed factory tests: factory_Start, factory_HWID, factory_VPD
    
    Change-Id: I122092e2df71ae139f9ce92b9e2e3f0a108936ac
    Reviewed-on: https://gerrit.chromium.org/gerrit/16458
    Reviewed-by: Tammo Spalink <tammo@chromium.org>
    Tested-by: Hung-Te Lin <hungte@chromium.org>
    Commit-Ready: Hung-Te Lin <hungte@chromium.org>

--- shopfloor.py
+++ shopfloor.py
@@ -8,9 +8,13 @@
 This module provides a simple interface for all factory tests to access ChromeOS
 factory shop floor system.
 
-The test program can use set_enabled() and is_enabled() to set/query the status
-of shop floor system usage, set_server_url() to configure shop floor server
-location, then set_serial_number() to bind device, get_*() to retrieve data.
+The common flow is:
+    - Sets shop floor server URL by shopfloor.set_server_url(url).
+    - Tries shopfllor.check_serial_number(sn) until a valid value is found.
+    - Calls shopfloor.set_enabled(True) to notify other tests.
+    - Gets data by shopfloor.get_*() (ex, get_hwid()).
+    - Uploads reports by shopfloor.upload_report(blob, name).
+    - Finalize by shopfloor.finalize()
 
 For the protocol details, check:
  src/platform/factory-utils/factory_setup/shopfloor_server.
@@ -24,14 +28,14 @@ import factory_common
 from autotest_lib.client.cros import factory
 
 
-# Key names for factory state shared data
-KEY_ENABLED = "shopfloor.enabled"
-KEY_SERVER_URL = "shopfloor.server_url"
-KEY_SERIAL_NUMBER = "shopfloor.serial_number"
+# Name of the factory shared data key that maps to session info.
+KEY_SHOPFLOOR_SESSION = 'shopfloor.session'
 
-KEY_GET_HWID = "shopfloor.GetHWID"
-KEY_GET_VPD = "shopfloor.GetVPD"
-ALL_CACHED_DATA_KEYS = (KEY_GET_HWID, KEY_GET_VPD)
+# Session data will be serialized, so we're not using class/namedtuple. The
+# session is a simple dictionary with following keys:
+SESSION_SERIAL_NUMBER = 'serial_number'
+SESSION_SERVER_URL = 'server_url'
+SESSION_ENABLED = 'enabled'
 
 API_GET_HWID = 'GetHWID'
 API_GET_VPD = 'GetVPD'
@@ -42,23 +46,60 @@ _DEFAULT_SERVER_PORT = 8082
 # ----------------------------------------------------------------------------
 # Utility Functions
 
+def _fetch_current_session():
+    """Gets current shop floor session from factory states shared data.
+
+    If no session is stored yet, create a new default session.
+    """
+    if factory.has_shared_data(KEY_SHOPFLOOR_SESSION):
+        session = factory.get_shared_data(KEY_SHOPFLOOR_SESSION)
+    else:
+        session = {SESSION_SERIAL_NUMBER: None,
+                   SESSION_SERVER_URL: None,
+                   SESSION_ENABLED: False}
+        factory.set_shared_data(KEY_SHOPFLOOR_SESSION, session)
+    return session
+
+
+def _set_session(key, value):
+    """Sets shop floor session value to factory states shared data."""
+    # Currently there's no locking/transaction mechanism in factory shared_data,
+    # so there may be race-condition issue if multiple background tests try to
+    # set shop floor session data at the same time.  However since shop floor
+    # session should be singularily configured in the very beginning, let's fix
+    # this only if that really becomes an issue.
+    session = _fetch_current_session()
+    assert key in session, "Unknown session key: %s" % key
+    session[key] = value
+    factory.set_shared_data(KEY_SHOPFLOOR_SESSION, session)
+
+
+def _get_session(key):
+    """Gets shop floor session value from factory states shared data."""
+    session = _fetch_current_session()
+    assert key in session, "Unknown session key: %s" % key
+    return session[key]
+
+
+def reset():
+    """Resets session data from factory states shared data."""
+    if factory.has_shared_data(KEY_SHOPFLOOR_SESSION):
+        factory.del_shared_data(KEY_SHOPFLOOR_SESSION)
+
 
 def is_enabled():
-    """Checks if current factory is configured to use shop floor system.
-
-    Return True if shop floor is enabled, otherwise False.
-    """
-    return factory.get_shared_data(KEY_ENABLED, False)
+    """Checks if current factory is configured to use shop floor system."""
+    return _get_session(SESSION_ENABLED)
 
 
 def set_enabled(enabled):
     """Enable/disable using shop floor in current factory flow."""
-    factory.set_shared_data(KEY_ENABLED, enabled)
+    _set_session(SESSION_ENABLED, enabled)
 
 
 def set_server_url(url):
     """Sets default shop floor server URL for further calls."""
-    factory.set_shared_data(KEY_SERVER_URL, url)
+    _set_session(SESSION_SERVER_URL, url)
 
 
 def detect_default_server_url():
@@ -84,7 +125,7 @@ def get_instance(url=None):
     @return An object with all public functions from shopfloor.ShopFloorBase.
     """
     if not url:
-        url = factory.get_shared_data(KEY_SERVER_URL)
+        url = _get_session(SESSION_SERVER_URL)
     return xmlrpclib.ServerProxy(url, allow_none=True, verbose=False)
 
 
@@ -111,40 +152,12 @@ def check_server_status(instance=None):
 
 def set_serial_number(serial_number):
     """Sets a serial number as pinned in factory shared data."""
-    # TODO(hungte) Move serial number somewhere else (disk, memory) to prevent
-    # dependency on factory modules.
-    factory.set_shared_data(KEY_SERIAL_NUMBER, serial_number)
+    _set_session(SESSION_SERIAL_NUMBER, serial_number)
 
 
 def get_serial_number():
     """Gets current pinned serial number from factory shared data."""
-    return factory.get_shared_data(KEY_SERIAL_NUMBER)
-
-
-def get_data(key_name, api_name, force):
-    """Gets (and cache) a shop floor system data.
-
-    @param key_name: The key name of data to access (KEY_*).
-    @param api_name: The shop floor remote API name to query given key (API_*).
-    @param force: True to discard cache and re-fetch data from remote shop floor
-        server; False to use cache from factory shared data, if available.
-
-    @return: The data associated by key_name.
-    """
-    value = factory.get_shared_data(key_name, None)
-    if force or (value is None):
-        value = getattr(get_instance(), api_name)(get_serial_number())
-        factory.set_shared_data(key_name, value)
-    return value
-
-
-def expire_cached_data():
-    """Discards any data cached by get_data."""
-    # TODO(hungte) Drop the caching system if no partners really needs it.
-    for key in ALL_REMOTE_DATA_KEYS:
-        if not factory.has_shared_data(key):
-            continue
-        factory.del_shared_data(key)
+    return _get_session(SESSION_SERIAL_NUMBER)
 
 
 def check_serial_number(serial_number):
@@ -153,20 +166,14 @@ def check_serial_number(serial_number):
     return get_instance().GetHWID(serial_number)
 
 
-def get_hwid(force=False):
-    """Gets HWID associated with current pinned serial number.
-
-    @param force: False to use previously cached data; True to discard cache.
-    """
-    return get_data(KEY_GET_HWID, API_GET_HWID, force)
+def get_hwid():
+    """Gets HWID associated with current pinned serial number."""
+    return get_instance().GetHWID(get_serial_number())
 
 
-def get_vpd(force=False):
-    """Gets VPD associated with current pinned serial number.
-
-    @param force: False to use previously cached data; True to discard cache.
-    """
-    return get_data(KEY_GET_VPD, API_GET_VPD, force)
+def get_vpd():
+    """Gets VPD associated with current pinned serial number."""
+    return get_instance().GetVPD(get_serial_number())
 
 
 def upload_report(blob, name=None):
