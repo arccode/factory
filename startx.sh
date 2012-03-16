5deb02985a56b6c8743c5a0627007dd717f71a77
Hung-Te Lin <hungte@chromium.org>
2012-03-16 16:32:09 +0800

    factory: Refine X startup procedure to allow better board customization.
    Note board config file is explicitly renamed to prevent incompatibility.
    
    Now each board may define and override arbitrary steps (and variables) in X
    startup.
    
    BUG=chrome-os-partner:8179
    TEST=start factory # X starts correctly
    
    Change-Id: Ifd1b123466eff21cd5d5c71a870a21640f7b7ba9
    Reviewed-on: https://gerrit.chromium.org/gerrit/18351
    Reviewed-by: Jon Salz <jsalz@chromium.org>
    Commit-Ready: Hung-Te Lin <hungte@chromium.org>
    Tested-by: Hung-Te Lin <hungte@chromium.org>

--- startx.sh
+++ startx.sh
@@ -10,54 +10,81 @@ SERVER_READY=
 DISPLAY=":0"
 
 SUITE_FACTORY="$(dirname "$0")"/../../site_tests/suite_Factory
-BOARD_CONFIG="$(readlink -f "$SUITE_FACTORY")/board_config_x.sh"
+BOARD_SETUP="$(readlink -f "$SUITE_FACTORY")/board_setup_x.sh"
+
+# Default X server parameters
+X_ARG="-r -s 0 -p 0 -dpms -nolisten tcp vt01 -auth ${XAUTH_FILE}"
+
+board_pre_setup() {
+  true
+}
+
+board_post_setup() {
+  true
+}
+
+setup_xauth() {
+  MCOOKIE=$(head -c 8 /dev/urandom | openssl md5)
+  ${XAUTH} -q -f ${XAUTH_FILE} add ${DISPLAY} . ${MCOOKIE}
+}
+
+setup_cursor() {
+  # The following logic is copied from /sbin/xstart.sh to initialize the
+  # touchpad device adequately.
+  if [ -d /home/chronos -a ! -f /home/chronos/.syntp_enable ] ; then
+    # serio_rawN devices come from the udev rules for the Synaptics binary
+    # driver throwing the PS/2 interface into raw mode.  If we find any
+    # that are TP devices, put them back into normal mode to use the default
+    # driver instead.
+    for P in /sys/class/misc/serio_raw*
+    do
+      # Note: It's OK if globbing fails, since the next line will drop us out
+      udevadm info -q env -p $P | grep -q ID_INPUT_TOUCHPAD=1 || continue
+      # Rescan everything; things that don't have another driver will just
+      # restart in serio_rawN mode again, since they won't be looking for
+      # the disable in their udev rules.
+      SERIAL_DEVICE=/sys$(dirname $(dirname $(udevadm info -q path -p $P)))
+      echo -n "rescan" > ${SERIAL_DEVICE}/drvctl
+    done
+  else
+    udevadm trigger
+    udevadm settle
+  fi
+}
 
 user1_handler () {
   echo "X server ready..." 1>&2
   SERVER_READY=y
 }
 
-trap user1_handler USR1
-MCOOKIE=$(head -c 8 /dev/urandom | openssl md5)
-${XAUTH} -q -f ${XAUTH_FILE} add ${DISPLAY} . ${MCOOKIE}
+start_x_server() {
+  trap user1_handler USR1
+  /bin/sh -c "
+    trap '' USR1 TTOU TTIN
+    exec /usr/bin/X $X_ARG 2>/var/log/factory.X.log" &
 
-# The following logic is copied from /sbin/xstart.sh to initialize the
-# touchpad device adequately.
-if [ -d /home/chronos -a ! -f /home/chronos/.syntp_enable ] ; then
-  # serio_rawN devices come from the udev rules for the Synaptics binary
-  # driver throwing the PS/2 interface into raw mode.  If we find any
-  # that are TP devices, put them back into normal mode to use the default
-  # driver instead.
-  for P in /sys/class/misc/serio_raw*
-  do
-    # Note: It's OK if globbing fails, since the next line will drop us out
-    udevadm info -q env -p $P | grep -q ID_INPUT_TOUCHPAD=1 || continue
-    # Rescan everything; things that don't have another driver will just
-    # restart in serio_rawN mode again, since they won't be looking for
-    # the disable in their udev rules.
-    SERIAL_DEVICE=/sys$(dirname $(dirname $(udevadm info -q path -p $P)))
-    echo -n "rescan" > ${SERIAL_DEVICE}/drvctl
+  while [ -z ${SERVER_READY} ]; do
+    sleep .1
   done
-else
-  udevadm trigger
-  udevadm settle
+}
+
+# Load board-specific parameters, and override any startup procedures.
+if [ -s $BOARD_SETUP ]; then
+  echo "Loading board-specific X parameters..." 1>&2
+  . $BOARD_SETUP
 fi
 
-/bin/sh -c "\
-trap '' USR1 TTOU TTIN
-exec /usr/bin/X -nolisten tcp vt01 -auth ${XAUTH_FILE} \
--r -s 0 -p 0 -dpms 2> /var/log/factory.X.log" &
+board_pre_setup
 
-while [ -z ${SERVER_READY} ]; do
-  sleep .1
-done
+setup_xauth
+setup_cursor
+
+start_x_server
 
 export DISPLAY=${DISPLAY}
 export XAUTHORITY=${XAUTH_FILE}
 
-if [ -x $BOARD_CONFIG ]; then
-  $BOARD_CONFIG
-fi
+board_post_setup
 
 /sbin/initctl emit factory-ui-started
 cat /proc/uptime > /tmp/uptime-x-started
