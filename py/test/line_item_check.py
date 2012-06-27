ae995c337228872240b78d1e0573052082c6f4cf
Jon Salz <jsalz@chromium.org>
2012-06-27 17:06:42 +0800

    Fix another broken import.
    BUG=None
    TEST=Manual
    
    Change-Id: Ia440a7ac9f129f57e315330b0efeb47365a18189
    Reviewed-on: https://gerrit.chromium.org/gerrit/26299
    Commit-Ready: Jon Salz <jsalz@chromium.org>
    Reviewed-by: Jon Salz <jsalz@chromium.org>
    Tested-by: Jon Salz <jsalz@chromium.org>

--- py/test/line_item_check.py
+++ py/test/line_item_check.py
@@ -15,8 +15,8 @@ from autotest_lib.client.bin import test
 from cros.factory.test import factory
 from autotest_lib.client.common_lib import error
 from autotest_lib.client.common_lib import utils
-from cros.factory import task
-from cros.factory import ui as ful
+from cros.factory.test import task
+from cros.factory.test import ui as ful
 
 
 def _make_decision_widget(message, key_action_mapping):
