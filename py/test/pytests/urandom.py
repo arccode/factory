2f4fa18ec79965ffed5c82dd7f9a93a6d3399968
Shuo-Peng Liao <deanliao@chromium.org>
2012-07-19 15:27:02 +0800

    Add CPU stress test by generating random numbers for a period of time.
    BUG=chrome-os-partner:9743
    TEST=Manual
    
    Change-Id: I35a6e169a138ad06fb1a8ade904123625730f56c
    Reviewed-on: https://gerrit.chromium.org/gerrit/27869
    Tested-by: Shuo-Peng Liao <deanliao@chromium.org>
    Reviewed-by: Jon Salz <jsalz@chromium.org>
    Commit-Ready: Shuo-Peng Liao <deanliao@chromium.org>

---
+++ py/test/pytests/urandom.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+'''A factory test to stress CPU.
+
+It stresses CPU by generating random number using /dev/urandom for a specified
+period of time.
+
+Test parameter:
+  duration_secs: Number of seconds to stress CPU.
+'''
+
+import time
+import unittest
+from cros.factory.test import factory
+
+class UrandomTest(unittest.TestCase):
+  def runTest(self):
+    duration_secs = self.test_info.args['duration_secs']
+    factory.console.info('Getting /dev/urandom for %d seconds', duration_secs)
+
+    with open('/dev/urandom') as f:
+      end_time = time.time() + duration_secs
+      while time.time() <= end_time:
+        data = f.read(1024*1024)
+        self.assertTrue(data, '/dev/urandom returns nothing!')
