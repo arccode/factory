683b74e2547e1152fc9d74aa9addaa5692efbb4e
Tai-Hsu Lin <sheckylin@chromium.org>
2012-07-25 11:15:51 +0800

    factory: Add a function to identify the working Ethernet interface
    The CL adds a function to identify the true Ethernet interfaces as
    opposed to the false ones brought up by 4G modules. The function
    does not uses flimflam and therefore is safe to run anytime during
    the factory flow without interfering with the running tests.
    
    BUG=chrome-os-partner:10758
    TEST=Tested on real devices -> works.
    
    Change-Id: I874f4b88933aabe189a8163110d3f4d74ac6f591
    Reviewed-on: https://gerrit.chromium.org/gerrit/28354
    Reviewed-by: Jon Salz <jsalz@chromium.org>
    Tested-by: Tai-Hsu Lin <sheckylin@chromium.org>
    Commit-Ready: Tai-Hsu Lin <sheckylin@chromium.org>

--- py/utils/net_utils.py
+++ py/utils/net_utils.py
@@ -4,9 +4,12 @@
 
 """Networking-related utilities."""
 
+import glob
 import httplib
+import os
+import subprocess
+import time
 import xmlrpclib
-
 
 DEFAULT_TIMEOUT = 10
 
@@ -39,3 +42,35 @@ class TimeoutXMLRPCServerProxy(xmlrpclib.ServerProxy):
       kwargs['transport'] = TimeoutXMLRPCTransport(
         timeout=timeout)
     xmlrpclib.ServerProxy.__init__(self, uri, *args, **kwargs)
+
+def FindUsableEthDevice():
+  """Find the real ethernet interface when the flimflam is unavailable.
+
+  Some devices with 4G modules may bring up fake eth interfaces during
+  the factory flow. Flimflam is often used to tell the real interface type
+  in the case. Unfortunately, we may sometimes need to turn it off to
+  perform tests on network components. We thus need another way to reliably
+  distinguish the real interface type.
+  """
+  good_eth = None
+  last_level = 0
+  candidates = glob.glob('/sys/class/net/eth*')
+  for path in candidates:
+    dev = os.path.basename(path)
+    p = subprocess.Popen('ethtool %s' % dev, shell=True,
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stat = p.communicate()[0]
+
+    # A 4G introduced ethernet interface would not be able to report its
+    # setting data because it won't get online during the factory flow.
+    # In case that there are several real ethernet interfaces available,
+    # we favor the one that has the cable connected end-to-end.
+    current_level = 0
+    if 'Supported ports:' in stat:
+      current_level = 1
+      if 'Link detected: yes' in stat:
+        current_level = 2
+    if current_level > last_level:
+      good_eth = dev
+      last_level = current_level
+  return good_eth
