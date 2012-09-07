48e8ad2504946cb878c043e0b7b193ce1284359d
Chun-Ta Lin <itspeter@google.com>
2012-09-07 20:30:04 +0800

    add a video playback test.
    Add a video playback test based on
    https://gerrit.chromium.org/gerrit/#/c/26312/1 .
    This test exposes options to assign file to play, loop, control ui, and
    time limitation.
    
    BUG=chrome-os-partner:9143
    TEST=make lint test, manually on DUT.
    
    Change-Id: I0a38d3b28c2ac327d4628e52853521c67bfa9f96
    Reviewed-on: https://gerrit.chromium.org/gerrit/32535
    Reviewed-by: Chinyue Chen <chinyue@chromium.org>
    Commit-Ready: Chun-Ta Lin <itspeter@google.com>
    Tested-by: Chun-Ta Lin <itspeter@google.com>

---
+++ py/test/pytests/video_playback.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import unittest
+
+from cros.factory.test import factory
+from cros.factory.test import test_ui
+from cros.factory.test.args import Arg
+
+DEFAULT_SECONDS = 10
+
+
+class VideoPlaybackTest(unittest.TestCase):
+  '''Video Playback Test.'''
+  ARGS = [
+    Arg('video_file', str,
+        'Relative path to load the video.',
+        default='', optional=True),
+    Arg('loop', bool,
+        'Whether we want to loop the video.',
+        default=False, optional=True),
+    Arg('time_limit', int,
+        'Seconds to force terminate the test.',
+        default=DEFAULT_SECONDS, optional=True),
+    Arg('show_controls', bool,
+        'Whether we want to show the control UI.',
+        default=False, optional=True),
+  ]
+
+  def runTest(self):
+    factory.console.info('Video Playback test started')
+    factory.console.info('video_file=[%s]' % self.args.video_file)
+    factory.console.info('time_limit=%s secs' % self.args.time_limit)
+    ui = test_ui.UI()
+    ui.CallJSFunction('init',
+                      self.args.video_file,
+                      self.args.loop,
+                      self.args.time_limit,
+                      self.args.show_controls)
+    ui.Run()
+    factory.console.info('Video Playback test finished')
---
+++ py/test/pytests/video_playback_static/video_playback.js
@@ -0,0 +1,18 @@
+// Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function finish() {
+    window.test.pass();
+}
+
+function init(video_file, loop, time_limit, control_ui) {
+    video_tag = document.getElementById('v');
+    video_tag.src = video_file;
+    video_tag.loop = loop;
+    video_tag.controls = control_ui;
+    if (time_limit > 0) {
+        setTimeout(finish, time_limit*1000);
+    }
+    video_tag.play();
+}
