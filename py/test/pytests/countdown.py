eb47ad28d02b3e8d9671631a0ea5bbba89a7e219
Chinyue Chen <chinyue@chromium.org>
2012-09-12 12:45:35 +0800

    Detect abnormal status.
    Detect system abnormal status and log alarm messages.
    
    BUG=chrome-os-partner:13116
    TEST=Manually on Link.
    
    Change-Id: Ifb06759eed2b3f9959acb4a64b762c6e0618b6f3
    Reviewed-on: https://gerrit.chromium.org/gerrit/33054
    Reviewed-by: Ricky Liang <jcliang@chromium.org>
    Commit-Ready: Chinyue Chen <chinyue@chromium.org>
    Tested-by: Chinyue Chen <chinyue@chromium.org>

--- py/test/pytests/countdown.py
+++ py/test/pytests/countdown.py
@@ -24,6 +24,10 @@ from cros.factory.test import factory, test_ui
 from cros.factory.test.args import Arg
 
 
+_DEFAULT_GRACE_SECS = 120
+_DEFAULT_TEMP_MAX_DELTA = 10
+
+
 class CountDownTest(unittest.TestCase):
   def FormatSeconds(self, secs):
     hours = int(secs / 3600)
@@ -42,6 +46,55 @@ class CountDownTest(unittest.TestCase):
         ' '.join(open('/proc/loadavg').read().split()[0:3]),
         id='system-load')
 
+  def SetupStatusDetection(self):
+    self._grace_secs = self.args.detection_params.get(
+        'grace_secs', _DEFAULT_GRACE_SECS)
+    self._temp_max_delta = self.args.detection_params.get(
+        'temp_max_delta', _DEFAULT_TEMP_MAX_DELTA)
+    self._temp_criteria = self.args.detection_params.get(
+        'temp_criteria', [])
+    self._relative_temp_criteria = self.args.detection_params.get(
+        'relative_temp_criteria', [])
+    self._fan_min_expected_rpm = self.args.detection_params.get(
+        'fan_min_expected_rpm')
+
+  def DetectAbnormalStatus(self, status, last_status):
+    # Start abnormal status detection after run-in for a while.
+    if self._elapsed_secs < self._grace_secs:
+      return
+
+    for index, (current, last) in enumerate(
+        zip(status.temperatures, last_status.temperatures)):
+      if abs(current - last) > self._temp_max_delta:
+        factory.console.warn(
+            'ALARM! Temperature index %d delta over %d (current: %d, last: %d)',
+            index, self._temp_max_delta, current, last)
+
+    for name, index, warning_temp, critical_temp in self._temp_criteria:
+      temp = status.temperatures[index]
+      if warning_temp < temp < critical_temp:
+        factory.console.warn(
+            'ALARM! %s over warning temperature (now: %d, warning: %d)',
+            name, temp, warning_temp)
+      if temp >= critical_temp:
+        factory.console.warn(
+            'ALARM! %s over critical temperature (now: %d, critical: %d)',
+            name, temp, critical_temp)
+
+    for (relation, first_index, second_index,
+        max_diff) in self._relative_temp_criteria:
+      first_temp = status.temperatures[first_index]
+      second_temp = status.temperatures[second_index]
+      if abs(first_temp - second_temp) > max_diff:
+        factory.console.warn(
+            'ALARM! Temperature between %s over %d (first: %d, second: %d)',
+            relation, max_diff, first_temp, second_temp)
+
+    if (self._fan_min_expected_rpm and
+        status.fan_rpm < self._fan_min_expected_rpm):
+      factory.console.warn('ALARM! Fan rpm %d less than min expected %d',
+          status.fan_rpm, self._fan_min_expected_rpm)
+
   ARGS = [
     Arg('title_en', (str, unicode), 'English title.', 'Countdown'),
     Arg('title_zh', (str, unicode), 'Chinese title.', u'倒數計時'),
@@ -50,7 +103,9 @@ class CountDownTest(unittest.TestCase):
         'countdown info.', False),
     Arg('duration_secs', int, 'Duration of time to countdown.'),
     Arg('log_interval', int,
-        'Interval of time in seconds to log system status.', 10)
+        'Interval of time in seconds to log system status.', 10),
+    Arg('detection_params', dict,
+        'Board-specific params for detecting abnormal status', {})
   ]
 
   def runTest(self):
@@ -72,6 +127,8 @@ class CountDownTest(unittest.TestCase):
     self._remaining_secs = self.args.duration_secs
     self._next_log_time = 0
     self._event_log = EventLog.ForAutoTest()
+    last_status = SystemStatus()
+    self.SetupStatusDetection()
 
     # Loop until count-down ends.
     while self._remaining_secs >= 0:
@@ -85,6 +142,8 @@ class CountDownTest(unittest.TestCase):
             datetime.datetime.now().isoformat(),
             self._elapsed_secs,
             sys_status.__dict__))
+        self.DetectAbnormalStatus(sys_status, last_status)
+        last_status = sys_status
         self._next_log_time = time.time() + self.args.log_interval
 
       time.sleep(1)
