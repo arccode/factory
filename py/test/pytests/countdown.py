35bd27a616bfb6a966b1f0e226f0ff15841672b9
Chinyue Chen <chinyue@chromium.org>
2012-09-18 18:29:44 +0800

    Countdown: Move abnormal detection args to be top-level args.
    BUG=None
    TEST=Manually on Link.
    
    Change-Id: If50ff81c11c1c42f7a3c4957c367179b6f6b87c6
    Reviewed-on: https://gerrit.chromium.org/gerrit/33518
    Tested-by: Chinyue Chen <chinyue@chromium.org>
    Reviewed-by: Jon Salz <jsalz@chromium.org>
    Commit-Ready: Chinyue Chen <chinyue@chromium.org>

--- py/test/pytests/countdown.py
+++ py/test/pytests/countdown.py
@@ -5,13 +5,8 @@
 
 '''A count down UI for run-in test.
 
-It only shows count down and system loads for run-in period.
-
-Test parameter:
-  duration_secs: Number of seconds to count down.
-  log_interval (default: 10): Interval of time in seconds to log system status.
-  position_top_right (default: False): If True, the countdown window will
-      be placed in the top-right corner. Otherwise, it is center aligned.
+It shows count down and system loads for run-in period. It also alarms if
+there's any abnormal status detected during run-in.
 '''
 
 import datetime
@@ -22,10 +17,6 @@ from cros.factory.event_log import EventLog
 from cros.factory.system import SystemStatus
 from cros.factory.test import factory, test_ui
 from cros.factory.test.args import Arg
-
-
-_DEFAULT_GRACE_SECS = 120
-_DEFAULT_TEMP_MAX_DELTA = 10
 
 
 class CountDownTest(unittest.TestCase):
@@ -46,79 +37,83 @@ class CountDownTest(unittest.TestCase):
         ' '.join(open('/proc/loadavg').read().split()[0:3]),
         id='system-load')
 
-  def SetupStatusDetection(self):
-    # TODO(chinyue): Fix in CL:33518
-    # pylint: disable=W0201
-    self._grace_secs = self.args.detection_params.get(
-        'grace_secs', _DEFAULT_GRACE_SECS)
-    self._temp_max_delta = self.args.detection_params.get(
-        'temp_max_delta', _DEFAULT_TEMP_MAX_DELTA)
-    self._temp_criteria = self.args.detection_params.get(
-        'temp_criteria', [])
-    self._relative_temp_criteria = self.args.detection_params.get(
-        'relative_temp_criteria', [])
-    self._fan_min_expected_rpm = self.args.detection_params.get(
-        'fan_min_expected_rpm')
-
   def DetectAbnormalStatus(self, status, last_status):
-    # Start abnormal status detection after run-in for a while.
-    if self._elapsed_secs < self._grace_secs:
-      return
+    warnings = []
 
-    for index, (current, last) in enumerate(
-        zip(status.temperatures, last_status.temperatures)):
+    if self.args.temp_max_delta:
+      for index, (current, last) in enumerate(
+          zip(status.temperatures, last_status.temperatures)):
+        # Ignore the case when both are None since it could just mean the
+        # sensor doesn't exist. If only one of them is None, then there
+        # is a problem.
+        if last is None and current is None:
+          continue
+        if last is None or current is None:
+          warnings.append(
+              'Cannot read temperature index %d (current: %r, last: %r)' %
+              (index, current, last))
+        if abs(current - last) > self.args.temp_max_delta:
+          warnings.append(
+              'Temperature index %d delta over %d (current: %d, last: %d)' %
+              (index, self.args.temp_max_delta, current, last))
 
-      # Ignore the case when both are None since it could just mean the
-      # sensor doesn't exist. If only one of them is None, then there
-      # is a problem.
-      if last is None and current is None:
-        continue
-      if last is None or current is None:
-        self.fail(
-            'Cannot read temperature index %d (current: %d, last: %d)' %
-            (index, current, last))
-
-      if abs(current - last) > self._temp_max_delta:
-        factory.console.warn(
-            'ALARM! Temperature index %d delta over %d (current: %d, last: %d)',
-            index, self._temp_max_delta, current, last)
-
-    for name, index, warning_temp, critical_temp in self._temp_criteria:
+    for name, index, warning_temp, critical_temp in self.args.temp_criteria:
       temp = status.temperatures[index]
       if warning_temp < temp < critical_temp:
-        factory.console.warn(
-            'ALARM! %s over warning temperature (now: %d, warning: %d)',
-            name, temp, warning_temp)
+        warnings.append('%s over warning temperature (now: %d, warning: %d)' %
+                        (name, temp, warning_temp))
       if temp >= critical_temp:
-        factory.console.warn(
-            'ALARM! %s over critical temperature (now: %d, critical: %d)',
-            name, temp, critical_temp)
+        warnings.append('%s over critical temperature (now: %d, critical: %d)' %
+                        (name, temp, critical_temp))
 
     for (relation, first_index, second_index,
-        max_diff) in self._relative_temp_criteria:
+        max_diff) in self.args.relative_temp_criteria:
       first_temp = status.temperatures[first_index]
       second_temp = status.temperatures[second_index]
       if abs(first_temp - second_temp) > max_diff:
-        factory.console.warn(
-            'ALARM! Temperature between %s over %d (first: %d, second: %d)',
-            relation, max_diff, first_temp, second_temp)
+        warnings.append('Temperature difference between %s over %d '
+                        '(first: %d, second: %d)' %
+                        (relation, max_diff, first_temp, second_temp))
 
-    if (self._fan_min_expected_rpm and
-        status.fan_rpm < self._fan_min_expected_rpm):
-      factory.console.warn('ALARM! Fan rpm %d less than min expected %d',
-          status.fan_rpm, self._fan_min_expected_rpm)
+    if (self.args.fan_min_expected_rpm and
+        status.fan_rpm < self.args.fan_min_expected_rpm):
+      warnings.append('Fan rpm %d less than min expected %d' %
+                      (status.fan_rpm, self.args.fan_min_expected_rpm))
+
+    in_grace_period = self._elapsed_secs < self.args.grace_secs
+    if warnings:
+      self._event_log.Log('warnings', elapsed_secs=self._elapsed_secs,
+                          in_grace_period=in_grace_period, warnings=warnings)
+      if not in_grace_period:
+        for w in warnings:
+          factory.console.warn(w)
 
   ARGS = [
     Arg('title_en', (str, unicode), 'English title.', 'Countdown'),
     Arg('title_zh', (str, unicode), 'Chinese title.', u'倒數計時'),
     Arg('position_top_right', bool,
-        'A workaround for some machines on which graphics test would overlay'
+        'A workaround for some machines on which graphics test would overlay '
         'countdown info.', False),
     Arg('duration_secs', int, 'Duration of time to countdown.'),
     Arg('log_interval', int,
         'Interval of time in seconds to log system status.', 10),
-    Arg('detection_params', dict,
-        'Board-specific params for detecting abnormal status', {})
+    Arg('grace_secs', int,
+        'Grace period before starting abnormal status detection.', 120,
+        optional=True),
+    Arg('temp_max_delta', int,
+        'Allowed difference between current and last temperature of a sensor.',
+        None, optional=True),
+    Arg('temp_criteria', (list, tuple),
+        'A list of rules to check that temperature is under the given range, '
+        'rule format: (name, temp_index, warning_temp, critical_temp)', [],
+        optional=True),
+    Arg('relative_temp_criteria', (list, tuple),
+        'A list of rules to check the difference between two temp sensors, '
+        'rule format: (relation, first_index, second_index, max_diff). '
+        'relation is a text output with warning messages to describe the two '
+        'temp sensors in the rule', [], optional=True),
+    Arg('fan_min_expected_rpm', int, 'Minimum fan rpm expected', None,
+        optional=True),
   ]
 
   def runTest(self):
@@ -141,7 +136,6 @@ class CountDownTest(unittest.TestCase):
     self._next_log_time = 0
     self._event_log = EventLog.ForAutoTest()
     last_status = SystemStatus()
-    self.SetupStatusDetection()
 
     # Loop until count-down ends.
     while self._remaining_secs >= 0:
