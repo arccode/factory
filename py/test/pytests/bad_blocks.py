9407befc1f833950ef6d41ebdb265f7d27753bde
Jon Salz <jsalz@chromium.org>
2012-10-18 14:15:10 +0800

    Fix bug that was causing certain assertions not to run.
    BUG=chrome-os-partner:15400
    TEST=Manual
    
    Change-Id: Idee2fb835c6824196709e6612b5615813b49f1d9
    Reviewed-on: https://gerrit.chromium.org/gerrit/35932
    Reviewed-by: Jon Salz <jsalz@chromium.org>
    Tested-by: Jon Salz <jsalz@chromium.org>
    (cherry picked from commit 686de48e5989e7c0cae68b60ef1b290aca55c924)
    Reviewed-on: https://gerrit.chromium.org/gerrit/38526
    Commit-Ready: Jon Salz <jsalz@chromium.org>

--- py/test/pytests/bad_blocks.py
+++ py/test/pytests/bad_blocks.py
@@ -191,7 +191,7 @@ class BadBlocksTest(unittest.TestCase):
           lines.append(line)
 
         if ch == '':
-          return
+          break
         buf = []
       else:
         buf.append(ch)
