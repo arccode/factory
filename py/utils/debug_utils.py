e785c5b730b333a06146f6d5fc734fcaf9133b50
Jon Salz <jsalz@chromium.org>
2012-10-25 15:54:04 +0800

    Dump all threads' stack traces to stderr on SIGUSR2.
    BUG=chrome-os-partner:15647
    TEST=Manual
    
    Change-Id: Ia3456ac65187b32daba8167a8ed1dd32f31d558f
    Reviewed-on: https://gerrit.chromium.org/gerrit/36542
    Reviewed-by: Shuo-Peng Liao <deanliao@chromium.org>
    Reviewed-by: Jon Salz <jsalz@chromium.org>
    Tested-by: Jon Salz <jsalz@chromium.org>
    (cherry picked from commit 18a24cc93735eba972ed54f0e5d143292598c228)
    Reviewed-on: https://gerrit.chromium.org/gerrit/38539
    Commit-Ready: Jon Salz <jsalz@chromium.org>

--- py/utils/debug_utils.py
+++ py/utils/debug_utils.py
@@ -11,17 +11,28 @@ import traceback
 def AddDebugHook():
   '''Adds a signal handler for USR2 to print debug information on SIGUSR2.
 
-  Currently just prints the stack trace of the active thread to stderr.
+  Currently prints all threads' stack traces to stderr.
   '''
-  def Handler(dummy_sig, frame):
+  def Handler(dummy_sig, dummy_frame):
     sys.stderr.write(
         '*****\n'
         '*\n'
         '* Caught SIGUSR2.  Dumping debug information.\n'
         '*\n'
-        '*****\n'
-        'Stack trace of active thread:\n')
-    traceback.print_stack(frame)
-    print >> sys.stderr, '***** End of debug information.'
+        '*****\n')
+
+    # pylint: disable=W0212
+    for thread_id, stack in sys._current_frames().items():
+      sys.stderr.write('Thread ID %s:' % thread_id)
+      for filename, line_no, function_name, text in (
+          traceback.extract_stack(stack)):
+        # Same format as the usual Python stack trace, but indented
+        # twice
+        sys.stderr.write('  File: "%s", line %d, in %s\n' % (
+            filename, line_no, function_name))
+        if text:
+          sys.stderr.write('    %s\n' % text.strip())
+
+    sys.stderr.write('***** End of debug information.\n')
 
   signal.signal(signal.SIGUSR2, Handler)
