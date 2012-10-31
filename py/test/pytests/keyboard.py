151fa0e2b1b8712823fb93fe3c2b74a1609c69c2
Ricky Liang <jcliang@chromium.org>
2012-10-31 11:27:57 +0800

    keyboard: Add Chrome-based keyboard test.
    BUG=chrome-os-partner:15798
    TEST=manually test on my DUT
    
    Change-Id: I6a6e2c535e218ef36fdac9334d342d0da9e39afc
    Reviewed-on: https://gerrit.chromium.org/gerrit/37004
    Reviewed-by: Shuo-Peng Liao <deanliao@chromium.org>
    Commit-Ready: Ricky Liang <jcliang@chromium.org>
    Tested-by: Ricky Liang <jcliang@chromium.org>

---
+++ py/test/pytests/keyboard.py
@@ -0,0 +1,160 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""
+A factory test to test the functionality of keyboard.
+
+dargs:
+  layout: Use specified layout other than derived from VPD. (default: get from
+      VPD)
+  keyboard_device_name: Device name of keyboard. (default: 'AT Translated Set 2
+      keyboard')
+  keyboard_event_id: Keyboard input event id. (default: 6)
+  timeout_secs: Timeout for the test. (default: 30 seconds)
+  key_order_list: (optional) A list of keycodes that need to be pressed
+      sequentially to pass the test.
+"""
+
+import os
+import re
+import subprocess
+import time
+import unittest
+
+from cros.factory.test import test_ui
+from cros.factory.test.args import Arg
+from cros.factory.test.ui_templates import OneSection
+from cros.factory.test.utils import StartDaemonThread
+from cros.factory.utils.process_utils import Spawn
+
+_MSG_TIME_REMAINING = lambda t: test_ui.MakeLabel(
+    'Time remaining: %d' % t, u'剩余时间：%d' % t, 'keyboard-test-info')
+
+_RE_EVTEST_EVENT = re.compile(
+    r'^Event: time .*?, type .*? \((.*?)\), code (.*?) \(.*?\), value (.*?)$')
+
+_ID_IMAGE = 'keyboard-test-image'
+_ID_COUNTDOWN_TIMER = 'keyboard-test-timer'
+_HTML_KEYBOARD = (
+    '<div id="%s" style="position: relative"></div>\n<div id="%s"></div>\n' %
+        (_ID_IMAGE, _ID_COUNTDOWN_TIMER))
+
+_KEYBOARD_TEST_DEFAULT_CSS = (
+    '.keyboard-test-info { font-size: 2em; }\n'
+    '.keyboard-test-key-untested { display: none; }\n'
+    '.keyboard-test-keydown { background-color: yellow; opacity: 0.5; }\n'
+    '.keyboard-test-keyup { background-color: green; opacity: 0.5; }\n')
+
+
+class KeyboardTest(unittest.TestCase):
+  """Tests if all the keys on a keyboard are functioning. The test checks for
+  keydown and keyup events for each key, following certain order if required,
+  and passes if both events of all keys are received."""
+  ARGS = [
+    Arg('layout', (str, unicode), 'Use specified layout other than derived '
+        'from VPD.', default=None, optional=True),
+    Arg('keyboard_device_name', (str, unicode), 'Device name of keyboard.',
+        default='AT Translated Set 2 keyboard'),
+    Arg('keyboard_event_id', int, 'Keyboard input event id.',
+        default=6),
+    Arg('timeout_secs', int, 'Timeout for the test.', default=30),
+    Arg('key_order_list', list, 'A list of keycodes that need to be pressed '
+        'sequentially to pass the test.', default=None, optional=True)
+  ]
+
+  def setUp(self):
+    self.ui = test_ui.UI()
+    self.template = OneSection(self.ui)
+    self.ui.AppendCSS(_KEYBOARD_TEST_DEFAULT_CSS)
+
+    # Initialize keyboard layout and bindings
+    self.layout = self.GetKeyboardLayout()
+    self.bindings = self.ReadBindings(self.layout)
+
+    # Initialize frontend presentation
+    self.template.SetState(_HTML_KEYBOARD)
+    self.ui.CallJSFunction('setUpKeyboardTest', self.layout, self.bindings,
+                           _ID_IMAGE, self.args.key_order_list)
+
+    self.monitor_process = None
+    self.EnableXKeyboard(False)
+    StartDaemonThread(target=self.MonitorEvtest)
+    StartDaemonThread(target=self.CountdownTimer)
+
+  def tearDown(self):
+    """Terminates the running process or we'll have trouble stopping the
+    test."""
+    self.TerminateProcess()
+    self.EnableXKeyboard(True)
+
+  def GetKeyboardLayout(self):
+    """Uses the given keyboard layout or auto-detect from VPD."""
+    if self.args.layout:
+      return self.args.layout
+    vpd_layout = Spawn(['vpd', '-g', 'initial_locale'],
+                       check_output=True).stdout_data.strip()
+    return vpd_layout if vpd_layout else 'en-US'
+
+  def ReadBindings(self, layout):
+    """Reads in key bindings and their associates figure regions."""
+    bindings = None
+    base = os.path.splitext(os.path.realpath(__file__))[0] + '_static'
+    bindings_filename = os.path.join(base, layout + '.bindings')
+    with open(bindings_filename, 'r') as f:
+      bindings = eval(f.read())
+    for k in bindings:
+      # Convert single tuple to list of tuples
+      if not isinstance(bindings[k], list):
+        bindings[k] = [bindings[k],]
+    return bindings
+
+  def EnableXKeyboard(self, enable):
+    """Enables/Disables keyboard at the X server."""
+    Spawn(['xinput', 'set-prop', self.args.keyboard_device_name,
+           'Device Enabled', '1' if enable else '0'], check_call=True)
+
+  def MonitorEvtest(self):
+    """Monitors keyboard events from output of evtest."""
+    self.monitor_process = Spawn(['evtest', '/dev/input/event%d' % (
+                                  self.args.keyboard_event_id)],
+                                 stdout=subprocess.PIPE)
+    while True:
+      re_obj = _RE_EVTEST_EVENT.search(self.monitor_process.stdout.readline())
+      if re_obj:
+        ev_type, ev_code, value = re_obj.group(1, 2, 3)
+        if ev_type == 'EV_KEY' and value == '1':
+          self.MarkKeydown(int(ev_code))
+        elif ev_type == 'EV_KEY' and value == '0':
+          self.MarkKeyup(int(ev_code))
+
+  def CountdownTimer(self):
+    """Starts a countdown timer and fails the test if timer reaches zero."""
+    time_remaining = self.args.timeout_secs
+    while time_remaining > 0:
+      self.ui.SetHTML(_MSG_TIME_REMAINING(time_remaining),
+                      id=_ID_COUNTDOWN_TIMER)
+      time.sleep(1)
+      time_remaining -= 1
+    self.TerminateProcess()
+    self.ui.CallJSFunction('failTest')
+
+  def TerminateProcess(self):
+    if self.monitor_process.poll() is None:
+      self.monitor_process.terminate()
+
+  def MarkKeydown(self, keycode):
+    """Calls Javascript to mark the given keycode as keydown."""
+    if not keycode in self.bindings:
+      return True
+    self.ui.CallJSFunction('markKeydown', keycode)
+
+  def MarkKeyup(self, keycode):
+    """Calls Javascript to mark the given keycode as keyup."""
+    if not keycode in self.bindings:
+      return True
+    self.ui.CallJSFunction('markKeyup', keycode)
+
+  def runTest(self):
+    self.ui.Run()
---
+++ py/test/pytests/keyboard_static/keyboard.js
@@ -0,0 +1,207 @@
+// Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * API for keyboard test.
+ * @constructor
+ * @param {string} layout
+ * @param {Object} bindings
+ * @param {string} container
+ * @param {Array} keyOrderList
+ */
+keyboardTest = function(layout, bindings, container, keyOrderList) {
+  this.layout = layout;
+  this.bindings = bindings;
+  this.container = container;
+  this.keyOrderList = keyOrderList;
+};
+
+/**
+ * Initializes keyboard layout image and div elements.
+ */
+keyboardTest.prototype.init = function() {
+  var img = new Image();
+  var container = this.container;
+  var bindings = this.bindings;
+  img.id = "layout-image";
+  img.onload = function () {
+    var xOffset = ($(container).clientWidth - this.width) / 2;
+    for (keycode in bindings) {
+      for (var i = 0; i < bindings[keycode].length; ++i) {
+        var div = document.createElement("div");
+        div.id = "keycode-" + keycode + "-" + i;
+        div.style.position = "absolute";
+        div.style.left = xOffset + bindings[keycode][i][0];
+        div.style.top = bindings[keycode][i][1];
+        div.style.width = bindings[keycode][i][2];
+        div.style.height = bindings[keycode][i][3];
+        div.style.zIndex = 2;
+        div.className = "keyboard-test-key-untested";
+        $(container).appendChild(div)
+      }
+    }
+  }
+  img.src = this.layout + ".png";
+  $(container).appendChild(img);
+};
+
+/**
+ * Marks the given keycode as "keydown" on the test ui. There may be multiple
+ * div tags mapping to the same keycode, so we have to iterate through all the
+ * div elements.
+ * @param {int} keycode
+ */
+keyboardTest.prototype.markKeydown = function(keycode) {
+  var divs = this.getClassArray("keyboard-test-key-untested");
+  if (!divs.length) {
+    return;
+  }
+  divs.forEach(function(element) {
+      if (!element)
+        return;
+      if (this.matchKeycode(element.id, keycode)) {
+        element.className = "keyboard-test-keydown";
+      }
+    }, this);
+};
+
+/**
+ * Marks the given keycode as "keyup" on the test ui. There may be multiple
+ * div tags mapping to the same keycode, so we have to iterate through all the
+ * div elements.
+ * @param {int} keycode
+ */
+keyboardTest.prototype.markKeyup = function(keycode) {
+  var divs = this.getClassArray("keyboard-test-keydown");
+  if (!divs.length) {
+    return;
+  }
+  if (this.keyOrderList && this.keyOrderList.indexOf(keycode) != -1) {
+    // Checks if the key has been pressed following the given order.
+    var index = this.keyOrderList.indexOf(keycode);
+    if (index > 0) {
+      var untested = this.getClassArray("keyboard-test-key-untested");
+      for (var i = 0; i < untested.length; ++i) {
+        if (this.matchKeycode(untested[i].id, this.keyOrderList[index - 1])) {
+          // Resets the test if the previous key is untested.
+          this.resetTest();
+          return;
+        }
+      }
+    }
+  }
+  divs.forEach(function(element) {
+      if (!element)
+        return;
+      if (this.matchKeycode(element.id, keycode)) {
+        element.className = "keyboard-test-keyup";
+      }
+    }, this);
+  this.checkTestComplete();
+};
+
+/**
+ * Resets the test by settings all keys to untested
+ */
+keyboardTest.prototype.resetTest = function() {
+  function reset(element) {
+    element.className = "keyboard-test-key-untested";
+  }
+  this.getClassArray("keyboard-test-keydown").forEach(reset);
+  this.getClassArray("keyboard-test-keyup").forEach(reset);
+};
+
+/**
+ * Checks if test is completed by checking the number of keys haven't passed
+ * the test.
+ */
+keyboardTest.prototype.checkTestComplete = function() {
+  if (this.getClassArray("keyboard-test-key-untested").length == 0 &&
+      this.getClassArray("keyboard-test-key-keydown").length == 0) {
+    window.test.pass();
+  }
+};
+
+/**
+ * Fails the test and prints out all the failed keys.
+ */
+keyboardTest.prototype.failTest = function() {
+  var failedKeys = new Array();
+
+  function getKeyCode(divId) {
+    codeLength = divId.lastIndexOf("-") - divId.indexOf("-") - 1;
+    return parseInt(divId.substr(divId.indexOf("-") + 1, codeLength));
+  }
+
+  this.getClassArray("keyboard-test-key-untested").forEach(function(element) {
+    failedKeys.push(getKeyCode(element.id));
+  });
+  this.getClassArray("keyboard-test-keydown").forEach(function(element) {
+    failedKeys.push(getKeyCode(element.id));
+  });
+
+  this.failMsg = "Keyboard test failed. Malfunction keys:";
+  failedKeys.forEach(function(element, index, array) {
+      this.failMsg += " " + element;
+      if (index != array.length -1) {
+        this.failMsg += ",";
+      }
+    }, this);
+  window.test.fail(this.failMsg);
+};
+
+/**
+ * Checks if the given id matches the given keycode.
+ * @param {string} id
+ * @param {int} keycode
+ * @return boolean
+ */
+keyboardTest.prototype.matchKeycode = function(id, keycode) {
+  return id.indexOf("keycode-" + keycode + "-") == 0;
+};
+
+/**
+ * Returns an Array coverted from the NodeList of the given class.
+ * @param {string} className
+ * @return Array
+ */
+keyboardTest.prototype.getClassArray = function(className) {
+  return Array.prototype.slice.call(document.getElementsByClassName(className));
+};
+
+/**
+ * Creates a keyboard test and runs it.
+ * @param {string} layout
+ * @param {Object} bindings
+ * @param {string} container
+ * @param {Array} keyOrderList
+ */
+function setUpKeyboardTest(layout, bindings, container, keyOrderList) {
+  window.keyboardTest = new keyboardTest(layout, bindings, container,
+                                         keyOrderList);
+  window.keyboardTest.init();
+}
+
+/**
+ * Marks a key as keydown.
+ * @param {int} keycode
+ */
+function markKeydown(keycode) {
+  window.keyboardTest.markKeydown(keycode);
+}
+
+/**
+ * Marks a key as keyup.
+ * @param {int} keycode
+ */
+function markKeyup(keycode) {
+  window.keyboardTest.markKeyup(keycode);
+}
+
+/**
+ * Fails the test.
+ */
+function failTest() {
+  window.keyboardTest.failTest();
+}
