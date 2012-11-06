629711cbf81e6e518cbe42a1aadba3e178b1ada3
Dean Liao <deanliao@chromium.org>
2012-11-06 11:57:47 +0800

    Runs tpm_selftest to perform TPM diagnosis.
    BUG=chrome-os-partner:14088
    TEST=manual
    
    Change-Id: I458fb2eb43ef9b02149c20ac1757c713f16e8271
    Reviewed-on: https://gerrit.chromium.org/gerrit/37413
    Reviewed-by: Bowgo Tsai <bowgotsai@chromium.org>
    Commit-Ready: Shuo-Peng Liao <deanliao@chromium.org>
    Tested-by: Shuo-Peng Liao <deanliao@chromium.org>
    Reviewed-by: Ricky Liang <jcliang@chromium.org>

---
+++ py/test/pytests/tpm_diagnosis.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Runs tpm_selftest to perform TPM self-diagnosis."""
+
+import logging
+import os
+import subprocess
+import threading
+import unittest
+
+from cros.factory.test.args import Arg
+from cros.factory.test.test_ui import Escape, MakeLabel, UI
+from cros.factory.test.ui_templates import OneScrollableSection
+from cros.factory.utils.process_utils import Spawn
+
+_TEST_TITLE = MakeLabel('TPM Self-diagnosis', u'TPM 自我诊断')
+_CSS = '#state {text-align:left;}'
+
+
+class TpmDiagnosisTest(unittest.TestCase):
+  ARGS = [
+    Arg('tpm_selftest', str, 'Path of tpm_selftest program.',
+        default='/usr/local/sbin/tpm_selftest'),
+    Arg('tpm_args', list, 'List of tpm_selftest args.',
+        default=['-l', 'debug']),
+    Arg('success_pattern', str, 'Pattern of success.',
+        default='tpm_selftest succeeded')
+  ]
+
+  def setUp(self):
+    self.assertTrue(os.path.isfile(self.args.tpm_selftest),
+                    msg='%s is missing.' % self.args.tpm_selftest)
+    self._ui = UI()
+    self._template = OneScrollableSection(self._ui)
+    self._template.SetTitle(_TEST_TITLE)
+    self._ui.AppendCSS(_CSS)
+
+  def DiagnoseTpm(self):
+    """Runs tpm_selftest.
+
+    It shows diagnosis result on factory UI.
+    """
+    p = Spawn([self.args.tpm_selftest] + self.args.tpm_args,
+              stdout=subprocess.PIPE, stderr=subprocess.STDOUT, log=True)
+    success = False
+    for line in iter(p.stdout.readline, ''):
+      logging.info(line.strip())
+      self._template.SetState(Escape(line), append=True)
+      if line.find(self.args.success_pattern) != -1:
+        success = True
+
+    if success:
+      self._ui.Pass()
+    else:
+      self._ui.Fail('TPM self-diagnose failed: %d.' % p.returncode)
+
+
+  def runTest(self):
+    threading.Thread(target=self.DiagnoseTpm).start()
+    self._ui.Run()
