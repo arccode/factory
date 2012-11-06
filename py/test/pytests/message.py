ebd33d48566b8f94a5e2e614c1caae6070a21b42
Jon Salz <jsalz@chromium.org>
2012-11-06 14:14:28 +0800

    Add simple "message" test.
    BUG=chrome-os-partner:15639
    TEST=Manual
    
    Change-Id: I0f966ab275fafd6a1890d4b3cc4315fe48327b23
    Reviewed-on: https://gerrit.chromium.org/gerrit/37419
    Tested-by: Jon Salz <jsalz@chromium.org>
    Reviewed-by: Andy Cheng <andycheng@chromium.org>
    (cherry picked from commit afc4a04fc33c447c1587442ba9a1d808bbe3e201)
    Reviewed-on: https://gerrit.chromium.org/gerrit/38554
    Commit-Ready: Jon Salz <jsalz@chromium.org>
    Reviewed-by: Jon Salz <jsalz@chromium.org>

---
+++ py/test/pytests/message.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Runs chromeos-firmwareupdate to force update EC/firmware."""
+
+
+import unittest
+
+
+from cros.factory.test.args import Arg
+from cros.factory.test.test_ui import MakeLabel, UI
+from cros.factory.test.ui_templates import OneSection
+
+
+class MessageTest(unittest.TestCase):
+  ARGS = [
+    Arg('html_en', str, 'Message (HTML in English).'),
+    Arg('html_zh', str,' Message (HTML, in Chinese).', optional=True),
+  ]
+
+  def runTest(self):
+    ui = UI(css='.message { font-size: 200% }')
+    template = OneSection(ui)
+    template.SetTitle(MakeLabel('Message', '讯息'))
+    template.SetState(MakeLabel(self.args.html_en, self.args.html_zh,
+                                'message'))
+    ui.BindStandardKeys(bind_fail_keys=False)
+    ui.Run()
