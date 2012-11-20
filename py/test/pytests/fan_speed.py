e804af1da5f3c78d4752933e528933145e71c757
Dean Liao <deanliao@chromium.org>
2012-11-20 17:23:20 +0800

    fan_speed: add spin_max_then_half test case with some fix.
    In some scenario, we want to spin up fan to maximum speed and then
    set target speed to half the max speed. With dargs spin_max_then_half
    we can perform the test.
    
    Also, fetching the maximum/minimum readings as stablized fan speed for
    fan spin up/down is not a good approach. In practice, I observed that
    when I spin down the fan, the fan speed may overshoot for some samples.
    So I changed to take average of latest N samples as stablized fan speed.
    
    For UI, I updated current test case and fan speed in real time.
    
    BUG=chrome-os-partner:16240
    TEST=manual
    Test with test case:
      OperatorTest(
        id='Fan',
        pytest_name='fan_speed',
        disable_services=['temp_metrics'],
        dargs={'duration_secs': 5,
               'spin_max_then_half': True,
               'max_rpm': 20000,
               'probe_interval_secs': 0.2,
               'error_margin': 300,
          }),
    
    Change-Id: Iedc3de7a1525ed4bed7b70d8b6851738566e0dc8
    Reviewed-on: https://gerrit.chromium.org/gerrit/38367
    Reviewed-by: Ricky Liang <jcliang@chromium.org>
    Commit-Ready: Shuo-Peng Liao <deanliao@chromium.org>
    Tested-by: Shuo-Peng Liao <deanliao@chromium.org>

--- py/test/pytests/fan_speed.py
+++ py/test/pytests/fan_speed.py
@@ -4,13 +4,37 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-'''A factory test to ensure the functionality of CPU fan.
+"""A factory test to ensure the functionality of CPU fan.
 
+It provides two types of test:
+A. target_rpm mode
+B. spin_max_then_half mode.
+For mode B, it first spins fan up to a max_rpm to get an empirical maximum
+fan rpm; then it runs mode A with half of the empirical max rpm as target_rpm.
+
+In mode A, the steps are:
 1. Sets the fan speed to a target RPM.
-2. Monitors the fan speed for a given period and records the largest reading.
-3. Checks that the reading is greater than the given minimum requirement, and
-   optionally that the reading has never exceeded a particular maximum.
-'''
+2. Monitors the fan speed for a given period (duration_secs) with sampling
+   interval (probe_interval_secs). Then it takes average of the latest
+   #num_samples_to_use samples as the stablized fan speed reading.
+3. Checks that the averaged reading is within range
+   [target_rpm - error_margin, target_rpm + error_margin].
+
+dargs:
+  duration_secs: Duration of monitoring fan speed in seconds.
+  error_margin: Fail the test if actual fan speed is off the target by the
+      margin.
+  max_rpm: A relatively high RPM for probing maximum fan speed. It is used
+      when spin_max_then_half=True.
+  num_samples_to_use: Number of lastest samples to count average as stablized
+      speed.
+  probe_interval_secs: Interval of probing fan speed in seconds.
+  spin_max_then_half: If True, spin the fan to max_rpm, measure the empirical
+      reading, and set fan speed to half of the empirical reading. Note that
+      if True, target_rpm is invalid.
+  target_rpm: Target RPM to set during test. Unused if spin_max_then_half
+      is set.
+"""
 
 import factory_common # pylint: disable=W0611
 import logging
@@ -19,70 +43,126 @@ import time
 import unittest
 
 from cros.factory import system
-from cros.factory.event_log import EventLog
+from cros.factory.system.ec import ECException
 from cros.factory.test.args import Arg
 from cros.factory.test.test_ui import MakeLabel, UI
 from cros.factory.test.ui_templates import OneSection
 
-_TEST_TITLE = MakeLabel('Fan Speed Test', u'风扇转速测试')
-_MSG_FAN_TESTING = MakeLabel('Testing Fan Speed...', u'测试风扇转速中...')
+_TEST_TITLE = MakeLabel('Fan Speed Test', zh=u'风扇转速测试')
+_MSG_FAN_SPEED = MakeLabel('Fan speed (RPM):', zh=u'风扇转速(RPM):')
+_ID_STATUS = 'fs_status'
+_ID_RPM = 'fs_rpm'
+_TEST_BODY = ('<div id="%s"></div><br>\n'
+              '%s <div id="%s"></div>') % (_ID_STATUS, _MSG_FAN_SPEED, _ID_RPM)
 
-def FanTooSlowMessage(observed_rpm, min_expected_rpm):
-  return 'Fan speed failed to reach minimum expected RPM: %s < %s' % (
-      (observed_rpm, min_expected_rpm))
-
-def FanTooFastMessage(observed_rpm, max_expected_rpm):
-  return 'Fan speed exceeded maximum expected RPM: %s > %s' % (
-      (observed_rpm, max_expected_rpm))
-
-class testFan(unittest.TestCase):
+class FanSpeedTest(unittest.TestCase):
   ARGS = [
-    Arg('target_rpm', (int, float), 'Target RPM to set during test.'),
+    Arg('target_rpm', int,
+        'Target RPM to set during test. Unused if spin_max_then_half is set.',
+        default=0, optional=True),
+    Arg('error_margin', int,
+        'Fail the test if actual fan speed is off the target by the margin.',
+        default=200),
     Arg('duration_secs', (int, float),
-        'The duration of time in seconds to monitor the fan speed.', 10),
-    Arg('min_expected_rpm', (int, float),
-        'Minumum expected RPM that the fan should achieve to pass the test.'),
-    Arg('max_expected_rpm', (int, float),
-        'Maximum expected RPM; if this is exceeded the test fails.',
-        optional=True),
+        'Duration of monitoring fan speed in seconds.', default=10),
+    Arg('spin_max_then_half', bool,
+        'If True, spin the fan to max_rpm, measure the actual reading, and '
+        'set fan speed to half of actual max speed. Note that if True, '
+        'target_rpm is invalid.',
+        default=False),
+    Arg('max_rpm', int,
+        'A relatively high RPM for probing maximum fan speed. It is used when '
+        'spin_max_then_half=True.',
+        default=10000),
+    Arg('probe_interval_secs', float,
+        'Interval of probing fan speed in seconds.',
+        default=0.2),
+    Arg('num_samples_to_use', int,
+        'Number of lastest samples to count average as stablized speed.',
+        default=5)
   ]
 
-  def __init__(self, *args, **kwargs):
-    super(testFan, self).__init__(*args, **kwargs)
+  def setUp(self):
+    self.assertTrue(
+      self.args.target_rpm > 0 or self.args.spin_max_then_half,
+      'Either set a valid target_rpm or spin_max_then_half=True.')
     self._ui = UI()
     self._template = OneSection(self._ui)
     self._template.SetTitle(_TEST_TITLE)
     self._ec = system.GetEC()
-    self._event_log = EventLog.ForAutoTest()
-
-  def MonitorFanSpeed(self):
-    end_time = time.time() + self.args.duration_secs
-    observed_rpm = []
-    # Monitor the fan speed for duration_secs seconds.
-    while time.time() < end_time:
-      fan_speed_rpm = self._ec.GetFanRPM()
-      observed_rpm.append(fan_speed_rpm)
-      logging.info('Observed RPM: %s', fan_speed_rpm)
-      self._event_log.Log('fan_speed', fan_speed_rpm=fan_speed_rpm)
-      time.sleep(0.5)
-
-    max_observed_rpm = max(observed_rpm)
-    if max_observed_rpm < self.args.min_expected_rpm:
-      self._ui.Fail(FanTooSlowMessage(max_observed_rpm,
-                                      self.args.min_expected_rpm))
-    elif (self.args.max_expected_rpm and
-          max_observed_rpm > self.args.max_expected_rpm):
-      self._ui.Fail(FanTooFastMessage(max_observed_rpm,
-                                      self.args.max_expected_rpm))
-    else:
-      self._ui.Pass()
 
   def tearDown(self):
+    logging.info('Set auto fan speed control.')
     self._ec.SetFanRPM(self._ec.AUTO)
 
+  def SetAndGetFanSpeed(self, target_rpm):
+    """Sets fan speed and observes readings for a while (blocking call).
+
+    Args:
+      target_rpm: target fan speed.
+
+    Returns:
+      The average of the latest #num_samples_to_use samples as stablized fan
+      speed reading.
+    """
+    observed_rpm = self._ec.GetFanRPM()
+    spin_up = target_rpm > observed_rpm
+
+    status = 'Spin %s fan speed: %d -> %d RPM.' % (
+      'up' if spin_up else 'down', observed_rpm, target_rpm)
+    status_zh = u'风扇%s速: %d -> %d PRM.' % (
+      u'加' if spin_up else u'减', observed_rpm, target_rpm)
+    self._ui.SetHTML(MakeLabel(status, status_zh), id=_ID_STATUS)
+    self._ui.SetHTML(observed_rpm, id=_ID_RPM)
+    logging.info(status)
+
+    self._ec.SetFanRPM(int(target_rpm))
+    # Probe fan speed for duration_secs seconds with sampling interval
+    # probe_interval_secs.
+    end_time = time.time() + self.args.duration_secs
+    observed_rpms = []
+    while time.time() < end_time:
+      observed_rpm = self._ec.GetFanRPM()
+      self._ui.SetHTML(observed_rpm, id=_ID_RPM)
+      observed_rpms.append(observed_rpm)
+      logging.info('Observed fan RPM: %s', observed_rpm)
+      time.sleep(self.args.probe_interval_secs)
+
+    num_samples = self.args.num_samples_to_use
+    total_samples = len(observed_rpms)
+    if num_samples > total_samples / 2:
+      logging.error('Insufficient #samples to get average fan speed. '
+                    'Use latest one instead.')
+      num_samples = 1
+    # Average the latest #num_samples readings as stablized fan speed.
+    return int(sum(observed_rpms[-num_samples:]) / num_samples)
+
+  def Run(self):
+    """Main test program running in a separate thread."""
+    try:
+      if self.args.spin_max_then_half:
+        logging.info('Spinning fan up to to get max fan speed...')
+        target_rpm = self.SetAndGetFanSpeed(self.args.max_rpm) / 2
+      else:
+        target_rpm = self.args.target_rpm
+
+      observed_rpm = self.SetAndGetFanSpeed(target_rpm)
+    except ECException as e:
+      self._ui.Fail('EC command failed: %s' % e)
+      return
+
+    lower_bound = target_rpm - self.args.error_margin
+    upper_bound = target_rpm + self.args.error_margin
+    if lower_bound <= observed_rpm <= upper_bound:
+      logging.info('Observed fan RPM: %d within target range: [%d, %d].',
+                   observed_rpm, lower_bound, upper_bound)
+      self._ui.Pass()
+    else:
+      self._ui.Fail(
+        'Observed fan RPM: %d out of target range: [%d, %d].' %
+        (observed_rpm, lower_bound, upper_bound))
+
   def runTest(self):
-    # Set fan speed to target RPM.
-    self._ec.SetFanRPM(self.args.target_rpm)
-    self._template.SetState(_MSG_FAN_TESTING)
-    threading.Thread(target=self.MonitorFanSpeed).start()
+    self._template.SetState(_TEST_BODY)
+    threading.Thread(target=self.Run).start()
     self._ui.Run()
