giggleby/shim

Add "ectool flashprotect" check to WP switch test.
BUG=chrome-os-partner:15894
TEST=Manual

Change-Id: Ia6f9e7bc6e3477f3eabd5468481a6e86949f816d
Reviewed-on: https://gerrit.chromium.org/gerrit/37986
Reviewed-by: Jon Salz <jsalz@chromium.org>
Tested-by: Jon Salz <jsalz@chromium.org>

py/test/pytests/write_protect_switch.py
@@ -6,6 +6,8 @@
 
 '''Verifies that the write-protect switch is on.'''
 
+import logging
+import re
 import unittest
 
 import factory_common  # pylint: disable=W0611
@@ -14,8 +16,16 @@ from cros.factory.utils.process_utils import Spawn
 class WriteProtectSwitchTest(unittest.TestCase):
   ARGS = []
   def runTest(self):
-    self.assertEqual(
-        '1',
-        Spawn(
-            ['crossystem', 'wpsw_cur'],
-            log=True, check_output=True, log_stderr_on_error=True).stdout_data)
+    self.assertEqual('1', Spawn(
+        ['crossystem', 'wpsw_cur'],
+        log=True, check_output=True, log_stderr_on_error=True).stdout_data)
+
+    ectool_flashprotect = Spawn(
+        ['ectool', 'flashprotect'],
+        log=True, check_output=True, log_stderr_on_error=True).stdout_data
+
+    logging.info('ectool flashprotect:\n%s', ectool_flashprotect)
+    # Multiline is important: we need to see wp_gpio_asserted on the same line
+    self.assertTrue(re.search('^Flash protect flags:.+wp_gpio_asserted',
+                              ectool_flashprotect, re.MULTILINE),
+                    'ectool flashprotect is missing wp_gpio_asserted')

arccode/factory

(cherry picked from commit 1cd39052cf09fa11618ae1dc63600731554b5d47)
Reviewed-on: https://gerrit.chromium.org/gerrit/38618
Commit-Ready: Jon Salz <jsalz@chromium.org>

py/test/pytests/write_protect_switch.py
@@ -6,6 +6,8 @@
 
 '''Verifies that the write-protect switch is on.'''
 
+import logging
+import re
 import unittest
 
 import factory_common  # pylint: disable=W0611
@@ -14,6 +16,16 @@ from cros.factory.utils.process_utils import SpawnOutput
 class WriteProtectSwitchTest(unittest.TestCase):
   ARGS = []
   def runTest(self):
-    self.assertEqual('1',
-                     SpawnOutput(['crossystem', 'wpsw_cur'], log=True,
-                                 log_stderr_on_error=True))
+    self.assertEqual('1', SpawnOutput(
+        ['crossystem', 'wpsw_cur'],
+        log=True, check_output=True, log_stderr_on_error=True))
+
+    ectool_flashprotect = SpawnOutput(
+        ['ectool', 'flashprotect'],
+        log=True, check_output=True, log_stderr_on_error=True)
+
+    logging.info('ectool flashprotect:\n%s', ectool_flashprotect)
+    # Multiline is important: we need to see wp_gpio_asserted on the same line
+    self.assertTrue(re.search('^Flash protect flags:.+wp_gpio_asserted',
+                              ectool_flashprotect, re.MULTILINE),
+                    'ectool flashprotect is missing wp_gpio_asserted')