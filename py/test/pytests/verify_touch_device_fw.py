95ea0378e74de5dcf22254c2f23ff03739fe0e3e
Jon Salz <jsalz@chromium.org>
2012-11-29 12:43:12 +0800

    Add test to verify touch device firmware versions.
    BUG=chrome-os-partner:16432
    TEST=Manual on device
    
    Change-Id: I03cd22b91e47960ebb3eb546a75c96688c1cdac4
    (cherry picked from commit b7534ec21797488d2a59bdf0a122d51cacd8c732)
    Reviewed-on: https://gerrit.chromium.org/gerrit/38891
    Commit-Ready: Jon Salz <jsalz@chromium.org>
    Reviewed-by: Jon Salz <jsalz@chromium.org>
    Tested-by: Jon Salz <jsalz@chromium.org>

---
+++ py/test/pytests/verify_touch_device_fw.py
@@ -0,0 +1,45 @@
+# -*- mode: python; coding: utf-8 -*-
+# Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+"""Checks touch device firmware and config checksum.
+
+This looks for a touch device configured with the given 'config_file'
+name, and verifies that its fw_version and config_csum (read from
+/sys) match the expected values.
+"""
+
+
+import glob
+import os
+import unittest
+
+from cros.factory.test.args import Arg
+
+
+class VerifyTouchDeviceFWTest(unittest.TestCase):
+  ARGS = [
+    Arg('config_file', str,
+        'Name of the touch config file (as in '
+        '/sys/bus/i2c/devices/*/config_file)'),
+    Arg('fw_version', str, 'Expected firmware version'),
+    Arg('config_csum', str, 'Expected config checksum'),
+  ]
+
+  def runTest(self):
+    # Find the appropriate config file.
+    configs = [x for x in glob.glob('/sys/bus/i2c/devices/*/config_file')
+               if open(x).read().strip() == self.args.config_file]
+    self.assertEqual(
+        1, len(configs),
+        'Expected to find one config file but found %s' % configs)
+    device_path = os.path.dirname(configs[0])
+
+    for atom in ('fw_version', 'config_csum'):
+      expected = getattr(self.args, atom)
+      actual = open(os.path.join(device_path, atom)).read().strip()
+      self.assertEquals(expected, actual,
+                        'Mismatched %s (expected %r, found %r)' % (
+                            atom, expected, actual))
