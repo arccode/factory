c1bffa67e5e11c1d45cc817576e8f312e994141b
Jon Salz <jsalz@chromium.org>
2012-12-22 08:53:45 +0800

    Change make_par to generate an archive that can run any executable.
    BUG=None
    TEST=make_par_unittest
    
    Change-Id: I358be6eb8dacd8ca219a645c5207df906f199406
    Reviewed-on: https://gerrit.chromium.org/gerrit/40145
    Tested-by: Jon Salz <jsalz@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Commit-Queue: Jon Salz <jsalz@chromium.org>

--- py/tools/make_par.py
+++ py/tools/make_par.py
@@ -4,11 +4,35 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-'''Creates a self-extracting Python executable.'''
+"""Creates a self-extracting Python executable."""
+
+DESCRIPTION = """Creates a self-extracting Python executable.
+
+The generated executable contains a copy of the entire factory code
+and can be used to executed any "well-behaved" executable, in
+particular the shopfloor server and tools like mount_partition.  Simply
+generate an output file whose name is the same as any symlink in the bin
+directory, or create a symlink to the generated archive with such a name.
+
+For instance:
+
+  make_par -o shopfloor_server
+  # ./shopfloor_server is now a fully-functional, standalone shopfloor server
+  # including all dependencies.
+
+or:
+
+  make_par  # Generates factory.par
+  ln -s factory.par shopfloor_server
+  ln -s factory.par mount_partition
+  # You can now run either ./shopfloor_server or ./mount_partition.
+"""
 
 import argparse
+import glob
 import logging
 import os
+import re
 import shutil
 import subprocess
 import sys
@@ -16,15 +40,45 @@ import tempfile
 
 import factory_common  # pylint: disable=W0611
 from cros.factory.test import factory
-from cros.factory.utils.process_utils import Spawn
+from cros.factory.utils.process_utils import Spawn, SpawnOutput
 
-HEADER = """#!/bin/sh
+# Template for the header that will be placed before the ZIP file to
+# execute a script based on the name which is used to execute it.  The
+# literal string "MODULES" is replaced with a map from executable name
+# to fully-qualified module name (e.g., {'mount_partition':
+# 'cros.factory.tools.mount_partition'}).
+HEADER_TEMPLATE = """#!/bin/sh
 
 exec \
   env PYTHONPATH="$(readlink -f $0):$PYTHONPATH" \
       PYTHONIOENCODING=utf-8 \
   python -c \
-    "import runpy; runpy.run_module('%(module)s', run_name='__main__')" "$@"
+"import os, runpy, sys
+
+# Remove '-c' from argument list.
+sys.argv = sys.argv[1:]
+
+# List of modules, based on symlinks in 'bin' when make_par was run.
+# The actual list is substituted in by the make_par script.
+modules = MODULES
+
+# Use the name we were executed as to determine which module to run.
+name = os.path.basename(sys.argv[0])
+module = modules.get(name)
+
+if not module:
+  # Display an error message describing the valid commands.
+  print >>sys.stderr, (
+    'Unable to run %s.  To run a file within this archive,' % name)
+  print >>sys.stderr, (
+    'rename it to (or create a symbolic link from) one of the following:')
+  print >>sys.stderr
+  for m in sorted(modules.keys()):
+    print >>sys.stderr, '  %s' % m
+  sys.exit(1)
+
+runpy.run_module(module, run_name='__main__')" "$0" "$@"
+
 echo Unable to exec "$*" >& 2
 exit 1
 
@@ -32,19 +86,15 @@ exit 1
 """
 
 def main(argv=None):
-  parser = argparse.ArgumentParser()
+  parser = argparse.ArgumentParser(
+    description=DESCRIPTION,
+    formatter_class=argparse.RawDescriptionHelpFormatter)
   parser.add_argument('--verbose', '-v', action='count')
   parser.add_argument(
-      '--output', '-o', metavar='FILE',
-      help='output file (defaults to <MODULE>.par)')
-  parser.add_argument(
-      'module', metavar='MODULE',
-      help='name of module to be run when the .par file is executed.')
+      '--output', '-o', metavar='FILE', default='factory.par',
+      help='output file (default: %(default)s')
   args = parser.parse_args(argv)
   logging.basicConfig(level=logging.WARNING - 10 * (args.verbose or 0))
-
-  if not args.output:
-    args.output = args.module.rpartition('.')[2] + '.par'
 
   tmp = tempfile.mkdtemp(prefix='make_par.')
   try:
@@ -54,22 +104,44 @@ def main(argv=None):
           log=True, check_call=True)
     par_file = os.path.join(tmp, 'factory.par')
 
+    # Build a map of runnable modules based on symlinks in bin.
+    modules = {}
+    factory_bin = os.path.join(factory.FACTORY_PATH, 'bin')
+    for f in glob.glob(os.path.join(factory_bin, '*')):
+      if not os.path.islink(f):
+        continue
+      dest = os.readlink(f)
+      match = re.match('\.\./py/(.+)\.py$', dest)
+      if not match:
+        continue
+
+      module = 'cros.factory.%s' % match.group(1).replace('/', '.')
+      name = module.rpartition('.')[2]
+      logging.info('Mapping binary %s to %s', name, module)
+      modules[name] = module
+
     # Concatenate the header and the par file.
     with open(args.output, 'wb') as out:
-      out.write(HEADER % dict(module=args.module))
+      out.write(HEADER_TEMPLATE.replace('MODULES', repr(modules)))
       shutil.copyfileobj(open(par_file), out)
       os.fchmod(out.fileno(), 0755)
 
     # Done!
     print 'Created %s (%d bytes)' % (args.output, os.path.getsize(args.output))
 
-    # Sanity check: make sure we can import the module
+    # Sanity check: make sure we can run 'make_par --help' within the
+    # archive, in a totally clean environment, and see the help
+    # message.
     try:
-      Spawn(['python', '-c', 'import %s' % args.module],
-            env={'PYTHONPATH': args.output},
-            check_call=True, log_stderr_on_error=True)
+      link = os.path.join(tmp, 'make_par')
+      os.symlink(os.path.realpath(args.output), link)
+      output = SpawnOutput(
+        [link, '--help'], env={}, cwd='/',
+        check_call=True, read_stdout=True,log_stderr_on_error=True)
+      if 'show this help message and exit' not in output:
+        logging.error('Unable to run "make_par --help": output is %r', output)
     except subprocess.CalledProcessError:
-      logging.error('Unable to import %s from %s', args.module, args.output)
+      logging.error('Unable to run "make_par --help" within the .par file')
       return False
 
     return True
--- py/tools/make_par_unittest.py
+++ py/tools/make_par_unittest.py
@@ -9,32 +9,44 @@ This also tests run_pytest.
 '''
 
 
+import os
+import shutil
+import tempfile
 import unittest
 
 import factory_common  # pylint: disable=W0611
 from cros.factory.tools import make_par
-from cros.factory.utils import file_utils
 from cros.factory.utils.process_utils import Spawn
 
 
 class MakePARTest(unittest.TestCase):
-  def testPAR(self):
-    with file_utils.UnopenedTemporaryFile(suffix='.par') as par:
-      self.assertTrue(make_par.main(['cros.factory.test.run_pytest',
-                                     '-o', par]))
+  def setUp(self):
+    self.tmp = tempfile.mkdtemp(prefix='make_par_unittest.')
+    self.par = os.path.join(self.tmp, 'factory.par')
+    self.assertTrue(make_par.main(['-o', self.par]))
 
-      for expected_retcode, script in ((0, 'pass'),
-                                       (1, 'raise ValueError')):
-        self.assertEquals(
-            expected_retcode,
-            Spawn([par, 'execpython', '--args', 'dict(script=%r)' % script],
-                  log=True, call=True,
-                  ignore_stdout=True, ignore_stderr=True).returncode)
+  def tearDown(self):
+    shutil.rmtree(self.tmp)
+
+  def testPAR(self):
+    link = os.path.join(self.tmp, 'run_pytest')
+    os.symlink(self.par, link)
+
+    for expected_retcode, script in ((0, 'pass'),
+                                     (1, 'raise ValueError')):
+      self.assertEquals(
+        expected_retcode,
+        Spawn([link, 'execpython', '--args', 'dict(script=%r)' % script],
+              log=True, call=True, env={}, cwd='/',
+              ignore_stdout=True, ignore_stderr=True).returncode)
 
   def testInvalidModule(self):
-    with file_utils.UnopenedTemporaryFile(suffix='.par') as par:
-      self.assertFalse(make_par.main(['cros.factory.nonexistant_module',
-                                      '-o', par]))
+    link = os.path.join(self.tmp, 'invalid')
+    os.symlink(self.par, link)
+
+    process = Spawn([link], call=True, read_stderr=True, env={}, cwd='/')
+    self.assertEqual(1, process.returncode)
+    self.assertTrue('To run a file within this archive,' in process.stderr_data)
 
 
 if __name__ == '__main__':
