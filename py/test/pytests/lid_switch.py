75e07568bf157b952905c4d84344b3088c7ebdf0
Vic Yang <victoryang@chromium.org>
2013-01-31 15:04:58 +0800

    lid_switch: Use evdev to sense lid signal
    It is now impossible to get lid event from DBus without triggering a
    shutdown. Let's switch to evdev event.
    
    BUG=none
    TEST=Run on Spring with/without event ID set in test list.
    
    Change-Id: If7baa48b77b27cf91ee1f883263934d45f894ca3
    Signed-off-by: Vic Yang <victoryang@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/42383
    Reviewed-by: Ricky Liang <jcliang@chromium.org>

--- py/test/pytests/lid_switch.py
+++ py/test/pytests/lid_switch.py
@@ -13,8 +13,8 @@ dargs:
   audio_volume: (optional) volume to play the ok audio. Default 100%.
 """
 
-import re
-import subprocess
+import asyncore
+import evdev
 import time
 import unittest
 
@@ -22,14 +22,8 @@ from cros.factory.test import test_ui
 from cros.factory.test.args import Arg
 from cros.factory.test.ui_templates import OneSection
 from cros.factory.test.utils import StartDaemonThread
-from cros.factory.utils.process_utils import Spawn
 
 _DEFAULT_TIMEOUT = 10
-
-_RE_DBUS_SIGNAL = re.compile(r'^.*interface=(.*?); member=(.*?)$')
-_DBUS_INTERFACE = 'org.chromium.PowerManager'
-_DBUS_LID_OPENED = 'LidOpened'
-_DBUS_LID_CLOSED = 'LidClosed'
 
 _MSG_PROMPT_CLOSE = test_ui.MakeLabel(
     'Close then open the lid', u'关上接着打开上盖', 'lid-test-info')
@@ -46,6 +40,19 @@ _HTML_LID_SWITCH = '<div id="%s"></div>\n<div id="%s"></div>\n' % (
 _LID_SWITCH_TEST_DEFAULT_CSS = '.lid-test-info { font-size: 2em; }'
 
 
+class InputDeviceDispatcher(asyncore.file_dispatcher):
+  def __init__(self, device, event_handler):
+    self.device = device
+    self.event_handler = event_handler
+    asyncore.file_dispatcher.__init__(self, device)
+
+  def recv(self, ign=None): # pylint:disable=W0613
+    return self.device.read()
+
+  def handle_read(self):
+    for event in self.recv():
+      self.event_handler(event)
+
 class LidSwitchTest(unittest.TestCase):
   ARGS = [
     Arg('timeout_secs', int, 'Timeout value for the test.',
@@ -55,42 +62,54 @@ class LidSwitchTest(unittest.TestCase):
         'signal. Defaults to play ok_*.ogg in /sounds.',
         default=None, optional=True),
     Arg('audio_volume', int, 'Audio volume to use when playing OK audio file.',
-        default=100)
+        default=100),
+    Arg('event_id', int, 'Event ID for evdev. None for auto probe.',
+        default=None, optional=True)
   ]
 
   def setUp(self):
     self.ui = test_ui.UI()
     self.template = OneSection(self.ui)
+    if self.args.event_id:
+      self.event_dev = evdev.InputDevice('/dev/input/event%d' %
+                                         self.args.event_id)
+    else:
+      self.event_dev = self.ProbeLidEventSource()
     self.ui.AppendCSS(_LID_SWITCH_TEST_DEFAULT_CSS)
     self.template.SetState(_HTML_LID_SWITCH)
     self.ui.SetHTML(_MSG_PROMPT_CLOSE, id=_ID_PROMPT)
-    self.monitor_process = None
-    # Create a thread to monitor dbus events.
-    StartDaemonThread(target=self.MonitorDbusSignal)
+    self.dispatcher = None
+    # Create a thread to monitor evdev events.
+    StartDaemonThread(target=self.MonitorEvdevEvent)
     # Create a thread to run countdown timer.
     StartDaemonThread(target=self.CountdownTimer)
 
   def tearDown(self):
-    self.TerminateProcess()
+    self.TerminateLoop()
 
-  def MonitorDbusSignal(self):
-    """Creates a process to monitor dbus signals and checks for lid events."""
-    self.monitor_process = Spawn(['dbus-monitor', '--system'],
-                                 stdout=subprocess.PIPE)
-    while True:
-      re_obj = _RE_DBUS_SIGNAL.search(self.monitor_process.stdout.readline())
-      if re_obj:
-        interface, member = re_obj.group(1, 2)
-        if interface == _DBUS_INTERFACE:
-          if member == _DBUS_LID_CLOSED:
-            self.ui.SetHTML(_MSG_PROMPT_OPEN, id=_ID_PROMPT)
-            self.PlayOkAudio()
-          elif member == _DBUS_LID_OPENED:
-            self.ui.Pass()
+  def ProbeLidEventSource(self):
+    """Probe for lid event source."""
+    for dev in map(evdev.InputDevice, evdev.list_devices()):
+      for event_type, event_codes in dev.capabilities().iteritems():
+        if (event_type == evdev.ecodes.EV_SW and
+            evdev.ecodes.SW_LID in event_codes):
+          return dev
 
-  def TerminateProcess(self):
-    if self.monitor_process.poll() is None:
-      self.monitor_process.terminate()
+  def HandleEvent(self, event):
+    if event.type == evdev.ecodes.EV_SW and event.code == evdev.ecodes.SW_LID:
+      if event.value == 1: # LID_CLOSED
+        self.ui.SetHTML(_MSG_PROMPT_OPEN, id=_ID_PROMPT)
+        self.PlayOkAudio()
+      elif event.value == 0: # LID_OPEN
+        self.ui.Pass()
+
+  def MonitorEvdevEvent(self):
+    """Creates a process to monitor evdev event and checks for lid events."""
+    self.dispatcher = InputDeviceDispatcher(self.event_dev, self.HandleEvent)
+    asyncore.loop()
+
+  def TerminateLoop(self):
+    self.dispatcher.close()
 
   def PlayOkAudio(self):
     if self.args.ok_audio_path:
