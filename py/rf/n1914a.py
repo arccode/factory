6ae878de1d4ba841ace8baf4a5899ecd0d22c9bb
Chun-ta Lin <itspeter@google.com>
2013-02-14 20:02:36 -0800

    Basic implementation for N1914A power meter.
    Implement basic functionality to measure power from N1914A, including
    the unittest.
    
    BUG=chrome-os-partner:17861
    TEST=make lint for styling.
         manually with equipment and make test for functionality.
    
    Change-Id: I6cf1b1507488c018dd42c69d02b490118da1592b
    Reviewed-on: https://gerrit.chromium.org/gerrit/43379
    Reviewed-by: Cheng-Yi Chiang <cychiang@chromium.org>
    Commit-Queue: Chun-Ta Lin <itspeter@google.com>
    Tested-by: Chun-Ta Lin <itspeter@google.com>

---
+++ py/rf/n1914a.py
@@ -0,0 +1,114 @@
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+'''
+Implementation for Agilent EPM Series Power Meter (N1914A) device.
+
+Because N1914A enables up to 4 ports, methods in this class default to
+expose a parameter called port to specify where action will take place.
+'''
+
+
+import factory_common  # pylint: disable=W0611
+from cros.factory.rf.agilent_scpi import AgilentSCPI
+
+
+class N1914A(AgilentSCPI):
+  '''
+  An Agilent EPM Series Power Meter (N1914A) device.
+  '''
+  def __init__(self, *args, **kwargs):
+    super(N1914A, self).__init__('N1914A', *args, **kwargs)
+    self.timeout = 20  # Weak power will cause a relatively long run.
+
+  # Format related methods.
+  def SetAsciiFormat(self):
+    '''Sets the numeric data transferred over SCPI to ASCII.'''
+    self.Send('FORM ASCii')
+
+  # TODO(itspeter): Implement related code for REAL Format.
+
+  # Sampling setting related methods.
+  def ToNormalMode(self, port):
+    '''Sets sampling mode to 20 readings per seconds.'''
+    self.Send('SENSe%d:MRATe NORMal' % port)
+
+  def ToDoubleMode(self, port):
+    '''Sets sampling mode to 40 readings per seconds.'''
+    self.Send('SENSe%d:MRATe DOUBle' % port)
+
+  # TODO(itspeter): Implement related code for Fast Mode.
+
+  # Range related methods.
+  def SetRange(self, port, range_setting=None):
+    '''Selects a sensor's range (lower or upper).
+
+    Args:
+      range_setting: None to enable auto-range feature. To speed up the
+        measurement, caller can specify the range manually based on the
+        expected power. To manually set the range, use 0 to indicate a
+        lower range and 1 for the upper range. Because range definition
+        varies from sensor to sensor, check the manual before using this
+        function.
+
+    '''
+    assert range_setting in [None, 0, 1]
+    if range_setting is None:
+      self.Send('SENSe%d:POWer:AC:RANGe:AUTO 1' % port)
+    else:
+      self.Send(['SENSe%d:POWer:AC:RANGe:AUTO 0' % port,
+                 'SENSe%d:POWer:AC:RANGe %d' % (port, range_setting)])
+
+  # Average related methods.
+  def SetAverageFilter(self, port, avg_length):
+    '''Sets the average filter.
+
+    There are three different average filters available, averaging disable,
+    auto averaging and average with a specific window length.
+
+    Args:
+      avg_length: Use None for averaging disable, -1 for auto averaging and
+        other positive numbers for specific window length.
+    '''
+    if avg_length is None:
+      # Disable the average filter.
+      self.Send('SENSe%d:AVERage:STATe 0' % port)
+    else:
+      self.Send('SENSe%d:AVERage:STATe 1' % port)
+      if avg_length > 0:
+        self.Send(['SENSe%d:AVERage:COUNt:AUTO 0' % port,
+                   'SENSe%d:AVERage:COUNt %d' % (port, avg_length)])
+      elif avg_length == -1:
+        # Use built-in auto average feature.
+        self.Send('SENSe%d:AVERage:COUNt:AUTO 1' % port)
+      else:
+        raise ValueError('Invalid avg_length setting [%s]' % avg_length)
+
+  # Frequency related methods.
+  def SetMeasureFrequency(self, port, freq):
+    self.Send(['SENSe%d:FREQuency %s' % (port, freq),
+               'SENSe%d:CORRection:GAIN2:STATe 0' % port])
+
+  # Trigger related methods.
+  def SetContinuousTrigger(self, port):
+    '''Sets the trigger to repeatedly active.'''
+    self.Send(['INITiate%d:CONTinuous ON' % port])
+
+  def SetOnetimeTrigger(self, port):
+    '''Sets the trigger to active only once.'''
+    self.Send(['INITiate%d:CONTinuous OFF' % port])
+
+  def SetTriggerToFreeRun(self, port):
+    '''Sets unconditional trigger (i.e. FreeRun mode).'''
+    self.Send(['TRIGger%d:SOURce IMMediate' % port])
+
+  def EnableTriggerImmediately(self, port):
+    '''Forces to trigger immediately.'''
+    self.Send(['INITiate%d:IMMediate' % port])
+
+  def MeasureOnce(self, port):
+    '''Performs a single measurement.'''
+    ret = self.Query('FETCh%d?' % port, formatter=float)
+    return ret
---
+++ py/rf/n1914a_unittest.py
@@ -0,0 +1,215 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Unit tests for n1914a module.
+
+If the IP of N1914A is omitted, it will start a local server to mock the
+test equipment. All of the unittests assumed to simulate on port 1 if no
+explicit annotation is given.
+"""
+
+import logging
+import threading
+import unittest2
+import SocketServer
+
+import factory_common  # pylint: disable=W0611
+from cros.factory.utils import test_utils
+from cros.factory.rf.n1914a import N1914A
+
+NORMAL_ERR_RESPONSE = '+0,"No error"\n'
+NORMAL_ESR_REGISTER = '+0\n'
+NORMAL_OPC_RESPONSE = '+1\n'
+
+# pylint: disable=W0232
+class MockTestServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+  allow_reuse_address = True
+
+class MockServerHandler(SocketServer.StreamRequestHandler):
+  """A mocking handler for socket.
+
+  This handler responses client based on its pre-defined lookup table.
+  Lookup table can be config with AddLookup() and ResetLookup().
+
+  Exceptions will be raised if recieves unexpected message from client.
+  """
+  responses_lookup = list()
+
+  @classmethod
+  def AddLookup(cls, input_line, response):
+    cls.responses_lookup.append((input_line, response))
+
+  @classmethod
+  def AddCommandsLookup(cls, commands, wait=True):
+    '''Wrapper for adding commands.'''
+    if type(commands) == str:
+      commands = [commands]
+
+    cls.AddLookup('*CLS', None)
+    for command in commands:
+      cls.AddLookup(command, None)
+      cls.AddLookup('SYST:ERR?', NORMAL_ERR_RESPONSE)
+    if wait:
+      cls.AddLookup('*OPC?', NORMAL_OPC_RESPONSE)
+
+  @classmethod
+  def AddQueryLookup(cls, command, response):
+    '''Wrapper for adding a success query.'''
+    cls.AddLookup('*CLS', None)
+    cls.AddLookup(command, response + '\n')
+    cls.AddLookup('*ESR?', NORMAL_ESR_REGISTER)
+    cls.AddLookup('SYST:ERR?', NORMAL_ERR_RESPONSE)
+
+  @classmethod
+  def ResetLookup(cls):
+    cls.responses_lookup = list()
+
+  def __init__(self, *args, **kwargs):
+    self.lookup = list(self.responses_lookup)
+    SocketServer.StreamRequestHandler.__init__(self, *args, **kwargs)
+
+  def handle(self):
+    while True:
+      line = self.rfile.readline().rstrip('\n')
+      if not line:
+        break
+      expected_input, output = self.lookup.pop(0)
+      if line == expected_input:
+        if output:
+          self.wfile.write(output)
+      else:
+        raise ValueError("Expecting [%s] but got [%s]" % (
+            expected_input, line))
+
+class N1914ATest(unittest2.TestCase):
+  EXPECTED_MODEL = 'Agilent Technologies,N1914A,MY50001187,A2.01.06'
+  HOST = 'localhost'
+
+  def _AddInitialLookup(self):
+    '''Adds necessary lookup for every connection.'''
+    MockServerHandler.ResetLookup()
+    MockServerHandler.AddLookup('*CLS', None)
+    MockServerHandler.AddLookup('*IDN?', self.EXPECTED_MODEL + '\n')
+    MockServerHandler.AddLookup('*ESR?', NORMAL_ESR_REGISTER)
+    MockServerHandler.AddLookup('SYST:ERR?', NORMAL_ERR_RESPONSE)
+
+  def _StartMockServer(self):
+    '''Starts a thread for the mock equipment.'''
+    server_port = test_utils.FindUnusedTCPPort()
+    mock_server = MockTestServer(
+        ('localhost', server_port), MockServerHandler)
+    # pylint: disable=E1101
+    server_thread = threading.Thread(target=mock_server.serve_forever)
+    server_thread.daemon = True
+    server_thread.start()
+    logging.info("Server loop running in thread %s with port %d",
+                 server_thread.name, server_port)
+    return (mock_server, server_port)
+
+  def _StartTest(self):
+    self.mock_server, self.server_port = self._StartMockServer()
+    self.n1914a = N1914A(host=self.HOST, port=self.server_port)
+
+  def testSetAsciiFormat(self):
+    QUERY = 'FORM?'
+    EXPECTED_RESPONSE = 'ASC'
+    MockServerHandler.AddCommandsLookup(['FORM ASCii'])
+    MockServerHandler.AddQueryLookup(QUERY, EXPECTED_RESPONSE)
+
+    self._StartTest()
+    self.n1914a.SetAsciiFormat()
+    self.assertEqual(self.n1914a.Query(QUERY), EXPECTED_RESPONSE)
+
+  def testBasicConnect(self):
+    self._StartTest()
+    # Check the id.
+    self.assertEqual(self.n1914a.id, self.EXPECTED_MODEL)
+
+  def testToNormalMode(self):
+    QUERY = 'SENSe1:MRATe?'
+    EXPECTED_RESPONSE = 'NORM'
+    MockServerHandler.AddCommandsLookup(['SENSe1:MRATe NORMal'])
+    MockServerHandler.AddQueryLookup(QUERY, EXPECTED_RESPONSE)
+
+    self._StartTest()
+    self.n1914a.ToNormalMode(port=1)
+    self.assertEqual(self.n1914a.Query(QUERY), EXPECTED_RESPONSE)
+
+  def testToDoubleMode(self):
+    QUERY = 'SENSe1:MRATe?'
+    EXPECTED_RESPONSE = 'DOUB'
+    MockServerHandler.AddCommandsLookup(['SENSe1:MRATe DOUBle'])
+    MockServerHandler.AddQueryLookup(QUERY, EXPECTED_RESPONSE)
+
+    self._StartTest()
+    self.n1914a.ToDoubleMode(port=1)
+    self.assertEqual(self.n1914a.Query(QUERY), EXPECTED_RESPONSE)
+
+  def testSetRange(self):
+    QUERY1 = 'SENSe1:POWer:AC:RANGe:AUTO?'
+    EXPECTED_RESPONSE_ENABLE = '1'
+    EXPECTED_RESPONSE_DISABLE = '0'
+    QUERY2 = 'SENSe1:POWer:AC:RANGe?'
+    EXPECTED_RESPONSE_2 = '+0'
+
+    MockServerHandler.AddCommandsLookup(['SENSe1:POWer:AC:RANGe:AUTO 1'])
+    MockServerHandler.AddQueryLookup(QUERY1, EXPECTED_RESPONSE_ENABLE)
+    MockServerHandler.AddCommandsLookup(
+        ['SENSe1:POWer:AC:RANGe:AUTO 0', 'SENSe1:POWer:AC:RANGe 0'])
+    MockServerHandler.AddQueryLookup(QUERY1, EXPECTED_RESPONSE_DISABLE)
+    MockServerHandler.AddQueryLookup(QUERY2, EXPECTED_RESPONSE_2)
+
+    self._StartTest()
+    self.n1914a.SetRange(port=1, range_setting=None)
+    self.assertEqual(self.n1914a.Query(QUERY1), EXPECTED_RESPONSE_ENABLE)
+
+    self.n1914a.SetRange(port=1, range_setting=0)
+    self.assertEqual(self.n1914a.Query(QUERY1), EXPECTED_RESPONSE_DISABLE)
+    self.assertEqual(self.n1914a.Query(QUERY2), EXPECTED_RESPONSE_2)
+
+    # TODO(itspeter): test if assertion will be trigger for invalid parameter.
+
+  def testSetAverageFilter(self):
+    QUERY1 = 'SENSe1:AVERage:STATe?'
+    EXPECTED_RESPONSE_ENABLE = '1'
+    EXPECTED_RESPONSE_DISABLE = '0'
+    QUERY2 = 'SENSe1:AVERage:COUNt:AUTO?'
+    QUERY3 = 'SENSe1:AVERage:COUNt?'
+    EXPECTED_RESPONSE_3 = '+100'
+
+    MockServerHandler.AddCommandsLookup(['SENSe1:AVERage:STATe 0'])
+    MockServerHandler.AddQueryLookup(QUERY1, EXPECTED_RESPONSE_DISABLE)
+
+    MockServerHandler.AddCommandsLookup(['SENSe1:AVERage:STATe 1'])
+    MockServerHandler.AddCommandsLookup(
+        ['SENSe1:AVERage:COUNt:AUTO 0', 'SENSe1:AVERage:COUNt 100'])
+
+    MockServerHandler.AddQueryLookup(QUERY1, EXPECTED_RESPONSE_ENABLE)
+    MockServerHandler.AddQueryLookup(QUERY2, EXPECTED_RESPONSE_DISABLE)
+    MockServerHandler.AddQueryLookup(QUERY3, EXPECTED_RESPONSE_3)
+
+    self._StartTest()
+    self.n1914a.SetAverageFilter(port=1, avg_length=None)
+    self.assertEqual(self.n1914a.Query(QUERY1), EXPECTED_RESPONSE_DISABLE)
+
+    self.n1914a.SetAverageFilter(port=1, avg_length=100)
+    self.assertEqual(self.n1914a.Query(QUERY1), EXPECTED_RESPONSE_ENABLE)
+    self.assertEqual(self.n1914a.Query(QUERY2), EXPECTED_RESPONSE_DISABLE)
+    self.assertEqual(self.n1914a.Query(QUERY3), EXPECTED_RESPONSE_3)
+
+  def setUp(self):
+    self._AddInitialLookup()
+    self.mock_server = None
+    self.server_port = None
+    self.n1914a = None
+
+  def tearDown(self):
+    self.n1914a.Close()
+    self.mock_server.shutdown() # pylint: disable=E1101
+
+if __name__ == '__main__':
+  logging.basicConfig(level=logging.INFO)
+  unittest2.main()
