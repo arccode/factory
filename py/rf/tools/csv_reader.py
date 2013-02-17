210d4dd456075d75a9302c8d49ebf405b5587e71
Chun-ta Lin <itspeter@google.com>
2013-02-16 16:29:12 -0800

    help functions to convert CSV to YAML format.
    Although YAML is powerful, we can still take advantage of incorporating with
    other existing tools in CSV format. In this CL, CSV files will be treated in two
    pre-defined types - dict and list of dict, depending on its columns. Each cell
    in CSV file is assumed to be a python evaluable syntax, which nearly no
    difference in text format for primitive types like int, float, str ...etc.
    More details and examples are included in the unittest and docstirng.
    
    BUG=chrome-os-partner:17647
    TEST=make test
    
    Change-Id: I57edeb8ee2a72719767e960e3ed6e2656deb8a44
    Reviewed-on: https://gerrit.chromium.org/gerrit/43474
    Reviewed-by: Ricky Liang <jcliang@chromium.org>
    Commit-Queue: Chun-Ta Lin <itspeter@google.com>
    Tested-by: Chun-Ta Lin <itspeter@google.com>

---
+++ py/rf/tools/csv_reader.py
@@ -0,0 +1,154 @@
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+'''This module provides functions to convert CSV (Comma Separated Values)
+files to YAML format as part of factory parameters generation.
+
+Although YAML is powerful, we can still take advantage of incorporating with
+other existing tools in CSV format. CSV files will be treated in two
+pre-defined types - dict and list of dict, depending on its columns. Each cell
+in CSV file is assumed to be a python evaluable syntax, which nearly no
+difference in text format for primitive types like int, float, str ...etc.
+More details and examples are demonstrated in the unittest.
+'''
+
+import csv
+import logging
+import os
+
+import factory_common  # pylint: disable=W0611
+
+KEY = '__key__'
+VALUE = '__value__'
+
+class CsvLink(object):
+  '''Special wrapper class for identifying an external link in cell.'''
+  def __init__(self, link=None):
+    self.link = link
+  def __eq__(self, rhs):
+    return self.link == rhs.link
+
+def ReadSingleCell(value):
+  '''Converts a single cell to a python object.
+
+  The value must be a python evaluable string, otherwise, exception
+  will be raised by eval() function.  If the special object, CsvLink,
+  was detected, reading will be proceeded recursively.
+  '''
+  if value == '':
+    return None
+  value_in_python = eval(value)
+  # Recursive reading
+  if isinstance(value_in_python, CsvLink):
+    try:
+      value_in_python = ReadCsv(value_in_python.link)
+    except Exception as e:
+      raise ValueError(
+          'Failed to load external csv - %s, %s',
+          (value_in_python.link, e))
+  return value_in_python
+
+def IsAnnotation(row_in_dict, fieldnames):
+  '''Returns if a row is an annotaion.
+
+  Annotation is defined pretty much in python style. If every cell in the row
+  is empty length or the first cell begins with a hash character (#), it is
+  considered as an annotation line.
+
+  Args:
+    row_in_dict: the dict object represents a single row by DictReader.next().
+    fieldnames: the list object represents the first row in
+      DictReader.fieldnames
+  '''
+  # Ignore a row if it starts with pound character.
+  if row_in_dict.get(fieldnames[0], '').startswith('#'):
+    return True
+
+  for key in fieldnames:
+    if row_in_dict.get(key, '') != '':
+      return False
+
+  return True
+
+
+def IsCsvADictHeader(source):
+  '''Reads its first row and see if it fits the pre-defined format.'''
+  with open(source, 'rb') as fd:
+    reader = csv.DictReader(fd)
+    fieldnames = reader.fieldnames
+    # Check fieldnames.
+    if fieldnames != [KEY, VALUE]:
+      return False
+  return True
+
+def ReadCsvAsDict(source):
+  '''Reads a csv and converts to python dict.
+
+  A dict formatted csv have only two columns: __key__ and __value__.
+  '''
+  data = {}
+  with open(source, 'rb') as fd:
+    reader = csv.DictReader(fd)
+    fieldnames = reader.fieldnames
+    # Check fieldnames.
+    if fieldnames != [KEY, VALUE]:
+      raise ValueError('Columns format is not a dict in %s' % source)
+    for idx, row in enumerate(reader):
+      if IsAnnotation(row, fieldnames):
+        continue
+
+      key = row.get(KEY)
+      if key in data:
+        raise ValueError('Duplicated key %s in %s' % (key, source))
+
+      value = ReadSingleCell(row.get(VALUE, ''))
+      data[key] = value
+      # Check if any fields left
+      if len(row) > 2:
+        raise ValueError('Unexpectecd data at row %d' % idx)
+  return data
+
+def ReadCsvAsListOfDict(source):
+  '''
+  Reads csv and treat it as a list of dict.
+
+  The dict's key will follow the column in first row.
+  '''
+  data = []
+  with open(source, 'rb') as fd:
+    reader = csv.DictReader(fd)
+    fieldnames = reader.fieldnames
+
+    # Check if fieldnames are unique.
+    if len(set(fieldnames)) != len(fieldnames):
+      raise ValueError('Duplicated column name in %s' % source)
+
+    for idx, row in enumerate(reader):
+      if IsAnnotation(row, fieldnames):
+        continue
+      converted_dict = dict()
+      # Check if there are dangling cell.
+      if None in row:
+        logging.debug(
+            'Cell without a column name is ignored during conversion\n'
+            'Row[%d] - %s', idx, row[None])
+      for key in fieldnames:
+        converted_dict[key] = ReadSingleCell(row.get(key, ''))
+      data.append(converted_dict)
+  return data
+
+
+def ReadCsv(source):
+  '''Reads a csv from source and returns as a python object.'''
+  original_directory = os.getcwd()
+  os.chdir(os.path.dirname(os.path.abspath(source)))
+  # Try dict first, because dict is a subset of list of dict.
+  try:
+    if IsCsvADictHeader(source):
+      ret = ReadCsvAsDict(source)
+    else:
+      ret = ReadCsvAsListOfDict(source)
+  finally:
+    os.chdir(original_directory)
+  return ret
---
+++ py/rf/tools/csv_reader_unittest.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging
+import os
+import unittest2
+
+import factory_common  # pylint: disable=W0611
+from cros.factory.rf.tools.csv_reader import ReadSingleCell
+from cros.factory.rf.tools.csv_reader import ReadCsv
+from cros.factory.rf.tools.csv_reader import ReadCsvAsDict
+from cros.factory.rf.tools.csv_reader import ReadCsvAsListOfDict
+
+
+class CsvReaderTest(unittest2.TestCase):
+  def setUp(self):
+    self.testdata_path = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), "testdata")
+    self.list_expected = [
+        {'key1': 3, 'key2': 4},
+        {'key1': 2, 'key2': 5},
+        {'key1': None, 'key2': 8},
+        {'key1': 1, 'key2': 9}]
+    self.dict_expected = {'key1': 33, 'key2': 2}
+
+  def testReadSingleCell(self):
+    self.assertEqual(ReadSingleCell("100"), 100)
+    self.assertEqual(ReadSingleCell("'str_test'"), "str_test")
+    self.assertEqual(ReadSingleCell("(300, 600)"), (300, 600))
+    self.assertEqual(ReadSingleCell(""), None)
+    self.assertEqual(ReadSingleCell("[1, 4, 7]"), [1, 4, 7])
+    self.assertEqual(ReadSingleCell("[1, 4, 7]"), [1, 4, 7])
+    self.assertRaisesRegexp(
+        NameError, "name .* is not defined",
+        ReadSingleCell, "invalid_syntax")
+    self.assertRaisesRegexp(
+        ValueError, "Failed to load external",
+        ReadSingleCell, "CsvLink('not_exist_file.csv')")
+
+  def testReadCsvAsADict(self):
+    loaded_dict = ReadCsvAsDict(
+        os.path.join(self.testdata_path, 'dict_normal.csv'))
+    self.assertEqual(loaded_dict, self.dict_expected)
+
+  def testReadCsvAsADictInvalidColumns(self):
+    self.assertRaisesRegexp(
+        ValueError, 'Columns format is not a dict', ReadCsvAsDict,
+        os.path.join(self.testdata_path, 'dict_invalid_column.csv'))
+
+  def testReadCsvAsADictDuplicatedKey(self):
+    self.assertRaisesRegexp(
+        ValueError, 'Duplicated key', ReadCsvAsDict,
+        os.path.join(self.testdata_path, 'dict_duplicated_key.csv'))
+
+  def testReadCsvAsListOfDict(self):
+    loaded_list = ReadCsvAsListOfDict(
+        os.path.join(self.testdata_path, 'list_of_dict_normal.csv'))
+    self.assertEqual(loaded_list, self.list_expected)
+
+  def testReadCsvAsListOfDictDuplicatedColumn(self):
+    self.assertRaisesRegexp(
+        ValueError, 'Duplicated column', ReadCsvAsListOfDict,
+        os.path.join(self.testdata_path, 'list_of_dict_duplicated_column.csv'))
+
+  def testReadCsv(self):
+    loaded_python_obj = ReadCsv(
+        os.path.join(self.testdata_path, 'recursive_example.csv'))
+    python_obj_expected = {'key1': self.dict_expected,
+                           'key2': 33,
+                           'key3': self.list_expected}
+    self.assertEqual(loaded_python_obj, python_obj_expected)
+
+if __name__ == '__main__':
+  logging.basicConfig(level=logging.INFO)
+  unittest2.main()
