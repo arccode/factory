c949c5ff3a8d696254ac3be870d765174000cefb
Bernie Thompson <bhthompson@chromium.org>
2013-02-22 17:08:34 -0800

    Add in eMMC Version Verification Test
    This test can be used to verify that a particular firmware revision (PRV) is
    used in the eMMC on a unit.
    
    A list of manufacturer IDs (MID) and product names (PNM) each associated with
    a regex allows flexibility in enabling different version checking for
    different manufacturers/parts.
    
    This test is structured based on smart_check_fw_version.py.
    
    BUG=chrome-os-partner:17640
    TEST=Manual, run below test on DUT, verify pass/fail as expected.
    OperatorTest(
        id='CheckeMMCFirmwareVersion',
        label_zh=u'检查eMMC固件版本',
        pytest_name='emmc_check_fw_version',
        dargs={'valid_versions': [
               ('45', 'SEM16G', r'..'),
               ('70', 'MMC16G', r'..')]}),
    OperatorTest(
        id='BVCheckeMMCFirmwareVersion',
        label_zh=u'检查eMMC固件版本',
        pytest_name='emmc_check_fw_version',
        dargs={'valid_versions': [
               ('45', 'SEM16G', r'9.'),
               ('70', 'MMC16G', r'..')]}),
    OperatorTest(
        id='NMCheckeMMCFirmwareVersion',
        label_zh=u'检查eMMC固件版本',
        pytest_name='emmc_check_fw_version',
        dargs={'valid_versions': [
               ('12', 'SEM16G', r'..'),
               ('23', 'MMC16G', r'..')]}),
    
    Run doctests:
    cd $EMMC_CHECK_FW_VERSION_PY_LOCATION
    mkdir -p cros
    touch cros/__init__.py
    ln -s ~/chromiumos/src/platform/factory/py cros/factory
    python -m doctest -v emmc_check_fw_version.py
    
    Change-Id: Ied434fcc06883f3a2e49d90fc4d624f6ba55b4e0
    Reviewed-on: https://gerrit.chromium.org/gerrit/44006
    Tested-by: Bernie Thompson <bhthompson@chromium.org>
    Reviewed-by: Katie Roberts-Hoffman <katierh@chromium.org>
    Commit-Queue: Bernie Thompson <bhthompson@chromium.org>

---
+++ py/test/pytests/emmc_check_fw_version.py
@@ -0,0 +1,131 @@
+# -*- mode: python; coding: utf-8 -*-
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+'''Checks eMMC firmware version using the CID register values from sysfs.
+
+This depends on the kernel exposing the CID fields directly in sysfs, this
+enables future proofing by leaving the CID parsing to the kernel. The CID
+register is only read for the purposes of logging.
+
+If the test fails, then the test displays an error message and hangs forever.
+
+Values used in this test, these are defined by JEDEC:
+
+CID: Card IDentification register, 128 bit wide register readable from a
+     standard eMMC device providing basic identification information.
+MID: Manufacturer ID, 8 bit field in CID register, unique to a given eMMC
+     vendor.
+PNM: Product name, 48 bit wide field in CID register, a 6 ASCII character
+     string providing the name of a eMMC device.
+PRV: Product revision, 8 bit field in CID register, a vendor specific value
+     typically representing a firmware version.
+'''
+
+
+import logging
+import re
+import os
+import unittest
+
+from cros.factory.test import test_ui
+from cros.factory.test import ui_templates
+from cros.factory.test.args import Arg
+
+class eMMCCheckFWVersionTest(unittest.TestCase):
+  ARGS = [
+    Arg('valid_versions', list, 'A list of tuples, specifying the MID, PNM '
+        'and a regex for the versions supported for that MID/PNM.'),
+    Arg('cid_path', str, 'Path to CID value',
+        default='/sys/devices/dw_mmc.0/mmc_host/mmc0/mmc0:0001/cid'),
+    Arg('mid_path', str, 'Path to MID value',
+        default='/sys/devices/dw_mmc.0/mmc_host/mmc0/mmc0:0001/manfid'),
+    Arg('pnm_path', str, 'Path to PNM value',
+        default='/sys/devices/dw_mmc.0/mmc_host/mmc0/mmc0:0001/name'),
+    Arg('prv_path', str, 'Path to PRV value',
+        default='/sys/devices/dw_mmc.0/mmc_host/mmc0/mmc0:0001/prv'),
+  ]
+
+  def _ValidatePRVField(self, mid, pnm, prv, valid_versions):
+    """Validate the PRV field for a given manufacturer.
+
+    Args:
+      mid: string of the hex representation of the MID value
+      pnm: string of the PNM value
+      prv: string of the hex representation of the PRV value
+      valid_versions: A list of tuples, specifying the MID, PNM, and a regex
+                      for the versions supported for that MID/PNM.
+
+    Returns:
+      Boolean, true if FW revision is valid, false if not.
+
+    Raises:
+      ValueError if the MID value is not found in the test valid_versions
+      AssertionErrror if prv length is not 2
+
+    # Doctests
+    >>> from cros.factory.test.args import Args
+    >>> test = eMMCCheckFWVersionTest()
+    >>> test._ValidatePRVField("00", "PARTNM", "01", [("00", "PARTNM", r"..")])
+    True
+    >>> test._ValidatePRVField("00", "PARTNM", "10", [("00", "PARTNM", r"2.")])
+    False
+    >>> test._ValidatePRVField("00", "BADPRT", "01", [("00", "PARTNM", r"..")])
+    Traceback (most recent call last):
+        ...
+    ValueError: MID 00 PNM BADPRT not found in test list, wrong eMMC?
+    >>> test._ValidatePRVField("99", "PARTNM", "01", [("00", "PARTNM", r"..")])
+    Traceback (most recent call last):
+        ...
+    ValueError: MID 99 PNM PARTNM not found in test list, wrong eMMC?
+    >>> test._ValidatePRVField("00", "PART", "123", [("00", "PART", r"..")])
+    Traceback (most recent call last):
+        ...
+    AssertionError: Invalid prv length
+    """
+    for valid_mid, valid_pnm, regex in valid_versions:
+      if valid_mid == mid and valid_pnm == pnm:
+        self.assertTrue(len(prv) == 2, 'Invalid prv length')
+        if re.match(regex, prv):
+          return True
+        else:
+          return False
+    raise ValueError('MID %s PNM %s not found in test list, wrong eMMC?' %
+                     (mid, pnm))
+
+  def runTest(self):
+    self.assertTrue(os.path.exists(self.args.cid_path), 'cid_path %s is not '
+                    'found, bad path?' % (self.args.cid_path))
+    self.assertTrue(os.path.exists(self.args.mid_path), 'mid_path %s is not '
+                    'found, bad path?' % (self.args.mid_path))
+    self.assertTrue(os.path.exists(self.args.pnm_path), 'pnm_path %s is not '
+                    'found, bad path?' % (self.args.pnm_path))
+    self.assertTrue(os.path.exists(self.args.prv_path), 'prv_path %s is not '
+                    'found, bad path?' % (self.args.prv_path))
+    cid = open(self.args.cid_path).read().strip()
+    mid = open(self.args.mid_path).read().strip()[-2:]
+    pnm = open(self.args.pnm_path).read().strip()
+    prv = open(self.args.prv_path).read().strip()[-2:]
+    logging.info('Raw CID value: %s' % cid)
+    logging.info('MID: %s, PNM: %s, PRV: %s' % (mid, pnm, prv))
+    if self._ValidatePRVField(mid, pnm, prv, self.args.valid_versions):
+      logging.info('eMMC firmware version is correct.')
+      return # Pass the test
+
+    ui = test_ui.UI()
+    template = ui_templates.OneSection(ui)
+    template.SetTitle(test_ui.MakeLabel(
+        'eMMC Firmware Version Incorrect',
+        'eMMC 韧体版本不对'))
+    template.SetState(
+        '<div class=test-status-failed style="font-size: 150%">' +
+        test_ui.MakeLabel(
+            'The eMMC firmware version (%s) is incorrect. '
+            '<br>Please run the eMMC firmware update tool.' % prv,
+
+            'eMMC 韧体版（%s）版本不对。'
+            '<br>必须更新 eMMC 韧体并重新安装工厂测试软件。' % prv) +
+        '</div>')
+    ui.Run()  # Forever
