37b6c84d0f8783d71d3ab153fe9823a487d46971
Bernie Thompson <bhthompson@chromium.org>
2013-02-28 15:49:43 -0800

    Add in suspend/resume pytest
    This is a basic suspend/resume test with the ability to select ranges of time
    for suspended and resumed that are selected randomly.
    
    BUG=chrome-os-partner:17634
    TEST=Manual, run the below test list on DUT.
    
    OperatorTest(
        label_en='Suspend/Resume (%s %s)' % (
            _RESUME_ITERATIONS,
            'time' if _RESUME_ITERATIONS == 1 else 'times'),
        label_zh=u'睡眠、唤醒 (%s 次)' % _RESUME_ITERATIONS,
        pytest_name='suspend_resume',
        dargs={'cycles': _RESUME_ITERATIONS}),
    
    Change-Id: I1994d94433a55c63883af5677662c9a535036a5d
    Reviewed-on: https://gerrit.chromium.org/gerrit/44340
    Commit-Queue: Bernie Thompson <bhthompson@chromium.org>
    Reviewed-by: Bernie Thompson <bhthompson@chromium.org>
    Tested-by: Bernie Thompson <bhthompson@chromium.org>

---
+++ py/test/pytests/suspend_resume.py
@@ -0,0 +1,138 @@
+# -*- mode: python; coding: utf-8 -*-
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+'''Suspends and resumes the device an adjustable number of times for
+adjustable random lengths of time.
+
+This uses the powerd_suspend utility and the rtc's wakealarm entry in sysfs.
+
+Note that the rtc sysfs entry may vary from device to device, the test_list
+must define the path to the correct sysfs entry for the specific device, the
+default assumes a typical /sys/class/rtc/rtc0 entry.
+'''
+
+
+import logging
+import os
+import random
+import re
+import threading
+import time
+import unittest
+
+from cros.factory.test import test_ui
+from cros.factory.test.args import Arg
+from cros.factory.test.ui_templates import OneSection
+from cros.factory.utils.process_utils import Spawn
+
+_TEST_TITLE = test_ui.MakeLabel('Suspend/Resume Test', zh=u'暂停/恢复测试')
+_MSG_CYCLE = test_ui.MakeLabel('Suspend/Resume:', zh=u'暂停/恢复:')
+_ID_CYCLES = 'sr_cycles'
+_ID_RUN = 'sr_run'
+_TEST_BODY = ('<font size="20">%s <div id="%s"></div> of \n'
+              '<div id="%s"></div></font>') % (_MSG_CYCLE, _ID_RUN, _ID_CYCLES)
+
+class SuspendResumeTest(unittest.TestCase):
+  ARGS = [
+    Arg('cycles', int, 'Number of cycles to suspend/resume', default=1),
+    Arg('suspend_delay_max_secs', int, 'Max time in sec during suspend per '
+        'cycle', default=10),
+    Arg('suspend_delay_min_secs', int, 'Min time in sec during suspend per '
+        'cycle', default=5),
+    Arg('resume_delay_max_secs', int, 'Max time in sec during resume per cycle',
+        default=10),
+    Arg('resume_delay_min_secs', int, 'Min time in sec during resume per cycle',
+        default=5),
+    Arg('resume_worst_case_secs', int, 'The worst case time a device is '
+        'expected to take to resume', default=20),
+    Arg('wakealarm_path', str, 'Path to the wakealarm file',
+        default='/sys/class/rtc/rtc0/wakealarm'),
+    Arg('time_path', str, 'Path to the time (since_epoch) file',
+        default='/sys/class/rtc/rtc0/since_epoch'),
+  ]
+
+  def setUp(self):
+    self.assertTrue(os.path.exists(self.args.wakealarm_path), 'wakealarm_path '
+                    '%s is not found, bad path?' % (self.args.wakealarm_path))
+    self.assertTrue(os.path.exists(self.args.time_path), 'time_path %s is not '
+                    'found, bad path?' % (self.args.time_path))
+    self._ui = test_ui.UI()
+    self._template = OneSection(self._ui)
+    self._template.SetTitle(_TEST_TITLE)
+    self._template.SetState(_TEST_BODY)
+
+  def _ReadSuspendCount(self):
+    """Read the current suspend count from /sys/kernel/debug/suspend_stats.
+    This assumes the first line of suspend_stats contains the number of
+    successfull suspend cycles.
+
+    Args:
+      None.
+
+    Returns:
+      Int, the number of suspends the system has executed since last reboot.
+    """
+    self.assertTrue(os.path.exists('/sys/kernel/debug/suspend_stats'),
+                    'suspend_stats file not found.')
+    line_content = open('/sys/kernel/debug/suspend_stats').read().strip()
+    return int(re.search(r'[0-9]+', line_content).group(0))
+
+  def _VerifySuspended(self, count, resume_at):
+    """Verify that a reasonable suspend has taken place.
+
+    Args:
+      count: expected number of suspends the system has executed
+      resume_at: expected time since epoch to have resumed
+
+    Returns:
+      Boolean, True if suspend was valid, False if not.
+    """
+    cur_time = int(open(self.args.time_path).read().strip())
+    if cur_time < resume_at:
+      logging.info('Premature wake detected (%ds early), suprious event? (got '
+                   'touched?)', resume_at - cur_time)
+      return False
+    elif cur_time > (resume_at + self.args.resume_worst_case_secs):
+      logging.info('Late wake detected (%ds > %ds delay), timer failure?',
+                   cur_time - resume_at, self.args.resume_worst_case_secs)
+      return False
+    actual_count = self._ReadSuspendCount()
+    if actual_count < count:
+      logging.info('Suspend count %d too low, expected %d, no suspend?',
+                   actual_count, count)
+      return False
+    elif actual_count > count:
+      logging.info('Suspend count %d too high, expected %d, spurious suspend?',
+                   actual_count, count)
+      return False
+    return True
+
+  def _Run(self):
+    """Main test program running in a separate thread."""
+    self._ui.SetHTML(self.args.cycles, id=_ID_CYCLES)
+    initial_suspend_count = self._ReadSuspendCount()
+    for run in range(1, self.args.cycles + 1):
+      self._ui.SetHTML(run, id=_ID_RUN)
+      cur_time = int(open(self.args.time_path).read().strip())
+      suspend_time = random.randint(self.args.suspend_delay_min_secs,
+                     self.args.suspend_delay_max_secs)
+      logging.info('Suspend %d of %d for %d seconds.',
+                   run, self.args.cycles, suspend_time)
+      resume_at = suspend_time + cur_time
+      open(self.args.wakealarm_path, 'w').write(str(resume_at))
+      Spawn('powerd_suspend', check_call=True, log_stderr_on_error=True)
+      self.assertTrue(self._VerifySuspended(initial_suspend_count + run,
+                      resume_at), 'Device may not have suspended properly.')
+      resume_time = random.randint(self.args.resume_delay_min_secs,
+                    self.args.resume_delay_max_secs)
+      logging.info('Resumed %d of %d for %d seconds',
+                   run, self.args.cycles, resume_time)
+      time.sleep(resume_time)
+    self._ui.Pass()
+
+  def runTest(self):
+    threading.Thread(target=self._Run).start()
+    self._ui.Run()
