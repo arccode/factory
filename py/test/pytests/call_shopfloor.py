6197ebe239d7aa5c9363866ec202506fe4e54893
Jon Salz <jsalz@chromium.org>
2013-03-07 17:12:08 +0800

    Add support for return value action.
    BUG=None
    TEST=Manual on device
    
    Change-Id: I9a5b93ac94670a0719e78fac68dcf48ce6629334
    Reviewed-on: https://gerrit.chromium.org/gerrit/44820
    Tested-by: Jon Salz <jsalz@chromium.org>
    Reviewed-by: Rong Chang <rongchang@chromium.org>
    Commit-Queue: Jon Salz <jsalz@chromium.org>

--- py/test/pytests/call_shopfloor.py
+++ py/test/pytests/call_shopfloor.py
@@ -4,6 +4,14 @@
 # found in the LICENSE file.
 
 
+"""Test to call a shopfloor method.
+
+The test may also perform an action based on the return value.
+See RETURN_VALUE_ACTIONS for the list of possible actions.
+"""
+
+
+import logging
 import types
 import unittest
 
@@ -23,14 +31,33 @@ class Env(object):
 
 
 class CallShopfloor(unittest.TestCase):
+  # Possible values for the "action" handler
+  RETURN_VALUE_ACTIONS = {
+      # Update device data with the returned dictionary.
+      'update_device_data': shopfloor.UpdateDeviceData
+  }
+
   ARGS = [
     Arg('method', str,
         'Name of shopfloor method to call'),
     Arg('args', (list, types.FunctionType),
-        'Method arguments')
+        'Method arguments'),
+    Arg('action', str,
+        ('Action to perform with return value; one of %s' %
+         sorted(RETURN_VALUE_ACTIONS.keys())),
+        optional=True),
   ]
 
   def runTest(self):
+    if self.args.action:
+      action_handler = self.RETURN_VALUE_ACTIONS.get(self.args.action)
+      self.assertTrue(
+          action_handler,
+          'Invalid action %r; should be one of %r' % (
+              self.args.action, sorted(self.RETURN_VALUE_ACTIONS.keys())))
+    else:
+      action_handler = lambda value: None
+
     env = type('Env', (), dict(state=factory.get_state_instance(),
                                shopfloor=shopfloor))
     if isinstance(self.args.args, types.FunctionType):
@@ -38,4 +65,7 @@ class CallShopfloor(unittest.TestCase):
       self.assertTrue(isinstance(self.args.args, types.FunctionType))
 
     method = getattr(shopfloor.get_instance(detect=True), self.args.method)
-    method(*self.args.args)
+    logging.info('Invoking %s(%s)',
+                 self.args.method, ', '.join(repr(x) for x in self.args.args))
+
+    action_handler(method(*self.args.args))
