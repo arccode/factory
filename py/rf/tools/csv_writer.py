8303a2a72fdddd4ccb3c1d40913716a65cce8dd8
Chun-ta Lin <itspeter@google.com>
2013-03-11 14:07:32 +0800

    help function to write CSV files.
    wrapper for csv.writerow() to export python dict objects, an unittest
    is included in this change list.
    
    BUG=chrome-os-partner:17967
    TEST=make lint; make test.
    
    Change-Id: I90cd39234cdb873a4323a62681deecd99171fac2
    Reviewed-on: https://gerrit.chromium.org/gerrit/45054
    Reviewed-by: Vic Yang <victoryang@chromium.org>
    Commit-Queue: Chun-Ta Lin <itspeter@google.com>
    Tested-by: Chun-Ta Lin <itspeter@google.com>

---
+++ py/rf/tools/csv_writer.py
@@ -0,0 +1,47 @@
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""This module provides help functions to write CSV (Comma Separated Values)
+files.
+"""
+
+import csv
+import logging
+import pprint
+
+def WriteCsv(target, list_of_dict, key_orders):
+  '''Writes a list of dict to a CSV file.
+
+  The columns' order are assigned by key_orders. For columns that is not
+  listed in key_orders, their order are not guaranteed.
+
+  Args:
+    target: the file path of output csv.
+    list_of_dict: a list of dicts converts to CSV. Every single dict will
+      contributed as a single row in the CSV.
+    key_orders: columns' order from left to right.
+  '''
+  # Scaning over list_of_dict to extract all the columns.
+  fieldnames = set()
+  for dict_obj in list_of_dict:
+    fieldnames.update(set(dict_obj.keys()))
+
+  # Prepare the fieldnames to pass into DictWriter.
+  for key in key_orders:
+    fieldnames.discard(key)
+  for key in fieldnames:
+    key_orders.append(key)
+
+  logging.debug('Determined key_orders = \n%s\n', pprint.pformat(key_orders))
+
+  with open(target, 'wb') as fd:
+    writer = csv.DictWriter(fd, key_orders)
+    # Workaround for python before 2.7 that doesn't have a writer.writeheader
+    header_dict = dict()
+    for key in key_orders:
+      header_dict[key] = key
+    writer.writerow(header_dict)
+
+    for dict_obj in list_of_dict:
+      writer.writerow(dict_obj)
---
+++ py/rf/tools/csv_writer_unittest.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging
+import unittest2
+
+import factory_common  # pylint: disable=W0611
+from cros.factory.rf.tools.csv_writer import WriteCsv
+from cros.factory.utils.file_utils import UnopenedTemporaryFile
+
+
+class CsvWriterTest(unittest2.TestCase):
+  def testReadSingleCell(self):
+    with UnopenedTemporaryFile() as tmp_file:
+      WriteCsv(tmp_file,
+               [{'col_1': 1}, {'col_2': 2}, {'col_3': 3}],
+               ['col_2', 'col_1'])
+      with open(tmp_file, 'r') as fd:
+        self.assertEqual(fd.readline().strip(), 'col_2,col_1,col_3')
+        self.assertEqual(fd.readline().strip(), ',1,')
+        self.assertEqual(fd.readline().strip(), '2,,')
+        self.assertEqual(fd.readline().strip(), ',,3')
+
+if __name__ == '__main__':
+  logging.basicConfig(level=logging.INFO)
+  unittest2.main()
