0bebb9e23bcefd33883b8444710093db32df6cb4
Rong Chang <rongchang@chromium.org>
2013-03-18 20:52:54 +0800

    ShopFloor v2 constants and launcher env accessors
    This change includes system wide constants, env accessors.
    
    Usage:
      from cros.factory.shopfloor.launcher import constants
      from cros.factory.shopfloor.launcher import env
    
    BUG=chrome-os-partner:17963
    TEST=none
    
    Change-Id: Ic66b9ba5510fec1f4d8508c3af664f1edc5896fb
    Reviewed-on: https://gerrit.chromium.org/gerrit/45733
    Commit-Queue: Rong Chang <rongchang@chromium.org>
    Reviewed-by: Rong Chang <rongchang@chromium.org>
    Tested-by: Rong Chang <rongchang@chromium.org>

---
+++ py/shopfloor/launcher/constants.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Uber shopfloor constants, accessors and their helper functions."""
+
+# Constants
+DEFAULT_BIND_ADDRESS = '0.0.0.0'
+DEFAULT_BIND_PORT = 8082
+DEFAULT_RSYNC_PORT = DEFAULT_BIND_PORT + 1
+COMMAND_PORT = DEFAULT_BIND_PORT + 2
+DEFAULT_FCGI_PORT = DEFAULT_BIND_PORT + 3
+
+SHOPFLOOR_INSTALL_DIR = '/var/db/factory'
+SHOPFLOOR_SYMLINK_DIR = '/usr/local/bin'
+FCGI_EXEC = 'shopfloor.fcgi'
+RESOURCES_DIR = 'resources'
+UPDATES_DIR = 'updates'
+
+# Maximum number of file descriptors when run as root
+HTTPD_MAX_FDS = 32768
+# Maximum number of connections
+HTTPD_MAX_CONN = HTTPD_MAX_FDS / 2
+
---
+++ py/shopfloor/launcher/env.py
@@ -0,0 +1,45 @@
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+"""
+Shop Floor Launcher shared environment
+
+To use it, import the class and use class variables and class methods without
+instanciate.
+
+Example:
+  from cros.factory.shopfloor.launcher import env
+  system_dir = env.runtime_dir
+"""
+
+
+import os
+
+import factory_common  # pylint: disable=W0611
+from cros.factory.shopfloor.launcher import constants
+
+# ShopFloor runtime root dir
+runtime_dir = constants.SHOPFLOOR_INSTALL_DIR
+# ShopFloor bind address
+bind_address = constants.DEFAULT_BIND_ADDRESS
+# ShopFloor httpd bind port
+bind_port = constants.DEFAULT_BIND_PORT
+# Factory update rsync server port
+rsync_port = constants.DEFAULT_RSYNC_PORT
+# FastCGI service bind port
+fcgi_port = constants.DEFAULT_FCGI_PORT
+
+def GetFCGIExec():
+  """Gets FastCGI program path."""
+  return os.path.join(runtime_dir, constants.FCGI_EXEC)
+
+def GetResourcesDir():
+  """Gets shopfloor resources dir."""
+  return os.path.join(runtime_dir, constants.RESOURCES_DIR)
+
+def GetUpdatesDir():
+  """Gets update dir."""
+  return os.path.join(runtime_dir, constants.UPDATES_DIR)
+
