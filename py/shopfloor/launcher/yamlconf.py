de858801dd7e5f7dd7612a5f9e96d90a96ccb0a6
Rong Chang <rongchang@chromium.org>
2013-03-18 21:03:27 +0800

    Shopfloor launcher configuration schema class
    yamlconf.py contains launcher, rf and other YAML configurations.
    
    The change includes basic skeleton of launcher schema only.
    
    BUG=chrome-os-partner:17963
    TEST=none
    
    Change-Id: I0670a5e0d191191593da41613d955e451468355c
    Reviewed-on: https://gerrit.chromium.org/gerrit/45734
    Commit-Queue: Rong Chang <rongchang@chromium.org>
    Reviewed-by: Rong Chang <rongchang@chromium.org>
    Tested-by: Rong Chang <rongchang@chromium.org>

---
+++ py/shopfloor/launcher/yamlconf.py
@@ -0,0 +1,83 @@
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+import yaml
+
+import factory_common  # pylint: disable=W0611
+from cros.factory.schema import FixedDict, Dict, Scalar, List
+from cros.factory.shopfloor.launcher import constants
+
+
+class LauncherYAMLConfig(dict):
+  """ShopFloor Launcher YAML config file validator."""
+
+  _SCHEMA = FixedDict(
+      'Fixed top level launcher config fields',
+      optional_items={
+          # Network installer settings.
+          'network_install': FixedDict(
+              'network_install_fields',
+              items={
+                'board': Dict(
+                    'boards',
+                    Scalar('Board name', str),
+                    FixedDict(
+                        'Resources for the board',
+                        items={
+                            'config': Scalar('download_conf', str),
+                            'efi': Scalar('EFI partition image', str),
+                            'firmware': Scalar('Firmware updater', str),
+                            'hwid': Scalar('HWID resource', str),
+                            'oem': Scalar('OEM partition image', str),
+                            'rootfs-release': Scalar(
+                                'Release rootfs and Kernel',
+                                 str),
+                            'rootfs-test': Scalar(
+                                'Factory test rootfs and Kernel',
+                                str),
+                            'state': Scalar('Stateful partition image', str)},
+                        optional_items={
+                            'complete': Scalar(
+                                'Complete script to run after download',
+                                str)}))},
+                optional_items={
+                  'netboot_kernel': Scalar('Network tftp boot uImage', str),
+                  'dhcp': FixedDict(
+                      'DHCP server settings',
+                      items={
+                          'interface': List('Interface list',
+                                            Scalar('Ethernet interface', str)),
+                          'subnet': Scalar('DHCP subnet', str),
+                          'netmask': Scalar('Subnet netmask', str),
+                          'range': List('DHCP range, start IP and end IP',
+                                        Scalar('IP address', str))})})},
+      items={
+          # Info fields includes version and note. Both fields are human
+          # readable one-line strings for command line utility to display.
+          'info': FixedDict(
+              'Human readable info strings',
+              items={'version': Scalar('Factory bundle version', str)},
+              optional_items={'note': Scalar('One line note for this release',
+                                             str)}),
+
+          # Base ShopFloor configurations, includes factory.par resource, the
+          # module resource for factory integration and the http daemon bind
+          # port.
+          'shopfloor': FixedDict(
+              'ShopFloor software configuration',
+              items={
+                  'factory_software': Scalar('software_res', str),
+                  'shopfloor_module': Scalar('module_string', str)},
+              optional_items={'port': Scalar('port_number', int)})})
+
+  def __init__(self, config_file):
+    with open(config_file, 'r') as f:
+      launcher_config = yaml.load(f)
+      self._SCHEMA.Validate(launcher_config)
+      if 'port' not in launcher_config['shopfloor']:
+        launcher_config['shopfloor']['port'] = (
+            constants.DEFAULT_BIND_PORT)
+      super(LauncherYAMLConfig, self).__init__(launcher_config)
+
