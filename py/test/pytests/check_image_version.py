c5348b06274036ee25246207f763d6f903fb4f88
Vic Yang <victoryang@chromium.org>
2013-04-12 17:26:26 +0800

    Add a test to check image version
    If the image version is smaller than minimum allowed version:
      - Flashs netboot firmware and reboots
      - Only shows error message if netboot firmware is not provided
    
    BUG=chrome-os-partner:18646
    TEST=Manual
    
    Change-Id: I7c2e1ac831608c89fc29a278af8f6706e9b7e271
    Signed-off-by: Vic Yang <victoryang@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/47983
    Reviewed-by: Cheng-Yi Chiang <cychiang@chromium.org>

---
+++ py/test/pytests/check_image_version.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# DESCRIPTION :
+# This factory test checks image version in lsb-release. If the version doesn't
+# match what's provided in the test argument, flash netboot firmware if it is
+# provided.
+
+from distutils.version import StrictVersion
+import unittest
+
+from cros.factory.event_log import Log
+from cros.factory.system import SystemInfo
+from cros.factory.test import factory
+from cros.factory.test import test_ui
+from cros.factory.test import ui_templates
+from cros.factory.test.args import Arg
+from cros.factory.test.factory_task import FactoryTask, FactoryTaskManager
+from cros.factory.utils.process_utils import Spawn
+
+
+_TEST_TITLE = test_ui.MakeLabel('Check Image Version', u'检查映像版本')
+
+_CSS = """
+.start-font-size {
+  font-size: 2em;
+}
+"""
+
+# Messages for tasks
+_MSG_VERSION_MISMATCH = test_ui.MakeLabel(
+    'Factory image version is incorrect. Please re-image this device.',
+    u'映像版本不正确，请重新安装。',
+    'start-font-size test-error')
+_MSG_NETBOOT = test_ui.MakeLabel(
+    'Factory image version is incorrect. Press space to re-image.',
+    u'映像版本不正确，请按空白键重新安装。')
+_MSG_REIMAGING = test_ui.MakeLabel(
+    'Flashing netboot firmware...',
+    u'烧录网路开机固件...')
+_MSG_FLASH_ERROR = test_ui.MakeLabel(
+    'Error flashing netboot firmware!',
+    u'烧录网路开机固件失败',
+    'start-font-size test-error')
+
+# Javascripts and HTML for tasks
+_JS_SPACE = '''
+    function enableSpaceKeyPressListener() {
+      window.addEventListener(
+          "keypress",
+          function(event) {
+            if (event.keyCode == " ".charCodeAt(0)) {
+              window.test.sendTestEvent("space_pressed", {});
+            }
+          });
+      window.focus();
+    }'''
+_LSB_RELEASE_PATH = '/etc/lsb-release'
+
+
+class ImageCheckTask(FactoryTask):
+  def __init__(self, test): # pylint: disable=W0231
+    self._test = test
+
+  def PromptReimage(self):
+    self._test.template.SetState(_MSG_NETBOOT)
+    self._test.ui.RunJS(_JS_SPACE)
+    self._test.ui.CallJSFunction('enableSpaceKeyPressListener')
+    self._test.ui.AddEventHandler('space_pressed', self.Reimage)
+
+  def Reimage(self, dummy_event):
+    self._test.template.SetState(_MSG_REIMAGING)
+    try:
+      Spawn(['/usr/local/factory/bin/flash_netboot',
+             '-i', self._test.args.netboot_fw,
+             '-y'], check_call=True, log=True, log_stderr_on_error=True)
+      Spawn(['reboot'])
+    except: # pylint: disable=W0702
+      self._test.template.SetState(_MSG_FLASH_ERROR)
+
+  def Run(self):
+    ver = SystemInfo().factory_image_version
+    Log('image_version', version=ver)
+    if StrictVersion(ver) < StrictVersion(self._test.args.min_version):
+      factory.console.error('Current factory image version is incorrect: %s',
+                            ver)
+      if self._test.args.netboot_fw:
+        self.PromptReimage()
+      else:
+        self._test.template.SetState(_MSG_VERSION_MISMATCH)
+      return
+    self.Pass()
+
+
+class CheckImageVersionTest(unittest.TestCase):
+  ARGS = [
+    Arg('min_version', str, 'Minimum allowed factory image version.'),
+    Arg('netboot_fw', str, 'The path to netboot firmware image.',
+        default=None, optional=True)]
+
+  def __init__(self, *args, **kwargs):
+    super(CheckImageVersionTest, self).__init__(*args, **kwargs)
+    self._task_list = [ImageCheckTask(self)]
+    self.ui = test_ui.UI()
+    self.template = ui_templates.OneSection(self.ui)
+    self.ui.AppendCSS(_CSS)
+    self.template.SetTitle(_TEST_TITLE)
+
+  def runTest(self):
+    FactoryTaskManager(self.ui, self._task_list).Run()
