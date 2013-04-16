b841d9640857f99c21f6e6b1a3775ef8eb9fb109
Cheng-Yi Chiang <cychiang@chromium.org>
2013-04-16 18:43:19 +0800

    CHERRY-PICK: Add core_dump_manager.py to handle core dump files
    CoreDumpManager sets core_pattern to put core dump files into
    /var/factory/crash.
    It provides ScanFiles() and ClearFiles() to handle core dump files.
    
    BUG=chrome-os-partner:18644
    TEST=unittest.
         Also check coredump on dut is in /var/factory/crash
    
    Original Change-Id: I2a0d2774368cd484726476de90807fa58dc84963
    Original Reviewed-on: https://gerrit.chromium.org/gerrit/48245
    Original Tested-by: Cheng-Yi Chiang <cychiang@chromium.org>
    Original Commit-Queue: Cheng-Yi Chiang <cychiang@chromium.org>
    Original Reviewed-by: Cheng-Yi Chiang <cychiang@chromium.org>
    
    Change-Id: I19ee8d8d6557f8e80b0cb415b39fe8cfbef00f97
    Reviewed-on: https://gerrit.chromium.org/gerrit/50107
    Reviewed-by: Vic Yang <victoryang@chromium.org>
    Tested-by: Cheng-Yi Chiang <cychiang@chromium.org>

---
+++ py/system/core_dump_manager.py
@@ -0,0 +1,95 @@
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import glob
+import logging
+import os
+
+import factory_common # pylint: disable=W0611
+from cros.factory.test import utils
+
+DEFAULT_CRASH_PATH = '/var/factory/crash'
+
+
+class CoreDumpManagerException(Exception):
+  pass
+
+
+class CoreDumpManager(object):
+  """The manager that takes care of core dump files.
+
+  Properties:
+    crash_dir: The directory of core dump files.
+    watchlist: The file patterns to match. E.g. ['*glbench*']
+  """
+  def __init__(self, watchlist=None, crash_dir=DEFAULT_CRASH_PATH):
+    self._crash_dir = crash_dir
+    self._watchlist = watchlist
+    self._SetCoreDump()
+
+  def _SetCoreDump(self):
+    """Sets core dump files to be unlimited and set core_pattern."""
+    if utils.in_chroot():
+      return
+    if not os.path.exists(self._crash_dir):
+      os.mkdir(self._crash_dir)
+    with open('/proc/sys/kernel/core_pattern', 'w') as f:
+      f.write(os.path.join(self._crash_dir, 'core.%p:%s:%u:%e'))
+
+  def ScanFiles(self):
+    """Scans the core dump directory and returns matched list of files.
+
+    Scans the core dump directory and check if there is any file
+    matching watchlist. Delete all the files that do not match watchlist.
+
+    Returns:
+      A list of file paths that match watchlist. Returns [] if watchlist is
+      not specified, or there is no matched file.
+
+    Raises:
+      CoreDumpManagerException: If CoreDumpManager fails to remove unused
+          core dump files.
+    """
+    if not self._watchlist:
+      return []
+    watched_files = sum([glob.glob(os.path.join(self._crash_dir, x))
+        for x in self._watchlist], [])
+
+    try:
+      for root, dirs, files in os.walk(self._crash_dir):
+        for f in files:
+          if os.path.join(root, f) not in watched_files:
+            os.unlink(os.path.join(root, f))
+        for d in dirs:
+          os.rmdir(os.path.join(root, d))
+    except Exception as e:  # pylint: disable=W0703
+      logging.exception('Unable to remove unused core dump files')
+      raise CoreDumpManagerException(e)
+    if watched_files:
+      logging.warning('Found core dump files %s !', watched_files)
+    return watched_files
+
+  def ClearFiles(self, files):
+    """Removes files under core dump directory.
+
+    Args:
+      files: A list of file paths to remove. The path should be under
+          crash directry.
+
+    Raises:
+      CoreDumpManagerException: If any path in files is not in crash
+          directory or can not be removed.
+    """
+    for f in files:
+      if os.path.dirname(f) != self._crash_dir:
+        logging.error('Should not remove file %s that is not under'
+                      ' crash directory %s', f, self._crash_dir)
+        raise CoreDumpManagerException('file path is not correct')
+      try:
+        os.unlink(f)
+      except Exception as e:  # pylint: disable=W0703
+        logging.exception('Unable to remove used core dump files')
+        raise CoreDumpManagerException(e)
+      else:
+        logging.info('Removed %s', f)
---
+++ py/system/core_dump_manager_unittest.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# pylint: disable=W0212
+
+import factory_common  # pylint: disable=W0611
+
+import logging
+import mox
+import os
+import shutil
+import tempfile
+import unittest
+
+from cros.factory.system import core_dump_manager
+
+class CoreDumpManagerTest(unittest.TestCase):
+  def CreateFiles(self):
+    self.watched_file = tempfile.NamedTemporaryFile(
+        prefix='watch', dir=self.crash_dir, delete=False)
+    self.other_file = tempfile.NamedTemporaryFile(
+        prefix='other', dir=self.crash_dir, delete=False)
+
+  def setUp(self):
+    self.watchlist = ['*watch*']
+    self.crash_dir = tempfile.mkdtemp(prefix='core_dump_manager_unittest.')
+    self.mocker = mox.Mox()
+    self.watched_file = None
+    self.other_file = None
+
+  def tearDown(self):
+    shutil.rmtree(self.crash_dir)
+    self.mocker.UnsetStubs()
+
+  def testScan(self):
+    logging.info('Test ScanFiles()')
+    core_dump_manager.CoreDumpManager._SetCoreDump = \
+        self.mocker.CreateMockAnything()
+    core_dump_manager.CoreDumpManager._SetCoreDump()
+    self.mocker.ReplayAll()
+    manager = core_dump_manager.CoreDumpManager(
+        watchlist=self.watchlist, crash_dir=self.crash_dir)
+    self.CreateFiles()
+    # Should get a list containing watched_file.
+    self.assertEquals(manager.ScanFiles(), [self.watched_file.name])
+    # Other files should get deleted in ScanFiles().
+    self.assertEquals(os.listdir(self.crash_dir),
+                      [os.path.basename(self.watched_file.name)])
+    self.mocker.VerifyAll()
+
+  def testClear(self):
+    logging.info('Test ClearFiles()')
+    core_dump_manager.CoreDumpManager._SetCoreDump = \
+        self.mocker.CreateMockAnything()
+    core_dump_manager.CoreDumpManager._SetCoreDump()
+    self.mocker.ReplayAll()
+    manager = core_dump_manager.CoreDumpManager(
+        watchlist=self.watchlist, crash_dir=self.crash_dir)
+    self.CreateFiles()
+    watch_file = manager.ScanFiles()
+    # Should get a list containing watched_file.
+    # Other files should get deleted in ScanFiles().
+    self.assertEquals(watch_file, [self.watched_file.name])
+    manager.ClearFiles(watch_file)
+    # The watched file is deleted after ClearFiles()
+    self.assertEquals(os.listdir(self.crash_dir), [])
+    self.mocker.VerifyAll()
+
+
+if __name__ == '__main__':
+  logging.basicConfig(level=logging.INFO)
+  unittest.main()
+
