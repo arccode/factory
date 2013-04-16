b22cff596ad6f1adfbf1ad9f92001333e70e1cc4
Rong Chang <rongchang@chromium.org>
2013-04-17 03:45:14 +0800

    Access factory update server state
    Shopfloor server v1 includes the factory update server in thei
    shopfloor module. We're migrating to multiple process in v2, hence
    the factory update server will be launched externally.
    
    This change adds an update server state class that compatible to
    factory update server. So the shopfloor server can be ran in dual
    mode.
    
    BUG=chrome-os-partner:17963
    TEST=none
    
    Change-Id: Icb968bcda27a65c2bf8473143002994b3c25ce33
    Reviewed-on: https://gerrit.chromium.org/gerrit/48247
    Reviewed-by: Shuo-Peng Liao <deanliao@chromium.org>
    Commit-Queue: Rong Chang <rongchang@chromium.org>
    Tested-by: Rong Chang <rongchang@chromium.org>

---
+++ py/shopfloor/launcher/update_state.py
@@ -0,0 +1,85 @@
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Update Server State.
+
+When update server launched by shopfloor launcher, this class provides
+interfaces to query updater state.
+"""
+
+# TODO(rong): Update server state interfaces will be moved back to shopfloor
+# after we deprecate v1. The data shared between processes should store into
+# a local db.
+
+import glob
+import os
+
+
+FACTORY_DIR = 'factory'
+LATEST_MD5SUM = 'latest.md5sum'
+
+
+class FactoryUpdater(object):
+  """The class reports update bundle state.
+
+  Properties:
+    state_dir: Update state directory (generally shopfloor_data/update)
+    factory_dir: Updater bundle directory to hold previous and current contents
+        or factory bundles.
+    hwid_path: The path of hwid bundle.
+  """
+
+  def __init__(self, state_dir):
+    """Constructor.
+
+    Args:
+      state_dir: Update state directory (generally shopfloor_data/update).
+    """
+    self.state_dir = state_dir
+    self.factory_dir = os.path.join(state_dir, FACTORY_DIR)
+    if not os.path.exists(self.factory_dir):
+      os.mkdir(self.factory_dir)
+
+  @property
+  def hwid_path(self):
+    """Gets hardware ID bundle pathname.
+
+    Returns:
+      When there's exactly 1 file that matches 'hwid_*.sh', this function
+      returns the pathname. Otherwise it returns None.
+    """
+    hwid_files = glob.glob(os.path.join(self.state_dir, 'hwid_*.sh'))
+    if len(hwid_files) == 1 and os.path.isfile(hwid_files[0]):
+      return hwid_files[0]
+    return None
+
+  # TODO(rong): Remove Start() and Stop() after we drop shopfloor v1
+  def Start(self):
+    pass
+
+  def Stop(self):
+    pass
+
+  def GetTestMd5sum(self):
+    """Returns the MD5SUM of the current update tarball."""
+    md5file = os.path.join(self.state_dir, FACTORY_DIR, LATEST_MD5SUM)
+    if not os.path.isfile(md5file):
+      return None
+    with open(md5file, 'r') as f:
+      return f.readline().strip()
+
+  def NeedsUpdate(self, device_md5sum):
+    """Checks if device_md5sum needs the update.
+
+    Args:
+      device_md5sum: The md5sum of factory environment on device.
+
+    Returns:
+      True: When there's an update available and its MD5SUM is not equal to
+            device's MD5SUM.
+      False: Where there's no ubdate bundle or both hashsums are equal.
+    """
+    current_md5sum = self.GetTestMd5sum()
+    return current_md5sum and (current_md5sum != device_md5sum)
+
