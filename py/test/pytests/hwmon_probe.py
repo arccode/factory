eaf8b8d897b692f68e19d0dce579aef2dd6be078
Gediminas Ramanauskas <gedis@chromium.org>
2013-04-22 15:52:56 -0700

    adding hwmon_probe pytest
    hwmon_probe pytest reads hwmon temperature sensors on the motherboard
    and verifies that the values make sense. These sensors are used to
    prevent device overheating.
    
    BUG=chrome-os-partner:19075
    TEST=hwmon_probe factory pytest works
    
    Change-Id: I8ffcc416a7061ca6fc62caa79524f8eee1b43ac3
    Reviewed-on: https://gerrit.chromium.org/gerrit/49136
    Commit-Queue: Gediminas Ramanauskas <gedis@chromium.org>
    Reviewed-by: Gediminas Ramanauskas <gedis@chromium.org>
    Tested-by: Gediminas Ramanauskas <gedis@chromium.org>

---
+++ py/test/pytests/hwmon_probe.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""
+A factory test to test the functionality of hwmon temp sensors.
+
+dargs:
+  hwmon_count: Number of hwmon temp sensors the system is supposed to have.
+  max_temp_delta_celsius: Max allowed delta between temp sensor and an average
+      temp. (default: 5 celsius)
+  min_temp_celsius: Min allowed temperature recorded by hwmon. (default: 15
+      celsius)
+  max_temp_celsius: Max allowed temperature recorded by hwmon. (default: 140
+      celsius)
+"""
+
+import glob
+import logging
+import os
+import unittest
+
+from cros.factory.test.args import Arg
+
+
+class HwmonProbeTest(unittest.TestCase):
+  """Tests hwmon temp sensors by reading all their values and identifying that
+  their indicated temperatures are in range as specified by min_temp_celsius and
+  max_temp_celsius. Also, ensures that deviation of any hwmon temp sensor is
+  less or equal to max_temp_delta_celsius from the mean temperature."""
+  ARGS = [
+    Arg('hwmon_count', int,
+        'Number of hwmon temp sensors the system is supposed to have.'),
+    Arg('delta_temp_max_celsius', int,
+        'Max allowed delta between temp sensor and an average temp.',
+        default=5),
+    Arg('min_temp_celsius', int,
+        'Min allowed temperature recorded by hwmon.', default=15),
+    Arg('max_temp_celsius', int,
+        'Max allowed temperature recorded by hwmon.', default=110),
+  ]
+
+  def _FindHwmons(self):
+    """Finds all hwmon temp sensors, verifies how many sensors are expected, and
+    returns a list of their paths."""
+    hwmon_temp_sensor = glob.glob('/sys/class/hwmon/hwmon*/device/temp*_input')
+
+    self.assertTrue(len(hwmon_temp_sensor) != 0, 'No hwmon devices found.')
+
+    logging.info('Found hwmons:\n%s', '\n'.join(map(str, hwmon_temp_sensor)))
+
+    self.assertEquals(self.args.hwmon_count, len(hwmon_temp_sensor),
+                      'System has %d hwmon sensors instead of expected %d' %
+                      (len(hwmon_temp_sensor), self.args.hwmon_count))
+
+    return hwmon_temp_sensor
+
+  def _ReadTemp(self, sensor):
+    """Reads hwmon temperature, verifies it's within the expected range, and
+    returns the value to the caller"""
+    self.assertTrue(os.path.isfile(sensor) and os.access(sensor, os.R_OK),
+                    'Unable to locate sensor %s' % sensor)
+    try:
+      with open(sensor, 'r') as f:
+        t = int(f.readline().rstrip())/1000
+    except Exception as e:
+      raise IOError('Unable to open hwmon sensor %s : %s' % (sensor, e))
+
+    self.assertTrue(t > self.args.min_temp_celsius and
+                    t < self.args.max_temp_celsius,
+                    'Temp %d out of allowed temp range' % t)
+    return t
+
+  def runTest(self):
+    sensor_values = []
+
+    hwmon_temp_sensor = self._FindHwmons()
+
+    sensor_values = [self._ReadTemp(sensor) for sensor in hwmon_temp_sensor]
+
+    average_sensor_value = sum(sensor_values)/len(sensor_values)
+
+    for sensor_value in sensor_values:
+      self.assertLessEqual(abs(sensor_value-average_sensor_value),
+                           self.args.delta_temp_max_celsius,
+                           'temp %d C is out of range. Average temp: %d C' %
+                           (sensor_value, average_sensor_value))
