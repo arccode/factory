a9ca07283ef7144048257ac5a49803cad16617f8
Tom Wai-Hong Tam <waihong@chromium.org>
2013-04-23 16:40:43 +0800

    Minijack: Abstract the underlying database operations
    This CL introduces Database, Table, Executor, ExecutorFactory.
    
    Database abstracts the underlying database. It now uses sqlite3 as an
    implementation. Its main purpose is to get a Table instance.
    
    Table abstracts the behaviors of a database table, like table creation,
    row insertion, etc. It controls the database using SQL commands.
    
    Executor hides the underlying database operations, like executing an
    SQL query, fetching results, etc.
    
    ExecutorFactory generates Executor objects. It is passed from Database
    to Table, such that Table can be generate an executor. It helps separating
    Table from the database access.
    
    Check the detailed usages in the unit tests.
    
    Some of the code comes from py/minijack/parser/parser_base.py.
    
    BUG=chrome-os-partner:18749,chrome-os-partner:18798
    TEST=make test
    
    Change-Id: I6b0309025781ab1f4cae48fbc9260ac16cd6926c
    Reviewed-on: https://gerrit.chromium.org/gerrit/48885
    Reviewed-by: Andy Cheng <andycheng@chromium.org>
    Commit-Queue: Tom Wai-Hong Tam <waihong@chromium.org>
    Tested-by: Tom Wai-Hong Tam <waihong@chromium.org>

--- py/minijack/db.py
+++ py/minijack/db.py
@@ -2,7 +2,12 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import logging
 import inspect
+import sqlite3
+
+class DatabaseException(Exception):
+  pass
 
 class Field(object):
   '''The base class of fields.
@@ -227,3 +232,287 @@ def ToModelSubclass(model):
     return model
   else:
     raise ValueError('Not a valid Model subclass: %s' % model)
+
+class Table(object):
+  '''A database table.
+
+  It abstracts the behaviors of a database table, like table creation,
+  row insertion, etc. It controls the database using SQL operators.
+
+  Properties:
+    _executor_factory: A factory of executor objects.
+    _model: The model dict, the schema of the table.
+    _table_name: The name of the table.
+    _primary_key: A list of the primary key fields.
+  '''
+  def __init__(self, executor_factory):
+    self._executor_factory = executor_factory
+    self._model = None
+    self._table_name = None
+    self._primary_key = []
+
+  def Init(self, model):
+    '''Initializes the table, creating it if not exists.'''
+    self._model = ToModelSubclass(model)
+    self._table_name = model.GetModelName()
+    self._primary_key = model.GetPrimaryKey()
+    # Construct the SQL command.
+    columns = [k + ' ' + v for k, v in model.GetDbSchema().iteritems()]
+    if self._primary_key:
+      columns.append('PRIMARY KEY ( %s )' % ', '.join(self._primary_key))
+    sql_cmd = ('CREATE TABLE IF NOT EXISTS %s ( %s )' %
+               (self._table_name,
+                ', '.join(columns)))
+    executor = self._executor_factory.NewExecutor()
+    executor.Execute(sql_cmd, commit=True)
+
+  def InsertRow(self, row):
+    '''Inserts the row into the table.
+
+    Args:
+      row: A model instance containing the insert content.
+
+    Raises:
+      DatabaseException if not a valid model instance.
+    '''
+    if not self._model.IsValid(row):
+      raise DatabaseException('Insert a row with a wrong model.')
+
+    field_names = row.GetNonEmptyFieldNames()
+    field_values = row.GetNonEmptyFieldValues()
+    sql_cmd = ('INSERT INTO %s ( %s ) VALUES ( %s )' %
+               (self._table_name,
+                ', '.join(field_names),
+                ', '.join('?' * len(field_names))))
+    executor = self._executor_factory.NewExecutor()
+    executor.Execute(sql_cmd, field_values, True)
+
+  def UpdateRow(self, row):
+    '''Updates the row in the table.
+
+    Args:
+      row: A model instance containing the update content.
+
+    Raises:
+      DatabaseException if not a valid model instance.
+    '''
+    if not self._model.IsValid(row):
+      raise DatabaseException('Update a row with a wrong model.')
+
+    field_names = row.GetNonEmptyFieldNames()
+    field_values = row.GetNonEmptyFieldValues()
+    conditions = row.CloneOnlyPrimaryKey()
+    condition_names = conditions.GetNonEmptyFieldNames()
+    condition_values = conditions.GetNonEmptyFieldValues()
+    sql_cmd = ('UPDATE %s SET %s WHERE %s' %
+               (self._table_name,
+                ', '.join([f + ' = ?' for f in field_names]),
+                ' AND '.join(f + ' = ?' for f in condition_names)))
+    executor = self._executor_factory.NewExecutor()
+    executor.Execute(sql_cmd, field_values + condition_values, True)
+
+  def DoesRowExist(self, condition):
+    '''Checks if a row exists or not.
+
+    Args:
+      condition: A model instance describing the checking condition.
+
+    Returns:
+      True if exists; otherwise, False.
+    '''
+    return bool(self.GetOneRow(condition))
+
+  def GetOneRow(self, condition):
+    '''Gets the first row which matches the given condition.
+
+    Args:
+      condition: A model instance describing the checking condition.
+
+    Returns:
+      A model instance containing the first matching row.
+    '''
+    return self.GetRows(condition, one_row=True)
+
+  def GetRows(self, condition, one_row=False):
+    '''Gets all the rows which match the given condition.
+
+    Args:
+      condition: A model instance describing the checking condition.
+      one_row: True if only returns the first row; otherwise, all the rows.
+
+    Returns:
+      A list of model instances containing all the matching rows.
+
+    Raises:
+      DatabaseException if not a valid model instance.
+    '''
+    if not self._model.IsValid(condition):
+      raise DatabaseException('The condition is a wrong model.')
+
+    field_names = condition.GetNonEmptyFieldNames()
+    field_values = condition.GetNonEmptyFieldValues()
+    sql_cmd = ('SELECT %s FROM %s%s%s' %
+               (', '.join(condition.GetFieldNames()),
+                self._table_name,
+                ' WHERE ' if field_names else '',
+                ' AND '.join([f + ' = ?' for f in field_names])))
+    executor = self._executor_factory.NewExecutor()
+    executor.Execute(sql_cmd, field_values)
+    if one_row:
+      return executor.FetchOne(model=condition)
+    else:
+      return executor.FetchAll(model=condition)
+
+  def UpdateOrInsertRow(self, row):
+    '''Updates the row or insert it if not exists.
+
+    Args:
+      row: A model instance containing the update content.
+
+    Raises:
+      DatabaseException if not a valid model instance.
+    '''
+    if not self._model.IsValid(row):
+      raise DatabaseException('Update/insert a row with a wrong model.')
+
+    # If there is no primary key in the table, just insert it.
+    if not self._primary_key:
+      self.InsertRow(row)
+      return
+
+    # Create a model containing the primary key as checking condition.
+    condition = row.CloneOnlyPrimaryKey()
+    if set(self._primary_key) != set(condition.GetNonEmptyFieldNames()):
+      raise DatabaseException('Update/insert a row without a primary key.')
+
+    # Search the primary key from the table to determine update or insert.
+    if self.DoesRowExist(condition):
+      self.UpdateRow(row)
+    else:
+      self.InsertRow(row)
+
+class Executor(object):
+  '''A database executor.
+
+  It abstracts the underlying database execution behaviors, like executing
+  an SQL query, fetching results, etc.
+
+  Properties:
+    _conn: The connection of the sqlite3 database.
+    _cursor: The cursor of the sqlite3 database.
+  '''
+  def __init__(self, conn):
+    self._conn = conn
+    self._cursor = None
+
+  def Execute(self, sql_cmd, args=None, commit=False):
+    '''Executes an SQL command.
+
+    Args:
+      sql_cmd: The SQL command.
+      args: The arguments passed to the SQL command.
+      commit: True to commit the transaction, used when modifying the content.
+    '''
+    logging.debug('Execute SQL command: %s, %s;', sql_cmd, args)
+    self._cursor = self._conn.cursor()
+    if args:
+      self._cursor.execute(sql_cmd, args)
+    else:
+      self._cursor.execute(sql_cmd)
+    if commit:
+      self._conn.commit()
+
+  def FetchOne(self, model=None):
+    '''Fetches one row of the previous query.
+
+    Args:
+      model: The model instance or class, describing the schema. The return
+             value is the same type of this model class. None to return a
+             raw tuple.
+
+    Returns:
+      A model instance if the argument model is valid; otherwise, a raw tuple.
+    '''
+    result = self._cursor.fetchone()
+    if result and model:
+      model = ToModelSubclass(model)
+      return model(result)
+    else:
+      return result
+
+  def FetchAll(self, model=None):
+    '''Fetches all rows of the previous query.
+
+    Args:
+      model: The model instance or class, describing the schema. The return
+             value is the same type of this model class. None to return a
+             raw tuple.
+
+    Returns:
+      A list of model instances if the argument model is valid; otherwise, a
+      list of raw tuples.
+    '''
+    results = self._cursor.fetchall()
+    if results and model:
+      model = ToModelSubclass(model)
+      return [model(result) for result in results]
+    else:
+      return results
+
+class ExecutorFactory(object):
+  '''A factory to generate Executor objects.
+
+  Properties:
+    _conn: The connection of the sqlite3 database.
+  '''
+  def __init__(self, conn):
+    self._conn = conn
+
+  def NewExecutor(self):
+    '''Generates a new Executor object.'''
+    return Executor(self._conn)
+
+class Database(object):
+  '''A database to store Minijack results.
+
+  It abstracts the underlying database. It uses sqlite3 as an implementation.
+
+  Properties:
+    _conn: The connection of the sqlite3 database.
+    _tabels: A dict of the created tables.
+    _executor_factory: A factory of executor objects.
+  '''
+  def __init__(self):
+    self._conn = None
+    self._tables = {}
+    self._executor_factory = None
+
+  def Init(self, filename):
+    '''Initializes the database.'''
+    self._conn = sqlite3.connect(filename)
+    # Make sqlite3 always return bytestrings for the TEXT data type.
+    self._conn.text_factory = str
+    self._executor_factory = ExecutorFactory(self._conn)
+
+  def GetExecutorFactory(self):
+    '''Gets the executor factory.'''
+    return self._executor_factory
+
+  def GetOrCreateTable(self, model):
+    '''Gets or creates a table using the schema of the given model.'''
+    if isinstance(model, str):
+      table_name = model
+    else:
+      table_name = model.GetModelName()
+    if table_name not in self._tables:
+      if not isinstance(model, str):
+        table = Table(self._executor_factory)
+        table.Init(model)
+        self._tables[table_name] = table
+      else:
+        raise DatabaseException('Table %s not initialized.' % table_name)
+    return self._tables[table_name]
+
+  def Close(self):
+    '''Closes the database.'''
+    self._conn.close()
--- py/minijack/db_unittest.py
+++ py/minijack/db_unittest.py
@@ -3,6 +3,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import re
 import unittest
 
 import factory_common  # pylint: disable=W0611
@@ -110,6 +111,142 @@ class ModelTest(unittest.TestCase):
   def testToModelSubclass(self):
     self.assertIs(db.ToModelSubclass(self.foo_model), FooModel)
     self.assertIs(db.ToModelSubclass(FooModel), FooModel)
+
+class DatabaseTest(unittest.TestCase):
+  def setUp(self):
+    self.database = db.Database()
+    self.database.Init(':memory:')
+
+  def testGetOrCreateTable(self):
+    foo_table = self.database.GetOrCreateTable(FooModel)
+    foo_table_by_name = self.database.GetOrCreateTable('FooModel')
+    self.assertIs(foo_table, foo_table_by_name)
+
+    bar_table = self.database.GetOrCreateTable(BarModel)
+    bar_table_by_instance = self.database.GetOrCreateTable(BarModel())
+    self.assertIs(bar_table, bar_table_by_instance)
+    self.assertIsNot(foo_table, bar_table)
+
+    with self.assertRaises(db.DatabaseException):
+      self.database.GetOrCreateTable('NotExist')
+
+  def testTableExists(self):
+    self.database.GetOrCreateTable(FooModel)
+    self.database.GetOrCreateTable(BarModel)
+
+    # Verify the tables exist by querying the sqlite_master table.
+    executor_factory = self.database.GetExecutorFactory()
+    executor = executor_factory.NewExecutor()
+    executor.Execute('SELECT name FROM sqlite_master WHERE type = "table"')
+    results = executor.FetchAll()
+    self.assertItemsEqual([('FooModel',), ('BarModel',)], results)
+
+  def testTableSchema(self):
+    self.database.GetOrCreateTable(FooModel)
+
+    # Verify the table schema by querying the sqlite_master table.
+    executor_factory = self.database.GetExecutorFactory()
+    executor = executor_factory.NewExecutor()
+    executor.Execute('SELECT sql FROM sqlite_master '
+                     'WHERE type = "table" AND name = "FooModel"')
+    (sql,) = executor.FetchOne()
+    pattern = 'CREATE TABLE FooModel \( (.*), PRIMARY KEY \( (.*) \) \)'
+    self.assertRegexpMatches(sql, pattern)
+    matches = re.match(pattern, sql)
+    self.assertItemsEqual(['field_i INTEGER', 'field_r REAL', 'field_t TEXT'],
+                          matches.group(1).split(', '))
+    self.assertItemsEqual(['field_i'],
+                          matches.group(2).split(', '))
+
+  def testInsertRow(self):
+    foo_table = self.database.GetOrCreateTable(FooModel)
+    foo_table.InsertRow(FooModel(field_i=56, field_t='Five Six'))
+
+    # Verify the table content by querying the table.
+    executor_factory = self.database.GetExecutorFactory()
+    executor = executor_factory.NewExecutor()
+    executor.Execute('SELECT * FROM FooModel')
+    result = executor.FetchOne()
+    self.assertItemsEqual((56, None, 'Five Six'), result)
+
+  def testUpdateRow(self):
+    foo_table = self.database.GetOrCreateTable(FooModel)
+    foo_table.InsertRow(FooModel(field_i=56, field_t='Five Six'))
+    foo_table.UpdateRow(FooModel(field_i=56, field_r=5.6))
+
+    # Verify the table content by querying the table.
+    executor_factory = self.database.GetExecutorFactory()
+    executor = executor_factory.NewExecutor()
+    executor.Execute('SELECT * FROM FooModel')
+    result = executor.FetchOne()
+    self.assertItemsEqual((56, 5.6, 'Five Six'), result)
+
+  def testDoesRowExists(self):
+    foo_table = self.database.GetOrCreateTable(FooModel)
+    foo_table.InsertRow(FooModel(field_i=56, field_t='Five Six'))
+    self.assertTrue(foo_table.DoesRowExist(FooModel(field_i=56)))
+    self.assertFalse(foo_table.DoesRowExist(FooModel(field_i=78)))
+
+  def testGetOneRow(self):
+    foo_table = self.database.GetOrCreateTable(FooModel)
+    foo_table.InsertRow(FooModel(field_i=56, field_t='Five Six'))
+    foo_table.InsertRow(FooModel(field_i=78, field_r=7.8))
+    row = foo_table.GetOneRow(FooModel(field_i=78))
+    self.assertDictEqual({
+      'field_i': 78,
+      'field_r': 7.8,
+      'field_t': '',
+    }, row.GetFields())
+    # Get not-matched.
+    self.assertIs(None, foo_table.GetOneRow(FooModel(field_i=34)))
+
+  def testGetRows(self):
+    foo_table = self.database.GetOrCreateTable(FooModel)
+    foo_table.InsertRow(FooModel(field_i=56, field_t='Five Six'))
+    foo_table.InsertRow(FooModel(field_i=78, field_r=7.8))
+    # Get all rows.
+    rows = foo_table.GetRows(FooModel())
+    self.assertEqual(2, len(rows))
+    self.assertDictEqual({
+      'field_i': 56,
+      'field_r': 0.0,
+      'field_t': 'Five Six',
+    }, rows[0].GetFields())
+    self.assertDictEqual({
+      'field_i': 78,
+      'field_r': 7.8,
+      'field_t': '',
+    }, rows[1].GetFields())
+    # Get one row matched.
+    rows = foo_table.GetRows(FooModel(field_i=78))
+    self.assertEqual(1, len(rows))
+    # Get not-matched.
+    rows = foo_table.GetRows(FooModel(field_i=90))
+    self.assertEqual(0, len(rows))
+
+  def testUpdateOrInsertRow(self):
+    foo_table = self.database.GetOrCreateTable(FooModel)
+    foo_table.UpdateOrInsertRow(FooModel(field_i=56, field_t='Five Six'))
+    foo_table.UpdateOrInsertRow(FooModel(field_i=78, field_r=7.8))
+    foo_table.UpdateOrInsertRow(FooModel(field_i=56, field_r=5.6))
+    rows = foo_table.GetRows(FooModel())
+    self.assertEqual(2, len(rows))
+    self.assertDictEqual({
+      'field_i': 56,
+      'field_r': 5.6,
+      'field_t': 'Five Six',
+    }, rows[0].GetFields())
+    self.assertDictEqual({
+      'field_i': 78,
+      'field_r': 7.8,
+      'field_t': '',
+    }, rows[1].GetFields())
+    # Update or insert a row without a primary key.
+    with self.assertRaises(db.DatabaseException):
+      foo_table.UpdateOrInsertRow(FooModel(field_r=3.4, field_t='Three Four'))
+
+  def tearDown(self):
+    self.database.Close()
 
 if __name__ == "__main__":
   unittest.main()
