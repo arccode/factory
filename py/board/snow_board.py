16298987c329e184330bd1676744a7fe7efc2a51
Gediminas Ramanauskas <gedis@chromium.org>
2013-04-30 16:37:09 -0700

    Adding board specific thermal and charging support for snow
    Copied from the spring board CL:
        Author: Vic Yang <victoryang@chromium.org>
        Date:   Fri Feb 1 16:27:55 2013 +0800
    
        Change-Id: If08faf1ffa9c1b7712d06ead54b5d6484c2502c3
        Signed-off-by: Vic Yang <victoryang@chromium.org>
        Reviewed-on: https://gerrit.chromium.org/gerrit/42443
        Reviewed-by: Cheng-Yi Chiang <cychiang@chromium.org>
    
    BUG=chrome-os-partner:19075
    TEST=thermal_load pytest succeeds on snow
    
    Change-Id: I9b5e062af6a21348d1aac55a34e3d5a8180e2419
    Reviewed-on: https://gerrit.chromium.org/gerrit/49683
    Commit-Queue: Gediminas Ramanauskas <gedis@chromium.org>
    Reviewed-by: Gediminas Ramanauskas <gedis@chromium.org>
    Tested-by: Gediminas Ramanauskas <gedis@chromium.org>

--- py/board/snow_board.py
+++ py/board/snow_board.py
@@ -14,6 +14,22 @@ from cros.factory.test import factory
 class SnowBoard(ChromeOSBoard):
   """Board interface for Snow."""
 
+  def GetTemperatures(self):
+    with open('/sys/class/thermal/thermal_zone0/temp', 'r') as f:
+      return [int(f.readline().rstrip())/1000]
+
+  def GetMainTemperatureIndex(self):
+    return 0
+
+  def GetTemperatureSensorNames(self):
+    return ['CPU']
+
+  def GetFanRPM(self):
+    raise NotImplementedError
+
+  def SetFanRPM(self, rpm):
+    raise NotImplementedError
+
   def SetChargeState(self, state):
     try:
       if state == Board.ChargeState.CHARGE:
@@ -31,3 +47,7 @@ class SnowBoard(ChromeOSBoard):
         raise BoardException('Unknown SnowBoard charge state: %s' % state)
     except Exception as e:
       raise BoardException('Unable to set charge state in SnowBoard: %s' % e)
+
+  def GetChargerCurrent(self):
+    """Charger current is not available on snow board."""
+    raise NotImplementedError
