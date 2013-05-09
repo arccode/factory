aefb68c4261792eb221de61551eaa4edf07cbde5
Dean Liao <deanliao@chromium.org>
2013-05-09 18:45:38 +0800

    Dummy class of BFTFixture.
    The dummy BFTFixture class is used for developers to verify BFT-based
    Goofy tests without BFT fixture presents. It asks the user to act like
    fixture to plug/unplug things.
    
    BUG=chrome-os-partner:18116
    TEST=none
    
    Change-Id: I6c6c013b4ebc58a41e3abd24b3f915a17be40f98
    Reviewed-on: https://gerrit.chromium.org/gerrit/50658
    Commit-Queue: Shuo-Peng Liao <deanliao@chromium.org>
    Reviewed-by: Shuo-Peng Liao <deanliao@chromium.org>
    Tested-by: Shuo-Peng Liao <deanliao@chromium.org>

---
+++ py/test/fixture/dummy_bft_fixture.py
@@ -0,0 +1,73 @@
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import time
+
+import factory_common  # pylint: disable=W0611
+from cros.factory.test import factory
+from cros.factory.test.fixture.bft_fixture import BFTFixture
+
+
+class DummyBFTFixture(BFTFixture):
+  """A dummy class for BFT fixture.
+
+  It is used when we want to verify a BFT test case without BFT fixture.
+  For methods asking the fixture to do something, we place a timer for
+  tester to act like the fixture.
+  For methods getting status, like GetFixtureId, we returns a default value.
+  """
+  # For EngageDevice/DisengageDevice, it sleeps _delay_secs for user to mimic
+  # fixture's action.
+  _delay_secs = 3
+
+  # For GetLEDColor, this dummy object returns the color set by ScanLED.
+  _expect_color = None
+
+  def Init(self, **kwargs):
+    pass
+
+  def Disconnect(self):
+    pass
+
+  def SetDeviceEngaged(self, device, engage):
+    self._DisplayPrompt('Please %s device: %s' %
+                        ('engage' if engage else 'disengage',
+                         device))
+
+  def Ping(self):
+    pass
+
+  def GetFixtureId(self):
+    return chr(1)
+
+  def ScanBarcode(self):
+    self._DisplayPrompt('Please type a barcode.')
+
+  def SimulateKeystrokes(self):
+    self._DisplayPrompt('Please input keystoke sequence.')
+
+  def ScanLED(self, color):
+    self._expect_color = color
+
+  def GetLEDColor(self):
+    return self._expect_color
+
+  @property
+  def delay_secs(self):
+    return self._delay_secs
+
+  @delay_secs.setter
+  def delay_secs(self, delay_secs):
+    self._delay_secs = delay_secs
+
+  def _DisplayPrompt(self, prompt):
+    """Asks user to do something to ack like a real fixture.
+
+    It sleeps for _delay_secs for user to complete the action.
+
+    Args:
+      prompt: The prompt message.
+    """
+    factory.console.info(prompt)
+    time.sleep(self._delay_secs)
