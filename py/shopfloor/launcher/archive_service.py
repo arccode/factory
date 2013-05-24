c5fa738f9d676c64dd8a4afc56cfce67b392de37
Rong Chang <rongchang@chromium.org>
2013-05-24 14:34:23 +0800

    Add service configuration for report archiver
    BUG=chrome-os-partner:19197
    TEST=manual
    
    Change-Id: I7da4fe8d82b6bd7ceb1e0c662cdfeb96cb4779aa
    Reviewed-on: https://gerrit.chromium.org/gerrit/56574
    Reviewed-by: Vic Yang <victoryang@chromium.org>
    Commit-Queue: Rong Chang <rongchang@chromium.org>
    Tested-by: Rong Chang <rongchang@chromium.org>

---
+++ py/shopfloor/launcher/archive_service.py
@@ -0,0 +1,37 @@
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Reports archive service."""
+
+import os
+
+import factory_common  # pylint: disable=W0611
+from cros.factory.shopfloor.launcher import env
+from cros.factory.shopfloor.launcher.service import ServiceBase
+from cros.factory.test.utils import TryMakeDirs
+
+
+class ArchiveService(ServiceBase):
+  """Report archive configuration.
+
+  Args:
+    dummy_config: Launcher YAML config dictionary.
+  """
+  def __init__(self, dummy_config):
+    # ServiceBase is an old-style python class.
+    ServiceBase.__init__(self)
+
+    svc_conf = {
+      'executable': os.path.join(env.runtime_dir, 'archive_report'),
+      'name': 'archive_report',
+      'args': ['--period', '10'],
+      'path': env.runtime_dir,
+      'logpipe': False,
+      'auto_restart': True}
+    self.SetConfig(svc_conf)
+
+    TryMakeDirs(os.path.join(env.runtime_dir, 'shopfloor_data', 'reports'))
+    TryMakeDirs(os.path.join(env.runtime_dir, 'shopfloor_data', 'recycle_bin'))
+
+Service = ArchiveService
