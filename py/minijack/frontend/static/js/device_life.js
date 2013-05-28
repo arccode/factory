4f4e4feb074ab77bdfc476fc60ca0297a62760ae
Tom Wai-Hong Tam <waihong@chromium.org>
2013-05-28 15:17:48 +0800

    MJFE: Introduce a new UI for the Device and Event pages
    The new UI uses DataTables jQuery plug-in such that an user can do many
    interactions on the test table, like column sorting, column grouping,
    column showing/hiding, row count selection, mutilple pages, search, etc.
    
    The Goto-Home and Goto-Device links are added in the top-right corner.
    
    Some styles are changed too.
    
    BUG=chrome-os-partner:19468
    TEST=Manaul, manage runserver
    Demo: http://go/minijack_frontend_next
    
    Change-Id: Ibda2bb9b691ebf4b53944c38f8128d6795fc70c2
    Reviewed-on: https://gerrit.chromium.org/gerrit/56787
    Reviewed-by: Rong Chang <rongchang@chromium.org>
    Tested-by: Tom Wai-Hong Tam <waihong@chromium.org>
    Commit-Queue: Tom Wai-Hong Tam <waihong@chromium.org>

---
+++ py/minijack/frontend/static/js/device_life.js
@@ -0,0 +1,44 @@
+// Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+$(document).ready(function() {
+  var oTable = $('#test_table').dataTable({
+    "aLengthMenu": [[20, 40, 60, 80, 100, 200, -1],
+                    [20, 40, 60, 80, 100, 200, "All"]],
+    "aaSorting": [[6, "desc"]],
+    "bJQueryUI": true,
+    "bScrollCollapse": true,
+    "iDisplayLength": -1,
+    "oColVis": {
+      "sSize": "css",
+      "sAlign": "right",
+    },
+    "sDom": '<lCfr>t<ip>',
+    "sPaginationType": "full_numbers",
+    "sScrollX": "100%",
+  });
+
+  var aDateColumns = [6, 7];
+  var aSelectableColumns = [3, 4, 5, 6, 7, 9, 10];
+  var aHiddenColumns = [1, 2, 5];
+
+  /* Add a select menu for each TH element in the table header */
+  $("thead th").each(function(i) {
+    if ($.inArray(i, aSelectableColumns) != -1) {
+      if ($.inArray(i, aDateColumns) !== -1)
+        this.innerHTML += fnCreateSelect(oTable.fnGetColumnData(i, fnCutDate));
+      else
+        this.innerHTML += fnCreateSelect(oTable.fnGetColumnData(i));
+
+      $('select', this).change(function() {
+        oTable.fnFilter($(this).val(), i);
+      });
+    }
+  });
+
+  $.each(aHiddenColumns, function(index, value) {
+    oTable.fnSetColumnVis(value, false);
+  });
+
+});
