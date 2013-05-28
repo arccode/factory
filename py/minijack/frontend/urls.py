a7d1aedb31d99763ce3a9d0ce53bc940e86ea8fc
Tom Wai-Hong Tam <waihong@chromium.org>
2013-05-28 16:07:01 +0800

    MJFE: Change the default page of Minijack Frontend
    MJFE can't use / as its default page as xmlrpc used it. Change the default
    change to /build and also redirect /index to it.
    
    BUG=chrome-os-partner:19468
    TEST=Manaul, manage runserver
    Browse /build and /index got the same page and valid.
    
    Change-Id: If663f3e5d261241fb3dce35bb67669e63797b00e
    Reviewed-on: https://gerrit.chromium.org/gerrit/56789
    Reviewed-by: Rong Chang <rongchang@chromium.org>
    Tested-by: Tom Wai-Hong Tam <waihong@chromium.org>
    Commit-Queue: Tom Wai-Hong Tam <waihong@chromium.org>

--- py/minijack/frontend/urls.py
+++ py/minijack/frontend/urls.py
@@ -4,6 +4,7 @@
 
 from django.conf.urls.defaults import patterns, url
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 import factory_common  # pylint: disable=W0611
 from cros.factory.minijack.frontend import settings, views
@@ -12,5 +13,6 @@ from cros.factory.minijack.frontend import settings, views
 urlpatterns = patterns('',
   url(r'^device/(?P<device_id>[^/]*)$', views.GetDeviceView, name='device'),
   url(r'^event/(?P<event_id>[^/]*)$', views.GetEventView, name='event'),
-  url(r'^$', views.GetBuildView, name='build'),
+  url(r'^build$', views.GetBuildView, name='build'),
+  url(r'^index$', RedirectView.as_view(url='/build')),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
