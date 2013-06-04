adbe7fddbf7c2e35ac3b1f328183ae70033c28e4
Tom Wai-Hong Tam <waihong@chromium.org>
2013-06-04 11:44:04 +0800

    MJFE: Disable the pylint E1120 about the @classonlymethod decorator
    RedirectView.as_view uses @classonlymethod, a subclass of @classmethod.
    However, pylint only knows the @classmethod and doesn't know this new
    @classonlymethod and complains missing the cls argument.
    
    So disable the lint check.
    
    BUG=chrome-os-partner:19468
    TEST=make lint
    
    Change-Id: I3cdf3c20b648e00c5492aac3942bd1915735d9e8
    Reviewed-on: https://gerrit.chromium.org/gerrit/57423
    Reviewed-by: Jon Salz <jsalz@chromium.org>
    Commit-Queue: Tom Wai-Hong Tam <waihong@chromium.org>
    Tested-by: Tom Wai-Hong Tam <waihong@chromium.org>

--- py/minijack/frontend/urls.py
+++ py/minijack/frontend/urls.py
@@ -14,5 +14,7 @@ urlpatterns = patterns('',
   url(r'^device/(?P<device_id>[^/]*)$', views.GetDeviceView, name='device'),
   url(r'^event/(?P<event_id>[^/]*)$', views.GetEventView, name='event'),
   url(r'^build$', views.GetBuildView, name='build'),
-  url(r'^index$', RedirectView.as_view(url='/build')),
+  # RedirectView.as_view uses @classonlymethod, a subclass of @classmethod.
+  # Pylint doesn't know the @classonlymethod and complains.
+  url(r'^index$', RedirectView.as_view(url='/build')),  # pylint: disable=E1120
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
