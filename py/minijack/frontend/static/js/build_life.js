482b2705d328fb1949aeb4bda674f58522536c4d
Tom Wai-Hong Tam <waihong@chromium.org>
2013-06-05 08:48:50 +0800

    MJFE: Add the detail rows on devices
    When an user clicks the icon of a device, it shows the detail rows which
    have the controls to ping, ssh, screenshot, and add notes.
    
    This change only makes the UI of the above functions. The implementation
    will be done later.
    
    TEST=Manaul, manage runserver
    Demo: http://go/minijack_frontend_next
    
    Change-Id: I74c10a3b7c5fba4d5fc307448bccf9c747810959
    Reviewed-on: https://gerrit.chromium.org/gerrit/57588
    Reviewed-by: Rong Chang <rongchang@chromium.org>
    Commit-Queue: Tom Wai-Hong Tam <waihong@chromium.org>
    Tested-by: Tom Wai-Hong Tam <waihong@chromium.org>

--- py/minijack/frontend/static/js/build_life.js
+++ py/minijack/frontend/static/js/build_life.js
@@ -6,21 +6,25 @@ $(document).ready(function() {
   var oTable = $("#device_table").dataTable({
     "aLengthMenu": [[20, 40, 60, 80, 100, 200, -1],
                     [20, 40, 60, 80, 100, 200, "All"]],
-    "aaSorting": [[3, "desc"]],
+    "aaSorting": [[4, "desc"]],
+    "aoColumnDefs": [
+      {"bSortable": false, "aTargets": [0]},
+    ],
     "bJQueryUI": true,
     "bScrollCollapse": true,
     "iDisplayLength": 100,
     "oColVis": {
-      "sSize": "css",
+      "aiExclude": [0],
       "sAlign": "right",
+      "sSize": "css",
     },
     "sDom": "<lCfr><ip>t<ip>",
     "sPaginationType": "full_numbers",
     "sScrollX": "100%",
   });
 
-  var aDateColumns = [1, 3, 8, 17];
-  var aSelectableColumns = [1, 2, 3, 6, 8, 9, 10, 13, 14, 15, 17];
+  var aDateColumns = [2, 4, 9, 18];
+  var aSelectableColumns = [2, 3, 4, 7, 9, 10, 11, 14, 15, 16, 18];
 
   /* Add a select menu for each TH element in the table header */
   $("thead th").each(function(i) {
@@ -41,36 +45,79 @@ $(document).ready(function() {
   });
 
   fnSelectSuite(0);
+
+  $("#device_table tbody tr td img").live("click", function() {
+    var nTr = $(this).parents("tr")[0];
+    if (oTable.fnIsOpen(nTr)) {
+      this.src = "/static/images/details_open.png";
+      oTable.fnClose(nTr);
+    } else {
+      this.src = "/static/images/details_close.png";
+      oTable.fnOpen(nTr, fnFormatDetails(oTable.fnGetData(nTr)), "cell");
+    }
+  });
+
 });
+
+
+function fnFormatDetails(aData) {
+  var sOut = '<table class="detail">';
+  var aIp = aData[8].split(/, /);
+  for (var i = 0; i < aIp.length; i++) {
+    if (aIp[i]) {
+      var aKeyValue = aIp[i].split('=')
+      sOut += '<tr>';
+      sOut += '<th>' + aKeyValue[0] + '</th>'
+      sOut += '<td>' + aKeyValue[1] + '</td>';
+      sOut += '<td>' + aData[9] + '</td>';
+      sOut += '<td><button id="ping" disabled>Ping</button>';
+      sOut += '<button id="ssh" disabled>SSH</button>';
+      sOut += '<button id="screenshot" disabled>Screenshot</button></td>';
+      sOut += '</tr>';
+    }
+  }
+  sOut += '<tr><td><button id="add_note" disabled>Add Note</button></td>';
+  sOut += '<td colspan="3"></td></tr>';
+  if (aData[15]) {
+    sOut += '<tr><th>' + aData[15] + '</th>';
+    sOut += '<td>' + aData[16] + '</td>';
+    sOut += '<td>' + aData[18] + '</td>';
+    sOut += '<td>' + aData[17] + '</td></tr>';
+  }
+  sOut += '</table>';
+
+  return sOut;
+}
 
 
 function fnSelectSuite(iSuite) {
   /*
    * Columns:
-   *    0 - device_id
-   *    1 - goofy_init_time
-   *    2 - latest_test
-   *    3 - test_time
-   *    4 - serial
-   *    5 - mlb_serial
-   *    6 - hwid
-   *    7 - ips
-   *    8 - ips_time
-   *    9 - latest_ended_test
-   *   10 - ended_status
-   *   11 - c_passed
-   *   12 - c_failed
-   *   13 - mj_status
-   *   14 - latest_note_lv
-   *   15 - note_name
-   *   16 - note_text
-   *   17 - note_time
+   *    0 - detail_icon
+   *    1 - device_id
+   *    2 - goofy_init_time
+   *    3 - latest_test
+   *    4 - test_time
+   *    5 - serial
+   *    6 - mlb_serial
+   *    7 - hwid
+   *    8 - ips
+   *    9 - ips_time
+   *   10 - latest_ended_test
+   *   11 - ended_status
+   *   12 - c_passed
+   *   13 - c_failed
+   *   14 - mj_status
+   *   15 - latest_note_lv
+   *   16 - note_name
+   *   17 - note_text
+   *   18 - note_time
    */
-  var iTotalColumns = 18
+  var iTotalColumns = 19
   var aaVisibleColumns = [
-    [0, 2, 3, 4, 5, 6, 7, 13],
-    [0, 1, 2, 3, 9, 10, 11, 12],
-    [0, 2, 3, 14, 15, 16, 17],
+    [0, 1, 3, 4, 5, 6, 7, 14],
+    [0, 2, 3, 4, 10, 11, 12, 13],
+    [0, 3, 4, 15, 16, 17, 18],
   ];
   var oTable = $("#device_table").dataTable();
 
