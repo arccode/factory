585f85e16cd74d3720d305aaca7c3a60e97de536
Dean Liao <deanliao@chromium.org>
2013-06-21 21:15:54 +0800

    flash_netboot: refactor FlashNetboot class.
    Refactor FlashNetboot class so that pytest can call it directly to avoid
    nested process spawning.
    
    BUG=chrome-os-partner:20402
    TEST=manual
    
    Change-Id: I77806482a9820a68e7dd214a152e5be1e0ef271d
    Reviewed-on: https://gerrit.chromium.org/gerrit/59553
    Tested-by: Shuo-Peng Liao <deanliao@chromium.org>
    Reviewed-by: Vic Yang <victoryang@chromium.org>
    Commit-Queue: Shuo-Peng Liao <deanliao@chromium.org>

---
+++ py/system/flash_netboot.py
@@ -0,0 +1,111 @@
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""A handy API to flash netboot with VPD presered.
+
+Usage:
+  netboot_flasher = FlashNetboot(image_filename, on_output=Prompt)
+  Prompt(netboot_flasher.WarningMessage())
+  // Prompt to continue.
+  netboot_flasher.Run()
+  // Reboot.
+"""
+
+from contextlib import nested
+import glob
+import logging
+import os
+import shutil
+import subprocess
+
+import factory_common  # pylint: disable=W0611
+from cros.factory.utils.file_utils import UnopenedTemporaryFile
+from cros.factory.utils.process_utils import Spawn
+
+
+class FlashNetboot(object):
+  """Flashs netboot firmware.
+
+  Args:
+    image_file_patttern: Netboot firmware image. Allows file pattern which
+        matches exactly one file.
+    on_output: Output callback. Default None: output to stdout.
+  """
+  def __init__(self, image_file_pattern, on_output=None):
+    self._on_output = on_output
+    self._fw_main = None
+    self._ro_vpd = None
+    self._rw_vpd = None
+    self._image = None
+
+    images = glob.glob(image_file_pattern)
+    if not images:
+      raise ValueError('Firmware image %s does not exist' % image_file_pattern)
+    if len(images) > 1:
+      raise ValueError('Multiple firmware images %s exist' % image_file_pattern)
+    self._image = images[0]
+
+  def Run(self):
+    """Flashs netboot firmware."""
+    with nested(UnopenedTemporaryFile(prefix='fw_main.'),
+                UnopenedTemporaryFile(prefix='vpd.ro.'),
+                UnopenedTemporaryFile(prefix='vpd.rw.')) as files:
+      self._fw_main, self._ro_vpd, self._rw_vpd = files
+      self._PreserveVPD()
+      shutil.copyfile(self._image, self._fw_main)
+      self._PackVPD()
+      self._FlashFirmware()
+
+  def WarningMessage(self):
+    return (
+        '\n'
+        '\n'
+        '*** You are about to flash netboot firmware on this machine with:\n'
+        '***   %s\n'
+        '***\n'
+        '*** This process is unrecoverable and this machine WILL need to\n'
+        '*** go through network installation again.\n'
+        '***\n'
+        '*** Once this process starts, aborting it or powering off the\n'
+        '*** machine may leave the machine in an unknown state.\n'
+        '***\n' % self._image)
+
+  def _Flashrom(self, params):
+    cmd = ['flashrom'] + params
+    if self._on_output is None:
+      Spawn(cmd, log=True, check_call=True)
+    else:
+      p = Spawn(cmd, log=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+      for line in iter(p.stdout.readline, ''):
+        self._on_output(line)
+
+  def _PreserveSection(self, fw_main_file, section_file, section_name):
+    """Reads out a firmware section.
+
+    Args:
+      fw_main_file: The dummy file served as full firmware image to flashrom.
+      section_file: The file where the firmware section is saved to.
+      section_name: The name of the section to read out.
+    """
+    logging.info('Saving %s to %s', section_name, section_file)
+    self._Flashrom(['-r', fw_main_file,
+                    '-i', '%s:%s' % (section_name, section_file)])
+
+  def _PreserveVPD(self):
+    self._PreserveSection(self._fw_main, self._rw_vpd, 'RW_VPD')
+    self._PreserveSection(self._fw_main, self._ro_vpd, 'RO_VPD')
+
+  def _PackVPD(self):
+    logging.info('Packing RO/RW VPD into %s', self._fw_main)
+    img_size = os.stat(self._fw_main).st_size
+    self._Flashrom([
+        '-p', 'dummy:image=%s,size=%d,emulate=VARIABLE_SIZE' % (self._fw_main,
+                                                                img_size),
+        '-w', self._fw_main,
+        '-i', 'RO_VPD:%s' % self._ro_vpd,
+        '-i', 'RW_VPD:%s' % self._rw_vpd])
+
+  def _FlashFirmware(self):
+    logging.info('Flashing firmware %s...', self._image)
+    self._Flashrom(['-w', self._image])
--- py/tools/flash_netboot.py
+++ py/tools/flash_netboot.py
@@ -4,54 +4,21 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+"""A tool to flash netboot firmware."""
 
 import argparse
-from contextlib import nested
-import glob
 import logging
-import os
-import shutil
 import sys
 import time
 
 import factory_common  # pylint: disable=W0611
-from cros.factory.utils.file_utils import UnopenedTemporaryFile
+from cros.factory.system.flash_netboot import FlashNetboot
 from cros.factory.utils.process_utils import Spawn
 
 
-def PreserveSection(fw_main_file, section_file, section_name):
-  """Reads out a firmware section.
-
-  Args:
-    fw_main_file: The dummy file served as full firmware image to flashrom.
-    section_file: The file where the firmware section is saved to.
-    section_name: The name of the section to read out.
-  """
-  logging.info('Saving %s to %s', section_name, section_file)
-  Spawn(['flashrom', '-r', fw_main_file, '-i',
-         '%s:%s' % (section_name, section_file)],
-        log=True, check_call=True)
-
-
-def PreserveVPD(fw_main, ro_vpd, rw_vpd):
-  PreserveSection(fw_main, rw_vpd, 'RW_VPD')
-  PreserveSection(fw_main, ro_vpd, 'RO_VPD')
-
-def PackVPD(fw_main, ro_vpd, rw_vpd):
-  logging.info('Packing RO/RW VPD into %s', fw_main)
-  img_size = os.stat(fw_main).st_size
-  Spawn(['flashrom',
-         '-p', 'dummy:image=%s,size=%d,emulate=VARIABLE_SIZE' %
-         (fw_main, img_size),
-         '-w', fw_main,
-         '-i', 'RO_VPD:%s' % ro_vpd,
-         '-i', 'RW_VPD:%s' % rw_vpd], log=True, check_call=True)
-
-def FlashFirmware(fw_image):
-  logging.info('Flashing firmware %s...', fw_image)
-  Spawn(['flashrom', '-w', fw_image], log=True, check_call=True)
-
 def main():
+  logging.basicConfig(level=logging.INFO)
+
   parser = argparse.ArgumentParser(
       description="Flash netboot firmware with VPD preserved.")
   parser.add_argument('--image', '-i', help='Netboot firmware image',
@@ -61,27 +28,12 @@ def main():
                       help="Don't ask for confirmation")
   args = parser.parse_args()
 
-  images = glob.glob(args.image)
-  if not images:
-    parser.error('Firmware image %s does not exist' % args.image)
-  if len(images) > 1:
-    parser.error('Multiple firmware images %s exist' % images)
-  image = images[0]
+  try:
+    netboot_flasher = FlashNetboot(args.image)
+  except ValueError as e:
+    parser.error(e.message)
 
-  logging.basicConfig(level=logging.INFO)
-
-  sys.stdout.write(
-      ('\n'
-       '\n'
-       '*** You are about to flash netboot firmware on this machine with:\n'
-       '***   %s\n'
-       '***\n'
-       '*** This process is unrecoverable and this machine WILL need to\n'
-       '*** go through network installation again.\n'
-       '***\n'
-       '*** Once this process starts, aborting it or powering off the\n'
-       '*** machine may leave the machine in an unknown state.\n'
-       '***\n') % image)
+  sys.stdout.write(netboot_flasher.WarningMessage())
 
   if not args.yes:
     sys.stdout.write('*** Continue? [y/N] ')
@@ -89,18 +41,13 @@ def main():
     if not answer or answer[0] not in 'yY':
       sys.exit('Aborting.')
 
-  with nested(UnopenedTemporaryFile(prefix='fw_main.'),
-              UnopenedTemporaryFile(prefix='vpd.ro.'),
-              UnopenedTemporaryFile(prefix='vpd.rw.')) as files:
-    PreserveVPD(*files)
-    shutil.copyfile(image, files[0])
-    PackVPD(*files)
-    FlashFirmware(files[0])
+  netboot_flasher.Run()
 
   sys.stdout.write('Rebooting.  See you on the other side!\n')
   Spawn(['reboot'], check_call=True)
   time.sleep(60)
   sys.exit('Unable to reboot.')  # Should never happen
+
 
 if __name__ == '__main__':
   main()
