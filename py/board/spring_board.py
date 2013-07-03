3ca259589016496149e7f0c112b23ab7c48923bb
Vic Yang <victoryang@chromium.org>
2013-07-03 11:59:06 +0800

    Remove SpringPower module
    Now that we have kernel driver for Spring charger, we should drop this
    specialized module.
    
    BUG=chrome-os-partner:20636
    TEST=Boot and check charge manager works.
    
    Change-Id: Icb5fc267600c2e5128214f27b8ef72ec381df3d6
    Signed-off-by: Vic Yang <victoryang@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/60844
    Reviewed-by: Cheng-Yi Chiang <cychiang@chromium.org>

--- py/board/spring_board.py
+++ py/board/spring_board.py
@@ -5,44 +5,14 @@
 # found in the LICENSE file.
 
 import factory_common  # pylint: disable=W0611
-import re
 
 from cros.factory.board.chromeos_board import ChromeOSBoard
 from cros.factory.system.board import Board, BoardException
-from cros.factory.system.power import Power, PowerException
-
-class SpringPower(Power):
-  """Power interface for spring board."""
-  def __init__(self, board):
-    super(SpringPower, self).__init__()
-    self._board = board
-
-  def CheckACPresent(self):
-    """Returns the presence of AC.
-    AC is present if
-    1. AC Voltage > 4000 mV
-    AND
-    2. USB Device Type & 0x480 = 0."""
-    # pylint: disable=W0212
-    powerinfo_output = self._board._CallECTool(['powerinfo'])
-    obj_ac = re.search('AC Voltage: (\d+) mV', powerinfo_output)
-    obj_type = re.search('USB Device Type: (0x\d+)', powerinfo_output)
-    if obj_ac:
-      ac_voltage = obj_ac.group(1)
-    else:
-      raise PowerException('Can not get AC voltage.')
-    if obj_type:
-      device_type = obj_type.group(1)
-    else:
-      raise PowerException('Can not get USB device type.')
-
-    return int(ac_voltage) > 4000 and (int(device_type, 16) & 0x480 == 0)
 
 class SpringBoard(ChromeOSBoard):
   """Board interface for Spring."""
   def __init__(self):
     super(SpringBoard, self).__init__()
-    self.power = SpringPower(self)
 
   def GetTemperatures(self):
     with open('/sys/class/thermal/thermal_zone0/temp', 'r') as f:
