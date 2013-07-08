dde40cb95f029cfa408ff11eadaedba75ffc2965
Cheng-Yi Chiang <cychiang@chromium.org>
2013-07-08 18:26:46 +0800

    probe_sim_card_tray: Add timeout in each step
    Add timeout for InsertTask and RemoveTask
    Also, fix py/test/countdown_timer.py
      1. It can accept a disabling event.
      2. The time_remaining calculation is more precise.
    
    BUG=chrome-os-partner:20598
    TEST=run probe_sim_card_tray test with following argument:
    
        OperatorTest(
            id='ProbeSIMCardTray',
            label_zh=u'SIM卡卡盘',
            pytest_name='probe_sim_card_tray',
            dargs={'tray_already_present': False,
                   'only_check_presence': False})
    
    Change-Id: I51dce1b4bad1a4da5907bc21d3a69959a91be8f9
    Reviewed-on: https://gerrit.chromium.org/gerrit/61080
    Reviewed-by: Ricky Liang <jcliang@chromium.org>
    Commit-Queue: Cheng-Yi Chiang <cychiang@chromium.org>
    Tested-by: Cheng-Yi Chiang <cychiang@chromium.org>

--- py/test/countdown_timer.py
+++ py/test/countdown_timer.py
@@ -4,6 +4,8 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import logging
+import threading
 import time
 
 from cros.factory.test.test_ui import MakeLabel
@@ -13,10 +15,12 @@ _MSG_TIME_REMAINING = lambda t: MakeLabel('Time remaining: %d' % t,
                                           u'剩余时间：%d' % t)
 
 
-def StartCountdownTimer(timeout_secs, timeout_handler, ui, element_id):
-  """Starts a daemon thread for CountdownTimer and updates factory UI.
+def StartCountdownTimer(timeout_secs, timeout_handler, ui, element_id,
+                        disable_event=None):
+  """Starts a thread for CountdownTimer and updates factory UI.
 
   It updates UI for time remaining and calls timeout_handler when timeout.
+  The thread is a daemon thread if disable_event is None.
 
   Usage:
     Inside a factory test's setUp method:
@@ -30,27 +34,51 @@ def StartCountdownTimer(timeout_secs, timeout_handler, ui, element_id):
     timeout_handler: (callback) called when timeout reaches.
     ui: a test_ui.UI instance.
     element_id: The HTML element to place time remaining info.
+    disable_event: a threading.Event object to stop timer.
   """
   tick = lambda t: ui.SetHTML(_MSG_TIME_REMAINING(t), id=element_id)
-  StartDaemonThread(target=CountdownTimer,
-                    args=(timeout_secs, timeout_handler, tick))
+  if disable_event:
+    thread = threading.Thread(target=CountdownTimer,
+        args=(timeout_secs, timeout_handler, tick, disable_event))
+    thread.start()
+  else:
+    StartDaemonThread(target=CountdownTimer,
+                      args=(timeout_secs, timeout_handler, tick))
 
 
-def CountdownTimer(timeout_secs, timeout_handler, tick=None):
+def CountdownTimer(timeout_secs, timeout_handler, tick=None,
+                   disable_event=None):
   """A countdown timer.
 
   It calls timeout_handler when the countdown is over. During the countdown,
   it calls tick every second if provided.
+  It stops when disable_event is set if it is provided.
 
   Args:
     timeout_secs: (int) #seconds to timeout.
     timeout_handler: (callback) called when timeout reaches.
     tick: (optional callback) called with a remaining seconds every seconds.
+    disable_event: a threading.Event object to stop timer.
   """
-  time_remaining = timeout_secs
+  logging.info('Timer is up with timeout_secs: %d', timeout_secs)
+  end_time = time.time() + timeout_secs
+
+  def _GetTimeRemaining():
+    return end_time - time.time()
+
+  time_remaining = _GetTimeRemaining()
   while time_remaining > 0:
     if tick:
       tick(time_remaining)
-    time.sleep(1)
-    time_remaining -= 1
+    if disable_event:
+      disable_event.wait(1)
+      if disable_event.is_set():
+        break
+    else:
+      time.sleep(1)
+    time_remaining = _GetTimeRemaining()
+    logging.debug('time_remaining: %s', time_remaining)
+  if disable_event and disable_event.is_set():
+    logging.info('Timer is disabled')
+    return
   timeout_handler()
--- py/test/pytests/probe_sim_card_tray.py
+++ py/test/pytests/probe_sim_card_tray.py
@@ -24,6 +24,7 @@ from cros.factory.test import factory
 from cros.factory.test import test_ui
 from cros.factory.test import ui_templates
 from cros.factory.test.args import Arg
+from cros.factory.test.countdown_timer import StartCountdownTimer
 from cros.factory.test.event import Event
 from cros.factory.test.factory_task import FactoryTask, FactoryTaskManager
 from cros.factory.utils.file_utils import WriteFile, ReadLines
@@ -37,12 +38,19 @@ _REMOVE_TRAY_INSTRUCTION = test_ui.MakeLabel(
     'Detected! Please remove the SIM card tray', u'已經偵测SIM卡卡盘, 請移除SIM卡卡盘',
     'instruction-font-size')
 
+_ID_INSTRUCTION = 'sim-card-tray-test-container'
+_ID_COUNTDOWN_TIMER = 'sim-card-tray-test-timer'
+
 _CSS = """
 .instruction-font-size {
   font-size: 2em;
 }
 """
 
+_HTML_SIM_CARD_TRAY = """
+<div id="%s" style="position: relative; width: 100%%; height: 60%%;"></div>
+<div id="%s"></div>
+""" % (_ID_INSTRUCTION, _ID_COUNTDOWN_TIMER)
 
 _INSERT_CHECK_PERIOD_SECS = 1
 _GPIO_PATH = os.path.join('/sys', 'class', 'gpio')
@@ -57,18 +65,17 @@ class WaitTrayThread(threading.Thread):
     tray_event: The target tray event. ProbeTrayTask.INSERT or
       ProbeTrayTask.REMOVE.
     on_success: The callback function when tray_event is detected.
-    force_stop: A threading.Event for user to stop the thread.
   """
-  def __init__(self, get_detect, tray_event, on_success, force_stop):
+  def __init__(self, get_detect, tray_event, on_success):
     threading.Thread.__init__(self, name='WaitTrayThread')
     self._get_detect = get_detect
     self._tray_event = tray_event
     self._on_success = on_success
-    self._force_stop = force_stop
-    self._force_stop.clear()
+    self.daemon = True
 
   def run(self):
-    while not self._force_stop.is_set():
+    logging.info('wait for %s event', self._tray_event)
+    while True:
       ret = self._get_detect()
       if self._tray_event == ret:
         self.Detected()
@@ -93,9 +100,15 @@ class ProbeTrayTask(FactoryTask):
     self._template = test.template
     self._instruction = instruction
     self._tray_event = tray_event
+    self._timeout_secs = test.args.timeout_secs
     self._wait_tray = WaitTrayThread(test.GetDetection,
-        self._tray_event, self.PostSuccessEvent, test.force_stop)
+        self._tray_event, self.PostSuccessEvent)
     self._pass_event = str(uuid.uuid4())
+    self._disable_timer = threading.Event()
+
+  def Timeout(self):
+    """Callback function for CountDownTimer"""
+    self.Fail('Timeout after %s seconds' % self._timeout_secs)
 
   def PostSuccessEvent(self):
     """Posts an event to trigger self.Pass()
@@ -103,13 +116,19 @@ class ProbeTrayTask(FactoryTask):
     It is called by another thread. It ensures that self.Pass() is called
     via event queue to prevent race condition.
     """
+    self._disable_timer.set()
     self._ui.PostEvent(Event(Event.Type.TEST_UI_EVENT,
                              subtype=self._pass_event))
 
   def Run(self):
-    self._template.SetState(self._instruction)
+    self._ui.SetHTML(self._instruction, id=_ID_INSTRUCTION)
     self._ui.AddEventHandler(self._pass_event, lambda _: self.Pass())
     self._wait_tray.start()
+    StartCountdownTimer(self._timeout_secs, self.Timeout,
+        self._ui, _ID_COUNTDOWN_TIMER, self._disable_timer)
+
+  def Cleanup(self):
+    self._disable_timer.set()
 
 
 class InsertTrayTask(ProbeTrayTask):
@@ -131,11 +150,40 @@ class ProbeTrayException(Exception):
 
 
 class ProbeSimCardTrayTest(unittest.TestCase):
+  """
+  Test to probe sim card tray.
+
+  Usage examples:
+    1.Just check presence or absence:
+      tray_already_present=True/False
+    2.Ask user to insert tray:
+      tray_already_present=False,
+      insert=True,
+      only_check_presence=False
+    3.Ask user to remove tray:
+      tray_already_present=True,
+      remove=True,
+      only_check_presence=False
+    4.Ask user to insert then remove tray.
+      tray_already_present=False,
+      insert=True,
+      remove=True,
+      only_check_presence=False
+    5.Ask user to remove then insert tray.
+      tray_already_present=True,
+      insert=True,
+      remove=True,
+      only_check_presence=False
+  """
   ARGS = [
+      Arg('timeout_secs', int,
+          'timeout in seconds for insertion/removal', default=10),
       Arg('tray_already_present', bool,
           'SIM card tray is in machine before test starts', default=False),
       Arg('tray_detection_gpio', int,
           'SIM card tray detection gpio number', default=159),
+      Arg('insert', bool, 'Check sim card tray insertion', default=False),
+      Arg('remove', bool, 'Check sim card tray removal', default=False),
       Arg('only_check_presence', bool,
           'Only checks sim card tray presence matches tray_already_present. '
           'No user interaction required', default=True)]
@@ -143,11 +191,11 @@ class ProbeSimCardTrayTest(unittest.TestCase):
   def setUp(self):
     self.ui = test_ui.UI()
     self.template = ui_templates.OneSection(self.ui)
+    self.template.SetState(_HTML_SIM_CARD_TRAY)
     self.ui.AppendCSS(_CSS)
     self._task_manager = None
     self._detection_gpio_path = os.path.join(_GPIO_PATH,
         'gpio%d' % self.args.tray_detection_gpio)
-    self.force_stop = threading.Event()
 
   def ExportGPIO(self):
     """Exports GPIO of tray detection pin.
@@ -201,15 +249,19 @@ class ProbeSimCardTrayTest(unittest.TestCase):
           self.args.tray_already_present)
       return
 
+    task_list = []
     if self.args.tray_already_present:
-      task_list = [RemoveTrayTask(self),
-                   InsertTrayTask(self)]
+      self.assertTrue(self.args.remove, 'Must set remove to Ture '
+          'since tray_already_present is True')
+      task_list.append(RemoveTrayTask(self))
+      if self.args.insert:
+        task_list.append(InsertTrayTask(self))
     else:
-      task_list = [InsertTrayTask(self),
-                   RemoveTrayTask(self)]
+      self.assertTrue(self.args.insert, 'Must set insert to Ture '
+          'since tray_already_present is False')
+      task_list.append(InsertTrayTask(self))
+      if self.args.remove:
+        task_list.append(RemoveTrayTask(self))
 
-    def Done():
-      self.force_stop.set()
-
-    self._task_manager = FactoryTaskManager(self.ui, task_list, on_finish=Done)
+    self._task_manager = FactoryTaskManager(self.ui, task_list)
     self._task_manager.Run()
