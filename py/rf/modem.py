e9f32d4009e1d261982515fd53975c4dd2eb0fa1
Chun-ta Lin <itspeter@google.com>
2013-07-31 23:42:29 +0800

    Add an option to suppress the echo of modem.
    In addition to the echo suppress, The following changes are also
    included:
    1) use readlines() instead of readline() to avoid some
    buffered data like: ['\r\n', '+CEREG: 2\r\n', '\r\n', '+CEREG: 0\r\n']
    
    2) Option disable_operation will put modem into a mode more silent.
    
    3) Changing the logging level to extract more verbose information as we
    still in the stage to understand the modem behavior.
    
    BUG=chrome-os-partner:21514
    TEST=Manually with https://gerrit-int.chromium.org/#/c/42272/
    
    Change-Id: I5705d441a35a791b78d2d7998f6e2379ca5f444c
    Reviewed-on: https://gerrit.chromium.org/gerrit/63898
    Reviewed-by: Cheng-Yi Chiang <cychiang@chromium.org>
    Commit-Queue: Chun-Ta Lin <itspeter@google.com>
    Tested-by: Chun-Ta Lin <itspeter@google.com>

--- py/rf/modem.py
+++ py/rf/modem.py
@@ -8,28 +8,34 @@ import serial
 import logging
 
 import factory_common  # pylint: disable=W0611
-from cros.factory.common import Error, TimeoutError
+from cros.factory.common import Error
 
 
 class Modem(object):
-  def __init__(self, port, timeout=2):
+  def __init__(self, port, timeout=2,
+               cancel_echo=False, disable_operation=False):
     """Initiates a modem serial port communication.
 
     Args:
       port: the relative port path starts from '/dev/'
       timeout: timeout seconds that passed to pyserial
+      cancel_echo: AT command to suppress the echo
+      disable_operation: Put modem into a non-operation mode so it will
+        not throw unexpected messages.
     """
     self.ser = serial.Serial('/dev/%s' % port, timeout=timeout)
+
+    if cancel_echo:
+      self.SendLine('ATE0')
+
+    if disable_operation:
+      self.SendLine('AT+CFUN=0')
 
     # Send few AT commands to
     # 1) make sure the modem is still responsing.
     # 2) Clean previous left messages.
     self.SendLine('AT')
-    for _ in range(2):
-      try:
-        self.ReadLine()
-      except TimeoutError:
-        pass
+    logging.info('Clean current buffer data %r', self.ser.readlines())
 
     # Send an AT command and expect 'OK'
     self.SendCommand('AT')
@@ -38,12 +44,12 @@ class Modem(object):
   def ReadLine(self):
     '''Reads a line from the modem.'''
     line = self.ser.readline()
-    logging.debug('modem[ %r', line)
+    logging.info('modem[ %r', line)
     return line.rstrip('\r\n')
 
   def SendLine(self, line):
     '''Sends a line to the modem.'''
-    logging.debug('modem] %r', line)
+    logging.info('modem] %r', line)
     self.ser.write(line + '\r')
 
   def SendCommand(self, command):
