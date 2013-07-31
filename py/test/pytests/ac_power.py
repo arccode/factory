a2222a82925906e9b594f232dbcfd80d964947d9
Dean Liao <deanliao@chromium.org>
2013-07-31 11:35:50 +0800

    CHERRY-PICK: ac_power: silent charger type mismatch warning for N times.
    As EC needs time to identify charger type, we silent charger type
    mismatch warning for first N times.
    
    Update AC adapter status (offline /  online with type) on UI.
    
    Fix busy status polling when retries is None.
    
    BUG=chrome-os-partner:21397
    TEST=manual on DUT
    
    Change-Id: I6c6c6e8ce3415441a00ec771a736ce56214dd4e6
    Reviewed-on: https://gerrit.chromium.org/gerrit/63879
    Reviewed-by: Cheng-Yi Chiang <cychiang@chromium.org>
    Commit-Queue: Shuo-Peng Liao <deanliao@chromium.org>
    Tested-by: Shuo-Peng Liao <deanliao@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/170431
    Tested-by: Dean Liao <deanliao@chromium.org>
    Commit-Queue: Dean Liao <deanliao@chromium.org>

--- py/test/pytests/ac_power.py
+++ py/test/pytests/ac_power.py
@@ -4,9 +4,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""
-A test to instruct the operator to plug/unplug AC power.
-"""
+"""A test to instruct the operator / BFT fixture to plug/unplug AC power."""
 
 import threading
 import time
@@ -31,9 +29,14 @@ _PLUG_AC = lambda x: test_ui.MakeLabel(
 _UNPLUG_AC = test_ui.MakeLabel('Unplug the charger.', u'请移除充电器')
 
 _PROBE_TIMES_ID = 'probed_times'
-_PROBE_TIMES = lambda total: '%s <span id="%s">1</span> / %d' % (
+_PROBE_TIMES = lambda total: '%s <span id="%s">0</span> / %d' % (
     test_ui.MakeLabel('Probed', u'侦测次数'), _PROBE_TIMES_ID, total)
 
+_AC_STATUS_ID = 'ac_status'
+_NO_AC = test_ui.MakeLabel('No AC adapter', u'沒有充电器')
+_AC_TYPE_PROBING = test_ui.MakeLabel('Identifying AC adapter...',
+                                     u'充电器型号识别中...')
+_AC_TYPE = test_ui.MakeLabel('AC adapter type: ', u'充电器型号: ')
 
 class ACPowerTest(unittest.TestCase):
   """A test to instruct the operator to plug/unplug AC power.
@@ -41,6 +44,12 @@ class ACPowerTest(unittest.TestCase):
   Args:
     power_type: The type of the power. None to skip power type check.
     online: True if expecting AC power. Otherwise, False.
+    bft_fixture: If assigned, it commands the BFT fixture to
+        plug/unplug an AC adapter.
+    retries: Maximum number of retries allowed to pass the test.
+    polling_period_secs: Polling period in seconds.
+    silent_warning: Skips first N charger type mismatch before giving a
+        warning.
   """
 
   ARGS = [
@@ -56,6 +65,11 @@ class ACPowerTest(unittest.TestCase):
         optional=True),
     Arg('polling_period_secs', (int, float),
         'Polling period in seconds.', default=1),
+    Arg('silent_warning', int,
+        'Skips first N charger type mismatch before giving a warning. '
+        'Because EC needs about 1.6 seconds to identify charger type after '
+        'it is plugged in, it skips first N mismatched probe.',
+        default=2),
   ]
 
   def setUp(self):
@@ -66,14 +80,19 @@ class ACPowerTest(unittest.TestCase):
 
     instruction = (_PLUG_AC(self.args.power_type)
                    if self.args.online else _UNPLUG_AC)
+    probe_count_message = ''
     if self.args.retries is not None:
-      instruction += '<br>' + _PROBE_TIMES(self.args.retries)
-    self._template.SetState(instruction)
+      probe_count_message = _PROBE_TIMES(self.args.retries)
+    self._template.SetState(
+        '%s<br>%s<div id="%s"></div>' %
+        (instruction, probe_count_message, _AC_STATUS_ID))
 
     self._power_state = dict()
     self._done = threading.Event()
     self._power = system.GetBoard().power
     self._last_type = None
+    self._last_ac_present = None
+    self._skip_warning_remains = self.args.silent_warning
 
     # Prepare fixture auto test if needed.
     self.fixture = None
@@ -83,15 +102,34 @@ class ACPowerTest(unittest.TestCase):
   def Done(self):
     self._done.set()
 
+  def UpdateACStatus(self, status):
+    self._ui.SetHTML(status, id=_AC_STATUS_ID)
+
   def CheckCondition(self):
-    if self._power.CheckACPresent() != self.args.online:
-      return False
+    ac_present = self._power.CheckACPresent()
     current_type = self._power.GetACType()
+
+    # Reset silent warning countdown when AC present status change.
+    # Also reset _last_type as we want to give a warning for each
+    # mismatched charger attached.
+    if self._last_ac_present != ac_present:
+      self._last_ac_present = ac_present
+      self._skip_warning_remains = self.args.silent_warning
+      self._last_type = None
+
+    if ac_present != self.args.online:
+      if not ac_present:
+        self.UpdateACStatus(_NO_AC)
+      return False
+
     if self.args.power_type and self.args.power_type != current_type:
-      if self._last_type != current_type:
-        factory.console.warning('Expecting %s but see %s',
-                                self.args.power_type,
-                                current_type)
+      if self._skip_warning_remains > 0:
+        self.UpdateACStatus(_AC_TYPE_PROBING)
+        self._skip_warning_remains -= 1
+      elif self._last_type != current_type:
+        self.UpdateACStatus(_AC_TYPE + current_type)
+        factory.console.warning(
+            'Expecting %s but see %s', self.args.power_type, current_type)
         self._last_type = current_type
       return False
     return True
@@ -106,11 +144,11 @@ class ACPowerTest(unittest.TestCase):
     while not self._done.is_set():
       if self.CheckCondition():
         break
-      if self.args.retries is None:
-        continue
-      # retries is set
-      num_probes += 1
-      self._ui.SetHTML(str(num_probes), id=_PROBE_TIMES_ID)
-      if self.args.retries < num_probes:
-        self.fail('Failed after probing %d times' % num_probes)
+      if self.args.retries is not None:
+        # retries is set.
+        num_probes += 1
+        self._ui.SetHTML(str(num_probes), id=_PROBE_TIMES_ID)
+        if self.args.retries < num_probes:
+          self.fail('Failed after probing %d times' % num_probes)
+      # Prevent busy polling.
       time.sleep(self.args.polling_period_secs)
