230d5aa0782a0e735e0dc0ff2535ddd1c5e80414
Gediminas Ramanauskas <gedis@chromium.org>
2013-08-06 12:10:11 -0700

    CHERRY-PICK: Countdown should not fail if temp sensor returns None
    BUG=chrome-os-partner:21537
    TEST=DozingStress test
    
    Change-Id: Ibc1e1973c8fee9e278eaa2bff31fa485445a9147
    Reviewed-on: https://gerrit.chromium.org/gerrit/64804
    Reviewed-by: Jay Kim <yongjaek@chromium.org>
    Commit-Queue: Gediminas Ramanauskas <gedis@chromium.org>
    Tested-by: Gediminas Ramanauskas <gedis@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/64820
    Reviewed-by: Chinyue Chen <chinyue@chromium.org>

--- py/test/pytests/countdown.py
+++ py/test/pytests/countdown.py
@@ -79,11 +79,11 @@ class CountDownTest(unittest.TestCase):
         # is a problem.
         if last is None and current is None:
           continue
-        if last is None or current is None:
+        elif last is None or current is None:
           warnings.append(
               'Cannot read temperature index %d (current: %r, last: %r)' %
               (index, current, last))
-        if abs(current - last) > self.args.temp_max_delta:
+        elif abs(current - last) > self.args.temp_max_delta:
           warnings.append(
               'Temperature index %d delta over %d (current: %d, last: %d)' %
               (index, self.args.temp_max_delta, current, last))
