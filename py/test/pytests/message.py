128424b8ed6d257ca7b7528456a6eeae1432cb64
Cheng-Yi Chiang <cychiang@chromium.org>
2013-08-26 15:38:09 +0800

    CHERRY-PICK: message.py: Add argument for text/background color and text size
    Now we can set different text color and background color, and also
    the text size.
    
    BUG=chrome-os-partner:22201
    TEST=run message tests in the test_list
    
    Original Change-Id: I12716530d0c7ad8c1398462d0d33e14a4422fc88
    Original Reviewed-on: https://gerrit.chromium.org/gerrit/66927
    Original Reviewed-by: Vic Yang <victoryang@chromium.org>
    Original Commit-Queue: Cheng-Yi Chiang <cychiang@chromium.org>
    Original Tested-by: Cheng-Yi Chiang <cychiang@chromium.org>
    (cherry picked from commit 2775fc4ad581c106ee815a3aa98aa1b8aa93eda3)
    
    Change-Id: Ie8130f3603c0ef9985777b1a194a7869817b6186
    Reviewed-on: https://chromium-review.googlesource.com/178261
    Reviewed-by: Vic Yang <victoryang@chromium.org>
    Tested-by: Cheng-Yi Chiang <cychiang@chromium.org>
    Commit-Queue: Cheng-Yi Chiang <cychiang@chromium.org>

--- py/test/pytests/message.py
+++ py/test/pytests/message.py
@@ -18,17 +18,32 @@ from cros.factory.test.test_ui import MakeLabel, UI
 from cros.factory.test.ui_templates import OneSection
 
 
+CSS_TEMPLATE = """
+.message { font-size: %(text_size)s%%; color: %(text_color)s; }
+.state { background-color: %(background_color)s; }
+"""
+
+
 class MessageTest(unittest.TestCase):
   ARGS = [
     Arg('html_en', str, 'Message (HTML in English).'),
     Arg('html_zh', str,' Message (HTML, in Chinese).', optional=True),
+    Arg('text_size', str, 'size of message in percentage', default='200'),
+    Arg('text_color', str, 'color of message (in CSS)', default='black'),
+    Arg('background_color', str, 'background color (in CSS)', default='white')
   ]
 
   def runTest(self):
-    ui = UI(css='.message { font-size: 200% }')
+    css = (CSS_TEMPLATE %
+        dict(text_size=self.args.text_size,
+             text_color=self.args.text_color,
+             background_color=self.args.background_color))
+    ui = UI(css=css)
     template = OneSection(ui)
     template.SetTitle(MakeLabel('Message', '讯息'))
-    template.SetState(MakeLabel(self.args.html_en, self.args.html_zh,
-                                'message'))
+    template.SetState(
+        '<div class="state">' +
+        MakeLabel(self.args.html_en, self.args.html_zh, 'message') +
+        '</div>')
     ui.BindStandardKeys(bind_fail_keys=False)
     ui.Run()
