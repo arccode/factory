237987879c0ae43e875b57f3bbba347757553bca
Mao Huang <littlecvr@chromium.org>
2013-09-10 14:34:23 +0800

    Change media_util's indentation style from 4-space to 2-space.
    This CL merely changes the indentation style. Other problems will be
    fixed in the next CL.
    
    BUG=none
    TEST=make lint
    
    Change-Id: I658dd9ee2b43bee6acecd698e897b94e5267b7c8
    Reviewed-on: https://chromium-review.googlesource.com/168730
    Reviewed-by: Dean Liao <deanliao@chromium.org>
    Reviewed-by: Mao Huang <littlecvr@chromium.org>
    Tested-by: Mao Huang <littlecvr@chromium.org>
    Commit-Queue: Mao Huang <littlecvr@chromium.org>

--- py/test/media_util.py
+++ py/test/media_util.py
@@ -16,122 +16,122 @@ _UDEV_ACTION_REMOVE = 'remove'
 
 
 class MediaMonitor():
-    """A wrapper to monitor media events.
+  """A wrapper to monitor media events.
 
-    This class offers an easy way to monitor the insertion and removal
-    activities of media devices.
+  This class offers an easy way to monitor the insertion and removal
+  activities of media devices.
 
-    Usage example:
-        monitor = MediaMonitor()
-        monitor.start(on_insert=on_insert, on_remove=on_remove)
-        monitor.stop()
-    """
-    def __init__(self):
-        self._monitoring = False
+  Usage example:
+    monitor = MediaMonitor()
+    monitor.start(on_insert=on_insert, on_remove=on_remove)
+    monitor.stop()
+  """
+  def __init__(self):
+    self._monitoring = False
 
-    def udev_event_callback(self, _, action, device):
-        if action == _UDEV_ACTION_INSERT:
-            logging.info("Device inserted %s" % device.device_node)
-            self.on_insert(device.device_node)
-        elif action == _UDEV_ACTION_REMOVE:
-            logging.info('Device removed : %s' % device.device_node)
-            self.on_remove(device.device_node)
+  def udev_event_callback(self, _, action, device):
+    if action == _UDEV_ACTION_INSERT:
+      logging.info("Device inserted %s" % device.device_node)
+      self.on_insert(device.device_node)
+    elif action == _UDEV_ACTION_REMOVE:
+      logging.info('Device removed : %s' % device.device_node)
+      self.on_remove(device.device_node)
 
-    def start(self, on_insert, on_remove):
-        if self._monitoring:
-            raise Exception("Multiple start() call is not allowed")
-        self.on_insert = on_insert
-        self.on_remove = on_remove
-        # Setup the media monitor,
-        context = pyudev.Context()
-        self.monitor = pyudev.Monitor.from_netlink(context)
-        self.monitor.filter_by(subsystem='block', device_type='disk')
-        observer = pyudev.glib.GUDevMonitorObserver(self.monitor)
-        observer.connect('device-event', self.udev_event_callback)
-        self._monitoring = True
-        self._observer = observer
-        self.monitor.start()
-        logging.info("Monitoring media actitivity")
+  def start(self, on_insert, on_remove):
+    if self._monitoring:
+      raise Exception("Multiple start() call is not allowed")
+    self.on_insert = on_insert
+    self.on_remove = on_remove
+    # Setup the media monitor,
+    context = pyudev.Context()
+    self.monitor = pyudev.Monitor.from_netlink(context)
+    self.monitor.filter_by(subsystem='block', device_type='disk')
+    observer = pyudev.glib.GUDevMonitorObserver(self.monitor)
+    observer.connect('device-event', self.udev_event_callback)
+    self._monitoring = True
+    self._observer = observer
+    self.monitor.start()
+    logging.info("Monitoring media actitivity")
 
-    def stop(self):
-        # TODO(itspeter) : Add stop functionality as soon as
-        #                  pyudev.Monitor support it.
-        self._monitoring = False
+  def stop(self):
+    # TODO(itspeter) : Add stop functionality as soon as
+    #                  pyudev.Monitor support it.
+    self._monitoring = False
 
 
 class MountedMedia():
-    """A context manager to automatically mount and unmount specified device.
+  """A context manager to automatically mount and unmount specified device.
 
-    Usage example:
-        To mount the third partition of /dev/sda.
+  Usage example:
+    To mount the third partition of /dev/sda.
 
-        with MountedMedia('/dev/sda', 3) as media_path:
-            print("Mounted at %s." % media_path)
+    with MountedMedia('/dev/sda', 3) as media_path:
+      print("Mounted at %s." % media_path)
+  """
+
+  def __init__(self, dev_path, partition=None):
+    """Constructs a context manager to automatically mount/umount.
+
+    Args:
+      dev_path: The absolute path to the device.
+      partition: A optional number indicated which partition of the device
+                 should be mounted. If None is given, the dev_path will be
+                 the mounted partition.
+    Returns:
+      A MountedMedia instance with initialized proper path.
+
+    Example:
+      with MountedMedia('/dev/sdb', 1) as path:
+        with open(os.path.join(path, 'test'), 'w') as f:
+          f.write('test')
     """
+    self._mounted = False
+    if partition is None:
+      self._dev_path = dev_path
+      return
 
-    def __init__(self, dev_path, partition=None):
-        """Constructs a context manager to automatically mount/umount.
+    if dev_path[-1].isdigit():
+      # Devices enumerated in numbers (ex, mmcblk0).
+      self._dev_path = '%sp%d' % (dev_path, partition)
+    else:
+      # Devices enumerated in alphabets (ex, sda)
+      self._dev_path = '%s%d' % (dev_path, partition)
 
-        Args:
-            dev_path: The absolute path to the device.
-            partition: A optional number indicated which partition of the device
-                       should be mounted. If None is given, the dev_path will be
-                       the mounted partition.
-        Returns:
-            A MountedMedia instance with initialized proper path.
+    # For devices not using partition table (floppy mode),
+    # allow using whole device as first partition.
+    if (not os.path.exists(self._dev_path)) and (partition == 1):
+      logging.info('Using device without partition table - %s', dev_path)
+      self._dev_path = dev_path
 
-        Example:
-            with MountedMedia('/dev/sdb', 1) as path:
-                with open(os.path.join(path, 'test'), 'w') as f:
-                    f.write('test')
-        """
-        self._mounted = False
-        if partition is None:
-            self._dev_path = dev_path
-            return
+  def __enter__(self):
+    self._mount_media()
+    return self.mount_dir.name
 
-        if dev_path[-1].isdigit():
-            # Devices enumerated in numbers (ex, mmcblk0).
-            self._dev_path = '%sp%d' % (dev_path, partition)
-        else:
-            # Devices enumerated in alphabets (ex, sda)
-            self._dev_path = '%s%d' % (dev_path, partition)
+  def __exit__(self, type, value, traceback):
+    if self._mounted:
+      self._umount_media()
 
-        # For devices not using partition table (floppy mode),
-        # allow using whole device as first partition.
-        if (not os.path.exists(self._dev_path)) and (partition == 1):
-            logging.info('Using device without partition table - %s', dev_path)
-            self._dev_path = dev_path
+  def _mount_media(self):
+    """Mount a partition of media at temporary directory.
 
-    def __enter__(self):
-        self._mount_media()
-        return self.mount_dir.name
+    Exceptions are throwed if anything goes wrong.
+    """
+    # Create an temporary mount directory to mount.
+    self.mount_dir = tempdir(unique_id='MountedMedia')
+    logging.info("Media mount directory created: %s" % self.mount_dir.name)
+    exit_code, output = commands.getstatusoutput(
+        'mount %s %s' % (self._dev_path, self.mount_dir.name))
+    if exit_code != 0:
+      self.mount_dir.clean()
+      raise Exception("Failed to mount. Message-%s" % output)
+    self._mounted = True
 
-    def __exit__(self, type, value, traceback):
-        if self._mounted:
-            self._umount_media()
-
-    def _mount_media(self):
-        """Mount a partition of media at temporary directory.
-
-        Exceptions are throwed if anything goes wrong.
-        """
-        # Create an temporary mount directory to mount.
-        self.mount_dir = tempdir(unique_id='MountedMedia')
-        logging.info("Media mount directory created: %s" % self.mount_dir.name)
-        exit_code, output = commands.getstatusoutput(
-                'mount %s %s' % (self._dev_path, self.mount_dir.name))
-        if exit_code != 0:
-            self.mount_dir.clean()
-            raise Exception("Failed to mount. Message-%s" % output)
-        self._mounted = True
-
-    def _umount_media(self):
-        """Umounts the partition of the media."""
-        # Umount media and delete the temporary directory.
-        exit_code, output = commands.getstatusoutput(
-                'umount %s' % self.mount_dir.name)
-        if exit_code != 0:
-            raise Exception("Failed to umount. Message-%s" % output)
-        self.mount_dir.clean()
-        self._mounted = False
+  def _umount_media(self):
+    """Umounts the partition of the media."""
+    # Umount media and delete the temporary directory.
+    exit_code, output = commands.getstatusoutput(
+        'umount %s' % self.mount_dir.name)
+    if exit_code != 0:
+      raise Exception("Failed to umount. Message-%s" % output)
+    self.mount_dir.clean()
+    self._mounted = False
--- py/test/media_util_unittest.py
+++ py/test/media_util_unittest.py
@@ -14,8 +14,8 @@ import pyudev
 import unittest
 
 from autotest_lib.client.common_lib.autotemp import tempfile
-from cros.factory.media_util import MediaMonitor
-from cros.factory.media_util import MountedMedia
+from cros.factory.test.media_util import MediaMonitor
+from cros.factory.test.media_util import MountedMedia
 
 # udev constants
 _UDEV_ACTION_INSERT = 'add'
@@ -27,157 +27,157 @@ _VIRTUAL_PATITION_NUMBER = 3
 
 
 class TestMountedMedia(unittest.TestCase):
-    def setUp(self):
-        """Creates a temp file to mock as a media device."""
-        self._virtual_device = tempfile(unique_id='media_util_unitttest')
-        exit_code, ret = commands.getstatusoutput(
-            'truncate -s 1048576 %s && mkfs -F -t ext3 %s' %
-            (self._virtual_device.name, self._virtual_device.name))
-        self.assertEqual(0, exit_code)
+  def setUp(self):
+    """Creates a temp file to mock as a media device."""
+    self._virtual_device = tempfile(unique_id='media_util_unitttest')
+    exit_code, ret = commands.getstatusoutput(
+      'truncate -s 1048576 %s && mkfs -F -t ext3 %s' %
+      (self._virtual_device.name, self._virtual_device.name))
+    self.assertEqual(0, exit_code)
 
-        exit_code, ret = commands.getstatusoutput('losetup --show -f %s' %
-            self._virtual_device.name)
-        self._free_loop_device = ret
-        self.assertEqual(0, exit_code)
+    exit_code, ret = commands.getstatusoutput('losetup --show -f %s' %
+      self._virtual_device.name)
+    self._free_loop_device = ret
+    self.assertEqual(0, exit_code)
 
-    def tearDown(self):
-        exit_code, ret = commands.getstatusoutput(
-            'losetup -d %s' % self._free_loop_device)
-        self.assertEqual(0, exit_code)
-        self._virtual_device.clean()
+  def tearDown(self):
+    exit_code, ret = commands.getstatusoutput(
+      'losetup -d %s' % self._free_loop_device)
+    self.assertEqual(0, exit_code)
+    self._virtual_device.clean()
 
-    def testFailToMount(self):
-        """Tests the MountedMedia throws exceptions when it fails."""
-        def with_wrapper():
-            with MountedMedia('/dev/device_not_exist') as path:
-                pass
-        self.assertRaises(Exception, with_wrapper)
+  def testFailToMount(self):
+    """Tests the MountedMedia throws exceptions when it fails."""
+    def with_wrapper():
+      with MountedMedia('/dev/device_not_exist') as path:
+        pass
+    self.assertRaises(Exception, with_wrapper)
 
-    def testNormalMount(self):
-        """Tests mounting partition."""
-        with MountedMedia(self._free_loop_device) as path:
-            with open(os.path.join(path, _WRITING_TEST_FILENAME), 'w') as f:
-                f.write(_WRITING_TEST_STR)
-            with open(os.path.join(path, _WRITING_TEST_FILENAME), 'r') as f:
-                self.assertEqual(_WRITING_TEST_STR, f.readline())
+  def testNormalMount(self):
+    """Tests mounting partition."""
+    with MountedMedia(self._free_loop_device) as path:
+      with open(os.path.join(path, _WRITING_TEST_FILENAME), 'w') as f:
+        f.write(_WRITING_TEST_STR)
+      with open(os.path.join(path, _WRITING_TEST_FILENAME), 'r') as f:
+        self.assertEqual(_WRITING_TEST_STR, f.readline())
 
-    def testPartitionMountSDA(self):
-        """Tests mounting partition.
+  def testPartitionMountSDA(self):
+    """Tests mounting partition.
 
-           This tests mounting partition with devices enumerated
-           in alphabets (ex, sda).
-        """
-        virtual_partition = tempfile(unique_id='virtual_partition',
-                                     suffix='sdc%d' %
-                                     _VIRTUAL_PATITION_NUMBER)
-        exit_code, ret = commands.getstatusoutput(
-            'ln -s -f %s %s' %
-            (self._free_loop_device, virtual_partition.name))
-        self.assertEqual(0, exit_code)
+       This tests mounting partition with devices enumerated
+       in alphabets (ex, sda).
+    """
+    virtual_partition = tempfile(unique_id='virtual_partition',
+                                 suffix='sdc%d' %
+                                 _VIRTUAL_PATITION_NUMBER)
+    exit_code, ret = commands.getstatusoutput(
+      'ln -s -f %s %s' %
+      (self._free_loop_device, virtual_partition.name))
+    self.assertEqual(0, exit_code)
 
-        with MountedMedia(virtual_partition.name[:-1],
-                          _VIRTUAL_PATITION_NUMBER) as path:
-            with open(os.path.join(path, _WRITING_TEST_FILENAME), 'w') as f:
-                f.write(_WRITING_TEST_STR)
-            with open(os.path.join(path, _WRITING_TEST_FILENAME), 'r') as f:
-                self.assertEqual(_WRITING_TEST_STR, f.readline())
-        virtual_partition.clean()
+    with MountedMedia(virtual_partition.name[:-1],
+                      _VIRTUAL_PATITION_NUMBER) as path:
+      with open(os.path.join(path, _WRITING_TEST_FILENAME), 'w') as f:
+        f.write(_WRITING_TEST_STR)
+      with open(os.path.join(path, _WRITING_TEST_FILENAME), 'r') as f:
+        self.assertEqual(_WRITING_TEST_STR, f.readline())
+    virtual_partition.clean()
 
-    def testPartitionMountMMCBLK0(self):
-        """Tests mounting partition.
+  def testPartitionMountMMCBLK0(self):
+    """Tests mounting partition.
 
-           This tests mounting partition with devices enumerated
-           in alphabets (ex, mmcblk0).
-        """
-        virtual_partition = tempfile(unique_id='virtual_partition',
-                                     suffix='mmcblk0p%d' %
-                                     _VIRTUAL_PATITION_NUMBER)
-        exit_code, ret = commands.getstatusoutput(
-            'ln -s -f %s %s' %
-            (self._free_loop_device, virtual_partition.name))
-        self.assertEqual(0, exit_code)
+       This tests mounting partition with devices enumerated
+       in alphabets (ex, mmcblk0).
+    """
+    virtual_partition = tempfile(unique_id='virtual_partition',
+                                 suffix='mmcblk0p%d' %
+                                 _VIRTUAL_PATITION_NUMBER)
+    exit_code, ret = commands.getstatusoutput(
+      'ln -s -f %s %s' %
+      (self._free_loop_device, virtual_partition.name))
+    self.assertEqual(0, exit_code)
 
-        with MountedMedia(virtual_partition.name[:-2],
-                          _VIRTUAL_PATITION_NUMBER) as path:
-            with open(os.path.join(path, _WRITING_TEST_FILENAME), 'w') as f:
-                f.write(_WRITING_TEST_STR)
-            with open(os.path.join(path, _WRITING_TEST_FILENAME), 'r') as f:
-                self.assertEqual(_WRITING_TEST_STR, f.readline())
-        virtual_partition.clean()
+    with MountedMedia(virtual_partition.name[:-2],
+                      _VIRTUAL_PATITION_NUMBER) as path:
+      with open(os.path.join(path, _WRITING_TEST_FILENAME), 'w') as f:
+        f.write(_WRITING_TEST_STR)
+      with open(os.path.join(path, _WRITING_TEST_FILENAME), 'r') as f:
+        self.assertEqual(_WRITING_TEST_STR, f.readline())
+    virtual_partition.clean()
 
-    def testPartitionMountFloppy(self):
-        """Tests mounting a device without partition table."""
-        with MountedMedia(self._free_loop_device, 1) as path:
-            with open(os.path.join(path, _WRITING_TEST_FILENAME), 'w') as f:
-                f.write(_WRITING_TEST_STR)
-            with open(os.path.join(path, _WRITING_TEST_FILENAME), 'r') as f:
-                self.assertEqual(_WRITING_TEST_STR, f.readline())
+  def testPartitionMountFloppy(self):
+    """Tests mounting a device without partition table."""
+    with MountedMedia(self._free_loop_device, 1) as path:
+      with open(os.path.join(path, _WRITING_TEST_FILENAME), 'w') as f:
+        f.write(_WRITING_TEST_STR)
+      with open(os.path.join(path, _WRITING_TEST_FILENAME), 'r') as f:
+        self.assertEqual(_WRITING_TEST_STR, f.readline())
 
 
 class TestMediaMonitor(unittest.TestCase):
-    def setUp(self):
-        """Creates a temp file to mock as a media device."""
-        self._virtual_device = tempfile(unique_id='media_util_unitttest')
-        exit_code, ret = commands.getstatusoutput(
-            'truncate -s 1048576 %s' % self._virtual_device.name)
-        self.assertEqual(0, exit_code)
+  def setUp(self):
+    """Creates a temp file to mock as a media device."""
+    self._virtual_device = tempfile(unique_id='media_util_unitttest')
+    exit_code, ret = commands.getstatusoutput(
+      'truncate -s 1048576 %s' % self._virtual_device.name)
+    self.assertEqual(0, exit_code)
 
-        exit_code, ret = commands.getstatusoutput('losetup --show -f %s' %
-            self._virtual_device.name)
-        self._free_loop_device = ret
-        self.assertEqual(0, exit_code)
+    exit_code, ret = commands.getstatusoutput('losetup --show -f %s' %
+      self._virtual_device.name)
+    self._free_loop_device = ret
+    self.assertEqual(0, exit_code)
 
-    def tearDown(self):
-        exit_code, ret = commands.getstatusoutput(
-            'losetup -d %s' % self._free_loop_device)
-        self.assertEqual(0, exit_code)
-        self._virtual_device.clean()
+  def tearDown(self):
+    exit_code, ret = commands.getstatusoutput(
+      'losetup -d %s' % self._free_loop_device)
+    self.assertEqual(0, exit_code)
+    self._virtual_device.clean()
 
-    def testMediaMonitor(self):
-        def on_insert(dev_path):
-            self.assertEqual(self._free_loop_device, dev_path)
-            self._media_inserted = True
-            gtk.main_quit()
-        def on_remove(dev_path):
-            self.assertEqual(self._free_loop_device, dev_path)
-            self._media_removed = True
-            gtk.main_quit()
-        def one_time_timer_mock_insert():
-            monitor._observer.emit('device-event',
-                                   _UDEV_ACTION_INSERT,
-                                   self._mock_device)
-            return False
-        def one_time_timer_mock_remove():
-            monitor._observer.emit('device-event',
-                                   _UDEV_ACTION_REMOVE,
-                                   self._mock_device)
-            return False
+  def testMediaMonitor(self):
+    def on_insert(dev_path):
+      self.assertEqual(self._free_loop_device, dev_path)
+      self._media_inserted = True
+      gtk.main_quit()
+    def on_remove(dev_path):
+      self.assertEqual(self._free_loop_device, dev_path)
+      self._media_removed = True
+      gtk.main_quit()
+    def one_time_timer_mock_insert():
+      monitor._observer.emit('device-event',
+                             _UDEV_ACTION_INSERT,
+                             self._mock_device)
+      return False
+    def one_time_timer_mock_remove():
+      monitor._observer.emit('device-event',
+                             _UDEV_ACTION_REMOVE,
+                             self._mock_device)
+      return False
 
-        self._media_inserted = False
-        self._media_removed = False
-        self._context = pyudev.Context()
-        self._mock_device = pyudev.Device.from_name(
-            self._context, 'block',
-            os.path.basename(self._free_loop_device))
+    self._media_inserted = False
+    self._media_removed = False
+    self._context = pyudev.Context()
+    self._mock_device = pyudev.Device.from_name(
+      self._context, 'block',
+      os.path.basename(self._free_loop_device))
 
-        # Start the monitor.
-        TIMEOUT_SECOND = 1
-        monitor = MediaMonitor()
-        monitor.start(on_insert=on_insert, on_remove=on_remove)
-        # Simulating the insertion of a valid media device.
-        timer_tag = glib.timeout_add_seconds(TIMEOUT_SECOND,
-                                             one_time_timer_mock_insert)
-        gtk.main()
-        # Simulating the removal of a valid media device.
-        glib.source_remove(timer_tag)
-        timer_tag = glib.timeout_add_seconds(TIMEOUT_SECOND,
-                                             one_time_timer_mock_remove)
-        gtk.main()
+    # Start the monitor.
+    TIMEOUT_SECOND = 1
+    monitor = MediaMonitor()
+    monitor.start(on_insert=on_insert, on_remove=on_remove)
+    # Simulating the insertion of a valid media device.
+    timer_tag = glib.timeout_add_seconds(TIMEOUT_SECOND,
+                                         one_time_timer_mock_insert)
+    gtk.main()
+    # Simulating the removal of a valid media device.
+    glib.source_remove(timer_tag)
+    timer_tag = glib.timeout_add_seconds(TIMEOUT_SECOND,
+                                         one_time_timer_mock_remove)
+    gtk.main()
 
-        monitor.stop()
-        self.assertEqual(True, self._media_inserted)
-        self.assertEqual(True, self._media_removed)
+    monitor.stop()
+    self.assertEqual(True, self._media_inserted)
+    self.assertEqual(True, self._media_removed)
 
 
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
