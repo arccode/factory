8f0c8dbccb63e7f512724eefc7cc2f2fee33dbf1
Dean Liao <deanliao@chromium.org>
2013-09-10 21:09:53 +0800

    Add ArduinoController class to talk to Arduino controller.
    Arduino can be used to set/get GPIO to control fixture.
    This is the first implementation of controlling Arduino board
    and can be extended later.
    
    BUG=chrome-os-partner:22479,chrome-os-partner:22608
    TEST="make test" and verify it an Arduino board.
    
    Change-Id: I35c47c157ceea1bfba215077e729b0da81846e83
    Reviewed-on: https://chromium-review.googlesource.com/168746
    Reviewed-by: Dean Liao <deanliao@chromium.org>
    Commit-Queue: Dean Liao <deanliao@chromium.org>
    Tested-by: Dean Liao <deanliao@chromium.org>

---
+++ py/test/fixture/arduino.py
@@ -0,0 +1,156 @@
+#!/usr/bin/python
+
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging
+import serial
+import time
+
+import factory_common  # pylint: disable=W0611
+from cros.factory.utils.serial_utils import SerialDevice
+
+
+class ArduinoController(SerialDevice):
+  """Used to communicate with fixture with Arduino microcontroller.
+
+  An optional ready_delay_secs is used that Arduino Uno is reset for
+  each connection and it takes about 2 seconds to reboot. It is better
+  to wait before any action.
+
+  For Arduino code, it looks like:
+
+  int targetPin = 12;
+
+  void setup()  {
+    Serial.begin(9600);
+    pinMode(targetPin, OUTPUT);
+    digitalWrite(targetPin, LOW);
+  }
+
+  void loop() {
+    if (Serial.available() > 0) {
+      int cmd = Serial.read();
+      switch (cmd) {
+      case 'H':
+        digitalWrite(targetPin, HIGH);
+        Serial.write('H');
+        break;
+      case 'L':
+        digitalWrite(targetPin, LOW);
+        Serial.write('L');
+        break;
+      case 1:
+        Serial.write(1);
+        break;
+      case 2:
+        Serial.write(2);
+        break;
+      case 3:
+        Serial.write(3);
+        break;
+      default:
+        Serial.write('X');  // unsupport
+      }
+    }
+  }
+  """
+  def __init__(self, send_receive_interval_secs=0.5, retry_interval_secs=0.2,
+               log=True, ready_delay_secs=2.0):
+    """Constructor.
+
+    Besides parameters in parent's ctor, it adds ready_delay_secs which is
+    used to wait for Arduino to boot after reset.
+    """
+    super(ArduinoController, self).__init__(
+        send_receive_interval_secs=send_receive_interval_secs,
+        retry_interval_secs=retry_interval_secs, log=log)
+    self._ready_delay_secs = ready_delay_secs
+
+  def __del__(self):
+    self.Disconnect()
+
+  def Connect(self, **kwargs):
+    """Opens a serial connection.
+
+    After connection establish, it waits for ready_delay_secs as Arduino
+    is reset for each connection, which takes a while to reboot. After that, it
+    pings the Arduino board to make sure it works.
+
+    Args:
+      driver: driver name of the target serial connection. Used to look up port
+          if port is not specified. Default 'cdc_acm'.
+      port, baudrate, bytesize, parity, stopbits, timeout, writeTimeout: See
+          serial.Serial().
+
+    Raises:
+      SerialException on errors.
+    """
+    if 'driver' not in kwargs:
+      kwargs['driver'] = 'cdc_acm'
+
+    super(ArduinoController, self).Connect(**kwargs)
+    time.sleep(self._ready_delay_secs)
+
+    if not self.Ping():
+      self.Disconnect()
+      raise serial.SerialException('Ping Arduino (port %s) failed' % self._port)
+
+  def Ping(self, retry=0):
+    """Pings Arduino. Used for handshaking.
+
+    It sends command '1', '2', '3' and expects response '1', '2', '3',
+    respectively. For the command '1', we can retry N times (to wait for
+    Arduino ready). But for command "2" and "3", Arduino should response
+    as expected without retry.
+
+    Args:
+      retry: number of retry.
+    """
+    return (self.SendExpectReceive(chr(1), chr(1), retry=retry) and
+            self.SendExpectReceive(chr(2), chr(2)) and
+            self.SendExpectReceive(chr(3), chr(3)))
+
+  def Reset(self, wait_ready=True):
+    """Resets Arduino controller.
+
+    Args:
+      wait_ready: True to wait for ready_delay_secs after reset.
+    """
+    # Pull down DTR can reset Arduino Uno.
+    self._serial.setDTR(False)
+    time.sleep(0.05)
+    self._serial.setDTR(True)
+    if wait_ready:
+      time.sleep(self._ready_delay_secs)
+
+
+def _Blink(arduino, times=1, interval_secs=0.1):
+  """Blinks LED light in Arduino board.
+
+  This is an example of how to use ArduinoController object.
+  """
+  for nth in range(times):
+    if nth:
+      time.sleep(interval_secs)
+
+    if not arduino.SendExpectReceive('H', 'H', retry=2):
+      raise Exception('Failed to send command "H"')
+
+    time.sleep(interval_secs)
+
+    if not arduino.SendExpectReceive('L', 'L', retry=2):
+      raise Exception('Failed to send command "L"')
+
+
+def main():
+  logging.basicConfig(level=logging.INFO)
+  arduino = ArduinoController()
+  arduino.Connect()
+  _Blink(arduino, times=3, interval_secs=0.5)
+  arduino.Reset()
+
+
+if __name__ == '__main__':
+  main()
---
+++ py/test/fixture/arduino_unittest.py
@@ -0,0 +1,121 @@
+#!/usr/bin/python -u
+#
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Unittest for serial_utils."""
+
+import mox
+import serial
+import time
+import unittest
+
+import factory_common  # pylint: disable=W0611
+from cros.factory.test.fixture.arduino import ArduinoController
+from cros.factory.utils.serial_utils import SerialDevice
+
+_DEFAULT_DRIVER = 'cdc_acm'
+_DEFAULT_READY_DELAY_SECS = 2.0
+
+# pylint: disable=E1120
+class ArduinoControllerTest(unittest.TestCase):
+  def setUp(self):
+    self.mox = mox.Mox()
+    self.device = ArduinoController()
+
+  def tearDown(self):
+    del self.device
+    self.mox.UnsetStubs()
+    self.mox.VerifyAll()
+
+  def StubOutForConnect(self):
+    self.mox.StubOutWithMock(SerialDevice, 'Connect')
+    self.mox.StubOutWithMock(time, 'sleep')
+    self.mox.StubOutWithMock(self.device, 'Ping')
+
+  def testConnectDefault(self):
+    self.StubOutForConnect()
+    SerialDevice.Connect(driver=_DEFAULT_DRIVER)
+    time.sleep(_DEFAULT_READY_DELAY_SECS)
+    self.device.Ping().AndReturn(True)
+
+    self.mox.ReplayAll()
+    self.device.Connect()
+
+  def testCustomReadyDelay(self):
+    ready_delay_secs = 0.5
+    self.device = ArduinoController(ready_delay_secs=ready_delay_secs)
+    self.StubOutForConnect()
+    SerialDevice.Connect(driver=_DEFAULT_DRIVER)
+    time.sleep(ready_delay_secs)
+    self.device.Ping().AndReturn(True)
+
+    self.mox.ReplayAll()
+    self.device.Connect()
+
+  def testCustomDriver(self):
+    custom_driver = 'CustomDriver'
+    self.StubOutForConnect()
+    SerialDevice.Connect(driver=custom_driver)
+    time.sleep(_DEFAULT_READY_DELAY_SECS)
+    self.device.Ping().AndReturn(True)
+
+    self.mox.ReplayAll()
+    self.device.Connect(driver=custom_driver)
+
+  def testConnectPingFailed(self):
+    self.StubOutForConnect()
+    SerialDevice.Connect(driver=_DEFAULT_DRIVER)
+    time.sleep(_DEFAULT_READY_DELAY_SECS)
+    self.device.Ping().AndReturn(False)
+
+    self.mox.ReplayAll()
+    self.assertRaises(serial.SerialException, self.device.Connect)
+
+  def testPing(self):
+    self.mox.StubOutWithMock(self.device, 'SendExpectReceive')
+    self.device.SendExpectReceive(chr(1), chr(1), retry=0).AndReturn(True)
+    self.device.SendExpectReceive(chr(2), chr(2)).AndReturn(True)
+    self.device.SendExpectReceive(chr(3), chr(3)).AndReturn(True)
+
+    self.mox.ReplayAll()
+    self.assertTrue(self.device.Ping())
+
+  def testPingFail(self):
+    self.mox.StubOutWithMock(self.device, 'SendExpectReceive')
+    self.device.SendExpectReceive(chr(1), chr(1), retry=0).AndReturn(True)
+    self.device.SendExpectReceive(chr(2), chr(2)).AndReturn(False)
+
+    self.mox.ReplayAll()
+    self.assertFalse(self.device.Ping())
+
+  def testReset(self):
+    mock_serial = self.mox.CreateMock(serial.Serial)
+    self.mox.StubOutWithMock(time, 'sleep')
+    self.device._serial = mock_serial
+    mock_serial.setDTR(False)
+    time.sleep(0.05)
+    mock_serial.setDTR(True)
+    time.sleep(_DEFAULT_READY_DELAY_SECS)
+    mock_serial.close()
+
+    self.mox.ReplayAll()
+    self.device.Reset()
+
+  def testResetNoWait(self):
+    mock_serial = self.mox.CreateMock(serial.Serial)
+    self.mox.StubOutWithMock(time, 'sleep')
+    self.device._serial = mock_serial
+    mock_serial.setDTR(False)
+    time.sleep(0.05)
+    mock_serial.setDTR(True)
+    mock_serial.close()
+
+    self.mox.ReplayAll()
+    self.device.Reset(wait_ready=False)
+
+
+if __name__ == '__main__':
+  unittest.main()
+
