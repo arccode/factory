f8bfcdd224f207ed1707bbd3f2948759ec25df2c
Jay (Yong Jae) Kim <yongjaek@google.com>
2013-09-18 15:40:28 -0700

    CHERRY-PICK: Add touchscreen component probing
    BUG=chrome-os-partner:22782
    TEST=gooftool probe on Peppy Touch
    
    Change-Id: I0dcf8f4630e15a690b8204e388aad5fe773f90d3
    Reviewed-on: https://chromium-review.googlesource.com/169877
    Reviewed-by: Mohammed Habibulla <moch@chromium.org>
    Reviewed-by: Dave Parker <dparker@chromium.org>
    Commit-Queue: Jay Kim <yongjaek@chromium.org>
    Tested-by: Jay Kim <yongjaek@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/187640
    Reviewed-by: Jay Kim <yongjaek@chromium.org>
    Commit-Queue: Justin Chuang <jchuang@chromium.org>
    Tested-by: Justin Chuang <jchuang@chromium.org>

--- py/gooftool/probe.py
+++ py/gooftool/probe.py
@@ -415,6 +415,54 @@ class _TouchpadData():  # pylint: disable=W0232
           break
     return cls.cached_data
 
+class _TouchscreenData():  # pylint: disable=W0232
+  """Return Obj with hw_ident and fw_ident string fields."""
+
+  @classmethod
+  def Atmel(cls):
+    input_file = '/proc/bus/input/devices'
+    re_device_name = re.compile(r'^N: Name="(Atmel.*Touchscreen)"$',
+                                re.MULTILINE)
+    re_sysfs = re.compile(r'^S: Sysfs=(.*)$', re.MULTILINE)
+    with open(input_file, 'r') as f:
+      buf = f.read()
+    devices = buf.split('\n\n')
+    for d in devices:
+      match = re_device_name.findall(d)
+      if not match:
+        continue
+      device_name = match[0]
+      sysfs_path = os.path.join('/sys', re_sysfs.findall(d)[0].lstrip('/'))
+      with open(os.path.join(sysfs_path, '..', '..', 'fw_version'), 'r') as f:
+        fw_version = f.read().strip()
+      with open(os.path.join(sysfs_path, '..', '..', 'config_csum'), 'r') as f:
+        config_csum = f.read().strip()
+      return Obj(ident_str=device_name, fw_version=fw_version,
+                 config_csum=config_csum)
+
+  @classmethod
+  def Generic(cls):
+    # TODO(hungte) add more information from id/*
+    # format: N: Name="??? touchscreen"
+    input_file = '/proc/bus/input/devices'
+    cmd = 'grep -iE "^N.*(touch *screen)" %s' % input_file
+    info = Shell(cmd).stdout.splitlines()
+    info = [re.sub('^[^"]*"(.*)"$', r'\1', device) for device in info]
+    return Obj(ident_str=(', '.join(info) if info else None), fw_version=None)
+
+  cached_data = None
+
+  @classmethod
+  def Get(cls):
+    if cls.cached_data is None:
+      cls.cached_data = Obj(ident_str=None, fw_version=None)
+      for vendor_fun in [cls.Atmel, cls.Generic]:
+        data = vendor_fun()
+        if data is not None:
+          cls.cached_data = data
+          break
+    return cls.cached_data
+
 
 def _ProbeFun(probe_map, probe_class, *arch_targets):   # pylint: disable=C9011
   """Decorator that populates probe_map.
@@ -805,6 +853,21 @@ def _ProbeStorage():
 @_ComponentProbe('touchpad')
 def _ProbeTouchpad():
   data = _TouchpadData.Get()
+  if data.ident_str is None:
+    return []
+
+  results = {'id': data.ident_str}
+  results.update(DictCompactProbeStr(data.ident_str))
+  for key in ('fw_version', 'config_csum'):
+    value = getattr(data, key, '')
+    if value:
+      results[key] = value
+  return [results]
+
+
+@_ComponentProbe('touchscreen')
+def _ProbeTouchscreen():
+  data = _TouchscreenData.Get()
   if data.ident_str is None:
     return []
 
