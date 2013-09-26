08767599569c1f0e05f3f4faf37f760508d27677
Dean Liao <deanliao@chromium.org>
2013-09-26 20:12:58 +0800

    Move touchscreen from private overlay to public.
    CQ-DEPEND=CL:*145367
    BUG=chrome-os-partner:22935
    TEST=manual on Pixel
    1. Create a branch to factory-3004.B on platform/factory.
    2. Cherry-pick https://chromium-review.googlesource.com/#/c/39311/
       (we need setFullScreen function in goofy.js).
    3. Cherry-pick this CL.
    4. goofy_remote to a Pixel with factory image.
    5. Add a test with pytest='touchscreen' then test it.
    
    Change-Id: I1bbb01621bdcf7d7bab31c4ad2888560de435af8
    Reviewed-on: https://chromium-review.googlesource.com/170757
    Reviewed-by: Dean Liao <deanliao@chromium.org>
    Commit-Queue: Dean Liao <deanliao@chromium.org>
    Tested-by: Dean Liao <deanliao@chromium.org>

---
+++ py/test/pytests/touchscreen.py
@@ -0,0 +1,330 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""
+A factory test to test the functionality of touchscreen.
+
+dargs:
+  touchscreen_event_id: Touchscreen input event id. (default: 7)
+"""
+
+import logging
+import re
+import subprocess
+import unittest
+
+from cros.factory.test import test_ui
+from cros.factory.test.args import Arg
+from cros.factory.test.ui_templates import OneSection
+from cros.factory.test.utils import StartDaemonThread
+from cros.factory.utils.process_utils import Spawn
+
+
+#Event: time 1352876832.935291, type 3 (EV_ABS), code 53
+_RE_ABS = r'^Event: time .*?, type .*? \(EV_ABS\), code .*? '
+
+#Event: time 1352876919.881157, type 1 (EV_KEY), code 333
+_RE_KEY = r'^Event: time .*?, type .*? \(EV_KEY\), code .*? '
+
+#Event: time 1352876832.935301, -------------- SYN_REPORT ------------
+_RE_EVTEST_SYNREPORT = re.compile(
+    r'^Event: time .*?, -* SYN_REPORT -*$')
+
+#Event: time 1352876832.935291, type 3 (EV_ABS), code 53
+# (ABS_MT_POSITION_X), value 436
+_RE_EVTEST_X_MOVE_EVENT = re.compile(
+    _RE_ABS + r'\(ABS_MT_POSITION_X\), value (.*?)$')
+
+#Event: time 1352876832.935292, type 3 (EV_ABS), code 54
+# (ABS_MT_POSITION_Y), value 764
+_RE_EVTEST_Y_MOVE_EVENT = re.compile(
+    _RE_ABS + r'\(ABS_MT_POSITION_Y\), value (.*?)$')
+
+#Event: time 1356667900.951634, type 3 (EV_ABS), code 47
+# (ABS_MT_SLOT), value 0
+# This event means there are multiple fingers.
+_RE_EVTEST_SLOT_EVENT = re.compile(
+    _RE_ABS + r'\(ABS_MT_SLOT\), value (.*?)$')
+
+#Event: time 1356670305.408711, type 3 (EV_ABS), code 57
+# (ABS_MT_TRACKING_ID), value -1
+# Value > 0 in this event means new finger start touching.
+# Value -1 in this event means finger left touchscreen.
+_RE_EVTEST_TRACKING_ID_EVENT = re.compile(
+    _RE_ABS + r'\(ABS_MT_TRACKING_ID\), value (.*?)$')
+
+#    Event code 53 (ABS_MT_POSITION_X)
+#      Value      0
+#      Min        0
+#      Max     2040
+#      Resolution      20
+_RE_EVTEST_X_MAX = re.compile(
+    r'\(ABS_MT_POSITION_X\).*?Max[\s]*([\d]*)', flags=re.DOTALL)
+
+#    Event code 54 (ABS_MT_POSITION_Y)
+#      Value      0
+#      Min        0
+#      Max     1360
+#      Resolution      20
+_RE_EVTEST_Y_MAX = re.compile(
+    r'\(ABS_MT_POSITION_Y\).*?Max[\s]*([\d]*)', flags=re.DOTALL)
+
+#Testing ... (interrupt to exit)
+_RE_EVTEST_START_TESTING = re.compile(
+    r'Testing ...')
+
+#Input device name: "XXXXXX"
+_RE_EVTEST_NAME = re.compile(
+    r'Input device name: "(.*?)"$', flags=re.MULTILINE)
+
+_ID_CONTAINER = 'touchscreen-test-container'
+
+# The style is in touchscreen.css
+# The layout contains one div for touchscreen.
+_HTML_TOUCHSCREEN = (
+    '<link rel="stylesheet" type="text/css" href="touchscreen.css">'
+    '<div id="%s"></div>\n' % _ID_CONTAINER)
+
+_X_SEGMENTS = 8
+_Y_SEGMENTS = 8
+
+
+class TouchEvent:
+  '''The class to store touchscreen touch event.'''
+  def __init__(self):
+    self.x = None
+    self.y = None
+    self.sync = None
+    self.leave = None
+
+  def Clear(self):
+    self.x = self.y = self.sync = self.leave = None
+
+
+def GetProp(message, pattern):
+  '''
+  Gets the property from searching pattern in message.
+  Args:
+    message: A string to search for pattern.
+    pattern: A regular expression object which will capture a value if pattern
+             can be found.
+  '''
+  val = None
+  obj = pattern.search(message)
+  if obj:
+    val = obj.group(1)
+  return val
+
+def CheckUpdate(new_value, old_value):
+  '''Returns new_value if new_value is not None, return old_value otherwise.'''
+  return new_value if new_value else old_value
+
+class TouchscreenTest(unittest.TestCase):
+  '''
+  Tests the function of touchscreen. The test detects that finger
+  has left on every sector of touchscreen.
+  Properties:
+    self.ui: test ui.
+    self.template: ui template handling html layout.
+    self.x_max: max grid value of horizontal movement.
+    self.y_max: max grid valud of vertical movement.
+    self.touchscreen_device_name: This can be probed from evtest.
+        We need touchscreen device name to enable/disable it using xinput.
+    self.touch_event: the detected touch event. The event will be drew
+        and reset if there are a sync event AND a leave event.
+    self.monitor_process: the evtest process to get touchscreen input.
+        This should get terminated when test stops.
+    self.checked: user has already pressed spacebar to check touchscreen.
+  '''
+  ARGS = [
+    Arg('touchscreen_event_id', int, 'Touchscreen input event id.',
+        default=7)
+  ]
+
+  def setUp(self):
+    # Initialize frontend presentation
+    self.ui = test_ui.UI()
+    self.template = OneSection(self.ui)
+    self.ui.AppendHTML(_HTML_TOUCHSCREEN)
+    self.ui.CallJSFunction('setupTouchscreenTest', _ID_CONTAINER,
+        _X_SEGMENTS, _Y_SEGMENTS)
+
+    # Initialize properties
+    self.x_max = None
+    self.y_max = None
+    self.touchscreen_device_name = None
+    self.touch_event = TouchEvent()
+    self.monitor_process = None
+    self.checked = False
+
+    logging.info('start monitor daemon thread')
+    StartDaemonThread(target=self.MonitorEvtest)
+
+  def tearDown(self):
+    '''
+    Terminates the running process or we'll have trouble stopping the
+    test. Enable the touchscreen at X to enable touchscreen function in test ui.
+    '''
+    self.TerminateProcess()
+    self.EnableTouchscreenX(True)
+
+  def MonitorEvtest(self):
+    '''
+    Starts evtest process, gets the spec of touchscreen,
+    disables touchscreen at X, and monitors touchscreen events
+    from output of evtest.
+    '''
+    self.monitor_process = Spawn(['evtest', '/dev/input/event%d' % (
+                                  self.args.touchscreen_event_id)],
+                                  stdout=subprocess.PIPE)
+    self.GetSpec()
+    self.EnableTouchscreenX(False)
+    self.MonitorEvent()
+
+  def GetSpecMessages(self):
+    '''
+    Gets spec messages from evtest output before testing starts.
+    Messages are like:
+
+      Input driver version is 1.0.1
+      Input device ID: bus 0x18 vendor 0x0 product 0x0 version 0x0
+      Input device name: "<TOUCHSCREEN_NAME>"
+      Supported events:
+        Event type 0 (EV_SYN)
+        Event type 1 (EV_KEY)
+          Event code 330 (BTN_TOUCH)
+        Event type 3 (EV_ABS)
+          Event code 0 (ABS_X)
+            ...
+          Event code 1 (ABS_Y)
+            ...
+          Event code 24 (ABS_PRESSURE)
+            ...
+          Event code 47 (ABS_MT_SLOT)
+            ...
+          Event code 48 (ABS_MT_TOUCH_MAJOR)
+            ...
+          Event code 53 (ABS_MT_POSITION_X)
+            Value      0
+            Min        0
+            Max     <X_RANGE>
+            Resolution     <X_RESOLUTION>
+          Event code 54 (ABS_MT_POSITION_Y)
+            Value      0
+            Min        0
+            Max     <Y_RANGE>
+            Resolution     <Y_RESOLUTION>
+          Event code 57 (ABS_MT_TRACKING_ID)
+            ...
+          Event code 58 (ABS_MT_PRESSURE)
+            ...
+      Testing ... (interrupt to exit)
+    '''
+    logging.info('getting spec message..')
+    spec_message_lines = []
+    spec_messages = None
+    while True:
+      spec_message_line = self.monitor_process.stdout.readline()
+      if _RE_EVTEST_START_TESTING.search(spec_message_line):
+        spec_messages = ''.join(spec_message_lines)
+        break
+      else:
+        spec_message_lines.append(spec_message_line)
+    return spec_messages
+
+  def GetSpec(self):
+    '''Gets device name, x_max and y_max from evtest output'''
+    spec_messages = self.GetSpecMessages()
+    logging.info('parsing spec message...\n %s', spec_messages)
+    self.x_max = int(GetProp(spec_messages, _RE_EVTEST_X_MAX))
+    self.y_max = int(GetProp(spec_messages, _RE_EVTEST_Y_MAX))
+    self.touchscreen_device_name = GetProp(spec_messages, _RE_EVTEST_NAME)
+
+    logging.info('get device %s spec , x_max = %d, y_max = %d',
+                 self.touchscreen_device_name, self.x_max, self.y_max)
+
+  def EnableTouchscreenX(self, enable):
+    '''Enables/Disables touchscreen at the X server.'''
+    Spawn(['xinput', 'set-prop', self.touchscreen_device_name,
+           'Device Enabled', '1' if enable else '0'], check_call=True)
+
+  def MonitorEvent(self):
+    '''Gets event message from evtest and process it'''
+    while True:
+      event_message = self.monitor_process.stdout.readline()
+      self.ProcessTouchEvent(event_message)
+
+  def ProcessTouchEvent(self, event_message):
+    '''
+    Parses event_message, update touch_event and draws it upon receving
+    sync event AND leave event.
+    Args:
+      event_message: one line of event message from evtest.
+    '''
+    self.ParseMessageAndUpdateTouchEvent(event_message)
+    if self.touch_event.sync and self.touch_event.leave:
+      self.DrawTouchEvent()
+      self.touch_event.Clear()
+
+  def DrawTouchEvent(self):
+    '''
+    Marks a scroll sector as tested or a move sector as tested.
+    '''
+    if self.touch_event.x and self.touch_event.y:
+      x_ratio = float(self.touch_event.x) / float(self.x_max + 1)
+      y_ratio = float(self.touch_event.y) / float(self.y_max + 1)
+      self.MarkSectorTested(x_ratio, y_ratio)
+
+  def ParseMessageAndUpdateTouchEvent(self, event_message):
+    '''
+    Parses event_message and updates touch_event.
+    Args:
+      event_message: one line of event message from evtest.
+    '''
+    self.touch_event.x = CheckUpdate(
+        GetProp(event_message, _RE_EVTEST_X_MOVE_EVENT), self.touch_event.x)
+    self.touch_event.y = CheckUpdate(
+        GetProp(event_message, _RE_EVTEST_Y_MOVE_EVENT), self.touch_event.y)
+    tracking_id_str = GetProp(event_message, _RE_EVTEST_TRACKING_ID_EVENT)
+    tracking_id = int(tracking_id_str) if tracking_id_str else None
+    if tracking_id > 0:
+      self.touch_event.leave = False
+    elif tracking_id == -1:
+      self.touch_event.leave = True
+    if _RE_EVTEST_SYNREPORT.search(event_message):
+      self.touch_event.sync = True
+    if _RE_EVTEST_SLOT_EVENT.search(event_message):
+      self.ui.CallJSFunction('twoFingersException')
+
+  def TerminateProcess(self):
+    '''terminate the process if it is running.'''
+    if self.monitor_process.poll() is None:
+      self.monitor_process.terminate()
+
+  def MarkSectorTested(self, x_ratio, y_ratio):
+    '''
+    Gets the segment from x_ratio and y_ratio then calls Javascript to
+    mark the sector as tested.
+    '''
+    x_segment = int(x_ratio * _X_SEGMENTS)
+    y_segment = int(y_ratio * _Y_SEGMENTS)
+    logging.info('mark x-%d y-%d sector tested', x_segment, y_segment)
+    self.ui.CallJSFunction('markSectorTested', x_segment, y_segment)
+
+  def OnSpacePressed(self):
+    '''Calls JS function to switch display on/off.'''
+    self.checked = True
+    self.ui.CallJSFunction('switchDisplayOnOff')
+
+  def OnFailPressed(self):
+    '''Fails the test only if self.checked is True.'''
+    if self.checked:
+      self.ui.CallJSFunction('failTest')
+      self.checked = False
+
+  def runTest(self):
+    self.ui.BindKey(' ', lambda _: self.OnSpacePressed())
+    self.ui.BindKey(test_ui.ESCAPE_KEY, lambda _: self.OnFailPressed())
+    self.ui.Run()
---
+++ py/test/pytests/touchscreen_static/touchscreen.js
@@ -0,0 +1,222 @@
+// Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * API for touchscreen test.
+ * @constructor
+ * @param {string} container
+ * @param {int} xSegments
+ * @param {int} ySegments
+ */
+TouchscreenTest = function(container, xSegments, ySegments) {
+  this.container = container;
+  this.xSegments = xSegments;
+  this.ySegments = ySegments;
+  this.display = false;
+  this.enInstruct = "Press Space to display and use one finger to " +
+                    "touch each sector; Esc to fail.";
+  this.zhInstruct = "按空格键显示并用一个手指触摸每区; 按Esc键失败。";
+};
+
+/**
+ * Creates a touchscreen test and runs it.
+ * @param {string} container
+ * @param {int} xSegments
+ * @param {int} ySegments
+ */
+function setupTouchscreenTest(container, xSegments, ySegments) {
+  window.touchscreenTest = new TouchscreenTest(container, xSegments, ySegments);
+  window.touchscreenTest.init();
+  window.touchscreenTest.setupFullScreenElement();
+}
+
+/**
+ * Initializes touchscreen caption div elements.
+ */
+TouchscreenTest.prototype.init = function() {
+  var caption = document.createElement("div");
+  caption.className = "touchscreen-caption";
+  appendSpanEnZh(caption, this.enInstruct, this.zhInstruct);
+  $(this.container).appendChild(caption);
+};
+
+/**
+ * Initializes fullscreen div elements.
+ * The touch table contains xSegment by ySegment divs
+ */
+TouchscreenTest.prototype.setupFullScreenElement = function() {
+  this.fullScreenElement = document.createElement("div");
+  this.fullScreenElement.className = "touchscreen-full-screen-hide";
+  var touchscreenTable = createTable(this.ySegments, this.xSegments, "touch",
+                                     "touchscreen-test-sector-untested");
+  this.fullScreenElement.appendChild(touchscreenTable);
+  $(this.container).appendChild(this.fullScreenElement);
+};
+
+/**
+ * Toggles the fullscreen display visibility.
+ */
+TouchscreenTest.prototype.switchDisplayOnOff = function() {
+  this.display = !this.display;
+  if (this.display) {
+    this.switchDisplayOn();
+  } else {
+    this.switchDisplayOff();
+  }
+};
+
+/**
+ * Switches the fullscreen display on. Sets fullScreenElement
+ * visibility to visible and enlarges the test iframe to fullscreen.
+ */
+TouchscreenTest.prototype.switchDisplayOn = function() {
+  this.display = true;
+  this.fullScreenElement.className = "touchscreen-full-screen-show";
+  window.test.setFullScreen(true);
+};
+
+/**
+ * Switches the fullscreen display off. Sets fullScreenElement
+ * visibility to hidden and restores the test iframe to normal.
+ */
+TouchscreenTest.prototype.switchDisplayOff = function() {
+  this.display = false;
+  this.fullScreenElement.className = "touchscreen-full-screen-hide";
+  window.test.setFullScreen(false);
+};
+
+/**
+ * Marks the given (x,y) sector as "tested" on the test ui.
+ * @param {int} x
+ * @param {int} y
+ */
+TouchscreenTest.prototype.markSectorTested = function(x, y) {
+  var id = "touch-x-" + x + "-y-" + y;
+  var element = document.getElementById(id);
+  if (element) {
+    element.className = "touchscreen-test-sector-tested";
+  }
+  this.checkTestComplete();
+};
+
+/**
+ * Checks if test is completed by checking the number of sectors that
+ * haven't passed the test.
+ * */
+TouchscreenTest.prototype.checkTestComplete = function() {
+  if (this.getClassArray("touchscreen-test-sector-untested").length == 0)  {
+    window.test.pass();
+  }
+};
+
+/**
+ * Fails the test and prints out all the failed items.
+ */
+TouchscreenTest.prototype.failTest = function() {
+  var failedSectors = new Array();
+
+  this.getClassArray("touchscreen-test-sector-untested").forEach(
+    function(element) {
+      failedSectors.push((element.id));
+    }
+  );
+
+  this.failMsg = "Touchscreen test failed. Malfunction sectors:";
+  failedSectors.forEach(function(element, index, array) {
+    this.failMsg += " " + element;
+    if (index != array.length -1) {
+      this.failMsg += ",";
+    }
+  }, this);
+  window.test.fail(this.failMsg);
+};
+
+/**
+ * Returns an Array converted from the NodeList of the given class.
+ * @param {string} className
+ * @return {Array}
+ */
+TouchscreenTest.prototype.getClassArray = function(className) {
+  return Array.prototype.slice.call(document.getElementsByClassName(className));
+};
+
+/**
+ * Appends en span and zh span to the input element.
+ * @param {Element} div the element to which we want to append spans.
+ * @param {string} en the English text to be appended.
+ * @param {string} zh the Simplified-Chinese text to be appended.
+ */
+function appendSpanEnZh(div, en, zh) {
+  var en_span = document.createElement("span");
+  var zh_span = document.createElement("span");
+  en_span.className = "goofy-label-en";
+  en_span.innerHTML = en;
+  zh_span.className = "goofy-label-zh";
+  zh_span.innerHTML = zh;
+  div.appendChild(en_span);
+  div.appendChild(zh_span);
+}
+
+/**
+ * Creates a table element with specified row number and column number.
+ * Each td in the table contains one div with id prefix-x-x_number-y-y_number
+ * and the specified CSS class.
+ * @param {int} rowNumber
+ * @param {int} colNumber
+ * @param {String} prefix
+ * @param {String} className
+ * @return {table}
+ */
+function createTable(rowNumber, colNumber, prefix, className) {
+  var table = document.createElement("table");
+  table.style.width = "100%";
+  table.style.height = "100%";
+  var tableBody = document.createElement("tbody");
+  for (var y = 0; y < rowNumber; ++y) {
+    var row = document.createElement("tr");
+    for (var x = 0; x < colNumber; ++x) {
+      var cell = document.createElement("td");
+      var div = document.createElement("div");
+      div.id = prefix + "-x-" + x + "-" + "y-" + y;
+      div.innerHTML = div.id;
+      div.className = className;
+      cell.appendChild(div);
+      row.appendChild(cell);
+    }
+    tableBody.appendChild(row);
+  }
+  table.appendChild(tableBody);
+  return table;
+}
+
+/**
+ * Switches the display.
+ */
+function switchDisplayOnOff() {
+  window.touchscreenTest.switchDisplayOnOff();
+}
+
+/**
+ * Marks a sector as tested.
+ * @param {int} x
+ * @param {int} y
+ */
+function markSectorTested(x, y) {
+  window.touchscreenTest.markSectorTested(x, y);
+}
+
+/**
+ * Fails the test.
+ */
+function failTest() {
+  window.touchscreenTest.failTest();
+}
+
+/**
+ * Shows message of two fingers and fail the test.
+ */
+function twoFingersException() {
+  alert('Use only one finger!' + ' 请用一个手指');
+  window.touchscreenTest.failTest();
+}
