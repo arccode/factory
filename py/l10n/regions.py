46f03ebc43843feaba595f1c94aa2e79e06590ae
Jon Salz <jsalz@chromium.org>
2013-10-02 12:10:35 +0800

    Add main method to print all regions.
    BUG=chrome-os-partner:22420
    TEST=Run regions.py
    
    Change-Id: Ifc7d8ae41406c26f14a8bace0c19c47653d951a3
    Reviewed-on: https://chromium-review.googlesource.com/171550
    Tested-by: Jon Salz <jsalz@chromium.org>
    Reviewed-by: Bowgo Tsai <bowgotsai@chromium.org>
    Tested-by: Bowgo Tsai <bowgotsai@chromium.org>
    Commit-Queue: Jon Salz <jsalz@chromium.org>

--- py/l10n/regions.py
+++ py/l10n/regions.py
@@ -1,12 +1,24 @@
+#!/usr/bin/env python
 # Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+
+"""Authoritative source for factory region/locale configuration.
+
+Run this module to display all known regions (use --help to see options).
+"""
+
+
+import argparse
+import sys
 
 import factory_common  # pylint: disable=W0611
 from cros.factory import cros_locale
 from cros.factory.test.utils import Enum
 
+
 KeyboardMechanicalLayout = Enum(['ANSI', 'ISO', 'JIS'])
+
 
 class Region(object):
   """Comprehensive, standard locale configuration per country/region.
@@ -28,6 +40,9 @@ class Region(object):
     keyboard_mechanical_layout: The keyboard's mechanical layout (ANSI
       [US-like], ISO [UK-like], or JIS).
   """
+  FIELDS = ['region_code', 'keyboard', 'time_zone', 'language_code',
+            'keyboard_mechanical_layout']
+
   def __init__(self, region_code, keyboard, time_zone, language_code,
                keyboard_mechanical_layout):
     assert region_code != 'uk'
@@ -44,11 +59,13 @@ class Region(object):
     self.language_code = language_code
     self.keyboard_mechanical_layout = keyboard_mechanical_layout
 
+
 _KML = KeyboardMechanicalLayout
 _REGIONS_LIST = [
     Region('us', 'xkb:us::eng',     'America/Los_Angeles', 'en-US', _KML.ANSI),
     Region('gb', 'xkb:gb:extd:eng', 'Europe/London',       'en-GB', _KML.ISO),
 ]
+
 
 # Attempt to read regions from the overlay.  No worries if they're not
 # available.
@@ -59,3 +76,42 @@ except ImportError:
   pass
 
 REGIONS = dict((x.region_code, x) for x in _REGIONS_LIST)
+
+
+def main():
+  parser = argparse.ArgumentParser(description=(
+      'Display all known regions and their parameters. '
+      'To include any regions in the board overlay, run '
+      '"make overlay-$BOARD && overlay-$BOARD/py/l10n/regions.py" '
+      'from the platform/factory directory.'))
+  parser.add_argument('--format', choices=('human-readable', 'csv'),
+                      default='human-readable',
+                      help='Output format (default=%(default)s)')
+  args = parser.parse_args()
+
+  # List of list of lines to print.
+  lines = [Region.FIELDS]
+  for region in sorted(REGIONS.values(), key=lambda v: v.region_code):
+    lines.append([getattr(region, field) for field in Region.FIELDS])
+
+  if args.format == 'csv':
+    # Just print the lines in CSV format.
+    for l in lines:
+      print ','.join(l)
+  elif args.format == 'human-readable':
+    num_columns = len(lines[0])
+
+    # Calculate maximum length of each column.
+    max_lengths = []
+    for column_no in xrange(num_columns):
+      max_lengths.append(max(len(line[column_no]) for line in lines))
+
+    # Print each line, padding as necessary to the max column length.
+    for line in lines:
+      for column_no in xrange(num_columns):
+        sys.stdout.write(line[column_no].ljust(max_lengths[column_no] + 2))
+      sys.stdout.write('\n')
+
+
+if __name__ == '__main__':
+  main()
