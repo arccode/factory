9914a91b810cf717001cbaa1401f24d76b08ad3e
bowgotsai <bowgotsai@chromium.org>
2013-11-01 23:33:42 +0800

    Support writing into RO_VPD or RW_VPD for write_devcie_data_to_vpd
    For ethernet MAC address, we need to get it from shopfloor and write it
    into RO_VPD.
    
    BUG=chrome-os-partner:23618
    TEST=Manual
    
    Change-Id: I8b90d93169486eb87153b750aa9d8fa7778d1800
    Reviewed-on: https://chromium-review.googlesource.com/175421
    Commit-Queue: Bowgo Tsai <bowgotsai@chromium.org>
    Tested-by: Bowgo Tsai <bowgotsai@chromium.org>
    Reviewed-by: Heng-ruey Hsu <henryhsu@chromium.org>

--- py/test/pytests/write_device_data_to_vpd.py
+++ py/test/pytests/write_device_data_to_vpd.py
@@ -19,6 +19,9 @@ from cros.factory.test import test_ui
 from cros.factory.test import ui_templates
 from cros.factory.test.args import Arg
 
+_MSG_WRITING_VPD = lambda vpd_section: test_ui.MakeLabel(
+    'Writing device data to %s VPD...' % vpd_section.upper(),
+    '机器资料正在写入到 %s VPD...' % vpd_section.upper())
 
 class CallShopfloor(unittest.TestCase):
   ARGS = [
@@ -26,15 +29,20 @@ class CallShopfloor(unittest.TestCase):
          ('List of keys for device_data we want to write into RW_VPD.'
           'Each key is a tuple of (prefix, key) meaning that the pair '
           '(prefix + key, value) should be added into RW_VPD if there is '
-          'a pair (key, value) in device_data.'))
+          'a pair (key, value) in device_data.')),
+    Arg('vpd_section', str,
+        'It should be rw or ro which means RW_VPD or RO_VPD to write.',
+        default='rw', optional=True),
   ]
 
   def runTest(self):
+    if self.args.vpd_section not in ['ro', 'rw']:
+      self.fail('Invalid vpd_section:% r, should be %r or %r.' %
+          (self.args.vpd_section, 'ro', 'rw'))
+
     ui = test_ui.UI()
     template = ui_templates.OneSection(ui)
-    template.SetState(test_ui.MakeLabel(
-        'Writing device data to RW VPD...',
-        '机器资料正在写入到 RW VPD...'))
+    template.SetState(_MSG_WRITING_VPD(self.args.vpd_section))
 
     device_data = shopfloor.GetDeviceData()
     data_to_write = {}
@@ -45,5 +53,5 @@ class CallShopfloor(unittest.TestCase):
     if missing_keys:
       self.fail('Missing device data keys: %r' % sorted(missing_keys))
 
-    vpd.rw.Update(dict((k, str(v))
-                       for k, v in data_to_write.iteritems()))
+    getattr(vpd, self.args.vpd_section).Update(
+        dict((k, str(v)) for k, v in data_to_write.iteritems()))
