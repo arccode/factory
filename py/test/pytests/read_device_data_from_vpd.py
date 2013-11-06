fa89d292861315515a9e27d01ed4005636e59f8f
bowgotsai <bowgotsai@chromium.org>
2013-11-06 13:51:26 +0800

    Supporting read from RO_VPD or RW_VPD for read_devcie_data_to_vpd
    BUG=None
    TEST=Manual
    
    Change-Id: I91803584a9f89ab304f459b78cc0261a023406a8
    Reviewed-on: https://chromium-review.googlesource.com/175892
    Tested-by: Bowgo Tsai <bowgotsai@chromium.org>
    Reviewed-by: Heng-ruey Hsu <henryhsu@chromium.org>
    Commit-Queue: Bowgo Tsai <bowgotsai@chromium.org>

--- py/test/pytests/read_device_data_from_vpd.py
+++ py/test/pytests/read_device_data_from_vpd.py
@@ -21,6 +21,10 @@ from cros.factory.test import ui_templates
 from cros.factory.test.args import Arg
 
 
+_MSG_READING_VPD = lambda vpd_section: test_ui.MakeLabel(
+    'Reading device data from %s VPD...' % vpd_section.upper(),
+    '正在从 %s VPD 读机器资料...' % vpd_section.upper())
+
 class CallShopfloor(unittest.TestCase):
   ARGS = [
     Arg('device_data_keys', list,
@@ -29,7 +33,10 @@ class CallShopfloor(unittest.TestCase):
           'pair (key, value) should be added into device_data if there is '
           'a pair (prefix + key, value) in RW_VPD . If key is *, it means '
           'all keys with the prefix should be added.'),
-        default=[('factory.device_data.', '*')], optional=True)
+        default=[('factory.device_data.', '*')], optional=True),
+    Arg('vpd_section', str,
+        'It should be rw or ro which means RW_VPD or RO_VPD to read.',
+        default='rw', optional=True),
   ]
 
   @staticmethod
@@ -41,13 +48,15 @@ class CallShopfloor(unittest.TestCase):
       return vpd_key == prefix + key
 
   def runTest(self):
+    if self.args.vpd_section not in ['ro', 'rw']:
+      self.fail('Invalid vpd_section:% r, should be %r or %r.' %
+          (self.args.vpd_section, 'ro', 'rw'))
+
     ui = test_ui.UI()
     template = ui_templates.OneSection(ui)
-    template.SetState(test_ui.MakeLabel(
-        'Reading device data from RW VPD...',
-        '正在从 RW VPD 读机器资料...'))
+    template.SetState(_MSG_READING_VPD(self.args.vpd_section))
 
-    vpd_data = vpd.rw.GetAll()
+    vpd_data = getattr(vpd, self.args.vpd_section).GetAll()
     device_data = {}
     for matcher in self.args.device_data_keys:
       for key in vpd_data:
