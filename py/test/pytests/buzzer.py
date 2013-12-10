271c63c1a996bdcb1c9cecd59994827c25834bc2
henryhsu <henryhsu@chromium.org>
2013-12-10 15:02:57 +0800

    Add buzzer test case
    This is test list example
    OperatorTest(
        id='BUZZER',
        label_zh=u'蜂鸣器测试',
        pytest_name='buzzer',
        dargs={'init_commands': ['iotools io_write8 0x43 0xb6',
                                 'iotools io_write8 0x42 0x8e',
                                 'iotools io_write8 0x42 0x0a'],
               'start_command': 'iotools io_write8 0x61 0x3',
               'stop_command': 'iotools io_write8 0x61 0x0'})
    
    BUG=chrome-os-partner:24392
    TEST=manually test on DUT
    
    Change-Id: I70a65f5d425aad8c6500cb209a46a69cbd1d8651
    Reviewed-on: https://chromium-review.googlesource.com/179391
    Reviewed-by: Bowgo Tsai <bowgotsai@chromium.org>
    Tested-by: Heng-ruey Hsu <henryhsu@chromium.org>
    Commit-Queue: Bowgo Tsai <bowgotsai@chromium.org>

---
+++ py/test/pytests/buzzer.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import random
+import time
+import unittest
+
+from cros.factory.test import test_ui
+from cros.factory.test.args import Arg
+from cros.factory.test.ui_templates import OneSection
+from cros.factory.utils.process_utils import Spawn
+
+_MSG_BUZZER_INFO = test_ui.MakeLabel(
+    'How many beeps do you hear? <br>'
+    'Press space to start.',
+    zh='你听到几声哔声？<br>'
+    '压下空白键开始测试',
+    css_class='buzzer-test-info')
+
+_MSG_BUZZER_TEST = test_ui.MakeLabel(
+    'How many beeps do you hear? <br>'
+    'Press the number you hear to pass the test.<br>'
+    'Press \'r\' to play again.',
+    zh='你听到几声哔声？<br>'
+    '请按下数字代表你听到几声哔声<br>'
+    '按下 \'r\' 重播',
+    css_class='buzzer-test-info')
+
+_HTML_BUZZER = """
+<table style="width: 70%%; margin: auto;">
+  <tr>
+    <td align="center"><div id="buzzer_title"></div></td>
+  </tr>
+</table>
+"""
+
+_CSS_BUZZER = """
+  .buzzer-test-info { font-size: 2em; }
+"""
+
+_JS_BUZZER = """
+window.onkeydown = function(event) {
+  if (event.keyCode == 32 || event.keyCode == 82) { // space and 'R'
+    test.sendTestEvent("StartTest", '');
+  } else if (event.keyCode >= 48 && event.keyCode <= 57) { // 0 ~ 9
+    test.sendTestEvent("CheckResult", event.keyCode - 48)
+  }
+}
+"""
+
+class AudioLoopTest(unittest.TestCase):
+  ARGS = [
+    # Common arguments
+    Arg('init_commands', list, 'Setup buzzer commands', []),
+    Arg('start_command', str, 'Start beep command', None),
+    Arg('stop_command', str, 'Stop beep command', None),
+    Arg('beep_duration', float, 'How long for one beep', 0.3),
+    Arg('beep_interval', float, 'How long between beeps', 0.5),
+  ]
+
+  def setUp(self):
+    self._pass_digit = random.randint(1, 5)
+    self.ui = test_ui.UI()
+    self.template = OneSection(self.ui)
+    self.ui.AppendCSS(_CSS_BUZZER)
+    self.template.SetState(_HTML_BUZZER)
+    self.ui.RunJS(_JS_BUZZER)
+    self.ui.SetHTML(_MSG_BUZZER_INFO, id='buzzer_title')
+    self.InitialBuzzer(self.args.init_commands)
+    self.ui.AddEventHandler('StartTest', self.StartTest)
+    self.ui.AddEventHandler('CheckResult', self.CheckResult)
+
+  def InitialBuzzer(self, commands):
+    for command in commands:
+      Spawn(command.split(' '), check_call=True)
+
+  def BeepOne(self, start, stop):
+    Spawn(start.split(' '), check_call=True)
+    time.sleep(self.args.beep_duration)
+    Spawn(stop.split(' '), check_call=True)
+
+  def StartTest(self, event): # pylint: disable=W0613
+    self.ui.SetHTML(_MSG_BUZZER_TEST, id='buzzer_title')
+    for i in xrange(self._pass_digit): # pylint: disable=W0612
+      self.BeepOne(self.args.start_command, self.args.stop_command)
+      time.sleep(self.args.beep_interval)
+
+  def CheckResult(self, event):
+    if event.data != self._pass_digit:
+      self.ui.Fail('Wrong number to press.')
+    else:
+      self.ui.Pass()
+
+  def runTest(self):
+    self.ui.Run()
