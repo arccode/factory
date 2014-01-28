484879ce6064b23e4e0c0a36eef67a8d01ba063a
Jon Salz <jsalz@chromium.org>
2014-01-28 14:09:18 +0800

    Doc tweaks: shorten title and add optional title suffix.
    BUG=None
    TEST=Built manually
    
    Change-Id: Ib84a320d22abcf025010a6bf538b923c55d2d118
    Reviewed-on: https://chromium-review.googlesource.com/184070
    Reviewed-by: Jon Salz <jsalz@chromium.org>
    Tested-by: Jon Salz <jsalz@chromium.org>
    Commit-Queue: Jon Salz <jsalz@chromium.org>

--- doc/conf.py
+++ doc/conf.py
@@ -42,7 +42,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Chromium OS Factory'
+project = u'CrOS Factory'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -104,7 +104,8 @@ html_theme = 'default'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = ('%s %s documentation %s' % (
+        project, version, os.environ.get('DOC_TITLE_SUFFIX', ''))).strip()
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
@@ -186,8 +187,8 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'factory.tex', u'Chromium OS Factory Documentation',
-   u'Chromium OS Factory', 'manual'),
+  ('index', 'factory.tex', u'CrOS Factory Documentation',
+   u'CrOS Factory', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -216,8 +217,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'factory', u'Chromium OS Factory Documentation',
-     [u'Chromium OS Factory'], 1)
+    ('index', 'factory', u'CrOS Factory Documentation',
+     [u'CrOS Factory'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -230,8 +231,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'factory', u'Chromium OS Factory Documentation',
-   u'Chromium OS Factory', 'factory', 'One line description of project.',
+  ('index', 'factory', u'CrOS Factory Documentation',
+   u'CrOS Factory', 'factory', 'One line description of project.',
    'Miscellaneous'),
 ]
 
