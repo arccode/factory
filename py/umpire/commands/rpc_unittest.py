d51386d739d595d497481206862306d1b7c4f7d9
Vic (Chun-Ju) Yang <victoryang@chromium.org>
2014-04-28 12:04:27 +0800

    Set temp directory for Umpire RPC unittest
    We use 'trial' for unittest, and it leaves temp files in the current
    working directory. Let's set temp directory to /tmp.
    
    BUG=None
    TEST=Run the test and check current working directory.
    
    Change-Id: I693ee05b676de3e2790ffdd8a5eaba3c256aea06
    Signed-off-by: Vic (Chun-Ju) Yang <victoryang@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/197193
    Reviewed-by: Ricky Liang <jcliang@chromium.org>
    Reviewed-by: Rong Chang <rongchang@chromium.org>

--- py/umpire/commands/rpc_unittest.py
+++ py/umpire/commands/rpc_unittest.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env trial
+#!/usr/bin/trial --temp-directory=/tmp/_trial_temp/
 #
 # Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
