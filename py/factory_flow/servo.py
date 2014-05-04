8a77301f8fd5261ddabbdc33cfda9b530c2db291
Ricky Liang <jcliang@chromium.org>
2014-05-04 17:15:42 +0800

    factory_flow: Auto-install required packages for servod.
    BUG=chrome-os-partner:26795
    TEST=manually on my host
    
    Change-Id: I54d509eff53822b8dd4a63da1c64d5107744feea
    Reviewed-on: https://chromium-review.googlesource.com/198142
    Reviewed-by: Ricky Liang <jcliang@chromium.org>
    Commit-Queue: Ricky Liang <jcliang@chromium.org>
    Tested-by: Ricky Liang <jcliang@chromium.org>

--- py/factory_flow/servo.py
+++ py/factory_flow/servo.py
@@ -4,7 +4,10 @@
 
 """A wrapper of the servo module from autotest repo."""
 
+import logging
+import os
 import re
+import subprocess
 
 import factory_common   # pylint: disable=W0611
 from cros.factory.test import utils
@@ -19,6 +22,8 @@ class Servo(object):
   """A wrapper class for servo.Servo in autotest repo."""
 
   def __init__(self, board, host, port=9999, serial=None):
+    self._InstallRequiredPackages()
+
     # Do autotest imports here to stop it from messing around with logging
     # settings.
     # pylint: disable=W0612, F0401
@@ -40,6 +45,33 @@ class Servo(object):
 
     self._servo = servo.Servo(hosts.ServoHost(servo_host=host, servo_port=port),
                               serial)
+
+  def _InstallRequiredPackages(self):
+    # Check if flashrom is installed with ft2232_spi support.
+    flashrom_equery = process_utils.CheckOutput(
+        ['equery', '--no-color', '--no-pipe', 'uses', 'flashrom'])
+    use_ft2232_spi = re.search(
+        r'^\s*([+-])\s*([+-])\s*ft2232_spi', flashrom_equery, re.MULTILINE)
+    if use_ft2232_spi.group(2) != '+':
+      logging.info(
+          'Your flashrom does not have ft2232_spi support. Re-building '
+          'flashrom package with USE=ft223_spi...')
+      process_utils.Spawn(
+          ['sudo', '-E', 'emerge', 'flashrom'], env=dict(USE='ft2232_spi'),
+          log=True, check_call=True, log_stderr_on_error=True,
+          ignore_stdout=True)
+
+    # Check if flash_ec is accessible to root.
+    try:
+      process_utils.Spawn(['which', 'flash_ec'], sudo=True, check_call=True)
+    except subprocess.CalledProcessError:
+      logging.info('Cannot locate flash_ec through sudo. Creating symbolic '
+                   'link of flash_ec in /usr/local/bin...')
+      process_utils.Spawn(
+          ['ln', '-s', os.path.join(
+              os.environ['CROS_WORKON_SRCROOT'], 'src', 'platform', 'ec',
+              'util', 'flash_ec'),
+           '/usr/local/bin'], sudo=True, log=True, check_call=True)
 
   def __getattr__(self, name):
     """Delegates getter of all unknown attributes to servo.Servo object."""
