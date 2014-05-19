b65331b6bf90aee42555c56e88b9b04aa8172758
Rong Chang <rongchang@chromium.org>
2014-05-19 15:00:23 +0800

    [umpire] Add resource map web application
    Devices under test query resource from Umpire server through HTTP GET
    access. This change adds web application that handles HTTP request and
    maps X-Umpire-DUT header to resource key-value pairs.
    
    BUG=chrome-os-partner:28282
    TEST=None
    
    Change-Id: I0100625ec3296ca1fc3eaa6cb744f5b620f55e56
    Reviewed-on: https://chromium-review.googlesource.com/200397
    Reviewed-by: Dean Liao <deanliao@chromium.org>
    Commit-Queue: Rong Chang <rongchang@chromium.org>
    Tested-by: Rong Chang <rongchang@chromium.org>

---
+++ py/umpire/webapp_resourcemap.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# pylint: disable=E1101
+
+"""Umpire resource map web application.
+
+The class handles http://umpire_address:umpire_port/resourcemap' HTTP GET.
+"""
+
+import logging
+
+import factory_common  # pylint: disable=W0611
+from cros.factory.umpire.bundle_selector import ParseDUTHeader, GetResourceMap
+from cros.factory.umpire.web.wsgi import WSGISession
+
+
+_PATH_INFO = '/resourcemap'
+
+
+class ResourceMapApp(object):
+
+  """Web application callable class.
+
+  Args:
+    env: UmpireEnv object.
+  """
+
+  def __init__(self, env):
+    self.env = env
+
+  def __call__(self, environ, start_response):
+    """Gets resource map from DUT info and return text/plain result."""
+    session = WSGISession(environ, start_response)
+    logging.debug('resourcemap app: %s', str(session))
+    if session.REQUEST_METHOD == 'GET':
+      dut_info = ParseDUTHeader(session.HTTP_X_UMPIRE_DUT)
+      return session.Response(GetResourceMap(dut_info, self.env))
+    return session.BadRequest400()
+
+  def GetPathInfo(self):
+    return _PATH_INFO
