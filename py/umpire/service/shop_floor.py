6631b804c0d6a2281fb6ebb70fe36d633d4e4cb9
Rong Chang <rongchang@chromium.org>
2014-05-19 15:40:16 +0800

    [umpire] Add shop floor service
    Shop floor service prepares FastCGI processes. With given process
    configuration, the FastCGI process listens to specified port and checks
    request token.
    
    BUG=chrome-os-partner:28282
    TEST=None
    
    Change-Id: Id440b8e4706fdfd37c95147103a647fbf947232e
    Reviewed-on: https://chromium-review.googlesource.com/201062
    Tested-by: Rong Chang <rongchang@chromium.org>
    Reviewed-by: Dean Liao <deanliao@chromium.org>
    Commit-Queue: Rong Chang <rongchang@chromium.org>

---
+++ py/umpire/service/shop_floor.py
@@ -0,0 +1,81 @@
+# Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+"""Shop floor service for launching shop floor XMLRPC in bundles."""
+
+
+import os
+
+import factory_common  # pylint: disable=W0611
+from cros.factory.umpire.service import umpire_service
+
+
+SHOP_FLOOR_FCGI = 'usr/local/factory/bin/shop_floor_launcher'
+PROCESS_NAME_PREFIX = 'shop_floor_'
+
+
+class ShopFloorService(umpire_service.UmpireService):
+
+  """Shop floor service.
+
+  Example:
+    shopfloor_service = GetServiceInstance('shop_floor')
+    procs = svc.CreateProcesses(umpire_config_attrdict, umpire_env)
+    shopfloor_service.Start(procs)
+  """
+
+  def __init__(self):
+    super(ShopFloorService, self).__init__()
+    self.properties['num_shopfloor_handlers'] = 0
+
+  def CreateProcesses(self, dummy_config, env):
+    """Creates list of shop floor processes via config.
+
+    Args:
+      dummy_config: Umpire config AttrDict object.
+      env: UmpireEnv object.
+
+    Returns:
+      A list of ServiceProcess.
+    """
+    active_bundles = env.config.GetActiveBundles()
+    processes = []
+    for bundle in active_bundles:
+      # Get toolkit path and shop floor handler.
+      toolkit_dir = env.GetBundleDeviceToolkit(bundle['id'])
+      handler = bundle['shop_floor']['handler']
+      if not (toolkit_dir and handler):
+        continue
+      # Prepare handler configuration.
+      handler_config = bundle['shop_floor'].get('handler_config', dict())
+      # Convert {'mount_point': '/path/to/dir'} to process parameters:
+      #   ['--mount_point', '/path/to/dir']
+      process_parameters = sum([['--%s' % key, value] for key, value in
+                                handler_config.iteritems()], list())
+      # Allocate port and token.
+      (fcgi_port, token) = env.shop_floor_manager.Allocate(bundle['id'])
+      # Set process configuration.
+      proc_config = {
+          'executable': os.path.join(toolkit_dir, SHOP_FLOOR_FCGI),
+          'name': PROCESS_NAME_PREFIX + bundle['id'],
+          'args': ['--module', handler] + process_parameters,
+          'path': '/tmp'}
+      proc = umpire_service.ServiceProcess(self)
+      proc.SetNonhashArgs(['--port', str(fcgi_port), '--token', token])
+      proc.SetConfig(proc_config)
+      # Adds release callbacks on error and stopped state.
+      def ReleaseResource(release_port):
+        env.shop_floor_manager.Release(release_port)
+
+      proc.AddStateCallback(
+          [umpire_service.State.ERROR, umpire_service.State.STOPPED],
+          ReleaseResource,
+          fcgi_port)
+      processes.append(proc)
+    self.properties['num_shopfloor_handlers'] = len(processes)
+    return processes
+
+# Create a shop floor service instance
+_service_instance = ShopFloorService()
---
+++ py/umpire/service/shop_floor_unittest.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+import logging
+import os
+import pprint
+import shutil
+import tempfile
+import unittest
+
+import factory_common  # pylint: disable=W0611
+from cros.factory.umpire.config import UmpireConfig
+from cros.factory.umpire.service import shop_floor
+from cros.factory.umpire.umpire_env import UmpireEnv
+
+
+class MockShopFloorManager(object):
+  def Allocate(self, *dummy_args, **dummy_kwargs):
+    return (9876, 'dummy_token')
+
+
+class TestShopFloorService(unittest.TestCase):
+  def setUp(self):
+    self.env = UmpireEnv()
+    self.temp_dir = tempfile.mkdtemp()
+    self.env.base_dir = self.temp_dir
+    os.makedirs(self.env.resources_dir)
+    self.env.shop_floor_manager = MockShopFloorManager()
+
+  def tearDown(self):
+    if os.path.isdir(self.temp_dir):
+      shutil.rmtree(self.temp_dir)
+
+  def testGenerateProcesses(self):
+    umpire_config = {
+        # 4 active rulesets, 3 active.
+        'rulesets': [
+            {'bundle_id': 'bundle_1', 'note': 'note_1', 'active': True},
+            {'bundle_id': 'bundle_2', 'note': 'note_2', 'active': True},
+            {'bundle_id': 'bundle_3', 'note': 'note_3', 'active': False},
+            {'bundle_id': 'bundle_d', 'note': 'note_d', 'active': True}],
+        # 4 bundles.
+        'bundles': [
+            {'id': 'bundle_1', 'note': 'note_b_1',
+             'shop_floor': {'handler': 'handler_1'},
+             'resources': {'device_factory_toolkit':
+                           'install_factory_toolkit.run#ftk_1#00000001'}},
+            {'id': 'bundle_2', 'note': 'note_b_2',
+             'shop_floor': {'handler': 'handler_2'},
+             'resources': {'device_factory_toolkit':
+                           'install_factory_toolkit.run#ftk_2#00000002'}},
+            {'id': 'bundle_3', 'note': 'note_b_3',
+             'shop_floor': {'handler': 'handler_3'},
+             'resources': {'device_factory_toolkit':
+                           'install_factory_toolkit.run#ftk_3#00000003'}},
+            {'id': 'bundle_d', 'note': 'note_b_d',
+             'shop_floor': {'handler': 'handler_d'},
+             'resources': {'device_factory_toolkit':
+                           'install_factory_toolkit.run#ftk_4#00000004'}}]}
+    # Activate configuration.
+    self.env.config = UmpireConfig(umpire_config, validate=False)
+    os.makedirs(self.env.device_toolkits_dir)
+    map(lambda h: os.makedirs(os.path.join(self.env.device_toolkits_dir, h)),
+        ['00000001', '00000002', '00000003', '00000004'])
+    # Create processes.
+    service = shop_floor.ShopFloorService()
+    processes = service.CreateProcesses(None, self.env)
+    # Check process count.
+    num_actives = sum([b['active'] for b in umpire_config['rulesets']])
+    logging.debug('process config:\n%s',
+                  pprint.pformat(sum([[p.config, p.nonhash_args]
+                                      for p in processes], list()),
+                                 indent=2))
+    self.assertEqual(num_actives, len(processes))
+
+
+if __name__ == '__main__':
+  if os.environ.get('LOG_LEVEL'):
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(levelname)5s %(message)s')
+  else:
+    logging.disable(logging.CRITICAL)
+
+  unittest.main()
