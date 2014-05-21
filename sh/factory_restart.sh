292e88719a0021ea7a0734e398d3b1c5750cb73b
Ricky Liang <jcliang@chromium.org>
2014-05-21 10:47:38 +0800

    Kill stressapptest process in factory_restart.
    BUG=none
    TEST=manually on my DUT
    
    Change-Id: Ia4fe85fc105df44751c259cac3a623896f9c62b9
    Reviewed-on: https://chromium-review.googlesource.com/200694
    Reviewed-by: Ricky Liang <jcliang@chromium.org>
    Commit-Queue: Ricky Liang <jcliang@chromium.org>
    Tested-by: Ricky Liang <jcliang@chromium.org>

--- sh/factory_restart.sh
+++ sh/factory_restart.sh
@@ -95,7 +95,7 @@ while [ $# -gt 0 ]; do
   esac
 done
 
-KILL_REGEXP='^(python|/usr/bin/python|.*pytest)'
+KILL_REGEXP='^(python|/usr/bin/python|.*pytest|.*stressapptest)'
 
 echo -n "Stopping factory test programs... "
 pkill -f "$KILL_REGEXP" 2>/dev/null
