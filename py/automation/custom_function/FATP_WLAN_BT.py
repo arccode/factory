29d36069e7d44b406963396a21605a321415db46
Ricky Liang <jcliang@chromium.org>
2014-05-21 17:18:33 +0800

    Remove obsolete modules.
    The automation module is deprecated by factory test automator and
    factory_flow tool.
    
    BUG=chrome-os-partner:26795
    TEST=make test
    TEST=manually on my DUT
    
    Change-Id: If4323580bff4801caaa8486232050e2353f7355e
    Reviewed-on: https://chromium-review.googlesource.com/200741
    Tested-by: Ricky Liang <jcliang@chromium.org>
    Reviewed-by: Vic Yang <victoryang@chromium.org>
    Commit-Queue: Ricky Liang <jcliang@chromium.org>

--- py/automation/custom_function/FATP_WLAN_BT.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-def Main(factory_automation, test_path):  # pylint: disable=W0613
-  factory_automation.driver.switch_to_frame(0)
-  factory_automation.driver.find_element_by_id('pass').click()
