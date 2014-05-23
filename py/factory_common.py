080eb93df4620ff997bf36b27f98d4ca8cc58c59
Hung-Te Lin <hungte@chromium.org>
2014-05-23 15:00:48 +0800

    factory_common: Prevent duplicated folders in sys.path.
    The module wrapper, factory_common, should not insert same factory root
    directory into Python path if it's already available.
    
    BUG=none
    TEST=Run goofy successfully.
    
    Change-Id: Iaa33663fefc0c3d002649b84caace711ee9f68fe
    Reviewed-on: https://chromium-review.googlesource.com/201205
    Tested-by: Hung-Te Lin <hungte@chromium.org>
    Reviewed-by: Dean Liao <deanliao@chromium.org>
    Commit-Queue: Hung-Te Lin <hungte@chromium.org>

--- py/factory_common.py
+++ py/factory_common.py
@@ -7,8 +7,14 @@
 """A helper module to set up sys.path so that cros.factory.* can be located."""
 
 import os, sys
-sys.path.append(
-    os.path.join(
-        os.path.dirname(os.path.dirname(
-                os.path.realpath(__file__.replace('.pyc', '.py')))),
-        'py_pkg'))
+
+# factory_common.py itself is usually a symlink that refers to top level of
+# factory code. To find that, we need to get the source name (the compiled
+# binary *.pyc is usually not a symlink), derive the path to "py_pkg" folder,
+# and then append into Python path (sys.path) if it's not available yet.
+
+pk_pkg = os.path.join(os.path.dirname(os.path.dirname(
+    os.path.realpath(__file__.replace('.pyc', '.py')))), 'py_pkg')
+
+if pk_pkg not in sys.path:
+  sys.path.append(pk_pkg)
