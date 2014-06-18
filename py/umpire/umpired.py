3404a222e1fd8354106c2e1ad36064e50f8c5585
Cheng-Yi Chiang <cychiang@chromium.org>
2014-06-18 23:38:02 +0800

    [umpire] Umpired: change the default yaml file of umpired
    Use umpired for real. It will look for umpired_template.yaml from private
    overlay as the initial config file.
    
    BUG=chrome-os-partner:29745
    TEST=run umpired
    
    Change-Id: I3c496c33a1bd60d9a06baa0e1fcb82feced08ba3
    Reviewed-on: https://chromium-review.googlesource.com/204530
    Reviewed-by: Cheng-Yi Chiang <cychiang@chromium.org>
    Commit-Queue: Cheng-Yi Chiang <cychiang@chromium.org>
    Tested-by: Cheng-Yi Chiang <cychiang@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/205342
    Reviewed-by: Dean Liao <deanliao@chromium.org>

--- py/umpire/umpired.py
+++ py/umpire/umpired.py
@@ -3,15 +3,14 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""Sample umpired implementation.
+"""Umpired implementation.
 
-This is a minimalist umpired implementation for development testing purpose.
+This is a minimalist umpired implementation.
 """
 
 import logging
 import optparse
 import os
-import sys
 
 import factory_common  # pylint: disable=W0611
 from cros.factory.umpire import rpc_dut
@@ -22,12 +21,13 @@ from cros.factory.umpire.umpire_env import UmpireEnv
 from cros.factory.umpire.webapp_resourcemap import ResourceMapApp
 
 
-TEST_YAML = os.path.join(
-    os.path.dirname(sys.modules[__name__].__file__),
-    'testdata', 'umpired_test.yaml')
+# This template file is from private overlay.
+TEMPLATE_YAML = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    'umpired_template.yaml')
 
 
-def StartServer(testmode=False, config_file=TEST_YAML):
+def StartServer(testmode=False, config_file=TEMPLATE_YAML):
   # Instanciate environment and load default configuration file
   env = UmpireEnv()
   if testmode:
@@ -73,7 +73,7 @@ def main():
       '-t', '--test', dest='testmode', action='store_true', default=False,
       help='test run testdata/umpired_test.yaml')
   parser.add_option(
-      '-y', '--yaml', dest='custom_path', default=TEST_YAML,
+      '-y', '--yaml', dest='custom_path', default=TEMPLATE_YAML,
       help='use another test yaml config file')
   (options, unused_args) = parser.parse_args()
   StartServer(testmode=options.testmode, config_file=options.custom_path)
