4162bf9adc2f2e7c0a653367d387e5d9d2adc7d7
Mao Huang <littlecvr@chromium.org>
2014-07-25 17:53:43 +0800

    Add lte_verify_config test
    This is a simple test that verifies the LTE module config. Given a list
    of commands, the test issues the commands to the module and checks their
    responses, and fails if the responses are not as expected.
    
    BUG=chrome-os-partner:27891
    TEST=Manually tested
    
    Change-Id: Ib609a407aae2080c82d3116d0672e54ed1f97ad4
    Reviewed-on: https://chromium-review.googlesource.com/209728
    Tested-by: Mao Huang <littlecvr@chromium.org>
    Reviewed-by: Bowgo Tsai <bowgotsai@chromium.org>
    Commit-Queue: Mao Huang <littlecvr@chromium.org>

---
+++ py/test/pytests/lte_verify_config.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2014 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Verifies the LTE module config.
+
+Usage example::
+
+  FactoryTest(
+      id='LTEVerifyConfig',
+      label_zh=u'确认 LTE 参数',
+      pytest_name='lte_verify_config',
+      dargs={
+          'modem_path': 'ttyACM0',
+          'config_to_check': [
+              # Single line response example.
+              ('AT_COMMAND_1', 'RESPONSE_1'),
+              # Multi-line response example.
+              ('AT_COMMAND_2', ['RESPONSE_2_LINE_1', RESPONSE_2_LINE_2]),
+"""
+
+import unittest
+
+import factory_common  # pylint: disable=W0611
+
+from cros.factory.rf import modem
+from cros.factory.test.args import Arg
+
+
+class LTEVerifyConfig(unittest.TestCase):
+  ARGS = [
+    Arg('modem_path', str, 'The path of the serial port.'),
+    Arg('config_to_check', list, 'A list of tuples. For each tuple, the first '
+        'element is the command and the second element is the expected '
+        'response. Expected response can be a single string indicating only '
+        'one line response or a list of strings indicating multiline response.')
+  ]
+
+  def setUp(self):
+    self.modem = modem.Modem(self.args.modem_path)
+
+  def runTest(self):
+    for cmd, expected_response in self.args.config_to_check:
+      if isinstance(expected_response, str):
+        expected_response = [expected_response, 'OK']
+      else:
+        expected_response += ['OK']
+      response = self.modem.SendCommandWithCheck(cmd)
+      if response != expected_response:
+        raise ValueError(
+            'Should get %s but got %s' % (expected_response, response))
