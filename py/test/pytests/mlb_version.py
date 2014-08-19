f8c18e22f14197dd9185c5ab807387a311027a38
Ricky Liang <jcliang@chromium.org>
2014-08-19 12:39:01 +0800

    Add mlb_version factory test.
    BUG=chrome-os-partner:30014
    TEST=Unit tests.
    TEST=Manually run the factory test on my Samus Proto2 DUT.
    
    Change-Id: I8e63b883d6f1831eaf1a331040bedb94d736f595
    Reviewed-on: https://chromium-review.googlesource.com/213042
    Tested-by: Ricky Liang <jcliang@chromium.org>
    Reviewed-by: Peter Ammon <pca@google.com>
    Commit-Queue: Ricky Liang <jcliang@chromium.org>

---
+++ py/test/pytests/mlb_version.py
@@ -0,0 +1,44 @@
+# Copyright 2014 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""A factory test to verify MLB board version."""
+
+import unittest
+
+import factory_common   # pylint: disable=W0611
+from cros.factory import system
+from cros.factory.test import args
+from cros.factory.test import phase
+
+
+class MLBVersionTest(unittest.TestCase):
+  """A factory test to verify MLB board version."""
+
+  ARGS = [
+    args.Arg(
+        'expected_version', (str, unicode),
+        ('The expected version string. If not given, try to match the board '
+         'version with current build phase'),
+        optional=True),
+  ]
+
+  def runTest(self):
+    board_version = system.GetBoard().GetBoardVersion()
+    if self.args.expected_version:
+      self.assertEquals(
+          self.args.expected_version, board_version,
+          ('Board version mismatch. Expect to see board version %s, but the '
+           'actual board version is %s') %
+          (self.args.expected_version, board_version))
+    else:
+      current_phase = phase.GetPhase()
+      if current_phase == phase.PVT_DOGFOOD:
+        expected_version_prefix = 'PVT'
+      else:
+        expected_version_prefix = str(current_phase)
+      self.assertTrue(
+          board_version.upper().startswith(expected_version_prefix),
+          ('In phase %s, expect board version to start with %s, '
+           'but got board version %s') %
+          (current_phase, expected_version_prefix, board_version))
---
+++ py/test/pytests/mlb_version_unittest.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python -Bu
+# Copyright 2014 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Unit tests for mlb_version factory test."""
+
+import mox
+import unittest
+
+import factory_common   # pylint: disable=W0611
+from cros.factory.test.pytests import mlb_version
+from cros.factory import system
+from cros.factory.test import phase
+
+
+class MockBoard(object):
+  """A mock board class."""
+  def __init__(self, version):
+    self.version = version
+
+  def GetBoardVersion(self):
+    return self.version
+
+
+class MLBVersionTestUnittest(unittest.TestCase):
+  """Unit tests for mlb_version factory test."""
+  def setUp(self):
+    self.mox = mox.Mox()
+    self.mox.StubOutWithMock(system, 'GetBoard')
+    self.mox.StubOutWithMock(phase, 'GetPhase')
+
+    class FakeArgs(object):
+      """A fake factory test args object."""
+      expected_version = None
+
+    self.test = mlb_version.MLBVersionTest()
+    self.test.args = FakeArgs()
+
+  def tearDown(self):
+    self.mox.UnsetStubs()
+
+  def testNoArgs(self):
+    proto2b_board = MockBoard('Proto2B')
+    pvt3_board = MockBoard('PVT3')
+    # Proto2b board in phase PROTO.
+    system.GetBoard().AndReturn(proto2b_board)
+    phase.GetPhase().AndReturn(phase.PROTO)
+    # Proto2b board in phase EVT.
+    system.GetBoard().AndReturn(proto2b_board)
+    phase.GetPhase().AndReturn(phase.EVT)
+    # Proto2b board in phase PVT_DOGFOOD
+    system.GetBoard().AndReturn(proto2b_board)
+    phase.GetPhase().AndReturn(phase.PVT_DOGFOOD)
+    # PVT3 board in phase PVT_DOGFOOD.
+    system.GetBoard().AndReturn(pvt3_board)
+    phase.GetPhase().AndReturn(phase.PVT_DOGFOOD)
+    # PVT3 board in phase PVT.
+    system.GetBoard().AndReturn(pvt3_board)
+    phase.GetPhase().AndReturn(phase.PVT)
+
+    self.mox.ReplayAll()
+
+    # Proto2b board in phase PROTO. This should pass.
+    self.test.runTest()
+    # Proto2b board in phase EVT. This should fail due to mismatch version
+    # prefix.
+    self.assertRaisesRegexp(
+        AssertionError,
+        (r'In phase EVT, expect board version to start with EVT, but got board '
+         r'version Proto2B'),
+        self.test.runTest)
+    # Proto2b board in phase PVT_DOGFOOD. This should fail due to mismatch
+    # version prefix.
+    self.assertRaisesRegexp(
+        AssertionError,
+        (r'In phase PVT_DOGFOOD, expect board version to start with PVT, but '
+         r'got board version Proto2B'),
+        self.test.runTest)
+    # PVT3 board in phase PVT_DOGFOOD. This should pass.
+    self.test.runTest()
+    # PVT3 board in phase PVT. This should pass.
+    self.test.runTest()
+
+    self.mox.VerifyAll()
+
+  def testWithArgs(self):
+    proto2b_board = MockBoard('Proto2B')
+    pvt3_board = MockBoard('PVT3')
+    # Expect to see board version 'Proto2B'.
+    self.test.args.expected_version = 'Proto2B'
+    # Test on Proto2b board.
+    system.GetBoard().AndReturn(proto2b_board)
+    # Test on PVT3 board.
+    system.GetBoard().AndReturn(pvt3_board)
+
+    self.mox.ReplayAll()
+
+    # Test on Proto2b board. This should pass.
+    self.test.runTest()
+    # Test on PVT3 board. This should fail with mismatched board version.
+    self.assertRaisesRegexp(
+        AssertionError,
+        (r'Board version mismatch\. Expect to see board version Proto2B, but '
+         r'the actual board version is PVT3'),
+        self.test.runTest)
+
+    self.mox.VerifyAll()
+
+
+if __name__ == '__main__':
+  unittest.main()
