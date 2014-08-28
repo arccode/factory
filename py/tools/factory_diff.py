8b588c2adad892ea1a582f6c4ddbfd0ef91d5d92
Ricky Liang <jcliang@chromium.org>
2014-08-28 14:01:57 +0800

    factory_diff: Handle new kernel repo structure and branch naming.
    The kernel repo structure has changed and so does the branch naming.
    Update factory_diff to handling the changes accordingly.
    
    BUG=None
    TEST=`factory_diff -b samus -s`
    TEST=`factory_diff -b panther_goofy --branch factory-samus-6207.B -s`
    
    Change-Id: Ie6fab2c94a9bbebe1f9ed100899e2e6b07fe2c00
    Reviewed-on: https://chromium-review.googlesource.com/214672
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Tested-by: Ricky Liang <jcliang@chromium.org>
    Commit-Queue: Ricky Liang <jcliang@chromium.org>

--- py/tools/factory_diff.py
+++ py/tools/factory_diff.py
@@ -28,6 +28,7 @@ import sys
 
 import factory_common  # pylint: disable=W0611
 from cros.factory.utils.process_utils import CheckOutput, Spawn
+from cros.factory.tools import build_board
 
 DiffEntry = namedtuple('DiffEntry',
                        ['left_right', 'hash', 'author', 'subject'])
@@ -49,13 +50,16 @@ OTHER_REPO_LIST = ['src/platform/touch_updater', 'src/platform/mosys',
                    'src/platform/factory_installer', 'src/platform/ec',
                    'src/third_party/autotest/files',
                    'src/third_party/xf86-video-armsoc', 'src/third_party/adhd',
-                   'src/third_party/chromiumos-overlay',
-                   'src/third_party/kernel/files']
+                   'src/third_party/chromiumos-overlay']
+
+# The kernel repo has several different versions. The actual kernel version will
+# be determined during runtime based on the board name provided.
+KERNEL_REPO_PATTERN = 'src/third_party/kernel/%(version)s'
 
 # m/master does not point to cros/master in the repo in this list. We can only
 # know where it points to if the tree was inited without -b and has
 # m/master branch.
-DIFFERENT_MASTER_REPO_LIST = ['src/third_party/kernel/files']
+DIFFERENT_MASTER_REPO_LIST = glob.glob('src/third_party/kernel/*')
 
 # Repo in this list like 'src/third_party/chromiumos-overlay' does not contain
 # other branches by default. 'git fetch cros' can fetch all the branches.
@@ -114,6 +118,14 @@ def GetPrivateOverlay(board):
                         '\n'.join(path))
       return path[0]
   return None
+
+
+def GetBoardKernelVersion(board):
+  """Gets the kernel version used by the given board."""
+  KERNEL_SRC_USE_RE = re.compile(r'\+kernel-(\d+_\d+)', re.MULTILINE)
+  return KERNEL_SRC_USE_RE.search(
+      CheckOutput(['equery-%s' % board, 'uses', 'linux-sources'])
+      ).group(1).replace('_', '.')
 
 
 def GetDiffList(diff):
@@ -212,6 +224,12 @@ def DiffRepo(repo_path, args, init_with_master):
   master_branch_prefix = 'm' if init_with_master else prefix
   master_branch = master_branch_prefix + '/master'
   compare_branch = prefix + '/' + args.branch
+  if (repo_path == (KERNEL_REPO_PATTERN % dict(
+      version=GetBoardKernelVersion(args.board.full_name)))):
+    # The kernel repo has a slightly different branch naming. There is a
+    # '-chromeos-<kernel_version>' suffix after the branch name.
+    compare_branch += '-chromeos-%s' % GetBoardKernelVersion(
+        args.board.full_name)
 
   # Force fetching for if repo is in FORCE_FETCH_REPO_LIST.
   # When fetching for the branch, prefix can only be 'cros' or 'cros-internal'.
@@ -279,9 +297,11 @@ def main():
   args = parser.parse_args()
 
   args.board = args.board or GetDefaultBoardOrNone()
+  if args.board:
+    args.board = build_board.BuildBoard(args.board)
 
   if not args.branch:
-    args.branch = GetBranch(args.board)
+    args.branch = GetBranch(args.board.short_name)
   if not args.branch:
     logging.error('Cannot determine factory branch name. '
                   'Specify --branch to continue.')
@@ -290,9 +310,12 @@ def main():
   repo_list = FACTORY_REPO_LIST
   if args.show_other_repos:
     repo_list += OTHER_REPO_LIST
+    # Add the active kernel repo of the give board into list.
+    repo_list += [KERNEL_REPO_PATTERN %
+                  dict(version=GetBoardKernelVersion(args.board.full_name))]
 
   if args.board:
-    repo_list.append(GetPrivateOverlay(args.board))
+    repo_list.append(GetPrivateOverlay(args.board.short_name))
 
   if RefExist('src/platform/factory', 'm/master'):
     init_with_master = True
