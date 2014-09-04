71006918f47b98c065d6cb59f2de44c7727c5859
Joseph Hwang <josephsih@chromium.org>
2014-09-04 15:24:13 +0800

    touchscreen_calibration: create touchscreen_calibration directory
    As an effort of refactoring the touchscreen calibration test to be
    more flexible, there will be more files to be added later. It is
    about time for this test to have its own directory.
    
    BUG=chrome-os-partner:31264
    TEST=manually verified the code with the old test fixture.
    
    Change-Id: If1d2774731a3d517a2ce8a64421d789f6ca439fd
    Reviewed-on: https://chromium-review.googlesource.com/216264
    Reviewed-by: Chung-yih Wang <cywang@chromium.org>
    Tested-by: Shyh-In Hwang <josephsih@chromium.org>
    Commit-Queue: Shyh-In Hwang <josephsih@chromium.org>

---
+++ py/test/pytests/touchscreen_calibration/factory_common.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""A helper module to set up sys.path so that cros.factory.* can be located."""
+
+import os, sys
+
+# factory_common.py itself is usually a symlink that refers to top level of
+# factory code. To find that, we need to get the source name (the compiled
+# binary *.pyc is usually not a symlink), derive the path to "py_pkg" folder,
+# and then append into Python path (sys.path) if it's not available yet.
+
+pk_pkg = os.path.join(os.path.dirname(os.path.dirname(
+    os.path.realpath(__file__.replace('.pyc', '.py')))), 'py_pkg')
+
+if pk_pkg not in sys.path:
+  sys.path.append(pk_pkg)
