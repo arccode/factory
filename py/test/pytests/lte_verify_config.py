7d9692782bb728582af44e2e28de3604490a95dd
jiazi Yang <Tomato_Yang@asus.com>
2014-09-11 10:34:10 +0800

    CHERRY-PICK: Quawks: Modify LTEVerifyConfig item
    Add EnterFactoryMode and ExitFactoryMode before and after the test.
    
    BUG=chrome-os-partner:31813
    CQ-DEPEND=CL:*175811
    TEST=Manual test passed on quawks device
    
    Original-Change-Id: I5270fa0a0c6dc1a6666b54220786a1aedd50331b
    Reviewed-on: https://chromium-review.googlesource.com/217473
    Tested-by: Jiazi Yang <Tomato_Yang@asus.com>
    Reviewed-by: Bowgo Tsai <bowgotsai@chromium.org>
    Commit-Queue: Jiazi Yang <Tomato_Yang@asus.com>
    (cherry picked from commit 774e862d8611457c2b3fc43f5d99e1834e96f203)
    
    Change-Id: I382ff3e6d6d5450ac3f9350cfaed43ee4e2106f8
    Reviewed-on: https://chromium-review.googlesource.com/219290
    Tested-by: Bowgo Tsai <bowgotsai@chromium.org>
    Reviewed-by: Mao Huang <littlecvr@chromium.org>
    Commit-Queue: Bowgo Tsai <bowgotsai@chromium.org>

--- py/test/pytests/lte_verify_config.py
+++ py/test/pytests/lte_verify_config.py
@@ -9,11 +9,13 @@
 Usage example::
 
   FactoryTest(
+      exclusive=['NETWORKING'],
       id='LTEVerifyConfig',
       label_zh=u'确认 LTE 参数',
       pytest_name='lte_verify_config',
       dargs={
           'modem_path': 'ttyACM0',
+          'attempts': 3,
           'config_to_check': [
               # Single line response example.
               ('AT_COMMAND_1', 'RESPONSE_1'),
@@ -25,13 +27,18 @@ import unittest
 
 import factory_common  # pylint: disable=W0611
 
-from cros.factory.rf import modem
+from cros.factory.board import modem_utils  # pylint: disable=E0611
+from cros.factory.test import factory
 from cros.factory.test.args import Arg
+from cros.factory.rf import modem
 
 
 class LTEVerifyConfig(unittest.TestCase):
   ARGS = [
     Arg('modem_path', str, 'The path of the serial port.'),
+    Arg('attempts', int,
+        'Number of tries to enter factory mode, since the firmware AT+CFUN=4 '
+        'is not stable enough.', default=2),
     Arg('config_to_check', list, 'A list of tuples. For each tuple, the first '
         'element is the command and the second element is the expected '
         'response. Expected response can be a single string indicating only '
@@ -41,13 +48,31 @@ class LTEVerifyConfig(unittest.TestCase):
   def setUp(self):
     self.modem = modem.Modem(self.args.modem_path)
 
+  def EnterFactoryMode(self):
+    factory.console.info('LTE: Entering factory test mode')
+    self.modem = modem_utils.EnterFactoryMode(
+        attempts=self.args.attempts)
+    factory.console.info('LTE: Entered factory test mode')
+
+  def ExitFactoryMode(self):
+    factory.console.info('LTE: Exiting factory test mode')
+    if self.modem:
+      modem_utils.ExitFactoryMode(self.modem)
+      factory.console.info('LTE: Exited factory test mode')
+    else:
+      factory.console.info('No modem object exists to exit')
+
   def runTest(self):
-    for cmd, expected_response in self.args.config_to_check:
-      if isinstance(expected_response, str):
-        expected_response = [expected_response, 'OK']
-      else:
-        expected_response += ['OK']
-      response = self.modem.SendCommandWithCheck(cmd)
-      if response != expected_response:
-        raise ValueError(
-            'Should get %s but got %s' % (expected_response, response))
+    try:
+      self.EnterFactoryMode()
+      for cmd, expected_response in self.args.config_to_check:
+        if isinstance(expected_response, str):
+          expected_response = [expected_response, 'OK']
+        else:
+          expected_response += ['OK']
+        response = self.modem.SendCommandWithCheck(cmd)
+        if response != expected_response:
+          raise ValueError(
+              'Should get %s but got %s' % (expected_response, response))
+    finally:
+      self.ExitFactoryMode()
