dbeb0e238217cec6be3b041586cc160d04c6e305
bowgotsai <bowgotsai@chromium.org>
2014-09-23 19:48:37 +0800

    Add migrate_board_dir script
    It's a common usage to copy one board-specific folder from one project
    to another similar project. For example, copy the chromeos-factory-board
    under one private overlay to another private overlay.
    
    After copy, we should also rename board name in the file/folder names
    and also rename the strings in file. For example:
    
    1. Renames file name:
       rambi_shopfloor.py -> cranky_shopfloor.py
    
    2. Renames folder name:
       chromeos-bsp-rambi-private -> chromeos-bsp-cranky-private
    
    3. Renames strings in files:
       RambiBoard -> CrankyBoard
       rambi_shopfloor -> cranky_shopfloor
       RAMBI_SHOPFLOOR_BACKEND -> CRANKY_SHOPFLOOR_BACKEND
    
    4. Updates the year in the copyright header:
       'Copyright 2013 ...' -> 'Copyright 2014 ...'.
    
    BUG=chrome-os-partner:32208
    TEST=unittest
    
    Change-Id: I050008a28ac3881d8042161e7d6b334b937d238c
    Reviewed-on: https://chromium-review.googlesource.com/219248
    Tested-by: Bowgo Tsai <bowgotsai@chromium.org>
    Reviewed-by: Dean Liao <deanliao@chromium.org>
    Commit-Queue: Bowgo Tsai <bowgotsai@chromium.org>

---
+++ py/tools/migrate_board_dir.py
@@ -0,0 +1,256 @@
+#!/usr/bin/python
+#
+# Copyright 2014 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+import argparse
+from collections import namedtuple
+from datetime import date
+import os
+import re
+import shutil
+import sys
+
+import factory_common  # pylint: disable=W0611
+from cros.factory.utils.file_utils import UnopenedTemporaryFile
+
+
+ReplacePattern = namedtuple('ReplacePattern',
+                            ['old_substring', 'new_substring'])
+
+DESCRIPTION = """Migrates a board-specific folder from one board to
+another board. For example, copying chromeos-factory-board folder
+under one private overlay to another private overlay. It also renames
+board-specific folder/file names and renames board-specific strings
+in files.
+"""
+
+EXAMPLES = r"""Examples:
+  1. Migrating chromeos-factory-board folder from rambi private overlay
+     to cranky private overlay:
+
+     py/tools/migrate_board_dir.py \
+         --src_board_name rambi \
+         --dst_board_name cranky \
+         --src_dir ~/trunk/src/private-overlays/overlay-rambi-private/\
+chromeos-base/chromeos-factory-board \
+         --dst_dir ~/trunk/src/private-overlays/overlay-cranky-private/\
+chromeos-base/chromeos-factory-board \
+         --reset_ebuild_file
+
+  2. Migrating chromeos-bsp-rambi folder (for battery cutoff scripts) from
+     rambi public overlay to cranky public overlay as chromeos-bsp-cranky:
+
+     py/tools/migrate_board_dir.py \
+         --src_board_name rambi \
+         --dst_board_name cranky \
+         --src_dir ~/trunk/src/overlays/overlay-rambi/chromeos-base/\
+chromeos-bsp-rambi \
+         --dst_dir ~/trunk/src/overlays/overlay-cranky/chromeos-base/\
+chromeos-bsp-cranky \
+         --reset_ebuild_file
+"""
+
+
+class MigrateBoardException(Exception):
+  pass
+
+
+def PrepareDirectoryCopy(src_dir, dst_dir,
+                         in_stream=sys.stdin, out_stream=sys.stdout):
+  """Checks that src_dir exists but dst_dir doesn't exist.
+
+  If the dst_dir already exists, prompts a message showing that the
+  directory will be deleted before copy. The user can select 'y' to
+  proceed or 'n' to cancel the operation.
+
+  Args:
+    src_dir: The source directory to copy the files from.
+    dst_dir: The destination directory to copy the files to.
+    in_stream: a stream to read user input from.
+    out_stream: a stream to write output messages to.
+  """
+  if not os.path.isdir(src_dir):
+    out_stream.write('Source directory: %r not found.\n' % src_dir)
+    sys.exit(1)
+
+  if os.path.isdir(dst_dir):
+    out_stream.write('dst_dir: %r already exists.\n'
+                     'The folder will be deleted before copy.\n'
+                     'Are you sure you want to proceed?\n'
+                     'Press "y" to continue and press "n" to exit.\n' % dst_dir)
+    while True:
+      input_char = in_stream.readline().strip()
+      if input_char == 'n':
+        sys.exit(0)
+      if input_char == 'y':
+        shutil.rmtree(dst_dir)
+        out_stream.write(
+            'Directory: %r was removed before migration.\n' % dst_dir)
+        break
+      out_stream.write('Only accept input y/n.\n')
+
+
+def CopyFilesAndRename(src_dir, dst_dir, rename_pattern, reset_ebuild_file):
+  """Copies all files under src_dir to dst_dir.
+
+  It will rename folder/file names based on the value in rename_pattern.
+  For example, renaming rambi_shopfloor.py to cranky_shopfloor.py if the
+  rename_pattern is ReplacePattern('rambi', 'cranky').
+
+  It also changes the path name of a symlink. For example,
+  change a symlink path from ../rambi_mock_shopfloor_backend.py
+  to ../cranky_mock_shopfloor_backend.py.
+
+  If will reset r\d+.ebuild files to r1.ebuild if reset_ebuild_file is True.
+
+  Args:
+    src_dir: The source directory to copy the files from.
+    dst_dir: The destination directory to copy the files to.
+    rename_pattern: A ReplacePattern tuple representing that the old substring
+        in folder/file name will be replaced with the new substring.
+    reset_ebuild_file: True for resetting *-r\d+.ebuild files to *-r1.ebuild.
+  """
+  names = os.listdir(src_dir)
+  os.mkdir(dst_dir, os.stat(src_dir).st_mode)
+  errors = []
+
+  for name in names:
+    src_name = os.path.join(src_dir, name)
+    dst_name = os.path.join(dst_dir, name.replace(rename_pattern.old_substring,
+                                                  rename_pattern.new_substring))
+    # Resets ebuild version if necessary.
+    if reset_ebuild_file and re.search(r'r\d+\.ebuild$', dst_name):
+      dst_name = re.sub(r'r\d+\.ebuild$', 'r1.ebuild', dst_name)
+
+    try:
+      # Handles symlink file and rename the symlink path.
+      if os.path.islink(src_name):
+        target = os.readlink(src_name).replace(rename_pattern.old_substring,
+                                               rename_pattern.new_substring)
+        os.symlink(target, dst_name)
+      elif os.path.isdir(src_name):
+        CopyFilesAndRename(src_name, dst_name,
+                           rename_pattern, reset_ebuild_file)
+      else:
+        shutil.copy2(src_name, dst_name)
+    except (IOError, os.error) as why:
+      errors.append((src_name, dst_name, str(why)))
+    # Catch the Error from the recursive CopyFilesAndRename so that we can
+    # continue with other files.
+    except MigrateBoardException as err:
+      errors.extend(err.args[0])
+
+  try:
+    shutil.copystat(src_dir, dst_dir)
+  except OSError as why:
+    errors.append((src_dir, dst_dir, str(why)))
+
+  if errors:
+    raise MigrateBoardException(errors)
+
+
+def ReplaceStringInFiles(root_dir, replace_patterns):
+  """Replaces strings based on replace_patterns for all files in root_dir.
+
+  For example, renaminig string 'RambiBoard' to 'CrankyBoard' if there
+  is a tuple ('Rambi', 'Cranky') in replace_patterns.
+
+  Args:
+    root_dir: The root directory containing files that we want to
+        replace strings.
+    replace_patterns: A list of ReplacePatterns (old_substring, new_substring)
+        that we want to replace old_substring to new_substring in all files
+        under the root_dir.
+  """
+  for dirpath, _, filenames in os.walk(root_dir):
+    for filename in filenames:
+      filepath = os.path.join(dirpath, filename)
+      # No need to process the symlink because the target file will
+      # be processed.
+      if os.path.islink(filepath):
+        continue
+      with open(filepath, 'r') as old_file:
+        with UnopenedTemporaryFile() as new_filepath:
+          shutil.copystat(filepath, new_filepath)
+          with open(new_filepath, 'w') as new_file:
+            for line in old_file:
+              for replace_pattern in replace_patterns:
+                line = re.sub(replace_pattern.old_substring,
+                              replace_pattern.new_substring,
+                              line)
+              new_file.write(line)
+          shutil.move(new_filepath, filepath)
+
+
+def GenerateReplacePatterns(src_board_name, dst_board_name):
+  """Generates a list of ReplacePattern for string substitution in files.
+
+  Args:
+   src_board_name: The board name of source directory to copy the files from.
+   dst_board_name: The board name of destination directory to copy the files to.
+
+  Returns:
+    A list of ReplacePatterns including the following patterns:
+    1. All characters are lowercase, ex: 'rambi' -> 'cranky'.
+    2. All characters are uppercase, ex: 'RAMBI' -> 'CRANKY'.
+    3. The first character is capital, ex: 'Rambi' -> 'Cranky'.
+    4. The year in copyright header, ex: 'Copyright 2013' -> 'Copyright 2014'.
+  """
+  replace_patterns = [ReplacePattern(getattr(src_board_name, pattern)(),
+                                     getattr(dst_board_name, pattern)())
+                      for pattern in ['lower', 'upper', 'title']]
+
+  # Changes the year in the copyright header.
+  # Also converts old style format to new style format.
+  # Old style: Copyright (c) 2013 The Chromium OS Authors.
+  # New style: Copyright 2014 The Chromium OS Authors.
+  COPYRIGHT = 'Copyright .* The Chromium OS Authors'
+  COPYRIGHT_THIS_YEAR = 'Copyright %d The Chromium OS Authors' % (
+      date.today().year)
+  replace_patterns.append(ReplacePattern(COPYRIGHT, COPYRIGHT_THIS_YEAR))
+
+  return replace_patterns
+
+
+def ParseArgs():
+  parser = argparse.ArgumentParser(
+      description=DESCRIPTION,
+      epilog=EXAMPLES,
+      formatter_class=argparse.RawDescriptionHelpFormatter)
+
+  parser.add_argument('--src_board_name', dest='src_board_name',
+                      help=("The board name of the source directory to "
+                            "copy the files from."))
+  parser.add_argument('--dst_board_name', dest='dst_board_name',
+                      help=("The board name of the destination directory to "
+                            "copy the files to."))
+
+  parser.add_argument('--src_dir', dest='src_dir',
+                      help=("The source directory to copy the files from."))
+  parser.add_argument('--dst_dir', dest='dst_dir',
+                      help=("The destinaton directory to copy the files to."))
+
+  parser.add_argument('--reset_ebuild_file', action='store_true',
+                      help=("whether to reset ebuild files to version r1."))
+
+  return parser.parse_args()
+
+
+def main():
+  args = ParseArgs()
+  PrepareDirectoryCopy(args.src_dir, args.dst_dir)
+  CopyFilesAndRename(args.src_dir, args.dst_dir,
+                     ReplacePattern(args.src_board_name, args.dst_board_name),
+                     args.reset_ebuild_file)
+  ReplaceStringInFiles(args.dst_dir,
+                       GenerateReplacePatterns(args.src_board_name,
+                                               args.dst_board_name))
+  print ('Migration complete!\n'
+         'Please check the result under: %r.') % args.dst_dir
+
+
+if __name__ == '__main__':
+  main()
---
+++ py/tools/migrate_board_dir_unittest.py
@@ -0,0 +1,273 @@
+#!/usr/bin/python
+#
+# Copyright 2014 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+import mox
+import os
+import shutil
+import sys
+import tempfile
+import unittest
+from StringIO import StringIO
+
+import factory_common  # pylint: disable=W0611
+from cros.factory.tools import migrate_board_dir
+from cros.factory.tools.migrate_board_dir import MigrateBoardException
+from cros.factory.tools.migrate_board_dir import ReplacePattern
+from cros.factory.utils import file_utils
+
+
+def CheckFileContent(path_contents_dict):
+  """Checks the contents of a list of files are as expected or not.
+
+  Args:
+    path_contents_dict: a dict of the format {'path1': 'content1',
+        'path2', content2'} indicating the expected content for each file.
+        A special content 'linkto: source' means a symlink pointing to source.
+  """
+  for path, expected_content in path_contents_dict.iteritems():
+    if expected_content.startswith('linkto:'):
+      expected_linkto = expected_content.split(':')[1].strip()
+      actual_linkto = os.readlink(path)
+      if expected_linkto != actual_linkto:
+        raise ValueError('Symlink path error (expected %r but got %r).' % (
+            expected_linkto, actual_linkto))
+    else:
+      actual_content = file_utils.ReadFile(path)
+      if expected_content != actual_content:
+        raise ValueError('File content error (expected %r but got %r' % (
+            expected_content, actual_content))
+
+
+def CreateFileWithContent(path_contents_dict):
+  """Creates a list of files with the desired content.
+
+  Args:
+    path_contents_dict: a dict of the format {'path1': 'content1',
+        'path2', content2'} indicating the desired content for each file.
+        A special content 'linkto: source' means creating a symlink pointing
+        to source.
+  """
+  for path, content in path_contents_dict.iteritems():
+    if not os.path.exists(os.path.dirname(path)):
+      os.makedirs(os.path.dirname(path))
+
+    if content.startswith('linkto:'):
+      linkto = content.split(':')[1].strip()
+      file_utils.ForceSymlink(linkto, path)
+    else:
+      file_utils.WriteFile(path, content)
+
+
+class PrepareDirectoryCopyTest(unittest.TestCase):
+  def setUp(self):
+    self.temp_dir = tempfile.mkdtemp(prefix='migrate_board_dir_unittest')
+    self.mox = mox.Mox()
+    self.mock_instream = self.mox.CreateMock(sys.stdin)
+    self.mock_outstream = StringIO()
+
+  def tearDown(self):
+    shutil.rmtree(self.temp_dir)
+    self.mox.UnsetStubs()
+
+  def testNoSourceDirectory(self):
+    nonexistent_src_dir = os.path.join(self.temp_dir, 'nonexistent_src')
+    with self.assertRaises(SystemExit) as sys_exit:
+      migrate_board_dir.PrepareDirectoryCopy(
+          nonexistent_src_dir, mox.IgnoreArg(), sys.stdin, self.mock_outstream)
+
+    # Checks sys.exit(1) and the output messages.
+    self.assertEqual(sys_exit.exception.code, 1)
+    self.assertEqual(
+        'Source directory: %r not found.\n' % nonexistent_src_dir,
+        self.mock_outstream.getvalue())
+
+  def testPressNToCancel(self):
+    self.mock_instream.readline().AndReturn('n\n')
+    self.mox.ReplayAll()
+
+    src_dir = os.path.join(self.temp_dir, 'src')
+    dst_dir = os.path.join(self.temp_dir, 'dst')
+    os.mkdir(src_dir)
+    os.mkdir(dst_dir)
+
+    with self.assertRaises(SystemExit) as sys_exit:
+      migrate_board_dir.PrepareDirectoryCopy(src_dir, dst_dir,
+                                             self.mock_instream)
+    # User presses 'n' to cancel the operation.
+    # Checks that dst_dir was not removed and sys.exit(0).
+    self.assertTrue(os.path.exists(dst_dir))
+    self.assertEqual(sys_exit.exception.code, 0)
+    self.mox.VerifyAll()
+
+  def testPressYToProceed(self):
+    self.mock_instream.readline().AndReturn('y\n')
+    self.mox.ReplayAll()
+
+    src_dir = os.path.join(self.temp_dir, 'src')
+    dst_dir = os.path.join(self.temp_dir, 'dst')
+    os.mkdir(src_dir)
+    os.mkdir(dst_dir)
+
+    migrate_board_dir.PrepareDirectoryCopy(
+        src_dir, dst_dir, self.mock_instream, self.mock_outstream)
+    # User presses 'y' to remove the dst_dir.
+    # Checks that dst_dir was removed.
+    self.assertFalse(os.path.exists(dst_dir))
+    self.assertTrue(self.mock_outstream.getvalue().endswith(
+        'Directory: %r was removed before migration.\n' % dst_dir))
+    self.mox.VerifyAll()
+
+
+class CopyFilesAndRenameTest(unittest.TestCase):
+  def setUp(self):
+    self.temp_dir = tempfile.mkdtemp(prefix='migrate_board_dir_unittest')
+    self.mox = mox.Mox()
+
+  def tearDown(self):
+    shutil.rmtree(self.temp_dir)
+    self.mox.UnsetStubs()
+
+  def testCopyFilesAndRenameSuccess(self):
+    src_dir = os.path.join(self.temp_dir, 'src')
+    dst_dir = os.path.join(self.temp_dir, 'dst')
+    src_files = {
+        os.path.join(src_dir, 'test-0.0.1.ebuild'): 'Test ebuild file.',
+        os.path.join(src_dir, 'dog_file1'): 'Test file.',
+    }
+    src_symlink_files = {
+        os.path.join(src_dir, 'test-0.0.1-r100.ebuild'):
+            'linkto: ./test-0.0.1.ebuild',
+        os.path.join(src_dir, 'dog_folder', 'dog_file2'):
+            'linkto: ../dog_file1',
+    }
+    expected_dst_files = {
+        os.path.join(dst_dir, 'test-0.0.1.ebuild'):
+            'Test ebuild file.',
+        os.path.join(dst_dir, 'cat_file1'):
+            'Test file.',
+        os.path.join(dst_dir, 'test-0.0.1-r1.ebuild'):
+            'linkto: ./test-0.0.1.ebuild',
+        os.path.join(dst_dir, 'cat_folder', 'cat_file2'):
+            'linkto: ../cat_file1',
+    }
+
+    CreateFileWithContent(src_files)
+    CreateFileWithContent(src_symlink_files)
+    migrate_board_dir.CopyFilesAndRename(
+        src_dir,
+        dst_dir,
+        ReplacePattern('dog', 'cat'), # Renames 'dog' to 'cat' in file names.
+        reset_ebuild_file=True)
+    CheckFileContent(expected_dst_files)
+
+  def testCopyFilesAndRenameWithFailure(self):
+    src_dir = os.path.join(self.temp_dir, 'src')
+    dst_dir = os.path.join(self.temp_dir, 'dst')
+    src_files = {
+        os.path.join(src_dir, 'no_such_file1'): 'No such file1.',
+        os.path.join(src_dir, 'dog_folder', 'no_such_file2'): 'No such file2.',
+        os.path.join(src_dir, 'dog_file3'): 'Test file3.',
+    }
+    CreateFileWithContent(src_files)
+
+    errors = []  # Stores the error for all file operations.
+    self.mox.StubOutWithMock(shutil, 'copy2')
+    self.mox.StubOutWithMock(shutil, 'copystat')
+
+    # Exception happens for no_such_file in the root folder.
+    src_file = os.path.join(src_dir, 'no_such_file1')
+    dst_file = os.path.join(dst_dir, 'no_such_file1')
+    raised_exception = IOError(
+        'IOError: [Errno 2] No such file or directory: %r' % src_file)
+    shutil.copy2(src_file, dst_file).InAnyOrder().AndRaise(raised_exception)
+    errors.append((src_file, dst_file, str(raised_exception)))
+
+    # Exception happens for no_such_file2 in the sub folder.
+    src_file = os.path.join(src_dir, 'dog_folder', 'no_such_file2')
+    dst_file = os.path.join(dst_dir, 'cat_folder', 'no_such_file2')
+    raised_exception = IOError(
+        'IOError: [Errno 2] No such file or directory: %r' % src_file)
+    shutil.copy2(src_file, dst_file).InAnyOrder().AndRaise(raised_exception)
+    errors.append((src_file, dst_file, str(raised_exception)))
+    # The sub folder should be copied and renamed as expected.
+    shutil.copystat(
+        os.path.join(src_dir, 'dog_folder'),
+        os.path.join(dst_dir, 'cat_folder')).InAnyOrder().AndReturn(0)
+
+    # Test copystat error.
+    raised_exception = OSError(
+        'OSError: [Errno 2] No such file or directory: %r' % src_dir)
+    shutil.copystat(src_dir, dst_dir).InAnyOrder().AndRaise(raised_exception)
+    errors.append((src_dir, dst_dir, str(raised_exception)))
+
+    # Normal files should still be copied and renamed as expected.
+    src_file = os.path.join(src_dir, 'dog_file3')
+    dst_file = os.path.join(dst_dir, 'cat_file3')
+    shutil.copy2(src_file, dst_file).InAnyOrder().AndReturn(0)
+
+    self.mox.ReplayAll()
+
+    with self.assertRaises(MigrateBoardException) as context_manager:
+      migrate_board_dir.CopyFilesAndRename(
+          src_dir,
+          dst_dir,
+          ReplacePattern('dog', 'cat'), # Renames 'dog' to 'cat' in file names.
+          reset_ebuild_file=True)
+    # Checks it includes all errors raised from recursive call in the exception.
+    self.assertEqual(set(context_manager.exception.args[0]), set(errors))
+
+
+class ReplaceStringInFilesTest(unittest.TestCase):
+  def setUp(self):
+    self.temp_dir = tempfile.mkdtemp(prefix='migrate_board_dir_unittest')
+
+  def tearDown(self):
+    shutil.rmtree(self.temp_dir)
+
+  def testReplaceString(self):
+    # Checks the following rename patterns:
+    # 1. dog -> cat
+    # 2. Dog -> Cat
+    # 3. DOG -> CAT
+    content1_before = """\
+This is a dog.
+Dog is man's best friend.
+WATCH OUT FOR THE DOGS!!!"""
+    content1_after = """\
+This is a cat.
+Cat is man's best friend.
+WATCH OUT FOR THE CATS!!!"""
+
+    # Checks the year in the license header will be updated.
+    # Also checks that (c) is removed.
+    content2_before = """\
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file."""
+    content2_after = """\
+# Copyright 2014 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file."""
+
+    files_before_replacement = {
+        os.path.join(self.temp_dir, 'file1'): content1_before,
+        os.path.join(self.temp_dir, 'sub_folder', 'file2'): content2_before,
+    }
+    files_after_replacement = {
+        os.path.join(self.temp_dir, 'file1'): content1_after,
+        os.path.join(self.temp_dir, 'sub_folder', 'file2'): content2_after,
+    }
+
+    CreateFileWithContent(files_before_replacement)
+    migrate_board_dir.ReplaceStringInFiles(
+        self.temp_dir,
+        migrate_board_dir.GenerateReplacePatterns('dog', 'cat'))
+    CheckFileContent(files_after_replacement)
+
+
+if __name__ == '__main__':
+  unittest.main()
