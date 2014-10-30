c7e3563d84c82eb52b222bbaca29d2721c69b905
Joel Kitching <kitching@google.com>
2014-10-30 12:11:28 +0800

    CHERRY-PICK: Fix autotest_common bug which overwrites cron module
    BUG=chrome-os-partner:33333
    TEST=Make sure verify passes (including dependency check and `make test`).
          Ensure that `make doc` works from within chroot.
          Attempt to manually include an autotest library in chroot and on DUT:
            import factory_common
            import cros.factory.test.autotest_common
            from autotest_lib.client.cros.networking import wifi_proxy
    
    Original-Change-Id: I16e51721b7aa8d324c93b9d7e3aa1aef774666d3
    
    Change-Id: Ie9fa3f1e6c47155531d5572a04373b2e91d5f8e4
    Reviewed-on: https://chromium-review.googlesource.com/226472
    Tested-by: Joel Kitching <kitching@google.com>
    Reviewed-by: Ricky Liang <jcliang@chromium.org>
    Commit-Queue: Joel Kitching <kitching@google.com>

--- py/test/autotest_common.py
+++ py/test/autotest_common.py
@@ -1,33 +1,41 @@
+#!/usr/bin/python
+#
 # Copyright 2014 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-'''
-Prepares autotest-compatible namespace for ChromeOS factory framework.
-'''
+"""A helper module to set up sys.path so that autotest_lib.client.* can be
+located."""
 
 from __future__ import print_function
+
+# pylint: disable=F0401
 
 import os
 import sys
 
+import factory_common   # pylint:disable=W0611
+from cros.factory.test import utils
+
+if utils.in_cros_device():
+  autotest_dir = '/usr/local/autotest'
+else:
+  CROS_WORKON_SRCROOT = os.environ['CROS_WORKON_SRCROOT']
+  autotest_dir = os.path.join(
+      CROS_WORKON_SRCROOT, 'src', 'third_party', 'autotest', 'files', 'client')
+
 # Temporarily remove the factory cros module, which causes autotest to get
 # confused when assigning autotest_lib.client.cros.
-if 'cros' in sys.modules:
+_cros = sys.modules.get('cros')
+if _cros:
   del sys.modules['cros']
 
-# Choose the correct location of the autotest source tree.
-src_root = os.environ.get('CROS_WORKON_SRCROOT')
-if src_root:
-  # chroot: autotest files will be in Chromium source tree.
-  autotest_bin_dir = os.path.join(src_root,
-      'src/third_party/autotest/files/client/bin')
-else:
-  # Test image: autotest files will be in /usr/local.
-  autotest_bin_dir = '/usr/local/autotest/bin'
-assert os.path.isdir(autotest_bin_dir), 'No autotest tree available'
-
-# Load 'common' module from autotest/bin namespace.
-sys.path.insert(0, autotest_bin_dir)
-import common  # pylint: disable=W0611
+sys.path.insert(0, autotest_dir)
+import setup_modules
 sys.path.pop(0)
+setup_modules.setup(base_path=autotest_dir,
+    root_module_name='autotest_lib.client')
+
+# Re-add the factory cros module.
+if _cros:
+  sys.modules['cros'] = _cros
