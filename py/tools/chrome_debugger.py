423a39c7b0acad7678a70257ec134d63ffc65c81
Hung-Te Lin <hungte@chromium.org>
2014-11-11 17:39:03 +0800

    Support Chrome remote debugging protocol.
    Factory software may to tweak Chrome by its remote debugging protocol, for
    example changing active page content at runtime.
    
    The new chrome_debugger module provides simple and easy access to Chrome remote
    debugging protocol without complete Telemetry support.
    
    BUG=chromium:431645
    TEST=unittest.
    Change-Id: I730006bd7527dc418aed0730fede1a00381f4004
    Reviewed-on: https://chromium-review.googlesource.com/228985
    Tested-by: Hung-Te Lin <hungte@chromium.org>
    Reviewed-by: Ricky Liang <jcliang@chromium.org>
    Commit-Queue: Hung-Te Lin <hungte@chromium.org>

---
+++ py/tools/chrome_debugger.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+# Copyright 2014 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Chrome remote debugging protocol.
+
+This module provides controlling Chrome browser by the Remote Debugging
+Protocol.
+
+Example:
+
+  To change current tab to Google,
+
+  chrome = ChromeRemoteDebugger()
+  while not chrome.IsReady():
+    time.sleep(1)
+  chrome.SetActivePage()
+  chrome.PageNavigate('http://www.google.com')
+
+See https://developer.chrome.com/devtools/docs/debugger-protocol for more
+information."""
+
+
+from __future__ import print_function
+
+import json
+import logging
+import sys
+import threading
+import time
+import urllib2
+
+from ws4py.client.threadedclient import WebSocketClient
+
+
+DEFAULT_CHROME_DEBUG_URL = 'http://127.0.0.1:9222'
+
+
+class ChromeRemoteDebugger(object):
+  """An interface to control Chrome brower by remote debugging protocol.
+
+  Args:
+    debug_url: An URL to connect to debug port (--remote-debugging-port) of
+               running Chrome instance.
+
+  Attributes:
+    id: An incremental identifier for debugging protocol.
+    lock: Internal lock.
+    active_websocket: An websocket client to active page.
+  """
+
+  ANY = 'any value'
+
+  def __init__(self, debug_url=DEFAULT_CHROME_DEBUG_URL):
+    self.debug_url = debug_url
+    self.id = 1
+    self.active_websocket = None
+    self.lock = threading.Lock()
+
+  def IsReady(self):
+    """Checks if a browser instance is available (with debugging ports enabled).
+
+    Returns:
+      True if an instance is ready, otherwise False.
+    """
+    with self.lock:
+      if self.active_websocket:
+        return True
+    try:
+      self.GetPages()
+      return True
+    except:
+      return False
+
+  def GetPages(self, page_type=ANY):
+    """Returns current pages of running Chrome browser.
+
+    Args:
+      page_type: Filters result by given type, or ANY to return all types.
+
+    Returns:
+      A list representing PageSet in Chrome remote debugging protocol.
+    """
+    page_set = json.load(urllib2.urlopen(self.debug_url + '/json'))
+    if page_type is not self.ANY:
+      return [page for page in page_set if page['type'] == page_type]
+    return page_set
+
+  def SetActivePage(self, page=ANY):
+    """Sets a page as active session for sending commands.
+
+    Args:
+      page: A page instance (returned by GetPages), or ANY to activate the first
+            normal webpage (type 'other').
+    """
+    if page is self.ANY:
+      page = self.GetPages('other')[0]
+    ws = None
+    if page is not None:
+      ws = WebSocketClient(page['webSocketDebuggerUrl'])
+      ws.connect()
+    with self.lock:
+      if self.active_websocket:
+        self.active_websocket.close()
+      self.active_websocket = ws
+
+  def SendCommand(self, command):
+    """Sends a remote debugging websocket command to Chrome browser.
+
+    Args:
+      command: A dictionary of remote debugging command.
+    """
+    command = command.copy()
+    with self.lock:
+      command.update({'id': self.id})
+      self.id += 1
+      self.active_websocket.send(json.dumps(command))
+
+  def PageNavigate(self, url):
+    """Navigates current page to the given URL.
+
+    Args:
+      url: An URL to open in browser.
+    """
+    self.SendCommand({'method': 'Page.navigate',
+                      'params': {'url': url}})
+
+
+if __name__ == '__main__':
+  if len(sys.argv) != 3:
+    exit('Usage: %s method_name json_params' % sys.argv[0])
+  chrome = ChromeRemoteDebugger()
+  chrome.SetActivePage()
+  chrome.SendCommand({'method': sys.argv[1],
+                      'params': json.loads(sys.argv[2])})
---
+++ py/tools/chrome_debugger_unittest.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# Copyright 2014 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from __future__ import print_function
+
+import json
+import logging
+import StringIO
+import unittest
+import urllib2
+
+import mox
+from ws4py.client.threadedclient import WebSocketClient
+
+import factory_common  # pylint: disable=W0611
+from cros.factory.tools import chrome_debugger
+
+
+class ChromeRemoteDebuggerTest(unittest.TestCase):
+
+  def setUp(self):
+    self.chrome = chrome_debugger.ChromeRemoteDebugger()
+    self.mox = mox.Mox()
+    self.mox.StubOutWithMock(urllib2, 'urlopen')
+    self.mox.StubOutWithMock(chrome_debugger, 'WebSocketClient')
+    self.mock_pageset_url = chrome_debugger.DEFAULT_CHROME_DEBUG_URL + '/json'
+    self.mock_pageset = [{
+        "id": "6CA5278B-EEC7-48F7-BBE2-297C6DEFB59A",
+        "title": "FactoryTestExtension",
+        "type": "background_page",
+        "url": "chrome-extension://pngocaclmlmihmhokaeejfiklacihcmb/....html",
+        "webSocketDebuggerUrl": "ws://6CA5278B-EEC7-48F7-BBE2-297C6DEFB59A"
+      }, {
+        "id": "79589549-AD32-4AC8-B4EE-6B06EFF2D7CF",
+        "title": "Blank page",
+        "type": "other",
+        "url": "about:blank",
+        "webSocketDebuggerUrl": "ws://79589549-AD32-4AC8-B4EE-6B06EFF2D7CF",
+     }, {
+       "id": "7711AF2B-B1CF-40C4-B269-0D29A72BEBBC",
+       "title": "Why Should I Care What Color the Bikeshed Is?",
+       "type": "other",
+       "url": "http://cyan.bikeshed.com/",
+       "webSocketDebuggerUrl": "ws://7711AF2B-B1CF-40C4-B269-0D29A72BEBBC"
+     }]
+    self.mock_pageset_stream = StringIO.StringIO(json.dumps(self.mock_pageset))
+    self.mock_websocket = self.mox.CreateMock(WebSocketClient)
+
+  def tearDown(self):
+    self.mox.UnsetStubs()
+
+  def testIsReady(self):
+    urllib2.urlopen(self.mock_pageset_url).AndRaise(
+        urllib2.URLError('Cannot connect'))
+    urllib2.urlopen(self.mock_pageset_url).AndReturn(self.mock_pageset_stream)
+    self.mox.ReplayAll()
+    self.assertFalse(self.chrome.IsReady())
+    self.assertTrue(self.chrome.IsReady())
+    self.mox.VerifyAll()
+
+  def testGetPages(self):
+    urllib2.urlopen(self.mock_pageset_url).AndReturn(self.mock_pageset_stream)
+    urllib2.urlopen(self.mock_pageset_url).AndReturn(self.mock_pageset_stream)
+    urllib2.urlopen(self.mock_pageset_url).AndReturn(self.mock_pageset_stream)
+    self.mox.ReplayAll()
+    self.assertEquals(self.chrome.GetPages(), self.mock_pageset)
+    self.mock_pageset_stream.seek(0)
+    self.assertEquals(self.chrome.GetPages('background_page'),
+                      [self.mock_pageset[0]])
+    self.mock_pageset_stream.seek(0)
+    self.assertEquals(self.chrome.GetPages('no-such-page'), [])
+    self.mox.VerifyAll()
+
+  def testSetActivePage(self):
+    self.mox.StubOutWithMock(self.chrome, 'GetPages')
+    self.chrome.GetPages('other').AndReturn(self.mock_pageset[1:])
+    chrome_debugger.WebSocketClient(self.mock_pageset[1]['webSocketDebuggerUrl']
+                                    ).AndReturn(self.mock_websocket)
+    self.mock_websocket.connect()
+    self.mock_websocket.close()
+    self.mox.ReplayAll()
+
+    self.chrome.SetActivePage()
+    self.chrome.SetActivePage(None)
+    self.mox.VerifyAll()
+
+  def testSendCommand(self):
+    command = {'method': 'test', 'params': {'param1': 'value1'}}
+    expected = command.copy()
+    expected.update({'id': 1})
+    self.chrome.active_websocket = self.mock_websocket
+    self.chrome.active_websocket.send(json.dumps(expected))
+    self.mox.ReplayAll()
+
+    self.assertEquals(1, self.chrome.id)
+    self.chrome.SendCommand(command)
+    self.assertEquals(2, self.chrome.id)
+    self.chrome.active_websocket = None
+    self.mox.VerifyAll()
+
+  def testPageNavigate(self):
+    url = 'http://blah'
+    expected = {'method': 'Page.navigate', 'params': {'url': url}}
+    expected.update({'id': 1})
+    self.chrome.active_websocket = self.mock_websocket
+    self.chrome.active_websocket.send(json.dumps(expected))
+    self.mox.ReplayAll()
+
+    self.chrome.PageNavigate(url)
+    self.chrome.active_websocket = None
+    self.mox.VerifyAll()
+
+
+if __name__ == '__main__':
+  logging.basicConfig(level=logging.INFO)
+  unittest.main()
