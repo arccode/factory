7a02bc2317107e89641a0331176fb1fd7af78fb2
Joel Kitching <kitching@google.com>
2014-11-12 14:20:44 +0800

    wifi throughput: allow service arguments override
    Allow any arguments to be overridden for particular services.
    Arguments division is made as follows:
    - SERVICE_ARGS: only for services (e.g. ssid, password)
    - SHARED_ARGS: can be overridden (e.g. min_signal_strength)
    - ARGS: test arguments (e.g. event_log_name) plus SHARED_ARGS
    Also, add a disable_eth flag to choose whether or not to
    disable ethernet during the test.
    
    CQ-DEPEND=CL:*183507,CL:229237
    BUG=chrome-os-partner:32132
    TEST=Manually test on DUT, directly calling run_py_test.py:
      CROS_FACTORY_TEST_PATH=test_data \
      ./run_py_test.py \
      --args '{"event_log_name": "wifi_throughput", \
               "min_signal_strength": 30, \
               "services": {"ssid": "GoogleGuest"}, \
               "iperf_host": "10.0.0.1"}' \
      wifi_throughput
    
    Original-Change-Id: Ia6865aff32b8d13be0f87920ece44e5385e4a051
    Change-Id: I1531b60a5c0b2fca99f0a08b7f7ec4b127c20920
    Reviewed-on: https://chromium-review.googlesource.com/229238
    Reviewed-by: Mao Huang <littlecvr@chromium.org>
    Tested-by: Joel Kitching <kitching@google.com>
    Commit-Queue: Joel Kitching <kitching@google.com>

--- py/test/pytests/wifi_throughput.py
+++ py/test/pytests/wifi_throughput.py
@@ -27,7 +27,7 @@ from cros.factory.system.service_manager import SetServiceStatus
 from cros.factory.system.service_manager import Status
 from cros.factory.test import factory
 from cros.factory.test import leds
-from cros.factory.test.args import Arg
+from cros.factory.test.args import Arg, Args
 from cros.factory.test.fixture import arduino
 from cros.factory.utils.net_utils import GetWLANInterface, GetWLANMACAddress
 from cros.factory.utils.net_utils import SwitchEthernetInterfaces
@@ -86,17 +86,27 @@ def retryWithTimeout(f, log_text=None, fail_text=None, timeout=None, sleep=1):
 
 
 class WiFiThroughput(unittest.TestCase):
-  ARGS = [
-    Arg('event_log_name', str, 'Name of the event_log.  We might want to '
-        're-run the throughput test at different points in the factory, so '
-        'this can be used to separate them.  e.g. "rf_throughput_chamber"',
+  # Arguments that can only be applied to each WiFi service connection.  These
+  # will be checked as key-values in the test's "service" argument (see below).
+  _SERVICE_ARGS = [
+    Arg('ssid', str,
+        'SSID of WiFi service.',
         optional=False),
-    Arg('services', (list, tuple),
-        'A list of WiFi services (ssid, password) tuple to test. '
-        'e.g. [("ssid1", "password1"), ("ssid2", "password2")]. '
-        'Set password to None or "" if it is an open network. '
-        'If services are not specified, this test will check for any AP.',
-        optional=True),
+    Arg('password', str,
+        'Password of WiFi service.',
+        optional=True, default=None)
+  ]
+
+  # Arguments that can be directly applied to each WiFi service connection, OR
+  # directly provided as a test argument.  "service" arguments take precedence.
+  # e.g.
+  #   test arg: min_signal_strength=50
+  #   service A arg: min_signal_strength=40
+  #   service B arg: [doesn't provide min_signal_strength]
+  # result:
+  #   service A min_signal_strength=40
+  #   service B min_signal_strength=50
+  _SHARED_ARGS = [
     Arg('min_signal_strength', int,
         'Minimum signal strength required (range from 0 to 100).',
         optional=True),
@@ -118,10 +128,33 @@ class WiFiThroughput(unittest.TestCase):
         'Required minimum throughput in bits/sec.  If the average throughput '
         'is lower than this, we will fail.',
         optional=True, default=None),
+  ]
+
+  # Test-level arguments.  _SHARED_ARGS is concatenated at the end, since we
+  # want the option to provide arguments as global defaults (as in the example
+  # above).
+  ARGS = [
+    Arg('event_log_name', str, 'Name of the event_log.  We might want to '
+        're-run the conductive test at different points in the factory, so '
+        'this can be used to separate them.  e.g. "rf_throughput_chamber"',
+        optional=False),
     Arg('arduino_high_pins', list,
         'A list of ints.  If not None, set arduino pins in the list to high.',
         optional=True, default=None),
-  ]
+    Arg('disable_eth', bool,
+        'Whether we should take down the ethernet device during this test.',
+        optional=True, default=False),
+    Arg('services', (list, dict),
+        'A list of dicts, each representing a WiFi service to test.  At '
+        'minimum, each must have a "ssid" field.  Usually, a "password" field '
+        'is also included.  (Omit or set to None or "" for an open network.) '
+        'Additionally, the following fields can be provided to override '
+        'arguments passed to this test (refer to _SHARED_ARGS): '
+        'min_signal_strength, iperf_host, transmit_time, transmit_interval, '
+        'min_throughput.  If services are not specified, this test will simply '
+        'list APs.',
+        optional=True, default=[]),
+  ] + _SHARED_ARGS
 
   def __init__(self, *args, **kwargs):
     super(WiFiThroughput, self).__init__(*args, **kwargs)
@@ -160,34 +193,34 @@ class WiFiThroughput(unittest.TestCase):
         fail_text='Timed out while searching for WiFi services')
     return found_ssids
 
-  def _ConnectToService(self, ssid, password):
+  def _ConnectToService(self, ap_config):
     # Look for requested service.
     service = retryWithTimeout(
         lambda: self.wifi.find_matching_service({
           self.wifi.SERVICE_PROPERTY_TYPE: 'wifi',
-          self.wifi.SERVICE_PROPERTY_NAME: ssid}),
-        log_text='Looking for service %s...' % ssid,
-        fail_text='Unable to find service %s' % ssid)
+          self.wifi.SERVICE_PROPERTY_NAME: ap_config.ssid}),
+        log_text='Looking for service %s...' % ap_config.ssid,
+        fail_text='Unable to find service %s' % ap_config.ssid)
 
     # Check signal strength.
-    if self.args.min_signal_strength is not None:
+    if ap_config.min_signal_strength is not None:
       strength = self.wifi.get_dbus_property(service, 'Strength')
-      if strength >= self.args.min_signal_strength:
+      if strength >= ap_config.min_signal_strength:
         factory.console.info('%s strength %d >= %d [pass]',
-            ssid, strength, self.args.min_signal_strength)
+            ap_config.ssid, strength, ap_config.min_signal_strength)
       else:
         raise Exception('%s strength %d < %d [fail]' %
-            (ssid, strength, self.args.min_signal_strength))
+            (ap_config.ssid, strength, ap_config.min_signal_strength))
 
     # Check for connection state.
     is_active = self.wifi.get_dbus_property(service, 'IsActive')
     if is_active:
-      raise Exception('Unexpectedly already connected to %s' % ssid)
+      raise Exception('Unexpectedly already connected to %s' % ap_config.ssid)
 
     # Try connecting.
-    logging.info('Connecting to %s...', ssid)
+    logging.info('Connecting to %s...', ap_config.ssid)
     security_dict = {
-        self.wifi.SERVICE_PROPERTY_PASSPHRASE: password}
+        self.wifi.SERVICE_PROPERTY_PASSPHRASE: ap_config.password}
     (success, _, _, _, reason) = self.wifi.connect_to_wifi_network(
         ssid=ssid,
         security=('psk' if password else 'none'),
@@ -195,12 +228,12 @@ class WiFiThroughput(unittest.TestCase):
         save_credentials=False,
         autoconnect=False)
     if not success:
-      raise Exception('Unable to connect to %s: %s' % (ssid, reason))
+      raise Exception('Unable to connect to %s: %s' % (ap_config.ssid, reason))
     else:
-      factory.console.info('Successfully connected to %s', ssid)
+      factory.console.info('Successfully connected to %s', ap_config.ssid)
       return service
 
-  def _RunIperfAndCheckOutput(self):
+  def _RunIperfAndCheckOutput(self, ap_config):
     '''Invokes an iperf client and parses throughput.
 
     This function uses the following data members:
@@ -216,13 +249,13 @@ class WiFiThroughput(unittest.TestCase):
       Exception on failure.
     '''
     logging.info('Running iperf connecting to host %s for %d seconds',
-        self.args.iperf_host, self.args.transmit_time)
+        ap_config.iperf_host, ap_config.transmit_time)
 
     # Invoke the iperf command.
     iperf_cmd = ['iperf',
-        '--client', self.args.iperf_host,
-        '--time', str(self.args.transmit_time),
-        '--interval', str(self.args.transmit_interval),
+        '--client', ap_config.iperf_host,
+        '--time', str(ap_config.transmit_time),
+        '--interval', str(ap_config.transmit_interval),
         '--reportstyle', 'c']
 
     # We enclose the iperf call in timeout, since when given an unreachable
@@ -230,7 +263,7 @@ class WiFiThroughput(unittest.TestCase):
     timeout_cmd = ['timeout',
         '--signal', 'KILL',
         # Add 5 seconds to allow for process overhead and connection time.
-        str(self.args.transmit_time + 5)] + iperf_cmd
+        str(ap_config.transmit_time + 5)] + iperf_cmd
     iperf = process_utils.Spawn(timeout_cmd, stdout=subprocess.PIPE, log=True)
 
     # iperf outputs CSV with the following as its columns, with rows[:-1] for
@@ -271,13 +304,13 @@ class WiFiThroughput(unittest.TestCase):
     if len(iperf_dict) < 2:
       raise Exception(
           'Failed to make a connection to iperf host %s, or received bogus '
-          'output from iperf' % self.args.iperf_host)
+          'output from iperf' % ap_config.iperf_host)
     if (not all(row['transferred_bits'] > 0 for row in iperf_dict)
         or not all(row['bits_per_second'] > 0 for row in iperf_dict)):
       raise Exception(
           'Succeeded connnecting to %s, but received bogus output '
           'from iperf (transferred_bits or bits_per_second <= 0)' %
-              self.args.iperf_host)
+              ap_config.iperf_host)
 
     def bitsToMbits(x):
       return x / (10.0 ** 6)
@@ -285,34 +318,26 @@ class WiFiThroughput(unittest.TestCase):
     factory.console.info(
         'Successfully connected to %s, transferred: %.2f Mbits, '
         'time spent: %d sec, throughput: %.2f Mbits/sec',
-        self.args.iperf_host,
+        ap_config.iperf_host,
         bitsToMbits(iperf_dict[-1]['transferred_bits']),
-        self.args.transmit_time,
+        ap_config.transmit_time,
         bitsToMbits(iperf_dict[-1]['bits_per_second']))
 
-    if self.args.min_throughput is not None:
+    if ap_config.min_throughput is not None:
       # Ensure the average throughput is over the threshold.
-      if iperf_dict[-1]['bits_per_second'] < self.args.min_throughput:
+      if iperf_dict[-1]['bits_per_second'] < ap_config.min_throughput:
         raise Exception(
             'Throughput %.2f < %.2f Mbits/s didn\'t meet '
             'the threshold requirement' % (
                 bitsToMbits(iperf_dict[-1]['bits_per_second']),
-                bitsToMbits(self.args.min_throughput)))
+                bitsToMbits(ap_config.min_throughput)))
     return iperf_dict
 
   def setUp(self):
-    # Keyboard lights and arduino pins.
-    self._StartOperatorFeedback()
-
-    # Ensure all required services are enabled.
-    for service in _SERVICE_LIST:
-      if GetServiceStatus(service) == Status.STOP:
-        SetServiceStatus(service, Status.START)
-
     # Initialize the log dict, which will later be fed into event log.
     system_info = SystemInfo()
     self.log = {
-        'args': self.args,
+        'args': self.args.ToDict(),
         'run': {
             'path': os.environ.get('CROS_FACTORY_TEST_PATH'),
             'invocation': os.environ.get('CROS_FACTORY_TEST_INVOCATION')},
@@ -325,6 +350,35 @@ class WiFiThroughput(unittest.TestCase):
         'test': {},
         'failures': []}
 
+    # Process service arguments with the Args class, taking test-wide
+    # argument values as default if service argument is absent.  Now, we only
+    # need to read the self.args.services dictionary to get any _SERVICE_ARGS or
+    # _SHARED_ARGS values.
+    args_dict = self.args.ToDict()
+    service_args = []
+    for arg in self._SERVICE_ARGS + self._SHARED_ARGS:
+      service_args.append(Arg(
+          name=arg.name,
+          type=arg.type,
+          help=arg.help,
+          default=args_dict.get(arg.name, arg.default),
+          optional=arg.optional))
+    service_arg_parser = Args(*service_args)
+    if not isinstance(self.args.services, list):
+      self.args.services = [self.args.services]
+    new_services = []
+    for service_dict in self.args.services:
+      new_services.append(service_arg_parser.Parse(service_dict))
+    self.args.services = new_services
+
+    # Keyboard lights and arduino pins.
+    self._StartOperatorFeedback()
+
+    # Ensure all required services are enabled.
+    for service in _SERVICE_LIST:
+      if GetServiceStatus(service) == Status.STOP:
+        SetServiceStatus(service, Status.START)
+
     # Initialize our WifiProxy library.
     self.wifi = wifi_proxy.WifiProxy()
 
@@ -334,14 +388,16 @@ class WiFiThroughput(unittest.TestCase):
     # TODO(kitching): Change the network topology of tests with this problem so
     # that we don't have to take down ethernet.  This way, goofy-split won't
     # disconnect its display when the test is run.
-    logging.info('Disabling ethernet interfaces')
-    SwitchEthernetInterfaces(False)
+    if self.args.disable_eth:
+      logging.info('Disabling ethernet interfaces')
+      SwitchEthernetInterfaces(False)
 
   def tearDown(self):
     self._EndOperatorFeedback()
 
     # Enable ethernet devices.
-    SwitchEthernetInterfaces(True)
+    if self.args.disable_eth:
+      SwitchEthernetInterfaces(True)
 
   def _RunTestChecks(self):
     # Check that we have an online WLAN interface.
@@ -384,14 +440,10 @@ class WiFiThroughput(unittest.TestCase):
     else:
       logging.info('No services found')
 
+    # Test WiFi signal strength for each service.
     if self.args.services:
-      # Test WiFi signal strength for each service.
-      if not isinstance(self.args.services, list):
-        self.args.services = [self.args.services]
-
-      # Set up log data structure.
-      for ssid, password in self.args.services:
-        self._RunOneServiceCheck(ssid, password)
+      for ap_config in self.args.services:
+        self._RunOneServiceCheck(ap_config)
 
     # Log this test run.
     self._Log()
@@ -402,14 +454,14 @@ class WiFiThroughput(unittest.TestCase):
       for error_msg in log['failures']:
         all_failures_str.append("* [%s] %s" % (ssid, error_msg))
     if all_failures_str:
-      error_msg = ('Error in connecting and running iperf '
+      error_msg = ('Error in connecting and/or running iperf '
                    'on one or more services')
       factory.console.info('%s:\n%s', error_msg, '\n'.join(all_failures_str))
       self.fail(error_msg)
 
-  def _RunOneServiceCheck(self, ssid, password):
+  def _RunOneServiceCheck(self, ap_config):
     # Set up logging dict.
-    self.log['test'][ssid] = {
+    self.log['test'][ap_config.ssid] = {
         'ifconfig': None,
         'iwconfig': None,
         'ap': None,
@@ -418,24 +470,24 @@ class WiFiThroughput(unittest.TestCase):
 
     # Try connecting.
     try:
-      factory.console.info('Trying to connect to service %s...', ssid)
-      service = self._ConnectToService(ssid, password)
+      factory.console.info('Trying to connect to service %s...', ap_config.ssid)
+      service = self._ConnectToService(ap_config)
     except Exception as e:
-      self.log['test'][ssid]['failures'].append(e.message)
-      factory.console.info('Failed connecting to service %s', ssid)
+      self.log['test'][ap_config.ssid]['failures'].append(e.message)
+      factory.console.info('Failed connecting to service %s', ap_config.ssid)
       logging.exception(e.message)
       return
 
     # Show ifconfig and iwconfig output.
-    self.log['test'][ssid]['ifconfig'] = process_utils.SpawnOutput(['ifconfig'],
-        check_call=True, log=True)
-    logging.info(self.log['test'][ssid]['ifconfig'])
-    self.log['test'][ssid]['iwconfig'] = process_utils.SpawnOutput(['iwconfig'],
-        check_call=True, log=True)
-    logging.info(self.log['test'][ssid]['iwconfig'])
+    self.log['test'][ap_config.ssid]['ifconfig'] = process_utils.SpawnOutput(
+        ['ifconfig'], check_call=True, log=True)
+    logging.info(self.log['test'][ap_config.ssid]['ifconfig'])
+    self.log['test'][ap_config.ssid]['iwconfig'] = process_utils.SpawnOutput(
+        ['iwconfig'], check_call=True, log=True)
+    logging.info(self.log['test'][ap_config.ssid]['iwconfig'])
 
     # Save network ssid details.
-    self.log['test'][ssid]['ap'] = {
+    self.log['test'][ap_config.ssid]['ap'] = {
         'ssid': self.wifi.get_dbus_property(service, 'Name'),
         'security': self.wifi.get_dbus_property(service, 'Security'),
         'strength': self.wifi.get_dbus_property(service, 'Strength'),
@@ -443,25 +495,26 @@ class WiFiThroughput(unittest.TestCase):
         'frequency': self.wifi.get_dbus_property(service, 'WiFi.Frequency')}
 
     # Try to test throughput with iperf if requested.
-    if self.args.iperf_host is not None:
+    if ap_config.iperf_host is not None:
       try:
         factory.console.info('Trying to run iperf on host %s...',
-            self.args.iperf_host)
-        self.log['test'][ssid]['iperf'] = self._RunIperfAndCheckOutput()
+            ap_config.iperf_host)
+        self.log['test'][ap_config.ssid][
+            'iperf'] = self._RunIperfAndCheckOutput(ap_config)
       except Exception as e:
-        self.log['test'][ssid]['failures'].append(e.message)
+        self.log['test'][ap_config.ssid]['failures'].append(e.message)
         factory.console.info('Ran iperf on host %s with error: %s',
-            self.args.iperf_host, e.message)
+            ap_config.iperf_host, e.message)
         logging.exception(e.message)
 
     # Finally, disconnect from this network.
     if not self.wifi.disconnect_service_synchronous(
         service, _DEFAULT_TIMEOUT_SECS):
-      error_str = 'Failed to disconnect from %s' % ssid
-      self.log['test'][ssid]['failures'].append(error_str)
+      error_str = 'Failed to disconnect from %s' % ap_config.ssid
+      self.log['test'][ap_config.ssid]['failures'].append(error_str)
       logging.info(error_str)
     else:
-      logging.info('Disconnected successfully from %s', ssid)
+      logging.info('Disconnected successfully from %s', ap_config.ssid)
 
     # Return success status.
-    return self.log['test'][ssid]['failures'] == []
+    return self.log['test'][ap_config.ssid]['failures'] == []
