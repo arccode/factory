47ed58e676c9020c4a590d4b110a144ba7a88ba8
Hung-Te Lin <hungte@chromium.org>
2014-12-24 17:45:54 +0800

    tpm_verify_ek: Support CrOS Core device with new standalone tools.
    To support verifying TPM endorsement on ChromeOS Core (and those not running
    cryptohome), we should allow specifying device type in test parameter, and use
    the 'tpm-manager' standalone tool if available.
    
    BUG=chrome-os-partner:31800,chromium:415229
    TEST=Manually verified on Link, by setting is_cros_core to True and False.
    CQ-DEPEND=CL:237242,CL:237243
    
    Change-Id: I3e83517ba6430666f7076090d7356578edf20398
    Reviewed-on: https://chromium-review.googlesource.com/237545
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Commit-Queue: Hung-Te Lin <hungte@chromium.org>
    Tested-by: Hung-Te Lin <hungte@chromium.org>

--- py/test/pytests/tpm_verify_ek.py
+++ py/test/pytests/tpm_verify_ek.py
@@ -12,35 +12,42 @@ will fail but emit a reasonable error message (and it will pass on the
 next boot).
 """
 
+import distutils.spawn
 import logging
 import unittest
 
 import factory_common  # pylint: disable=W0611
+from cros.factory.test.args import Arg
 from cros.factory.utils.file_utils import Sync
 from cros.factory.utils.process_utils import Spawn, CheckOutput
 from cros.factory.utils.string_utils import ParseDict
 
 class TPMVerifyEK(unittest.TestCase):
-  def _TPMStatus(self):
-    """Returns TPM status as a dictionary.
+  ARGS = [
+      # Chromebooks and Chromeboxes should set this to False.
+      Arg('is_cros_core', bool, 'Verify with ChromeOS Core endoresement',
+          default=False)
+  ]
 
-    e.g., {'TPM Being Owned': 'false',
-           'TPM Ready': 'true',
-           'TPM Password': 'd641b63ce6ff',
-           'TPM Enabled': 'true',
-           'TPM Owned': 'true'}
-    """
-    status_txt = CheckOutput(['cryptohome', '--action=tpm_status'])
-    status = ParseDict(status_txt.splitlines())
-    logging.info('TPM status: %r', status)
-    return status
+  def VerifyByCryptoHome(self):
+    """Verifies TPM endorsement by CryptoHome service."""
 
-  def runTest(self):
-    # Always clear TPM on next boot, in case any problems arise.
-    Spawn(['crossystem', 'clear_tpm_owner_request=1'],
-          log=True, check_call=True)
+    def _TPMStatus():
+      """Returns TPM status as a dictionary.
 
-    status = self._TPMStatus()
+      e.g., {'TPM Being Owned': 'false',
+             'TPM Ready': 'true',
+             'TPM Password': 'd641b63ce6ff',
+             'TPM Enabled': 'true',
+             'TPM Owned': 'true'}
+      """
+      status_txt = CheckOutput(['cryptohome', '--action=tpm_status'])
+      status = ParseDict(status_txt.splitlines())
+      logging.info('TPM status: %r', status)
+      return status
+
+    # Make sure TPM is enabled.
+    status = _TPMStatus()
     self.assertEquals('true', status['TPM Enabled'])
 
     # Check explicitly for the case where TPM is owned but password is
@@ -61,13 +68,36 @@ class TPMVerifyEK(unittest.TestCase):
     # Sync, to make sure TPM password was written to disk.
     Sync()
 
-    self.assertEquals('true', self._TPMStatus()['TPM Owned'])
+    self.assertEquals('true', _TPMStatus()['TPM Owned'])
 
     # Verify the endorsement key.
-    process = Spawn(['cryptohome', '--action=tpm_verify_ek'],
+    process = Spawn(['cryptohome', '--action=tpm_verify_ek'] + (
+                    ["--cros_core"] if self.args.is_cros_core else []),
                     read_stderr=True,
                     log=True, check_call=True)
     # Make sure there's no stderr from tpm_verify_ek (since that, plus
     # check_call=True, is the only reliable way to make sure it
     # worked).
     self.assertEquals('', process.stderr_data)
+
+  def VerifyByTpmManager(self):
+    """Verifies TPM endorsement by tpm-manager (from CryptoHome package)."""
+
+    # Take ownership of the TPM (if not already taken).
+    Spawn(['tpm-manager', 'initialize'], log=True, check_call=True)
+
+    # Verify TPM endorsement.
+    Spawn(['tpm-manager', 'verify_endorsement'] + (
+          ["--cros_core"] if self.args.is_cros_core else []),
+          log=True, check_call=True)
+
+  def runTest(self):
+    # Always clear TPM on next boot, in case any problems arise.
+    Spawn(['crossystem', 'clear_tpm_owner_request=1'],
+          log=True, check_call=True)
+
+    # Check if we have tpm-manager in system.
+    if distutils.spawn.find_executable('tpm-manager'):
+      self.VerifyByTpmManager()
+    else:
+      self.VerifyByCryptoHome()
