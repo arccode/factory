127e103fa5a5a2554ee0ba8c9f3d420f6d7ff573
Aaron.Chuang <aaron.chuang@quantatw.com>
2014-12-25 16:33:51 +0800

    CHERRY-PICK: [Whale] Support to trigger scanner.
    CQ-DEPEND=CL:237602
    BUG=None
    TEST=Manually test on the Whale fixture.
    
    Reviewed-on: https://chromium-review.googlesource.com/237582
    Reviewed-by: Dean Liao <deanliao@chromium.org>
    Commit-Queue: Aaron Chuang <aaron.chuang@quantatw.com>
    Tested-by: Aaron Chuang <aaron.chuang@quantatw.com>
    Change-Id: I8215c7802b050ed713c64ccd2ca210939d0ddb10
    Reviewed-on: https://chromium-review.googlesource.com/238667
    Commit-Queue: Dean Liao <deanliao@chromium.org>
    Tested-by: Dean Liao <deanliao@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

--- py/test/pytests/barcode_scan_to_file.py
+++ py/test/pytests/barcode_scan_to_file.py
@@ -11,12 +11,17 @@ from __future__ import print_function
 import logging
 import os
 import re
+import time
 import unittest
+
 import factory_common  # pylint: disable=W0611
 from cros.factory.test.args import Arg
 from cros.factory.test import factory
+from cros.factory.test.fixture.whale import whale_bft_fixture
 from cros.factory.test import test_ui
 from cros.factory.test import ui_templates
+
+_CHECK_BARCODE_SECS = 0.3
 
 
 class BarcodeScanToFileTest(unittest.TestCase):
@@ -32,6 +37,10 @@ class BarcodeScanToFileTest(unittest.TestCase):
         'True to ignore case from input.', default=False),
     Arg('save_path', str,
         'The file path of saving barcode'),
+    Arg('bft_params', dict,
+        'Parameters to initialize WhaleBFTFixture. It is a dict which contains '
+        'at least "host" and "port" that points to BeagleBone servod.'
+        optional=True),
   ]
 
   def ShowError(self, message, message_zh):
@@ -79,6 +88,15 @@ class BarcodeScanToFileTest(unittest.TestCase):
 
   def setUp(self):
     self.ui = test_ui.UI()
+    if self.args.bft_params is not None:
+      self._bft = whale_bft_fixture.WhaleBFTFixture()
+      self._bft.Init(**self.args.bft_params)
+    else:
+      self._bft = None
+
+    # clean barcode file first
+    if os.path.exists(self.args.save_path):
+      os.remove(self.args.save_path)
 
   def runTest(self):
     template = ui_templates.OneSection(self.ui)
@@ -106,4 +124,11 @@ class BarcodeScanToFileTest(unittest.TestCase):
          'document.getElementById("scan-value").value)'))
     self.ui.AddEventHandler('scan_value', self.HandleScanValue)
 
-    self.ui.Run()
+    if self._bft:
+      self.ui.Run(blocking=False)
+      while True:
+        self._bft.TriggerScanner()
+        time.sleep(_CHECK_BARCODE_SECS)
+    else:
+      # manual scan
+      self.ui.Run()
