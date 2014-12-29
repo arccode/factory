91f78aa3b589bd8d00f73bb651f850306a425840
Hung-Te Lin <hungte@chromium.org>
2014-12-29 16:22:36 +0800

    Make goofy_control to work on systems without full VT support.
    VT support is not available on all systems (for example, headless and devices
    with Freon enabled). We want to improve the logging system so people can always
    find the log somewhere, and get noticed when the system failed to start.
    
    BUG=chromium:445168
    TEST=Start factory on Freon. Works fine (although VT switching is not available)
         Start factory on Non-Freon boards and everything still works.
    
    Change-Id: I4e00587e04e8280c6aa098e9dba5e1e3dd787dc6
    Reviewed-on: https://chromium-review.googlesource.com/237727
    Tested-by: Hung-Te Lin <hungte@chromium.org>
    Reviewed-by: Ricky Liang <jcliang@chromium.org>
    Commit-Queue: Hung-Te Lin <hungte@chromium.org>

--- sh/goofy_control.sh
+++ sh/goofy_control.sh
@@ -9,6 +9,9 @@ set -e
 
 FACTORY="$(dirname "$(dirname "$(readlink -f "$0")")")"
 FACTORY_LOG_FILE=/var/factory/log/factory.log
+SESSION_LOG_FILE=/var/log/factory_session.log
+INTERACTIVE_CONSOLES=""
+LOG_PROCESSES=""
 
 BOARD_SETUP=("$FACTORY/board/board_setup_factory.sh"
              "$FACTORY/custom/board_setup_factory.sh")
@@ -31,11 +34,65 @@ factory_setup() {
     true
 }
 
+# Clean up when error happens.
+on_error() {
+  local pid
+  # Try to show console because stopping UI may take a while.
+  show_interactive_console
+  stop -n ui >/dev/null 2>&1 || true
+  for pid in ${LOG_PROCESSES}; do
+    kill -9 "${pid}" &
+  done
+  # Show console again because stopping UI may change active console.
+  show_interactive_console
+}
+
+# Initialize output system (create logs and redirect output).
+init_output() {
+  echo "Redirecting output to ${SESSION_LOG_FILE}"
+  exec >"${SESSION_LOG_FILE}" 2>&1
+  echo "New factory session: $(date +'%Y%m%d %H:%M:%s')"
+
+  # When VT is available, TTYs were reserved as:
+  #  1 - UI (Chrome or X)
+  #  2 - getty (login)
+  #  3 - tail -f /var/log/factory.log
+  # So for Goofy session, we want to print the logs in following order:
+  #  - /dev/tty4 if available (Systems with VT)
+  #  - /dev/console if available
+  local tty
+  for tty in /dev/tty4 /dev/console $(tty); do
+    if [ -c "${tty}" ] && (echo "" >>"${tty}") 2>/dev/null; then
+      tail -f "${SESSION_LOG_FILE}" >>"${tty}" &
+      LOG_PROCESSES="${LOG_PROCESSES} $!"
+      INTERACTIVE_CONSOLES="${INTERACTIVE_CONSOLES} ${tty}"
+    fi
+  done
+  trap on_error EXIT
+
+  # This should already exist, but just in case...
+  mkdir -p "$(dirname "$FACTORY_LOG_FILE")"
+  ln -sf "$FACTORY_LOG_FILE" /var/log
+}
+
+# Try to show the interactive console if available.
+show_interactive_console() {
+  local tty
+  local vt_index
+  for tty in ${INTERACTIVE_CONSOLES}; do
+    vt_index="${tty#/dev/tty}"
+    if [ "${vt_index}" = "${tty}" ]; then
+      continue
+    fi
+    chvt "${vt_index}" && return || true
+  done
+}
+
+# Load board-specific parameters, if any.
 load_setup() {
-  # Load board-specific parameters, if any.
   for f in "${BOARD_SETUP[@]}"; do
     if [ -s $f ]; then
-      echo "Loading board-specific parameters $f..." 1>&2
+      echo "Loading board-specific parameters $f..."
       . $f
       break
     fi
@@ -44,11 +101,11 @@ load_setup() {
   if [[ -f ${AUTOMATION_MODE_TAG_FILE} ]]; then
     local mode="$(cat ${AUTOMATION_MODE_TAG_FILE})"
     if [[ -n "${mode}" ]]; then
-      echo "Enable factory test automation with mode: ${mode}" 1>&2
+      echo "Enable factory test automation with mode: ${mode}"
       GOOFY_ARGS="${GOOFY_ARGS} --automation-mode=${mode}"
     fi
     if [[ -f ${STOP_AUTO_RUN_ON_START_TAG_FILE} ]]; then
-      echo "Suppress test list auto-run on start" 1>&2
+      echo "Suppress test list auto-run on start"
       GOOFY_ARGS="${GOOFY_ARGS} --no-auto-run-on-start"
     fi
   fi
@@ -56,23 +113,86 @@ load_setup() {
   factory_setup
 }
 
+# Checks disk usage and abort if running out of disk space.
 check_disk_usage() {
   # Show error in red
-  echo -e "\033[1;31m"
-  "$FACTORY/bin/disk_space" 2>&1 1>/dev/null || exit 1
-  echo -e "\033[0m"
+  printf "\e[1;31m"
+  if "$FACTORY/bin/disk_space"; then
+    printf "\e[0m"
+    return
+  fi
+  echo "
+  /-\_     ___                   ___           --------+ +--------|
+  \_  \    / |______     ________| |_________  |+----+ | | +----+ |
+    \  \  /  _____. \    | .______________. |  ||    | | | |    | |
+     \_/ /  /     | |    | |              | |  ||____| | | |____| |
+         | |      | |    | |              | |  | ____  | |  ____  |
+         | |      | |    |_|  /-|   |-\_  |_|  ||    | | | |    | |
+ .--.   _|/     __| |       _/  /   \_  \_     ||    | | | |    | |
+  \  \ /_ |    /    /     _/  _/      \   \    ||____| | | |____| |
+   \--\  \/    \___/     /   /         \_  \   | ______| |______  |
+                         ---/            \  |  ||               | |
+         ____________                     \/   ||  ._________.  | |
+         \ ._____.  |    ___________________   ||  | _______ |  | |
+    /-+  | |     |  |    |________ ________|   ||  | |     | |  | |
+    | |  \ |     | /             | |           ||  | |_____| |  | |
+   /  /   | \   _| |             | |           ||  |  _____  |  | |
+   | |    \  \ /  /              | |           ||  | |     | |  | |
+  /  /     \  v  /               | |           ||  | |_____| |  | |
+  | /      _>   <                | |           ||  |_________|  | |
+ /  |  ___/  /\  \____   ._______| |________.  ||               | |
+ |-/  /_____/  \______\  |__________________|  |/               \_|
+
+    _   _         ____  _     _      ____                       _ _ _
+   | \ | | ___   |  _ \(_)___| | __ / ___| _ __   __ _  ___ ___| | | |
+   |  \| |/ _ \  | | | | / __| |/ / \___ \| ._ \ / _\ |/ __/ _ \ | | |
+   | |\  | (_) | | |_| | \__ \   <   ___) | |_) | (_| | (_|  __/_|_|_|
+   |_| \_|\___/  |____/|_|___/_|\_\ |____/| .__/ \__,_|\___\___(_|_|_)
+                                          |_|
+  "
+  printf "\e[0m"
+  exit 1
+}
+
+# Initialize system TTY.
+init_tty() {
+  # Preventing ttyN (developer shell console) to go blank after some idle time
+  local tty=""
+  for tty in /dev/tty[2-4]; do
+    (setterm -cursor on -blank 0 -powerdown 0 -powersave off
+     >"${tty}") 2>/dev/null || true
+  done
+}
+
+# Initialize kernel modules and system daemons.
+init_modules() {
+  # We disable powerd in factory image, but this folder is needed for some
+  # commands like power_supply_info to work.
+  mkdir -p /var/lib/power_manager
+
+  # Preload modules here
+  modprobe i2c-dev 2>/dev/null || true
+}
+
+# Initialize firewall settings.
+init_firewall() {
+  # Open ports in the firewall so that the presenter can reach us
+  # Note we want these ports to be expanded as a list, and so are unquoted
+  local port=
+  for port in $GOOFY_LINK_PORTS $GOOFY_UI_PORT; do
+    /sbin/iptables -A INPUT -p tcp --dport ${port} -j ACCEPT
+  done
 }
 
 # http://crbug.com/410233: If TPM is owned, UI may get freak.
-ensure_tpm_not_owned() {
+check_tpm() {
   if [ "$(crossystem mainfw_type 2>/dev/null)" = "nonchrome" ] ||
      [ "$(cat /sys/class/misc/tpm0/device/owned 2>/dev/null)" != "1" ]; then
     return
   fi
   # If TPM is owned, we have to reboot otherwise UI may get freak.
   # Alert user and try to clear TPM.
-  local tty=/dev/tty5
-  chvt 5 || tty=/dev/console
+  stop -n ui >/dev/null 2>&1 &
   echo "
         Sorry, you must clear TPM owner before running factory UI.
         We are going to do that for you (and then reboot) in 10 seconds.
@@ -80,76 +200,72 @@ ensure_tpm_not_owned() {
         If you want to abort, do Ctrl-Alt-F2, login, and run
 
           stop factory
-
-       " >"$tty"
+       "
+  show_interactive_console
   for i in $(seq 10 -1 0); do
-    echo " > Clear & reboot in ${i} seconds..." >"$tty"
+    echo " > Clear & reboot in ${i} seconds..."
     sleep 1
   done
 
   crossystem clear_tpm_owner_request=1
-  echo "Restarting system..." >"$tty"
+  echo "Restarting system..."
   reboot
   # Wait forever.
   sleep 1d
 }
 
 start_factory() {
-  # This should already exist, but just in case...
-  mkdir -p "$(dirname "$FACTORY_LOG_FILE")"
-  ln -sf "$FACTORY_LOG_FILE" /var/log
+  init_output
 
-  load_setup
   echo "
     Starting factory program...
 
     If you don't see factory window after more than one minute,
     try to switch to VT2 (Ctrl-Alt-F2), log in, and check the messages by:
-      tail $FACTORY_LOG_FILE
+      tail $SESSION_LOG_FILE $FACTORY_LOG_FILE
 
     If it keeps failing, try to reset by:
       factory_restart -a
   "
 
-  # We disable powerd in factory image, but this folder is needed for some
-  # commands like power_supply_info to work.
-  mkdir -p /var/lib/power_manager
+  load_setup
 
-  # Preload modules here
-  modprobe i2c-dev 2>/dev/null || true
+  init_modules
+  init_tty
+  init_firewall
+
+  check_tpm
   check_disk_usage
 
-  ensure_tpm_not_owned
-
-  # Open ports in the firewall so that the presenter can reach us
-  # Note we want these ports to be expanded as a list, and so are unquoted
-  local port=
-  for port in $GOOFY_LINK_PORTS $GOOFY_UI_PORT; do
-    /sbin/iptables -A INPUT -p tcp --dport ${port} -j ACCEPT
-  done
-
   if [ -z "$(status ui | grep start)" ]; then
+    echo "Request to start UI..."
     start -n ui &
-  fi
-
-  if [ -f "${RUN_GOOFY_PRESENTER_TAG_FILE}" ] && \
-     [ -f "${RUN_GOOFY_DEVICE_TAG_FILE}" ]; then
-    GOOFY_ARGS="${GOOFY_ARGS} --standalone"
-    PRESENTER_ARGS="${PRESENTER_ARGS} --standalone"
   fi
 
   export DISPLAY=":0"
   export XAUTHORITY="/home/chronos/.Xauthority"
 
-  # Run goofy_presenter if goofy_presenter tag file is present
+  if [ -f "${RUN_GOOFY_PRESENTER_TAG_FILE}" ] &&
+     [ -f "${RUN_GOOFY_DEVICE_TAG_FILE}" ]; then
+    GOOFY_ARGS="${GOOFY_ARGS} --standalone"
+    PRESENTER_ARGS="${PRESENTER_ARGS} --standalone"
+  fi
+
+  # Run goofy_presenter if goofy_presenter tag file is present.
+  # Note presenter output is only kept in SESSION_LOG_FILE.
   if [ -f "${RUN_GOOFY_PRESENTER_TAG_FILE}" ]; then
-    "$FACTORY/bin/goofy_presenter" $PRESENTER_ARGS >>"$FACTORY_LOG_FILE" 2>&1 &
+    echo "Starting Goofy Presenter... ($PRESENTER_ARGS)"
+    "$FACTORY/bin/goofy_presenter" $PRESENTER_ARGS &
   fi
 
   # Run goofy(device) if the goofy_device tag file is present,
   # or goofy_presenter tag file is missing
   if [ -f "${RUN_GOOFY_DEVICE_TAG_FILE}" \
        -o ! -f "${RUN_GOOFY_PRESENTER_TAG_FILE}" ]; then
+    echo "Starting Goofy Device... ($GOOFY_ARGS)"
+    echo "
+    --- $(date +'%Y%m%d %H:%M:%S') Starting new Goofy session ($GOOFY_ARGS) ---
+         " >>"$FACTORY_LOG_FILE"
     "$FACTORY/bin/goofy" $GOOFY_ARGS >>"$FACTORY_LOG_FILE" 2>&1 &
   fi
 
@@ -157,7 +273,6 @@ start_factory() {
 }
 
 stop_factory() {
-  load_setup
   # Try to kill X, and any other Python scripts, five times.
   echo -n "Stopping factory."
   for i in $(seq 5); do
@@ -169,7 +284,7 @@ stop_factory() {
   echo "
 
     Factory tests terminated. To check error messages, try
-      tail $FACTORY_LOG_FILE
+      tail ${SESSION_LOG_FILE} ${FACTORY_LOG_FILE}
 
     To restart, press Ctrl-Alt-F2, log in, and type:
       factory_restart
@@ -179,17 +294,21 @@ stop_factory() {
     "
 }
 
-case "$1" in
-  "start" )
-    start_factory "$@"
-    ;;
+main() {
+  case "$1" in
+    "start" )
+      start_factory "$@"
+      ;;
 
-  "stop" )
-    stop_factory "$@"
-    ;;
+    "stop" )
+      stop_factory "$@"
+      ;;
 
-  * )
-    echo "Usage: $0 [start|stop]"
-    exit 1
-    ;;
-esac
+    * )
+      echo "Usage: $0 [start|stop]" >&2
+      exit 1
+      ;;
+  esac
+}
+
+main "$@"
