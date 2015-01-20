4f59d593324b012e26c34f79d2bdf3b2a4803fc0
Wei-Ning Huang <wnhuang@google.com>
2015-01-20 18:42:56 +0800

    led: implement challenge test
    Implement a challenge LED test so the operaters have to check the colors of
    LED instead of blindly pressing enter to pass the test.
    
    BUG=chrome-os-partner:34030
    TEST=run led test with challenge=True
    
    Change-Id: I175ef5f1166f03c5f8c23e5d5477b220f696f44e
    Reviewed-on: https://chromium-review.googlesource.com/241940
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Commit-Queue: Wei-Ning Huang <wnhuang@chromium.org>
    Tested-by: Wei-Ning Huang <wnhuang@chromium.org>

--- py/test/pytests/led/led.py
+++ py/test/pytests/led/led.py
@@ -10,6 +10,8 @@ or SMT fixture confirm LED functionality.
 
 from collections import namedtuple
 import logging
+import random
+import time
 import unittest
 
 import factory_common  # pylint: disable=W0611
@@ -32,6 +34,7 @@ _TEST_TITLE = test_ui.MakeLabel('LED Test', u'LED 测试')
 
 # True to test all colors regardless of failure.
 _FAIL_LATER = True
+_SHOW_RESULT_SECONDS = 0.5
 
 I18nLabel = namedtuple('I18nLabel', 'en zh')
 LEDColor = Board.LEDColor
@@ -49,6 +52,68 @@ _INDEX_LABEL = {
     LEDIndex.BATTERY: I18nLabel('battery ', u'电池 '),
     LEDIndex.ADAPTER: I18nLabel('adapter ', u'电源适配器 ')}
 
+_COLOR_CODE = {
+    LEDColor.YELLOW: ('#ffff00', 'black'),
+    LEDColor.GREEN: ('#00ff00', 'black'),
+    LEDColor.RED: ('#ff0000', 'black'),
+    LEDColor.WHITE: ('#ffffff', 'black'),
+    LEDColor.BLUE: ('#0000ff', 'white'),
+    LEDColor.OFF: ('#000000', 'white')}
+
+_JS_OP_RESPONSE = """
+function UpdateResult(result) {
+  res = $('result');
+  if (result) {
+    res.innerHTML = '<span class="result-pass">PASS</span>';
+  } else {
+    res.innerHTML = '<span class="result-fail">FAIL</span>';
+  }
+}
+"""
+
+_HTML_KEY_TEMPLATE = """
+<span class="led-btn" style="background-color: %s; color: %s">%d</span>
+"""
+
+_HTML_RESULT = (
+    '<div class="result-line">' +
+    test_ui.MakeLabel("Result: ", u"测试结果: ") +
+    '<span id="result"></span></div>')
+
+_CSS_ITERACTIVE = """
+.sub-title {
+  font-size: 200%;
+  font-weight: bold;
+  line-height: 100px;
+}
+
+.led-btn {
+  display: inline-block;
+  width: 50px;
+  height: 50px;
+  line-height: 50px;
+  font-size: 200%;
+  padding: 5px;
+  margin: 3px;
+  border-radius: 3px;
+  font-weight: bold;
+  border: 1px solid #7D7D7D;
+}
+
+.result-line {
+  margin-top: 30px;
+}
+
+.result-pass {
+  color: green;
+  font-weight: bold;
+}
+
+.result-fail {
+  color: red;
+  font-weight: bold;
+}
+"""
 
 class CheckLEDTask(InteractiveFactoryTask):
   """An InteractiveFactoryTask that ask operator to check LED color.
@@ -57,16 +122,23 @@ class CheckLEDTask(InteractiveFactoryTask):
     ui: test_ui.UI instance.
     template: ui_templates.OneSection() instance.
     board: Board instance to control LED.
+    nth: The number of task.
     color: LEDColor to inspect.
-    index: target LED to inspect. None means default LED.
+    index: Target LED to inspect. None means default LED.
+    challenge: Whether this is a challenge test or not.
+    colors: The sequence of LEDColor in arg.colors.
   """
 
-  def __init__(self, ui, template, board, color, index):
+  def __init__(self, ui, template, board, nth, color, index, challenge=False,
+               colors=None):
     super(CheckLEDTask, self).__init__(ui)
     self._template = template
     self._board = board
+    self._nth = nth
     self._color = color
     self._index = index
+    self._challenge = challenge
+    self._colors = colors
 
   def Run(self):
     """Lights LED in color and asks operator to verify it."""
@@ -78,6 +150,12 @@ class CheckLEDTask(InteractiveFactoryTask):
 
   def _InitUI(self):
     """Sets instructions and binds pass/fail key."""
+    if not self._challenge:
+      self._InitUINormal()
+    else:
+      self._InitUIChallange()
+
+  def _InitUINormal(self):
     index_label = _INDEX_LABEL[self._index]
     if self._color == LEDColor.OFF:
       instruction = test_ui.MakeLabel(
@@ -93,6 +171,40 @@ class CheckLEDTask(InteractiveFactoryTask):
     self._template.SetState(instruction)
 
     self.BindPassFailKeys(fail_later=_FAIL_LATER)
+
+  def _InitUIChallange(self):
+    index_label = _INDEX_LABEL[self._index]
+
+    desc = test_ui.MakeLabel(
+        '<span class="sub-title">Test %d</span><br />'
+        'Please press number key according to the %sLED color'
+        % (self._nth, index_label.en),
+        u'<span class="sub-title">测试 %d</span><br />'
+        u'请根据 %sLED 的颜色按下数字键' % (self._nth, index_label.zh))
+
+    btn_ui = ''.join([_HTML_KEY_TEMPLATE % (_COLOR_CODE[c] + (j + 1,))
+                      for j, c in enumerate(self._colors)])
+
+    ui = [desc, '<br /><br />', btn_ui, _HTML_RESULT]
+
+    def Pass(_):
+      self._ui.CallJSFunction("UpdateResult", True)
+      time.sleep(_SHOW_RESULT_SECONDS)
+      self.Pass()
+
+    def Fail(_):
+      self._ui.CallJSFunction("UpdateResult", False)
+      time.sleep(_SHOW_RESULT_SECONDS)
+      self.Fail('LED color incorrect or wrong button pressed')
+
+    target = self._colors.index(self._color)
+
+    for i, _ in enumerate(self._colors):
+      self._ui.BindKey(str(i + 1), Pass if i == target else Fail)
+
+    self._ui.AppendCSS(_CSS_ITERACTIVE)
+    self._template.SetState(''.join(ui))
+    self._ui.RunJS(_JS_OP_RESPONSE)
 
 
 class FixtureCheckLEDTask(FactoryTask):
@@ -138,6 +250,8 @@ class LEDTest(unittest.TestCase):
   """Tests if the onboard LED can light yellow/green/red colors."""
   ARGS = [
       Arg('bft_fixture', dict, TEST_ARG_HELP, optional=True),
+      Arg('challenge', bool, 'Show random LED sequence and let the operator '
+          'select LED number instead of pre-defined sequence.', default=False),
       Arg(
           'colors', (list, tuple),
           'List of colors or (index, color) to test. color must be in '
@@ -172,7 +286,13 @@ class LEDTest(unittest.TestCase):
     self._template.SetTitle(_TEST_TITLE)
 
     tasks = []
-    for index_color in self.args.colors:
+    colors = [x if isinstance(x, str) else x[1] for x in self.args.colors]
+
+    # Shuffle the colors so operators can't guess the sequence.
+    if self.args.challenge:
+      random.shuffle(self.args.colors)
+
+    for i, index_color in enumerate(self.args.colors):
       if isinstance(index_color, str):
         color = index_color
         index = None
@@ -183,8 +303,8 @@ class LEDTest(unittest.TestCase):
         tasks.append(FixtureCheckLEDTask(self._fixture, self._board, color,
                                          index))
       else:
-        tasks.append(CheckLEDTask(self._ui, self._template, self._board, color,
-                                  index))
+        tasks.append(CheckLEDTask(self._ui, self._template, self._board, i + 1,
+                                  color, index, self.args.challenge, colors))
 
     self._task_manager = FactoryTaskManager(self._ui, tasks)
     self._task_manager.Run()
@@ -197,7 +317,7 @@ class LEDTest(unittest.TestCase):
       color: One of Board.LEDColor.
     """
     if not self._board:
-      # Sanity check. It shoud not happen.
+      # Sanity check. It should not happen.
       return
 
     if not leds:
