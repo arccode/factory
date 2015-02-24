1d810fba2192ef658f8f253f99eff55a806e95c5
Chun-Ta Lin <itspeter@google.com>
2015-02-24 04:56:06 -0800

    Generic RPC server for non Brillo device
    When integrating with different test program in different platform, we
    need somehow exchange information between them and Brillo factory. The
    RPC server defines concise interface that protocol can be further
    implemented by partner.
    
    BUG=chrome-os-partner:33765
    TEST=Manually on DUT and Windows machine.
    TEST=make lint LINT_WHITELIST="py/rf/generic_interactive_rpc_server.py"
    
    Original Change-Id: Ied9dd08f54127a98c07dc6b790c4f98d9a3c334a
    Original Reviewed-on: https://chrome-internal-review.googlesource.com/192757
    Original Reviewed-by: Chun-ta Lin <itspeter@google.com>
    Original Commit-Queue: Chun-ta Lin <itspeter@google.com>
    Original Trybot-Ready: Chun-ta Lin <itspeter@google.com>
    Original Tested-by: Chun-ta Lin <itspeter@google.com>
    
    Change-Id: I3195a7dfb88b278297491257ab3bbaa74e794c1c
    Reviewed-on: https://chromium-review.googlesource.com/252670
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Tested-by: Chun-ta Lin <itspeter@chromium.org>
    Commit-Queue: Chun-ta Lin <itspeter@chromium.org>
    Trybot-Ready: Chun-ta Lin <itspeter@chromium.org>

---
+++ py/rf/generic_interactive_rpc_server.py
@@ -0,0 +1,57 @@
+# Copyright 2015 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging
+import SimpleXMLRPCServer
+
+# port for the listening the RPC call
+INTERACTIVE_RPC_SERVER_PORT = 7601
+
+
+class GenericInteractiveRpcServer(object):
+  """A simple RPC server interacts with other platform.
+
+  Partner is expected to inherit this and implement the Execute() function.
+  """
+  def __init__(self, port):
+    self._message = 'Initialing RPC Server'
+    logging.info('Start DUT server.')
+    server = SimpleXMLRPCServer.SimpleXMLRPCServer(
+        ('0.0.0.0', port), allow_none=True)
+    server.register_introspection_functions()
+    server.register_function(self.GetStatus)
+    server.register_function(self.SetStatus)
+    server.register_function(self.Execute)
+    # Start the server thread
+    server.serve_forever()
+
+  def GetStatus(self):
+    return self._message
+
+  def SetStatus(self, message):
+    self._message = message
+    return True
+
+  def Execute(self, cmd_type, payload):
+    # TODO(partner): Determine what action to take based on cmd_type
+    # TODO(partner): Extract the info from payload for additional message
+    # TODO(partner): Excute the external binary.
+    # TODO(partner): Wrap the output in JSON format and return.
+
+    # A simple example:
+    #   Remote on the DUT side is calling to list files under root directory.
+    #   d = xmlrpclib.ServerProxy('http://localhost:7601')
+    #   print d.Execute('LIST', {'path': '/'})
+    #
+    # Coressponding snippet code here could be:
+    # if cmd_type != 'LIST':
+    #   raise ValueError('Unrecognized cmd_type %s' % cmd_type)
+    # ret = subprocess.check_output(['dir', payload.get('path')])
+    # logging.info(json.dumps(ret))
+    # return json.dumps(ret)
+    raise NotImplementedError
+
+if __name__ == '__main__':
+  logging.basicConfig(level=logging.DEBUG)
+  GenericInteractiveRpcServer(INTERACTIVE_RPC_SERVER_PORT)
