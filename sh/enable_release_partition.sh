8d3c7611da4e896bd89fdac77ef4f8cab36f5c98
bowgotsai <bowgotsai@chromium.org>
2015-03-10 10:15:36 +0800

    prepare_wipe: Move enable/disable kernel to another script
    prepare_wipe installs wipe_splash.png and tag file
    'factory_install_reset' to stateful partition, then enables release
    kernel and disables factory kernel to start wiping on next boot.
    
    As we invoke factory wiping directly without reboot, the wipe_splash.png
    and tag file won't be required in stateful partition. But we still need
    to switch active kernel. The CL moves this part to another script
    enable_release_partition.sh for in-place wiping. Then prepare_wipe.sh
    invokes enable_release_partition.sh for backward compatibility.
    
    BUG=chromium:427793
    TEST=Manual test on Paine:
         /usr/local/factory/sh/enable_release_partition.sh "/dev/sda5"
         /usr/local/factory/sh/prepare_wipe.sh "/dev/sda5"
    
    Change-Id: Id4d790f17a3cbaa9c5c9237948bdfe1e8f9fbbc3
    Reviewed-on: https://chromium-review.googlesource.com/257318
    Tested-by: Bowgo Tsai <bowgotsai@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Commit-Queue: Bowgo Tsai <bowgotsai@chromium.org>

---
+++ sh/enable_release_partition.sh
@@ -0,0 +1,86 @@
+#!/bin/sh
+# Copyright 2015 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# This script performs following tasks to switch active partition:
+# - enable release partition and disable factory release partition
+# - (legacy firmware) perform "postinst" to configure legacy boot loaders
+# - rollback if anything goes wrong
+
+. "$(dirname "$0")/common.sh" || exit 1
+set -e
+
+# Kernel activation parameters
+ENABLED_KERNEL_PRIORITY="3"
+
+# Variables for cleaning up
+NEED_ROLLBACK=""
+
+# usage: enable_kernel partition_no
+enable_kernel() {
+  alert "Enabling kernel on $CGPT_DEVICE #$1 ..."
+  cgpt add -i "$1" -P "$ENABLED_KERNEL_PRIORITY" -S 1 -T 0 "$CGPT_DEVICE"
+  cgpt prioritize -i "$1" -P "$ENABLED_KERNEL_PRIORITY" "$CGPT_DEVICE"
+}
+
+# usage: disable_kernel partition_no
+disable_kernel() {
+  alert "Disabling kernel on $CGPT_DEVICE #$1 ..."
+  cgpt add -i "$1" -P 0 -S 0 -T 0 "$CGPT_DEVICE"
+}
+
+rollback_changes() {
+  # don't stop, even if we encounter any issues
+  local failure_msg="WARNING: Failed to rollback some changes..."
+  alert "WARNING: Rolling back changes."
+  crossystem disable_dev_request=0 || alert "$failure_msg"
+  if [ -n "$CGPT_CONFIG" ]; then
+    cgpt_restore_status "$CGPT_DEVICE" "$CGPT_CONFIG" || alert "$failure_msg"
+  fi
+}
+
+cleanup() {
+  if [ -n "$NEED_ROLLBACK" ]; then
+    rollback_changes
+  fi
+}
+
+main() {
+  if [ "$#" != "1" ]; then
+    alert "Usage: $0 release_rootfs"
+    exit 1
+  fi
+
+  local release_rootfs="$1"
+  local factory_rootfs="$(echo "$release_rootfs" | tr '35' '53')"
+  [ "$release_rootfs" != "$factory_rootfs" ] ||
+    die "Unknown type of device: $release_rootfs"
+  cgpt_init "$(device_remove_partno "$release_rootfs")" ||
+    die "Failed to initialize device: $release_rootfs"
+
+  local release_partno="$(echo "$release_rootfs" | sed -r 's/.*([0-9])$/\1/')"
+  local factory_partno="$(echo "$factory_rootfs" | sed -r 's/.*([0-9])$/\1/')"
+  [ -n "$release_partno" -a -n "$factory_partno" ] ||
+    die "Failed to identify release/factory partition numbers: $release_rootfs"
+
+  NEED_ROLLBACK="YES"
+  if chromeos_is_legacy_firmware; then
+    # When booting with legacy firmware, we need to update the legacy boot
+    # loaders to activate new kernel; on a real ChromeOS firmware, only CGPT
+    # header is used, and postinst is already performed in verify_rootfs.
+    alert "Running on legacy system, invoke postinst."
+    chromeos_invoke_postinst "$release_rootfs"
+  fi
+  disable_kernel "$(( factory_partno - 1 ))"
+  enable_kernel "$(( release_partno - 1 ))"
+  crossystem disable_dev_request=1
+  alert "Syncing disks..."
+  sync
+  sleep 3  # For sync to take place.
+  NEED_ROLLBACK=""
+  alert "Enable release partition: Complete."
+}
+
+trap cleanup EXIT
+main "$@"
--- sh/prepare_wipe.sh
+++ sh/prepare_wipe.sh
@@ -1,55 +1,34 @@
 #!/bin/sh
-# Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+# Copyright 2015 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
 # This script performs following tasks to prepare a wiping process at reboot:
 # - install "wiping" splash image and tag file
-# - enable release partition and disable factory release partition
-# - (legacy firmware) perform "postinst" to configure legacy boot loaders
+# - enable release partition
 # - rollback if anything goes wrong
 # To assign additional wiping tags, use FACTORY_WIPE_TAGS envinronment variable.
-# Ex: FACTORY_WIPE_TAGS="fast" gft_prepare_wipe.sh /dev/sda5
+# Ex: FACTORY_WIPE_TAGS="fast" prepare_wipe.sh /dev/sda5
 
 . "$(dirname "$0")/common.sh" || exit 1
 set -e
-
-# Kernel activation parameters
-ENABLED_KERNEL_PRIORITY="3"
 
 # Variables for cleaning up
 NEED_ROLLBACK=""
 
 # Location for splash and tag files
-SCRIPT_DIR="$(dirname $0)"
+SCRIPT_DIR="$(dirname "$0")"
 STATEFUL_PARTITION="/mnt/stateful_partition"
 WIPE_TAG_FILE="$STATEFUL_PARTITION/factory_install_reset"
 SPLASH_FILE="$STATEFUL_PARTITION/wipe_splash.png"
 SPLASH_SOURCE="$SCRIPT_DIR/../misc/wipe_splash.png"
 TAGS="factory"
 
-# usage: enable_kernel partition_no
-enable_kernel() {
-  alert "Enabling kernel on $CGPT_DEVICE #$1 ..."
-  cgpt add -i "$1" -P "$ENABLED_KERNEL_PRIORITY" -S 1 -T 0 "$CGPT_DEVICE"
-  cgpt prioritize -i "$1" -P "$ENABLED_KERNEL_PRIORITY" "$CGPT_DEVICE"
-}
-
-# usage: disable_kernel partition_no
-disable_kernel() {
-  alert "Disabling kernel on $CGPT_DEVICE #$1 ..."
-  cgpt add -i "$1" -P 0 -S 0 -T 0 "$CGPT_DEVICE"
-}
-
 rollback_changes() {
   # don't stop, even if we encounter any issues
   local failure_msg="WARNING: Failed to rollback some changes..."
   alert "WARNING: Rolling back changes."
-  crossystem disable_dev_request=0 || alert "$failure_msg"
   rm -f "$WIPE_TAG_FILE" 2>/dev/null || alert "$failure_msg"
-  if [ -n "$CGPT_CONFIG" ]; then
-    cgpt_restore_status "$CGPT_DEVICE" "$CGPT_CONFIG" || alert "$failure_msg"
-  fi
 }
 
 cleanup() {
@@ -83,36 +62,12 @@ main() {
     exit 1
   fi
 
-  local release_rootfs="$1"
-  local factory_rootfs="$(echo "$release_rootfs" | tr '35' '53')"
-  [ "$release_rootfs" != "$factory_rootfs" ] ||
-    die "Unknown type of device: $release_rootfs"
-  cgpt_init "$(device_remove_partno "$release_rootfs")" ||
-    die "Failed to initialize device: $release_rootfs"
-
-  local release_partno="$(echo "$release_rootfs" | sed -r 's/.*([0-9])$/\1/')"
-  local factory_partno="$(echo "$factory_rootfs" | sed -r 's/.*([0-9])$/\1/')"
-  [ -n "$release_partno" -a -n "$factory_partno" ] ||
-    die "Failed to identify release/factory partition numbers: $release_rootfs"
-
   NEED_ROLLBACK="YES"
   install_splash
   install_wipe_tag
-  if chromeos_is_legacy_firmware; then
-    # When booting with legacy firmware, we need to update the legacy boot
-    # loaders to activate new kernel; on a real ChromeOS firmware, only CGPT
-    # header is used, and postinst is already performed in gft_verify_rootfs.
-    alert "Running on legacy system, invoke postinst."
-    chromeos_invoke_postinst "$release_rootfs"
-  fi
-  disable_kernel "$(( factory_partno - 1 ))"
-  enable_kernel "$(( release_partno - 1 ))"
-  crossystem disable_dev_request=1
-  alert "Syncing disks..."
-  sync
-  sleep 3  # For sync to take place.
+  . "$SCRIPT_DIR/enable_release_partition.sh" "$1" || exit 1
   NEED_ROLLBACK=""
-  alert "Complete."
+  alert "Prepare wipe: Complete."
 }
 
 trap cleanup EXIT
