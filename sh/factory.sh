492437c7e4ba78b868e23378765cc351a973c9dc
bowgotsai <bowgotsai@chromium.org>
2015-04-08 14:50:34 +0800

    "factory" tool: loads board setup script before starting factory actions.
    The "factory" tool is useful for miscellaneous factory actions,
    ex: shows device data dictionary, displays test lists, etc.
    The current script doesn't load the board setup script so board specific
    class (/usr/local/factory/py/board/xxx_board.py) won't be used due to
    lack of CROS_FACTORY_BOARD_CLASS. This will introduce command errors in
    some projects because some dependencies in the default implementation
    'chromeos_board.py' cannot be met, ex: function GetTemperatureSensorNames(),
    GetMainTemperatureIndex(), etc. It needs to invoke board-specific functions
    defined in xxx_board.py instead of that in chromeos_board.py.
    
    The commit adds a new bash script factory.sh to load the board setup script
    for CROS_FACTORY_BOARD_CLASS before invoking factory.py for miscellaneous
    factory actions. It also changes the symlink ./bin/factory to point to the
    newly added factory.sh.
    
    BUG=chrome-os-partner:38102
    TEST=Run 'factory test-list --list' on Guado, check it can load
         cros.factory.board.guado_board.GuadoBoard as the board class.
    
    Change-Id: If75cb8bf3be5a66096704ae7e3c32c7320dbd28d
    Reviewed-on: https://chromium-review.googlesource.com/264631
    Tested-by: Bowgo Tsai <bowgotsai@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Commit-Queue: Bowgo Tsai <bowgotsai@chromium.org>

---
+++ sh/factory.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Copyright 2015 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# This scripts loads the board setup script first then invokes
+# factory.py for miscellaneous factory actions.
+
+FACTORY_DIR="$(dirname "$(dirname "$(readlink -f "$0")")")"
+BOARD_SETUP_SCRIPT="${FACTORY_DIR}/board/board_setup_factory.sh"
+
+# Load the board setup script to get CROS_FACTORY_BOARD_CLASS.
+if [ -s "${BOARD_SETUP_SCRIPT}" ]; then
+  echo "Loading board-specific parameters ${BOARD_SETUP_SCRIPT}..."
+  . "${BOARD_SETUP_SCRIPT}"
+fi
+
+"${FACTORY_DIR}/py/tools/factory.py" "$@"
