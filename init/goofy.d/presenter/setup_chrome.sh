2d9bbe1cbb4af7cdfb12b497418097bdc37873b8
Hung-Te Lin <hungte@chromium.org>
2015-04-23 16:20:58 +0800

    init: Add debug-friendly features.
    In factory environment, we usually don't want Chrome to restart automatically
    (which may flash several times and then reboot due to exceeding re-spawn limit).
    Also sometimes we will want Chrome crash logs.
    
    BUG=none
    TEST=manually started factory UI.
    
    Change-Id: I0b85e33616b1a0ea76162e64dd80698742268fb4
    Reviewed-on: https://chromium-review.googlesource.com/266867
    Tested-by: Hung-Te Lin <hungte@chromium.org>
    Reviewed-by: Wei-Ning Huang <wnhuang@chromium.org>
    Commit-Queue: Hung-Te Lin <hungte@chromium.org>

--- init/goofy.d/presenter/setup_chrome.sh
+++ init/goofy.d/presenter/setup_chrome.sh
@@ -8,3 +8,10 @@ mount --bind "$(dirname $0)/chrome_dev.conf" /etc/chrome_dev.conf
 
 # For factory environment, we don't need to preserve Chrome session data.
 mount -t tmpfs none /home/chronos
+
+# Enable all debug-friendly features, by information from following page:
+# http://www.chromium.org/chromium-os/how-tos-and-troubleshooting/debugging-tips
+touch /var/run/disable_chrome_restart
+# Chromium dumps may fill up system space in early protos so we want to do it
+# manually. Uncomment the line below if you need to get dumps.
+# touch /mnt/stateful_partition/etc/enable_chromium_coredumps
