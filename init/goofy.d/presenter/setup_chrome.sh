71dd14db533c1576333bebdd16253e6320a834cc
Hung-Te Lin <hungte@chromium.org>
2015-05-04 17:44:03 +0800

    factory: Fix "ToT Chrome does not start on Factory images".
    The "disable_chrome_restart flag" does not work properly on ToT if the file is
    created before Chrome starts.
    
    BUG=none
    TEST=Manually boots a factory test image.
    
    Change-Id: Ia97fb53ee0a57300035041ea1535852de59e3ffd
    Reviewed-on: https://chromium-review.googlesource.com/269076
    Tested-by: Hung-Te Lin <hungte@chromium.org>
    Reviewed-by: Wei-Ning Huang <wnhuang@chromium.org>
    Commit-Queue: Hung-Te Lin <hungte@chromium.org>

--- init/goofy.d/presenter/setup_chrome.sh
+++ init/goofy.d/presenter/setup_chrome.sh
@@ -11,7 +11,9 @@ mount -t tmpfs none /home/chronos
 
 # Enable all debug-friendly features, by information from following page:
 # http://www.chromium.org/chromium-os/how-tos-and-troubleshooting/debugging-tips
-touch /var/run/disable_chrome_restart
+# On ToT, having disable_chrome_restart before job 'ui' starts would block
+# Chrome to execute. Please manually do the line below after Chrome starts.
+# touch /var/run/disable_chrome_restart
 # Chromium dumps may fill up system space in early protos so we want to do it
 # manually. Uncomment the line below if you need to get dumps.
 # touch /mnt/stateful_partition/etc/enable_chromium_coredumps
