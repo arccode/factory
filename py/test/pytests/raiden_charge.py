8fff4cd0956eb9e970b277ffa8bdcf60bd9668ab
Johny Lin <johnylin@chromium.org>
2015-05-14 15:08:54 +0800

    [Dolphin] raiden charge test of adb-target scenario
    Add an argument to choose from testing locally or remote ADB
    connection manner.
    
    As for adb-target scenario, the test is ran by host to test raiden port
    on remote device (Android-based machine). Dolphin is controlled by host
    and charge current information on remote device is obtained via adb
    connection.
    
    There is a limit that adb device cannot be connected during discharging,
    so only INA information on Plankton is considered.
    
    BUG=None
    TEST=manual test.
      1. test local scenario by samus.
      2. test adb-target scenario with fixture-based scheme. Use
          auron_yuna as host and make connection to ryu (Android
          image) via Dolphin fixture.
    CQ-DEPEND=CL:*216327
    
    Change-Id: I760fdb8f72591a138db3f5a560897b40d9cd688f
    Reviewed-on: https://chromium-review.googlesource.com/271196
    Reviewed-by: Pin-chih Lin <johnylin@chromium.org>
    Commit-Queue: Pin-chih Lin <johnylin@chromium.org>
    Tested-by: Pin-chih Lin <johnylin@chromium.org>

--- py/test/pytests/raiden_charge.py
+++ py/test/pytests/raiden_charge.py
@@ -6,9 +6,11 @@
 
 """Test Raiden USB type-C port charging function.
 
-A simple battery test that with Plankton-Raiden control, we verify charge-5V,
-charge-12V, charge-20V, and discharge. This test also takes INA current value on
-Plankton Raiden board as a judgement.
+This test can be tested locally or in remote ADB connection manner.
+
+Use Plankton-Raiden board control to verify device battery current. It supports
+charge-5V, charge-12V, charge-20V, and discharge. This test also takes INA
+current and voltage value on Plankton-Raiden board into account as a judgement.
 """
 
 import logging
@@ -19,7 +21,6 @@ import factory_common  # pylint: disable=W0611
 
 from cros.factory import system
 from cros.factory.system.board import Board
-from cros.factory.system import power
 from cros.factory.test import factory
 from cros.factory.test import test_ui
 from cros.factory.test import ui_templates
@@ -29,6 +30,12 @@ from cros.factory.test.fixture import bft_fixture
 from cros.factory.utils import time_utils
 
 _TEST_TITLE = test_ui.MakeLabel('Raiden Charging Test', u'Raiden 充电测试')
+_TESTING_ADB_CONNECTION = test_ui.MakeLabel(
+    'Waiting for ADB device connection...',
+    u'等待机器连线...')
+_TESTING_PROTECT = test_ui.MakeLabel(
+    'Checking Plankton INA current for protection...',
+    u'检查电流保护...')
 _TESTING_CHARGE = lambda v: test_ui.MakeLabel(
     'Testing battery %dV charging...' % v,
     u'测试电池 %dV 充电中...' % v)
@@ -99,8 +106,8 @@ class RaidenChargeBFTTest(unittest.TestCase):
           default=0.12)
   ]
 
-  # Supported charging voltage
-  _SUPPORT_CHARGE_VOLT = [5, 12, 20]
+  _SUPPORT_CHARGE_VOLT = [5, 12, 20]  # supported charging voltage
+  _DISCHARGE_VOLT = 5  # discharging voltage
 
   def setUp(self):
     self.VerifyArgs()
@@ -108,10 +115,15 @@ class RaidenChargeBFTTest(unittest.TestCase):
     self._template = ui_templates.OneSection(self._ui)
     self._template.SetTitle(_TEST_TITLE)
     self._board = system.GetBoard()
-    self._power = power.Power()
+    self._power = self._board.power
     self._bft_fixture = bft_fixture.CreateBFTFixture(**self.args.bft_fixture)
+    self._adb_remote_test = (self.dut.__class__.__name__ == 'AdbTarget')
+    if self._adb_remote_test:
+      self._template.SetState(_TESTING_ADB_CONNECTION)
 
   def tearDown(self):
+    if self._adb_remote_test:
+      self._bft_fixture.SetDeviceEngaged('ADB_HOST', engage=False)
     self._bft_fixture.Disconnect()
 
   def VerifyArgs(self):
@@ -152,11 +164,12 @@ class RaidenChargeBFTTest(unittest.TestCase):
       raise factory.FactoryTestFailure(
           'min_discharge_current_mA must be less than zero')
 
-  def SampleCurrentAndVoltage(self, duration_secs):
+  def SampleCurrentAndVoltage(self, duration_secs, charging):
     """Samples battery current and Plankton INA current/voltage for duration.
 
     Args:
       duration_secs: The duration in seconds to sample current.
+      charging: True if testing charging; False if discharging.
 
     Returns:
       A tuple of three lists (sampled battery current, sampled INA current,
@@ -167,13 +180,17 @@ class RaidenChargeBFTTest(unittest.TestCase):
     sampled_ina_voltage = []
     end_time = time_utils.MonotonicTime() + duration_secs
     while time_utils.MonotonicTime() < end_time:
-      sampled_battery_current.append(self._board.GetBatteryCurrent())
+      # Skip sampling ADB target current while discharging since we lose ADB
+      # connection during that time.
+      if not (self._adb_remote_test and not charging):
+        sampled_battery_current.append(self._board.GetBatteryCurrent())
       ina_values = self._bft_fixture.ReadINAValues()
       sampled_ina_current.append(ina_values['current'])
       sampled_ina_voltage.append(ina_values['voltage'])
       time.sleep(self.args.current_sampling_period_secs)
 
-    logging.info('Sampled battery current: %s', str(sampled_battery_current))
+    if not (self._adb_remote_test and not charging):
+      logging.info('Sampled battery current: %s', str(sampled_battery_current))
     logging.info('Sampled ina current: %s', str(sampled_ina_current))
     logging.info('Sampled ina voltage: %s', str(sampled_ina_voltage))
     return (sampled_battery_current, sampled_ina_current, sampled_ina_voltage)
@@ -186,9 +203,9 @@ class RaidenChargeBFTTest(unittest.TestCase):
     """
     if not self.args.check_protect_ina_current:
       return
-    current_min = self.args.protect_ina_current_range[0]
-    current_max = self.args.protect_ina_current_range[1]
+    current_min, current_max = self.args.protect_ina_current_range
 
+    self._template.SetState(_TESTING_PROTECT)
     self._bft_fixture.SetDeviceEngaged('CHARGE_5V', engage=True)
 
     ina_current = 0
@@ -207,21 +224,18 @@ class RaidenChargeBFTTest(unittest.TestCase):
                 (ina_current, current_min, current_max,
                  self.args.protect_ina_retry_times))
 
-  def TestCharging(self, current_min_threshold, voltage_tolerance,
-                   testing_volt):
+  def TestCharging(self, current_min_threshold, testing_volt):
     """Tests charge scenario. It will monitor within args.charge_duration_secs.
 
     Args:
       current_min_threshold: Minimum threshold to check charging current. If
           None, skip the test.
-      voltage_tolerance: Error ratio threshold to check charging voltage.
       testing_volt: An integer to specify testing charge voltage.
 
     Raises:
       FactoryTestFailure: If the sampled battery charge current does not pass
           the given threshold in dargs.
     """
-
     if current_min_threshold is None:
       return
 
@@ -236,21 +250,19 @@ class RaidenChargeBFTTest(unittest.TestCase):
     self._template.SetState(_TESTING_CHARGE(testing_volt))
     # Plankton-Raiden board setting: engage
     self._bft_fixture.SetDeviceEngaged(command_device, engage=True)
+    if self._adb_remote_test:
+      time.sleep(1)  # adb connection instable while voltage changing
 
     (sampled_battery_current, sampled_ina_current, sampled_ina_voltage) = (
-        self.SampleCurrentAndVoltage(self.args.charge_duration_secs))
-    # Fail if all battery current samples are below threshold
-    if not any(
-        c > current_min_threshold for c in sampled_battery_current):
+        self.SampleCurrentAndVoltage(self.args.charge_duration_secs,
+                                     charging=True))
+    # Fail if all battery current samples are below threshold.
+    if not any(c > current_min_threshold for c in sampled_battery_current):
       raise factory.FactoryTestFailure(
           'Battery charge current did not reach defined threshold %f mA' %
           current_min_threshold)
-    # Fail if all Plankton INA voltage samples are not within specified range
-    tolerance = testing_volt * 1000 * voltage_tolerance
-    if not any(abs(
-        v - testing_volt * 1000.0) <= tolerance for v in sampled_ina_voltage):
-      raise factory.FactoryTestFailure(
-          'Plankton INA voltage did not meet expected charge voltage')
+    # Fail if all Plankton INA voltage samples are not within specified range.
+    self.CheckINASampleVoltage(sampled_ina_voltage, testing_volt, charging=True)
     # If args.check_ina_current, fail if average of Plankton INA current
     # samples is not within specified range.
     self.CheckINASampleCurrent(sampled_ina_current, charging=True)
@@ -258,13 +270,16 @@ class RaidenChargeBFTTest(unittest.TestCase):
   def TestDischarging(self):
     """Tests discharging within args.discharge_duration_secs.
 
-    The test runs under high system load to maximize battery discharge current.
+    For local test scenario, the test runs under high system load to maximize
+    battery discharge current.
+
+    For adb remote test, it only measures Plankton INA voltage and current since
+    we lose adb connection during device discharging.
 
     Raises:
       FactoryTestFailure: If the sampled battery discharge current does not pass
           the given threshold in dargs.
     """
-
     current_min_threshold = self.args.min_discharge_current_mA
     if current_min_threshold is None:
       return
@@ -273,19 +288,48 @@ class RaidenChargeBFTTest(unittest.TestCase):
     self._template.SetState(_TESTING_DISCHARGE)
     self._bft_fixture.SetDeviceEngaged('CHARGE_5V', engage=False)
 
-    # Discharge under high system load.
-    with utils.LoadManager(self.args.discharge_duration_secs):
-      (sampled_battery_current, sampled_ina_current, _) = (
-          self.SampleCurrentAndVoltage(self.args.discharge_duration_secs))
-    # Fail if all samples are over threshold.
-    if not any(
-        c < current_min_threshold for c in sampled_battery_current):
-      raise factory.FactoryTestFailure(
-          'Battery discharge current did not reach defined threshold %f mA' %
-          current_min_threshold)
+    if self._adb_remote_test:
+      (_, sampled_ina_current, sampled_ina_voltage) = (
+          self.SampleCurrentAndVoltage(self.args.discharge_duration_secs,
+                                       charging=False))
+    else:
+      # Discharge under high system load.
+      with utils.LoadManager(self.args.discharge_duration_secs):
+        (sampled_battery_current, sampled_ina_current, sampled_ina_voltage) = (
+            self.SampleCurrentAndVoltage(self.args.discharge_duration_secs,
+                                         charging=False))
+      # Fail if all samples are over threshold.
+      if not any(c < current_min_threshold for c in sampled_battery_current):
+        raise factory.FactoryTestFailure(
+            'Battery discharge current did not reach defined threshold %f mA' %
+            current_min_threshold)
+
+    # Fail if all Plankton INA voltage samples are not within specified range.
+    self.CheckINASampleVoltage(
+        sampled_ina_voltage, self._DISCHARGE_VOLT, charging=False)
     # If args.check_ina_current, fail if average of Plankton INA current
     # samples is not within specified range.
     self.CheckINASampleCurrent(sampled_ina_current, charging=False)
+
+  def CheckINASampleVoltage(self, ina_sample, testing_volt, charging):
+    """Checks if average INA voltage is within range on Plankton.
+
+    Args:
+      ina_sample: A list of sampled Plankton INA voltage.
+      testing_volt: Expected testing voltage in V.
+      charging: True if testing charging; False if discharging.
+
+    Raises:
+      FactoryTestFailure if samples are not within range.
+    """
+    tolerance = testing_volt * 1000 * self.args.ina_voltage_tolerance
+    # Fail if error ratios of all voltage samples are higher than tolerance
+    if not any(abs(
+        v - testing_volt * 1000.0) <= tolerance for v in ina_sample):
+      raise factory.FactoryTestFailure(
+          'Plankton INA voltage did not meet expected %s %dV, sampled voltage '
+          '= %s' % ('charge' if charging else 'discharge',
+                    testing_volt, str(ina_sample)))
 
   def CheckINASampleCurrent(self, ina_sample, charging):
     """Checks if average INA current is within range on Plankton.
@@ -326,15 +370,20 @@ class RaidenChargeBFTTest(unittest.TestCase):
         int(self._power.GetBatteryAttribute('cycle_count').strip()) != 0):
       raise factory.FactoryTestFailure('Battery cycle count is not zero')
 
-    # Set charge state to 'charge'
-    self._board.SetChargeState(Board.ChargeState.CHARGE)
-    logging.info('Set charge state: CHARGE')
+    if self._adb_remote_test:
+      # Get adb target battery capacity and warn if almost full
+      capacity = self._board.GetBatteryCapacity()
+      factory.console.info('Current battery capacity = %d %%', capacity)
+      if capacity > 95:
+        factory.console.warning('Current battery capacity is almost full. '
+                                'It may cause charge failure!!')
+    else:
+      # Set charge state to 'charge'
+      self._board.SetChargeState(Board.ChargeState.CHARGE)
+      logging.info('Set charge state: CHARGE')
 
     self.Check5VINACurrent()
-    self.TestCharging(self.args.min_charge_5V_current_mA,
-                      self.args.ina_voltage_tolerance, testing_volt=5)
-    self.TestCharging(self.args.min_charge_12V_current_mA,
-                      self.args.ina_voltage_tolerance, testing_volt=12)
-    self.TestCharging(self.args.min_charge_20V_current_mA,
-                      self.args.ina_voltage_tolerance, testing_volt=20)
+    self.TestCharging(self.args.min_charge_5V_current_mA, testing_volt=5)
+    self.TestCharging(self.args.min_charge_12V_current_mA, testing_volt=12)
+    self.TestCharging(self.args.min_charge_20V_current_mA, testing_volt=20)
     self.TestDischarging()
