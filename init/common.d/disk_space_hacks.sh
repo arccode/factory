0bc0121e53016b76ea9a26b5b026967d95ff3b5b
bowgotsai <bowgotsai@chromium.org>
2015-07-22 19:49:28 +0800

    init: removes unused files under stateful partition
    This script removes some unused files under stateful partition to get
    more disk space for goofy_control to start.
    
    BUG=chrome-os-partner:42502
    TEST=Started factory test environment and make sure
         those dirs/files are removed
    
    Change-Id: I6b68af198d7cdbb8d18efd46bd0849b9afaa7a9b
    Reviewed-on: https://chromium-review.googlesource.com/287375
    Tested-by: Bowgo Tsai <bowgotsai@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Commit-Queue: Bowgo Tsai <bowgotsai@chromium.org>

---
+++ init/common.d/disk_space_hacks.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# Copyright 2015 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# goofy_control.sh will check disk usage and abort when running out of
+# disk space. Some files in stateful partition aren't needed for factory
+# test but they consume several hundreds of MB. This script removes those
+# unused files to get more disk space for goofy_control to start.
+
+ROOT="/mnt/stateful_partition"
+UNUSED_STATEFUL_DIRS="
+  ${ROOT}/var_overlay/db/pkg
+  ${ROOT}/dev_image/telemetry
+  ${ROOT}/dev_image/lib/portage
+  ${ROOT}/dev_image/lib/debug/
+  ${ROOT}/dev_image/*-cros-linux-gnu
+"
+
+main() {
+  for dir in ${UNUSED_STATEFUL_DIRS}; do
+    if [ -d "${dir}" ]; then
+      echo "Removing unused dir ${dir} ..."
+      rm -rf "${dir}"
+    fi
+  done
+}
+
+main "$@"
