18634cb1721835b764b6a3eef73075cebf836c2e
Wei-Ning Huang <wnhuang@google.com>
2015-09-22 14:59:06 +0800

    spatial_sensor_calibration: prompt user for device placement
    Prompt user to place device in the face-up position. Also minimize the
    call to 'vpd' to save time.
    
    BUG=chrome-os-partner:45531
    TEST=manually
    
    Change-Id: I8bcd7fba2a84f5ef4a008cb456f8897b68dabc9c
    Reviewed-on: https://chromium-review.googlesource.com/301432
    Commit-Ready: Wei-Ning Huang <wnhuang@chromium.org>
    Tested-by: Wei-Ning Huang <wnhuang@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

--- py/test/pytests/spatial_sensor_calibration.py
+++ py/test/pytests/spatial_sensor_calibration.py
@@ -20,6 +20,7 @@ The step for calibration is as follows:
 """
 
 import os
+import threading
 import time
 import unittest
 
@@ -35,6 +36,15 @@ DEFAULT_NAME = ('Accelerometer', u'加速度计')
 DEFAULT_SYSPATH = '/sys/bus/iio/devices/iio:device0/'
 DEFAULT_ENTRY_TEMPLATE = 'in_accel_%s_calibbias'
 
+CSS = """
+body { font-size: 2em; }
+.error { color: red; }
+"""
+
+
+class InvalidPositionError(Exception):
+  pass
+
 
 class SpatialSensorCalibration(unittest.TestCase):
   ARGS = [
@@ -44,26 +54,64 @@ class SpatialSensorCalibration(unittest.TestCase):
           default=DEFAULT_NAME),
       Arg('device_path', str, 'Path to the device IIO sysfs entry.',
           default=DEFAULT_SYSPATH),
+      Arg('raw_entry_template', str,
+          'Template for the sysfs raw value entry.',
+          default=DEFAULT_ENTRY_TEMPLATE),
       Arg('calibbias_entry_template', str,
           'Template for the sysfs calibbias value entry.',
           default=DEFAULT_ENTRY_TEMPLATE),
       Arg('stabilize_time', int, 'Time to wait until calibbias stabilize.',
           default=1),
+      Arg('prompt', bool, 'Prompt user to put the device in correct facing',
+          default=True),
+      Arg('placement_range', list, 'A list of tuple asserting the range of '
+          'X, Y, Z. Each element is a tuple (min, max) or None if it\'s a '
+          'don\'t care.', default=[None, None, None])
   ]
 
   def setUp(self):
-    ui = test_ui.UI()
-    self._template = ui_templates.OneSection(ui)
+    self._ui = test_ui.UI(css=CSS)
+    self._template = ui_templates.OneSection(self._ui)
+    self._start_event = threading.Event()
 
   def runTest(self):
+    previous_fail = False
+    while True:
+      try:
+        if self.args.prompt:
+          self.Prompt(previous_fail)
+          self._ui.Run(blocking=False)
+          self._start_event.wait()
+
+        self.RunCalibration()
+      except InvalidPositionError:
+        previous_fail = True
+        self._start_event.clear()
+      else:
+        break
+
+  def RunCalibration(self):
     self.WaitForDevice()
+    self.VerifyDevicePosition()
 
     self._template.SetState(test_ui.MakeLabel(
         'Calibrating %s...' % self.args.name[0],
         u'正在校正 %s...' % self.args.name[1]))
 
     self.EnableAutoCalibration(self.args.device_path)
-    self.RetrieveCalibbias()
+    self.RetrieveCalibbiasAndWriteVPD()
+    self._ui.Pass()
+
+  def Prompt(self, prev_fail=False):
+    prompt_en = '<div class="error">%s</div><br/>%s' % (
+        'Device not in position' if prev_fail else '',
+        'Please put the device in face-up position (press Enter to continue)')
+    prompt_zh = '<div class="error">%s</div><br/>%s' % (
+        u'装置位置不正确' if prev_fail else '',
+        u'请将装置面向上(按 Enter 继续)')
+
+    self._template.SetState(test_ui.MakeLabel(prompt_en, prompt_zh))
+    self._ui.BindKey(test_ui.ENTER_KEY, lambda _: self._start_event.set())
 
   def WaitForDevice(self):
     self._template.SetState(test_ui.MakeLabel('Waiting for device...',
@@ -71,6 +119,20 @@ class SpatialSensorCalibration(unittest.TestCase):
     sync_utils.WaitFor(self.dut.IsReady, self.args.timeout_secs)
     if not self.dut.IsReady():
       self.fail('failed to find deivce')
+
+  def VerifyDevicePosition(self):
+    for i, axis in enumerate(['x', 'y', 'z']):
+      _range = self.args.placement_range[i]
+      if _range is None:
+        continue
+
+      key = self.args.raw_entry_template % axis
+      value = int(self.dut.Read(self.dut.path.join(self.args.device_path, key)))
+      if value <= _range[0] or value >= _range[1]:
+        factory.console.error(
+            'Device not in correct position: %s-axis value: %d. '
+             'Valid range (%d, %d)' % (axis, value, _range[0], _range[1]))
+        raise InvalidPositionError
 
   def EnableAutoCalibration(self, path):
     RETRIES = 5
@@ -86,14 +148,14 @@ class SpatialSensorCalibration(unittest.TestCase):
         raise RuntimeError('calibrate activation failed')
     time.sleep(self.args.stabilize_time)
 
-  def ReadCalibbiasAndWriteVPD(self, axis):
-    self._template.SetState(test_ui.MakeLabel('Writing calibration data...',
-                                              u'正在写入校正结果...'))
-    key = self.args.calibbias_entry_template % axis
-    value = self.dut.Read(self.dut.path.join(self.args.device_path, key))
+  def RetrieveCalibbiasAndWriteVPD(self):
+    cmd = ['vpd']
 
-    self.dut.CheckCall('vpd -s %s=%s' % (key, value))
-
-  def RetrieveCalibbias(self):
     for axis in ['x', 'y', 'z']:
-      self.ReadCalibbiasAndWriteVPD(axis)
+      self._template.SetState(test_ui.MakeLabel('Writing calibration data...',
+                                                u'正在写入校正结果...'))
+      key = self.args.calibbias_entry_template % axis
+      value = self.dut.Read(self.dut.path.join(self.args.device_path, key))
+      cmd.extend(['-s', '%s=%s' % (key, value.strip())])
+
+    self.dut.CheckCall(cmd)
