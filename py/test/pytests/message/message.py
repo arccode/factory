273cf9216975cb9fd16d08e78883c8380d901126
Moja Hsu <mojahsu@google.com>
2015-09-24 11:58:54 +0800

    message: auto proceed function
    ODM require auto proceed function. They don't want to press any key for
    this case. Operator also can press key in auto proceed function.
    
    BUG=None
    TEST=manually on my DUT for original way and auto proceed way.
    
    Change-Id: I028a76d703eb25cc9379a187a73835781efe10da
    Reviewed-on: https://chromium-review.googlesource.com/302250
    Commit-Ready: Hsu Wei-Cheng <mojahsu@chromium.org>
    Tested-by: Hsu Wei-Cheng <mojahsu@chromium.org>
    Reviewed-by: Wei-Ning Huang <wnhuang@chromium.org>

--- py/test/pytests/message/message.py
+++ py/test/pytests/message/message.py
@@ -7,15 +7,19 @@
 """Displays a message.
 
 The operator can press the space or enter key to proceed.
+It can also be automatically proceed if we specify argument 'seconds'.
 """
 
 
 from __future__ import print_function
+import time
 import unittest
 
 
 import factory_common  # pylint: disable=W0611
+from cros.factory.test import test_ui
 from cros.factory.test.args import Arg
+from cros.factory.test.factory_task import FactoryTask, FactoryTaskManager
 from cros.factory.test.test_ui import MakeLabel, UI
 from cros.factory.test.ui_templates import OneSection
 
@@ -24,6 +28,29 @@ CSS_TEMPLATE = """
 .message { font-size: %(text_size)s%%; color: %(text_color)s; }
 .state { background-color: %(background_color)s; }
 """
+_HTML_REMAIN = '<br><div id="remain"></div>'
+
+
+class ShowingTask(FactoryTask):
+  """The task to show message for seconds """
+  def __init__(self, ui, seconds):  # pylint: disable=W0231
+    self._ui = ui
+    self._seconds = seconds
+    self._done = False
+
+    self._ui.BindKey(test_ui.SPACE_KEY, lambda _: self.Done())
+    self._ui.BindKey(test_ui.ENTER_KEY, lambda _: self.Done())
+
+  def Done(self):
+    self._done = True
+
+  def Run(self):
+    seconds = self._seconds
+    while seconds != 0 and not self._done:
+      self._ui.SetHTML(str(seconds), id='remain')
+      time.sleep(1)
+      seconds = seconds - 1
+    self.Pass()
 
 
 class MessageTest(unittest.TestCase):
@@ -34,7 +61,11 @@ class MessageTest(unittest.TestCase):
           optional=True),
       Arg('text_size', str, 'size of message in percentage', default='200'),
       Arg('text_color', str, 'color of message (in CSS)', default='black'),
-      Arg('background_color', str, 'background color (in CSS)', default='white')
+      Arg('background_color', str, 'background color (in CSS)',
+          default='white'),
+      Arg('seconds', int, 'duration to display message.'
+          'Specify None to show until key press.',
+          default=None, optional=True)
   ]
 
   def runTest(self):
@@ -48,6 +79,11 @@ class MessageTest(unittest.TestCase):
     template.SetState(
         '<div class="state">' +
         MakeLabel(self.args.html_en, self.args.html_zh, 'message') +
+        _HTML_REMAIN +
         '</div>')
-    ui.BindStandardKeys(bind_fail_keys=False)
-    ui.Run()
+    if self.args.seconds:
+      task = ShowingTask(ui, self.args.seconds)
+      FactoryTaskManager(ui, [task]).Run()
+    else:
+      ui.BindStandardKeys(bind_fail_keys=False)
+      ui.Run()
