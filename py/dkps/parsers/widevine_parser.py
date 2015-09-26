b37d1bd807df19b4e5ecfa9b82b4b7b856f62a04
Mao Huang <littlecvr@chromium.org>
2015-09-26 09:36:04 +0800

    dkps: add Widevine keybox XML parser
    Each DRM key issuing organization gives their DRM keys in different format. This
    parser is able to parse the Widevine keybox XML file into python list of
    objects.
    
    BUG=None
    TEST=Unit test
    
    Change-Id: I477730dc91ed53a5d2e97a1f3c9065e81c703fee
    Reviewed-on: https://chromium-review.googlesource.com/302700
    Commit-Ready: Mao Huang <littlecvr@chromium.org>
    Tested-by: Mao Huang <littlecvr@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

---
+++ py/dkps/parsers/widevine_parser.py
@@ -0,0 +1,49 @@
+# Copyright 2015 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Module to parse Widevine keybox XML file content."""
+
+import xml.etree.cElementTree as ET
+
+
+def Parse(serialized_drm_key_list):
+  """Parse the Widevine key XML file content.
+
+  This function turns each Widevine key into a dict that contains the following
+  keys:
+    - DeviceID
+    - Key
+    - ID
+    - Magix
+    - CRC
+  Each of the keys above maps to the same name element in Widevine keybox,
+  except DeviceID which is an attribute of the keybox.
+
+  Example, a Widevine keybox:
+
+    <Keybox DeviceID="device01">
+      <Key>key01</Key>
+      <ID>id01</ID>
+      <Magic>magic01</Magic>
+      <CRC>crc01</CRC>
+    </Keybox>
+
+  will be converted into a python dict:
+
+    {'DeviceID': 'device01',
+     'Key': 'key01',
+     'ID': 'id01',
+     'Magic': 'magic01',
+     'CRC': 'crc01'},
+  """
+  widevine_key_list = []
+
+  root = ET.fromstring(serialized_drm_key_list.strip())
+  for child in root.iter('Keybox'):
+    widevine_key = {'DeviceID': child.attrib['DeviceID'].strip()}
+    for key in ['Key', 'ID', 'Magic', 'CRC']:
+      widevine_key[key] = child.find(key).text
+    widevine_key_list.append(widevine_key)
+
+  return widevine_key_list
---
+++ py/dkps/parsers/widevine_parser_unittest.py
@@ -0,0 +1,35 @@
+# Copyright 2015 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Unit test for Widevine parser module."""
+
+import os
+import unittest
+
+import factory_common  # pylint: disable=W0611
+from cros.factory.dkps.parsers import widevine_parser
+
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+MOCK_WIDEVINE_FILE_PATH = os.path.join(SCRIPT_DIR, 'testdata', 'widevine.xml')
+
+EXPECTED_PARSED_MOCK_WIDEVINE_KEY_LIST = [
+    {'DeviceID': 'device01', 'Key': 'key01', 'ID': 'id01',
+     'Magic': 'magic01', 'CRC': 'crc01'},
+    {'DeviceID': 'device02', 'Key': 'key02', 'ID': 'id02',
+     'Magic': 'magic02', 'CRC': 'crc02'},
+    {'DeviceID': 'device03', 'Key': 'key03', 'ID': 'id03',
+     'Magic': 'magic03', 'CRC': 'crc03'}]
+
+
+class WidevineParserTest(unittest.TestCase):
+  def runTest(self):
+    with open(MOCK_WIDEVINE_FILE_PATH) as f:
+      self.assertEqual(EXPECTED_PARSED_MOCK_WIDEVINE_KEY_LIST,
+                       widevine_parser.Parse(f.read()))
+
+
+if __name__ == '__main__':
+  unittest.main()
