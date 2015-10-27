190aa4addbc9a286ace2aaaba0401a91a48700c9
Shun-Hsing Ou <shunhsingou@chromium.org>
2015-10-27 11:25:38 +0800

    Example CL for in-place wiping and battery cutoff
    This is an example CL for using in-place wiping.
    
    BUG=chromium:544021
    TEST=None
    
    Change-Id: Idd4c878d005f0e5d5cfe462a476ecb9bd348efea
    Reviewed-on: https://chromium-review.googlesource.com/308816
    Commit-Ready: Shun-Hsing Ou <shunhsingou@chromium.org>
    Tested-by: Shun-Hsing Ou <shunhsingou@chromium.org>
    Reviewed-by: Bowgo Tsai <bowgotsai@chromium.org>

--- py/test/test_lists/generic_grt.py
+++ py/test/test_lists/generic_grt.py
@@ -134,5 +134,10 @@ def GRT(args):
               hwid_version=3,
               sync_event_logs=args.enable_flush_event_logs,
               waive_tests=args.grt_waive_tests,
+              wipe_in_place=True,
+              cutoff_options=dict(
+                  method='battery_cutoff',
+                  check_ac='remove_ac',
+                  min_battery_percent=args.grt_finalize_battery_min_pct),
               enforced_release_channels=args.grt_enforced_release_channels
           ))
