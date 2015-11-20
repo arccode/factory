ad9ec468b9a31d2698e958ee8558b3d2a8b761f1
Wei-Han Chen <stimim@google.com>
2015-11-21 04:08:38 +0800

    factory: entry for fixture-based testing
    For fixture-based testing, there are some common setup and teardown
    process for all test lists. fixture_entry.FixtureEntry is a test case to
    do those things. By default, FixtureEntry performs actions for start
    testing a DUT. Set arguement 'start_fixture_tests' to False will
    perform actions to stop testing a DUT.
    
    CQ-DEPEND=CL:*241265
    BUG=chromium:551806
    TEST=python fixture_entry_unittest.py
    
    Change-Id: I6bc43672b2a80e8f71393c4f5e61ce167a364bd4
    Reviewed-on: https://chromium-review.googlesource.com/313286
    Commit-Ready: Wei-Han Chen <stimim@chromium.org>
    Tested-by: Wei-Han Chen <stimim@chromium.org>
    Reviewed-by: Chih-Yu Huang <akahuang@chromium.org>

---
+++ py/test/pytests/fixture_entry.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+# Copyright 2015 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Starts or ends a fixture-based testing.
+
+This factory test invoke functions to setup or teardown a fixture-based
+testlist.
+"""
+
+import unittest
+
+import factory_common # pylint: disable=W0611
+
+from cros.factory.test import factory
+from cros.factory.test import test_ui
+from cros.factory.test import ui_templates
+from cros.factory.test.args import Arg
+from cros.factory.utils import sync_utils
+
+
+_CSS = """
+.prompt {
+  font-size: 2em;
+}
+
+.warning {
+  color: red;
+}
+"""
+
+_TITLE_START = test_ui.MakeLabel('Start Fixture Test', u'开始治具测试')
+_TITLE_END = test_ui.MakeLabel('End Fixture Test', u'结束治具测试')
+
+_MSG_INSERT = test_ui.MakeLabel(
+    'Please attach DUT.',
+    u'INSERT 请插入测试装置。',
+    'prompt')
+
+_MSG_SEND_RESULT = test_ui.MakeLabel(
+    'Sending test results to shopfloor...',
+    u'SENDING 传送测试结果给服务器...',
+    'prompt')
+
+_MSG_REMOVE_DUT = test_ui.MakeLabel(
+    'Please remove DUT.',
+    u'REMOVE 请移除测试装置。',
+    'prompt')
+
+_MSG_RESTART_TESTS = test_ui.MakeLabel(
+    'Restarting all tests...',
+    u'RESTARTING 测试结束，正在重设测试列表...',
+    'prompt')
+
+class FixtureEntry(unittest.TestCase):
+  """The factory test to start fixture test process."""
+  ARGS = [
+      Arg('start_fixture_tests', bool,
+          'To start or stop the factory fixture tests.', default=True,
+          optional=True),
+  ]
+
+  def setUp(self):
+    self._state = factory.get_state_instance()
+    self._ui = test_ui.UI()
+    self._ui.AppendCSS(_CSS)
+    self._template = ui_templates.OneSection(self._ui)
+    self._template.SetTitle(_TITLE_START if self.args.start_fixture_tests else
+                            _TITLE_END)
+
+  def RestartAllTests(self):
+    self._state.ScheduleRestart()
+
+  def SendTestResult(self):
+    # TODO(stimim): send test result to shopfloor
+    pass
+
+  def runTest(self):
+    self._ui.Run(blocking=False)
+
+    if self.args.start_fixture_tests:
+      self._template.SetState(_MSG_INSERT)
+      sync_utils.WaitFor(self.dut.IsReady, None)
+    else:
+      self._template.SetState(_MSG_SEND_RESULT)
+      self.SendTestResult()
+
+      self._template.SetState(_MSG_REMOVE_DUT)
+      sync_utils.WaitFor(lambda: not self.dut.IsReady(), None)
+
+      self._template.SetState(_MSG_RESTART_TESTS)
+      self.RestartAllTests()
---
+++ py/test/pytests/fixture_entry_unittest.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+# Copyright 2015 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# We need to inject mock objects to protected members of FactoryEntry:
+# pylint: disable=W0212
+
+"""Unit tests for fixture_entry factory test."""
+
+import logging
+import mox
+import unittest
+
+import factory_common # pylint: disable=W0611
+from cros.factory.goofy.goofy_rpc import GoofyRPC
+from cros.factory.test import factory
+from cros.factory.test import test_ui
+from cros.factory.test import ui_templates
+from cros.factory.test.pytests import fixture_entry
+from cros.factory.test.dut.base import BaseTarget
+from cros.factory.utils import sync_utils
+
+class FakeArgs(object):
+  def __init__(self, dargs):
+    for (key, value) in dargs.iteritems():
+      self.__dict__[key] = value
+
+
+class FactoryEntryUnitTest(unittest.TestCase):
+  def setUp(self):
+    self.mox = mox.Mox()
+    self.test = fixture_entry.FixtureEntry()
+
+    self.mock_ui = self.mox.CreateMock(test_ui.UI)
+    self.mock_template = self.mox.CreateMock(ui_templates.OneSection)
+
+  def tearDown(self):
+    self.mox.UnsetStubs()
+
+  def testSetUpForStartTesting(self):
+    mock_state = self.mox.CreateMock(GoofyRPC)
+
+    self.mox.StubOutWithMock(factory, 'get_state_instance')
+    self.mox.StubOutWithMock(test_ui, 'UI')
+    self.mox.StubOutWithMock(ui_templates, 'OneSection')
+
+    # for start testing
+    factory.get_state_instance().AndReturn(mock_state)
+    test_ui.UI().AndReturn(self.mock_ui)
+    self.mock_ui.AppendCSS(mox.IsA(str))
+    ui_templates.OneSection(self.mock_ui).AndReturn(self.mock_template)
+    self.mock_template.SetTitle(fixture_entry._TITLE_START)
+
+    self.mox.ReplayAll()
+
+    self.test.args = FakeArgs({'start_fixture_tests': True})
+    self.test.setUp()
+    self.assertEqual(self.test._state, mock_state) # pylint: disable=W0212
+
+    self.mox.VerifyAll()
+
+  def testEndFixtureBasedTest(self):
+    mock_state = self.mox.CreateMock(GoofyRPC)
+    self.test._state = mock_state # pylint: disable=W0212
+    self.test.args = FakeArgs({'start_fixture_tests': False})
+    self.test._ui = self.mock_ui
+    self.test._template = self.mock_template
+
+    self.mox.StubOutWithMock(sync_utils, 'WaitFor')
+
+    self.mock_ui.Run(blocking=False)
+    self.mock_template.SetState(mox.IsA(basestring))
+
+    self.mock_template.SetState(mox.IsA(basestring))
+    sync_utils.WaitFor(mox.IsA(type(lambda: None)), None)
+
+    self.mock_template.SetState(mox.IsA(basestring))
+    mock_state.ScheduleRestart()
+
+    self.mox.ReplayAll()
+
+    self.test.runTest()
+
+    self.mox.VerifyAll()
+
+  def testStartFixtureBasedTest(self):
+    mock_dut = self.mox.CreateMock(BaseTarget)
+    self.test.dut = mock_dut
+    self.test._ui = self.mock_ui
+    self.test._template = self.mock_template
+    self.test.args = FakeArgs({'start_fixture_tests': True})
+
+    self.mox.StubOutWithMock(sync_utils, 'WaitFor')
+
+    self.mock_ui.Run(blocking=False)
+    self.mock_template.SetState(mox.IsA(basestring))
+    sync_utils.WaitFor(mock_dut.IsReady, mox.IsA(None))
+
+    self.mox.ReplayAll()
+
+    self.test.runTest()
+
+    self.mox.VerifyAll()
+
+
+if __name__ == '__main__':
+  logging.basicConfig(level=logging.INFO)
+  unittest.main()
