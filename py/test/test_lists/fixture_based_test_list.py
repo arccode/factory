072aa68fd1183037e89789f43e5ef3b204b64e31
Wei-Han Chen <stimim@google.com>
2015-12-09 16:05:59 +0800

    factory: add fixture_based_test_list.py
    This is a function dectorator, which will add common test cases in
    fixture based testing, currently it will prepare dut connection, add
    FixtureEntry test cases in the begining and end.
    
    Example:
      # define main test list for SMT test
      @FixtureBased('main', 'SMT Test', 'SMT Test', dut_options)
      # the created test list object will be passed in as test_list
      # variable.
      def CreateTestLists(test_list):
        with TestGroup(id='SMT'):
          SetOptions(test_list.options, args)
          PressToStart(args)
    
    CQ-DEPEND=CL:313286, CL:311981
    BUG=chromium:551806
    TEST=manual test
    
    Change-Id: Iae730ccb30a91f72dc0eec72b8090213d4998753
    Reviewed-on: https://chromium-review.googlesource.com/317104
    Commit-Ready: Wei-Han Chen <stimim@chromium.org>
    Tested-by: Wei-Han Chen <stimim@chromium.org>
    Reviewed-by: Chih-Yu Huang <akahuang@chromium.org>

---
+++ py/test/test_lists/fixture_based_test_list.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright 2015 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import factory_common  # pylint: disable=W0611
+
+from cros.factory.test.test_lists.test_lists import (OperatorTest, TestList)
+from cros.factory.test import dut
+
+def StartFixtureTest(test_list_id, label_en, label_zh):
+  OperatorTest(
+      id='StartFixtureTest_%s' % test_list_id,
+      label_en=u'Start %s' % label_en,
+      label_zh=u'开始 %s' % label_zh,
+      pytest_name='fixture_entry')
+
+
+def EndFixtureTest(test_list_id, label_en, label_zh):
+  OperatorTest(
+      id='EndFixtureTest_%s' % test_list_id,
+      label_en=u'End %s' % label_en,
+      label_zh=u'结束 %s' % label_zh,
+      pytest_name='fixture_entry',
+      dargs={'start_fixture_tests': False})
+
+
+def FixtureBased(test_list_id, label_en, label_zh, dut_options=None):
+  """ A decorator to add common test items for fixture based tests
+
+      Example:
+
+      dut_options = {'dut_class': 'SSHTarget', 'host': 'dynamic'}
+      @FixtureBased('main', 'CoolProduct EVT', dut_options)
+      def CreateTestLists(test_list):
+        # dut_options is automatically set to test_list,
+        # you can set other options for test_list here
+        with TestGroup(...):
+          ...
+        OperatorTest(...)
+
+      Then CreateTestLists() will create a test list named 'main', with
+      label 'CoolProduct EVT'.
+  """
+  if not dut_options:
+    dut_options = {}
+
+  def Wrap(CreateTestLists):
+    def CreateFixtureTestList():
+      dut.PrepareConnection(**dut_options)
+      with TestList(test_list_id, label_en) as test_list:
+        test_list.dut_options = dut_options
+        StartFixtureTest(test_list_id, label_en, label_zh)
+        CreateTestLists(test_list)
+        EndFixtureTest(test_list_id, label_en, label_zh)
+    return CreateFixtureTestList
+  return Wrap
