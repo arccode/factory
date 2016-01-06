b9a30b5a05fef31366c97e7e245d28f0fa5f74f1
Wei-Han Chen <stimim@google.com>
2016-01-06 10:39:43 +0800

    dut: find mount point by 'realpath' and '/proc/mounts'
    In previous implementation, the behavior of 'df' is different on
    Android, 'df' also fails when the path doesn't exists. The new
    implementation use 'realpath' to find the resolved path, and match the
    path with mount points in /proc/mounts.
    
    BUG=None
    TEST=make test, manual test
    
    Change-Id: Id2d8c6bd3966347e9f8467411c5b9cd2db15c936
    Reviewed-on: https://chromium-review.googlesource.com/320551
    Commit-Ready: Wei-Han Chen <stimim@chromium.org>
    Tested-by: Wei-Han Chen <stimim@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

--- py/test/dut/storage.py
+++ py/test/dut/storage.py
@@ -7,7 +7,6 @@
 
 import logging
 import re
-from subprocess import CalledProcessError
 
 import factory_common  # pylint: disable=W0611
 from cros.factory.test.dut import component
@@ -38,23 +37,49 @@ class Storage(component.DUTComponent):
                defaults to 'rw').
     """
 
-    # Get mount point of file system
-    # TODO(stimim): implement this by 'readlink' or 'realpath'
-    try:
+    def _GetMountPointByDiskFree(path):
       # the output should look like:
       # Mounted on
       # /usr/local
-      output = self._dut.CheckOutput(['df', '--output=target', path])
-    except CalledProcessError:
-      logging.exception('remount: Cannot get mount point of %s', path)
-      return False
+      output = self._dut.CallOutput(['df', '--output=target', path])
+      if not output:
+        return None
+      match = re.search(r'^(/[/\w]+)$', output, re.MULTILINE)
+      if not match:
+        logging.warning('remount: The output of df is unexpected:\n%s', output)
+        return None
+      return match.group(1)
 
-    match = re.search(r'^(/[/\w]+)$', output, re.MULTILINE)
-    if not match:
-      logging.error('remount: The output of df is unexpected:\n%s', output)
-      return False
+    def _GetMountPointFromProcMounts(path):
+      # resolve all symbolic links
+      realpath = self._dut.path.realpath(path)
+      logging.info('remount: %s is resolved to %s', path, realpath)
 
-    mount_point = match.group(1)
+      output = self._dut.CallOutput('cat /proc/mounts')
+      if not output:
+        logging.error('remount: Cannot read /proc/mounts')
+        return None
+
+      # The format of /proc/mounts is documented in fstab(5),
+      # second field of each line is the mount point
+      mount_points = [x[1] for x in map(str.split, output.splitlines())]
+
+      # '/' is always a mount point
+      best_match = '/'
+      for mount_point in mount_points:
+        if (realpath.startswith(mount_point) and
+            len(mount_point) > len(best_match)):
+          best_match = mount_point
+      return best_match
+
+    mount_point = _GetMountPointByDiskFree(path)
+    if not mount_point:
+      logging.info('remount: Cannot get mount point by df, try /proc/mounts')
+      mount_point = _GetMountPointFromProcMounts(path)
+
+    if not mount_point:
+      logging.error('remount: Cannot get mount point of %s', path)
+      return False
 
     cmd = ['mount', '-o', 'remount,%s' % options, mount_point]
     if self._dut.Call(cmd) != 0:
