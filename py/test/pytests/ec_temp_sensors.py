b8ea113984295a0f1287cc7fda20bc26c3ae24a3
Moja Hsu <mojahsu@google.com>
2016-01-07 14:53:37 +0800

    dut: Fix typo of the import
    Fix typo in
    from cros.factory.tests import dut
    to
    from cros.factory.test import dut
    
    BUG=None
    TEST=run ec_temp_sensors on chromebook
    
    Change-Id: Ice932f0a92fac39e7752b07645d1a2fbb050424a
    Reviewed-on: https://chromium-review.googlesource.com/320649
    Commit-Ready: Hsu Wei-Cheng <mojahsu@chromium.org>
    Tested-by: Hsu Wei-Cheng <mojahsu@chromium.org>
    Reviewed-by: Shun-Hsing Ou <shunhsingou@chromium.org>

--- py/test/pytests/ec_temp_sensors.py
+++ py/test/pytests/ec_temp_sensors.py
@@ -14,7 +14,7 @@ import logging
 import unittest
 
 import factory_common  # pylint: disable=W0611
-from cros.factory.tests import dut
+from cros.factory.test import dut
 from cros.factory.test.args import Arg
 
 
