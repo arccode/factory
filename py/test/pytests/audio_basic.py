3d163d50d35d7c84d388b854177782526f0ff708
Moja Hsu <mojahsu@google.com>
2016-01-19 15:21:54 +0800

    audio_basic: Apply dut.audio framework and fine tune test flow.
    Apply dut.audio framework to audio.py.
    Make it support dut based testing, so it can support android platform
    now.
    Test input and output channel by channel.
    Prevent operator from pressing space directly.
    
    BUG=None
    TEST=Test it for headphone/Speaker on Pixel and PixelC
         make test, make doc
    
    Change-Id: I4b67a91da0fb51693c77d91e3830379823c360b0
    Reviewed-on: https://chromium-review.googlesource.com/322560
    Commit-Ready: Hsu Wei-Cheng <mojahsu@chromium.org>
    Tested-by: Hsu Wei-Cheng <mojahsu@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

--- py/test/pytests/audio_basic.py
+++ py/test/pytests/audio_basic.py
@@ -9,7 +9,8 @@
 # This is a factory test to test the audio.  Operator will test both record and
 # playback for headset and built-in audio.  Recordings are played back for
 # confirmation.  An additional pre-recorded sample is played to confirm speakers
-# operate independently
+# operate independently.
+# We will test each channel of input and output.
 
 
 import logging
@@ -17,25 +18,30 @@ import os
 import unittest
 
 import factory_common  # pylint: disable=W0611
+from cros.factory.test import dut
 from cros.factory.test import test_ui
 from cros.factory.test.args import Arg
 from cros.factory.test.ui_templates import OneSection
-from cros.factory.utils.process_utils import Spawn, TerminateOrKillProcess
+from cros.factory.utils import file_utils
+from cros.factory.utils.process_utils import Spawn
 
-_CMD_PLAY_AUDIO = ['aplay', '-D', 'hw:0,0']
-_CMD_RECORD_AUDIO = ['arecord', '-D', 'hw:0,0', '-f', 'dat', '-t', 'wav']
-_CMD_CONFIG_AUDIO = ['amixer', '-c', '0', 'cset']
-_SAMPLE_FILE = 'fhorn.wav'
+_RECORD_SEC = 3
+_RECORD_RATE = 48000
+_SOUND_DIRECTORY = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), '..', '..', 'goofy',
+    'static', 'sounds')
+
 
 _MSG_AUDIO_INFO = test_ui.MakeLabel(
-    'Press & hold \'R\' to record, Playback will follow<br>'
-    'Press & hold \'P\' to play sample<br>'
-    'Press space to mark pass',
-    zh='压住 \'R\' 键开始录音，之后会重播录到的声音<br>'
-    '压住 \'P\' 键播放范例<br>'
-    '压下空白表示成功',
+    'Press \'R\' to record %d seconds, Playback will follow<br>'
+    'Press \'P\' to play sample for each channel<br>'
+    'Press space to mark pass' % _RECORD_SEC,
+    zh='按 \'R\' 键开始录音%d秒，之后会重播录到的声音<br>'
+    '按 \'P\' 键播放范例<br>'
+    '压下空白表示成功' % _RECORD_SEC,
     css_class='audio-test-info')
-
+_MSG_RECORD_INFO = test_ui.MakeLabel('Start recording', u'开始录音',
+                                     css_class='audio-test-info')
 _HTML_AUDIO = """
 <table style="width: 70%%; margin: auto;">
   <tr>
@@ -68,25 +74,59 @@ window.onkeydown = function(event) {
     test.sendTestEvent("MarkPass", '');
   }
 }
-
-window.onkeyup = function(event) {
-  if (event.keyCode == 82) { // 'R'
-    test.sendTestEvent("HandleRecordEvent", 'stop');
-  } else if (event.keyCode == 80) { // 'P'
-    test.sendTestEvent("HandleSampleEvent", 'stop');
-  }
-}
 """
+
+PLAY_SAMPLE_VALUE = (1 << 0)
+RECORD_VALUE = (1 << 1)
+PASS_VALUE = (PLAY_SAMPLE_VALUE | RECORD_VALUE)
+
+
+def GetPlaybackRecordLabel(channel):
+  return test_ui.MakeLabel('Playback sound (Mic channel %d)' % channel,
+                           u'重播录到的声音(麦克风通道%d)' % channel,
+                           css_class='audio-test-info')
+
+
+def GetPlaybackLabel(channel):
+  return test_ui.MakeLabel('Playback sound to channel %d' % channel,
+                           u'播放范例到通道%d)' % channel,
+                           css_class='audio-test-info')
 
 
 class AudioBasicTest(unittest.TestCase):
   ARGS = [
-      Arg('audio_title', str, 'Title of audio test', 'Headset Audio Test'),
-      Arg('amixer_init_config', list, 'Initial config of amixer', None,
-          optional=True)
+      Arg('audio_title', tuple, 'Label Title of audio test (en, zh)',
+          ('Headset', u'外接耳机')),
+      Arg('audio_conf', str, 'Audio config file path', optional=True),
+      Arg('initial_actions', list, 'List of tuple (card, actions)', []),
+      Arg(
+          'input_dev', tuple,
+          'Input ALSA device. (card_name, sub_device).'
+          'For example: ("audio_card", "0").', ('0', '0')),
+      Arg(
+          'output_dev', tuple,
+          'Output ALSA device. (card_name, sub_device).'
+          'For example: ("audio_card", "0").', ('0', '0')),
+      Arg('output_channels', int, 'number of output channels.', 2),
+      Arg('input_channels', int, 'number of input channels.', 2),
   ]
 
   def setUp(self):
+    self._dut = dut.Create()
+    if self.args.audio_conf:
+      self._dut.audio.ApplyConfig(self.args.audio_conf)
+
+    # Tansfer input and output device format
+    self._in_card = self._dut.audio.GetCardIndexByName(self.args.input_dev[0])
+    self._in_device = self.args.input_dev[1]
+    self._out_card = self._dut.audio.GetCardIndexByName(self.args.output_dev[0])
+    self._out_device = self.args.output_dev[1]
+
+    # Init audio card before show html
+    for card, action in self.args.initial_actions:
+      card = self._dut.audio.GetCardIndexByName(card)
+      self._dut.audio.ApplyAudioConfig(action, card)
+
     # Initialize frontend presentation
     self.ui = test_ui.UI()
     self.template = OneSection(self.ui)
@@ -94,52 +134,80 @@ class AudioBasicTest(unittest.TestCase):
     self.template.SetState(_HTML_AUDIO)
     self.ui.RunJS(_JS_AUDIO)
     msg_audio_title = test_ui.MakeLabel(
-        self.args.audio_title,
+        self.args.audio_title[0], self.args.audio_title[1],
         css_class='audio-test-info')
     self.ui.SetHTML(msg_audio_title, id='audio_title')
     self.ui.SetHTML(_MSG_AUDIO_INFO, id='audio_info')
     self.current_process = None
     self.key_press = None
-    base = os.path.dirname(os.path.realpath(__file__))
-    self.file_path = os.path.join(base, '..', '..', 'goofy', 'static', 'sounds',
-                                  _SAMPLE_FILE)
-
-    if self.args.amixer_init_config:
-      self.ConfigAmixerSetting(self.args.amixer_init_config)
+    # prevent operator from pressing space directly,
+    # make sure he presses P and R.
+    self.event_value = 0
 
     self.ui.AddEventHandler('HandleRecordEvent', self.HandleRecordEvent)
     self.ui.AddEventHandler('HandleSampleEvent', self.HandleSampleEvent)
     self.ui.AddEventHandler('MarkPass', self.MarkPass)
 
-  def ConfigAmixerSetting(self, config_list):
-    for config in config_list:
-      command = _CMD_CONFIG_AUDIO + ["name='%s'" % config['name'],
-                                     config['value']]
-      Spawn(command, check_call=True)
-
   def HandleRecordEvent(self, event):
     if event.data == 'start' and not self.key_press:
       self.key_press = 'R'
       logging.info('start record')
-      self.current_process = Spawn(_CMD_RECORD_AUDIO + ['/tmp/rec.wav'])
-    elif event.data == 'stop' and self.key_press == 'R':
-      TerminateOrKillProcess(self.current_process)
+      self.ui.SetHTML(_MSG_RECORD_INFO, id='audio_info')
+      dut_record_file_path = self._dut.temp.mktemp(False)
+      self._dut.audio.RecordWavFile(dut_record_file_path, self._in_card,
+                                    self._in_device, _RECORD_SEC,
+                                    self.args.input_channels, _RECORD_RATE)
       logging.info('stop record and start playback')
-      Spawn(_CMD_PLAY_AUDIO + ['/tmp/rec.wav'], check_call=True)
+      # playback the record file by each channel.
+      with file_utils.UnopenedTemporaryFile(suffix='.wav') as full_wav_path:
+        self._dut.link.Pull(dut_record_file_path, full_wav_path)
+        for i in xrange(self.args.input_channels):
+          with file_utils.UnopenedTemporaryFile(suffix='.wav') as wav_path:
+            # Get channel i from full_wav_path to a stereo wav_path
+            # Since most devices support 2 channels.
+            Spawn(['sox', full_wav_path, wav_path, 'remix', str(i + 1),
+                   str(i + 1)], log=True, check_call=True)
+            with self._dut.temp.TempFile() as dut_path:
+              self._dut.link.Push(wav_path, dut_path)
+              self.ui.SetHTML(GetPlaybackRecordLabel(i + 1), id='audio_info')
+              self._dut.audio.PlaybackWavFile(dut_path, self._out_card,
+                                              self._out_device)
+      self._dut.CheckCall(['rm', '-f', dut_record_file_path])
+      self.ui.SetHTML(_MSG_AUDIO_INFO, id='audio_info')
       self.key_press = None
+      self.event_value |= RECORD_VALUE
 
   def HandleSampleEvent(self, event):
     if event.data == 'start' and not self.key_press:
       self.key_press = 'P'
       logging.info('start play sample')
-      self.current_process = Spawn(_CMD_PLAY_AUDIO + [self.file_path])
-    elif event.data == 'stop' and self.key_press == 'P':
-      TerminateOrKillProcess(self.current_process)
+      lang = self.ui.GetUILanguage()
+      for i in xrange(self.args.output_channels):
+        ogg_path = os.path.join(_SOUND_DIRECTORY, '%d_%s.ogg' % (i + 1, lang))
+        number_wav_path = '%s.wav' % ogg_path
+        Spawn(['sox', ogg_path, '-c1', number_wav_path], check_call=True)
+        with file_utils.UnopenedTemporaryFile(suffix='.wav') as wav_path:
+          # we will only keep (i + 1) channel and mute others.
+          # We use number sound to indicate which channel to be played.
+          # Create .wav file with n channels but ony has one channel data.
+          remix_option = ['0'] * self.args.output_channels
+          remix_option[i] = '1'
+          Spawn(['sox', number_wav_path, wav_path, 'remix'] + remix_option,
+                log=True, check_call=True)
+          with self._dut.temp.TempFile() as dut_path:
+            self._dut.link.Push(wav_path, dut_path)
+            self.ui.SetHTML(GetPlaybackLabel(i + 1), id='audio_info')
+            self._dut.audio.PlaybackWavFile(dut_path, self._out_card,
+                                            self._out_device)
+        os.unlink(number_wav_path)
       logging.info('stop play sample')
+      self.ui.SetHTML(_MSG_AUDIO_INFO, id='audio_info')
       self.key_press = None
+      self.event_value |= PLAY_SAMPLE_VALUE
 
   def MarkPass(self, event):  # pylint: disable=W0613
-    self.ui.Pass()
+    if self.event_value == PASS_VALUE:
+      self.ui.Pass()
 
   def runTest(self):
     self.ui.Run()
