arccode/factory

commit 44c716f17aec3259d2bbc93b24de52aa98b5843d

factory: SSHLink: find availible net interfaces for DHCP server
SSHLink.PrepareLink will automatically find all availible network
interfaces that is not default gateway, and start a bind DHCP service
for each interface.

BUG=chromium:551806
TEST=make test

Change-Id: I2df452ba4b6554fd0601e96e02ddf7ec153dadbb
Reviewed-on: https://chromium-review.googlesource.com/318972
Commit-Ready: Wei-Han Chen <stimim@chromium.org>
Tested-by: Wei-Han Chen <stimim@chromium.org>
Reviewed-by: Chih-Yu Huang <akahuang@chromium.org>

py/utils/net_utils.py
@@ -563,10 +563,10 @@ def GetUnusedIPV4RangeCIDR(preferred_prefix_bits=24, exclude_ip_prefix=None):
   # currently on the machine interfaces.
   if not exclude_ip_prefix:
     exclude_ip_prefix = []
-    for iface in GetNetworkInterfaces():
-      ip_mask = GetEthernetIp(iface, True)
-      if ip_mask[0] and ip_mask[1]:
-        exclude_ip_prefix.append(ip_mask)
+  for iface in GetNetworkInterfaces():
+    ip_mask = GetEthernetIp(iface, True)
+    if ip_mask[0] and ip_mask[1]:
+      exclude_ip_prefix.append(ip_mask)
 
   # available_ranges_bits stores (ip_range, available_subnet_range_bits)
   # For example: available_ranges_bits = {0xc0a80000: 8}

py/utils/dhcp_utils.py
@@ -10,25 +10,44 @@ from __future__ import print_function
 import jsonrpclib
 import logging
 import os
+import random
 import signal
 import sys
+import time
 
 import factory_common  # pylint: disable=W0611
+from cros.factory.test import network
+from cros.factory.test import utils
+from cros.factory.test.utils import service_manager
 from cros.factory.utils import jsonrpc_utils
 from cros.factory.utils import net_utils
 from cros.factory.utils import process_utils
+from cros.factory.utils import sync_utils
+
+
+class InterfaceProperty(object):  # pylint: disable=R0923
+  """
+  A data structure that stores configurations for an interface
+  Properties
+  """
+  def __init__(self, name, cidr=None):
+    assert isinstance(name, str)
+    assert cidr is None or isinstance(cidr, net_utils.CIDR)
+
+    self.name = name
+    self.cidr = cidr
 
 
 class DHCPManager(object):
   """The manager that provides DHCP service.
 
   Properties set from __init__ arguments:
-    interface: The name of the network interface for DHCP.
-    my_ip: The IP address for the DHCP server. The interface used will be
-      set to this IP address.
-    ip_start: The start of DHCP IP range.
-    ip_end: The end of DHCP IP range.
-    lease_time: How long in seconds before the DHCP lease expires.
+    interfaces:
+      A list of InterfaceProperty(s) to bind, or None to bind all available
+      interfaces
+    interface_blacklist:
+      A list of interfaces (str) that shouldn't be managed, note that the
+      default gateway interface is always excluded.
     bootp: A tuple (ip, filename, hostname) specifying the boot parameters.
     on_add: Optional callback function that's called when a client is issued
       a new IP address. The callback takes the IP address as the only argument.
@@ -49,27 +68,55 @@ class DHCPManager(object):
   # Lease file name prefix
   LEASE_PREFIX = 'dnsmasq_leases_'
 
-  def __init__(self, interface,
-               my_ip='192.168.0.1',
-               netmask='255.255.255.0',
-               ip_start='192.168.0.10',
-               ip_end='192.168.0.20',
+  def __init__(self,
+               interfaces=None,
+               interface_blacklist=None,
+               exclude_ip_prefix=None,
                lease_time=3600,
                bootp=None,
                on_add=None,
                on_old=None,
                on_del=None):
-    self._interface = interface
-    self._my_ip = my_ip
-    self._netmask = netmask
-    self._ip_start = ip_start
-    self._ip_end = ip_end
+    self._interfaces = interfaces
+    self._interface_blacklist = interface_blacklist or []
+    self._exclude_ip_prefix = exclude_ip_prefix or []
     self._lease_time = lease_time
     self._bootp = bootp
     self._rpc_server = None
     self._process = None
     self._dhcp_action = {'add': on_add, 'old': on_old, 'del': on_del}
     self._callback_port = None
+    self._handled_interfaces = []
+
+  def _GetAvailibleInterfaces(self):
+    return [InterfaceProperty(interface)
+            for interface in network.GetUnmanagedEthernetInterfaces()
+            if interface not in self._interface_blacklist]
+
+  def _CollectInterfaceAndIPRange(self):
+    interfaces = self._interfaces or self._GetAvailibleInterfaces()
+    dhcp_ranges = []
+
+    used_range = list(self._exclude_ip_prefix or []) # make a copy
+
+    for interface in interfaces:
+      cidr = interface.cidr
+      if not cidr:
+        cidr = net_utils.GetUnusedIPV4RangeCIDR(exclude_ip_prefix=used_range)
+      # Make sure the interface is up
+      net_utils.SetEthernetIp(str(cidr.SelectIP(1)), interface.name,
+                              str(cidr.Netmask()), force=True)
+
+      used_range.append((str(cidr.SelectIP(1)), str(cidr.Netmask())))
+      ip_start = cidr.SelectIP(2)
+      ip_end = cidr.SelectIP(-3)
+      dhcp_ranges.extend(['--dhcp-range',
+                          '%s,%s,%d' % (ip_start, ip_end, self._lease_time)])
+    interfaces = [interface.name for interface in interfaces]
+    return interfaces, dhcp_ranges
+
+  def GetHandledInterfaces(self):
+    return self._handled_interfaces
 
   def StartDHCP(self):
     """Starts DHCP service."""
@@ -85,32 +132,31 @@ class DHCPManager(object):
                                          '%s%d' % (self.CALLBACK_PREFIX,
                                                    self._callback_port))
     os.symlink(callback_file_target, callback_file_symlink)
-    dhcp_range = '%s,%s,%d' % (self._ip_start, self._ip_end, self._lease_time)
+
+    interfaces, dhcp_ranges = self._CollectInterfaceAndIPRange()
+    self._handled_interfaces = interfaces
+
     dns_port = net_utils.FindUnusedTCPPort()
+    uid = random.getrandbits(64)
     lease_file = os.path.join(self.VARRUN_DIR,
-                              '%s%s' % (self.LEASE_PREFIX, self._interface))
+                              '%s%016x' % (self.LEASE_PREFIX, uid))
     pid_file = os.path.join(self.VARRUN_DIR,
-                            '%s%s' % (self.PID_PREFIX, self._interface))
-    # Make sure the interface is up
-    net_utils.SetEthernetIp(self._my_ip, self._interface, self._netmask)
+                            '%s%016x' % (self.PID_PREFIX, uid))
     # Start dnsmasq and have it call back to us on any DHCP event.
 
     cmd = ['dnsmasq',
            '--no-daemon',
-           '--dhcp-range', dhcp_range,
-           '--interface', self._interface,
            '--port', str(dns_port),
            '--no-dhcp-interface=%s' % net_utils.GetDefaultGatewayInterface(),
            '--dhcp-leasefile=%s' % lease_file,
            '--pid-file=%s' % pid_file,
            '--dhcp-script', callback_file_symlink]
+    cmd += ['--interface=%s' % ','.join(interfaces)]
+    cmd += dhcp_ranges
     if self._bootp:
       cmd.append('--dhcp-boot=%s,%s,%s' %
                  (self._bootp[1], self._bootp[2], self._bootp[0]))
     self._process = process_utils.Spawn(cmd, sudo=True, log=True)
-    # Make sure the IP address is set on the interface
-    net_utils.SetEthernetIp(self._my_ip, self._interface, self._netmask,
-                            force=True)
     # Make sure DHCP packets are not blocked
     process_utils.Spawn(['iptables',
                          '--insert', 'INPUT',
@@ -126,7 +172,8 @@ class DHCPManager(object):
     self._process = None
     self._rpc_server.Destroy()
     self._rpc_server = None
-    net_utils.Ifconfig(self._interface, enable=False)
+    for interface in self._handled_interfaces:
+      net_utils.Ifconfig(interface, enable=False)
     callback_file_symlink = os.path.join(self.VARRUN_DIR,
                                          '%s%d' % (self.CALLBACK_PREFIX,
                                                    self._callback_port))
@@ -173,6 +220,58 @@ class DHCPManager(object):
         os.unlink(os.path.join(cls.VARRUN_DIR, run_file))
 
 
+def StartDHCPManager(interfaces=None,
+                     blacklist_file=None,
+                     exclude_ip_prefix=None,
+                     lease_time=None,
+                     on_add=None,
+                     on_old=None,
+                     on_del=None):
+
+  DHCPManager.CleanupStaleInstance()
+  if utils.in_cros_device():
+    # Wait for shill to start
+    sync_utils.WaitFor(lambda: service_manager.GetServiceStatus('shill') ==
+                       service_manager.Status.START, 15)
+
+  # Get bootp parameters from gateway DHCP server
+  def _GetDefaultGatewayInterface():
+    try:
+      return net_utils.GetDefaultGatewayInterface()
+    except RuntimeError:
+      return False
+  default_iface = sync_utils.WaitFor(_GetDefaultGatewayInterface, 10)
+  bootp_params = network.GetDHCPBootParameters(default_iface)
+
+  # arguments for DHCP manager
+  kargs = {
+      'interfaces': interfaces,
+      'interface_blacklist': network.GetDHCPInterfaceBlacklist(blacklist_file),
+      'exclude_ip_prefix': exclude_ip_prefix,
+      'lease_time': lease_time,
+      'bootp': bootp_params,
+      'on_add': on_add,
+      'on_old': on_old,
+      'on_del': on_del}
+
+  # remove None to use default value
+  kargs = {k: v for (k, v) in kargs.iteritems() if v is not None}
+
+  manager = DHCPManager(**kargs)
+  manager.StartDHCP()
+
+  # Start NAT service
+  interfaces = manager.GetHandledInterfaces()
+  managed_interfaces = [x for x in net_utils.GetEthernetInterfaces()
+                        if x not in interfaces]
+  if not managed_interfaces:
+    return manager
+  nat_out_interface = managed_interfaces[0]
+  net_utils.StartNATService(interfaces, nat_out_interface)
+
+  return manager
+
+
 class DummyDHCPManager(object):
   """A dummy DHCPManager.
 
@@ -187,6 +286,9 @@ class DummyDHCPManager(object):
 
   def StopDHCP(self):
     pass
+
+  def GetHandledInterfaces(self):
+    return []
 
   @classmethod
   def CleanupStaleInstance(cls):

py/goofy/goofy_presenter_unittest.py
@@ -29,9 +29,8 @@ class GoofyPresenterTest(unittest.TestCase):
   """Base class for GoofyPresenter test cases."""
 
   def setUp(self):
-    link_manager.dhcp_utils.DHCPManager = dhcp_utils.DummyDHCPManager
-    link_manager.net_utils.GetUnmanagedEthernetInterfaces = lambda: ['eth0']
-    link_manager.net_utils.StartNATService = lambda intf_in, intf_out: None
+    dummy_server = dhcp_utils.DummyDHCPManager()
+    link_manager.dhcp_utils.StartDHCPManager = lambda **kargs: dummy_server
     self.goofy = GoofyPresenter()
     self.goofy.link_manager.Start()
 

py/goofy/link_manager.py
@@ -18,12 +18,8 @@ import time
 
 import factory_common  # pylint: disable=W0611
 from cros.factory.test import factory
-from cros.factory.test import utils
 from cros.factory.test import network
-from cros.factory.test.utils import service_manager
 from cros.factory.utils import dhcp_utils
-from cros.factory.utils import net_utils
-from cros.factory.utils import sync_utils
 from cros.factory.utils.jsonrpc_utils import JSONRPCServer
 from cros.factory.utils.jsonrpc_utils import TimeoutJSONRPCTransport
 from cros.factory.utils.process_utils import Spawn
@@ -331,7 +327,7 @@ class DUTLinkManager(object):
     self._ping_server = PingServer(PRESENTER_PING_PORT)
     self._thread = threading.Thread(target=self.MonitorLink)
 
-    self._dhcp_servers = []
+    self._dhcp_server = None
     self._dhcp_event_ip = None
     self._relay_process = None
 
@@ -378,69 +374,14 @@ class DUTLinkManager(object):
     except: # pylint: disable=W0702
       pass
 
-  def _GetDHCPInterfaceBlacklist(self):
-    """Returns the blacklist of DHCP interfaces.
-
-    This parses board/dhcp_interface_blacklist for a list of network
-    interfaces on which we don't want to run DHCP.
-    """
-    blacklist_file = os.path.join(factory.FACTORY_PATH, 'board',
-                                  'dhcp_interface_blacklist')
-    if os.path.exists(blacklist_file):
-      with open(blacklist_file) as f:
-        return [line.strip() for line in f.readlines()]
-    return []
-
-  def _StartDHCPServers(self):
-    dhcp_utils.DHCPManager.CleanupStaleInstance()
-    if utils.in_cros_device():
-      # Wait for shill to start
-      sync_utils.WaitFor(lambda: service_manager.GetServiceStatus('shill') ==
-                         service_manager.Status.START, 15)
-      # Give shill some time to run DHCP
-      time.sleep(3)
-
-    # Get bootp parameters from gateway DHCP server
-    default_iface = net_utils.GetDefaultGatewayInterface()
-    bootp_params = network.GetDHCPBootParameters(default_iface)
-
-    # OK, shill has done its job now. Let's see what interfaces are not managed.
-    interface_blacklist = self._GetDHCPInterfaceBlacklist()
-    interfaces = [interface
-                  for interface in network.GetUnmanagedEthernetInterfaces()
-                  if interface not in interface_blacklist]
-
-    for interface in interfaces:
-      network_cidr = net_utils.GetUnusedIPV4RangeCIDR()
-      # DHCP server IP assignment:
-      # my_ip: the first available IP. e.g.: 192.168.0.1
-      # ip_start: the second available IP. e.g.: 192.168.0.2
-      # ip_end: the third from the last IP. since .255 is the broadcast address
-      # and .254 is usually used by gateway, skip it to avoid unexpected
-      # problems.
-      dhcp_server = dhcp_utils.DHCPManager(
-          interface=interface,
-          my_ip=str(network_cidr.SelectIP(1)),
-          netmask=str(network_cidr.Netmask()),
-          ip_start=str(network_cidr.SelectIP(2)),
-          ip_end=str(network_cidr.SelectIP(-3)),
-          lease_time=3600,
-          bootp=bootp_params,
-          on_add=self.OnDHCPEvent,
-          on_old=self.OnDHCPEvent)
-      dhcp_server.StartDHCP()
-      self._dhcp_servers.append(dhcp_server)
-
-    # Start NAT service
-    managed_interfaces = [x for x in net_utils.GetEthernetInterfaces()
-                          if x not in interfaces]
-    if not managed_interfaces:
-      return
-    nat_out_interface = managed_interfaces[0]
-    net_utils.StartNATService(interfaces, nat_out_interface)
+  def _StartDHCPServer(self):
+    self._dhcp_server = dhcp_utils.StartDHCPManager(
+        lease_time=3600,
+        on_add=self.OnDHCPEvent,
+        on_old=self.OnDHCPEvent)
 
   def _StartOverlordRelay(self):
-    interface_blacklist = self._GetDHCPInterfaceBlacklist()
+    interface_blacklist = network.GetDHCPInterfaceBlacklist()
     interfaces = [interface
                   for interface in network.GetUnmanagedEthernetInterfaces()
                   if interface not in interface_blacklist]
@@ -462,13 +403,13 @@ class DUTLinkManager(object):
       self._dhcp_event_ip = LOCALHOST
       self._duts[LOCALHOST] = self.DUT(LOCALHOST, STANDALONE)
     else:
-      self._StartDHCPServers()
+      self._StartDHCPServer()
       self._StartOverlordRelay()
 
   def Stop(self):
     """Stops and destroys the link manager."""
-    for dhcp_server in self._dhcp_servers:
-      dhcp_server.StopDHCP()
+    if self._dhcp_server:
+      self._dhcp_server.StopDHCP()
     self._server.Destroy()
     self._abort_event.set()
     self._kick_event.set()

py/goofy/link_manager_unittest.py
@@ -26,8 +26,9 @@ class LinkManagerTest(unittest.TestCase):
     self.dut_link = None
     self.presenter_link = None
     self.hook = mox.MockAnything()
-    link_manager.dhcp_utils.DHCPManager = dhcp_utils.DummyDHCPManager
-    link_manager.net_utils.GetUnmanagedEthernetInterfaces = lambda: ['eth0']
+
+    dhcp_server = dhcp_utils.DummyDHCPManager()
+    link_manager.dhcp_utils.StartDHCPManager = lambda **kargs: dhcp_server
 
     link_manager.PRESENTER_LINK_RPC_PORT = net_utils.FindUnusedTCPPort()
     link_manager.PRESENTER_PING_PORT = net_utils.FindUnusedTCPPort()

py/test/dut/links/ssh.py
@@ -9,12 +9,14 @@ import logging
 import pipes
 import subprocess
 import threading
+import types
 
 import factory_common  # pylint: disable=W0611
 from cros.factory.test import factory
 from cros.factory.test.dut import link
 from cros.factory.utils import file_utils
-from cros.factory.utils.dhcp_utils import DHCPManager
+from cros.factory.utils import type_utils
+from cros.factory.utils import dhcp_utils
 
 
 _DEVICE_DATA_KEY = 'DYNAMIC_SSH_TARGET_IP'
@@ -29,15 +31,41 @@ class SSHLink(link.DUTLink):
   """A DUT target that is connected via SSH interface.
 
   Properties:
-    host: A string for SSH host.
+    host: A string for SSH host, if it's None, will get from shared data.
     user: A string for the user accont to login. Defaults to 'root'.
     port: An integer for the SSH port on remote host.
     identify: An identity file to specify credential.
+
+  dut_options example:
+    dut_options for fixed-IP:
+      {
+        'board_class': 'CoolBoard',
+        'link_class': 'SSHLink',
+        'host': '1.2.3.4',
+        'identity': '/path/to/identity/file'
+        'start_dhcp_server': False
+      }
+    dut_options for DHCP:
+      {
+        'board_class': 'CoolBoard',
+        'link_class': 'SSHLink',
+        'host': None,
+        'identity': '/path/to/identity/file',
+        'start_dhcp_server': True,
+        'dhcp_server_args': {
+          'lease_time': 3600,
+          'interface_blacklist_file': '/path/to/blacklist/file',
+          'exclude_ip_prefix': [('10.0.0.0', 24), ...],
+          # the following three properties can oly be set in python script,
+          # not in environment variable (CROS_FACTORY_DUT_OPTIONS)
+          'on_add': None,
+          'on_old': None,
+          'on_del': None,
+        }
+      }
   """
 
-  DYNAMIC_HOST = 'dynamic'
-
-  def __init__(self, host, user='root', port=22, identity=None):
+  def __init__(self, host=None, user='root', port=22, identity=None):
     self._host = host
     self.user = user
     self.port = port
@@ -45,7 +73,7 @@ class SSHLink(link.DUTLink):
 
   @property
   def host(self):
-    if self._host == SSHLink.DYNAMIC_HOST:
+    if self._host == None:
       if not factory.has_shared_data(_DEVICE_DATA_KEY):
         raise ClientNotExistError()
       return factory.get_shared_data(_DEVICE_DATA_KEY)
@@ -125,35 +153,122 @@ class SSHLink(link.DUTLink):
   _dhcp_manager_lock = threading.Lock()
 
   @classmethod
-  def DHCPConnected(cls, dut_ip, unused_mac):
-    """Event handler for adding new client or lease renewal
-
-    Save the IP address in device data.
-    """
-    factory.set_shared_data(_DEVICE_DATA_KEY, dut_ip)
+  def SetLinkIP(cls, ip):
+    factory.set_shared_data(_DEVICE_DATA_KEY, ip)
 
   @classmethod
-  def DHCPDisconnected(cls, unused_ip, unused_mac):
-    """Event handler for lease expired
-
-    Remove the IP address from device data.
-    """
+  def ResetLinkIP(cls):
     if factory.has_shared_data(_DEVICE_DATA_KEY):
       factory.del_shared_data(_DEVICE_DATA_KEY)
 
   @classmethod
-  def PrepareLink(cls, **dut_options):
-    host = dut_options['host']
-    if host == SSHLink.DYNAMIC_HOST:
-      with cls._dhcp_manager_lock:
-        if cls._dhcp_manager:
-          return
-        # TODO(stimim): automatically find out which network interface should be
-        #               used.
-        cls._dhcp_manager = DHCPManager(
-            'eth1',
-            lease_time=5,
-            on_add=cls.DHCPConnected,
-            on_old=cls.DHCPConnected,
-            on_del=cls.DHCPDisconnected)
-        cls._dhcp_manager.StartDHCP()
+  def PrepareLink(cls, start_dhcp_server=True, dhcp_server_args=None):
+    """Prepare for SSHLink connection
+
+    Arguments:
+      start_dhcp_server (default: False):
+        Start the default DHCP server or not
+      dhcp_server_args (default: None):
+        If ``start_dhcp_server`` is True, this will be passed to the default
+        DHCP server (ssh.LinkManager)
+    """
+    if not start_dhcp_server:
+      return
+    with cls._dhcp_manager_lock:
+      if cls._dhcp_manager:
+        return
+      options = dict(lease_time=5)
+      options.update(dhcp_server_args or {})
+
+      cls._dhcp_manager = cls.LinkManager(**options)
+      cls._dhcp_manager.Start()
+
+  class LinkManager(object):
+    def __init__(self,
+                 lease_time=3600,
+                 interface_blacklist_file=None,
+                 exclude_ip_prefix=None,
+                 on_add=None,
+                 on_old=None,
+                 on_del=None):
+      """
+        A LinkManager will automatically start a DHCP server for each availiable
+        network interfaces, if the interface is not default gateway or in the
+        blacklist.
+
+        This LinkManager will automatically save IP of the latest client in
+        system-wise shared data, make it availible to SSHLinks whose host is set
+        to None.
+
+        Options:
+          lease_time:
+            lease time of DHCP servers
+          interface_blacklist_file:
+            a path to the file of blacklist, each line represents an interface
+            (e.g. eth0, wlan1, ...)
+          exclude_ip_prefix:
+            some IP range cannot be used becase of system settings, this argument
+            should be a list of tuple of (ip, prefix_bits).
+          on_add, on_old, on_del:
+            callback functions for DHCP servers.
+      """
+      self._lease_time = lease_time
+      self._blacklist_file = interface_blacklist_file
+      self._on_add = on_add
+      self._on_old = on_old
+      self._on_del = on_del
+      self._dhcp_server = None
+      self._exclude_ip_prefix = exclude_ip_prefix
+
+      self._duts = type_utils.UniqueStack()
+
+    def _SetLastDUT(self):
+      last_dut = self._duts.Get()
+      if last_dut:
+        SSHLink.SetLinkIP(last_dut[0])
+      else:
+        SSHLink.ResetLinkIP()
+
+    def _OnDHCPAdd(self, ip, mac_address):
+      # update last device
+      self._duts.Add((ip, mac_address))
+      self._SetLastDUT()
+
+      # invoke callback function
+      if isinstance(self._on_add, types.FunctionType):
+        self._on_add(ip, mac_address)
+
+    def _OnDHCPOld(self, ip, mac_address):
+      # update last device
+      self._duts.Add((ip, mac_address))
+      self._SetLastDUT()
+
+      # invoke callback function
+      if isinstance(self._on_old, types.FunctionType):
+        self._on_old(ip, mac_address)
+
+    def _OnDHCPDel(self, ip, mac_address):
+      # remove the device
+      self._duts.Del((ip, mac_address))
+      self._SetLastDUT()
+
+      # invoke callback function
+      if isinstance(self._on_del, types.FunctionType):
+        self._on_del(ip, mac_address)
+
+    def _StartHDCPServer(self):
+      self._dhcp_server = dhcp_utils.StartDHCPManager(
+          blacklist_file=self._blacklist_file,
+          exclude_ip_prefix=self._exclude_ip_prefix,
+          lease_time=self._lease_time,
+          on_add=self._OnDHCPAdd,
+          on_old=self._OnDHCPOld,
+          on_del=self._OnDHCPDel)
+
+    def Start(self):
+      self._SetLastDUT()
+      self._StartHDCPServer()
+
+    def Stop(self):
+      if self._dhcp_server:
+        self._dhcp_server.StopDHCP()

py/test/network.py
@@ -284,3 +284,13 @@ def GetDHCPBootParameters(interface):
         return (ip, dhcp['file'].strip('\x00'), dhcp['sname'].strip('\x00'))
 
   return None
+
+
+def GetDHCPInterfaceBlacklist(blacklist_file=None):
+  if not blacklist_file:
+    blacklist_file = os.path.join(factory.FACTORY_PATH, 'board',
+                                  'dhcp_interface_blacklist')
+    if os.path.exists(blacklist_file):
+      with open(blacklist_file) as f:
+        return map(str.strip, f.readlines())
+    return []

py/test/test_lists/fixture_based_test_list.py
@@ -31,7 +31,9 @@ def FixtureBased(test_list_id, label_en, label_zh, dut_options=None):
 
       Example:
 
-      dut_options = {'dut_class': 'SSHTarget', 'host': 'dynamic'}
+      ::
+
+      dut_options = {'link_class': SSHLink, 'host': None}
       @FixtureBased('main', 'CoolProduct EVT', dut_options)
       def CreateTestLists(test_list):
         # dut_options is automatically set to test_list,

commit a90887748677d06044c852d558cef6bb147fbff6

net_utils: fix GetUnusedIPV4RangeCIDR and unittest
Fix incorrect change caused by 44c716f17aec3259d2bbc93b24de52aa98b5843d.
Should add an extra variable to determine if we want to include local
interface IPs.

BUG=none
TEST=`py/utils/net_utils_unittest.py`

Change-Id: I612aa436eddcd03fead067bb420a021e687363f1
Reviewed-on: https://chromium-review.googlesource.com/322523
Commit-Ready: Wei-Ning Huang <wnhuang@chromium.org>
Tested-by: Wei-Ning Huang <wnhuang@chromium.org>
Reviewed-by: Wei-Han Chen <stimim@chromium.org>

py/utils/net_utils.py
@@ -159,7 +159,9 @@ class TimeoutXMLRPCServerProxy(xmlrpclib.ServerProxy):
     xmlrpclib.ServerProxy.__init__(self, uri, *args, **kwargs)
 
 
-def FindUsableEthDevice(raise_exception=False, name_patterns=DEFAULT_ETHERNET_NAME_PATTERNS):
+def FindUsableEthDevice(raise_exception=False,
+                        name_patterns=DEFAULT_ETHERNET_NAME_PATTERNS):
+  # pylint: disable=W0102
   """Find the real ethernet interface when the flimflam is unavailable.
 
   Some devices with 4G modules may bring up fake eth interfaces during
@@ -324,6 +326,7 @@ def GetWLANInterface():
 
 
 def GetEthernetInterfaces(name_patterns=DEFAULT_ETHERNET_NAME_PATTERNS):
+  # pylint: disable=W0102
   """Returns the interfaces for Ethernet.
 
   Args:
@@ -335,11 +338,14 @@ def GetEthernetInterfaces(name_patterns=DEFAULT_ETHERNET_NAME_PATTERNS):
   """
   interfaces = []
   for name in name_patterns:
-    interfaces += [os.path.basename(path) for path in glob.glob('/sys/class/net/' + name)]
+    interfaces += [os.path.basename(path) for path in
+                   glob.glob('/sys/class/net/' + name)]
   return interfaces
 
 
-def SwitchEthernetInterfaces(enable, name_patterns=DEFAULT_ETHERNET_NAME_PATTERNS):
+def SwitchEthernetInterfaces(enable,
+                             name_patterns=DEFAULT_ETHERNET_NAME_PATTERNS):
+  # pylint: disable=W0102
   """Switches on/off all Ethernet interfaces.
 
   Args:
@@ -543,12 +549,14 @@ def GetDefaultGatewayInterface():
     raise RuntimeError('no default gateway found')
 
 
-def GetUnusedIPV4RangeCIDR(preferred_prefix_bits=24, exclude_ip_prefix=None):
+def GetUnusedIPV4RangeCIDR(preferred_prefix_bits=24, exclude_ip_prefix=None,
+                           exclude_local_interface_ip=False):
   """Find unused IP ranges in IPV4 private address space.
 
   Args:
-    preferred_prefix_bits: the preferred prefix length in bits
+    preferred_prefix_bits: The preferred prefix length in bits.
     exclude_ip_prefix: A list of tuple of (ip, prefix_bits) to exclude.
+    exclude_local_interface_ip: Also exclude IP used by the local interface.
 
   Returns:
     A CIDR object representing the network range.
@@ -559,14 +567,15 @@ def GetUnusedIPV4RangeCIDR(preferred_prefix_bits=24, exclude_ip_prefix=None):
       CIDR('192.168.0.0', 16)
   ]
 
-  # If no exclude_ip_prefix is specified, populate it with all the ip/prefix
-  # currently on the machine interfaces.
-  if not exclude_ip_prefix:
-    exclude_ip_prefix = []
-  for iface in GetNetworkInterfaces():
-    ip_mask = GetEthernetIp(iface, True)
-    if ip_mask[0] and ip_mask[1]:
-      exclude_ip_prefix.append(ip_mask)
+  exclude_ip_prefix = exclude_ip_prefix or []
+
+  # Exclude local interface IP, populate it with all the ip/prefix currently on
+  # the machine interfaces.
+  if exclude_local_interface_ip:
+    for iface in GetNetworkInterfaces():
+      ip_mask = GetEthernetIp(iface, True)
+      if ip_mask[0] and ip_mask[1]:
+        exclude_ip_prefix.append(ip_mask)
 
   # available_ranges_bits stores (ip_range, available_subnet_range_bits)
   # For example: available_ranges_bits = {0xc0a80000: 8}

py/test/utils/dhcp_utils.py
@@ -13,7 +13,6 @@ import os
 import random
 import signal
 import sys
-import time
 
 import factory_common  # pylint: disable=W0611
 from cros.factory.test import network
@@ -102,7 +101,8 @@ class DHCPManager(object):
     for interface in interfaces:
       cidr = interface.cidr
       if not cidr:
-        cidr = net_utils.GetUnusedIPV4RangeCIDR(exclude_ip_prefix=used_range)
+        cidr = net_utils.GetUnusedIPV4RangeCIDR(exclude_ip_prefix=used_range,
+                                                exclude_local_interface_ip=True)
       # Make sure the interface is up
       net_utils.SetEthernetIp(str(cidr.SelectIP(1)), interface.name,
                               str(cidr.Netmask()), force=True)