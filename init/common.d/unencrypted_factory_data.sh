3582b046ca360d2e70d78a5ff8e058633ffc71fc
Hung-Te Lin <hungte@chromium.org>
2016-02-19 22:47:18 +0800

    init: Add new rule 'unencrypted_factory_data'.
    For most Chromebook projects it seems more reasonable to put factory
    data in the unencrypted (which implies larger) part of stateful partition,
    which allows us not worrying about the size of encrypted block.
    
    BUG=chrome-os-partner:49833
    TEST=Manual. Boots factory UI and found /var/factory being symlink to
         /mnt/stateful_partition/var_overlay/factory.
    CQ-DEPEND=CL:328861
    
    Change-Id: Idc20fcd992f4f0f41e9314634d16e4e0c874cb9f
    Reviewed-on: https://chromium-review.googlesource.com/328574
    Commit-Ready: Hung-Te Lin <hungte@chromium.org>
    Tested-by: Hung-Te Lin <hungte@chromium.org>
    Reviewed-by: Wei-Ning Huang <wnhuang@chromium.org>

---
+++ init/common.d/unencrypted_factory_data.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# Copyright 2016 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# The /var is by default mounted as encrypted, using 1/10 of total stateful
+# partition (/mnt/stateful_partition/encrypted.block). If stateful partition
+# size is changed, the encrypted block won't change unless it's deleted (or
+# key changed). Since the stateful partition of a factory image is usually
+# 1GB, the encrypted block will be only 100MB and is usually not enough for
+# factory data (/var/factory). Considering that we will wipe data during
+# finalization and the factory data does not have user privacy, it seems
+# reasonable to redirect factory data (/var/factory) to the unencrypted path on
+# stateful parition.
+
+ROOT="/mnt/stateful_partition"
+
+main() {
+  local overlay="${ROOT}/var_overlay/factory"
+  local dest="/var/factory"
+
+  # Ensure the mount point still exist.
+  mkdir -p "${dest}"
+
+  if [ ! -d "${overlay}" ]; then
+    # Safely migrate from existing (or newly created) /var/factory.
+    mv "${dest}" "${overlay}"
+    mkdir -p "${dest}"
+  fi
+
+  # We use mount-bind instead of symlink because there are already many programs
+  # (factory_bug, dut_upload, ...) assuming the factory data can be simply
+  # collected using tar without solving symlinks.
+  mount --bind "${overlay}" "${dest}"
+}
+
+main "$@"
