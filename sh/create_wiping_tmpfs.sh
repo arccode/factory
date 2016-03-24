6c4f1184f80dd1202d8b40bfa7a03c6375196967
Shun-Hsing Ou <shunhsingou@chromium.org>
2016-03-24 15:33:37 +0800

    create_wipe_tmpfs: Create more complete wiping environment.
    Create more complete tmp filesystem for wiping to avoid some problems,
    e.g. we are not able to run dhcpc and open VT2 of frecon in the original
    implementation.
    
    BUG=chrome-os-partner:51605,chrome-os-partner:50659
    TEST=Manually on Chell
    
    Change-Id: I07600f8d6f7cc3bcfb0b9532aedb43b4d23f65b6
    Reviewed-on: https://chromium-review.googlesource.com/334682
    Commit-Ready: Shun-Hsing Ou <shunhsingou@chromium.org>
    Tested-by: Shun-Hsing Ou <shunhsingou@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

--- sh/create_wiping_tmpfs.sh
+++ sh/create_wiping_tmpfs.sh
@@ -28,6 +28,15 @@ PANGO_MODULE="$(pango-querymodules --system |
 # "src_dir:dst_dir" meaning copy the src_file/src_dir in factory rootfs to
 # dst_file/dst_dir in the tmpfs.
 FILES_DIRS_COPIED_FROM_ROOTFS="
+  /bin
+  /etc
+  /lib
+  /lib64
+  /opt
+  /root
+  /sbin
+  /usr/share/fonts/notocjk
+  /usr/share/cache/fontconfig
   ${ASSETS_DIR}/images
   ${ASSETS_DIR}/text/boot_messages
   ${SCRIPT_DIR}/battery_cutoff.sh
@@ -40,11 +49,6 @@ FILES_DIRS_COPIED_FROM_ROOTFS="
   ${SCRIPT_DIR}/wipe_in_tmpfs.sh
   ${PANGO_MODULE}
   ${MISC_DIR}/chromeos-common.sh
-  /etc/fonts
-  /etc/pango
-  /lib/modules
-  /usr/share/fonts/notocjk
-  /usr/share/cache/fontconfig
 "
 
 # Layout of directories to be created in tmpfs
@@ -59,6 +63,7 @@ TMPFS_LAYOUT_DIRS="
   root
   sys
   tmp
+  var
 "
 
 # Dependency list of binary programs.
@@ -117,6 +122,8 @@ create_tmpfs_layout() {
   ln -s . "${TMPFS_PATH}/usr"
   ln -s . "${TMPFS_PATH}/local"
   ln -s bin "${TMPFS_PATH}/sbin"
+  ln -s /run "${TMPFS_PATH}/var/run"
+  ln -s /run/lock "${TMPFS_PATH}/var/lock"
 }
 
 copy_dependent_binary_files() {
