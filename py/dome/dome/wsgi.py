0dd3f4b18cb3f8819076cf1de467568c88bb077d
Mao Huang <littlecvr@chromium.org>
2016-03-25 19:30:27 +0800

    dome: initial commit
    This is the skeleton generated by django-admin:
    - startproject dome
    - startapp bundles
    
    with the following modifications:
    - add copyright text to each file
    - remove unused imports
    - fix coding style (indentation, blank lines, etc.)
    
    BUG=chrome-os-partner:51877
    TEST=./manage.py migrate && ./manage.py runserver
    
    Change-Id: I7451fa7b1380c2fabc13ab0ba4a71310116c643a
    Reviewed-on: https://chromium-review.googlesource.com/335022
    Commit-Ready: Mao Huang <littlecvr@chromium.org>
    Tested-by: Mao Huang <littlecvr@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

---
+++ py/dome/dome/wsgi.py
@@ -0,0 +1,20 @@
+# Copyright 2016 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""WSGI config for dome project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dome.settings')
+
+application = get_wsgi_application()
---
+++ py/dome/manage.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# Copyright 2016 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import sys
+
+
+if __name__ == '__main__':
+  os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dome.settings')
+
+  from django.core.management import execute_from_command_line
+
+  execute_from_command_line(sys.argv)
