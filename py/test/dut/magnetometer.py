0e3fd54ce3660d1b62949b871ffad11489728a0a
Ting Shen <phoenixshen@google.com>
2016-03-29 05:51:35 +0800

    dut: magnetometer module
    New module for magnetometer.
    
    BUG=None
    TEST=None
    
    Change-Id: I3ac218e3828ab67a500202a148ddf845e17b6f4f
    Reviewed-on: https://chromium-review.googlesource.com/335424
    Commit-Ready: Ting Shen <phoenixshen@chromium.org>
    Tested-by: Ting Shen <phoenixshen@chromium.org>
    Reviewed-by: Shun-Hsing Ou <shunhsingou@chromium.org>

---
+++ py/test/dut/magnetometer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+#
+# Copyright 2016 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import factory_common  # pylint: disable=W0611
+from cros.factory.test.dut import component
+
+
+class Magnetometer(component.DUTComponent):
+  """Base class for magnetometer component module."""
+
+  def __init__(self, board):
+    super(Magnetometer, self).__init__(board)
+
+  def GetRawDataAverage(self, capture_count=1):
+    """Reads several records of raw data and returns the average.
+
+    Args:
+      capture_count: how many records to read to compute the average.
+
+    Returns:
+      A dict of the format {'signal_name': average value}
+    """
+    raise NotImplementedError
