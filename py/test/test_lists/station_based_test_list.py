1ec3a4a477a3f7864fba8e5f6129447f077f0457
Wei-Han Chen <stimim@google.com>
2016-04-29 10:16:59 +0800

    station-based: add prestart_steps for station_based_test_list
    Some operations (e.g. scan serial number) better to be done before the
    DUT is connected to the station. This change allows this to be possible.
    
    We also stop clearing device data on StartStationTest, since we've
    cleared it on EndStationTest. And we might want to setup device data in
    'prestart_steps'.
    
    BUG=chromium:506788
    TEST=None
    
    Change-Id: Ibd25d6821edecc87c886af3d55152eaf41593fe4
    Reviewed-on: https://chromium-review.googlesource.com/341516
    Commit-Ready: Wei-Han Chen <stimim@chromium.org>
    Tested-by: Wei-Han Chen <stimim@chromium.org>
    Reviewed-by: Chih-Yu Huang <akahuang@chromium.org>

--- py/test/test_lists/station_based_test_list.py
+++ py/test/test_lists/station_based_test_list.py
@@ -17,7 +17,8 @@ def StartStationTest(test_list_id, label_en, label_zh, prompt_start):
       label_en=u'Start %s' % label_en,
       label_zh=u'开始 %s' % label_zh,
       pytest_name='station_entry',
-      dargs={'prompt_start': prompt_start})
+      dargs={'prompt_start': prompt_start,
+             'clear_device_data': False})
 
 
 def EndStationTest(test_list_id, label_en, label_zh):
@@ -32,30 +33,45 @@ def EndStationTest(test_list_id, label_en, label_zh):
 def StationBased(test_list_id, label_en, label_zh,
                  dut_options=None,
                  automated_sequence=True,
-                 prompt_start=True):
+                 prompt_start=True,
+                 prestart_steps=None):
   """A decorator to add common test items for station-based tests.
 
   Args:
+    dut_options: Override default (e.g. environment variable) DUT options.
     automated_sequence: Wrap the test list in an AutomatedSequence test group,
       preventing any test inside from being run individually.
     prompt_start: Prompt the operator to press the spacebar before the test
       sequence actually begins.  Default is True.
+    prestart_steps: A list of callable object that will be called before
+      adding StartStationTest.
 
   Example:
 
   ::
 
     dut_options = {'link_class': SSHLink, 'host': None}
-    @StationBased('main', 'CoolProduct EVT', dut_options)
+    @StationBased('main', 'CoolProduct EVT', dut_options,
+                  prestart_steps=ScanBarcode)
     def CreateTestLists(test_list):
       # dut_options is automatically set to test_list,
       # you can set other options for test_list here
-      with TestGroup(...):
+      with TestGroup(id='TestGroupA', ...):
         ...
-      OperatorTest(...)
+      OperatorTest(id='Test1', ...)
 
   Then CreateTestLists() will create a test list named 'main', with
   label 'CoolProduct EVT'.
+
+  And the test list will be::
+    main  # (test group)
+      ScanBarcode  # (test created by prestart_steps)
+      StartStationTest  # (added by this wrapper)
+      TestGroupA
+        ...
+      ...
+      Test1
+      EndStationTest  # (added by this wrapper)
   """
   if not dut_options:
     dut_options = {}
@@ -73,6 +89,9 @@ def StationBased(test_list_id, label_en, label_zh,
               id=test_list_id, label_en=label_en, label_zh=label_zh)
 
         with group:
+          if prestart_steps:
+            for step in prestart_steps:
+              step()
           StartStationTest(test_list_id, label_en, label_zh, prompt_start)
           CreateTestLists(test_list)
           EndStationTest(test_list_id, label_en, label_zh)
