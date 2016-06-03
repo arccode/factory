d667ba80e297cabd224813fdbe1a9e27d6f3ec5c
Mao Huang <littlecvr@chromium.org>
2016-06-03 14:37:26 +0800

    dome: bundles: implement Bundle class
    This class provides methods to manipulate bundles in umpire config.
    
    BUG=chromium:617496
    TEST=Manually tested
    
    Change-Id: I22cec7ea78425b8187a59af709fff716e8cc8597
    Reviewed-on: https://chromium-review.googlesource.com/350623
    Commit-Ready: Mao Huang <littlecvr@chromium.org>
    Tested-by: Mao Huang <littlecvr@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

--- py/dome/bundles/models.py
+++ py/dome/bundles/models.py
@@ -13,18 +13,110 @@ TODO(littlecvr): make umpire config complete such that it contains all the
                  information we need.
 """
 
+import os
+import xmlrpclib
+
+import yaml
+
+# TODO(littlecvr): DRY
+UMPIRE_BASE_DIR = os.path.join('/', 'var', 'db', 'factory', 'umpire')
+
+
 class Bundle(object):
-  """Provide functions to manipulate bundles in umpire."""
+  """Provide functions to manipulate bundles in umpire.
 
-  def __init__(self, name):
-    pass
+  Umpire RPC calls aren't quite complete. For example, they do not provide any
+  function to list bundles. So this class assumes that umpire runs on the same
+  machine and is stored at its default location.
+
+  TODO(littlecvr): complete the umpire RPC calls, decouple dome and umpire.
+  """
+
+  def __init__(self, name, note, file_path=None):
+    self.name = name
+    self.note = note
+
+    self.file_path = file_path
+
+    # TODO(littlecvr): add rulesets in umpire config.
+    # TODO(littlecvr): add parts (toolkit, FSI, etc.)
 
   @staticmethod
-  def ListAll():
-    """Return all bundles as a list."""
-    raise NotImplementedError
+  def _GetUmpireConfig(board):
+    # TODO(littlecvr): DRY
+    active_config_path = os.path.join(
+        UMPIRE_BASE_DIR, board, 'active_umpire.yaml')
+    with open(active_config_path) as f:
+      return yaml.load(f.read())
 
   @staticmethod
-  def UploadNew():
-    """Upload a new bundle."""
-    raise NotImplementedError
+  def ListAll(board):
+    """Return all bundles as a list.
+
+    This function returns all bundles as a list, no matter if the bundle is
+    active or not. It is the front-end's responsibility to filter only bundles
+    the user wants to see.
+
+    Args:
+      board: a string for name of the board.
+
+    Return:
+      A list of all bundles.
+    """
+    umpire_config = Bundle._GetUmpireConfig(board)
+
+    bundle_list = []
+    for b in umpire_config.get('bundles'):
+      bundle_list.append(Bundle(b['id'], b.get('note')))
+
+    return bundle_list
+
+  @staticmethod
+  def UploadNew(board, name, note, file_path):
+    """Upload a new bundle.
+
+    Args:
+      board: the board name in string.
+      name: name of the new bundle, in string. This corresponds to the "id"
+          field in umpire config.
+      note: commit message. This corresponds to the "note" field in umpire
+          config.
+      file_path: path to the bundle file. If the file is a temporary file, the
+          caller is responsible for removing it.
+
+    Return:
+      The newly created bundle.
+    """
+    # create umpire RPC server object
+    umpire_config = Bundle._GetUmpireConfig(board)
+    # TODO(littlecvr): DRY
+    port = umpire_config['port'] + 2  # cli port offset
+    # TODO(littlecvr): DRY
+    umpire_rpc_server = xmlrpclib.ServerProxy('http://localhost:%d' % port)
+
+    # import bundle
+    staging_config_path = umpire_rpc_server.ImportBundle(
+        os.path.realpath(file_path), name, note)
+
+    # make the bundle active
+    with open(staging_config_path) as f:
+      config = yaml.load(f.read())
+    for ruleset in config['rulesets']:
+      if ruleset['bundle_id'] == name:
+        ruleset['active'] = True
+        break
+    with open(staging_config_path, 'w') as f:
+      f.write(yaml.dump(config, default_flow_style=False))
+
+    # validate and deploy
+    umpire_status = umpire_rpc_server.GetStatus()
+    config_to_deploy_text = umpire_status['staging_config']
+    config_to_deploy_res = umpire_status['staging_config_res']
+    umpire_rpc_server.ValidateConfig(config_to_deploy_text)
+    umpire_rpc_server.Deploy(config_to_deploy_res)
+
+    bundle_list = Bundle.ListAll(board)
+    for b in bundle_list:
+      if b.name == name:
+        return b
+    return None
