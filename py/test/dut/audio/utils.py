2630aaaf85d86c56e35121365a2e907f40608c3c
mojahsu <mojahsu@google.com>
2016-06-27 18:28:18 +0800

    dut: Decide which audio layer to use properly.
    We may have both amixer and tinymix in system. So only check one of them
    is not enough. Adjust it to check all needed programs for the audio
    layer.
    
    BUG=None
    TEST=Tested manually
    
    Change-Id: I38fa22a197106a7349be84726071cc3c3bb9db16
    Reviewed-on: https://chromium-review.googlesource.com/356286
    Commit-Ready: Hsu Wei-Cheng <mojahsu@chromium.org>
    Tested-by: Shun-Hsing Ou <shunhsingou@chromium.org>
    Reviewed-by: Shun-Hsing Ou <shunhsingou@chromium.org>

--- py/test/dut/audio/utils.py
+++ py/test/dut/audio/utils.py
@@ -13,9 +13,12 @@ from cros.factory.test.dut.audio import tinyalsa
 def CreateAudioControl(dut, config_path=base.DEFAULT_CONFIG_PATH):
   """Creates an AudioControl instance."""
   # Use which to probe audio control class
-  if dut.Call(['which', 'amixer']) == 0:
+  # We need to check if all tools required are in the device.
+  alsa_commands = ['amixer', 'aplay', 'arecord']
+  tinyalsa_commands = ['tinymix', 'tinyplay', 'tinycap']
+  if all(dut.Call(['which', command]) == 0 for command in alsa_commands):
     return alsa.AlsaAudioControl(dut, config_path)
-  elif dut.Call(['which', 'tinymix']) == 0:
+  elif all(dut.Call(['which', command]) == 0 for command in tinyalsa_commands):
     return tinyalsa.TinyalsaAudioControl(dut, config_path)
   else:
     raise NotImplementedError
