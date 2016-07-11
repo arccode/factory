2ee6580b8993c624e91eb1572e0dd516bca71c8e
mojahsu <mojahsu@google.com>
2016-07-12 07:12:23 +0800

    adding hwmon_write pytest
    hwmon_write pytest writes value to specified hwmon devices.
    
    BUG=None
    TEST=hwmon_write factory pytest works
    
    Change-Id: I97d68e7282ae889f6a239050eaf2a4e0aceeb5a1
    Reviewed-on: https://chromium-review.googlesource.com/359954
    Commit-Ready: Hsu Wei-Cheng <mojahsu@chromium.org>
    Tested-by: Hsu Wei-Cheng <mojahsu@chromium.org>
    Reviewed-by: Shun-Hsing Ou <shunhsingou@chromium.org>

---
+++ py/test/pytests/hwmon_write.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2016 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Write value to hwmon files."""
+
+import unittest
+
+import factory_common  # pylint: disable=W0611
+from cros.factory.test import dut
+from cros.factory.test import factory
+from cros.factory.utils.arg_utils import Arg
+
+
+class HwmonWriteTest(unittest.TestCase):
+  """Write value the the hwmon files."""
+  ARGS = [
+      Arg('name', str, 'The name of the hwmon.'),
+      Arg('index', int,
+          'The hwmon index if we have more than 1 with the same name.',
+          default=0),
+      Arg('file', str, 'The filename to write.'),
+      Arg('value', str, 'The value we want to write to the file.'),
+  ]
+
+  def setUp(self):
+    self._dut = dut.Create()
+
+  def runTest(self):
+    devices = self._dut.hwmon.FindDevices('name', self.args.name)
+    device = devices[self.args.index]
+    path = self._dut.path.join(device.GetPath(), self.args.file)
+    factory.console.info('Write %s to %s', self.args.value, path)
+    self._dut.WriteFile(path, self.args.value)
