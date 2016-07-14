c248a77a096c49458772fb04ef26719045cd263a
Mao Huang <littlecvr@chromium.org>
2016-07-14 12:09:07 +0800

    dome: implement easy deployment
    - Add docker files for builder and runner
    - Add required config files:
      + requirements.txt
      + uwsgi.ini
      + nginx.conf
    - Add an deploying script
    
    Steps to deploy:
    1. sudo ./dome.sh, it will automatically build Dome, and start both
       dome_uwsgi, dome_nginx containers. If you don't want sudo, you need
       to `mkdir /docker_shared` and chown to yourself first, add yourself
       into the docker group.
    2. Open the URL http://localhost:8000/ in browser.
    3. Saw the Dome welcome page.
    
    BUG=b:30999924
    TEST=Manually tested by following the steps mentioned above
    
    Change-Id: I770dfd07e2cd61eb82181eaaa875300fc454216c
    Reviewed-on: https://chromium-review.googlesource.com/379679
    Commit-Ready: Mao Huang <littlecvr@chromium.org>
    Tested-by: Mao Huang <littlecvr@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

---
+++ py/dome/docker/Dockerfile.builder
@@ -0,0 +1,41 @@
+# Copyright 2016 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# This is the image for building Dome. There is another image for running Dome.
+# The reason to separate them is because building requires much more
+# dependencies. The dependencies are really big and we don't want to pull them
+# all into the running image since they are useless after the build.
+
+FROM node:4.5-slim
+MAINTAINER Mao Huang <littlecvr@google.com>
+
+# mixing ARG and ENV to make CMD able to use the variable, this technique is
+# described here: https://docs.docker.com/engine/reference/builder/#arg
+ARG workdir="/usr/src/app"
+ENV workdir="${workdir}"
+
+WORKDIR "${workdir}"
+
+# copy package.json and pull in dependencies first, so we don't need to do this
+# again if package.json hasn't been modified
+COPY frontend/package.json "${workdir}/"
+# npm outputs messages to stderr and docker makes them red, which is pretty
+# scaring, redirect them to stdout
+RUN npm install 2>&1
+RUN npm dedupe 2>&1
+
+# build
+COPY frontend "${workdir}/"
+RUN npm run build
+
+# make sure others can read
+RUN chmod 644 index.html bundle.js main.css
+
+ARG output_file="frontend.tar"
+ENV output_file="${output_file}"
+
+RUN tar cvf "${output_file}" index.html bundle.js main.css
+
+# nothing to do here
+CMD ["echo"]
---
+++ py/dome/dome.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+# Copyright 2016 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# TODO(littlecvr): functionize this file
+
+set -e
+
+SCRIPT_DIR=$(realpath $(dirname "${BASH_SOURCE[0]}"))
+
+DOCKER_SHARED_DIR="/docker_shared/dome"
+
+DB_FILE="db.sqlite3"
+BUILDER_WORKDIR="/usr/src/app"
+BUILDER_OUTPUT_FILE="frontend.tar"
+DOME_DIR="/var/db/factory/dome"
+
+BUILDER_DOCKERFILE="docker/Dockerfile.builder"
+DOME_DOCKERFILE="docker/Dockerfile.dome"
+
+BUILDER_IMAGE_NAME="cros/dome-builder"
+DOME_IMAGE_NAME="cros/dome"
+
+BUILDER_CONTAINER_NAME="dome_builder"
+UWSGI_CONTAINER_NAME="dome_uwsgi"
+NGINX_CONTAINER_NAME="dome_nginx"
+
+DOME_PORT="8000"
+
+# stop existing containers if needed
+docker stop "${BUILDER_CONTAINER_NAME}" 2>/dev/null || true
+docker rm "${BUILDER_CONTAINER_NAME}" 2>/dev/null || true
+docker stop "${UWSGI_CONTAINER_NAME}" 2>/dev/null || true
+docker rm "${UWSGI_CONTAINER_NAME}" 2>/dev/null || true
+docker stop "${NGINX_CONTAINER_NAME}" 2>/dev/null || true
+docker rm "${NGINX_CONTAINER_NAME}" 2>/dev/null || true
+
+# build the dome builder image
+docker build \
+  --file "${BUILDER_DOCKERFILE}" \
+  --tag "${BUILDER_IMAGE_NAME}" \
+  --build-arg workdir="${BUILDER_WORKDIR}" \
+  --build-arg output_file="${BUILDER_OUTPUT_FILE}" \
+  "${SCRIPT_DIR}"
+
+# copy the builder's output from container to host
+mkdir -p "${SCRIPT_DIR}/build"
+docker run --name "${BUILDER_CONTAINER_NAME}" "${BUILDER_IMAGE_NAME}"
+docker cp \
+  "${BUILDER_CONTAINER_NAME}:${BUILDER_WORKDIR}/${BUILDER_OUTPUT_FILE}" \
+  "${SCRIPT_DIR}/build/"
+docker rm "${BUILDER_CONTAINER_NAME}"
+
+# build the dome runner image
+docker build \
+  --file "${DOME_DOCKERFILE}" \
+  --tag "${DOME_IMAGE_NAME}" \
+  --build-arg dome_dir="${DOME_DIR}" \
+  --build-arg builder_output_file="${BUILDER_OUTPUT_FILE}" \
+  "${SCRIPT_DIR}"
+
+# make sure database file exists or mounting volume will fail
+mkdir -p "${DOCKER_SHARED_DIR}"
+touch "${DOCKER_SHARED_DIR}/${DB_FILE}"
+
+# Migrate the database if needed (won't remove any data if the database already
+# exists, but will apply the schema changes). This command may ask user
+# questions and need the user input, so make it interactive.
+docker run \
+  --rm \
+  --interactive \
+  --tty \
+  --volume "${DOCKER_SHARED_DIR}/${DB_FILE}:${DOME_DIR}/${DB_FILE}" \
+  "${DOME_IMAGE_NAME}" \
+  python manage.py migrate
+
+# start uwsgi, the bridge between django and nginx
+docker run \
+  --detach \
+  --restart unless-stopped \
+  --name "${UWSGI_CONTAINER_NAME}" \
+  --volume /run \
+  --volume "${DOCKER_SHARED_DIR}/${DB_FILE}:${DOME_DIR}/${DB_FILE}" \
+  "${DOME_IMAGE_NAME}" \
+  uwsgi --ini uwsgi.ini
+
+# start nginx
+docker run \
+  --detach \
+  --restart unless-stopped \
+  --name "${NGINX_CONTAINER_NAME}" \
+  --volumes-from "${UWSGI_CONTAINER_NAME}" \
+  --publish ${DOME_PORT}:80 \
+  "${DOME_IMAGE_NAME}" \
+  nginx -g 'daemon off;'
