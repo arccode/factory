8708e4e71a7aa4488cbb56cd2e09fcbe58e7aac8
Hung-Te Lin <hungte@chromium.org>
2016-08-25 21:54:46 +0800

    device: Improve SSH Link performance by using control master.
    Enable SSH "ControlMaster" multiplexing if possible. This will
    significantly improve command execution speed on DUT when using SSH
    Link.
    
    BUG=none
    TEST=make test
    
    Change-Id: I452489c18025f47adbbb261870b7dff4cc3c7b53
    Reviewed-on: https://chromium-review.googlesource.com/375739
    Commit-Ready: Wei-Han Chen <stimim@chromium.org>
    Tested-by: Wei-Han Chen <stimim@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

--- py/device/links/ssh.py
+++ py/device/links/ssh.py
@@ -6,16 +6,22 @@
 """Implementation of cros.factory.device.DeviceLink using SSH."""
 
 import logging
+import os
 import pipes
+import Queue
 import subprocess
+import tempfile
 import threading
+import time
 import types
 
 import factory_common  # pylint: disable=W0611
-from cros.factory.test import factory
 from cros.factory.device import link
+from cros.factory.test import factory
 from cros.factory.test.utils import dhcp_utils
 from cros.factory.utils import file_utils
+from cros.factory.utils import process_utils
+from cros.factory.utils import sync_utils
 from cros.factory.utils import type_utils
 
 
@@ -38,7 +44,9 @@ class SSHLink(link.DeviceLink):
     use_ping: A bool, whether using ping(8) to check connection with DUT or not.
               If it's False, will use ssh(1) instead. This is useful if DUT
               drops incoming ICMP packets.
-    connect_timeout: An interger for ssh(1) connection timeout in seconds.
+    connect_timeout: An integer for ssh(1) connection timeout in seconds.
+    control_persist: An integer for ssh(1) to keep master connection remain
+              opened for given seconds, or None to not using master control.
 
   dut_options example:
     dut_options for fixed-IP:
@@ -70,13 +78,14 @@ class SSHLink(link.DeviceLink):
   """
 
   def __init__(self, host=None, user='root', port=22, identity=None,
-               use_ping=True, connect_timeout=1):
+               use_ping=True, connect_timeout=1, control_persist=300):
     self._host = host
     self.user = user
     self.port = port
     self.identity = identity
     self.use_ping = use_ping
     self.connect_timeout = connect_timeout
+    self.control_persist = control_persist
 
   @property
   def host(self):
@@ -110,24 +119,78 @@ class SSHLink(link.DeviceLink):
     options = ['-o', 'UserKnownHostsFile=/dev/null',
                '-o', 'StrictHostKeyChecking=no',
                '-o', 'ConnectTimeout=%d' % self.connect_timeout]
+    if self.control_persist is not None:
+      options += ['-o', 'ControlMaster=auto',
+                  '-o', 'ControlPath=/tmp/.ssh-%r@%h:%p',
+                  '-o', 'ControlPersist=%s' % self.control_persist]
     if self.port:
       options += ['-P' if is_scp else '-p', str(self.port)]
     if self.identity:
       options += ['-i', self.identity]
     return sig, options
 
+  def _DoSCP(self, src, dest, is_push, options=None, max_retry=3):
+    remote_sig, scp_options = self._signature(True)
+
+    if is_push:
+      dest = '%s:%s' % (remote_sig, dest)
+    else:
+      src = '%s:%s' % (remote_sig, src)
+
+    if options:
+      if isinstance(options, list):
+        scp_options += options
+      elif isinstance(options, basestring):
+        scp_options.append(options)
+      else:
+        raise ValueError('options must be a list or string (got %r)' % options)
+
+    def _TryOnce():
+      with tempfile.TemporaryFile() as stderr:
+        proc = subprocess.Popen(['scp'] + scp_options + [src, dest],
+                                stderr=stderr)
+        self._StartWatcher(proc)
+        returncode = proc.wait()
+
+        if returncode != 0:
+          # SCP returns error code "1" for SSH connection failure,
+          # which means "generic error".
+          # Therefore, we cannot tell the difference between connection failure
+          # and other errors (e.g. file not found) by looking at return code
+          # only, we need to parse the error message.
+          stderr.flush()
+          stderr.seek(0)
+          error = stderr.read()
+          SSH_CONNECT_ERROR_MSG = [
+              'ssh: connect to host',
+              'Connection timed out',]
+          if [msg for msg in SSH_CONNECT_ERROR_MSG if msg in error]:
+            # this is a connection issue
+            logging.warning(error)
+            return False
+        # either succeeded, or failed by other reasons, stop trying
+        return (True, returncode)
+
+    def _Callback(retry_time, max_retry_times):
+      logging.info('SCP: src=%s, dst=%s (%d/%d)',
+                   src, dest, retry_time, max_retry_times)
+
+    result = sync_utils.Retry(
+        max_retry, 0.1, callback=_Callback, target=_TryOnce)
+
+    returncode = result[1] if result else 255
+    if returncode:
+      raise subprocess.CalledProcessError(
+          returncode, 'SCP failed: src=%s, dst=%s' % (src, dest))
+    return 0
+
   def Push(self, local, remote):
     """See DeviceLink.Push"""
-    remote_sig, options = self._signature(True)
-    return subprocess.check_call(['scp'] + options +
-                                 [local, '%s:%s' % (remote_sig, remote)])
+    return self._DoSCP(local, remote, is_push=True)
 
   def PushDirectory(self, local, remote):
     """See DeviceLink.PushDirectory"""
-    remote_sig, options = self._signature(True)
-    options.append('-r')
-    return subprocess.check_call(['scp'] + options +
-                                 [local, '%s:%s' % (remote_sig, remote)])
+    return self._DoSCP(local, remote, is_push=True, options='-r')
 
   def Pull(self, remote, local=None):
     """See DeviceLink.Pull"""
@@ -137,9 +200,12 @@ class SSHLink(link.DeviceLink):
         with open(path) as f:
           return f.read()
 
-    remote_sig, options = self._signature(True)
-    subprocess.check_call(['scp'] + options +
-                          ['%s:%s' % (remote_sig, remote), local])
+    return self._DoSCP(remote, local, is_push=False)
+
+  def _StartWatcher(self, subproc):
+    watcher = self.__class__.ControlMasterWatcher(self)
+    watcher.Start()  # make sure the watcher is running
+    watcher.AddProcess(subproc.pid, os.getpid())
 
   def Shell(self, command, stdin=None, stdout=None, stderr=None):
     """See DeviceLink.Shell"""
@@ -154,8 +220,11 @@ class SSHLink(link.DeviceLink):
       shell = False
 
     logging.debug('SSHLink: Run [%r]', command)
-    return subprocess.Popen(command, shell=shell, close_fds=True, stdin=stdin,
+    proc = subprocess.Popen(command, shell=shell, close_fds=True, stdin=stdin,
                             stdout=stdout, stderr=stderr)
+    self._StartWatcher(proc)
+
+    return proc
 
   def IsReady(self):
     """See DeviceLink.IsReady"""
@@ -203,6 +272,113 @@ class SSHLink(link.DeviceLink):
 
       cls._dhcp_manager = cls.LinkManager(**options)
       cls._dhcp_manager.Start()
+
+  class ControlMasterWatcher(object):
+    __metaclass__ = type_utils.Singleton
+
+    def __init__(self, link_instance):
+      assert isinstance(link_instance, SSHLink)
+
+      self._link = link_instance
+      self._thread = threading.Thread(target=self.Run)
+      self._proc_queue = Queue.Queue()
+
+      self._user = self._link.user
+      self._host = self._link._host  # pylint: disable=protected-access
+      self._port = self._link.port
+      self._link_class_name = self._link.__class__.__name__
+
+    def IsRunning(self):
+      if not self._thread:
+        return False
+      if not self._thread.is_alive():
+        self._thread = None
+        return False
+      return True
+
+    def Start(self):
+      if self.IsRunning():
+        return
+
+      if self._link.control_persist is None:
+        logging.debug('%s %s@%s:%s is not using control master, don\'t start',
+                      self._link_class_name, self._user, self._host, self._port)
+        return
+
+      self._thread = process_utils.StartDaemonThread(target=self.Run)
+
+    def AddProcess(self, pid, ppid=None):
+      """Add an SSH process to monitor.
+
+      If any of added SSH process is still running, ControlMasterWatcher will
+      keep monitoring network connectivity.  If network is down, control master
+      will be killed.
+
+      Args:
+        pid: PID of process using SSH
+        ppid: parent PID of given process
+      """
+      if not self.IsRunning():
+        logging.warning('Watcher is not running, %d is not added', pid)
+        return
+      self._proc_queue.put((pid, ppid))
+
+    def Run(self):
+      logging.info('start monitoring control master')
+
+      # an alias to prevent duplicated pylint warnings
+      # pylint: disable=protected-access
+      _GetLinkSignature = self._link._signature
+
+      def _IsControlMasterRunning():
+        sig, options = _GetLinkSignature(False)
+        return subprocess.call(
+            ['ssh', '-O', 'check'] + options + [sig, 'true']) == 0
+
+      def _StopControlMaster():
+        sig, options = _GetLinkSignature(False)
+        subprocess.call(['ssh', '-O', 'exit'] + options + [sig, 'true'])
+
+      def _CallTrue():
+        sig, options = _GetLinkSignature(False)
+        proc = subprocess.Popen(['ssh'] + options + [sig, 'true'])
+        time.sleep(1)
+        returncode = proc.poll()
+        if returncode != 0:
+          proc.kill()
+          return False
+        else:
+          return True
+
+      def _PollingCallback(is_process_alive):
+        if not is_process_alive:
+          return True  # returns True to stop polling
+
+        try:
+          if not _IsControlMasterRunning():
+            logging.info('control master is not running, skipped')
+            return
+
+          if not _CallTrue():
+            logging.info('loss connection, stopping control master')
+            _StopControlMaster()
+        except Exception:
+          logging.info('monitoring %s to %s@%s:%s',
+                       self._link_class_name,
+                       self._user, self._host, self._port, exc_info=True)
+
+      while True:
+        # get a new process from queue to monitor
+        # since Queue.get will block if queue is empty, we don't need to sleep
+        pid, ppid = self._proc_queue.get()
+        logging.info('start monitoring control master until %d terminates', pid)
+
+        sync_utils.PollForCondition(
+            lambda: process_utils.IsProcessAlive(pid, ppid),
+            condition_method=_PollingCallback,
+            timeout_secs=None,
+            poll_interval_secs=1)
+
 
   class LinkManager(object):
     def __init__(self,
