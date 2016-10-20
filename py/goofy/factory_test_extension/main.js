7347e5e18ccf3bda61d6c2536ea11b6e03802b0b
Ting Shen <phoenixshen@google.com>
2016-10-20 16:19:05 +0800

    goofy: fix keyboard focus
    Add a redirect to make browser sets focus correctly.
    
    BUG=chromium:627372
    TEST=start goofy and press ctrl-alt-0
    
    Change-Id: I07ff3ee59d60d2407c072af50d195760c9e13777
    Reviewed-on: https://chromium-review.googlesource.com/401318
    Commit-Ready: Ting Shen <phoenixshen@chromium.org>
    Tested-by: Ting Shen <phoenixshen@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

---
+++ py/goofy/factory_test_extension/main.js
@@ -0,0 +1,6 @@
+// Copyright 2016 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// 4012 = default factory state server port.
+window.onload = () => location.href = 'http://127.0.0.1:4012/';
