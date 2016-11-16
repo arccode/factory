e2bccfad68dceba8647efd2b34d800622ee121a1
chuntsen <chuntsen@google.com>
2016-11-16 12:20:40 +0800

    instalog: output_archive.py: new plugin for archive and backup
    output_archive plugin can archive all events and their attachments.
    
    BUG=b:30449046
    TEST=Manually on local machine
    
    Change-Id: Ie66c809b3f8ef1e04d0c5c7dadf821b8f5c65b8f
    Reviewed-on: https://chromium-review.googlesource.com/411625
    Commit-Ready: Chun-Tsen Kuo <chuntsen@chromium.org>
    Tested-by: Chun-Tsen Kuo <chuntsen@chromium.org>
    Reviewed-by: Joel Kitching <kitching@chromium.org>

---
+++ py/instalog/plugins/output_archive.py
@@ -0,0 +1,130 @@
+#!/usr/bin/python2
+#
+# Copyright 2016 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Output archiver plugin.
+
+An archiver plugin to backup all events and their attachments to a tar.gz file.
+"""
+
+from __future__ import print_function
+
+import datetime
+import os
+import StringIO
+import tarfile
+import time
+
+import instalog_common  # pylint: disable=W0611
+from instalog import plugin_base
+from instalog.utils.arg_utils import Arg
+from instalog.utils import time_utils
+
+
+_ARCHIVE_MESSAGE_INTERVAL = 15  # 15sec
+_DEFAULT_INTERVAL = 1 * 60 * 60  # 1hr
+_DEFAULT_MAX_SIZE = 200 * 1024 * 1024  # 200mb
+
+
+class OutputArchiver(plugin_base.OutputPlugin):
+
+  ARGS = [
+      Arg('interval', int,
+          'How long to wait, in seconds, before creating the next archive.',
+          optional=True, default=_DEFAULT_INTERVAL),
+      Arg('max_size', int,
+          'If the total_size bigger than max_size, archive these events.',
+          optional=True, default=_DEFAULT_MAX_SIZE),
+      Arg('target_dir', (str, unicode),
+          'The directory in which to store archives. '
+          'Uses the plugin\'s data directory by default.',
+          optional=True, default=None),
+  ]
+
+  def SetUp(self):
+    """Sets up the plugin."""
+    if self.args.target_dir is None:
+      self.args.target_dir = self.GetDataDir()
+    if not os.path.isdir(self.args.target_dir):
+      os.makedirs(self.args.target_dir)
+
+  def Main(self):
+    """Main thread of the plugin."""
+    while not self.IsStopping():
+      if not self.PrepareAndArchive():
+        self.Sleep(1)
+
+  def GetEventsTarInfo(self, name, size):
+    """Makes the file info of events.json."""
+    info = tarfile.TarInfo(name)
+    info.size = size
+    info.mtime = time.time()
+    info.mode = 0644
+    info.type = tarfile.REGTYPE
+    return info
+
+  def Archive(self, events):
+    """Archives events."""
+    serialized_events_buffer = StringIO.StringIO()
+    att_count = 0
+
+    archive_name = time.strftime('InstalogEvents_%Y%m%d%H%M%S')
+    archive_path = os.path.join(self.args.target_dir, archive_name + '.tar.gz')
+    self.info('Archive %d events in %s', len(events), archive_path)
+    with tarfile.open(archive_path, 'w:gz') as tar:
+      for event in events:
+        for att_id, att_path in event.attachments.iteritems():
+          if os.path.isfile(att_path):
+            att_name = os.path.basename(att_path)
+            att_newpath = 'attachments/%03d/%s' % (att_count, att_name)
+            att_count += 1
+            tar.add(att_path, arcname=os.path.join(archive_name, att_newpath))
+            event.attachments[att_id] = att_newpath
+        serialized_events_buffer.write(event.Serialize() + '\n')
+      info = self.GetEventsTarInfo(os.path.join(archive_name, 'events.json'),
+                                   serialized_events_buffer.len)
+      serialized_events_buffer.seek(0)
+      tar.addfile(info, serialized_events_buffer)
+    return True
+
+  def PrepareAndArchive(self):
+    """Retrieves events, and archives them."""
+    event_stream = self.NewStream()
+    if not event_stream:
+      return False
+
+    events = []
+    time_last = time_utils.MonotonicTime()
+    total_size = 0
+    for event in event_stream.iter(timeout=self.args.interval):
+      events.append(event)
+      self.debug('len(events) = %d', len(events))
+      total_size += len(event.Serialize())
+      for att_id, att_path in event.attachments.iteritems():
+        if os.path.isfile(att_path):
+          total_size += os.path.getsize(att_path)
+
+      time_now = time_utils.MonotonicTime()
+      if (time_now - time_last) >= _ARCHIVE_MESSAGE_INTERVAL:
+        time_last = time_now
+        self.info('Currently at %.2f%% of %.2fMB before archiving',
+                  100.0 * total_size / self.args.max_size,
+                  self.args.max_size / 1024.0 / 1024)
+      if total_size >= self.args.max_size:
+        break
+
+    # Commit these events.
+    if len(events) == 0 or (len(events) > 0 and self.Archive(events)):
+      self.info('Commit %d events', len(events))
+      event_stream.Commit()
+      return True
+    else:
+      self.info('Abort %d events', len(events))
+      event_stream.Abort()
+      return False
+
+
+if __name__ == '__main__':
+  plugin_base.main()
