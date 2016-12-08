55ac9317c0b62883640c36eb8d4e54cb12cc5c4c
Chih-Yu Huang <akahuang@google.com>
2016-12-08 17:16:11 +0800

    probe: implement VPD probe function.
    BUG=chromium:667651
    TEST=py/probe/functions/vpd_unittest.py
    
    Change-Id: Ib311ee67ad84a088159e50bf86b9441e6e72ba79
    Reviewed-on: https://chromium-review.googlesource.com/417463
    Commit-Ready: Chih-Yu Huang <akahuang@chromium.org>
    Tested-by: Chih-Yu Huang <akahuang@chromium.org>
    Reviewed-by: Joel Kitching <kitching@chromium.org>

---
+++ py/probe/functions/vpd.py
@@ -0,0 +1,25 @@
+# Copyright 2016 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import factory_common  # pylint: disable=unused-import
+from cros.factory.probe.functions import shell
+from cros.factory.utils.arg_utils import Arg
+
+
+class VPDFunction(shell.ShellFunction):
+  """Reads the information from VPD."""
+
+  ARGS = [
+      Arg('key', str, 'The field of VPD.'),
+      Arg('from_rw', bool,
+          'True to read from RW_VPD, and False to read from RO_VPD. '
+          'Default is to read from RO_VPD.', default=False),
+  ]
+
+  def __init__(self, **kwargs):
+    super(VPDFunction, self).__init__(**kwargs)
+
+    partition = 'RW_VPD' if self.args.from_rw else 'RO_VPD'
+    self.args.command = 'vpd -i %s -g %s' % (partition, self.args.key)
+    self.args.split_line = False
---
+++ py/probe/functions/vpd_unittest.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright 2016 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import mock
+import unittest
+
+import factory_common  # pylint: disable=unused-import
+from cros.factory.probe.functions import vpd
+
+
+class VPDFunctionTest(unittest.TestCase):
+
+  @mock.patch('cros.factory.utils.process_utils.CheckOutput', return_value='tw')
+  def testSimpleCommand(self, MockCheckOutput):
+    results = vpd.VPDFunction(key='region')()
+    self.assertEquals(results, [{'region': 'tw'}])
+    MockCheckOutput.assert_called_once_with(
+        'vpd -i RO_VPD -g region', shell=True, log=True)
+
+  @mock.patch('cros.factory.utils.process_utils.CheckOutput', return_value='')
+  def testNoResult(self, MockCheckOutput):
+    results = vpd.VPDFunction(key='FAKE', from_rw=True)()
+    self.assertEquals(results, [])
+    MockCheckOutput.assert_called_once_with(
+        'vpd -i RW_VPD -g FAKE', shell=True, log=True)
+
+
+if __name__ == '__main__':
+  unittest.main()
