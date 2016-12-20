943f107c9fe973e1fd3dfa9723b071e7689ac23f
Ting Shen <phoenixshen@google.com>
2016-12-20 16:37:42 +0800

    usb_probe: support bus_id and multiple vid:pid's
    1. Use dut api
    2. Add an optional bus_id filter
    3. Replace vid and pid by a list of strings in format 'VID:PID' to
    support searching for multiple devices.
    
    BUG=chrome-os-partner:60896
    TEST=Run usb_probe test
    
    Change-Id: I861ba108e603ef430b3efa3582cd4797653fe7fb
    Reviewed-on: https://chromium-review.googlesource.com/422708
    Commit-Ready: Ting Shen <phoenixshen@chromium.org>
    Tested-by: Ting Shen <phoenixshen@chromium.org>
    Reviewed-by: Youcheng Syu <youcheng@google.com>

--- py/test/pytests/usb_probe.py
+++ py/test/pytests/usb_probe.py
@@ -7,62 +7,78 @@
 It uses the lsusb utility to check if there's an USB device with given VID:PID
 or containing a specified string in lsusb.
 
-If search_string is defined it searches for it in lsusb -v and passes if the
-string exists, vid and pid are ignored in this case.
+If search_string is defined, it searches for it in 'lsusb -v' and passes if the
+string exists, usb_id_list are ignored in this case.
 
-If vid and pid are defined it searches for them in lsusb -v and passes if they
-exist.
+If usb_id_list is defined, it searches for them in 'lsusb -d id' and passes
+if any id in the list is found.
 
-dargs:
-  vid: (str) optional 4-digit vendor ID.
-  pid: (str) optional 4-digit product ID.
-  use_re: (bool) optional flag to treat string as a regular expression.
-  search_string: (str) optional manual string to check for in lsusb -v
+If bus_id is defined, the result above is also filtered by given bus id.
 """
 
 import re
 import unittest
 
 import factory_common  # pylint: disable=unused-import
+from cros.factory.device import component
+from cros.factory.device import device_utils
 from cros.factory.test import event_log
 from cros.factory.utils.arg_utils import Arg
-from cros.factory.utils import process_utils
 
 
 class USBProbeTest(unittest.TestCase):
   ARGS = [
-      Arg('vid', str, '4-digit vendor ID', '', optional=True),
-      Arg('pid', str, '4-digit product ID', '', optional=True),
+      Arg('bus_id', str, 'Bus id in format [[bus]:][devnum].', optional=True),
+      Arg('usb_id_list', list, 'A list of "VID:[PID]" to be searched.',
+          optional=True),
       Arg('use_re', bool,
-          'true to treat vid, pid, search_string as a regular expression',
-          default=False, optional=True),
-      Arg('search_string', str, 'manual string to check for in lsusb -v', None,
+          'True to treat search_string as a regular expression.',
+          default=False),
+      Arg('search_string', str, 'manual string to check for in lsusb -v.',
           optional=True),
   ]
 
-  def _ProbeUSB(self, lsusb_string):
+  def _ProbeUSB(self, search_string, lsusb_options, use_re):
     """Search for a string in lsusb -v.
 
     If self.args.use_re is enabled, search lsusb via re.search.
 
     Args:
-      lsusb_string: string to search for
+      string_string: string to search for.
+      lsusb_options: extra options for lsusb command.
+      use_re: True to treat search_string as a regular expression.
 
     Returns:
       True if the string is found, false if not.
     """
-    response = process_utils.SpawnOutput(['lsusb', '-v'], log=True)
-    if self.args.use_re:
-      return bool(re.search(lsusb_string, response))
+    response = self._dut.CheckOutput(['lsusb'] + lsusb_options)
+    if use_re:
+      return bool(re.search(search_string, response))
     else:
-      return lsusb_string in response
+      return search_string in response
+
+  def setUp(self):
+    self._dut = device_utils.CreateDUTInterface()
 
   def runTest(self):
+    bus_id_option = ['-s', self.args.bus_id] if self.args.bus_id else []
+
     if self.args.search_string:
       usb_string = self.args.search_string
+      probed_result = self._ProbeUSB(usb_string, ['-v'] + bus_id_option,
+                                     self.args.use_re)
+      event_log.Log('usb_probe', result=probed_result, usb_string=usb_string)
+      self.assertTrue(probed_result,
+                      'String: %s was not found in lsusb -v.' % usb_string)
+    elif self.args.usb_id_list:
+      for usb_id in self.args.usb_id_list:
+        try:
+          self._ProbeUSB('', ['-d', usb_id] + bus_id_option, False)
+          event_log.Log('usb_probe', usb_id=usb_id)
+          return
+        except component.CalledProcessError:
+          pass
+      self.fail('Cannot found any one of %r in lsusb.'
+                % self.args.usb_id_list)
     else:
-      usb_string = '%s:%s' % (self.args.vid, self.args.pid)
-    probed_result = self._ProbeUSB(usb_string)
-    event_log.Log('usb_probed', result=probed_result, usb_string=usb_string)
-    self.assertTrue(probed_result,
-                    'String: %s was not found in lsusb -v.' % usb_string)
+      self.fail('Either search_string or usb_id_list must be set')
