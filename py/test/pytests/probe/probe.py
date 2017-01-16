3c275b6e0e7a5096cfbbd452016b80a3aa2755c2
Chih-Yu Huang <akahuang@google.com>
2017-01-16 18:09:56 +0800

    pytest: add UI in probe pytest.
    We generate a table to show the probed component for each category
    and passes the rule or not for better debugging.
    
    BUG=chromium:677867
    TEST=manually test at device
    
    Change-Id: Ie82b332548bc3ee14a24ae210903b22c4ccf94ca
    Reviewed-on: https://chromium-review.googlesource.com/428613
    Commit-Ready: Chih-Yu Huang <akahuang@chromium.org>
    Tested-by: Chih-Yu Huang <akahuang@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>

--- py/test/pytests/probe/probe.py
+++ py/test/pytests/probe/probe.py
@@ -46,6 +46,8 @@ from cros.factory.device import device_utils
 from cros.factory.test.args import Arg
 from cros.factory.test import factory
 from cros.factory.test.utils import deploy_utils
+from cros.factory.test import test_ui
+from cros.factory.test import ui_templates
 
 
 # The config files should be placed in the py/test/pytests/probe/ folder.
@@ -59,6 +61,22 @@ OPERATOR_MAP = {
     '>=': operator.ge,
     'in': lambda a, b: a in b}
 
+_CSS = """
+table {
+  border-collapse: collapse;
+  margin-left: auto;
+  margin-right: auto;
+  padding-bottom: 1em;
+}
+th, td {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 0 1em;
+}
+.prompt {
+  font-size: 2em;
+}
+"""
 
 def EvaluateRule(a, op_str, b):
   return OPERATOR_MAP[op_str](a, b)
@@ -77,6 +95,9 @@ class ProbeTest(unittest.TestCase):
       ]
 
   def setUp(self):
+    self._ui = test_ui.UI(css=_CSS)
+    self._template = ui_templates.OneScrollableSection(self._ui)
+
     self._dut = device_utils.CreateDUTInterface()
     self.factory_tools = deploy_utils.FactoryPythonArchive(self._dut)
     self.config_file_path = os.path.join(
@@ -98,14 +119,40 @@ class ProbeTest(unittest.TestCase):
       for category, op_str, value in self.args.overridden_rules:
         rule_map[category] = (op_str, value)
 
+    table_html = ui_templates.Table(rows=len(probed_results) + 1, cols=4)
+    title = ['Category', 'Probed Components', 'Rule', 'Status']
+    for idx, content in enumerate(title):
+      table_html.SetContent(0, idx, '<b>%s</b>' % content)
+
     # Check every category meets the rule.
-    success = True
-    for category in probed_results:
+    all_passed = True
+    for row_idx, category in enumerate(probed_results, 1):
       count = sum(len(comps) for comps in probed_results[category].values())
       op_str, value = rule_map[category]
-      if not OPERATOR_MAP[op_str](count, value):
-        factory.console.error('Category "%s" does not meet rule: %s %s %s',
-                              category, count, op_str, value)
-        success = False
-    if not success:
-      self.fail()
+      status = OPERATOR_MAP[op_str](count, value)
+      all_passed &= status
+
+      # Set the table.
+      summary = []
+      for name, result in probed_results[category].iteritems():
+        if len(result) > 0:
+          summary.append('%s %s found.' % (len(result), name))
+      summary_str = '<br>'.join(summary) if summary else 'No component found.'
+      rule_str = 'count (%s) %s %s' % (count, op_str, value)
+      status_str = 'passed' if status else 'failed'
+      factory.console.info('Category "%s" %s %s, %s.',
+                           category, summary_str, rule_str, status_str)
+      table_html.SetContent(row_idx, 0, category)
+      table_html.SetContent(row_idx, 1, summary_str)
+      table_html.SetContent(row_idx, 2, rule_str)
+      table_html.SetContent(
+          row_idx, 3, '<div class=test-status-{0}>{0}</div>'.format(status_str))
+
+    html = [
+        table_html.GenerateHTML(), '<br>',
+        test_ui.MakeLabel('Press SPACE to continue', u'按空白键继续', 'prompt')]
+    self._template.SetState(''.join(html))
+    self._ui.BindKeyJS(
+        test_ui.SPACE_KEY,
+        'window.test.pass()' if all_passed else 'window.test.fail()')
+    self._ui.Run()
