f8ca03975920107d9203abfd2dfb1cc837f6d1b3
Hung-Te Lin <hungte@chromium.org>
2017-01-19 20:57:37 +0800

    pytests: vpd: Allow overriding device in registration codes.
    As more projects will share same test and release image, we are seeing
    the demand of allowing ECHO codes to use a device name that is different
    from board name in release image.
    
    The new override_registration_codes_device argument allows setting
    device name to a specified value so vpd test won't break.
    
    BUG=chrome-os-partner:61690
    TEST=None
    
    Change-Id: I79d2cd58d4467339770fe67c35429f717558167d
    Reviewed-on: https://chromium-review.googlesource.com/430573
    Commit-Ready: Hung-Te Lin <hungte@chromium.org>
    Tested-by: Hung-Te Lin <hungte@chromium.org>
    Reviewed-by: Shun-Hsing Ou <shunhsingou@chromium.org>

--- py/test/pytests/vpd.py
+++ py/test/pytests/vpd.py
@@ -147,8 +147,9 @@ class WriteVPDTask(factory_task.FactoryTask):
               'user': registration_codes.RegistrationCode.Type.UNIQUE_CODE,
               'group': registration_codes.RegistrationCode.Type.GROUP_CODE}[k]
           registration_codes.CheckRegistrationCode(
-              self.test.registration_code_map[k],
-              code_type, build_board.BuildBoard().short_name)
+              self.test.registration_code_map[k], code_type,
+              self.test.args.override_registration_codes_device or
+              build_board.BuildBoard().short_name)
         except ValueError as e:
           self.Fail(str(e))
 
@@ -429,6 +430,10 @@ class VPDTest(unittest.TestCase):
           'It does not require engineering mode. The dict should be of the '
           'format: {"ro": { RO_VPD key-value pairs }, "rw": { RW_VPD key-value '
           'pairs }}', default=None, optional=True),
+      Arg('override_registration_codes_device', str,
+          'A string to override the device in registration codes. If None, '
+          'use the board name in /etc/lsb-release.',
+          default=None, optional=True),
       Arg('store_registration_codes', bool,
           'Whether to store registration codes onto the machine.',
           default=False),
