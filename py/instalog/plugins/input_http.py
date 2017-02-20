b5ed251e6bec72dd77a7b390357621e490b28cb7
chuntsen <chuntsen@google.com>
2017-02-20 18:01:53 +0800

    Instalog: add new input HTTP plugin
    Add new input HTTP plugin, and we can receive events by HTTP.
    Add input HTTP plugin unitest.
    
    BUG=b:34647678
    TEST=Manually on local machine
    
    Change-Id: I6dc705051fedebd144722f20857dfc6ea6519b83
    Reviewed-on: https://chromium-review.googlesource.com/444694
    Commit-Ready: Chun-Tsen Kuo <chuntsen@chromium.org>
    Tested-by: Chun-Tsen Kuo <chuntsen@chromium.org>
    Reviewed-by: Joel Kitching <kitching@chromium.org>

---
+++ py/instalog/plugins/input_http.py
@@ -0,0 +1,219 @@
+#!/usr/bin/python2
+#
+# Copyright 2017 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Input HTTP plugin.
+
+Receives events from some HTTP requests.
+Can easily send events by curl:
+$ curl -i -X POST \
+       -F 'event={Payload}' \
+       -F 'event=[{Payload}, {Attachments}]' \
+       -F 'event=[{"name": "value"}, {"0": "att_0"}]' \
+       -F 'att_0=@/path/to/attachment_name' \
+       TARGET_HOSTNAME:TARGET_PORT
+(See datatypes.py Event.Deserialize for details of event format.)
+"""
+
+from __future__ import print_function
+
+import BaseHTTPServer
+import cgi
+import os
+import shutil
+import tempfile
+import threading
+
+import instalog_common  # pylint: disable=W0611
+from instalog import datatypes
+from instalog import log_utils
+from instalog import plugin_base
+from instalog.utils.arg_utils import Arg
+
+
+_DEFAULT_MAX_BYTES = 2 * 1024 * 1024 * 1024  # 2gb
+_DEFAULT_HOSTNAME = '0.0.0.0'
+_DEFAULT_PORT = 8899
+
+
+class HTTPHandler(BaseHTTPServer.BaseHTTPRequestHandler, log_utils.LoggerMixin):
+  """Processes HTTP request and responses."""
+
+  def __init__(self, request, client_address, server):
+    self.logger = server.context['logger']
+    self._plugin_api = server.context['plugin_api']
+    self._max_bytes = server.context['max_bytes']
+    self._tmp_dir = None
+    BaseHTTPServer.BaseHTTPRequestHandler.__init__(self, request,
+                                                   client_address, server)
+
+  def do_POST(self):
+    """Processes when receiving POST request."""
+    content_type = self.headers.getheader('Content-Type', '')
+    content_length = self.headers.getheader('Content-Length', None)
+    # Need to reject other Content-Type, because Content-Type =
+    # 'application/x-www-form-urlencoded' may use about 81 times of data size
+    # of memory.
+    if not content_type.startswith('multipart/form-data'):
+      self.send_response(406, 'Not Acceptable: Only accept Content-Type = '
+                              'multipart/form-data, please use output HTTP '
+                              'plugin or curl command')
+      self.send_header('Maximum-Bytes', self._max_bytes)
+      return
+    if not content_length:
+      self.send_response(411, 'Length Required: Need header Content-Length')
+      self.send_header('Maximum-Bytes', self._max_bytes)
+      return
+    # Content-Length may be wrong, and may cause some security issue.
+    if int(content_length) > self._max_bytes:
+      self.send_response(413, 'Request Entity Too Large: The request is bigger '
+                              'than %d bytes' % self._max_bytes)
+      self.send_header('Maximum-Bytes', self._max_bytes)
+      return
+    # Create the temporary directory for attachments.
+    self._tmp_dir = tempfile.mkdtemp(prefix='input_http_')
+    self.debug('Temporary directory for attachments: %s', self._tmp_dir)
+    self.info('Received POST request from %s:%d',
+              self.client_address[0], self.client_address[1])
+    status_code, resp_reason = self._ProcessRequest()
+    self.send_response(status_code, resp_reason)
+    self.send_header('Maximum-Bytes', self._max_bytes)
+
+    # Remove the temporary directory.
+    self.debug('Removing temporary directory %s...', self._tmp_dir)
+    shutil.rmtree(self._tmp_dir)
+
+  def _ProcessRequest(self):
+    """Checks the request and processes it.
+
+    Returns:
+      A tuple with (HTTP status code, the reason of the response)
+    """
+    events = []
+    try:
+      form = cgi.FieldStorage(
+          fp=self.rfile,
+          headers=self.headers,
+          environ={'REQUEST_METHOD': 'POST'}
+      )
+      event_list = form.getlist('event')
+      for serialize_event in event_list:
+        event = datatypes.Event.Deserialize(serialize_event)
+        for att_id, att_path in event.attachments.iteritems():
+          if att_path not in form or isinstance(form[att_path], list):
+            raise Exception('att_path should have exactly one in the request')
+          event.attachments[att_id] = self._RecvAttachment(form[att_path])
+        events.append(event)
+    except Exception as e:
+      self.warning('Bad request with error: %s', repr(e))
+      return 400, 'Bad request: ' + repr(e)
+    if len(events) == 0:
+      return 200, 'OK'
+    elif self._plugin_api.Emit(events):
+      return 200, 'OK'
+    else:
+      self.warning('Emit failed')
+      return 400, 'Bad request: Emit failed'
+
+  def _RecvAttachment(self, data):
+    """Receives attachment and saves it as tmp_path in _tmp_dir."""
+    fd, tmp_path = tempfile.mkstemp(prefix=data.name + '_', dir=self._tmp_dir)
+    with os.fdopen(fd, 'w') as f:
+      if data.file:
+        shutil.copyfileobj(data.file, f)
+      # cgi.MiniFieldStorage does not have attribute 'file', since it stores
+      # the binary data in-memory.
+      else:
+        f.write(data.value)
+    self.debug('Temporary save the attachment to: %s', tmp_path)
+    return tmp_path
+
+  def log_request(self, code='-', size='-'):
+    """Override log_request to Instalog format."""
+    self.info('Send response: %s %d', self.requestline, code)
+
+  def log_message(self, format, *args):  # pylint: disable=W0622
+    """Override log_message to Instalog format."""
+    self.error("%s - %s", self.client_address[0], format % args)
+
+
+class ThreadedHTTPServer(BaseHTTPServer.HTTPServer, log_utils.LoggerMixin):
+  """HTTP server that handles requests in separate threads."""
+
+  def __init__(self, logger, *args, **kwargs):
+    self.logger = logger
+    self.context = None
+    self._threads = set()
+    self._handle_request_thread = threading.Thread(target=self.serve_forever)
+    BaseHTTPServer.HTTPServer.__init__(self, *args, **kwargs)
+
+  def _ProcessRequestThread(self, request, client_address):
+    """Processes the request and handles exceptions."""
+    try:
+      self.finish_request(request, client_address)
+      self.shutdown_request(request)
+    except Exception:
+      self.handle_error(request, client_address)
+      self.shutdown_request(request)
+    finally:
+      self._threads.remove(threading.currentThread())
+
+  def process_request(self, request, client_address):
+    """Starts a new thread to process the request."""
+    t = threading.Thread(target=self._ProcessRequestThread,
+                         args=(request, client_address))
+    self._threads.add(t)
+    t.daemon = False
+    t.start()
+
+  def StartServer(self):
+    """Starts the HTTP server."""
+    self._handle_request_thread.start()
+
+  def StopServer(self):
+    """Stops the HTTP server."""
+    self.shutdown()
+    self._handle_request_thread.join()
+    # Wait the process request threads not yet finished.
+    threads_copy = self._threads.copy()
+    for t in threads_copy:
+      t.join()
+
+
+class InputHTTP(plugin_base.InputPlugin):
+
+  ARGS = [
+      Arg('hostname', (str, unicode), 'Hostname that server should bind to.',
+          optional=True, default=_DEFAULT_HOSTNAME),
+      Arg('port', int, 'Port that server should bind to.',
+          optional=True, default=_DEFAULT_PORT),
+      Arg('max_bytes', int, 'Maximum size of the request in bytes.',
+          optional=True, default=_DEFAULT_MAX_BYTES)
+  ]
+
+  def __init__(self, *args, **kwargs):
+    self._http_server = None
+    super(InputHTTP, self).__init__(*args, **kwargs)
+
+  def SetUp(self):
+    """Sets up the plugin."""
+    self._http_server = ThreadedHTTPServer(
+        self.logger, (self.args.hostname, self.args.port), HTTPHandler)
+    self._http_server.context = {
+        'max_bytes': self.args.max_bytes,
+        'logger': self.logger,
+        'plugin_api': self}
+    self._http_server.StartServer()
+    self.info('http now listening on %s:%d...',
+              self.args.hostname, self.args.port)
+
+  def TearDown(self):
+    """Tears down the plugin."""
+    self._http_server.StopServer()
+    self.info('Shutdown complete')
+
+
+if __name__ == '__main__':
+  plugin_base.main()
---
+++ py/instalog/plugins/input_http_unittest.py
@@ -0,0 +1,228 @@
+#!/usr/bin/python2
+#
+# Copyright 2017 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Unittests for input HTTP plugin."""
+
+from __future__ import print_function
+
+import httplib
+import logging
+import os
+import Queue
+import shutil
+import tempfile
+import threading
+import time
+import unittest
+import urllib
+
+import instalog_common  # pylint: disable=W0611
+from instalog import datatypes
+from instalog import log_utils
+from instalog import plugin_sandbox
+from instalog import testing
+from instalog.utils import net_utils
+
+from instalog.external import requests
+
+
+class TestInputHTTP(unittest.TestCase):
+
+  def _CreatePlugin(self):
+    self.core = testing.MockCore()
+    self.hostname = 'localhost'
+    self.port = net_utils.GetUnusedPort()
+    config = {
+        'hostname': 'localhost',
+        'port': self.port}
+    self.sandbox = plugin_sandbox.PluginSandbox(
+        'input_http', config=config, core_api=self.core)
+    self.sandbox.Start(True)
+    self.plugin = self.sandbox._plugin  # pylint: disable=protected-access
+
+  def setUp(self):
+    self._CreatePlugin()
+    self._tmp_dir = tempfile.mkdtemp(prefix='input_http_unittest_')
+
+  def tearDown(self):
+    self.sandbox.Stop(True)
+    self.assertTrue(self.core.AllStreamsExpired())
+    self.core.Close()
+    shutil.rmtree(self._tmp_dir)
+
+  def testConnect(self):
+    r = requests.post('http://localhost:' + str(self.port), files={'':''})
+    self.assertEqual(200, r.status_code)
+    self.assertIn('Maximum-Bytes', r.headers)
+    self.assertEqual(0, len(self.core.emit_calls))
+
+  def testShutdown(self):
+    """Tests that a request thread should terminate before shutting down."""
+    event = datatypes.Event({}, {'att_id': 'att'})
+    big_data = {'event': datatypes.Event.Serialize(event),
+                'att': '!' * 1024 * 1024 * 1024}  # 1gb
+    # Use a queue to get the request object out of the thread.
+    q = Queue.Queue()
+    def PostBig():
+      try:
+        r = requests.post('http://localhost:' + str(self.port), files=big_data)
+        q.put(r)
+      except Exception as e:
+        q.put(e)
+    t = threading.Thread(target=PostBig)
+    t.daemon = False
+    t.start()
+    time.sleep(0.2)  # Give the thread a chance to start up.
+    self.sandbox.Stop(True)
+    self.assertTrue(self.core.AllStreamsExpired())
+    self.core.Close()
+    self.assertEqual(400, q.get().status_code)
+    self._CreatePlugin()  # For tearDown to stop.
+
+  def testUnsupportedMethod(self):
+    r = requests.get('http://localhost:' + str(self.port))
+    self.assertEqual(501, r.status_code)
+    r = requests.put('http://localhost:' + str(self.port))
+    self.assertEqual(501, r.status_code)
+    r = requests.delete('http://localhost:' + str(self.port))
+    self.assertEqual(501, r.status_code)
+    r = requests.head('http://localhost:' + str(self.port))
+    self.assertEqual(501, r.status_code)
+    r = requests.options('http://localhost:' + str(self.port))
+    self.assertEqual(501, r.status_code)
+    r = requests.patch('http://localhost:' + str(self.port))
+    self.assertEqual(501, r.status_code)
+
+  def testNoAttachment(self):
+    event = datatypes.Event({}, {'att_id': 'att'})
+    data = {'event': datatypes.Event.Serialize(event)}
+    r = requests.post('http://localhost:' + str(self.port), files=data)
+    self.assertEqual(400, r.status_code)
+    self.assertEqual('Bad request: Exception(\'att_path should have exactly '
+                     'one in the request\',)', r.reason)
+    self.assertEqual(0, len(self.core.emit_calls))
+
+  def testSameNameAttachment(self):
+    event = datatypes.Event({}, {'att_id': 'att'})
+    att1 = 'THISISATT1'
+    att2 = 'THISISATT2'
+    data = [('event', datatypes.Event.Serialize(event)),
+            ('att', att1),
+            ('att', att2)]
+    r = requests.post('http://localhost:' + str(self.port), files=data)
+    self.assertEqual(400, r.status_code)
+    self.assertEqual('Bad request: Exception(\'att_path should have exactly '
+                     'one in the request\',)', r.reason)
+    self.assertEqual(0, len(self.core.emit_calls))
+
+  def testOneEvent(self):
+    event = datatypes.Event({'AA': 'BB'}, {'att_id': 'att'})
+    att = os.urandom(1024 * 1024)  # 1mb data
+    data = {'event': datatypes.Event.Serialize(event),
+            'att': att}
+    r = requests.post('http://localhost:' + str(self.port), files=data)
+    self.assertEqual(200, r.status_code)
+    self.assertEqual(1, len(self.core.emit_calls))
+    self.assertEqual(1, len(self.core.emit_calls[0]))
+    self.assertEqual(event.payload, self.core.emit_calls[0][0].payload)
+    with open(self.core.emit_calls[0][0].attachments['att_id']) as f:
+      self.assertEqual(att, f.read())
+
+  def testHTTPlibEvent(self):
+    client = httplib.HTTPConnection('localhost', self.port, timeout=180)
+    event = datatypes.Event({'AA': 'BB'}, {'att_id': 'att'})
+    att = os.urandom(1024 * 1024)  # 1mb data
+    params = urllib.urlencode({'event': datatypes.Event.Serialize(event),
+                               'att': att})
+    client.request('POST', '/', params)
+    self.assertEqual(406, client.getresponse().status)
+    self.assertEqual(0, len(self.core.emit_calls))
+
+  def testMultiEvent(self):
+    event1 = datatypes.Event({}, {'att_id': 'att1'})
+    event2 = datatypes.Event({'CC': 'DD'}, {})
+    event3 = datatypes.Event({'EE': 'FF'}, {'att_id': 'att2'})
+    att1 = os.urandom(10)
+    att2 = os.urandom(10)
+    data = [('event', datatypes.Event.Serialize(event1)),
+            ('event', datatypes.Event.Serialize(event2)),
+            ('event', datatypes.Event.Serialize(event3)),
+            ('att1', att1),
+            ('att2', att2)]
+    r = requests.post('http://localhost:' + str(self.port), files=data)
+    self.assertEqual(200, r.status_code)
+    self.assertEqual(1, len(self.core.emit_calls))
+    self.assertEqual(3, len(self.core.emit_calls[0]))
+    self.assertEqual(event1.payload, self.core.emit_calls[0][0].payload)
+    self.assertEqual(event2.payload, self.core.emit_calls[0][1].payload)
+    self.assertEqual(event3.payload, self.core.emit_calls[0][2].payload)
+    with open(self.core.emit_calls[0][0].attachments['att_id']) as f:
+      self.assertEqual(att1, f.read())
+    with open(self.core.emit_calls[0][2].attachments['att_id']) as f:
+      self.assertEqual(att2, f.read())
+
+  def testOneHugeAttachment(self):
+    """Tests the ability to transfer one huge attachment."""
+    # Since it can be slow to transfer 1 GB of data, only perform one
+    # iteration of the test.
+    att = '!' * 1024 * 1024 * 1024  # 1gb
+    event = datatypes.Event({}, {'att_id': 'att'})
+    data = {'event': datatypes.Event.Serialize(event),
+            'att': att}
+    r = requests.post('http://localhost:' + str(self.port), files=data)
+    self.assertEqual(200, r.status_code)
+    self.assertEqual(1, len(self.core.emit_calls))
+    self.assertEqual(1, len(self.core.emit_calls[0]))
+    with open(self.core.emit_calls[0][0].attachments['att_id']) as f:
+      self.assertEqual(att, f.read())
+
+  def testMultithreadedServing(self):
+    """Tests that the server has multithreading enabled."""
+    event = datatypes.Event({}, {'att_id': 'att'})
+    big_data = {'event': datatypes.Event.Serialize(event),
+                'att': '!' * 1024 * 1024 * 200}  # 200mb
+    small_data = {'event': datatypes.Event.Serialize(event),
+                  'att': '!' * 1024 * 1024}  # 1mb
+
+    # Use a queue to get the request object out of the thread.
+    q = Queue.Queue()
+    def PostBig():
+      r = requests.post('http://localhost:' + str(self.port), files=big_data)
+      q.put(r)
+    t = threading.Thread(target=PostBig)
+    t.daemon = False
+    t.start()
+    time.sleep(0.2)  # Give the big file thread a chance to start up.
+
+    start = time.time()
+    r = requests.post('http://localhost:' + str(self.port), files=small_data)
+    elapsed = time.time() - start
+    t.join()
+
+    self.assertEqual(200, r.status_code)
+    self.assertEqual(200, q.get().status_code)
+    logging.info('Time elapsed transferring small file: %s sec', elapsed)
+    self.assertTrue(elapsed < 0.5)  # Should take less than a half second.
+    self.assertEqual(2, len(self.core.emit_calls))
+
+  def testCurlCommand(self):
+    fd, att_path = tempfile.mkstemp(dir=self._tmp_dir)
+    att = os.urandom(1024 * 1024)  # 1mb
+    with os.fdopen(fd, 'w') as f:
+      f.write(att)
+    os.system('curl -X POST -F \'event=[{"GG": "HH"}, {"att_id": "att"}]\' '
+              '-F \'att=@%s\' localhost:%d' %
+              (att_path, self.port))
+    self.assertEqual(1, len(self.core.emit_calls))
+    self.assertEqual(1, len(self.core.emit_calls[0]))
+    self.assertEqual({'GG': 'HH'}, self.core.emit_calls[0][0].payload)
+    with open(self.core.emit_calls[0][0].attachments['att_id']) as f:
+      self.assertEqual(att, f.read())
+
+
+if __name__ == '__main__':
+  logging.basicConfig(level=logging.DEBUG, format=log_utils.LOG_FORMAT)
+  unittest.main()
