25019f5362b3e5ee17253cf84b37a19c1cf47026
You-Cheng Syu <youcheng@chromium.org>
2017-03-02 15:03:31 +0800

    pytest: Simplify stylus test with touch_monitor module
    With new touch_monitor module, we can simplify stylus test and let
    developers focus more on test logic.
    
    CQ-DEPEND=CL:424276
    BUG=none
    TEST=manually
    
    Change-Id: Ia7adf72267c831ce22b6e85f652d4a0d8b808876
    Reviewed-on: https://chromium-review.googlesource.com/448300
    Commit-Ready: Youcheng Syu <youcheng@chromium.org>
    Tested-by: Youcheng Syu <youcheng@chromium.org>
    Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>

--- py/test/pytests/stylus.py
+++ py/test/pytests/stylus.py
@@ -10,6 +10,7 @@ from cros.factory.test.i18n import test_ui as i18n_test_ui
 from cros.factory.test import test_ui
 from cros.factory.test import ui_templates
 from cros.factory.test.utils import evdev_utils
+from cros.factory.test.utils import touch_monitor
 from cros.factory.utils.arg_utils import Arg
 
 _ID_CANVAS = 'stylus-test-canvas'
@@ -24,18 +25,17 @@ _MSG_PROMPT = i18n_test_ui.MakeI18nLabelWithClass(
     'Press SPACE to start; Esc to fail.', _MSG_PROMPT_CSS_CLASS)
 
 
-class _AbsInfo(object):
+class StylusMonitor(touch_monitor.SingleTouchMonitor):
 
-  def __init__(self, absinfo):
-    self.value = absinfo.value
-    self.offset = absinfo.min
-    self.range = float(absinfo.max - absinfo.min)
+  def __init__(self, device, ui):
+    super(StylusMonitor, self).__init__(device)
+    self._ui = ui
 
-  def Update(self, value):
-    self.value = value
-
-  def GetNormalizedValue(self):
-    return min(max(0.0, (self.value - self.offset) / self.range), 1.0)
+  def OnMove(self):
+    """See SingleTouchMonitor.OnMove."""
+    state = self.GetState()
+    if state.keys[evdev.ecodes.BTN_TOUCH]:
+      self._ui.CallJSFunction('handler', state.x, state.y)
 
 
 class StylusTest(unittest.TestCase):
@@ -63,12 +63,7 @@ class StylusTest(unittest.TestCase):
   def setUp(self):
     self._device = evdev_utils.FindDevice(self.args.stylus_event_id,
                                           evdev_utils.IsStylusDevice)
-    abscaps = {
-        pair[0]: pair[1]
-        for pair in self._device.capabilities()[evdev.ecodes.EV_ABS]}
-    self._x = _AbsInfo(abscaps[evdev.ecodes.ABS_X])
-    self._y = _AbsInfo(abscaps[evdev.ecodes.ABS_Y])
-    self._touch = False
+    self._monitor = None
     self._dispatcher = None
 
     assert self.args.error_margin >= 0
@@ -80,22 +75,6 @@ class StylusTest(unittest.TestCase):
     self._ui.AppendCSS(_MSG_PROMPT_CSS)
     self._template.SetState(_MSG_PROMPT)
 
-  def _Handler(self, event):
-    if event.type == evdev.ecodes.EV_ABS:
-      if event.code == evdev.ecodes.ABS_X:
-        self._x.Update(event.value)
-      elif event.code == evdev.ecodes.ABS_Y:
-        self._y.Update(event.value)
-    elif event.type == evdev.ecodes.EV_KEY:
-      if event.code == evdev.ecodes.BTN_TOUCH:
-        self._touch = event.value != 0
-    elif event.type == evdev.ecodes.EV_SYN:
-      if self._touch:
-        self._ui.CallJSFunction(
-            'handler',
-            self._x.GetNormalizedValue(),
-            self._y.GetNormalizedValue())
-
   def _StartTest(self, _):
     self._ui.SetHTML(_HTML)
     self._ui.CallJSFunction('setupStylusTest',
@@ -105,13 +84,17 @@ class StylusTest(unittest.TestCase):
                             self.args.end_position,
                             self.args.step_size)
     self._ui.CallJSFunction('startTest')
+    self._device = evdev_utils.DeviceReopen(self._device)
+    self._device.grab()
+    self._monitor = StylusMonitor(self._device, self._ui)
     self._dispatcher = evdev_utils.InputDeviceDispatcher(self._device,
-                                                         self._Handler)
+                                                         self._monitor.Handler)
     self._dispatcher.StartDaemon()
 
   def tearDown(self):
     if self._dispatcher is not None:
       self._dispatcher.close()
+    self._device.ungrab()
 
   def runTest(self):
     self._ui.BindKeyJS(test_ui.ESCAPE_KEY, 'failTest();')
