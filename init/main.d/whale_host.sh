b181d9e1c056a1bbc6d7a9e0ba99f7f77e12912b
Wei-Han Chen <stimim@google.com>
2017-03-08 10:54:51 +0800

    whale: add init/main.d/whale_host.sh
    Whale NUC should enable this app to setup services.
    
    When the system is using frecon, terminals are not created until users
    switch to it using CTRL+ALT+Fn.  It is hard to write dolphin server
    output to terminal now.  Let's remove this feature now.
    
    BUG=b:35985483
    TEST=None
    
    Change-Id: I52545ed4ab53608d28ff34d8a50d2c7b74b7e630
    Reviewed-on: https://chromium-review.googlesource.com/455636
    Commit-Ready: Wei-Han Chen <stimim@chromium.org>
    Tested-by: Wei-Han Chen <stimim@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

---
+++ init/main.d/whale_host.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Copyright 2017 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+FACTORY_BASE="/usr/local/factory"
+DPSERVER_LEFT_LOG="/var/log/whale_dp_server_left.log"
+DPSERVER_RIGHT_LOG="/var/log/whale_dp_server_right.log"
+
+serve_whale_host() {
+
+  "${FACTORY_BASE}/bin/whale_server" 2>&1 &
+
+  # Open DP server port (9997-9998).
+  local rule1="INPUT -p tcp --dport 9997 -j ACCEPT"
+  local rule2="INPUT -p tcp --dport 9998 -j ACCEPT"
+  iptables -C "${rule1}" 2>/dev/null || iptables -A "${rule1}"
+  iptables -C "${rule2}" 2>/dev/null || iptables -A "${rule2}"
+
+  # Start DP servers.
+  "${FACTORY_BASE}/bin/dolphin_uno_server" --hdmi --checkhdmi --port=9997 \
+      --debug --uvc_port=2-1 --uvc_device_name=Left_Raiden \
+      >"$DPSERVER_LEFT_LOG" 2>&1 &
+  "${FACTORY_BASE}/bin/dolphin_uno_server" --hdmi --checkhdmi --port=9998 \
+      --debug --uvc_port=2-2 --uvc_device_name=Right_Raiden \
+      >"$DPSERVER_RIGHT_LOG" 2>&1 &
+}
+
+serve_whale_host
