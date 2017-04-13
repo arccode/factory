c3b4e0d8dce4688578f81e677e3fa6f90471266e
Hung-Te Lin <hungte@chromium.org>
2017-04-13 16:56:39 +0800

    init: Add a goofy.d/device service 'device_id.sh'
    A new service that will track and provision right device ID when system
    reboots.
    
    BUG=chromium:711200,chromium:711118
    TEST=manually booted on device.
    
    Change-Id: I0dfaa45bce50a48b9f70d199a422e570b3043cba
    Reviewed-on: https://chromium-review.googlesource.com/476455
    Commit-Ready: Pi-Hsun Shih <pihsun@chromium.org>
    Tested-by: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

---
+++ init/goofy.d/device/device_id.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# Copyright 2017 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Maintains '/var/factory/.device_id'. When .device_id is changed, invoke all
+# scripts inside device_id/ folder.
+
+SCRIPT="$(readlink -f "$0")"
+ON_CHANGE_DIR="${SCRIPT%.sh}"
+FACTORY_DIR="$(readlink -f "$(dirname "$(readlink -f "$0")")/../../../")"
+DATA_DIR=/var/factory
+
+DEVICE_ID_FILE="${DATA_DIR}/.device_id"
+LAST_DEVICE_ID_FILE="${DATA_DIR}/.last_device_id"
+
+mkdir -p "${DATA_DIR}"
+if [ -f "${DEVICE_ID_FILE}" ]; then
+  cp -f "${DEVICE_ID_FILE}" "${LAST_DEVICE_ID_FILE}"
+fi
+
+"${FACTORY_DIR}/bin/device_id" >"${DEVICE_ID_FILE}"
+
+if [ -f "${LAST_DEVICE_ID_FILE}" ]; then
+  if [ "$(cat "${DEVICE_ID_FILE}")" != "$(cat "${LAST_DEVICE_ID_FILE}")" ]; then
+    echo "Device ID changed!"
+    for file in "${ON_CHANGE_DIR}"/*; do
+      if [ -x "${file}" ]; then
+        echo "Invoke ${file}..."
+        "${file}"
+      fi
+    done
+  fi
+fi
---
+++ init/goofy.d/device/device_id/README.md
@@ -0,0 +1,5 @@
+# Chrome OS Factory Device ID Change Scripts
+
+Script here will be executed when a device ID change is detected.
+
+To add your own service, create a script file with execute (+x) permission.
