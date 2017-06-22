b195c0de753c2a18abf4c1a262a30274bc69c0c1
Peter Shih <pihsun@chromium.org>
2017-06-22 12:21:20 +0800

    goofy_ghost: Add unittest to test JSON schema.
    Add a simple unittest to test JSON schema is correct against default
    JSON and the sample JSON config.
    
    BUG=None
    TEST=make test
    
    Change-Id: I7d7fad0419101d74d7fd95f72d3c6b17c65563ab
    Reviewed-on: https://chromium-review.googlesource.com/544738
    Commit-Ready: Pi-Hsun Shih <pihsun@chromium.org>
    Tested-by: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Youcheng Syu <youcheng@chromium.org>

---
+++ py/tools/goofy_ghost/goofy_ghost_unittest.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# Copyright 2017 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+import json
+import os
+import unittest
+
+import jsonschema
+
+
+class GoofyGhostSchemaTest(unittest.TestCase):
+
+  def loadJSON(self, name):
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    with open(os.path.join(parent_dir, name), 'r') as fin:
+      return json.load(fin)
+
+  def runTest(self):
+    schema = self.loadJSON('goofy_ghost.schema.json')
+    jsonschema.validate(self.loadJSON('goofy_ghost.json'), schema)
+    jsonschema.validate(self.loadJSON('goofy_ghost.sample.json'), schema)
+
+
+if __name__ == '__main__':
+  unittest.main()
