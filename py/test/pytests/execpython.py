b1ce9feba72f1f658745be05ce9e939d69b88c8f
Ting Shen <phoenixshen@google.com>
2017-07-04 17:49:33 +0800

    execpython: add document string
    BUG=chromium:737920
    TEST=make doc
    
    Change-Id: I992fc9127e2f37f892e0d948278296532d23810c
    Reviewed-on: https://chromium-review.googlesource.com/559035
    Commit-Ready: Ting Shen <phoenixshen@chromium.org>
    Tested-by: Ting Shen <phoenixshen@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

--- py/test/pytests/execpython.py
+++ py/test/pytests/execpython.py
@@ -2,6 +2,38 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+"""A test to run arbitary python scripts.
+
+Description
+-----------
+This test executes the python script specified in `script` argument in an
+empty context with only the current `test_info` object
+(an instance of :py:class:`cros.factory.goofy.invocation.PytestInfo`) provided.
+
+This is intended to be a dummy test for other unittests like
+`goofy_unittest`, usual factory procedure should not use this test.
+
+Test Procedure
+--------------
+This is an automated test without user interaction.
+
+Runs the given script and fails if any exception raises during
+execution.
+
+Dependency
+----------
+None.
+
+Examples
+--------
+A test that always pass::
+
+  FactoryTest(pytest_name='execpython',
+              dargs=dict(
+                  script='assert 1 == 1'))
+
+"""
+
 import logging
 import unittest
 
