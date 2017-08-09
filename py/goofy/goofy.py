arccode/factory

commit acbe99ca1f410e5677887c6a431d3fc9862704f0

goofy: implement goofy_server
Currently cros.factory.test.state is not only a state data storage, but
also a complicated server that:
   1. Serves static files for Goofy UI.
   2. Handles RPC for Goofy.
   3. Partially handles test list status.

To achieve this, we see code that inject goofy_rpc functions into state
instance to get RPC support, which is not really intuitive.

As we'd like to support RPC for Goofy plugins, state server will be too
complicated and hard to maintain.

In this CL, we implement a goofy_server that is designed to handle above
memtioned web request, including serving static files for UI and
handling multiple RPC instances.

The goal is to make the responsibility of each components more clear:
  - state: handles shared data.
  - goofy/goofy_rpc: organizes the system and handles test status.
  - goofy_server: handles all web requests.
  - goofy plugins: additional functions for goofy.

BUG=chromium:658151
TEST=make test
     run amd64-generic-goofy toolkit.

Change-Id: Ie5b50fef113114542f3574e75a625046f99d5063
Reviewed-on: https://chromium-review.googlesource.com/446142
Commit-Ready: Shun-Hsing Ou <shunhsingou@chromium.org>
Tested-by: Shun-Hsing Ou <shunhsingou@chromium.org>
Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>

py/goofy/goofy.py
@@ -28,6 +28,7 @@ import factory_common  # pylint: disable=W0611
 from cros.factory.device import device_utils
 from cros.factory.goofy.goofy_base import GoofyBase
 from cros.factory.goofy.goofy_rpc import GoofyRPC
+from cros.factory.goofy import goofy_server
 from cros.factory.goofy.invocation import TestInvocation
 from cros.factory.goofy.link_manager import PresenterLinkManager
 from cros.factory.goofy.plugins import plugin_controller
@@ -40,6 +41,7 @@ from cros.factory.goofy.web_socket_manager import WebSocketManager
 from cros.factory.test.e2e_test.common import AutomationMode
 from cros.factory.test.e2e_test.common import AutomationModePrompt
 from cros.factory.test.e2e_test.common import ParseAutomationMode
+from cros.factory.test.env import goofy_proxy
 from cros.factory.test.env import paths
 from cros.factory.test.event import Event
 from cros.factory.test.event import EventClient
@@ -84,7 +86,6 @@ MAX_CRASH_FILE_SIZE = 64 * 1024
 Status = type_utils.Enum(['UNINITIALIZED', 'INITIALIZING', 'RUNNING',
                           'TERMINATING', 'TERMINATED'])
 
-
 def get_hwid_cfg():
   """Returns the HWID config tag, or an empty string if none can be found."""
   if 'CROS_HWID' in os.environ:
@@ -93,7 +94,6 @@ def get_hwid_cfg():
     with open(HWID_CFG_PATH, 'r') as hwid_cfg_handle:
       return hwid_cfg_handle.read().strip()
   return ''
-
 
 _inited_logging = False
 
@@ -137,8 +137,8 @@ class Goofy(GoofyBase):
     super(Goofy, self).__init__()
     self.uuid = str(uuid.uuid4())
     self.state_instance = None
-    self.state_server = None
-    self.state_server_thread = None
+    self.goofy_server = None
+    self.goofy_server_thread = None
     self.goofy_rpc = None
     self.event_server = None
     self.event_server_thread = None
@@ -242,12 +242,12 @@ class Goofy(GoofyBase):
       logging.info('Stopping web sockets')
       self.web_socket_manager.close()
       self.web_socket_manager = None
-    if self.state_server_thread:
-      logging.info('Stopping state server')
-      self.state_server.shutdown()
-      self.state_server_thread.join()
-      self.state_server.server_close()
-      self.state_server_thread = None
+    if self.goofy_server_thread:
+      logging.info('Stopping goofy server')
+      self.goofy_server.shutdown()
+      self.goofy_server_thread.join()
+      self.goofy_server.server_close()
+      self.goofy_server_thread = None
     if self.state_instance:
       self.state_instance.close()
     if self.event_server_thread:
@@ -291,21 +291,32 @@ class Goofy(GoofyBase):
     logging.info('Done destroying Goofy')
     self.status = Status.TERMINATED
 
-  def start_state_server(self):
+  def start_goofy_server(self):
+    self.goofy_server = goofy_server.GoofyServer(
+        (goofy_proxy.DEFAULT_GOOFY_ADDRESS, goofy_proxy.DEFAULT_GOOFY_PORT))
+    logging.info('Starting goofy server')
+    self.goofy_server_thread = threading.Thread(
+        target=self.goofy_server.serve_forever,
+        name='GoofyServer')
+    self.goofy_server_thread.start()
+
+    # Setup static file path
+    self.goofy_server.RegisterPath(
+        '/', os.path.join(paths.FACTORY_PACKAGE_PATH, 'goofy/static'))
+
+  def init_state_instance(self):
     # Before starting state server, remount stateful partitions with
     # no commit flag.  The default commit time (commit=600) makes corruption
     # too likely.
     sys_utils.ResetCommitTime()
+    self.state_instance = state.FactoryState()
+    self.goofy_server.AddRPCInstance(goofy_proxy.STATE_URL, self.state_instance)
 
-    self.state_instance, self.state_server = (
-        state.create_server(bind_address='0.0.0.0'))
+    # Setup Goofy RPC.
+    # TODO(shunhsingou): separate goofy_rpc and state server instead of
+    # injecting goofy_rpc functions into state.
     self.goofy_rpc = GoofyRPC(self)
     self.goofy_rpc.RegisterMethods(self.state_instance)
-    logging.info('Starting state server')
-    self.state_server_thread = threading.Thread(
-        target=self.state_server.serve_forever,
-        name='StateServer')
-    self.state_server_thread.start()
 
   def start_event_server(self):
     self.event_server = EventServer()
@@ -319,13 +330,13 @@ class Goofy(GoofyBase):
         callback=self.handle_event, event_loop=self.run_queue)
 
     self.web_socket_manager = WebSocketManager(self.uuid)
-    self.state_server.add_handler('/event',
-                                  self.web_socket_manager.handle_web_socket)
+    self.goofy_server.AddHTTPGetHandler(
+        '/event', self.web_socket_manager.handle_web_socket)
 
   def start_terminal_server(self):
     self.terminal_manager = TerminalManager()
-    self.state_server.add_handler('/pty',
-                                  self.terminal_manager.handle_web_socket)
+    self.goofy_server.AddHTTPGetHandler(
+        '/pty', self.terminal_manager.handle_web_socket)
 
   def set_visible_test(self, test):
     if self.visible_test == test:
@@ -1222,7 +1233,8 @@ class Goofy(GoofyBase):
           handshake_timeout=self.options.handshake_timeout,
           standalone=self.options.standalone)
 
-    self.start_state_server()
+    self.start_goofy_server()
+    self.init_state_instance()
     self.state_instance.set_shared_data('hwid_cfg', get_hwid_cfg())
     self.state_instance.set_shared_data('ui_scale_factor',
                                         self.options.ui_scale_factor)

py/goofy/goofy_unittest.py
@@ -28,18 +28,17 @@ from mox import IgnoreArg
 from ws4py.client import WebSocketBaseClient
 
 from cros.factory.goofy import goofy
-from cros.factory.test import factory
-from cros.factory.test import state
-
 from cros.factory.goofy.goofy import Goofy
 from cros.factory.goofy.prespawner import PytestPrespawner
 from cros.factory.goofy.test_environment import Environment
+from cros.factory.test.env import goofy_proxy
 from cros.factory.test.event import Event
+from cros.factory.test import factory
 from cros.factory.test.factory import TestState
+from cros.factory.test import state
 from cros.factory.test.test_lists import test_lists
 from cros.factory.utils import net_utils
 from cros.factory.utils.process_utils import Spawn
-
 
 def _BuildTestList(test_items, options):
   """Build a test list
@@ -142,12 +141,13 @@ class GoofyTest(unittest.TestCase):
     # Log the name of the test we're about to run, to make it easier
     # to grok the logs.
     logging.info('*** Running test %s', type(self).__name__)
-    state.DEFAULT_FACTORY_STATE_PORT = net_utils.FindUnusedTCPPort()
+    goofy_proxy.DEFAULT_GOOFY_PORT = net_utils.FindUnusedTCPPort()
+    state.DEFAULT_FACTORY_STATE_PORT = goofy_proxy.DEFAULT_GOOFY_PORT
     logging.info('Using port %d for factory state',
-                 state.DEFAULT_FACTORY_STATE_PORT)
+                 goofy_proxy.DEFAULT_GOOFY_PORT)
     self.mocker = mox.Mox()
     self.env = self.mocker.CreateMock(Environment)
-    self.state = state.get_instance()
+    self.state = goofy_proxy.get_rpc_proxy()
     self.before_init_goofy()
     self.mocker.ReplayAll()
     self.goofy = init_goofy(self.env, self.test_list, self.options,

py/test/env/goofy_proxy.py
@@ -1,0 +1,33 @@
+# Copyright 2017 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from jsonrpclib import jsonrpc
+
+import factory_common  # pylint: disable=unused-import
+from cros.factory.utils import net_utils
+
+# Default address and port that goofy server will bind on.
+DEFAULT_GOOFY_PORT = 0x0FAC
+DEFAULT_GOOFY_ADDRESS = net_utils.LOCALHOST
+
+# The URL for state and goofy server.
+# TODO(shunhsingou): currently goofy_rpc and state use the same instance and
+# URL path. Separate them in the future.
+STATE_URL = '/goofy'
+GOOFY_RPC_URL = '/goofy'
+GOOFY_SERVER_URL = '/'
+
+
+def get_rpc_proxy(address=None, port=None, url=GOOFY_RPC_URL):
+  """Gets an instance (for client side) to access the goofy server.
+
+  Args:
+    address: Address of the server to be connected.
+    port: Port of the server to be connected.
+    url: Target URL for the RPC server. Default to Goofy RPC.
+  """
+  address = address or DEFAULT_GOOFY_ADDRESS
+  port = port or DEFAULT_GOOFY_PORT
+  return jsonrpc.ServerProxy(
+      'http://%s:%d%s' % (address, port, url))

py/goofy/goofy_server.py
@@ -1,0 +1,317 @@
+#!/usr/bin/env python
+# Copyright 2017 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Goofy server that handles Web request that Goofy needs."""
+
+import logging
+import mimetypes
+import os
+import Queue
+import shutil
+import SocketServer
+import threading
+import time
+from uuid import uuid4
+
+from jsonrpclib import SimpleJSONRPCServer
+
+import factory_common  # pylint: disable=unused-import
+from cros.factory.utils import jsonrpc_utils
+from cros.factory.utils import sync_utils
+
+
+class PathResolver(object):
+  """Resolves paths in URLs."""
+
+  def __init__(self):
+    self._paths = {}
+    self._lock = threading.RLock()
+
+  @sync_utils.Synchronized
+  def AddPath(self, url_path, local_path):
+    """Adds a prefix mapping:
+
+    For example,
+
+      AddPath('/foo', '/usr/local/docs')
+
+    will cause paths to resolved as follows:
+
+      /foo      -> /usr/local/docs
+      /foo/index.html -> /usr/local/docs/index.html
+
+    Args:
+      url_path: The path in the URL
+    """
+    self._paths[url_path] = local_path
+
+  @sync_utils.Synchronized
+  def AddHandler(self, url_path, callback):
+    """Adds a handler on url_path
+
+    The handler should be a callback function that has the request object as
+    the only argument.
+    """
+    self._paths[url_path] = callback
+
+  @sync_utils.Synchronized
+  def RemoveHandler(self, url_path):
+    """Removes the handler"""
+    del self._paths[url_path]
+
+  def Resolve(self, url_path):
+    """Resolves a path mapping.
+
+    Returns the mapped file path or the handler. Returns None if no paths match.
+
+    Args:
+      url_path: A path in a URL (starting with /).
+    """
+    if not url_path.startswith('/'):
+      return None
+
+    prefix = url_path
+    while prefix != '':
+      value = self._paths.get(prefix)
+      if value:
+        suffix = url_path[len(prefix):]
+        if isinstance(value, str):
+          return value + suffix
+        elif suffix == '':
+          return value
+      prefix, unused_sep, suffix = prefix.rpartition('/')
+      # For '/xxx', we also need to check '/' as the prefix.
+      if prefix == '' and suffix != '':
+        prefix = '/'
+        url_path = '/' + url_path
+
+    # Nothing found.
+    return None
+
+
+class GoofyWebRequestHandler(
+    jsonrpc_utils.MultiPathJSONRPCRequestHandler):
+  """RequestHandler used by GoofyServer
+
+  This class extands SimpleJSONRPCRequestHandler to support HTTP GET request
+  for Goofy server. See more explaination in GoofyServer.
+  """
+
+  def do_GET(self):
+    logging.debug('HTTP GET request for path %s', self.path)
+
+    if '..' in self.path.split('/'):
+      logging.warn('Invalid path')
+      self.send_response(404)
+      return
+
+    if self.path.endswith('/'):
+      self.path += 'index.html'
+
+    # pylint: disable=protected-access
+    callback_or_path = self.server._resolver.Resolve(self.path)
+
+    if callable(callback_or_path):
+      return callback_or_path(self)
+
+    local_path = callback_or_path
+
+    mime_type = mimetypes.guess_type(self.path)
+    if not mime_type:
+      logging.warn('Unable to guess MIME type')
+      self.send_response(404)
+      return
+
+    if not local_path or not os.path.exists(local_path):
+      logging.warn('File not found: %s', (local_path or self.path))
+      self.send_response(404)
+      return
+
+    self.send_response(200)
+    self.send_header('Content-Type', mime_type[0])
+    self.send_header('Content-Length', os.path.getsize(local_path))
+    self.end_headers()
+    with open(local_path) as f:
+      shutil.copyfileobj(f, self.wfile)
+
+
+class GoofyServer(SocketServer.ThreadingMixIn,
+                  jsonrpc_utils.MultiPathJSONRPCServer):
+  """Server that handles Web request that Goofy used
+
+  GoofyServer supports the following functions:
+
+  - Multi-path JSON-RPC server: Handles different RPC instance on different
+    URL. See document of
+    `cros.factory.utils.jsonrpc_utils.MultiPathJSONRPCServer` and
+    `AddRPCInstance` for detail.
+
+  - Dynamically maps HTTP GET request to real path on the file system or data
+    on the memory. See `RegisterPath`, `URLForFile`, `URLForData`.
+
+  - JSON-RPC on '/' for `URLForFile` and `URLForData`. So those functions are
+    also available on the client side.
+
+  - Dynamically maps HTTP GET request to a callback function.
+    See `AddHTTPGetHandler` for detail.
+  """
+
+  _PREFIX_GENERATED_FILE = '/generated-files'
+  _PREFIX_GENERATED_DATA = '/generated-data'
+
+  def __init__(self, addr, logRequests=False):
+    # pylint: disable=protected-access
+    # We have some icons in SVG format, but this isn't recognized in
+    # the standard Python mimetypes set.
+    mimetypes.add_type('image/svg+xml', '.svg')
+
+    jsonrpc_utils.MultiPathJSONRPCServer.__init__(
+        self, addr, GoofyWebRequestHandler, logRequests=logRequests)
+    self._generated_data = {}
+    self._generated_data_expiration = Queue.PriorityQueue()
+    self._resolver = PathResolver()
+
+    # Used by sync_utils.Synchronized
+    self._lock = threading.RLock()
+
+    # Add RPC functions supported by this server.
+    self.AddRPCInstance('/', GoofyServerRPC(self))
+
+  @sync_utils.Synchronized
+  def AddRPCInstance(self, url, instance):
+    """Adds RPC instance to the server
+
+    The public functions of `instance` would be available via in JSON-RPC call
+    on `url`.
+
+    Example usage:
+
+    # Server
+    class MyClass(object):
+      def Foo(self):
+        # Do something
+        pass
+
+    server.AddRPCInstance('/MyClass', MyClass())
+
+    server.serve_forever()
+
+    # Client
+    from jsonrpclib import jsonrpc
+
+    proxy = jsonrpc.ServerProxy('http://<address>:<port>/MyClass')
+    proxy.Foo()
+    """
+
+    dispatcher = SimpleJSONRPCServer.SimpleJSONRPCDispatcher()
+    dispatcher.register_introspection_functions()
+    dispatcher.register_instance(instance)
+    self.add_dispatcher(url, dispatcher)
+
+  @sync_utils.Synchronized
+  def AddHTTPGetHandler(self, url, callback):
+    """Adds HTTP GET handler to the server
+
+    The handle is a callback function that takes the request object as the only
+    argument.
+    """
+    self._resolver.AddHandler(url, callback)
+
+  @sync_utils.Synchronized
+  def URLForFile(self, path):
+    """Returns a URL that can be used to serve a local file.
+
+    Args:
+      path: path to the local file
+
+    Returns:
+      url: A (possibly relative) URL that refers to the file
+    """
+    uuid = str(uuid4())
+    uri_path = '%s/%s/%s' % (self._PREFIX_GENERATED_FILE,
+                             uuid,
+                             os.path.basename(path))
+    self._resolver.AddPath('%s/%s' % (self._PREFIX_GENERATED_FILE, uuid),
+                           os.path.dirname(path))
+    return uri_path
+
+  @sync_utils.Synchronized
+  def URLForData(self, mime_type, data, expiration_secs=None):
+    """Returns a URL that can be used to serve a static collection of bytes.
+
+    Args:
+      mime_type: MIME type for the data
+      data: Data to serve
+      expiration_secs: If not None, the number of seconds in which
+          the data will expire.
+    """
+    uuid = str(uuid4())
+    expiration_deadline = None
+
+    if expiration_secs:
+      now = time.time()
+      expiration_deadline = now + expiration_secs
+      self._generated_data_expiration.put((expiration_deadline, uuid))
+
+    uri_path = '%s/%s' % (self._PREFIX_GENERATED_DATA, uuid)
+    self._resolver.AddHandler(
+        uri_path,
+        lambda handler: self._HandleGetGeneratedData(
+            handler, mime_type, data, expiration_deadline))
+
+    self._CheckGeneratedDataExpired()
+    return uri_path
+
+  def _HandleGetGeneratedData(self, handler, mime_type, data,
+                              expiration_deadline=None):
+    """The handler used by URLForData"""
+    self._CheckGeneratedDataExpired()
+    if expiration_deadline and time.time() > expiration_deadline:
+      logging.warn('Expired generated data')
+      handler.send_response(404)
+      return
+
+    handler.send_response(200)
+    handler.send_header('Content-Type', mime_type)
+    handler.send_header('Content-Length', len(data))
+    handler.end_headers()
+    handler.wfile.write(data)
+
+  def _CheckGeneratedDataExpired(self):
+    """Checks and expire temp data."""
+    # Reap old items.
+    now = time.time()
+    while True:
+      try:
+        item = self._generated_data_expiration.get_nowait()
+      except Queue.Empty:
+        break
+
+      if item[0] < now:
+        self._resolver.RemoveHandler(
+            '%s/%s' % (self._PREFIX_GENERATED_DATA, item[1]))
+      else:
+        # Not expired yet; put it back and we're done
+        self._generated_data_expiration.put(item)
+        break
+
+  @sync_utils.Synchronized
+  def RegisterPath(self, url_path, local_path):
+    """Register url_path to the local_path on the real file system"""
+    self._resolver.AddPath(url_path, local_path)
+
+class GoofyServerRPC(object):
+  """Native functions supported by GoofyServer."""
+  def __init__(self, server):
+    self._server = server
+
+  def URLForData(self, mime_type, data, expiration_secs=None):
+    return self._server.URLForData(mime_type, data, expiration_secs)
+
+  def URLForFile(self, path):
+    return self._server.URLForFile(path)
+
+  def RegisterPath(self, url_path, local_path):
+    return self._server.RegisterPath(url_path, local_path)

py/goofy/goofy_server_unittest.py
@@ -1,0 +1,218 @@
+#!/usr/bin/python -u
+#
+# Copyright 2017 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from jsonrpclib import jsonrpc
+import os
+import threading
+import time
+import unittest
+import urllib2
+
+import factory_common  # pylint: disable=unused-import
+from cros.factory.goofy import goofy_server
+from cros.factory.utils import file_utils
+from cros.factory.utils import net_utils
+from cros.factory.utils import sync_utils
+
+
+class PathResolverTest(unittest.TestCase):
+
+  def _Callback(self):
+    pass
+
+  def testWithRoot(self):
+    resolver = goofy_server.PathResolver()
+    resolver.AddPath('/', '/root')
+    resolver.AddPath('/a/b', '/c/d')
+    resolver.AddPath('/a', '/e')
+    resolver.AddHandler('/callback', self._Callback)
+
+    for url_path, expected in (
+        ('/', '/root'),
+        ('/a/b', '/c/d'),
+        ('/a', '/e'),
+        ('/a/b/X', '/c/d/X'),
+        ('/a/X', '/e/X'),
+        ('/X', '/root/X'),
+        ('/X/', '/root/X/'),
+        ('/X/Y', '/root/X/Y'),
+        ('Blah', None),
+        ('/callback', self._Callback)):
+      self.assertEqual(expected,
+                       resolver.Resolve(url_path))
+
+  def testNoRoot(self):
+    resolver = goofy_server.PathResolver()
+    resolver.AddPath('/a/b', '/c/d')
+    self.assertEqual(None, resolver.Resolve('/b'))
+    self.assertEqual('/c/d/X', resolver.Resolve('/a/b/X'))
+
+  def testRootHandler(self):
+    resolver = goofy_server.PathResolver()
+    resolver.AddHandler('/', self._Callback)
+    resolver.AddPath('/a', '/e')
+
+    self.assertEqual(resolver.Resolve('/'), self._Callback)
+    self.assertEqual(resolver.Resolve('/a/b'), '/e/b')
+    self.assertEqual(resolver.Resolve('/b'), None)
+
+
+class GoofyServerTest(unittest.TestCase):
+
+  def setUp(self):
+    def ServerReady():
+      try:
+        urllib2.urlopen(
+            'http://%s:%d%s' % (net_utils.LOCALHOST, self.port, '/not_exists'))
+      except urllib2.HTTPError as err:
+        if err.code == 404:
+          return True
+      return False
+
+    self.port = net_utils.FindUnusedTCPPort()
+    self.server = goofy_server.GoofyServer(
+        (net_utils.LOCALHOST, self.port))
+    self.server_thread = threading.Thread(
+        target=self.server.serve_forever,
+        name='GoofyServer')
+    self.server_thread.start()
+
+    # Wait for server to start.
+    sync_utils.WaitFor(ServerReady, 0.1)
+
+  def testAddRPCInstance(self):
+    class RPCInstance(object):
+      def __init__(self):
+        self.called = False
+
+      def Func(self):
+        self.called = True
+
+    instance = RPCInstance()
+    self.server.AddRPCInstance('/test', instance)
+
+    proxy = jsonrpc.ServerProxy(
+        'http://%s:%d/test' % (net_utils.LOCALHOST, self.port))
+    proxy.Func()
+    self.assertTrue(instance.called)
+
+  def testAddHTTPGetHandler(self):
+    data = '<html><body><h1>Hello</h1></body></html>'
+    mime_type = 'text/html'
+
+    def MyHandler(handler):
+      handler.send_response(200)
+      handler.send_header('Content-Type', mime_type)
+      handler.send_header('Content-Length', len(data))
+      handler.end_headers()
+      handler.wfile.write(data)
+
+    self.server.AddHTTPGetHandler('/test', MyHandler)
+
+    response = urllib2.urlopen(
+        'http://%s:%d/test' % (net_utils.LOCALHOST, self.port))
+    self.assertEqual(200, response.getcode())
+    self.assertEqual(data, response.read())
+    response.close()
+
+  def testRegisterPath(self):
+    data = '<html><body><h1>Hello</h1></body></html>'
+    with file_utils.TempDirectory() as path:
+      with open(os.path.join(path, 'index.html'), 'w') as f:
+        f.write(data)
+
+      self.server.RegisterPath('/', path)
+      response = urllib2.urlopen(
+          'http://%s:%d/' % (net_utils.LOCALHOST, self.port))
+      self.assertEqual(200, response.getcode())
+      self.assertEqual(data, response.read())
+      response.close()
+
+      # Check svg mime type
+      with open(os.path.join(path, 'test.svg'), 'w') as f:
+        f.write(data)
+      response = urllib2.urlopen(
+          'http://%s:%d/test.svg' % (net_utils.LOCALHOST, self.port))
+      self.assertEqual(200, response.getcode())
+      self.assertEqual(data, response.read())
+      response.close()
+
+  def testURLForData(self):
+    data = '<html><body><h1>Hello</h1></body></html>'
+
+    url = self.server.URLForData('text/html', data)
+
+    response = urllib2.urlopen(
+        'http://%s:%d%s' % (net_utils.LOCALHOST, self.port, url))
+    self.assertEqual(200, response.getcode())
+    self.assertEqual(data, response.read())
+    response.close()
+
+  def testGoofyServerRPC(self):
+    proxy = jsonrpc.ServerProxy(
+        'http://%s:%d/' % (net_utils.LOCALHOST, self.port))
+    self.assertItemsEqual(
+        ['URLForData',
+         'URLForFile',
+         'RegisterPath',
+         'system.listMethods',
+         'system.methodHelp',
+         'system.methodSignature'],
+        proxy.system.listMethods())
+
+    data = '<html><body><h1>Hello</h1></body></html>'
+    url = proxy.URLForData('text/html', data)
+    response = urllib2.urlopen(
+        'http://%s:%d%s' % (net_utils.LOCALHOST, self.port, url))
+    self.assertEqual(200, response.getcode())
+    self.assertEqual(data, response.read())
+    response.close()
+
+  def testURLForFile(self):
+    data = '<html><body><h1>Hello</h1></body></html>'
+    with file_utils.UnopenedTemporaryFile() as path:
+      with open(path, 'w') as f:
+        f.write(data)
+
+      url = self.server.URLForFile(path)
+      response = urllib2.urlopen(
+          'http://%s:%d%s' % (net_utils.LOCALHOST, self.port, url))
+      self.assertEqual(200, response.getcode())
+      self.assertEqual(data, response.read())
+      response.close()
+
+  def testURLForDataExpire(self):
+    data = '<html><body><h1>Hello</h1></body></html>'
+
+    url = self.server.URLForData('text/html', data, 0.8)
+
+    response = urllib2.urlopen(
+        'http://%s:%d%s' % (net_utils.LOCALHOST, self.port, url))
+    self.assertEqual(200, response.getcode())
+    self.assertEqual(data, response.read())
+    response.close()
+
+    time.sleep(1)
+
+    # The data should expired now.
+    with self.assertRaises(urllib2.HTTPError):
+      response = urllib2.urlopen(
+          'http://%s:%d%s' % (net_utils.LOCALHOST, self.port, url))
+
+  def testURLNotFound(self):
+    with self.assertRaisesRegexp(urllib2.HTTPError, '404: Not Found'):
+      response = urllib2.urlopen(
+          'http://%s:%d%s' % (net_utils.LOCALHOST, self.port, '/not_exists'))
+      response.close()
+
+  def tearDown(self):
+    self.server.shutdown()
+    self.server_thread.join()
+    self.server.server_close()
+
+
+if __name__ == '__main__':
+  unittest.main()

py/goofy/js/goofy.js
@@ -2833,7 +2833,7 @@ cros.factory.Goofy.prototype.sendRpc = function(
   cros.factory.logger.info('RPC request: ' + request);
   var factoryThis = this;
   goog.net.XhrIo.send(
-      '/', function() {
+      '/goofy', function() {
         cros.factory.logger.info('RPC response for ' + method + ': ' +
             this.getResponseText());
 

commit d5b96bf789309a78f48751a2887caca2e6962614

goofy: Goofy server should bind on all addresses.
In presenter mode, the device connect to Goofy server
via the DHCP IP. The Goofy server should bind on all
possible IPs (via binding 0.0.0.0).

This should be a regression bug intrdouced in commit
acbe99ca1f410e5677887c6a431d3fc9862704f0 during code
refactoring.

BUG=none
TEST=make test
TEST=manually test presenter mode

Change-Id: Icb0fa6e9685d241d171b57feec0c93a2d3af5463
Reviewed-on: https://chromium-review.googlesource.com/605411
Commit-Ready: Shen-En Shih <petershih@chromium.org>
Tested-by: Shen-En Shih <petershih@chromium.org>
Reviewed-by: Hung-Te Lin <hungte@chromium.org>

py/goofy/goofy.py
@@ -279,7 +279,7 @@ class Goofy(GoofyBase):
 
   def start_goofy_server(self):
     self.goofy_server = goofy_server.GoofyServer(
-        (goofy_proxy.DEFAULT_GOOFY_ADDRESS, goofy_proxy.DEFAULT_GOOFY_PORT))
+        (goofy_proxy.DEFAULT_GOOFY_BIND, goofy_proxy.DEFAULT_GOOFY_PORT))
     logging.info('Starting goofy server')
     self.goofy_server_thread = threading.Thread(
         target=self.goofy_server.serve_forever,

py/utils/net_utils.py
@@ -28,6 +28,7 @@ DEFAULT_TIMEOUT = 10
 # causes unexpected behaviour.  We want to force the numerical IPv4 address, so
 # that these systems run tests under IPv4.
 LOCALHOST = '127.0.0.1'
+INADDR_ANY = '0.0.0.0'
 MAX_PORT = 65535
 FULL_MASK = 2 ** 32 - 1
 FULL_MASK6 = 2 ** 128 - 1

py/goofy/goofy_unittest.py
@@ -169,7 +169,7 @@ class GoofyTest(unittest.TestCase):
 
     if self.mock_goofy_server:
       server = goofy.goofy_server.GoofyServer((
-          goofy_proxy.DEFAULT_GOOFY_ADDRESS,
+          goofy_proxy.DEFAULT_GOOFY_BIND,
           goofy_proxy.DEFAULT_GOOFY_PORT))
       # We do not use mox for server.serve_forever, since the method is run in
       # another thread, and mox object are NOT thread safe.

py/test/env/goofy_proxy.py
@@ -10,6 +10,7 @@ from cros.factory.utils import net_utils
 # Default address and port that goofy server will bind on.
 DEFAULT_GOOFY_PORT = 0x0FAC
 DEFAULT_GOOFY_ADDRESS = net_utils.LOCALHOST
+DEFAULT_GOOFY_BIND = net_utils.INADDR_ANY
 
 # The URL for state and goofy server.
 # TODO(shunhsingou): currently goofy_rpc and state use the same instance and