8223296fe8f70ec1cb41bc5050b0088186e112ff
Wei-Han Chen <stimim@google.com>
2017-08-28 16:10:24 +0800

    utils: add thread_utils for thread related utilities
    The first function set is `LocalEnv()` utilities, which allow you to
    save thread specific values in a dictionary.  The `SetLocalEnv()` is a
    with-statement context, which will revert the variables you set when
    leaving the with statement.  For example::
    
      def PrintFoo():
        print LocalEnv().get('foo')
    
      def RunTask():
        with SetLocalEnv(foo=1):
          PrintFoo()  # 1
          with SetLocalEnv(foo=2):
            PrintFoo()  # 2
          PrintFoo()  # 1
    
    BUG=None
    TEST=make test
    
    Change-Id: I94b5a19dbbb0c0718086285a94e388af245ef805
    Reviewed-on: https://chromium-review.googlesource.com/637450
    Commit-Ready: Wei-Han Chen <stimim@chromium.org>
    Tested-by: Wei-Han Chen <stimim@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

---
+++ py/utils/thread_utils.py
@@ -0,0 +1,81 @@
+# Copyright 2017 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import contextlib
+import threading
+
+
+"""LocalEnv (local environment data) is key value pairs saved in each thread.
+
+For each thread, ``LocalEnv()`` returns a dictionary bound to current thread.
+You can use ``SetLocalEnv()`` to override some entries in the dictionary, for
+example::
+
+  def PrintFoo():
+    print LocalEnv().get('foo')
+
+  def RunTask():
+    with SetLocalEnv(foo=1):
+      PrintFoo()  # 1
+      with SetLocalEnv(foo=2):
+        PrintFoo()  # 2
+      PrintFoo()  # 1
+
+As you can see, the value will be reverted when the program leaves the ``with``
+context.
+
+In most cases, you should just add an argument for function calls, instead of
+using ``LocalEnv()``.  ``LocalEnv()`` is designed for variables that are thread
+specific, and works like an option.  For example, the utility functions often
+needs to know **which device this function should use**.  It could be the
+station (``CreateStationInterface()``) or DUT (``CreateDUTInterface()``).  We
+don't want to add arguments for all utility functions, so instead we can use
+``LocalEnv()`` as::
+
+  def SomeUtilityFunction():
+    interface = LocalEnv().get('interface', DEFAULT_INTERFACE)
+    # use ``interface`` to do the task
+    # ...
+
+  def Func():
+    with SetLocalEnv(interface=CreateDUTInterface()):
+      SomeUtilityFunction()  # will perform on DUT interface
+    with SetLocalEnv(interface=CreateStationInterface()):
+      SomeUtilityFunction()  # will perform on station interface
+"""
+_local_env = threading.local()
+
+
+class LocalEnvException(Exception):
+  """Exception for LocalEnv."""
+
+
+def _InitLocalEnv():
+  """Initialize the stack if it is not initialized yet."""
+  if not hasattr(_local_env, 'stack'):
+    _local_env.stack = [{}]
+
+
+def LocalEnv():
+  """Get a dictionary saved in current thread context."""
+  _InitLocalEnv()
+  return _local_env.stack[-1]
+
+
+@contextlib.contextmanager
+def SetLocalEnv(**kwargs):
+  _InitLocalEnv()
+  stack_size = len(_local_env.stack)
+  new_env = _local_env.stack[-1].copy()
+  new_env.update(kwargs)
+  _local_env.stack.append(new_env)
+
+  try:
+    yield
+  finally:
+    if len(_local_env.stack) != stack_size + 1:
+      raise LocalEnvException(
+          'mismatched number of append and pop, expected: %d, actual: %d' % (
+              stack_size + 1, len(_local_env.stack)))
+    _local_env.stack.pop()
---
+++ py/utils/thread_utils_unittest.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# Copyright 2017 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import Queue
+import threading
+import unittest
+
+import factory_common  # pylint: disable=unused-import
+from cros.factory.utils import thread_utils
+
+
+class ThreadUtilsUnittest(unittest.TestCase):
+  def setUp(self):
+    self.errors = Queue.Queue()
+
+  def _TestOneThread(self, index):
+    try:
+      with thread_utils.SetLocalEnv(a=index):
+        self.assertEqual(thread_utils.LocalEnv(), {'a': index})
+        with thread_utils.SetLocalEnv(b=index + 1):
+          self.assertEqual(thread_utils.LocalEnv(),
+                           {'a': index, 'b': index + 1})
+        with thread_utils.SetLocalEnv(a=index + 2, b=index + 1):
+          self.assertEqual(thread_utils.LocalEnv(),
+                           {'a': index + 2, 'b': index + 1})
+        self.assertEqual(thread_utils.LocalEnv(), {'a': index})
+      self.assertEqual(thread_utils.LocalEnv(), {})
+    except Exception as e:
+      self.errors.put(e)
+
+  def testSingleThread(self):
+    self._TestOneThread(0)
+
+  def testMultiThread(self):
+    threads = []
+    for index in xrange(10):
+      threads.append(
+          threading.Thread(target=self._TestOneThread, args=(index, )))
+      threads[-1].start()
+
+    for thread in threads:
+      thread.join()
+
+    errors = []
+    while not self.errors.empty():
+      errors.append(self.errors.get())
+
+    self.assertFalse(errors)
+
+
+if __name__ == '__main__':
+  unittest.main()
+
