cc3d0877b2cab7d12eaedd1cfc700af5a8978ac9
Hung-Te Lin <hungte@chromium.org>
2017-08-28 13:37:09 +0800

    pytests: Change 'mosys_platform' to a new 'model_sku' test.
    For projects that need to setup different test arguments for different
    SKU (for example number of cameras) we want everything set in device
    data. The source may come from shopfloor#GetDeviceInfo, or SKU (via
    hardware straps).
    
    The new `model_sku` test will now confirm SKU information and set device
    data according to model and SKU.
    
    BUG=chromium:746932
    TEST=make test; manually run model_sku test.
    
    Change-Id: Ibd3634c1e734f24643e161cc23b829098e08702c
    Reviewed-on: https://chromium-review.googlesource.com/637286
    Commit-Ready: Hung-Te Lin <hungte@chromium.org>
    Tested-by: Hung-Te Lin <hungte@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

--- py/test/pytests/model_sku.py
+++ py/test/pytests/model_sku.py
@@ -2,14 +2,34 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""A test to confirm SKU information.
+"""A test to confirm and set SKU information.
 
 Description
 -----------
-A test to ask OP to confirm SKU information manually.
+A test to ask OP to confirm SKU information manually, then apply SKU or model
+specific settings.
 
-Ths is a test to verify hardware root of trust, so there's no options to
+This is a test to verify hardware root of trust, so there's no options to
 set auto verification for this test.
+
+After the SKU is confirmed, the test will load a JSON configuration specified by
+``config_name``. The config should be a dictionary containing what device data
+(usually ``component.*``) to set for matched model and SKU. For example, to set
+if the touchscreen is available for model 'coral' with default True, and only
+False for SKU 3::
+
+  {
+    "model": {
+      "coral": {
+        "component.has_touchscreen": true
+      }
+    },
+    "sku": {
+      "3": {
+        "component.has_touchscreen": false
+      }
+    }
+  }
 
 Test Procedure
 --------------
@@ -34,13 +54,17 @@ There's no options for this test::
 
 """
 
+import logging
 import unittest
 
 import factory_common  # pylint: disable=unused-import
 from cros.factory.device import device_utils
+from cros.factory.test import device_data
 from cros.factory.test import test_ui
 from cros.factory.test import ui_templates
 from cros.factory.test.i18n import test_ui as i18n_test_ui
+from cros.factory.utils.arg_utils import Arg
+from cros.factory.utils import config_utils
 
 
 _TABLE_CSS = '''
@@ -61,17 +85,43 @@ _TABLE_CSS = '''
 '''
 
 
-class MosysPlatformTest(unittest.TestCase):
+class PlatformSKUModelTest(unittest.TestCase):
+  """A test to confirm and set SKU and model information."""
+
+  ARGS = [
+      Arg('config_name', basestring,
+          'Name of JSON config to load for setting device data.', default=None),
+  ]
+
   def setUp(self):
     self._dut = device_utils.CreateDUTInterface()
     self._ui = test_ui.UI(css=_TABLE_CSS)
     self._template = ui_templates.TwoSections(self._ui)
+    self._config = config_utils.LoadConfig(config_name=self.args.config_name)
+    self._platform = {}
+
+  def ApplyConfig(self):
+    model = self._platform.get('model', '')
+    sku = self._platform.get('sku', '')
+    model_config = self._config.get('model', {}).get(model, {})
+    sku_config = self._config.get('sku', {}).get(sku, {})
+
+    config_utils.OverrideConfig(model_config, sku_config)
+    if model_config:
+      logging.info('Apply model/SKU config: %r', model_config)
+      device_data.UpdateDeviceData(model_config)
+
+  def OnEnter(self, unused_event):
+    del unused_event  # Not used.
+    logging.error('on enter')
+    self.ApplyConfig()
+    self._ui.Pass()
 
   def runTest(self):
     self._template.SetInstruction(
         i18n_test_ui.MakeI18nLabel('Please confirm following values'))
 
-    self._ui.BindKey(test_ui.ENTER_KEY, lambda _: self._ui.Pass())
+    self._ui.BindKey(test_ui.ENTER_KEY, self.OnEnter)
     self._ui.BindKey(test_ui.ESCAPE_KEY,
                      lambda _: self._ui.Fail('Failed by operator'))
 
@@ -84,6 +134,7 @@ class MosysPlatformTest(unittest.TestCase):
       output = self._dut.CallOutput(['mosys', 'platform', arg])
       if output is None:
         output = 'N/A'
+      self._platform[arg] = output.strip()
 
       table.SetContent(i, 0, arg)
       table.SetContent(i, 1, output)
