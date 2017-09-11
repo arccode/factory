3202a886817b3edfabf32577618798e115d95321
chuntsen <chuntsen@google.com>
2017-09-11 14:15:01 +0800

    Instalog: Refactor output_gcs plugin by using google-cloud-python
    Deprecate output_gcs, and refactor output_gcs by using
    google-cloud-python.
    
    BUG=b:64501648, b:62334611
    TEST=manually test on local device
    
    Change-Id: I53c81fdb4a0887ad5d4cef076d71befe04aed035
    Reviewed-on: https://chromium-review.googlesource.com/658167
    Commit-Ready: Chun-Tsen Kuo <chuntsen@chromium.org>
    Tested-by: Chun-Tsen Kuo <chuntsen@chromium.org>
    Reviewed-by: Joel Kitching <kitching@chromium.org>

--- py/instalog/plugins/output_gcs.py
+++ py/instalog/plugins/output_gcs.py
@@ -8,14 +8,21 @@
 
 from __future__ import print_function
 
+import os
+
+# pylint: disable=import-error
+from google.cloud import storage
+from google.oauth2 import service_account
+
 import instalog_common  # pylint: disable=unused-import
 from instalog import plugin_base
 from instalog.utils.arg_utils import Arg
 from instalog.utils import file_utils
-from instalog.utils import gcs_utils
 
 
 _DEFAULT_INTERVAL = 5
+_GCS_SCOPE = 'https://www.googleapis.com/auth/devstorage.read_write'
+_CHUNK_SIZE = 4 * 1024 * 1024  # 4mb
 
 
 class OutputCloudStorage(plugin_base.OutputPlugin):
@@ -44,13 +51,19 @@ class OutputCloudStorage(plugin_base.OutputPlugin):
   ]
 
   def __init__(self, *args, **kwargs):
-    self.gcs = None
+    self.client = None
+    self.bucket = None
+    self.bucket_id = None
+    self.dir_in_bucket = None
     super(OutputCloudStorage, self).__init__(*args, **kwargs)
 
   def SetUp(self):
     """Authenticates the connection to Cloud Storage."""
-    self.gcs = gcs_utils.CloudStorage(self.args.key_path)
-    self.args.target_dir = self.args.target_dir.rstrip('/')
+    self.args.target_dir = self.args.target_dir.strip('/')
+    self.bucket_id, _unused_slash, self.dir_in_bucket = (
+        self.args.target_dir.partition('/'))
+    self.client = self.BuildClient()
+    self.bucket = self.BuildBucket()
 
   def Main(self):
     """Main thread of the plugin."""
@@ -59,6 +72,20 @@ class OutputCloudStorage(plugin_base.OutputPlugin):
         # TODO(kitching): Find a better way to block the plugin when we are in
         #                 one of the PAUSING, PAUSED, or UNPAUSING states.
         self.Sleep(1)
+
+  def BuildClient(self):
+    """Builds a Storage client object."""
+    credentials = service_account.Credentials.from_service_account_file(
+        self.args.key_path, scopes=(_GCS_SCOPE,))
+    return storage.Client(credentials=credentials)
+
+  def BuildBucket(self):
+    """Builds a Storage bucket object."""
+    bucket = storage.Bucket(self.client, self.bucket_id)
+    if not bucket.exists():
+      raise ValueError('Bucket %s doesn\'t exist! Please create it before you '
+                       'run this plugin')
+    return bucket
 
   def ProcessNextBatch(self):
     """Gets the next event with attachments and uploads it.
@@ -106,21 +133,59 @@ class OutputCloudStorage(plugin_base.OutputPlugin):
     for att_id, att_path in event.attachments.iteritems():
       target_filename = (file_utils.SHA1InHex(att_path) if self.args.use_sha1
                          else att_id)
-      target_path = '%s/%s' % (self.args.target_dir, target_filename)
+      path_in_bucket = '%s/%s' % (self.dir_in_bucket, target_filename)
 
       # Upload the file.
-      self.info('Uploading %s --> %s', att_path, target_path)
-      resumable_uri = self.gcs.UploadFile(att_path, target_path)
+      self.info('Uploading to GCS: /%s/%s', self.bucket_id, path_in_bucket)
+      self.UploadFile(att_path, path_in_bucket)
+
 
       # Relocate the attachments entry into the event payload.
-      event.setdefault('__attachments__', {})[att_id] = 'gs:/%s' % target_path
-
-      if resumable_uri:
-        raise IOError('Could not upload file %s successfully: %r'
-                      % (att_id, event))
+      event.setdefault('__attachments__', {})[att_id] = 'gs:/%s/%s' % (
+          self.bucket_id, path_in_bucket)
 
     # Remove attachments from the event for re-emitting.
     event.attachments = {}
+
+  def UploadFile(self, local_path, target_path):
+    """Attempts to upload a file to GCS, with resumability.
+
+    Args:
+      local_path: Path to the file on local disk.
+      target_path: Target path in self.bucket.
+
+    Raises:
+      google.cloud.exceptions.GoogleCloudError if the upload response returns an
+      error status.
+      ValueError if the uploaded file on GCS doesn't exist or has different
+      md5_hash/size.
+    """
+    local_md5 = file_utils.MD5InBase64(local_path)
+    local_size = os.path.getsize(local_path)
+
+    blob = storage.Blob(target_path, self.bucket, chunk_size=_CHUNK_SIZE)
+    if blob.exists():
+      blob.reload()
+      if blob.md5_hash == local_md5 and blob.size == local_size:
+        self.warning('File already exists on remote end with same size (%d) '
+                     'and same MD5 hash (%s); skipping',
+                     blob.size, blob.md5_hash)
+        return
+      else:
+        self.error('File already exists on remote end, but size or MD5 hash '
+                   'doesn\'t match; size on remote %s = %d, size on local %s = '
+                   '%d; will overwrite',
+                   target_path, blob.size, local_path, local_size)
+
+    blob.upload_from_filename(local_path)
+    blob.reload()
+    if not blob.exists():
+      raise ValueError('File doesn\'t exist after uploading')
+    if blob.md5_hash != local_md5 or blob.size != local_size:
+      raise ValueError('Size or MD5 mismatch after uploading; '
+                       'local_size = %d, confirmed_size = %d; local_md5 = %s, '
+                       'confirmed_md5 = %s',
+                       local_size, blob.size, local_md5, blob.md5_hash)
 
 
 if __name__ == '__main__':
