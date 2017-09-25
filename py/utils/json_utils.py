f5ab5188eabfbf8b44df50a74fec1acfe61e5007
You-Cheng Syu <youcheng@chromium.org>
2017-09-25 14:52:40 +0800

    utils: Add module json_utils.
    As we're using more and more JSON files, it would be helpful to have a
    module that helps processing JSON strings and JSON files.
    
    BUG=none
    TEST=manually
    
    Change-Id: I8dab917ff4d9729e5d8e254356fc63a87d62b9dc
    Reviewed-on: https://chromium-review.googlesource.com/673564
    Commit-Ready: Youcheng Syu <youcheng@chromium.org>
    Tested-by: Youcheng Syu <youcheng@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

---
+++ py/utils/json_utils.py
@@ -0,0 +1,108 @@
+# Copyright 2017 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""JSON utilities.
+
+This module provides utilities to serialize or deserialize Python objects
+to/from JSON strings or JSON files.
+"""
+
+import json
+import os
+
+
+def LoadStr(s):
+  """Deserialize a JSON string to a Python object.
+
+  Args:
+    s: a JSON string.
+
+  Returns:
+    The deserialized Python object.
+  """
+  return json.loads(s)
+
+
+def LoadFile(file_path):
+  """Deserialize a file consists of a JSON string to a Python object.
+
+  Args:
+    file_path: The path of the file to be deserialize.
+
+  Returns:
+    The deserialized Python object.
+  """
+  with open(file_path) as f:
+    return json.load(f)
+
+
+def DumpStr(obj, pretty=False):
+  """Serialize a Python object to a JSON string.
+
+  Args:
+    pretty: True to output in human-friendly pretty format.
+
+  Returns:
+    The serialized JSON string.
+  """
+  if pretty:
+    return json.dumps(obj, indent=2, separators=(',', ': '), sort_keys=True)
+  return json.dumps(obj)
+
+
+def DumpFile(file_path, obj, pretty=True, newline=None):
+  """Write serialized JSON string of a Python object to a given file.
+
+  Args:
+    file_path: The path of the file.
+    obj: a Python object to be serialized.
+    pretty: True to output in human-friendly pretty format.
+    newline: True to append a newline in the end of output, default to the
+      previous argument ``pretty``.
+  """
+  if newline is None:
+    newline = pretty
+
+  with open(file_path, 'w') as f:
+    f.write(DumpStr(obj, pretty=pretty))
+    if newline:
+      f.write('\n')
+
+
+class JSONDatabase(dict):
+  """A dict bound to a JSON file."""
+
+  def __init__(self, file_path, allow_create=False):
+    """Initialize and read the JSON file.
+
+    Args:
+      file_path: The path of the JSON file.
+      allow_create: If True, the file will be automatically created if not
+        exists.
+    """
+    super(JSONDatabase, self).__init__()
+    self._file_path = file_path
+    if not allow_create or os.path.exists(file_path):
+      self.Load()
+    else:
+      self.Save()
+
+  def Load(self, file_path=None):
+    """Read a JSON file and replace the content of this object.
+
+    Args:
+      file_path: The path of the JSON file, defaults to argument ``file_path``
+        of initialization.
+    """
+    self.clear()
+    self.update(LoadFile(file_path or self._file_path))
+
+  def Save(self, file_path=None):
+    """Write the content to a JSON file.
+
+    Args:
+      file_path: The path of the JSON file, defaults to argument ``file_path``
+        of initialization.
+    """
+    DumpFile(file_path or self._file_path, self)
