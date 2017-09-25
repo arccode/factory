2e239cf78eedeceaf7cac6ac9bc6818dae37e6cf
Hung-Te Lin <hungte@chromium.org>
2017-09-26 04:06:21 +0800

    sh: Collect device ID on DUTs without `rootdev` command.
    When running on a standard Linux (i.e., not Chrome OS nor Chromium OS),
    usually there is no `rootdev` command. To support those systems, we've
    simulated how `rootdev` finds root device, by looking device number of
    root then find mapped device nodes.
    
    BUG=None
    TEST=manual; run bin/device_id on Ubuntu.
    
    Change-Id: I0c23a21c5145afc330ed6e837033ed418bfc60be
    Reviewed-on: https://chromium-review.googlesource.com/682814
    Commit-Ready: Hung-Te Lin <hungte@chromium.org>
    Tested-by: Hung-Te Lin <hungte@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

--- sh/device_id.sh
+++ sh/device_id.sh
@@ -5,12 +5,40 @@
 
 # A script to generate a "trusted" device identifier.
 
+# Finds root device.
+get_rootdev() {
+  if type rootdev >/dev/null 2>&1; then
+    rootdev -s -d
+    return
+  fi
+
+  local device_number="$(stat -c '%d' /)"
+  local major="$(( device_number / 256 ))"
+  local minor="$(( device_number % 256 ))"
+  local dev_path="/sys/dev/block/${major}:${minor}"
+
+  if [ -d "${dev_path}" ]; then
+    # dev_path may be /dev/dm-0, and we want to find first "real" device.
+    while true; do
+      local slaves=$(ls "${dev_path}/slaves" 2>/dev/null || true)
+      if [ -z "${slaves}" ] || ! [ -d "${dev_path}/slaves/${slaves}" ]; then
+        break
+      fi
+      dev_path="${dev_path}/slaves/${slaves}"
+    done
+    echo /dev/"$(basename "$(readlink -f "${dev_path}")")"
+    return
+  fi
+
+  return 1
+}
+
 # Use disk storage name and serial number to build device id.
 id_storage() {
   local device="$1"
 
   if [ -z "${device}" ]; then
-    device="$(rootdev -s -d)"
+    device="$(get_rootdev)"
   fi
 
   # Normally this is done by /etc/init/udev-trigger.conf.
