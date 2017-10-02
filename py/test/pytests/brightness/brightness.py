618b1d28ecc0a1b44a39f40358f62aa444d46117
Peter Shih <pihsun@chromium.org>
2017-10-02 16:10:01 +0800

    pytests: Change brightness test to use countdown_timer.
    BUG=None
    TEST=make test
    TEST=manually on DUT.
    
    Change-Id: I17f0322fbdf11c420a93f6bb1c25322f1cb52cac
    Reviewed-on: https://chromium-review.googlesource.com/694765
    Commit-Ready: Pi-Hsun Shih <pihsun@chromium.org>
    Tested-by: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

--- py/test/pytests/brightness/brightness.py
+++ py/test/pytests/brightness/brightness.py
@@ -9,25 +9,24 @@ import unittest
 
 import factory_common  # pylint: disable=unused-import
 from cros.factory.device import device_utils
+from cros.factory.test import countdown_timer
 from cros.factory.test.i18n import arg_utils as i18n_arg_utils
 from cros.factory.test.i18n import test_ui as i18n_test_ui
 from cros.factory.test import test_ui
 from cros.factory.test import ui_templates
 from cros.factory.utils.arg_utils import Arg
-from cros.factory.utils import process_utils
 
 
 _MSG_CSS_CLASS = 'brightness-test-info'
 _MSG_PASS_FAIL_PROMPT = i18n_test_ui.MakeI18nLabelWithClass(
     '<br>Press ENTER to pass, or ESC to fail.', _MSG_CSS_CLASS)
-_MSG_TIME_REMAINING = lambda time: i18n_test_ui.MakeI18nLabelWithClass(
-    'Time remaining: {time}', _MSG_CSS_CLASS, time=time)
 
 _ID_PROMPT = 'brightness-test-prompt'
 _ID_COUNTDOWN_TIMER = 'brightness-test-timer'
 
-_HTML_BRIGHTNESS_TEST = '<div id="%s"></div>\n<div id="%s"></div>\n' % (
-    _ID_PROMPT, _ID_COUNTDOWN_TIMER)
+_HTML_BRIGHTNESS_TEST = (
+    '<div id="%s"></div>\n<div id="%s" class="%s"></div>\n' %
+    (_ID_PROMPT, _ID_COUNTDOWN_TIMER, _MSG_CSS_CLASS))
 _BRIGHTNESS_TEST_DEFAULT_CSS = '.brightness-test-info { font-size: 2em; }'
 
 
@@ -54,7 +53,11 @@ class BrightnessTest(unittest.TestCase):
 
   def runTest(self):
     self.ui.RunInBackground(self._BrightnessChangeLoop)
-    process_utils.StartDaemonThread(target=self._CountdownTimer)
+    countdown_timer.StartCountdownTimer(
+        self.args.timeout_secs,
+        lambda: self.ui.Fail('Brightness test failed due to timeout.'),
+        self.ui,
+        _ID_COUNTDOWN_TIMER)
     self.ui.Run()
 
   def tearDown(self):
@@ -69,13 +72,3 @@ class BrightnessTest(unittest.TestCase):
       for level in self.args.levels:
         self._SetBrightnessLevel(level)
         time.sleep(self.args.interval_secs)
-
-  def _CountdownTimer(self):
-    """Starts a countdown timer and fails the test if timer reaches zero."""
-    time_remaining = self.args.timeout_secs
-    while time_remaining > 0:
-      label = _MSG_TIME_REMAINING(time_remaining)
-      self.ui.SetHTML(label, id=_ID_COUNTDOWN_TIMER)
-      time.sleep(1)
-      time_remaining -= 1
-    self.ui.Fail('Brightness test failed due to timeout.')
