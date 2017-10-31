75121f8c572c17a48974e2c0625a5d72ef37bfd1
Peter Shih <pihsun@chromium.org>
2017-10-31 16:25:14 +0800

    pytests: Refactor buzzer test to use TestCaseWithUI.
    BUG=b:68681772
    TEST=make test
    TEST=manually on DUT.
    
    Change-Id: I714760cace59581e51552ad294452489531681f6
    Reviewed-on: https://chromium-review.googlesource.com/746491
    Commit-Ready: Pi-Hsun Shih <pihsun@chromium.org>
    Tested-by: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

--- py/test/pytests/buzzer.py
+++ py/test/pytests/buzzer.py
@@ -6,40 +6,35 @@
 
 import random
 import time
-import unittest
 
 import factory_common  # pylint: disable=unused-import
 from cros.factory.test.i18n import test_ui as i18n_test_ui
 from cros.factory.test import test_ui
-from cros.factory.test import ui_templates
 from cros.factory.utils.arg_utils import Arg
 from cros.factory.utils import process_utils
 
-_MSG_BUZZER_INFO = i18n_test_ui.MakeI18nLabelWithClass(
+_MSG_BUZZER_INFO = i18n_test_ui.MakeI18nLabel(
     'How many beeps do you hear? <br>'
-    'Press space to start.',
-    'buzzer-test-info')
+    'Press space to start.')
 
-_MSG_BUZZER_TEST = i18n_test_ui.MakeI18nLabelWithClass(
+_MSG_BUZZER_TEST = i18n_test_ui.MakeI18nLabel(
     'How many beeps do you hear? <br>'
     'Press the number you hear to pass the test.<br>'
-    "Press 'r' to play again.",
-    'buzzer-test-info')
+    "Press 'r' to play again.")
 
-_HTML_BUZZER = """
-<table style="width: 70%%; margin: auto;">
-  <tr>
-    <td align="center"><div id="buzzer_title"></div></td>
-  </tr>
-</table>
-"""
+_HTML_BUZZER = '<div id="buzzer-title"></div>'
 
 _CSS_BUZZER = """
-  .buzzer-test-info { font-size: 2em; }
+  #buzzer-title {
+    font-size: 2em;
+    width: 70%;
+  }
 """
 
+_MAX_BEEP_TIMES = 5
 
-class BuzzerTest(unittest.TestCase):
+
+class BuzzerTest(test_ui.TestCaseWithUI):
   """Tests buzzer."""
   ARGS = [
       # Common arguments
@@ -51,16 +46,9 @@ class BuzzerTest(unittest.TestCase):
   ]
 
   def setUp(self):
-    self._pass_digit = random.randint(1, 5)
-    self.ui = test_ui.UI()
-    self.template = ui_templates.OneSection(self.ui)
+    self._pass_digit = random.randint(1, _MAX_BEEP_TIMES)
     self.ui.AppendCSS(_CSS_BUZZER)
     self.template.SetState(_HTML_BUZZER)
-    self.ui.BindKey(test_ui.SPACE_KEY, self.StartTest)
-    self.ui.BindKey('R', self.StartTest)
-    for num in xrange(10):
-      self.ui.BindKey(str(num), self.CheckResult, num)
-    self.ui.SetHTML(_MSG_BUZZER_INFO, id='buzzer_title')
     if self.args.init_commands:
       self.InitialBuzzer(self.args.init_commands)
 
@@ -75,18 +63,26 @@ class BuzzerTest(unittest.TestCase):
     if stop_cmd:
       process_utils.Spawn(stop_cmd, check_call=True)
 
-  def StartTest(self, event):
-    del event  # Unused.
-    self.ui.SetHTML(_MSG_BUZZER_TEST, id='buzzer_title')
-    for unused_i in xrange(self._pass_digit):
-      self.BeepOne(self.args.start_command, self.args.stop_command)
-      time.sleep(self.args.mute_duration_secs)
-
-  def CheckResult(self, event):
-    if event.data != self._pass_digit:
-      self.ui.Fail('Wrong number to press.')
-    else:
-      self.ui.Pass()
-
   def runTest(self):
-    self.ui.Run()
+    max_total_duration = _MAX_BEEP_TIMES * (
+        self.args.beep_duration_secs + self.args.mute_duration_secs)
+
+    self.ui.SetHTML(_MSG_BUZZER_INFO, id='buzzer-title')
+    self.ui.WaitKeysOnce(test_ui.SPACE_KEY)
+
+    self.ui.SetHTML(_MSG_BUZZER_TEST, id='buzzer-title')
+
+    while True:
+      start_time = time.time()
+      for unused_i in xrange(self._pass_digit):
+        self.BeepOne(self.args.start_command, self.args.stop_command)
+        time.sleep(self.args.mute_duration_secs)
+      # Try to make the test always run for about same duration, to avoid
+      # cheating by looking at when the buttons appear.
+      time.sleep(max(0, max_total_duration - (time.time() - start_time)))
+
+      all_keys = [str(num + 1) for num in range(_MAX_BEEP_TIMES)] + ['R']
+      key = self.ui.WaitKeysOnce(all_keys)
+      if key != 'R':
+        self.assertEqual(self._pass_digit, int(key), 'Wrong number to press.')
+        return
