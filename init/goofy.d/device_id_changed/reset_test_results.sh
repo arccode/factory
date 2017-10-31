41d7fe9de47097685dc6f20afb78ee4ed9519c17
Peter Shih <pihsun@chromium.org>
2017-10-31 15:00:27 +0800

    goofy: Fix goofy failed to start when device id changed.
    When device id is changed, reset_test_results.sh tries to move tests
    shelf to tests.bak. But since some dbm use more than one file,
    shelve_utils consider all files with prefix 'test.' as part of the
    shelve, and would fail to open the shelve.
    
    Change the backup name to fix this.
    
    BUG=b:68678112
    TEST=make test
    TEST=manually edit /var/factory/.device_id, and check that factory UI
         can be started after reboot.
    
    Change-Id: I70c840f1c3b772911349ede8b21b2d3e10d2452d
    Reviewed-on: https://chromium-review.googlesource.com/746502
    Commit-Ready: Pi-Hsun Shih <pihsun@chromium.org>
    Tested-by: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

--- init/goofy.d/device_id_changed/reset_test_results.sh
+++ init/goofy.d/device_id_changed/reset_test_results.sh
@@ -8,7 +8,9 @@
 # available at the moment.
 echo "Device ID change detected, resetting all test results..."
 TESTS_DATA="/var/factory/state/tests"
-BACKUP_TESTS_DATA="${TESTS_DATA}.bak"
+# We can't call the file tests.bak here, since shelve_utils assume that all
+# files with prefix 'tests.' part of the shelve.
+BACKUP_TESTS_DATA="${TESTS_DATA}_bak"
 if [ -e "${TESTS_DATA}" ]; then
   rm -rf "${BACKUP_TESTS_DATA}"
   mv -f "${TESTS_DATA}" "${BACKUP_TESTS_DATA}"
