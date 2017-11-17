5bd01744de46ab14a6bd5403046218fd08771f55
Peter Shih <pihsun@chromium.org>
2017-11-17 16:35:30 +0800

    pytests: Refactor ethernet to use TestCaseWithUI.
    BUG=b:68681772
    TEST=make test
    TEST=manually on DUT.
    
    Change-Id: I94659ee49d76043377beba83c7c36301991ea4b2
    Reviewed-on: https://chromium-review.googlesource.com/776317
    Commit-Ready: Pi-Hsun Shih <pihsun@chromium.org>
    Tested-by: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

--- py/test/pytests/ethernet.py
+++ py/test/pytests/ethernet.py
@@ -7,29 +7,18 @@
 import logging
 import re
 import time
-import unittest
 
 import factory_common  # pylint: disable=unused-import
 from cros.factory.device import device_utils
-from cros.factory.test import event as test_event
-from cros.factory.test import session
 from cros.factory.test.i18n import test_ui as i18n_test_ui
+from cros.factory.test import session
 from cros.factory.test import test_ui
-from cros.factory.test import ui_templates
 from cros.factory.utils.arg_utils import Arg
 from cros.factory.utils import net_utils
 
 _MSG_ETHERNET_INFO = i18n_test_ui.MakeI18nLabelWithClass(
     'Please plug ethernet cable into built-in ethernet port<br>'
     'Press space to start.', 'ethernet-test-info')
-
-_HTML_ETHERNET = """
-<table style="width: 70%%; margin: auto;">
-  <tr>
-    <td align="center"><div id="ethernet_title"></div></td>
-  </tr>
-</table>
-"""
 
 _CSS_ETHERNET = """
   .ethernet-test-info { font-size: 2em; }
@@ -38,10 +27,10 @@ _CSS_ETHERNET = """
 _LOCAL_FILE_PATH = '/tmp/test'
 
 
-class EthernetTest(unittest.TestCase):
+class EthernetTest(test_ui.TestCaseWithUI):
   """Test built-in ethernet port"""
   ARGS = [
-      Arg('auto_start', bool, 'Auto start option.', False),
+      Arg('auto_start', bool, 'Auto start option.', default=False),
       Arg('test_url', str, 'URL for testing data transmission.',
           default=None),
       Arg('md5sum', str, 'md5sum of the test file in test_url.',
@@ -49,10 +38,8 @@ class EthernetTest(unittest.TestCase):
       Arg('retry_interval_msecs', int,
           'Milliseconds before next retry.',
           default=1000),
-      Arg('iface', str, 'Interface name for testing.',
-          default=None),
-      Arg('interface_name_patterns',
-          list,
+      Arg('iface', str, 'Interface name for testing.', default=None),
+      Arg('interface_name_patterns', list,
           'The ethernet interface name patterns',
           default=net_utils.DEFAULT_ETHERNET_NAME_PATTERNS),
       Arg('link_only', bool, 'Only test if link is up or not', default=False),
@@ -62,25 +49,16 @@ class EthernetTest(unittest.TestCase):
       Arg('swconfig_ports', (int, list), 'swconfig port numbers. Either '
           'a single int or a list of int.', default=None),
       Arg('swconfig_expected_speed', (int, list),
-          ('expected link speed, if a list is given, each integer in the list '
-           'will be paired with each port in swconfig_ports.'),
+          'expected link speed, if a list is given, each integer in the list '
+          'will be paired with each port in swconfig_ports.',
           default=None)
   ]
 
   def setUp(self):
     self.dut = device_utils.CreateDUTInterface()
-    self.ui = test_ui.UI()
-    self.template = ui_templates.OneSection(self.ui)
     self.ui.AppendCSS(_CSS_ETHERNET)
-    self.template.SetState(_HTML_ETHERNET)
+    self.template.SetState(_MSG_ETHERNET_INFO)
 
-    if self.args.auto_start:
-      self.ui.AddEventHandler('StartTest', self.StartTest)
-      self.ui.PostEvent(test_event.Event(test_event.Event.Type.TEST_UI_EVENT,
-                                         subtype='StartTest'))
-    else:
-      self.ui.BindKey(test_ui.SPACE_KEY, self.StartTest)
-    self.ui.SetHTML(_MSG_ETHERNET_INFO, id='ethernet_title')
     if bool(self.args.test_url) != bool(self.args.md5sum):
       raise ValueError('Should both assign test_url and md5sum.')
     if self.args.use_swconfig:
@@ -96,8 +74,9 @@ class EthernetTest(unittest.TestCase):
     interfaces = []
     for pattern in self.args.interface_name_patterns:
       interfaces += [
-          self.dut.path.basename(path) for path in
-          self.dut.Glob('/sys/class/net/' + pattern)]
+          self.dut.path.basename(path)
+          for path in self.dut.Glob('/sys/class/net/' + pattern)
+      ]
     return interfaces
 
   def GetInterface(self):
@@ -110,14 +89,12 @@ class EthernetTest(unittest.TestCase):
   def GetCandidateInterface(self):
     devices = self.GetEthernetInterfaces()
     if not devices:
-      self.fail('No ethernet interface')
-      return None
-    else:
-      for dev in devices:
-        if 'usb' not in self.dut.path.realpath('/sys/class/net/%s' % dev):
-          session.console.info('Built-in ethernet device %s found.', dev)
-          self.dut.CheckCall(['ifconfig', dev, 'up'], log=True)
-          return dev
+      self.FailTask('No ethernet interface')
+    for dev in devices:
+      if 'usb' not in self.dut.path.realpath('/sys/class/net/%s' % dev):
+        session.console.info('Built-in ethernet device %s found.', dev)
+        self.dut.CheckCall(['ifconfig', dev, 'up'], log=True)
+        return dev
     return None
 
   def GetFile(self):
@@ -142,15 +119,12 @@ class EthernetTest(unittest.TestCase):
     return False
 
   def CheckLinkSimple(self, dev):
-    if dev:
-      status = self.dut.ReadSpecialFile(
-          '/sys/class/net/%s/carrier' % dev).strip()
-      if int(status):
-        self.ui.Pass()
-        return True
-      else:
-        self.ui.Fail('Link is down on dev %s' % dev)
-        return False
+    status = self.dut.ReadSpecialFile(
+        '/sys/class/net/%s/carrier' % dev).strip()
+    if int(status):
+      self.PassTask()
+    else:
+      self.FailTask('Link is down on dev %s' % dev)
 
   def GetEthernetIp(self, interface):
     output = self.dut.CallOutput(
@@ -162,7 +136,7 @@ class EthernetTest(unittest.TestCase):
       return None
 
   def CheckLinkSWconfig(self):
-    if type(self.args.swconfig_ports) == int:
+    if isinstance(self.args.swconfig_ports, int):
       self.args.swconfig_ports = [self.args.swconfig_ports]
 
     if not isinstance(self.args.swconfig_expected_speed, list):
@@ -180,55 +154,47 @@ class EthernetTest(unittest.TestCase):
       status = self.dut.CheckOutput(
           ['swconfig', 'dev', self.args.swconfig_switch,
            'port', str(port), 'get', 'link'])
-      if 'up' in status:
-        session.console.info('Link is up on switch %s port %d',
-                             self.args.swconfig_switch, port)
-        if speed:
-          speed_str = '{0}baseT'.format(speed)
-          if speed_str not in status:
-            self.ui.Fail('The negociated speed is not expected (%r not in %r)' %
-                         (speed_str, status))
-      else:
-        self.ui.Fail('Link is down on switch %s port %d' %
-                     (self.args.swconfig_switch, port))
-        return False
 
-    self.ui.Pass()
-    return True
+      if 'up' not in status:
+        self.FailTask('Link is down on switch %s port %d' %
+                      (self.args.swconfig_switch, port))
 
-  def StartTest(self, event):
-    del event  # Unused.
+      session.console.info('Link is up on switch %s port %d',
+                           self.args.swconfig_switch, port)
+      if speed:
+        speed_str = '{0}baseT'.format(speed)
+        if speed_str not in status:
+          self.FailTask('The negotiated speed is not expected (%r not in %r)' %
+                        (speed_str, status))
+
+    self.PassTask()
+
+  def runTest(self):
+    if not self.args.auto_start:
+      self.ui.WaitKeysOnce(test_ui.SPACE_KEY)
+
+    if self.args.use_swconfig:
+      self.CheckLinkSWconfig()
+
     # Only retry 5 times
     for unused_i in xrange(5):
       eth = self.GetInterface()
-      if self.args.link_only:
-        if not self.args.use_swconfig:
-          if self.CheckLinkSimple(eth):
-            break
+      if eth:
+        if self.args.link_only:
+          self.CheckLinkSimple(eth)
+        elif self.args.test_url:
+          if self.GetFile():
+            self.PassTask()
         else:
-          if self.CheckLinkSWconfig():
-            break
-      else:
-        if eth:
-          if self.args.test_url is None:
-            ethernet_ip = self.GetEthernetIp(eth)
-            if ethernet_ip:
-              session.console.info('Get ethernet IP %s for %s',
-                                   ethernet_ip, eth)
-              self.ui.Pass()
-              break
-          else:
-            if self.GetFile():
-              self.ui.Pass()
-              break
-        time.sleep(self.args.retry_interval_msecs / 1000.0)
+          ethernet_ip = self.GetEthernetIp(eth)
+          if ethernet_ip:
+            session.console.info('Get ethernet IP %s for %s', ethernet_ip, eth)
+            self.PassTask()
+      time.sleep(self.args.retry_interval_msecs / 1000.0)
 
     if self.args.link_only:
-      self.ui.Fail('Cannot find interface %s' % self.args.iface)
-    elif self.args.test_url is None:
-      self.ui.Fail('Cannot get ethernet IP')
+      self.FailTask('Cannot find interface %s' % self.args.iface)
+    elif self.args.test_url:
+      self.FailTask('Failed to download url %s' % self.args.test_url)
     else:
-      self.ui.Fail('Failed to download url %s' % self.args.test_url)
-
-  def runTest(self):
-    self.ui.Run()
+      self.FailTask('Cannot get ethernet IP')
