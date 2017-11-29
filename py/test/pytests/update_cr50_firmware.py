c40d8fe7a3eba2080efa265aae33cf18f4d9d6a3
Peter Shih <pihsun@chromium.org>
2017-11-29 15:34:28 +0800

    pytests: Refactor update_cr50_firmware to use TestCaseWithUI.
    BUG=b:68681772
    TEST=make test
    TEST=manually on DUT.
    
    Change-Id: Id7c884b6482c4929e688372e6c4d3c96393b4992
    Reviewed-on: https://chromium-review.googlesource.com/795480
    Commit-Ready: Pi-Hsun Shih <pihsun@chromium.org>
    Tested-by: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

--- py/test/pytests/update_cr50_firmware.py
+++ py/test/pytests/update_cr50_firmware.py
@@ -51,25 +51,20 @@ To update Cr50 firmware with the Cr50 firmware image in station::
   }
 """
 
-import logging
 import os
-import subprocess
-import unittest
 
 import factory_common  # pylint: disable=unused-import
 from cros.factory.device import device_utils
 from cros.factory.test import test_ui
-from cros.factory.test import ui_templates
 from cros.factory.utils.arg_utils import Arg
 from cros.factory.utils import sys_utils
 
-_CSS = 'test-template { text-align: left; }'
 
 TRUNKS_SEND = '/usr/sbin/trunks_send'
 FIRMWARE_RELATIVE_PATH = 'opt/google/cr50/firmware/cr50.bin.prod'
 
 
-class UpdateCr50FirmwareTest(unittest.TestCase):
+class UpdateCr50FirmwareTest(test_ui.TestCaseWithUI):
   ARGS = [
       Arg('firmware_file', str, 'The full path of the firmware.',
           default=None),
@@ -79,12 +74,12 @@ class UpdateCr50FirmwareTest(unittest.TestCase):
           default=False),
   ]
 
+  ui_class = test_ui.ScrollableLogUI
+
   def setUp(self):
     self.dut = device_utils.CreateDUTInterface()
-    self._ui = test_ui.UI(css=_CSS)
-    self._template = ui_templates.OneScrollableSection(self._ui)
 
-  def UpdateCr50Firmware(self):
+  def runTest(self):
     """Update Cr50 firmware."""
     self.assertEqual(
         1, len(filter(None, [self.args.firmware_file, self.args.from_release])),
@@ -107,18 +102,7 @@ class UpdateCr50FirmwareTest(unittest.TestCase):
       cmd = [TRUNKS_SEND, '--force', '--update', firmware_file]
     else:
       cmd = [TRUNKS_SEND, '--update', firmware_file]
-    p = self.dut.Popen(
-        cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, log=True)
 
-    for line in iter(p.stdout.readline, ''):
-      logging.info(line.strip())
-      self._template.SetState(test_ui.Escape(line), append=True)
-
-    if p.poll() != 0:
-      self._ui.Fail('Cr50 firmware update failed: %d.' % p.returncode)
-    else:
-      self._ui.Pass()
-
-  def runTest(self):
-    self._ui.RunInBackground(self.UpdateCr50Firmware)
-    self._ui.Run()
+    returncode = self.ui.PipeProcessOutputToUI(cmd)
+    self.assertEqual(0, returncode,
+                     'Cr50 firmware update failed: %d.' % returncode)
