163ae115311f08a0bb91c086ed6a063ac06033e6
Peter Shih <pihsun@chromium.org>
2017-11-30 11:21:38 +0800

    pytests: Refactor tpm_diagnosis to use TestCaseWithUI.
    BUG=b:68681772
    TEST=make test
    TEST=manually on DUT.
    
    Change-Id: I2d8bd4112a5b45a34d26599184f9f8084613bae4
    Reviewed-on: https://chromium-review.googlesource.com/798812
    Commit-Ready: Pi-Hsun Shih <pihsun@chromium.org>
    Tested-by: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

--- py/test/pytests/tpm_diagnosis.py
+++ py/test/pytests/tpm_diagnosis.py
@@ -4,21 +4,15 @@
 
 """Runs tpm_selftest to perform TPM self-diagnosis."""
 
-import logging
 import os
-import subprocess
-import unittest
+import threading
 
 import factory_common  # pylint: disable=unused-import
 from cros.factory.test import test_ui
-from cros.factory.test import ui_templates
 from cros.factory.utils.arg_utils import Arg
-from cros.factory.utils import process_utils
-
-_CSS = 'test-template { text-align: left; }'
 
 
-class TpmDiagnosisTest(unittest.TestCase):
+class TpmDiagnosisTest(test_ui.TestCaseWithUI):
   ARGS = [
       Arg('tpm_selftest', str, 'Path of tpm_selftest program.',
           default='/usr/local/sbin/tpm_selftest'),
@@ -28,34 +22,27 @@ class TpmDiagnosisTest(unittest.TestCase):
           default='tpm_selftest succeeded')
   ]
 
+  ui_class = test_ui.ScrollableLogUI
+
   def setUp(self):
     self.assertTrue(os.path.isfile(self.args.tpm_selftest),
                     msg='%s is missing.' % self.args.tpm_selftest)
-    self._ui = test_ui.UI(css=_CSS)
-    self._template = ui_templates.OneScrollableSection(self._ui)
 
-  def DiagnoseTpm(self):
+  def runTest(self):
     """Runs tpm_selftest.
 
     It shows diagnosis result on factory UI.
     """
-    p = process_utils.Spawn([self.args.tpm_selftest] + self.args.tpm_args,
-                            stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
-                            log=True)
-    success = False
-    for line in iter(p.stdout.readline, ''):
-      logging.info(line.strip())
-      self._template.SetState(test_ui.Escape(line), append=True)
-      if line.find(self.args.success_pattern) != -1:
-        success = True
+    success = threading.Event()
+    def _Callback(line):
+      if self.args.success_pattern in line:
+        success.set()
 
-    p.poll()
+    returncode = self.ui.PipeProcessOutputToUI(
+        [self.args.tpm_selftest] + self.args.tpm_args, callback=_Callback)
+
     self.assertTrue(
-        success,
+        success.is_set(),
         'TPM self-diagnose failed: Cannot find a success pattern: "%s". '
         'tpm_selftest returncode: %d.' % (self.args.success_pattern,
-                                          p.returncode))
-
-  def runTest(self):
-    self._ui.RunInBackground(self.DiagnoseTpm)
-    self._ui.Run()
+                                          returncode))
