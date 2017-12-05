c0bd2a296fda4786e0c9f6f0ad358d47baadf54e
Peter Shih <pihsun@chromium.org>
2017-12-05 15:31:46 +0800

    pytests: Refactor backlight to use TestCaseWithUI.
    BUG=b:68681772
    TEST=make test
    TEST=manually on DUT.
    
    Change-Id: Iaa6651fe2145be4878eff843ec35fbdbbec1b7a4
    Reviewed-on: https://chromium-review.googlesource.com/807965
    Commit-Ready: Pi-Hsun Shih <pihsun@chromium.org>
    Tested-by: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

--- py/test/pytests/backlight.py
+++ py/test/pytests/backlight.py
@@ -1,39 +1,30 @@
-# -*- coding: utf-8 -*-
-#
 # Copyright 2013 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+
 """A factory test to test the function of backlight of display panel."""
 
 import logging
 import random
-import unittest
 
 import factory_common  # pylint: disable=unused-import
 from cros.factory.device import device_utils
+from cros.factory.test.i18n import test_ui as i18n_test_ui
 from cros.factory.test import session
 from cros.factory.test import test_ui
-from cros.factory.test import ui_templates
 from cros.factory.utils.arg_utils import Arg
 
 _DEFAULT_ADJUST_LEVEL = 0.05
 _DEFAULT_RESET_LEVEL = 0.5
 
 
-class BacklightTest(unittest.TestCase):
+class BacklightTest(test_ui.TestCaseWithUI):
   """Tests the function of backlight of display panel.
 
   There are two subtests in this test. In each subtest, the test will make
   the screen bighter or darker when space is being pressed.
   During each subtest, pressing esc can reset the brightness.
   Operator needs to answer H for brighter screen and L for darker screen.
-
-  Attributes:
-    self.ui: test ui.
-    self.template: ui template handling html layout.
-    self.sequence: The testing sequence of 'high' and 'low'.
-    self.index: The index of current testing.
-    self.current_level: The current brightness level.
   """
   ARGS = [
       Arg('adjust_level', float,
@@ -44,64 +35,56 @@ class BacklightTest(unittest.TestCase):
           default=_DEFAULT_RESET_LEVEL),
   ]
 
-  def AdjustBrightness(self, level):
-    """Adjust the intensity."""
-    if level > 1:
-      level = 1
-    if level < 0:
-      level = 0
-    logging.info('Adjust brightness level to %r', level)
-    self.dut.display.SetBacklightBrightness(level)
-    self.current_level = level
-
   def setUp(self):
     """Initializes frontend presentation and properties."""
     self.dut = device_utils.CreateDUTInterface()
-    self.ui = test_ui.UI()
-    self.ui.AppendCSSLink('backlight.css')
-    self.template = ui_templates.OneSection(self.ui)
-    # Randomly sets the testing sequence.
-    self.sequence = ['low', 'high'] if random.randint(0, 1) else ['high', 'low']
+
+    self.sequence = [+1, -1]
+    random.shuffle(self.sequence)
+
     logging.info('testing sequence: %r', self.sequence)
-    self.index = 0
     self.current_level = 0
     self.ResetBrightness()
-    self.ui.CallJSFunction('setupBacklightTest')
+
+    self.ui.AppendCSS('test-template { font-size: 2em; }')
+    self.ui.SetState(
+        i18n_test_ui.MakeI18nLabel(
+            'Press Space to change backlight brightness;<br>'
+            'Press Esc to reset backlight brightness to original;<br>'
+            'After checking, Enter H if pressing Space changes the '
+            'backlight to be brighter;<br>'
+            'Enter L if pressing Space changes the backlight to be '
+            'dimmer.<br>'
+            'This test will be executed twice.'))
 
   def tearDown(self):
     self.ResetBrightness()
 
   def runTest(self):
-    """Sets the callback function of keys and run the test."""
-    self.ui.BindKey(test_ui.ESCAPE_KEY, lambda _: self.ResetBrightness())
-    self.ui.BindKey(test_ui.SPACE_KEY, lambda _: self.OnAdjustBrightness())
-    self.ui.BindKey('H', lambda _: self.OnAnswerPressed('high'))
-    self.ui.BindKey('L', lambda _: self.OnAnswerPressed('low'))
-    self.ui.Run()
-
-  def OnAnswerPressed(self, answer):
-    """The call back function for user input.
-
-    Args:
-      answer: 'high' or 'low'. The test will fail if the answer is incorrect.
-    """
-    logging.info('Pressed %r', answer)
-    if answer == self.sequence[self.index]:
-      session.console.info('Passed for %r', answer)
-      self.index = self.index + 1
+    for direction in self.sequence:
       self.ResetBrightness()
-      if self.index == len(self.sequence):
-        self.ui.Pass()
-    else:
-      self.ui.Fail('Wrong answer: %r' % answer)
+      while True:
+        key = self.ui.WaitKeysOnce(
+            [test_ui.ESCAPE_KEY, test_ui.SPACE_KEY, 'H', 'L'])
+        if key == test_ui.ESCAPE_KEY:
+          self.ResetBrightness()
+        elif key == test_ui.SPACE_KEY:
+          self.AdjustBrightness(self.current_level +
+                                direction * self.args.adjust_level)
+        else:
+          correct_key = 'H' if direction == +1 else 'L'
+          if key == correct_key:
+            session.console.info('Passed for %r', key)
+            break
+          else:
+            self.FailTask('Wrong answer: %r' % key)
 
-  def OnAdjustBrightness(self):
-    """Adjusts the brightness value by self.args.adjust_level."""
-    if self.sequence[self.index] == 'high':
-      target_level = self.current_level + self.args.adjust_level
-    elif self.sequence[self.index] == 'low':
-      target_level = self.current_level - self.args.adjust_level
-    self.AdjustBrightness(target_level)
+  def AdjustBrightness(self, level):
+    """Adjust the intensity."""
+    level = max(0, min(1, level))
+    logging.info('Adjust brightness level to %r', level)
+    self.dut.display.SetBacklightBrightness(level)
+    self.current_level = level
 
   def ResetBrightness(self):
     """Resets brightness back to normal value."""
