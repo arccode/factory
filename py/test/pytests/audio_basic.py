2d73fb0090d51fcf26483e347ff8439d3a5d420b
Peter Shih <pihsun@chromium.org>
2017-12-05 14:10:32 +0800

    pytests: Refactor audio_basic to use TestCaseWithUI.
    Also make better docs for audio_basic.
    
    BUG=b:68681772,chromium:737920
    TEST=make test
    
    Change-Id: I18d6ab6c16308e0d254d5bce569c92d3ade625ce
    Reviewed-on: https://chromium-review.googlesource.com/807841
    Commit-Ready: Pi-Hsun Shih <pihsun@chromium.org>
    Tested-by: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

--- py/test/pytests/audio_basic.py
+++ py/test/pytests/audio_basic.py
@@ -3,18 +3,45 @@
 # found in the LICENSE file.
 
 
-# DESCRIPTION :
-#
-# This is a factory test to test the audio.  Operator will test both record and
-# playback for headset and built-in audio.  Recordings are played back for
-# confirmation.  An additional pre-recorded sample is played to confirm speakers
-# operate independently.
-# We will test each channel of input and output.
+"""Test basic audio record and playback.
+
+Description
+-----------
+This test check both record and playback for headset and built-in audio by
+recording audio, play it back and ask operator for confirmation. An additional
+pre-recorded sample is played to confirm speakers operate independently. Each
+channel of input and output would be tested.
+
+Test Procedure
+--------------
+1. Instruction is shown on screen to ask operator to press either 'P' or 'R'.
+2. After operator press 'P', a pre-recorded sample is played back.
+3. After operator press 'R', 3 seconds of audio would be recorded and played
+   back.
+4. Operator press space key to confirm both 2. and 3. step completes without
+   problem, and the audio played back sounds correct.
+
+Dependency
+----------
+- External program `sox <http://sox.sourceforge.net/>`_.
+- Device API ``cros.factory.device.audio``.
+
+Examples
+--------
+To check that audio can be recorded and played, add this into test list::
+
+  {
+    "pytest_name": "audio_basic",
+    "args": {
+      "input_dev": ["device", "1"],
+      "output_dev": ["device", "0"]
+    }
+  }
+"""
 
 
 import logging
 import os
-import unittest
 
 import factory_common  # pylint: disable=unused-import
 from cros.factory.device import device_utils
@@ -22,10 +49,10 @@ from cros.factory.test.i18n import _
 from cros.factory.test.i18n import arg_utils as i18n_arg_utils
 from cros.factory.test.i18n import test_ui as i18n_test_ui
 from cros.factory.test import test_ui
-from cros.factory.test import ui_templates
 from cros.factory.utils.arg_utils import Arg
 from cros.factory.utils import file_utils
 from cros.factory.utils import process_utils
+
 
 _RECORD_SEC = 3
 _RECORD_RATE = 48000
@@ -34,55 +61,7 @@ _SOUND_DIRECTORY = os.path.join(
     'static', 'sounds')
 
 
-_MSG_AUDIO_INFO = i18n_test_ui.MakeI18nLabelWithClass(
-    "Press 'P' to first play a sample for each channel to ensure audio "
-    'output works.<br>'
-    "Press 'R' to record {record_sec} seconds, Playback will follow<br>"
-    'Press space to mark pass',
-    'audio-test-info',
-    record_sec=_RECORD_SEC)
-_MSG_RECORD_INFO = i18n_test_ui.MakeI18nLabelWithClass('Start recording',
-                                                       'audio-test-info')
-_HTML_AUDIO = """
-<table style="width: 70%%; margin: auto;">
-  <tr>
-    <td align="center"><div id="audio_title"></div></td>
-  </tr>
-  <tr>
-    <td><hr></td>
-  </tr>
-  <tr>
-    <td><div id="audio_info"></div></td>
-  </tr>
-  <tr>
-    <td><hr></td>
-  </tr>
-</table>
-"""
-
-_CSS_AUDIO = """
-  .audio-test-title { font-size: 2em; }
-  .audio-test-info { font-size: 2em; }
-"""
-
-PLAY_SAMPLE_VALUE = (1 << 0)
-RECORD_VALUE = (1 << 1)
-PASS_VALUE = (PLAY_SAMPLE_VALUE | RECORD_VALUE)
-
-
-def GetPlaybackRecordLabel(channel):
-  return i18n_test_ui.MakeI18nLabelWithClass(
-      'Playback sound (Mic channel {channel})',
-      'audio-test-info',
-      channel=channel)
-
-
-def GetPlaybackLabel(channel):
-  return i18n_test_ui.MakeI18nLabelWithClass(
-      'Playback sound to channel {channel}', 'audio-test-info', channel=channel)
-
-
-class AudioBasicTest(unittest.TestCase):
+class AudioBasicTest(test_ui.TestCaseWithUI):
   ARGS = [
       i18n_arg_utils.I18nArg(
           'audio_title', 'Label Title of audio test', default=_('Headset')),
@@ -103,7 +82,10 @@ class AudioBasicTest(unittest.TestCase):
     if self.args.audio_conf:
       self._dut.audio.LoadConfig(self.args.audio_conf)
 
-    # Tansfer input and output device format
+    self.assertEqual(2, len(self.args.input_dev))
+    self.assertEqual(2, len(self.args.output_dev))
+
+    # Transform input and output device format
     self._in_card = self._dut.audio.GetCardIndexByName(self.args.input_dev[0])
     self._in_device = self.args.input_dev[1]
     self._out_card = self._dut.audio.GetCardIndexByName(self.args.output_dev[0])
@@ -114,91 +96,87 @@ class AudioBasicTest(unittest.TestCase):
       card = self._dut.audio.GetCardIndexByName(card)
       self._dut.audio.ApplyAudioConfig(action, card)
 
-    # Initialize frontend presentation
-    self.ui = test_ui.UI()
-    self.template = ui_templates.OneSection(self.ui)
-    self.ui.AppendCSS(_CSS_AUDIO)
-    self.template.SetState(_HTML_AUDIO)
-    self.ui.BindKey('R', self.HandleRecordEvent)
-    self.ui.BindKey('P', self.HandleSampleEvent)
-    self.ui.BindKey(test_ui.SPACE_KEY, self.MarkPass)
+    self.ui.AppendCSS('test-template { font-size: 2em; }')
+    self.ui.SetInstruction(i18n_test_ui.MakeI18nLabel(self.args.audio_title))
 
-    msg_audio_title = i18n_test_ui.MakeI18nLabelWithClass(
-        self.args.audio_title, 'audio-test-info')
-    self.ui.SetHTML(msg_audio_title, id='audio_title')
-    self.ui.SetHTML(_MSG_AUDIO_INFO, id='audio_info')
-    self.current_process = None
-    self.key_press = None
-    # prevent operator from pressing space directly,
-    # make sure they press P and R.
-    self.event_value = 0
+    self.done_tests = set()
 
-  def HandleRecordEvent(self, event):
-    del event  # Unused.
-    if not self.key_press:
-      self.key_press = 'R'
-      logging.info('start record')
-      self.ui.SetHTML(_MSG_RECORD_INFO, id='audio_info')
-      dut_record_file_path = self._dut.temp.mktemp(False)
-      self._dut.audio.RecordWavFile(dut_record_file_path, self._in_card,
-                                    self._in_device, _RECORD_SEC,
-                                    self.args.input_channels, _RECORD_RATE)
-      logging.info('stop record and start playback')
-      # playback the record file by each channel.
-      with file_utils.UnopenedTemporaryFile(suffix='.wav') as full_wav_path:
-        self._dut.link.Pull(dut_record_file_path, full_wav_path)
-        for i in xrange(self.args.input_channels):
-          with file_utils.UnopenedTemporaryFile(suffix='.wav') as wav_path:
-            # Get channel i from full_wav_path to a stereo wav_path
-            # Since most devices support 2 channels.
-            process_utils.Spawn(
-                ['sox', full_wav_path, wav_path, 'remix', str(i + 1),
-                 str(i + 1)], log=True, check_call=True)
-            with self._dut.temp.TempFile() as dut_path:
-              self._dut.link.Push(wav_path, dut_path)
-              self.ui.SetHTML(GetPlaybackRecordLabel(i + 1), id='audio_info')
-              self._dut.audio.PlaybackWavFile(dut_path, self._out_card,
-                                              self._out_device)
-      self._dut.CheckCall(['rm', '-f', dut_record_file_path])
-      self.ui.SetHTML(_MSG_AUDIO_INFO, id='audio_info')
-      self.key_press = None
-      self.event_value |= RECORD_VALUE
+  def TestRecord(self):
+    logging.info('start record')
+    self.ui.SetState(i18n_test_ui.MakeI18nLabel('Start recording'))
 
-  def HandleSampleEvent(self, event):
-    del event  # Unused.
-    if not self.key_press:
-      self.key_press = 'P'
-      logging.info('start play sample')
-      locale = self.ui.GetUILocale()
-      for i in xrange(self.args.output_channels):
-        ogg_path = os.path.join(_SOUND_DIRECTORY, locale, '%d.ogg' % i + 1)
-        number_wav_path = '%s.wav' % ogg_path
-        process_utils.Spawn(
-            ['sox', ogg_path, '-c1', number_wav_path], check_call=True)
+    dut_record_file_path = self._dut.temp.mktemp(False)
+    self._dut.audio.RecordWavFile(dut_record_file_path, self._in_card,
+                                  self._in_device, _RECORD_SEC,
+                                  self.args.input_channels, _RECORD_RATE)
+
+    logging.info('stop record and start playback')
+    # playback the record file by each channel.
+    with file_utils.UnopenedTemporaryFile(suffix='.wav') as full_wav_path:
+      self._dut.link.Pull(dut_record_file_path, full_wav_path)
+      for channel_idx in xrange(1, self.args.input_channels + 1):
         with file_utils.UnopenedTemporaryFile(suffix='.wav') as wav_path:
-          # we will only keep (i + 1) channel and mute others.
-          # We use number sound to indicate which channel to be played.
-          # Create .wav file with n channels but ony has one channel data.
-          remix_option = ['0'] * self.args.output_channels
-          remix_option[i] = '1'
+          # Get channel channel_idx from full_wav_path to a stereo wav_path
+          # Since most devices support 2 channels.
           process_utils.Spawn(
-              ['sox', number_wav_path, wav_path, 'remix'] + remix_option,
-              log=True, check_call=True)
+              ['sox', full_wav_path, wav_path, 'remix', str(channel_idx),
+               str(channel_idx)], log=True, check_call=True)
           with self._dut.temp.TempFile() as dut_path:
             self._dut.link.Push(wav_path, dut_path)
-            self.ui.SetHTML(GetPlaybackLabel(i + 1), id='audio_info')
+            self.ui.SetState(
+                i18n_test_ui.MakeI18nLabel(
+                    'Playback sound (Mic channel {channel})',
+                    channel=channel_idx))
             self._dut.audio.PlaybackWavFile(dut_path, self._out_card,
                                             self._out_device)
-        os.unlink(number_wav_path)
-      logging.info('stop play sample')
-      self.ui.SetHTML(_MSG_AUDIO_INFO, id='audio_info')
-      self.key_press = None
-      self.event_value |= PLAY_SAMPLE_VALUE
+    self._dut.CheckCall(['rm', '-f', dut_record_file_path])
 
-  def MarkPass(self, event):
-    del event  # Unused.
-    if self.event_value == PASS_VALUE:
-      self.ui.Pass()
+  def TestPlay(self):
+    logging.info('start play sample')
+    locale = self.ui.GetUILocale()
+    for channel_idx in xrange(1, self.args.output_channels + 1):
+      ogg_path = os.path.join(_SOUND_DIRECTORY, locale, '%d.ogg' % channel_idx)
+      number_wav_path = '%s.wav' % ogg_path
+      process_utils.Spawn(
+          ['sox', ogg_path, '-c1', number_wav_path], check_call=True)
+      with file_utils.UnopenedTemporaryFile(suffix='.wav') as wav_path:
+        # we will only keep channel_idx channel and mute others.
+        # We use number sound to indicate which channel to be played.
+        # Create .wav file with n channels but only has one channel data.
+        remix_option = ['0'] * self.args.output_channels
+        remix_option[channel_idx - 1] = '1'
+        process_utils.Spawn(
+            ['sox', number_wav_path, wav_path, 'remix'] + remix_option,
+            log=True, check_call=True)
+        with self._dut.temp.TempFile() as dut_path:
+          self._dut.link.Push(wav_path, dut_path)
+          self.ui.SetState(
+              i18n_test_ui.MakeI18nLabel(
+                  'Playback sound to channel {channel}', channel=channel_idx))
+          self._dut.audio.PlaybackWavFile(dut_path, self._out_card,
+                                          self._out_device)
+      os.unlink(number_wav_path)
+    logging.info('stop play sample')
 
   def runTest(self):
-    self.ui.Run()
+    while True:
+      self.ui.SetState(
+          i18n_test_ui.MakeI18nLabel(
+              "Press 'P' to first play a sample for each channel to ensure "
+              'audio output works.<br>'
+              "Press 'R' to record {record_sec} seconds, Playback will "
+              'follow.<br>'
+              'Press space to mark pass.',
+              record_sec=_RECORD_SEC))
+      key_pressed = self.ui.WaitKeysOnce(['P', 'R', test_ui.SPACE_KEY])
+      if key_pressed == 'P':
+        self.TestPlay()
+        self.done_tests.add('P')
+      elif key_pressed == 'R':
+        self.TestRecord()
+        self.done_tests.add('R')
+      else:
+        # prevent operator from pressing space directly,
+        # make sure they press P and R.
+        if self.done_tests == set(['P', 'R']):
+          break
