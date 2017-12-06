1cc7a1e70dc174926de7bff7138f5e866bd34d41
Peter Shih <pihsun@chromium.org>
2017-12-06 17:26:24 +0800

    pytests: Refactor battery_current to use TestCaseWithUI.
    BUG=b:68681772
    TEST=make test
    TEST=manually on DUT.
    
    Change-Id: I6a2c3f68c07d2f80f52b7567c4c5c97f7c59ccbb
    Reviewed-on: https://chromium-review.googlesource.com/810350
    Commit-Ready: Pi-Hsun Shih <pihsun@chromium.org>
    Tested-by: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

--- py/test/pytests/battery_current.py
+++ py/test/pytests/battery_current.py
@@ -66,7 +66,6 @@ in test list::
 
 import logging
 import time
-import unittest
 
 import factory_common  # pylint: disable=unused-import
 from cros.factory.device import device_utils
@@ -74,29 +73,22 @@ from cros.factory.test.i18n import _
 from cros.factory.test.i18n import arg_utils as i18n_arg_utils
 from cros.factory.test.i18n import test_ui as i18n_test_ui
 from cros.factory.test import test_ui
-from cros.factory.test import ui_templates
 from cros.factory.utils.arg_utils import Arg
 from cros.factory.utils import sync_utils
 
 
-def _PROMPT_TEXT(charge, current, target):
+def _GetPromptText(current, target):
   return i18n_test_ui.MakeI18nLabel(
       'Waiting for {target_status} current to meet {target_current} mA.'
       ' (Currently {status} at {current} mA)',
-      target_status=_('charging') if charge else _('discharging'),
-      target_current=target,
+      target_status=_('charging') if target >= 0 else _('discharging'),
+      target_current=abs(target),
       status=_('charging') if current >= 0 else _('discharging'),
       current=abs(current))
 
-_CHARGE_TEXT = lambda c, t: _PROMPT_TEXT(True, c, t)
-_DISCHARGE_TEXT = lambda c, t: _PROMPT_TEXT(False, c, t)
-_USBPDPORT_PROMPT = (lambda prompt, v: i18n_test_ui.MakeI18nLabel(
-    'Insert power to {prompt}({voltage}mV)', prompt=prompt, voltage=v))
 
-
-class BatteryCurrentTest(unittest.TestCase):
-  """A factory test to test battery charging/discharging current.
-  """
+class BatteryCurrentTest(test_ui.TestCaseWithUI):
+  """A factory test to test battery charging/discharging current."""
   ARGS = [
       Arg('min_charging_current', int,
           'minimum allowed charging current', default=None),
@@ -116,12 +108,8 @@ class BatteryCurrentTest(unittest.TestCase):
   ]
 
   def setUp(self):
-    """Sets the test ui, template and the thread that runs ui. Initializes
-    _power."""
     self._dut = device_utils.CreateDUTInterface()
     self._power = self._dut.power
-    self._ui = test_ui.UI()
-    self._template = ui_templates.OneSection(self._ui)
     if self.args.usbpd_info:
       self._CheckUSBPDInfoArg(self.args.usbpd_info)
       self._usbpd_port = self.args.usbpd_info[0]
@@ -152,16 +140,19 @@ class BatteryCurrentTest(unittest.TestCase):
   def _CheckUSBPD(self):
     for unused_i in range(10):
       status = self._dut.usb_c.GetPDPowerStatus()
-      self._template.SetState(_USBPDPORT_PROMPT(self._usbpd_prompt, 0))
       if 'millivolt' not in status[self._usbpd_port]:
         logging.info('No millivolt detected in port %d', self._usbpd_port)
         return False
       millivolt = status[self._usbpd_port]['millivolt']
       logging.info('millivolt %d, acceptable range (%d, %d)', millivolt,
                    self._usbpd_min_millivolt, self._usbpd_max_millivolt)
-      self._template.SetState(_USBPDPORT_PROMPT(self._usbpd_prompt, millivolt))
-      if not (self._usbpd_min_millivolt <= millivolt and
-              millivolt <= self._usbpd_max_millivolt):
+      self.ui.SetState(
+          i18n_test_ui.MakeI18nLabel(
+              'Insert power to {prompt}({voltage}mV)',
+              prompt=self._usbpd_prompt,
+              voltage=millivolt))
+      if not (self._usbpd_min_millivolt <= millivolt <=
+              self._usbpd_max_millivolt):
         return False
       time.sleep(0.1)
     return True
@@ -170,21 +161,17 @@ class BatteryCurrentTest(unittest.TestCase):
     current = self._power.GetBatteryCurrent()
     target = self.args.min_charging_current
     self._LogCurrent(current)
-    self._template.SetState(_CHARGE_TEXT(current, target))
+    self.ui.SetState(_GetPromptText(current, target))
     return current >= target
 
   def _CheckDischarge(self):
     current = self._power.GetBatteryCurrent()
     target = self.args.min_discharging_current
     self._LogCurrent(current)
-    self._template.SetState(_DISCHARGE_TEXT(current, target))
+    self.ui.SetState(_GetPromptText(current, -target))
     return -current >= target
 
   def runTest(self):
-    self._ui.RunInBackground(self._runTest)
-    self._ui.Run()
-
-  def _runTest(self):
     """Main entrance of charger test."""
     self.assertTrue(self._power.CheckBatteryPresent())
     if self.args.max_battery_level:
