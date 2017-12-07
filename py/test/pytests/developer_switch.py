86008e780aaa784e5e5d87a168c75c579d4c3417
Peter Shih <pihsun@chromium.org>
2017-12-07 16:07:35 +0800

    pytests: Refactor developer_switch to use TestCaseWithUI.
    BUG=b:68681772
    TEST=make test
    TEST=manually on DUT.
    
    Change-Id: Ibbb1eebb0f74c07fba229ac57bd76d8f41f47f00
    Reviewed-on: https://chromium-review.googlesource.com/813447
    Commit-Ready: Pi-Hsun Shih <pihsun@chromium.org>
    Tested-by: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

--- py/test/pytests/developer_switch.py
+++ py/test/pytests/developer_switch.py
@@ -4,27 +4,12 @@
 
 """Verifies physical developer switch can be toggled and end in right state."""
 
-import unittest
-
 import factory_common  # pylint: disable=unused-import
 from cros.factory.test.i18n import test_ui as i18n_test_ui
 from cros.factory.test import test_ui
-from cros.factory.test import ui_templates
 from cros.factory.utils.arg_utils import Arg
 from cros.factory.utils import process_utils
 from cros.factory.utils import sync_utils
-
-_MSG_TURN_ON = i18n_test_ui.MakeI18nLabel(
-    'Please turn <b class="on">ON</b> Developer Switch.')
-
-_MSG_TURN_OFF = i18n_test_ui.MakeI18nLabel(
-    'Please turn <b class="off">OFF</b> Developer Switch.')
-
-_MSG_SW_ON = i18n_test_ui.MakeI18nLabel(
-    'Develop switch is currently <b class="on">ON</b>.')
-
-_MSG_SW_OFF = i18n_test_ui.MakeI18nLabel(
-    'Develop switch is currently <b class="off">OFF</b>.')
 
 _TEST_PAGE_CSS = """
 .on {
@@ -36,7 +21,7 @@ _TEST_PAGE_CSS = """
 """
 
 
-class DeveloperSwitchTest(unittest.TestCase):
+class DeveloperSwitchTest(test_ui.TestCaseWithUI):
   ARGS = [
       Arg('end_state', bool,
           'The state when leaving test. True as enabled and False as disabled.',
@@ -46,37 +31,40 @@ class DeveloperSwitchTest(unittest.TestCase):
   ]
 
   def setUp(self):
-    self._ui = test_ui.UI()
-    self._template = ui_templates.TwoSections(self._ui)
-    self._ui.AppendCSS(_TEST_PAGE_CSS)
+    self.ui.AppendCSS(_TEST_PAGE_CSS)
 
   def runTest(self):
-    self._ui.RunInBackground(self._runTest)
-    self._ui.Run()
-
-  def _runTest(self):
-
-    def GetState():
-      # This will be called so many times so we don't want to be logged.
-      return int(process_utils.SpawnOutput(
-          ['crossystem', 'devsw_cur'], log=False, check_output=True))
-
-    def UpdateUI(state):
-      if state:
-        self._template.SetState(_MSG_TURN_OFF)
-        self._template.SetInstruction(_MSG_SW_ON)
-      else:
-        self._template.SetState(_MSG_TURN_ON)
-        self._template.SetInstruction(_MSG_SW_OFF)
-
-    initial_state = GetState()
+    initial_state = self.GetState()
     timeout_secs = self.args.timeout_secs
-    UpdateUI(initial_state)
+    self.UpdateUI(initial_state)
 
-    sync_utils.WaitFor(lambda: GetState() != initial_state, timeout_secs)
-    UpdateUI(not initial_state)
-    sync_utils.WaitFor(lambda: GetState() == initial_state, timeout_secs)
-    UpdateUI(initial_state)
+    sync_utils.WaitFor(lambda: self.GetState() != initial_state, timeout_secs)
+    self.UpdateUI(not initial_state)
+
+    sync_utils.WaitFor(lambda: self.GetState() == initial_state, timeout_secs)
+    self.UpdateUI(initial_state)
+
     # Flip again if state is not end_state
-    sync_utils.WaitFor(lambda: GetState() != int(self.args.end_state),
+    sync_utils.WaitFor(lambda: self.GetState() != int(self.args.end_state),
                        timeout_secs)
+
+  def GetState(self):
+    # This will be called so many times so we don't want to be logged.
+    return int(process_utils.SpawnOutput(
+        ['crossystem', 'devsw_cur'], log=False, check_output=True))
+
+  def UpdateUI(self, state):
+    if state:
+      self.ui.SetState(
+          i18n_test_ui.MakeI18nLabel(
+              'Please turn <b class="off">OFF</b> Developer Switch.'))
+      self.ui.SetInstruction(
+          i18n_test_ui.MakeI18nLabel(
+              'Develop switch is currently <b class="on">ON</b>.'))
+    else:
+      self.ui.SetState(
+          i18n_test_ui.MakeI18nLabel(
+              'Please turn <b class="on">ON</b> Developer Switch.'))
+      self.ui.SetInstruction(
+          i18n_test_ui.MakeI18nLabel(
+              'Develop switch is currently <b class="off">OFF</b>.'))
