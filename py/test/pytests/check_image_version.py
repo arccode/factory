246cb2e619bf70a6a8beb2a839a9b08a10932302
Peter Shih <pihsun@chromium.org>
2017-12-07 14:42:39 +0800

    pytests: Refactor check_image_version to use TestCaseWithUI.
    BUG=b:68681772
    TEST=make test
    TEST=manually on DUT.
    
    Change-Id: Ife401ca676716add2d738affcb6767a2a0becdf0
    Reviewed-on: https://chromium-review.googlesource.com/813148
    Commit-Ready: Pi-Hsun Shih <pihsun@chromium.org>
    Tested-by: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

--- py/test/pytests/check_image_version.py
+++ py/test/pytests/check_image_version.py
@@ -80,106 +80,19 @@ Reimage if test image version is greater than or equal to 9876.5.2012_12_21_2359
 from distutils import version
 import logging
 import time
-import unittest
 
 import factory_common  # pylint: disable=unused-import
 from cros.factory.device import device_utils
 from cros.factory.test.event_log import Log
 from cros.factory.test.i18n import test_ui as i18n_test_ui
-from cros.factory.test import test_task
 from cros.factory.test import test_ui
-from cros.factory.test import ui_templates
 from cros.factory.test.utils import deploy_utils
 from cros.factory.test.utils import update_utils
 from cros.factory.tools import flash_netboot
 from cros.factory.utils.arg_utils import Arg
 
 
-_CSS = """
-.start-font-size {
-  font-size: 2em;
-}
-"""
-
-# Messages for tasks
-_MSG_VERSION_MISMATCH = i18n_test_ui.MakeI18nLabelWithClass(
-    'Image version is incorrect. Please re-image this device.',
-    'start-font-size test-error')
-_MSG_NETWORK = i18n_test_ui.MakeI18nLabel('Please connect to ethernet.')
-_MSG_NO_FIRMWARE_ON_SERVER = i18n_test_ui.MakeI18nLabel(
-    'Netboot firmware not available on factory server.')
-_MSG_NETBOOT = i18n_test_ui.MakeI18nLabel(
-    'Image version is incorrect. Press space to re-image.')
-_MSG_REIMAGING = i18n_test_ui.MakeI18nLabel('Flashing netboot firmware...')
-_MSG_FLASH_ERROR = i18n_test_ui.MakeI18nLabelWithClass(
-    'Error flashing netboot firmware!', 'start-font-size test-error')
-
-
-class ImageCheckTask(test_task.TestTask):
-
-  def __init__(self, test):
-    super(ImageCheckTask, self).__init__()
-    self._test = test
-    self.dut = test.dut
-
-  def CheckNetwork(self):
-    while not self.dut.status.eth_on:
-      time.sleep(0.5)
-      self._test.template.SetState(_MSG_NETWORK)
-
-  def PromptReimage(self):
-    self._test.template.SetState(_MSG_NETBOOT)
-    self._test.ui.BindKey(test_ui.SPACE_KEY, self.Reimage)
-
-  def Reimage(self):
-    # TODO(b/64881268): Run cros_payload to update release image directly.
-    if self._test.args.download_from_server:
-      updater = update_utils.Updater(update_utils.COMPONENTS.netboot_firmware)
-      if not updater.IsUpdateAvailable():
-        self._test.template.SetState(_MSG_NO_FIRMWARE_ON_SERVER)
-        return
-      updater.PerformUpdate(destination=self._test.args.netboot_fw)
-
-    self._test.template.SetState(_MSG_REIMAGING)
-    try:
-      with self.dut.temp.TempFile() as temp_file:
-        self.dut.link.Push(self._test.args.netboot_fw, temp_file)
-        factory_par = deploy_utils.CreateFactoryTools(self.dut)
-        factory_par.CheckCall(
-            ['flash_netboot', '-y', '-i', temp_file, '--no-reboot'], log=True)
-      self.dut.CheckCall(['reboot'], log=True)
-      self.Fail('Incorrect image version, DUT is rebooting to reimage.')
-    except Exception:
-      self._test.template.SetState(_MSG_FLASH_ERROR)
-
-  def CheckImageVersion(self):
-    if self._test.args.check_release_image:
-      ver = self.dut.info.release_image_version
-    else:
-      ver = self.dut.info.factory_image_version
-    Log('image_version', version=ver)
-    version_format = (version.LooseVersion if self._test.args.loose_version else
-                      version.StrictVersion)
-    logging.info('Using version format: %r', version_format.__name__)
-    logging.info('current version: %r', ver)
-    logging.info('expected version: %r', self._test.args.min_version)
-    return version_format(ver) < version_format(self._test.args.min_version)
-
-  def Run(self):
-    if self.CheckImageVersion():
-      if self._test.args.reimage:
-        self.CheckNetwork()
-        if self._test.args.require_space:
-          self.PromptReimage()
-        else:
-          self.Reimage()
-      else:
-        self._test.template.SetState(_MSG_VERSION_MISMATCH)
-      return
-    self.Pass()
-
-
-class CheckImageVersionTest(unittest.TestCase):
+class CheckImageVersionTest(test_ui.TestCaseWithUI):
   ARGS = [
       Arg('min_version', str, 'Minimum allowed test or release image version.'),
       Arg('loose_version', bool, 'Allow any version number representation.',
@@ -198,10 +111,57 @@ class CheckImageVersionTest(unittest.TestCase):
 
   def setUp(self):
     self.dut = device_utils.CreateDUTInterface()
-    self._task_list = [ImageCheckTask(self)]
-    self.ui = test_ui.UI()
-    self.template = ui_templates.OneSection(self.ui)
-    self.ui.AppendCSS(_CSS)
 
   def runTest(self):
-    test_task.TestTaskManager(self.ui, self._task_list).Run()
+    if self.CheckImageVersion():
+      return
+
+    if not self.args.reimage:
+      self.FailTask('Image version is incorrect. Please re-image this device.')
+
+    while not self.dut.status.eth_on:
+      self.ui.SetState(
+          i18n_test_ui.MakeI18nLabel('Please connect to ethernet.'))
+      time.sleep(0.5)
+
+    if self.args.require_space:
+      self.ui.SetState(
+          i18n_test_ui.MakeI18nLabel(
+              'Image version is incorrect. Press space to re-image.'))
+      self.ui.WaitKeysOnce(test_ui.SPACE_KEY)
+
+    self.Reimage()
+
+  def Reimage(self):
+    # TODO(b/64881268): Run cros_payload to update release image directly.
+    if self.args.download_from_server:
+      updater = update_utils.Updater(update_utils.COMPONENTS.netboot_firmware)
+      if not updater.IsUpdateAvailable():
+        self.FailTask('Netboot firmware not available on factory server.')
+      updater.PerformUpdate(destination=self.args.netboot_fw)
+
+    self.ui.SetState(i18n_test_ui.MakeI18nLabel('Flashing netboot firmware...'))
+    try:
+      with self.dut.temp.TempFile() as temp_file:
+        self.dut.link.Push(self.args.netboot_fw, temp_file)
+        factory_par = deploy_utils.CreateFactoryTools(self.dut)
+        factory_par.CheckCall(
+            ['flash_netboot', '-y', '-i', temp_file, '--no-reboot'], log=True)
+      self.dut.CheckCall(['reboot'], log=True)
+    except Exception:
+      self.FailTask('Error flashing netboot firmware!')
+    else:
+      self.FailTask('Incorrect image version, DUT is rebooting to reimage.')
+
+  def CheckImageVersion(self):
+    if self.args.check_release_image:
+      ver = self.dut.info.release_image_version
+    else:
+      ver = self.dut.info.factory_image_version
+    Log('image_version', version=ver)
+    version_format = (version.LooseVersion if self.args.loose_version else
+                      version.StrictVersion)
+    logging.info('Using version format: %r', version_format.__name__)
+    logging.info('current version: %r', ver)
+    logging.info('expected version: %r', self.args.min_version)
+    return version_format(ver) >= version_format(self.args.min_version)
