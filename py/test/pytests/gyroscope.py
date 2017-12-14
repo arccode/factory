38a4985bcbc35a2bb4f756639a30cc3037bfc640
Peter Shih <pihsun@chromium.org>
2017-12-14 18:28:02 +0800

    pytests: Refactor gyroscope to use TestCaseWithUI.
    BUG=b:68681772
    TEST=make test
    TEST=manually on DUT.
    
    Change-Id: Ib67c2839d0d7ae5c9dd1dd098d5abcf2cc4a4463
    Reviewed-on: https://chromium-review.googlesource.com/826845
    Commit-Ready: Pi-Hsun Shih <pihsun@chromium.org>
    Tested-by: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>

--- py/test/pytests/gyroscope.py
+++ py/test/pytests/gyroscope.py
@@ -17,136 +17,16 @@ Usage examples::
 
 import collections
 import time
-import unittest
 
 import factory_common  # pylint: disable=unused-import
 from cros.factory.device import device_utils
-from cros.factory.test import event
 from cros.factory.test.i18n import test_ui as i18n_test_ui
-from cros.factory.test import test_task
 from cros.factory.test import test_ui
-from cros.factory.test import ui_templates
 from cros.factory.utils.arg_utils import Arg
 from cros.factory.utils import sync_utils
-from cros.factory.utils import type_utils
 
 
-_MSG_SPACE = i18n_test_ui.MakeI18nLabelWithClass(
-    'Please put device on a horizontal plane then press space to '
-    'start calibration.', 'test-info')
-_MSG_IN_PROGRESS = i18n_test_ui.MakeI18nLabelWithClass(
-    'Please do not move the device.', 'test-info')
-_MSG_START_MOVING = i18n_test_ui.MakeI18nLabelWithClass(
-    'Please rotate the device.', 'test-info')
-_MSG_SUBTESTS = '<div class="{state}">{key}={value}</div>'
-
-def GetPreparingMessage(secs):
-  return i18n_test_ui.MakeI18nLabelWithClass(
-      'Calibration will be started within {secs} seconds.'
-      'Please do not move device.',
-      'test-info',
-      secs=secs)
-
-
-_MSG_PASS = i18n_test_ui.MakeI18nLabelWithClass('PASS', 'test-pass')
-_MSG_FAIL = i18n_test_ui.MakeI18nLabelWithClass('FAIL', 'test-fail')
-
-_BR = '<br>'
-
-_CSS = """
-  .test-info { font-size: 2em; }
-  .test-pass { font-size: 2em; color: green; }
-  .test-fail { font-size: 2em; color: red; }
-"""
-
-
-class ReadGyroscopeTask(test_task.TestTask):
-  """Horizontal calibration for accelerometers.
-
-  Attributes:
-    test: The main Gyroscope TestCase object.
-    gyroscope: The gyroscope object.
-    rotation_threshold: The expected value to read when dut start rotating.
-    stop_threshold: The expected value to read when dut stop moving.
-    timeout_secs: Maximum retry time for gyro to return expected value.
-    setup_time_secs: How many seconds to wait after pressing space to
-      start calibration.
-  """
-
-  def __init__(self, test, gyroscope, rotation_threshold, stop_threshold,
-               timeout_secs, setup_time_secs):
-    super(ReadGyroscopeTask, self).__init__()
-    self.test = test
-    self.gyroscope = gyroscope
-    self.rotation_threshold = rotation_threshold
-    self.stop_threshold = stop_threshold
-    self.timeout_secs = timeout_secs
-    self.setup_time_secs = setup_time_secs
-    self.template = test.template
-
-  def _UpdateState(self, max_values):
-    state_msg = ''.join(
-        _MSG_SUBTESTS.format(state=('test-pass' if v > self.rotation_threshold
-                                    else 'test-fail'),
-                             key=k, value=v)
-        for k, v in max_values.items())
-
-    self.template.SetState(state_msg)
-
-  def _WaitForDeviceStop(self):
-    """Wait until absolute value of all sensors less than stop_threshold."""
-
-    def CheckSensorState():
-      data = self.gyroscope.GetData()
-      return all(abs(v) < self.stop_threshold for v in data.values())
-
-    sync_utils.WaitFor(CheckSensorState, self.timeout_secs)
-
-  def _WaitForDeviceRotate(self):
-    """Wait until all sensors has absolute value > rotation_threshold."""
-
-    max_values = collections.defaultdict(float)
-    def CheckSensorMaxValues():
-      data = self.gyroscope.GetData()
-      for sensor_name in data:
-        max_values[sensor_name] = max(max_values[sensor_name],
-                                      abs(data[sensor_name]))
-      self._UpdateState(max_values)
-      return all(abs(v) > self.rotation_threshold for v in max_values.values())
-
-    sync_utils.WaitFor(CheckSensorMaxValues, self.timeout_secs)
-
-  def StartTask(self):
-    """Waits a period of time and then starts testing."""
-    for i in xrange(self.setup_time_secs):
-      self.template.SetState(GetPreparingMessage(self.setup_time_secs - i))
-      time.sleep(1)
-
-    try:
-      self.template.SetInstruction(_MSG_IN_PROGRESS)
-      self._WaitForDeviceStop()
-
-      self.template.SetInstruction(_MSG_START_MOVING)
-      self._WaitForDeviceRotate()
-    except type_utils.TimeoutError as e:
-      self.Fail(e.message)
-      return
-
-    self.template.SetState(' ' + _MSG_PASS + _BR, append=True)
-    self.Pass()
-
-  def Run(self):
-    """Prompts a message to ask operator to press space."""
-    if self.test.args.autostart:
-      self.test.ui.AddEventHandler('StartTask', lambda _: self.StartTask())
-      self.test.ui.PostEvent(event.Event(event.Event.Type.TEST_UI_EVENT,
-                                         subtype='StartTask'))
-    else:
-      self.template.SetState(_MSG_SPACE)
-      self.test.ui.BindKey(test_ui.SPACE_KEY, lambda _: self.StartTask())
-
-
-class Gyroscope(unittest.TestCase):
+class Gyroscope(test_ui.TestCaseWithUI):
 
   ARGS = [
       Arg('rotation_threshold', float,
@@ -168,16 +48,58 @@ class Gyroscope(unittest.TestCase):
     self.dut = device_utils.CreateDUTInterface()
     self.gyroscope = self.dut.gyroscope.GetController(
         location=self.args.location)
-    self.ui = test_ui.UI()
-    self.template = ui_templates.TwoSections(self.ui)
-    self.ui.AppendCSS(_CSS)
-    self._task_manager = None
+    self.ui.AppendCSS('test-template { font-size: 2em; }')
 
   def runTest(self):
-    task_list = [ReadGyroscopeTask(self, self.gyroscope,
-                                   self.args.rotation_threshold,
-                                   self.args.stop_threshold,
-                                   self.args.timeout_secs,
-                                   self.args.setup_time_secs)]
-    self._task_manager = test_task.TestTaskManager(self.ui, task_list)
-    self._task_manager.Run()
+    if not self.args.autostart:
+      self.ui.SetState(
+          i18n_test_ui.MakeI18nLabel(
+              'Please put device on a horizontal plane then press space to '
+              'start calibration.'))
+      self.ui.WaitKeysOnce(test_ui.SPACE_KEY)
+
+    for i in xrange(self.args.setup_time_secs):
+      self.ui.SetState(
+          i18n_test_ui.MakeI18nLabel(
+              'Calibration will be started within {secs} seconds.'
+              'Please do not move device.',
+              secs=self.args.setup_time_secs - i))
+      time.sleep(1)
+
+    self.ui.SetInstruction(
+        i18n_test_ui.MakeI18nLabel('Please do not move the device.'))
+    self._WaitForDeviceStop()
+
+    self.ui.SetInstruction(
+        i18n_test_ui.MakeI18nLabel('Please rotate the device.'))
+    self._WaitForDeviceRotate()
+
+  def _UpdateState(self, max_values):
+    html = []
+    for k, v in max_values.iteritems():
+      state = ('test-status-passed'
+               if v > self.args.rotation_threshold else 'test-status-failed')
+      html.append('<div class="%s">%s=%s</div>' % (state, test_ui.Escape(k), v))
+    self.ui.SetState(''.join(html))
+
+  def _WaitForDeviceStop(self):
+    """Wait until absolute value of all sensors less than stop_threshold."""
+
+    def CheckSensorState():
+      data = self.gyroscope.GetData()
+      return max(abs(v) for v in data.values()) < self.args.stop_threshold
+
+    sync_utils.WaitFor(CheckSensorState, self.args.timeout_secs)
+
+  def _WaitForDeviceRotate(self):
+    """Wait until all sensors has absolute value > rotation_threshold."""
+
+    max_values = collections.defaultdict(float)
+    def CheckSensorMaxValues():
+      data = self.gyroscope.GetData()
+      for sensor_name, value in data.iteritems():
+        max_values[sensor_name] = max(max_values[sensor_name], abs(value))
+      self._UpdateState(max_values)
+      return min(max_values.values()) > self.args.rotation_threshold
+
+    sync_utils.WaitFor(CheckSensorMaxValues, self.args.timeout_secs)
