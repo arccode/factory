68562ffc1796790ebb9c30bb8d50c682bb0db852
Peter Shih <pihsun@chromium.org>
2018-01-15 18:03:49 +0800

    station_entry_unittest: Use mox instead of mock.
    Use mox instead of mock, so we can just verify function calls that we
    care about, and don't have to list all possible calls to UI.
    
    BUG=None
    TEST=make test
    
    Change-Id: Ic0ddf35f96479edc22ffc84cbc4f4a79e3b06b58
    Reviewed-on: https://chromium-review.googlesource.com/866651
    Commit-Ready: Pi-Hsun Shih <pihsun@chromium.org>
    Tested-by: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

--- py/test/pytests/station_entry_unittest.py
+++ py/test/pytests/station_entry_unittest.py
@@ -2,26 +2,23 @@
 # Copyright 2015 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-#
-# We need to inject mock objects to protected members of FactoryEntry:
-# pylint: disable=protected-access
 
 """Unit tests for station_entry factory test."""
 
 import logging
 import unittest
 
-import mox
+import mock
 
 import factory_common  # pylint: disable=unused-import
 from cros.factory.device import device_utils
-from cros.factory.device import types
-from cros.factory.goofy.goofy_rpc import GoofyRPC
 from cros.factory.test import device_data
 from cros.factory.test.pytests import station_entry
 from cros.factory.test import state
 from cros.factory.test import test_ui
 from cros.factory.utils import sync_utils
+from cros.factory.utils import type_utils
+from cros.factory.unittest_utils import mock_time_utils
 
 
 class FakeArgs(object):
@@ -30,41 +27,43 @@ class FakeArgs(object):
       self.__dict__[key] = value
 
 
-_MOX_HTML_TYPE = mox.Or(mox.IsA(list), mox.IsA(basestring), mox.IsA(dict))
+_MOCK_TEST_STATES = 'mock_test_states'
 
 
 class FactoryEntryUnitTest(unittest.TestCase):
   def setUp(self):
-    self.mox = mox.Mox()
-    self.test = station_entry.StationEntry()
+    self._patchers = []
 
-    self.mock_ui = self.mox.CreateMock(test_ui.StandardUI)
+    self._timeline = mock_time_utils.TimeLine()
+    self._patchers.extend(mock_time_utils.MockAll(self._timeline))
+
+    self.test = station_entry.StationEntry()
+    self.test.ui = mock.Mock(spec=test_ui.StandardUI)
+
+    self.mock_state = mock.Mock()
+    self._CreatePatcher(state, 'get_instance').return_value = self.mock_state
+    self.mock_state.get_test_states.return_value = _MOCK_TEST_STATES
+
+    self.mock_dut = mock.Mock()
+    self._CreatePatcher(device_utils,
+                        'CreateDUTInterface').return_value = self.mock_dut
+
+    self.mock_clear_serial = self._CreatePatcher(device_data,
+                                                 'ClearAllSerialNumbers')
+
+    self._polling_sleep_context = sync_utils.WithPollingSleepFunction(
+        self._timeline.AdvanceTime)
+    self._polling_sleep_context.__enter__()
 
   def tearDown(self):
-    self.mox.UnsetStubs()
+    self._polling_sleep_context.__exit__(None, None, None)
+    for patcher in self._patchers:
+      patcher.stop()
 
-  def testSetUpForStartTesting(self):
-    mock_state = self.mox.CreateMock(GoofyRPC)
-
-    self.mox.StubOutWithMock(state, 'get_instance')
-
-    # for start testing
-    state.get_instance().AndReturn(mock_state)
-    self.mock_ui.ToggleTemplateClass(mox.IgnoreArg(), mox.IgnoreArg())
-    self.mock_ui.SetTitle(_MOX_HTML_TYPE)
-
-    self.mox.ReplayAll()
-
-    self.test.args = FakeArgs({'start_station_tests': True,
-                               'prompt_start': False,
-                               'timeout_secs': None,
-                               'invalidate_dut_info': True,
-                               'clear_serial_numbers': True, })
-    self.test.ui = self.mock_ui
-    self.test.setUp()
-    self.assertEqual(self.test._state, mock_state)
-
-    self.mox.VerifyAll()
+  def _CreatePatcher(self, *args, **kwargs):
+    patcher = mock.patch.object(*args, **kwargs)
+    self._patchers.append(patcher)
+    return patcher.start()
 
   def testLocalEndStationBasedTest(self):
     self._testEndStationBasedTest(is_local=True)
@@ -73,11 +72,6 @@ class FactoryEntryUnitTest(unittest.TestCase):
     self._testEndStationBasedTest(is_local=False)
 
   def _testEndStationBasedTest(self, is_local):
-    mock_dut_link = self.mox.CreateMock(types.DeviceLink)
-    self.test._dut = device_utils.CreateDUTInterface()
-    self.test._dut.link = mock_dut_link
-    mock_state = self.mox.CreateMock(GoofyRPC)
-    self.test._state = mock_state
     timeout_secs = 123
     self.test.args = FakeArgs({'start_station_tests': False,
                                'prompt_start': False,
@@ -85,33 +79,24 @@ class FactoryEntryUnitTest(unittest.TestCase):
                                'disconnect_dut': True,
                                'invalidate_dut_info': True,
                                'clear_serial_numbers': True, })
-    self.test.ui = self.mock_ui
 
-    self.mox.StubOutWithMock(device_data, 'ClearAllSerialNumbers')
-    self.mox.StubOutWithMock(sync_utils, 'WaitFor')
-    self.mox.StubOutWithMock(self.test, 'SendTestResult')
+    self.mock_dut.link.IsLocal.return_value = is_local
+    self.mock_dut.link.IsReady.side_effect = (
+        lambda: self._timeline.GetTime() < 10)
 
-    device_data.ClearAllSerialNumbers()
-    self.mock_ui.SetState(_MOX_HTML_TYPE).MultipleTimes()
-
-    mock_dut_link.IsLocal().AndReturn(is_local)
-    if not is_local:
-      sync_utils.WaitFor(mox.IsA(type(lambda: None)), timeout_secs,
-                         poll_interval=1)
-
-    self.test.SendTestResult()
-
-    self.mox.ReplayAll()
-
+    self.test.setUp()
     self.test.runTest()
 
-    self.mox.VerifyAll()
+    self.mock_dut.info.Invalidate.assert_called_once()
+    self.mock_clear_serial.assert_called_once()
+    self.mock_state.PostHookEvent.assert_called_once_with(
+        'TestResult', _MOCK_TEST_STATES)
+
+    if not is_local:
+      self.assertFalse(self.mock_dut.link.IsReady())
+      self._timeline.AssertTimeAt(10)
 
   def testStartStationBasedTest(self):
-    mock_dut_link = self.mox.CreateMock(types.DeviceLink)
-    self.test._dut = device_utils.CreateDUTInterface()
-    self.test._dut.link = mock_dut_link
-    self.test.ui = self.mock_ui
     timeout_secs = 123
     self.test.args = FakeArgs({'start_station_tests': True,
                                'prompt_start': False,
@@ -120,22 +105,35 @@ class FactoryEntryUnitTest(unittest.TestCase):
                                'invalidate_dut_info': True,
                                'clear_serial_numbers': True, })
 
-    self.mox.StubOutWithMock(device_data, 'ClearAllSerialNumbers')
-    self.mox.StubOutWithMock(self.test._dut.info, 'GetSerialNumber')
-    self.mox.StubOutWithMock(sync_utils, 'WaitFor')
+    self.mock_dut.link.IsReady.side_effect = (
+        lambda: self._timeline.GetTime() >= 10)
 
-    device_data.ClearAllSerialNumbers()
-    self.test._dut.info.GetSerialNumber('serial_number')
-    self.test._dut.info.GetSerialNumber('mlb_serial_number')
-    self.mock_ui.SetState(_MOX_HTML_TYPE).MultipleTimes()
-    sync_utils.WaitFor(mox.IsA(type(lambda: None)), timeout_secs,
-                       poll_interval=1)
-
-    self.mox.ReplayAll()
-
+    self.test.setUp()
     self.test.runTest()
 
-    self.mox.VerifyAll()
+    self.mock_dut.info.Invalidate.assert_called_once()
+    self.mock_clear_serial.assert_called_once()
+    self.assertEqual(self.mock_dut.info.GetSerialNumber.call_args_list,
+                     [(('serial_number',),), (('mlb_serial_number',),)])
+    self.mock_state.PostHookEvent.assert_not_called()
+
+    self.assertTrue(self.mock_dut.link.IsReady())
+    self._timeline.AssertTimeAt(10)
+
+  def testStartStationBasedTestTimeout(self):
+    timeout_secs = 123
+    self.test.args = FakeArgs({'start_station_tests': True,
+                               'prompt_start': False,
+                               'load_dut_storage': True,
+                               'timeout_secs': timeout_secs,
+                               'invalidate_dut_info': True,
+                               'clear_serial_numbers': True, })
+
+    self.mock_dut.link.IsReady.return_value = False
+
+    self.test.setUp()
+    self.assertRaisesRegexp(type_utils.TestFailure, 'DUT is not connected',
+                            self.test.runTest)
 
 
 if __name__ == '__main__':
