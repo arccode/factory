bb6cc984292ed7738e14765b8d6b7aa5f137aba0
Yong Hong <yhong@chromium.org>
2018-02-10 20:18:14 +0800

    probe: Adds probe functions for Elan touchscreen.
    Normally, an input device should export the vendor number and the
    product number to the corresponding input device data structure
    in kernel.  But the driver of Elan touchscreen doesn't do that.
    Therefore, factory has to implement a special probe function for them
    before the drivers are ready.
    
    Also, before the integration process of AVL and the probe framework
    ready, it's still more convience to have a single shared strong
    probe_statement for each Chromebook project.  Therefore, this CL
    directly revises `hwid/v3/default_probe_statement.json` to let the
    probe tool always try to probe the elan touchscreen.
    
    BUG=b:73099987
    TEST=make test, manually test.
    
    Change-Id: Icca63a680f2190aec33720e41475b49358d0227d
    Reviewed-on: https://chromium-review.googlesource.com/915583
    Commit-Ready: Yong Hong <yhong@google.com>
    Tested-by: Yong Hong <yhong@google.com>
    Reviewed-by: Yilun Lin <yllin@chromium.org>

---
+++ py/probe/functions/touchscreen_elan.py
@@ -0,0 +1,35 @@
+# Copyright 2018 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import glob
+import os
+
+import factory_common  # pylint: disable=unused-import
+from cros.factory.probe import function
+from cros.factory.probe.functions import sysfs
+
+
+class TouchscreenElanFunction(function.ProbeFunction):
+  """Probe the ELAN touchscreen information."""
+
+  I2C_DEVICES_PATH = '/sys/bus/i2c/devices'
+  DRIVER_NAME = 'elants_i2c'
+
+  def Probe(self):
+    results = []
+    for device_path in glob.glob(os.path.join(self.I2C_DEVICES_PATH, '*')):
+      driver_link = os.path.join(device_path, 'driver')
+      if not os.path.islink(driver_link):
+        continue
+
+      driver_name = os.path.basename(os.readlink(driver_link))
+      if driver_name != self.DRIVER_NAME:
+        continue
+
+      result = sysfs.ReadSysfs(
+          device_path, ['name', 'hw_version', 'fw_version'])
+      if result:
+        results.append(result)
+
+    return results
---
+++ py/probe/functions/touchscreen_elan_unittest.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# Copyright 2018 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import mock
+import unittest
+import tempfile
+
+import factory_common  # pylint: disable=unused-import
+from cros.factory.utils import file_utils
+from cros.factory.probe.functions import touchscreen_elan
+
+
+class TouchscreenElanFunctionTest(unittest.TestCase):
+  def setUp(self):
+    self.func = touchscreen_elan.TouchscreenElanFunction()
+    self.orig_dir = self.func.I2C_DEVICES_PATH
+    self.tmp_dir = self.func.I2C_DEVICES_PATH = tempfile.mkdtemp()
+
+    file_utils.WriteFile(os.path.join(self.tmp_dir, 'elants_i2c'), '')
+    file_utils.WriteFile(os.path.join(self.tmp_dir, 'xxx'), '')
+
+  def tearDown(self):
+    self.func.I2C_DEVICES_PATH = self.orig_dir
+
+  @mock.patch('cros.factory.probe.functions.sysfs.ReadSysfs')
+  def testNormal(self, read_sysfs_mock):
+    self._CreateDevice('dev1', {}, {'driver': '../elants_i2c'})
+    self.assertEquals(self.func.Probe(),
+                      [read_sysfs_mock.return_value])
+
+  @mock.patch('cros.factory.probe.functions.sysfs.ReadSysfs')
+  def testDriverIsNotALink(self, unused_read_sysfs_mock):
+    self._CreateDevice('dev1', {'driver': '../elants_i2c'}, {})
+    self.assertEquals(self.func.Probe(), [])
+
+  @mock.patch('cros.factory.probe.functions.sysfs.ReadSysfs')
+  def testNotCorrectDriver(self, unused_read_sysfs_mock):
+    self._CreateDevice('dev1', {'driver': '../xxx'}, {})
+    self.assertEquals(self.func.Probe(), [])
+
+  @mock.patch('cros.factory.probe.functions.sysfs.ReadSysfs',
+              return_value=None)
+  def testBadSysfsDir(self, unused_read_sysfs_mock):
+    self._CreateDevice('dev1', {}, {'driver': '../elants_i2c'})
+    self.assertEquals(self.func.Probe(), [])
+
+  def _CreateDevice(self, name, files, links):
+    file_utils.TryMakeDirs(os.path.join(self.tmp_dir, 'devices', name))
+    for file_name, file_content in files.iteritems():
+      path = os.path.join(self.tmp_dir, 'devices', name, file_name)
+      file_utils.WriteFile(path, file_content)
+    for link_name, link_target in links.iteritems():
+      path = os.path.join(self.tmp_dir, 'devices', link_name)
+      file_utils.ForceSymlink(link_target, path)
+
+
+if __name__ == '__main__':
+  unittest.main()
