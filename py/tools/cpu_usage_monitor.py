6224e209c11cd3a41c0123bb5ff7fcb75c40e612
Peter Shih <pihsun@chromium.org>
2018-03-28 16:30:20 +0800

    cpu_usage_monitor: Use DUT interface.
    Use dut.CheckOutput instead of process_utils.CheckOutput.
    
    Also refactor the unittest to be cleaner, and update the mock top output
    to a recent format.
    
    BUG=None
    TEST=make test
    
    Change-Id: I79f1f67b726e9f6a444835382a683cc3c0d5c40e
    Reviewed-on: https://chromium-review.googlesource.com/983280
    Commit-Ready: Pi-Hsun Shih <pihsun@chromium.org>
    Tested-by: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

--- py/tools/cpu_usage_monitor.py
+++ py/tools/cpu_usage_monitor.py
@@ -8,8 +8,8 @@ import logging
 import time
 
 import factory_common  # pylint: disable=unused-import
+from cros.factory.device import device_utils
 from cros.factory.utils import log_utils
-from cros.factory.utils import process_utils
 
 
 class CPUUsageMonitor(object):
@@ -29,39 +29,47 @@ class CPUUsageMonitor(object):
   def _GetLoadString(self):
     return ', '.join('%.1f' % load for load in self.dut.status.load_avg)
 
+  def GetStatus(self):
+    """Get the current CPU usage status.
+
+    Returns:
+      A string of current CPU usage.
+    """
+    msg = []
+    msg.append('Load average: %s' % self._GetLoadString())
+
+    # Get column legend from 'top' and throw away summary header and legend
+    top_output = self.dut.CheckOutput(
+        ['top', '-b', '-c', '-n', '1', '-w', '512']).splitlines()
+    column_ids = top_output[self.HEADER_LINES].split()
+    pid_column = column_ids.index('PID')
+    cpu_column = column_ids.index('%CPU')
+    command_column = column_ids.index('COMMAND')
+    top_output = top_output[self.HEADER_LINES + 1:]
+
+    # Find up to NUM_TOP_PROCESSES processes with CPU usage >= CPU_THRESHOLD
+    for process in top_output[0:self.NUM_TOP_PROCESSES]:
+      attr = process.split(None, command_column)
+      if float(attr[cpu_column]) < self.CPU_THRESHOLD:
+        break
+      command = attr[command_column][0:self.COMMAND_LENGTH]
+      msg.append('Process %s using %s%% CPU: %s' %
+                 (attr[pid_column], attr[cpu_column], command))
+
+    return '; '.join(msg)
+
   def Check(self):
     """Checks the current CPU usage status.
 
     Logs current load average and top three processes that use more than 10%
     CPU.
     """
-    msg = []
     try:
-      msg.append('Load average: %s' % self._GetLoadString())
-
-      # TODO(hungte) Change process_utils.CheckOutput to dut.CheckOutput so we
-      # can support getting remote system load.
-      # Get column legend from 'top' and throw away summary header and legend
-      top_output = process_utils.CheckOutput(
-          ['top', '-b', '-c', '-n', '1', '-w', '512']).split('\n')
-      column_ids = top_output[self.HEADER_LINES].split()
-      pid_column = column_ids.index('PID')
-      cpu_column = column_ids.index('%CPU')
-      command_column = column_ids.index('COMMAND')
-      top_output = top_output[self.HEADER_LINES + 1:]
-
-      # Find up to NUM_TOP_PROCESSES processes with CPU usage >= CPU_THRESHOLD
-      for process in top_output[0:self.NUM_TOP_PROCESSES]:
-        attr = process.split()
-        if float(attr[cpu_column]) < self.CPU_THRESHOLD:
-          break
-        command = ' '.join(attr[command_column:])[0:self.COMMAND_LENGTH]
-        msg.append('Process %s using %s%% CPU: %s' %
-                   (attr[pid_column], attr[cpu_column], command))
+      msg = self.GetStatus()
     except Exception:
       logging.exception('Unable to check CPU usage')
     else:
-      logging.info('; '.join(msg))
+      logging.info(msg)
 
   def CheckForever(self):
     while True:
@@ -76,9 +84,6 @@ def main():
   args = parser.parse_args()
   log_utils.InitLogging()
 
-  # TODO(hungte) This currently only reads from local system. We need to expore
-  # DUT options and allow reading from remote in future.
-  from cros.factory.device import device_utils
   monitor = CPUUsageMonitor(args.period_secs, device_utils.CreateDUTInterface())
   monitor.CheckForever()
 
--- py/tools/cpu_usage_monitor_unittest.py
+++ py/tools/cpu_usage_monitor_unittest.py
@@ -4,32 +4,29 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import logging
 import unittest
 
-import mox
+import mock
 
 import factory_common  # pylint: disable=unused-import
-from cros.factory.device import device_utils
 from cros.factory.tools.cpu_usage_monitor import CPUUsageMonitor
-from cros.factory.utils import process_utils
 
 MOCK_TOP_OUTPUT = \
 """top - 11:46:54 up  3:25,  0 users,  load average: 0.33, 2.32, 2.79
 Tasks: 224 total,   1 running, 223 sleeping,   0 stopped,   0 zombie
-Cpu(s): 10.3%us, 55.8%sy,  0.0%ni, 33.4%id,  0.2%wa,  0.0%hi,  0.2%si,  0.0%st
-Mem:   2067628k total,   789764k used,  1277864k free,   105392k buffers
-Swap:  1953124k total,        0k used,  1953124k free,   271360k cached
+%Cpu(s): 10.3 us, 55.8 sy,  0.0 ni, 33.4 id,  0.2 wa,  0.0 hi,  0.2 si,  0.0 st
+KiB Mem:   2067628 total,  1277864 free,   789764 used,   105392 buff/cache
+KiB Swap:  1953124 total,  1953124 free,        0 used.   271360 avail Mem
 
-  PID USER   PR  NI  VIRT  RES  SHR S %CPU %MEM    TIME+  COMMAND
-  32753 root   20   0  2284 1052  716 R    60  0.1   0:00.06 longer_than_10_char
-  30275 root   20   0  170m  19m 3664 S    25  0.9   0:07.04 i_use_cpu
-  30277 root   20   0  310m  12m 4436 S     4 12.6   0:03.04 i_use_memory
-      3 root   20   0     0    0    0 S     0  0.0   0:00.80 [ksoftirqd/0]
-      6 root   RT   0     0    0    0 S     0  0.0   0:00.00 [migration/0]
-      7 root   RT   0     0    0    0 S     0  0.0   0:00.30 [watchdog/0]
+  PID USER   PR  NI  VIRT  RES  SHR S  %CPU %MEM     TIME+ COMMAND
+32753 root   20   0  2284 1052  716 R    60  0.1   0:00.06 longer_than_10_char
+30275 root   20   0  170m  19m 3664 S    25  0.9   0:07.04 i_use_cpu
+30277 root   20   0  310m  12m 4436 S     4 12.6   0:03.04 i_use_memory
+    3 root   20   0     0    0    0 S     0  0.0   0:00.80 [ksoftirqd/0]
+    6 root   RT   0     0    0    0 S     0  0.0   0:00.00 [migration/0]
+    7 root   RT   0     0    0    0 S     0  0.0   0:00.30 [watchdog/0]
 """
-MOCK_LOAD = '1.2, 0.9, 0.8'
+MOCK_LOAD = [1.2, 0.9, 0.8]
 
 EXPECTED_OUTPUT = ('Load average: 1.2, 0.9, 0.8; ' +
                    'Process 32753 using 60% CPU: longer_tha; ' +
@@ -39,26 +36,18 @@ EXPECTED_OUTPUT = ('Load average: 1.2, 0.9, 0.8; ' +
 class TestCpuUsageMonitor(unittest.TestCase):
 
   def setUp(self):
-    self.monitor = CPUUsageMonitor(120, device_utils.CreateDUTInterface())
+    self.mock_dut = mock.Mock()
+    self.monitor = CPUUsageMonitor(120, self.mock_dut)
     self.monitor.COMMAND_LENGTH = 10
-    self.mox = mox.Mox()
-    self.mox.StubOutWithMock(logging, 'info')
-    self.mox.StubOutWithMock(CPUUsageMonitor, '_GetLoadString')
-    self.mox.StubOutWithMock(process_utils, 'CheckOutput')
-
-  def tearDown(self):
-    self.mox.VerifyAll()
-    self.mox.UnsetStubs()
 
   def testTopParsing(self):
-    # pylint: disable=protected-access
-    self.monitor._GetLoadString().AndReturn(MOCK_LOAD)
-    process_utils.CheckOutput(['top', '-b', '-c', '-n', '1', '-w',
-                               '512']).AndReturn(MOCK_TOP_OUTPUT)
-    logging.info(EXPECTED_OUTPUT)
+    type(self.mock_dut.status).load_avg = mock.PropertyMock(
+        return_value=MOCK_LOAD)
+    self.mock_dut.CheckOutput.return_value = MOCK_TOP_OUTPUT
 
-    self.mox.ReplayAll()
-    self.monitor.Check()
+    self.assertEqual(self.monitor.GetStatus(), EXPECTED_OUTPUT)
+    self.mock_dut.CheckOutput.assert_called_once_with(
+        ['top', '-b', '-c', '-n', '1', '-w', '512'])
 
 
 if __name__ == '__main__':
