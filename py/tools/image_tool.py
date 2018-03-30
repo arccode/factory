fc035fb0943dbd0185a385788ba8eb2481edef2a
Hung-Te Lin <hungte@chromium.org>
2018-03-30 13:31:20 +0800

    tools: Add new 'image_tool' for various setup/deploy utilities.
    The scripts in setup/ were written in simple bash/dash in order for
    partners to execute them without much dependency. However, the shell
    scripts are bloated and very hard to maintain recently.
    
    Since most partners setting up factory servers are using Ubuntu today
    and all running Python 2.6, it should be fine to assume Python is
    available and stop using shell scripts.
    
    This change adds a new 'image_tool' command with dummy implementation
    that will replace all scripts dealing with image creation or processing
    in future, for example:
    
         mount_partition => image_tool mount IMAGE PART MOUNTPOINT
    
    BUG=chromium:738360
    TEST=make test;
    
    Change-Id: Ia3fc3ffb8be8a58f6e34d54955fff0aeaf97d141
    Reviewed-on: https://chromium-review.googlesource.com/987673
    Commit-Ready: Hung-Te Lin <hungte@chromium.org>
    Tested-by: Hung-Te Lin <hungte@chromium.org>
    Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>

---
+++ py/tools/image_tool.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# Copyright 2018 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Utility to manipulate Chrome OS disk & firmware images for manufacturing.
+
+Run "image_tool help" for more info and a list of subcommands.
+
+To add a subcommand, just add a new SubCommand subclass to this file.
+"""
+
+
+from __future__ import print_function
+
+import argparse
+import inspect
+import logging
+import sys
+
+
+# TODO(hungte) Generalize this (copied from py/tools/factory.py) for all
+# commands to utilize easily.
+class SubCommand(object):
+  """A subcommand.
+
+  Properties:
+    name: The name of the command (set by the subclass).
+    parser: The ArgumentParser object.
+    subparser: The subparser object created with parser.add_subparsers.
+    subparsers: A collection of all subparsers.
+    args: The parsed arguments.
+  """
+  name = None  # Overridden by subclass
+
+  parser = None
+  args = None
+  subparser = None
+  subparsers = None
+
+  def __init__(self, parser, subparsers):
+    assert self.name
+    self.parser = parser
+    self.subparsers = subparsers
+    subparser = subparsers.add_parser(
+        self.name, help=self.__doc__.splitlines()[0],
+        description=self.__doc__)
+    subparser.set_defaults(subcommand=self)
+    self.subparser = subparser
+
+  def Init(self):
+    """Initializes the subparser.
+
+    May be implemented the subclass, which may use "self.subparser" to
+    refer to the subparser object.
+    """
+    pass
+
+  def Run(self):
+    """Runs the command.
+
+    Must be implemented by the subclass.
+    """
+    raise NotImplementedError
+
+
+class HelpCommand(SubCommand):
+  """Get help on COMMAND"""
+  name = 'help'
+
+  def Init(self):
+    self.subparser.add_argument('command', metavar='COMMAND', nargs='?')
+
+  def Run(self):
+    if self.args.command:
+      choice = self.subparsers.choices.get(self.args.command)
+      if not choice:
+        sys.exit('Unknown subcommand %r' % self.args.command)
+      choice.print_help()
+    else:
+      self.parser.print_help()
+
+
+def main():
+  parser = argparse.ArgumentParser(
+      description=(
+          'Tools to manipulate Chromium OS disk images for factory. '
+          'Use "image_tool help COMMAND" for more info on a '
+          'subcommand.'))
+  parser.add_argument('--verbose', '-v', action='count', default=0,
+                      help='Verbose output')
+  subparsers = parser.add_subparsers(title='subcommands')
+
+  for unused_key, v in sorted(globals().items()):
+    if v != SubCommand and inspect.isclass(v) and issubclass(v, SubCommand):
+      subcommand = v(parser, subparsers)
+      subcommand.Init()
+
+  args = parser.parse_args()
+  logging.basicConfig(level=logging.WARNING - args.verbose * 10)
+
+  args.subcommand.args = args
+  args.subcommand.Run()
+
+
+if __name__ == '__main__':
+  main()
---
+++ setup/lib/deprecated.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Copyright 2018 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+script="$(basename "$0")"
+script_dir="$(dirname "$0")"
+name="${script%%.*}"
+
+new_args="$@"
+
+echo "This script is deprecated. Please run 'image_tool' instead:
+
+  ${script_dir}/image_tool ${name} $(echo ${new_args})"
+exit 1
