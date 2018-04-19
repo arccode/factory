c5f8d06c3ae6e8f1d5e9e2e05f9f89743afeebce
chuntsen <chuntsen@google.com>
2018-04-19 18:20:17 +0800

    Umpire: Add migration 0007 for Instalog service
    Umprie's Instalog service use the newer buffer plugin, and it need to
    move some buffer files.
    The migration 0006 doesn't copy the consumers.json, and this may cause
    all output plugins down.
    
    BUG=b:66930704, b:67661968
    TEST=manually test on local device
    CQ-DEPEND=CL:1010056
    
    Change-Id: I0b8afaa7a76176b2edb308ccaff38f7119d09433
    Reviewed-on: https://chromium-review.googlesource.com/1019060
    Commit-Ready: Chun-Tsen Kuo <chuntsen@chromium.org>
    Tested-by: Chun-Tsen Kuo <chuntsen@chromium.org>
    Reviewed-by: Youcheng Syu <youcheng@chromium.org>

---
+++ py/umpire/server/migrations/0007.py
@@ -0,0 +1,29 @@
+# Copyright 2018 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Instalog service upgrade the buffer plugin.
+
+See https://chromium-review.googlesource.com/989530 for more details.
+"""
+
+import os
+import shutil
+
+
+_OLD_BUFFER_DIR = '/var/db/factory/umpire/umpire_data/instalog/data/buffer'
+
+
+def Migrate():
+  if os.path.isdir(_OLD_BUFFER_DIR):
+    for i in xrange(4):
+      for j in xrange(4):
+        if i == 0 and j == 0:
+          continue
+        new_buffer_dir = os.path.join(_OLD_BUFFER_DIR, '%d_%d' % (i, j))
+        if not os.path.exists(new_buffer_dir):
+          os.mkdir(new_buffer_dir)
+        sample_consumers_file = os.path.join(
+            _OLD_BUFFER_DIR, '0_0', 'consumers.json')
+        target_consumers_file = os.path.join(new_buffer_dir, 'consumers.json')
+        shutil.copyfile(sample_consumers_file, target_consumers_file)
