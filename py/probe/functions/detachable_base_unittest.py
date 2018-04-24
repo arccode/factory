81378214f0108af63957aacd9074ec7faaa58486
Peter Shih <pihsun@chromium.org>
2018-04-24 18:00:47 +0800

    Fix detachable_base_unittest.
    There are new items added in CL:1023732.
    
    BUG=None
    TEST=make test
    
    Change-Id: I92889584f54ce76e3394797bd3be23cbe0d228b9
    Reviewed-on: https://chromium-review.googlesource.com/1025619
    Commit-Ready: Pi-Hsun Shih <pihsun@chromium.org>
    Tested-by: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Marco Chen <marcochen@chromium.org>

--- py/probe/functions/detachable_base_unittest.py
+++ py/probe/functions/detachable_base_unittest.py
@@ -3,8 +3,9 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import mock
 import unittest
+
+import mock
 
 import factory_common  # pylint: disable=unused-import
 from cros.factory.probe.functions import detachable_base
@@ -23,10 +24,13 @@ class DetachableBaseFunctionTest(unittest.TestCase):
         [{'ro_version': 'hammer_info.py ro_version',
           'rw_version': 'hammer_info.py rw_version',
           'wp_screw': 'hammer_info.py wp_screw',
+          'wp_all': 'hammer_info.py wp_all',
           'touchpad_id': 'hammer_info.py touchpad_id',
           'touchpad_pid': 'hammer_info.py touchpad_pid',
           'touchpad_fw_version': 'hammer_info.py touchpad_fw_version',
-          'touchpad_fw_checksum': 'hammer_info.py touchpad_fw_checksum'}])
+          'touchpad_fw_checksum': 'hammer_info.py touchpad_fw_checksum',
+          'key_version': 'hammer_info.py key_version',
+          'challenge_status': 'hammer_info.py challenge_status'}])
 
 
 if __name__ == '__main__':
