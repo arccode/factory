f8485a4b5d23602bddd11df5890e9b18ab950dde
Peter Shih <pihsun@chromium.org>
2018-06-06 15:03:42 +0800

    Dome: Settings for backend should refer to static.
    settings.py should refer to the static/ folder (that is built by
    dome_builder), instead of the frontend/ folder.
    
    BUG=b:80217832
    TEST=make test
    TEST=manually
    
    Change-Id: Ibb0b33351f1f48f7e19bf02f82f7669bad9cc752
    Reviewed-on: https://chromium-review.googlesource.com/1088447
    Commit-Ready: Pi-Hsun Shih <pihsun@chromium.org>
    Tested-by: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

--- py/dome/backend/settings.py
+++ py/dome/backend/settings.py
@@ -70,7 +70,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'frontend'),
+            os.path.join(BASE_DIR, 'static'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
