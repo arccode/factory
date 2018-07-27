98c57a13df2fdeeda793b4edceb2d9405926c720
Wei-Han Chen <stimim@google.com>
2018-07-27 15:11:23 +0800

    diagnosis-tool: add devtools/generate-test-list.ts
    This will search 'src/app/tests', add all test items to
    'src/app/test-list.resource.ts'.
    
    This step is added to `npm run-script build` of diagnosis-tool project.
    From now on, we should use "npm run-script build" instead of
    "np build --prod".
    
    BUG=b:111100698
    TEST=npm run build
    
    Change-Id: I19d781ca8e6c49895def55c9dbcf75ba6f51bf1e
    Reviewed-on: https://chromium-review.googlesource.com/1152720
    Commit-Ready: Wei-Han Chen <stimim@chromium.org>
    Tested-by: Wei-Han Chen <stimim@chromium.org>
    Reviewed-by: Wei-Han Chen <stimim@chromium.org>

--- diagnosis-tool/src/app/app.module.ts
+++ diagnosis-tool/src/app/app.module.ts
@@ -6,7 +6,6 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {TEST_COMPONENTS} from './test-list.resource';
 import {TestListService} from './test-list.service';
 import {TestListComponent} from './test-list/test-list.component';
 import {TestDirective} from './test.directive';
