24966e84b9f84f651490ac176e3f4219b5fff6a3
Peter Shih <pihsun@chromium.org>
2018-07-31 12:09:17 +0800

    Dome: Add file-header lint rule.
    Add a file-header lint rule for tslint. Since tslint doesn't handle
    multiple single-line comment header well, we only check the first line
    of the license.
    
    BUG=b:80217832
    TEST=make test
    TEST=make lint-frontend in py/dome
    TEST=manually
    
    Change-Id: If7f9afc8cb03ad318ac1f4a9f737fe831b7aab21
    Reviewed-on: https://chromium-review.googlesource.com/1156110
    Commit-Ready: Pi-Hsun Shih <pihsun@chromium.org>
    Tested-by: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

--- py/dome/frontend/src/types.d.ts
+++ py/dome/frontend/src/types.d.ts
@@ -1,3 +1,7 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import {AnyAction} from 'redux';
 import {ThunkDispatch} from 'redux-thunk';
 import {StateType} from 'typesafe-actions';
