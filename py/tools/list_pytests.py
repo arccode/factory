1c96d30998e7c69cf8236c1cc5380323f49a3b46
You-Cheng Syu <youcheng@chromium.org>
2018-08-16 16:10:46 +0800

    list_pytests: Better GetPytestList.
    - Rename `_GetPytestList` to `GetPytestList` to make it "public" so that
      we can call it from other module later.
    - Change the argument `base_dir` to make it able to handle private
      overlays.
    - Recursively search all directories.
    - Use better heuristic methods to determine if a .py file is a pytest.
      It's now 100% accurate in the public factory repository.
    
    BUG=none
    TEST=manually verify the output of GetPytestList()
    
    Change-Id: I628618730537b4d67a4ae26763f103de8a665044
    Reviewed-on: https://chromium-review.googlesource.com/1175643
    Commit-Ready: Youcheng Syu <youcheng@chromium.org>
    Tested-by: Youcheng Syu <youcheng@chromium.org>
    Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>

--- py/tools/list_pytests.py
+++ py/tools/list_pytests.py
@@ -9,34 +9,42 @@ This script output all pytests in py/test/pytests in JSON format, with most
 non-pytest items filtered (unittests, utils, ...)
 """
 
-
 import argparse
-import glob
 import json
 import os
+import re
 
 import factory_common  # pylint: disable=unused-import
 from cros.factory.test.env import paths
 from cros.factory.test.test_lists import test_object
+from cros.factory.utils import file_utils
 
-_BLACKLIST_SUFFIX = ['_unittest', '_automator', '_e2etest', '_utils', '_host']
+
+_PATTERNS = (
+    r'^class .*\((unittest|test_case)\.TestCase\):',
+    r'^\s+ARGS = '
+)
 
 
-def _GetPytestList(base_dir):
-  pytest_dir = os.path.join(base_dir, 'test', 'pytests')
-  files = glob.glob(os.path.join(pytest_dir, '*.py'))
-  files.extend(glob.glob(os.path.join(pytest_dir, '*', '*.py')))
-  files.sort()
+def GetPytestList(base_dir):
+  def IsPytest(filepath):
+    # We don't directly load the file by pytest_utils because it doesn't support
+    # private overlays now.
+    root, ext = os.path.splitext(filepath)
+    if root.endswith('_unittest') or ext != '.py':
+      return False
+    content = file_utils.ReadFile(filepath)
+    return any(re.search(p, content, re.MULTILINE) for p in _PATTERNS)
 
-  def _IsExtraFile(filename):
-    name = os.path.splitext(os.path.basename(filename))[0]
-    if name in ['__init__', 'factory_common']:
-      return True
-    if any(name.endswith(suffix) for suffix in _BLACKLIST_SUFFIX):
-      return True
-    return False
-
-  return [f[len(pytest_dir)+1:] for f in files if not _IsExtraFile(f)]
+  res = []
+  pytest_dir = os.path.join(base_dir, 'py', 'test', 'pytests')
+  for dirpath, unused_dirnames, filenames in os.walk(pytest_dir):
+    for basename in filenames:
+      filepath = os.path.join(dirpath, basename)
+      if IsPytest(filepath):
+        res.append(os.path.relpath(filepath, pytest_dir))
+  res.sort()
+  return res
 
 
 def _ToI18nLabel(name):
@@ -52,11 +60,11 @@ def main():
       '-l', '--i18n-label', dest='label', action='store_true',
       help='Output the I18n label generated from pytest filename.')
   parser.add_argument(
-      'base_dir', nargs='?', default=paths.FACTORY_PYTHON_PACKAGE_DIR,
-      help='The Python base directory of factory repository.')
+      'base_dir', nargs='?', default=paths.FACTORY_DIR,
+      help='The base directory of factory repository (or factory-board/files).')
   options = parser.parse_args()
 
-  pytest_list = _GetPytestList(options.base_dir)
+  pytest_list = GetPytestList(options.base_dir)
 
   if options.label:
     pytest_list = map(_ToI18nLabel, pytest_list)
