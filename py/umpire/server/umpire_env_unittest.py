fda2fe8c137ff4a633ed631e053042ab47d5b14a
Hsin-Yi, Wang <hsinyi@chromium.org>
2018-11-05 15:45:48 +0800

    DUTconfig: umpire: add unittest
    Add unittest for update component/directory function
    
    BUG=b:116767069
    TEST=make test
    
    Change-Id: I35604464f46107edef3fcd2ddf4e37b50b7766aa
    Reviewed-on: https://chromium-review.googlesource.com/1317222
    Commit-Ready: Hsin-Yi Wang <hsinyi@chromium.org>
    Tested-by: Hsin-Yi Wang <hsinyi@chromium.org>
    Reviewed-by: Youcheng Syu <youcheng@chromium.org>

--- py/umpire/server/umpire_env_unittest.py
+++ py/umpire/server/umpire_env_unittest.py
@@ -10,6 +10,7 @@ import unittest
 import mox
 
 import factory_common  # pylint: disable=unused-import
+from cros.factory.umpire import common
 from cros.factory.umpire.server import resource
 from cros.factory.umpire.server import umpire_env
 from cros.factory.utils import file_utils
@@ -26,6 +27,8 @@ class UmpireEnvTest(unittest.TestCase):
   def setUp(self):
     self.env = umpire_env.UmpireEnvForTest()
     self.mox = mox.Mox()
+    parameter_json_file = self.env.parameter_json_file
+    shutil.copy(TEST_PARAMETER, parameter_json_file)
 
   def tearDown(self):
     self.mox.UnsetStubs()
@@ -71,9 +74,6 @@ class UmpireEnvTest(unittest.TestCase):
                      self.env.GetResourcePath('foobar', check=False))
 
   def testQueryParameters(self):
-    parameter_json_file = self.env.parameter_json_file
-    shutil.copy(TEST_PARAMETER, parameter_json_file)
-
     # Query w.sh in parent directory
     query_file = self.env.QueryParameters(None, 'w.sh')
     self.assertEqual(query_file, [('w.sh', 'some/path/w0.sh')])
@@ -86,6 +86,52 @@ class UmpireEnvTest(unittest.TestCase):
     # Query not existed component
     query_error = self.env.QueryParameters('dir0', 'not_existed_file')
     self.assertEqual(query_error, [])
+
+  def testUpdateParameterComponent(self):
+    test_file_path = os.path.join(self.env.base_dir, 'test.txt')
+    file_utils.TouchFile(test_file_path)
+
+    # Create new component
+    component = self.env.UpdateParameterComponent(None, 0, 'test.txt', None,
+                                                  test_file_path)
+    self.assertEqual(component, {
+        'id': 3,
+        'dir_id': 0,
+        'name': 'test.txt',
+        'revisions': [self.env.GetParameterDstPath(test_file_path)],
+        'using_ver': 0
+    })
+
+    # Update version
+    component = self.env.UpdateParameterComponent(0, None, 'w.sh', None,
+                                                  test_file_path)
+    self.assertEqual(component, {
+        'id': 0,
+        'dir_id': None,
+        'name': 'w.sh',
+        'revisions': [
+            'some/path/w0.sh',
+            self.env.GetParameterDstPath(test_file_path)
+        ],
+        'using_ver': 1
+    })
+
+    # Update version and change version at the same time
+    self.assertRaises(common.UmpireError, self.env.UpdateParameterComponent,
+                      0, None, 'w.sh', 1, test_file_path)
+
+    # Changing to invalid version
+    self.assertRaises(common.UmpireError, self.env.UpdateParameterComponent,
+                      0, None, 'w.sh', 10, None)
+
+  def testCreateParameterDirectory(self):
+    # Create directory
+    directory = self.env.CreateParameterDirectory(0, 'dir2')
+    self.assertEqual(directory, {
+        'id': 2,
+        'name': 'dir2',
+        'parent_id': 0
+    })
 
 
 if __name__ == '__main__':
