fd91ad2c3e8b73ef29393ff7ddbf340a7d0dcdc6
Cheng-Han Yang <chenghan@google.com>
2018-11-27 16:36:38 +0800

    pytests: Add manual check in check_serial_number.
    Add manual check to support matching serial number printed on the device
    (used in open box return), and move "CheckSerialNumber" definition to
    generic_common test list.
    
    BUG=None
    TEST=manually test on DUT
    
    Change-Id: I89d1fba834db985a4e83c85599b3ce062445617a
    Reviewed-on: https://chromium-review.googlesource.com/1351177
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Cheng-Han Yang <chenghan@chromium.org>
    Reviewed-by: Yong Hong <yhong@google.com>

--- py/test/pytests/check_serial_number.py
+++ py/test/pytests/check_serial_number.py
@@ -5,7 +5,7 @@
 """
 Description
 -----------
-Checks if serial number is set to a device.
+Checks if serial number is set correctly on a device.
 
 Some tests, for example, RF graphyte, assume DUTs have MLB serial number
 available (in VPD and device data).  And the test will fail if a DUT
@@ -16,7 +16,7 @@ This test gets serial number from different sources and check if they are equal.
 Test Procedure
 --------------
 This is an automated test.  It requires user interaction only if the test
-failed.  This test will,
+failed, or if the "manual_check" flag is turned on.  This test will,
 
 1. try to connect to device's state server
 2. the device must have device_id
@@ -32,21 +32,34 @@ Dependency
 
 Examples
 --------
-This test is already defined in `station_based.test_list.json` as
-"CheckSerialNumber", you can disable it by setting
-`constants.check_serial_number` to `false`::
+This test is already defined as "CheckSerialNumber"
+in `generic_common.test_list.json` and "StationCheckSerialNumber" in
+`station_based.test_list.json`. This test is turned on by default in station
+based test list. You can disable it by setting `constants.check_serial_number`
+to `false`::
 
   "constants": {
     "check_serial_number": false
   }
 
-By default, this pytest checks mlb_serial_number, if you want to check serial
+By default, this pytest checks mlb_serial_number. If you want to check serial
 number instead::
 
   {
     "pytest_name": "check_serial_number",
     "args": {
       "sn_name": "serial_number"
+    }
+  }
+
+By default, this pytest passes if the serial numbers match. If you want
+additional manual confirmation (e.g. also match the serial number printed on
+the device)::
+
+  {
+    "pytest_name": "check_serial_number",
+    "args": {
+      "manual_check": true
     }
   }
 
@@ -71,7 +84,8 @@ HTML = """
   <div>
     VPD: <span id='vpd-value'></span>
   <div>
-  <div id='message' style='color:red'></div>
+  <div id='pass_message' style='color:green'></div>
+  <div id='fail_message' style='color:red'></div>
 </div>
 """
 
@@ -80,7 +94,12 @@ class CheckDeviceState(test_case.TestCase):
   ARGS = [
       Arg('sn_name', str,
           'name of the serial number, e.g. "serial_number" or '
-          '"mlb_serial_number"', default='mlb_serial_number'),
+          '"mlb_serial_number"',
+          default='mlb_serial_number'),
+      Arg('manual_check', bool,
+          'If set to true, this test needs operator to press ENTER or ESC to '
+          'pass or fail the test case.',
+          default=False)
   ]
 
   def setUp(self):
@@ -101,10 +120,7 @@ class CheckDeviceState(test_case.TestCase):
 
     # must have device_id
     if not self.dut.info.device_id:
-      self.ui.SetHTML(
-          'No device_id<br />',
-          id='message',
-          append=True)
+      self.ui.SetHTML('No device_id<br />', id='fail_message', append=True)
       success = False
 
     device_data_sn = proxy.DataShelfGetValue(
@@ -115,27 +131,32 @@ class CheckDeviceState(test_case.TestCase):
 
     if not device_data_sn:
       self.ui.SetHTML('%s not in device data<br />' % sn_name,
-                      id='message', append=True)
+                      id='fail_message', append=True)
       success = False
 
     if not vpd_sn:
       self.ui.SetHTML('%s not in VPD<br />' % sn_name,
-                      id='message', append=True)
+                      id='fail_message', append=True)
       success = False
 
     if vpd_sn != device_data_sn:
       self.ui.SetHTML(
-          "Device data and VPD doesn't match<br />",
-          id='message',
-          append=True)
+          'Device data and VPD doesn\'t match<br />',
+          id='fail_message', append=True)
       success = False
 
     if success:
       session.console.info('OK: %s=%s', sn_name, device_data_sn)
+      if self.args.manual_check:
+        self.ui.SetHTML(
+            'Please press ENTER to pass or ESC to fail the test.<br />',
+            id='pass_message', append=True)
+        self.ui.BindStandardPassKeys()
+        self.ui.BindStandardFailKeys()
+        self.WaitTaskEnd()
     else:
       self.ui.SetHTML(
-          "Failed, Press SPACE to continue<br />",
-          id='message',
-          append=True)
-      self.ui.WaitKeysOnce(keys=[test_ui.SPACE_KEY, test_ui.ENTER_KEY])
+          'Failed, Press ENTER to continue<br />',
+          id='message', append=True)
+      self.ui.WaitKeysOnce(keys=[test_ui.ENTER_KEY])
       self.FailTask('Invalid device state (%s error)' % sn_name)
