1416ed6b65be7c6b26ee6fcf3711e3e75f5b30d9
chuntsen <chuntsen@google.com>
2019-01-07 19:06:56 +0800

    partition_table: Add a feature to expand stateful partition
    If the end of stateful partition not extends nearly the end of the
    storage device, this pytest can repair the partition headers and tables
    and expand stateful partition to all available free space.
    
    BUG=None
    TEST=manually test on my local device
    
    Change-Id: I76bdadf07ec863b930a6563996ed32a45fa23d21
    Reviewed-on: https://chromium-review.googlesource.com/1397822
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Wei-Han Chen <stimim@chromium.org>
    Reviewed-by: Wei-Han Chen <stimim@chromium.org>

--- py/test/pytests/partition_table.py
+++ py/test/pytests/partition_table.py
@@ -6,35 +6,59 @@
 """Checks that the partition table extends nearly to the end of the storage
 device.
 
-This ensures that the device was not accidentally pre-imaged with a image for
-a smaller storage device.
+Description
+-----------
+This test checks if the partition table allocates at least ``min_usage_pct``
+percent of the storage. If not, this test expands stateful patition to the end
+of the storage device by default.
 
-Note that:
+This test doesn't check the actual size of the stateful partition, rather the
+sector at which it ends.
 
-- We do *not* check the actual size of the stateful partition, rather
-  the sector at which it ends. If the stateful partition is not at the end,
-  consider adding a mode to this test to check its size rather than its
-  end sector.
+This test doesn't support remote device.
 
-- We check the entry in the partition table, not the size of the
-  filesystem itself, which may be significant smaller.
+Test Procedure
+--------------
+This is an automatic test that doesn't need any user interaction.
+
+Dependency
+----------
+- `pygpt` utility.
+
+Examples
+--------
+To run this pytest with default arguments, add this in test list::
+
+  {
+    "pytest_name": "partition_table"
+  }
+
+This is also predefined in ``generic_common.test_list.json`` as
+``PartitionTable``.
+
+If you can't expand stateful partition for some reason, override the argument
+by::
+
+  {
+    "inherit": "PartitionTable",
+    "args": {
+      "expand_stateful": false
+    }
+  }
 """
 
 
 import logging
 import os
-import re
-import unittest
 
 import factory_common  # pylint: disable=unused-import
 from cros.factory.device import device_utils
+from cros.factory.test import test_case
 from cros.factory.utils.arg_utils import Arg
-from cros.factory.utils import file_utils
-from cros.factory.utils import process_utils
-from cros.factory.utils import sys_utils
+from cros.factory.utils import pygpt
 
 
-class PartitionTableTest(unittest.TestCase):
+class PartitionTableTest(test_case.TestCase):
   ARGS = [
       Arg('min_usage_pct', (int, float),
           'Percentage of the storage device that must be before the end of the '
@@ -42,37 +66,47 @@ class PartitionTableTest(unittest.TestCase):
           'partition must end at a sector that is >=95% of the total number of '
           'sectors on the device.',
           default=95),
+      Arg('expand_stateful', bool,
+          'Repair partition headers and tables and expand stateful partition '
+          'to all available free space',
+          default=True)
   ]
 
-  longMessage = True
+  def setUp(self):
+    self.dut = device_utils.CreateDUTInterface()
+    self.gpt = None
 
   def runTest(self):
-    dev = device_utils.CreateDUTInterface().storage.GetMainStorageDevice()
-    stateful = process_utils.CheckOutput(['cgpt', 'find', '-l', 'STATE', dev],
-                                         log=True)
-
-    match = re.search(r'(\d+)$', stateful)
-    self.assertTrue(match,
-                    'Unable to determine partition number from %r' % stateful)
-    stateful_no = int(match.group(1))
-
-    partitions = sys_utils.PartitionManager(dev)
-    start_sector = partitions.GetPartitionOffsetInSector(stateful_no)
-    size_sectors = partitions.GetPartitionSizeInSector(stateful_no)
-    end_sector = start_sector + size_sectors
+    self.assertTrue(self.dut.link.IsLocal(),
+                    'This test only support local device')
+    dev = self.dut.storage.GetMainStorageDevice()
+    self.gpt = pygpt.GPT.LoadFromFile(dev)
+    stateful_no = self.dut.partitions.STATEFUL.index
+    stateful_part = self.gpt.GetPartition(stateful_no)
+    start_sector = stateful_part.FirstLBA
+    sector_count = stateful_part.blocks
+    end_sector = start_sector + sector_count
+    sector_size = self.gpt.block_size
 
     # Linux always considers sectors to be 512 bytes long independently of the
     # devices real block size.
     device_size = 512 * int(
-        file_utils.ReadFile('/sys/class/block/%s/size' % os.path.basename(dev)))
+        self.dut.ReadFile('/sys/class/block/%s/size' % os.path.basename(dev)))
 
-    pct_used = end_sector * partitions.GetSectorSize() * 100.0 / device_size
+    pct_used = end_sector * sector_size * 100.0 / device_size
 
     logging.info(
-        'start_sector=%d, size_sectors=%d, end_sector=%d, device_size=%d',
-        start_sector, size_sectors, end_sector, device_size)
+        'start_sector=%d, sector_count=%d, end_sector=%d, device_size=%d',
+        start_sector, sector_count, end_sector, device_size)
     logging.info('Stateful partition extends to %.3f%% of storage',
                  pct_used)
-    self.assertGreaterEqual(
-        pct_used, self.args.min_usage_pct,
-        'Stateful partition does not cover enough of storage device')
+    if pct_used < self.args.min_usage_pct:
+      if not self.args.expand_stateful:
+        self.FailTask('Stateful partition does not cover enough of storage '
+                      'device')
+
+      # Repair partition headers and tables
+      self.gpt.Resize(pygpt.GPT.GetImageSize(dev))
+
+      self.gpt.ExpandPartition(stateful_no)
+      self.gpt.WriteToFile(dev)
