a98e223d33352c246cbe7d25230a2b752704900e
ShihYing Chen <shihyingchen@chromium.org>
2019-03-08 21:48:34 +0800

    test_list_editor: frontend: Remove copyright.
    This change remove copyright footer.
    
    BUG=b:111771680
    TEST=npm run serve && npm run test
    
    Change-Id: I8f048bd351dfaa29a3f8da6ecc8d2ce865a1cef6
    Reviewed-on: https://chromium-review.googlesource.com/1511752
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: ShihYing Chen <shihyingchen@chromium.org>
    Reviewed-by: Youcheng Syu <youcheng@chromium.org>

--- py/test_list_editor/frontend/src/App.vue
+++ py/test_list_editor/frontend/src/App.vue
@@ -19,9 +19,6 @@
         </template>
       </v-container>
     </v-content>
-    <v-footer color="indigo" app>
-      <span class="white--text">&copy; 2018</span>
-    </v-footer>
   </v-app>
 </template>
 
