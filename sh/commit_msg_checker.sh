b63a175cf799af95894b08a64a0ead4301da0961
ShihYing Chen <shihyingchen@google.com>
2019-05-17 12:08:54 +0800

    factory: Add OEM names in commit message checker
    This CL adds OEM names into commit message checker. It's intended
    to be seperated from build stage. For the first batch, OEM
    data source is from Chromium OS Developer Information website.
    
    BUG=b:132592706
    TEST=Create commit with prohibited word and run 'repo upload .' and
    run EXPECT_SENSITIVE_OEMS=true sh/commit_msg_checker.sh
    
    Change-Id: I2bac4bfc49fa1f63babd1d92579abc44b8c88e79
    Reviewed-on: https://chromium-review.googlesource.com/1617141
    Commit-Ready: ShihYing Chen <shihyingchen@chromium.org>
    Tested-by: ShihYing Chen <shihyingchen@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Wei-Han Chen <stimim@chromium.org>
    Reviewed-by: ShihYing Chen <shihyingchen@chromium.org>

--- sh/commit_msg_checker.sh
+++ sh/commit_msg_checker.sh
@@ -9,10 +9,15 @@
 readonly commit_desc="$(git log --format=%s%n%n%b "${PRESUBMIT_COMMIT}^!")"
 readonly sensitive_build_stages=("PVT" "DVT" "EVT" "PROTO")
 
+# Data source: https://www.chromium.org/chromium-os/developer-information-for-chrome-os-devices
+readonly sensitive_oems=("ACER" "AOPEN" "ASUS" "CTL" "DELL" "HP" "LENOVO" "LG"
+  "PCMERGE" "POIN2" "SAMSUNG" "TOSHIBA" "VIEWSONIC")
+
 echo "EXPECT_SENSITIVE_BUILD_STAGES = ${EXPECT_SENSITIVE_BUILD_STAGES}"
+echo "EXPECT_SENSITIVE_OEMS = ${EXPECT_SENSITIVE_OEMS}"
 
 if [ "${EXPECT_SENSITIVE_BUILD_STAGES}" != "true" ]; then
-  for stage in ${sensitive_build_stages[@]}; do
+  for stage in "${sensitive_build_stages[@]}"; do
     grep_result=$(echo "${commit_desc}" | grep -wi "${stage}")
     if [[ ! -z "${grep_result}" ]]; then
       echo "Commit message contains sensitive word ${stage}, please fix it."
@@ -22,7 +27,21 @@ if [ "${EXPECT_SENSITIVE_BUILD_STAGES}" != "true" ]; then
     fi
   done
 else
-  echo "EXPECT_SENSITIVE_BUILD_STAGES is set to TRUE, skip build stage check."
+  echo "EXPECT_SENSITIVE_BUILD_STAGES is set to true, skip build stage check."
+fi
+
+if [ "${EXPECT_SENSITIVE_OEMS}" != "true" ]; then
+  for name in "${sensitive_oems[@]}"; do
+    grep_result="$(echo "${commit_desc}" | grep -wi "${name}")"
+    if [[ ! -z "${grep_result}" ]]; then
+      echo "Commit message contains sensitive word ${name}, please fix it."
+      echo "If your OEM name usage ${name} is reasonable,"\
+        "skip the check by EXPECT_SENSITIVE_OEMS=true repo upload"
+      exit 1
+    fi
+  done
+else
+  echo "EXPECT_SENSITIVE_OEMS is set to true, skip OEM name check."
 fi
 
 exit 0
