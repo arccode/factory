cbda47465c19f8b9cc1a20205842286275cc938a
Liang-Chieh Chen <jamesqaq@google.com>
2019-06-12 00:40:54 +0800

    bundle_creator: use the correct format in finalize_bundle
    Follow setup/BUNDLE.md to use the format {YYYYMMDD}_{phase} in
    finalize_bundle, so we can use the latest version of finalize_bundle
    without error in the future.
    
    BUG=none
    TEST=manually create a bundle
    
    Change-Id: I519e9ab223448f2b8aef37e97621f6c81a7d109d
    Reviewed-on: https://chromium-review.googlesource.com/1653548
    Tested-by: Liang-Chieh Chen <jamesqaq@chromium.org>
    Commit-Ready: Liang-Chieh Chen <jamesqaq@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>

--- py/bundle_creator/docker/util.py
+++ py/bundle_creator/docker/util.py
@@ -58,8 +58,8 @@ def CreateBundle(req):
 
   with file_utils.TempDirectory() as temp_dir:
     os.chdir(temp_dir)
-    bundle_name = '{:%Y%m%d_%H%M}_{}_{}'.format(
-        datetime.datetime.now(), req.phase, RandomString(6))
+    current_datetime = datetime.datetime.now()
+    bundle_name = '{:%Y%m%d}_{}'.format(current_datetime, req.phase)
 
     firmware_source = ('release_image/' + req.firmware_source
                        if req.HasField('firmware_source')
@@ -91,21 +91,22 @@ def CreateBundle(req):
 
     if process.wait() != 0:
       raise CreateBundleException(output)
-
-    bucket = storage_client.get_bucket(config.BUNDLE_BUCKET)
     bundle_filename = 'factory_bundle_{}_{}.tar.bz2'.format(
         req.project, bundle_name)
-    bundle_path = '{}/{}/{}'.format(req.board, req.project, bundle_filename)
-    blob = bucket.blob(bundle_path, chunk_size=100 * 1024 * 1024)
+    bucket = storage_client.get_bucket(config.BUNDLE_BUCKET)
+    blob_filename = 'factory_bundle_{}_{:%Y%m%d_%H%M}_{}_{}.tar.bz2'.format(
+        req.project, current_datetime, req.phase, RandomString(6))
+    blob_path = '{}/{}/{}'.format(req.board, req.project, blob_filename)
+    blob = bucket.blob(blob_path, chunk_size=100 * 1024 * 1024)
     # Set Content-Disposition for the correct default download filename
-    blob.content_disposition = 'filename="{}"'.format(bundle_filename)
+    blob.content_disposition = 'filename="{}"'.format(blob_filename)
     blob.upload_from_filename(bundle_filename)
     # Set read permission for the email of requestor, entity method here will
     # create a new acl entity and add it to blob.
     blob.acl.entity('user', req.email).grant_read()
     blob.acl.save()
 
-    gs_path = u'gs://{}/{}'.format(config.BUNDLE_BUCKET, bundle_path)
+    gs_path = u'gs://{}/{}'.format(config.BUNDLE_BUCKET, blob_path)
     # Since there is no way to modify metadata in blob class, use gsutil command
     # to set metadata
     metadata = {
