0c43e1f7ea5afc0b2cd80631535da526c5788c40
cyueh <cyueh@google.com>
2019-06-24 14:31:28 +0800

    probe: support probing firmware version of NVMe and SATA
    Previously in factory toolkit we only probe the firmware version of eMMC
    but not SATA or NVMe, and this CL implements the probe logic for them.
    Note that we currently do not want the firmware version to be part of
    the HWID.
    
    BUG=b:135661167
    TEST=manual
    
    Change-Id: Ida92167e0276b6a0dacb9ea3056d8cf202af08aa
    Reviewed-on: https://chromium-review.googlesource.com/1675405
    Tested-by: Cheng Yueh <cyueh@chromium.org>
    Commit-Ready: Cheng Yueh <cyueh@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Hong-Min Chu <hmchu@chromium.org>

--- py/probe/functions/generic_storage.py
+++ py/probe/functions/generic_storage.py
@@ -10,6 +10,7 @@ import string  # pylint: disable=deprecated-module
 import struct
 
 import factory_common  # pylint: disable=unused-import
+from cros.factory.probe.functions import file as file_module
 from cros.factory.probe.functions import sysfs
 from cros.factory.probe.lib import cached_probe_function
 from cros.factory.utils import file_utils
@@ -17,6 +18,7 @@ from cros.factory.utils import process_utils
 
 
 STORAGE_SYSFS_PATH = '/sys/class/block/*'
+SMARTCTL_PATH = '/usr/sbin/smartctl'
 
 
 def GetFixedDevices():
@@ -32,56 +34,36 @@ def GetFixedDevices():
     ret.append(node)
   return ret
 
-
-def GetEMMC5FirmwareVersion(node_path):
-  """Extracts eMMC 5.0 firmware version from EXT_CSD[254:261].
-
-  Args:
-    node_path: the node_path returned by GetFixedDevices(). For example,
-        '/sys/class/block/mmcblk0'.
-
-  Returns:
-    A string indicating the firmware version if firmware version is found.
-    Return None if firmware version doesn't present.
-  """
-  ext_csd = process_utils.GetLines(process_utils.SpawnOutput(
-      'mmc extcsd read /dev/%s' % os.path.basename(node_path),
-      shell=True, log=True))
+def CookVersion(raw_version):
+  """Returns human readable version string."""
+  version = ''.join('%02x' % c for c in raw_version)
   # The output for firmware version is encoded by hexdump of a ASCII
   # string or hexdump of hexadecimal values, always in 8 characters.
   # For example, version 'ABCDEFGH' is:
-  # [FIRMWARE_VERSION[261]]: 0x48
-  # [FIRMWARE_VERSION[260]]: 0x47
-  # [FIRMWARE_VERSION[259]]: 0x46
-  # [FIRMWARE_VERSION[258]]: 0x45
-  # [FIRMWARE_VERSION[257]]: 0x44
-  # [FIRMWARE_VERSION[256]]: 0x43
-  # [FIRMWARE_VERSION[255]]: 0x42
-  # [FIRMWARE_VERSION[254]]: 0x41
+  # [raw_version[7]]: 0x48
+  # [raw_version[6]]: 0x47
+  # [raw_version[5]]: 0x46
+  # [raw_version[4]]: 0x45
+  # [raw_version[3]]: 0x44
+  # [raw_version[2]]: 0x43
+  # [raw_version[1]]: 0x42
+  # [raw_version[0]]: 0x41
   #
   # Some vendors might use hexadecimal values for it.
   # For example, version 3 is:
-  # [FIRMWARE_VERSION[261]]: 0x00
-  # [FIRMWARE_VERSION[260]]: 0x00
-  # [FIRMWARE_VERSION[259]]: 0x00
-  # [FIRMWARE_VERSION[258]]: 0x00
-  # [FIRMWARE_VERSION[257]]: 0x00
-  # [FIRMWARE_VERSION[256]]: 0x00
-  # [FIRMWARE_VERSION[255]]: 0x00
-  # [FIRMWARE_VERSION[254]]: 0x03
+  # [raw_version[7]]: 0x00
+  # [raw_version[6]]: 0x00
+  # [raw_version[5]]: 0x00
+  # [raw_version[4]]: 0x00
+  # [raw_version[3]]: 0x00
+  # [raw_version[2]]: 0x00
+  # [raw_version[1]]: 0x00
+  # [raw_version[0]]: 0x03
   #
   # To handle both cases, this function returns a 64-bit hexadecimal value
   # and will try to decode it as a ASCII string or as a 64-bit little-endian
   # integer. It returns '4142434445464748 (ABCDEFGH)' for the first example
   # and returns '0300000000000000 (3)' for the second example.
-
-  pattern = re.compile(r'^\[FIRMWARE_VERSION\[(\d+)\]\]: (.*)$')
-  data = dict(m.groups() for m in map(pattern.match, ext_csd) if m)
-  if not data:
-    return None
-
-  raw_version = [int(data[str(i)], 0) for i in range(254, 262)]
-  version = ''.join(('%02x' % c for c in raw_version))
 
   # Try to decode it as a ASCII string.
   # Note vendor may choose SPACE (0x20) or NUL (0x00) to pad version string,
@@ -94,6 +76,49 @@ def GetEMMC5FirmwareVersion(node_path):
     version += ' (%s)' % struct.unpack_from('<q', version.decode('hex'))
   return version
 
+def GetStorageFirmwareVersion(node_path):
+  """Extracts firmware version.
+
+  Args:
+    node_path: the node_path returned by GetFixedDevices(). For example,
+        '/sys/class/block/mmcblk0'.
+
+  Returns:
+    A string indicating the firmware version if firmware version is found.
+    Return None if firmware version doesn't present.
+  """
+  dev_path = os.path.join(node_path, 'device')
+  # Use fwrev file (e.g., for eMMC)
+  emmc_fw_vr = file_module.ReadFile(os.path.join(dev_path, 'fwrev'))
+  if emmc_fw_vr:
+    # Cast a hex string to a 64-bit little-endian integer and then decode it to
+    # a list of 8 bytes.
+    # For exmaple, if emmc_fw_vr = '0x4142434445464748' then raw_version =
+    # [0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48]
+    raw_version = [((int(emmc_fw_vr, 0) >> ((7-i)*8)) & 255) for i in range(8)]
+    return CookVersion(raw_version)
+  # Use firmware_rev file (e.g., for NVMe)
+  # It is possible that the 8th byte is a space, and it will be stripped off if
+  # binary_mode is false.
+  nvme_fw_vr = file_module.ReadFile(os.path.join(dev_path, 'firmware_rev'),
+                                    binary_mode=True)
+  if nvme_fw_vr:
+    raw_version = [int(x, 0) for x in nvme_fw_vr.split(' ')[0:8]]
+    return CookVersion(raw_version)
+  # Use smartctl (e.g., for SATA)
+  sata_dev_path = os.path.join('/dev', os.path.basename(node_path))
+  smartctl = process_utils.SpawnOutput([SMARTCTL_PATH, '--all', sata_dev_path],
+                                       log=True)
+  matches = re.findall(r'(?m)^Firmware Version:\s+(.+)$', smartctl)
+  if matches:
+    cooked_version = matches[-1]
+    if re.search(r'(?m)^Device Model:\s+SanDisk', smartctl):
+      # Canonicalize SanDisk firmware versions by replacing 'CS' with '11'.
+      # According to b/35513546, 'CS's in firmware version of SanDisk are
+      # equivalent to '11's.
+      cooked_version = re.sub('^CS', '11', cooked_version)
+    return CookVersion(map(ord, cooked_version))
+  return None
 
 class GenericStorageFunction(cached_probe_function.CachedProbeFunction):
   """Probe the generic storage information."""
@@ -124,9 +149,9 @@ class GenericStorageFunction(cached_probe_function.CachedProbeFunction):
                             cls.EMMC_OPTIONAL_FIELDS))
     if not data:
       return None
-    emmc5_fw_ver = GetEMMC5FirmwareVersion(node_path)
-    if emmc5_fw_ver is not None:
-      data['emmc5_fw_ver'] = emmc5_fw_ver
+    fw_version = GetStorageFirmwareVersion(node_path)
+    if fw_version is not None:
+      data['fw_version'] = fw_version
     size_path = os.path.join(os.path.dirname(dev_path), 'size')
     data['sectors'] = (file_utils.ReadFile(size_path).strip()
                        if os.path.exists(size_path) else '')
