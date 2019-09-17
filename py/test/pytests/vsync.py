68ba2ce4458c58d275cb62083ecb094a5d618a9a
Fei Shao <fshao@chromium.org>
2019-09-17 17:51:36 +0800

    pytests: Refine vsync.py
    We now have proper VSync controller support from CL:1767165, so
    rewriting the manual part to adapt new device interface.
    
    BUG=None
    TEST=make test
    
    Change-Id: Ice96c5e5eecf501b1352dfc67bda35f625e85514
    Reviewed-on: https://chromium-review.googlesource.com/1809179
    Tested-by: Chun-Tsen Kuo <chuntsen@chromium.org>
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Wei-Han Chen <stimim@chromium.org>

--- py/test/pytests/vsync.py
+++ py/test/pytests/vsync.py
@@ -40,18 +40,12 @@ from cros.factory.utils import sync_utils
 from cros.factory.utils import type_utils
 
 
-DEFAULT_DEVICE_NAME = 'cros-ec-sync'
-DEFAULT_DEVICE_LOCATION = 'camera'
 DEFAULT_CAPTURE_NUMBER = 10
 DEFAULT_CAMERA_PATH = '/dev/camera-internal0'
 
 
 class SpatialSensorCalibration(test_case.TestCase):
   ARGS = [
-      Arg('device_name', str, 'The "name" atribute of the sensor.',
-          default=DEFAULT_DEVICE_NAME),
-      Arg('device_location', str, 'The "location" atribute of the sensor.',
-          default=DEFAULT_DEVICE_LOCATION),
       Arg('capture_number', int, 'The number of capture frames.',
           default=DEFAULT_CAPTURE_NUMBER),
       Arg('camera_path', str, 'The path of camera.',
@@ -64,30 +58,17 @@ class SpatialSensorCalibration(test_case.TestCase):
 
   def setUp(self):
     self._dut = device_utils.CreateDUTInterface()
-    self._device_path = None
+    self._vsync = self._dut.vsync_sensor.GetController()
 
     self.ui.ToggleTemplateClass('font-large', True)
 
-    for path in self._dut.Glob('/sys/bus/iio/devices/iio:device*'):
-      try:
-        name = self._dut.ReadFile(self._dut.path.join(path, 'name')).strip()
-        location = self._dut.ReadFile(
-            self._dut.path.join(path, 'location')).strip()
-      except Exception:
-        continue
-      if (name == self.args.device_name and
-          location == self.args.device_location):
-        if self._device_path is None:
-          self._device_path = path
-        else:
-          self.fail('failed to find a specified VSync device')
-
-    self.assertIsNotNone(self._device_path, '%s at %s not found' %
-                         (self.args.device_name, self.args.device_location))
+    self.assertIsNotNone(self._vsync, 'VSync controller not found')
 
   def runTest(self):
     self.WaitForDevice()
-    self.SetFrequency(1)
+    # The "frequency" entry of a VSync sensor is actually being used as an
+    # on/off switch, so this turns on the sensor.
+    self._vsync.SetFrequency(1)
     start_count = 0
     for idx in xrange(self.args.repeat_times):
       self.ui.SetState(
@@ -96,13 +77,14 @@ class SpatialSensorCalibration(test_case.TestCase):
       self._dut.CheckCall(
           ['yavta', '--capture=%d' % self.args.capture_number,
            self.args.camera_path])
-      end_count = self.GetInCount()
+      end_count = self._vsync.GetCount()
       session.console.info('VSync device in_count_raw (%d/%d): %d',
                            idx, self.args.repeat_times, end_count)
       if not start_count + self.args.capture_number <= end_count:
         self.fail('in_count_raw is not growing')
       start_count = end_count
-    self.SetFrequency(0)
+    # Turning off the sensor.
+    self._vsync.SetFrequency(0)
 
   def WaitForDevice(self):
     self.ui.SetState(_('Waiting for device...'))
@@ -110,12 +92,3 @@ class SpatialSensorCalibration(test_case.TestCase):
       sync_utils.WaitFor(self._dut.IsReady, self.args.timeout_secs)
     except type_utils.TimeoutError:
       self.fail('failed to find deivce')
-
-  def GetInCount(self):
-    return int(self._dut.ReadFile(
-        self._dut.path.join(self._device_path, 'in_count_raw')))
-
-  def SetFrequency(self, freq):
-    self._dut.WriteFile(
-        self._dut.path.join(self._device_path, 'frequency'),
-        str(freq))
