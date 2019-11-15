giggleby/shim

cros_payload: Add description payload.
Add a new payload `description` to store description of RMA shim in
cros_payload.

BUG=b:141155378
TEST=make test
CQ-DEPEND=CL:1844892

Change-Id: I42d647fbf0b716c822010e95f3dc6f9b26d811f0
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/1844891
Tested-by: Kevin Lin <kevinptt@chromium.org>
Reviewed-by: Cheng-Han Yang <chenghan@chromium.org>
Commit-Queue: Kevin Lin <kevinptt@chromium.org>
Auto-Submit: Kevin Lin <kevinptt@chromium.org>

py/tools/image_tool.py
@@ -86,7 +86,7 @@ DEFAULT_BLOCK_SIZE = pygpt.GPT.DEFAULT_BLOCK_SIZE
 # Components for cros_payload
 PAYLOAD_COMPONENTS = [
     'release_image', 'test_image',
-    'toolkit', 'firmware', 'hwid', 'complete', 'toolkit_config']
+    'toolkit', 'firmware', 'hwid', 'complete', 'toolkit_config', 'description']
 # Payload subtypes
 PAYLOAD_SUBTYPE_VERSION = 'version'
 
@@ -828,7 +828,7 @@ class ChromeOSFactoryBundle(object):
   def __init__(self, temp_dir, board, release_image, test_image, toolkit,
                factory_shim=None, enable_firmware=True, firmware=None,
                hwid=None, complete=None, netboot=None, toolkit_config=None,
-               setup_dir=None, server_url=None):
+               description=None, setup_dir=None, server_url=None):
     self._temp_dir = temp_dir
     # Member data will be looked up by getattr so we don't prefix with '_'.
     self._board = board
@@ -842,6 +842,7 @@ class ChromeOSFactoryBundle(object):
     self.complete = complete
     self.netboot = netboot
     self.toolkit_config = toolkit_config
+    self.description = description
     self.setup_dir = setup_dir
     self.server_url = server_url
 
@@ -896,6 +897,10 @@ class ChromeOSFactoryBundle(object):
           '--toolkit_config', dest='toolkit_config',
           default='-toolkit/*.json', type=ArgTypes.GlobPath,
           help='path to a config file to override test list constants')
+      parser.add_argument(
+          '--description', dest='description',
+          default='-description/*.txt', type=ArgTypes.GlobPath,
+          help='path to a plain text description file')
       parser.add_argument(
           '--board',
           help='board name for dynamic installation')
@@ -2105,7 +2110,8 @@ class CreateRMAImageCommmand(SubCommand):
           firmware=self.args.firmware,
           hwid=self.args.hwid,
           complete=self.args.complete,
-          toolkit_config=self.args.toolkit_config)
+          toolkit_config=self.args.toolkit_config,
+          description=self.args.description)
       bundle.CreateRMAImage(self.args.output)
       print('OK: Generated %s RMA image at %s' %
             (bundle.board, self.args.output))

arccode/factory

Cq-Depend: chromium:1900868
(cherry picked from commit 33793546b98e7045fb2b676eb7f9b15939f2d65d)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/1899029

py/tools/image_tool.py
@@ -101,7 +101,8 @@ PREFLASH_COMPONENTS = ['release_image', 'test_image', 'toolkit', 'hwid']
 # Components for cros_payload.
 PAYLOAD_COMPONENTS = [
     'release_image', 'test_image',
-    'toolkit', 'firmware', 'hwid', 'complete', 'toolkit_config', 'lsb_factory']
+    'toolkit', 'firmware', 'hwid', 'complete', 'toolkit_config', 'lsb_factory',
+    'description']
 # Payload types
 PAYLOAD_TYPE_TOOLKIT = 'toolkit'
 PAYLOAD_TYPE_TOOLKIT_CONFIG = 'toolkit_config'
@@ -1364,7 +1365,7 @@ class ChromeOSFactoryBundle(object):
   def __init__(self, temp_dir, board, release_image, test_image, toolkit,
                factory_shim=None, enable_firmware=True, firmware=None,
                hwid=None, complete=None, netboot=None, toolkit_config=None,
-               setup_dir=None, server_url=None):
+               description=None, setup_dir=None, server_url=None):
     self._temp_dir = temp_dir
     # Member data will be looked up by getattr so we don't prefix with '_'.
     self._board = board
@@ -1380,6 +1381,7 @@ class ChromeOSFactoryBundle(object):
     self.toolkit_config = toolkit_config
     # Always get lsb_factory from factory shim.
     self._lsb_factory = None
+    self.description = description
     self.setup_dir = setup_dir
     self.server_url = server_url
 
@@ -1475,6 +1477,11 @@ class ChromeOSFactoryBundle(object):
         default='-toolkit/*.json',
         type=ArgTypes.GlobPath,
         help='path to a config file to override test list constants')
+    parser.AddArgument(
+        (cls.RMA, cls.BUNDLE, cls.REPLACEABLE),
+        '--description', dest='description',
+        default='-description/*.txt', type=ArgTypes.GlobPath,
+        help='path to a plain text description file')
     parser.AddArgument(
         (cls.RMA, cls.BUNDLE),
         '--no-firmware',
@@ -2771,7 +2778,8 @@ class CreateRMAImageCommmand(SubCommand):
           firmware=self.args.firmware,
           hwid=self.args.hwid,
           complete=self.args.complete,
-          toolkit_config=self.args.toolkit_config)
+          toolkit_config=self.args.toolkit_config,
+          description=self.args.description)
       bundle.CreateRMAImage(self.args.output,
                             active_test_list=self.args.active_test_list)
       ChromeOSFactoryBundle.ShowRMAImage(output)