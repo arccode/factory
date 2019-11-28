a7758568e129992f3004172d97a57414542a313d
Meng-Huan Yu <menghuan@chromium.org>
2019-11-28 18:40:24 +0800

    factory_uninstall: Add factory_uninstall commands
    BUG=b:143200618
    TEST=manually test factory_uninstall on DUT
    TEST=make sure it will abort on non-CrOS env.
    
    Change-Id: I260db338c47ae82ff890ab508f156cc3fc95ae6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/1940020
    Reviewed-by: Meng-Huan Yu <menghuan@chromium.org>
    Tested-by: Meng-Huan Yu <menghuan@chromium.org>
    Commit-Queue: Meng-Huan Yu <menghuan@chromium.org>
    Auto-Submit: Meng-Huan Yu <menghuan@chromium.org>

---
+++ py/toolkit/uninstaller.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python2
+#
+# Copyright 2020 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Factory toolkit uninstaller.
+
+Remove all factory toolkit related files on CrOS device.
+"""
+
+from __future__ import print_function
+
+import argparse
+import os
+import sys
+
+from six.moves import input
+
+import factory_common  # pylint: disable=unused-import
+from cros.factory.test.env import paths
+from cros.factory.tools import install_symlinks
+from cros.factory.utils import file_utils
+from cros.factory.utils import log_utils
+from cros.factory.utils import process_utils
+from cros.factory.utils import sys_utils
+
+HELP_HEADER = """
+Uninstall the factory toolkit on a CrOS device.
+"""
+
+FACTORY_DIRS = ['/var/factory', paths.FACTORY_DIR]
+
+
+def AssertEnvironment():
+  if not sys_utils.InCrOSDevice():
+    raise Exception(
+        "ERROR: You're not on a CrOS device (for more details, please "
+        'check sys_utils.py:InCrOSDevice). The uninstaller only works on '
+        'CrOS device.')
+  if os.getuid() != 0:
+    raise Exception('You must be root to uninstall the factory toolkit on a '
+                    'CrOS device.')
+
+
+def MakeWarningMessage():
+  ret = file_utils.ReadFile(paths.FACTORY_TOOLKIT_VERSION_PATH)
+  ret += (
+      '\n'
+      '\n'
+      '*** You are about to uninstall the factory toolkit at:\n')
+
+  for p in FACTORY_DIRS:
+    ret += '***   %s\n' % p
+
+  ret += '***'
+
+  return ret
+
+
+def Main():
+  log_utils.InitLogging()
+
+  parser = argparse.ArgumentParser(
+      description=HELP_HEADER,
+      formatter_class=argparse.RawDescriptionHelpFormatter)
+  parser.add_argument('--yes', '-y', action='store_true',
+                      help="Don't ask for confirmation")
+
+  args = parser.parse_args()
+
+  AssertEnvironment()
+
+  print(MakeWarningMessage())
+  if not args.yes:
+    answer = input('*** Continue? [y/N] ')
+    if not answer or answer[0] not in 'yY':
+      sys.exit('Aborting.')
+
+  # To recover the symlinks under /usr/local/bin. We need to re-create the links
+  # to factory-mini.par.
+  install_symlinks.UninstallSymlinks('/usr/local/bin',
+                                     install_symlinks.MODE_FULL)
+  install_symlinks.InstallSymlinks('../factory-mini/factory-mini.par',
+                                   '/usr/local/bin', install_symlinks.MODE_MINI)
+
+  # Delete all factory related files.
+  for p in FACTORY_DIRS:
+    process_utils.Spawn(['rm', '-rf', p], check_call=True, log=True)
+
+
+if __name__ == '__main__':
+  Main()
