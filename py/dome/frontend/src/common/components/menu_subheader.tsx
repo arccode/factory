e1f1143f8cb85ace5b5cf07a7b27150740a4d878
ShihYing Chen <shihyingchen@chromium.org>
2020-01-13 17:44:58 +0800

    Dome: Remove materialv2 todo comment
    This CL removes todo comment for material v2.
    
    BUG=b:147211050
    TEST=npm run test
    
    Change-Id: I7738031d22b88cd69547e280e5890d2790edb0f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/1996971
    Tested-by: ShihYing Chen <shihyingchen@chromium.org>
    Auto-Submit: ShihYing Chen <shihyingchen@chromium.org>
    Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>

--- py/dome/frontend/src/common/components/menu_subheader.tsx
+++ py/dome/frontend/src/common/components/menu_subheader.tsx
@@ -16,8 +16,6 @@ import {Omit} from '@common/types';
 
 const styles = (theme: Theme) => createStyles({
   subtitle: {
-    // TODO(pihsun): Use new material UI typography preset after v2 is
-    // released. (https://github.com/mui-org/material-ui/issues/11667)
     ...theme.typography.subtitle1,
     color: theme.palette.grey[500],
     fontWeight: theme.typography.fontWeightMedium,
