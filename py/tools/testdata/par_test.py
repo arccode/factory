a328630cd370accd70269810ee41c7ea7a0da22f
Yilin Yang <kerker@google.com>
2020-01-16 16:59:11 +0800

    par_test: Add copyright
    BUG=chromium:999876
    TEST=presubmit check
    
    Change-Id: I58b0bd6bc8e93580902b3dff6fe445c18f3163cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/2006750
    Commit-Queue: Yilin Yang (kerker) <kerker@chromium.org>
    Tested-by: Yilin Yang (kerker) <kerker@chromium.org>
    Auto-Submit: Yilin Yang (kerker) <kerker@chromium.org>
    Reviewed-by: Meng-Huan Yu <menghuan@chromium.org>

--- py/tools/testdata/par_test.py
+++ py/tools/testdata/par_test.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python2
+# Copyright 2020 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
 """Test program for tiny_par_unittest.py."""
 
 
