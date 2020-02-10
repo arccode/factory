3fec3c2ceda9b08acacdae3b7eb73a57c37c7db8
Clark Chung <clarkchung@google.com>
2020-02-10 15:12:53 +0800

    hwid service: add health check in MemoryStore
    In order to keep the connection alive, hwid service should periodically
    ping redis server to prevent losing connection.
    
    BUG=b:149182969
    TEST=None
    
    Change-Id: If5b24e0956a520fa9be1430ea7d22e6a8d1cfc69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/2044262
    Reviewed-by: Yong Hong <yhong@chromium.org>
    Tested-by: Clark Chung <ckclark@chromium.org>
    Commit-Queue: Clark Chung <ckclark@chromium.org>

--- py/hwid/service/appengine/memcache_adaptor.py
+++ py/hwid/service/appengine/memcache_adaptor.py
@@ -38,7 +38,8 @@ class MemcacheAdaptor(object):
     self.namespace = namespace
     redis_host = os.environ.get('REDIS_HOST', 'localhost')
     redis_port = int(os.environ.get('REDIS_PORT', 6379))
-    self.client = redis.Redis(host=redis_host, port=redis_port)
+    self.client = redis.Redis(host=redis_host, port=redis_port,
+                              health_check_interval=30)
 
   def ClearAll(self):
     """Clear all items in cache.
