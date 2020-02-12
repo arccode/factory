c295a66bd55e6a3d45a0189b72536c28a5131456
Yilin Yang <kerker@google.com>
2020-02-12 16:04:43 +0800

    status_unittest: Migrate mox to mock
    `mox` is not supported in python3, we need to migrate to module `mock`.
    
    BUG=b:147990582
    TEST=status_unittest pass
    
    Change-Id: I33f7ebc8e4bed1a53d2112c3b09775d501f78c71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/2050156
    Commit-Queue: Yilin Yang (kerker) <kerker@chromium.org>
    Tested-by: Yilin Yang (kerker) <kerker@chromium.org>
    Reviewed-by: Cheng Yueh <cyueh@chromium.org>

--- py/device/status_unittest.py
+++ py/device/status_unittest.py
@@ -11,7 +11,7 @@
 import logging
 import unittest
 
-from mox3 import mox
+import mock
 
 from cros.factory.device import device_types
 from cros.factory.device import fan
@@ -23,35 +23,40 @@ from cros.factory.device import thermal
 class SystemStatusTest(unittest.TestCase):
   """Unittest for SystemStatus."""
 
-  def setUp(self):
-    self.mox = mox.Mox()
-
-  def tearDown(self):
-    self.mox.UnsetStubs()
-
   def runTest(self):
+    # Set up mocks for Board interface
+    board = mock.Mock(device_types.DeviceBoard)
+    board.power = mock.Mock(power.Power)
+    board.power.GetInfoDict.return_value = {}
+
+    # Make it raise exception, then charge_state will be None rather than
+    # mock object. If it's a mock object, it will make deepcopy fail.
+    # Check "battery" method in class SystemStatus.
+    board.power.GetChargeState.side_effect = IOError
+
+    board.fan = mock.Mock(fan.FanControl)
+    board.fan.GetFanRPM.return_value = [2000]
+
+    board.thermal = mock.Mock(thermal.Thermal)
+    board.thermal.GetTemperature.return_value = 37
+
     # Set up mocks for netifaces.
-    netifaces = status_module.netifaces = self.mox.CreateMockAnything()
+    def IfaddressesSideEffect(*args, **unused_kwargs):
+      if args[0] == 'eth0':
+        return {netifaces.AF_INET6: [{'addr': 'aa:aa:aa:aa:aa:aa'}],
+                netifaces.AF_INET: [{'broadcast': '192.168.1.255',
+                                     'addr': '192.168.1.100'}]}
+      elif args[0] == 'wlan0':
+        return {netifaces.AF_INET: [{'addr': '192.168.16.100'},
+                                    {'addr': '192.168.16.101'}]}
+      return None
+
+    netifaces = status_module.netifaces = mock.MagicMock()
     netifaces.AF_INET = 2
     netifaces.AF_INET6 = 10
 
-    # Set up mocks for Board interface
-    board = self.mox.CreateMock(device_types.DeviceBoard)
-    board.power = self.mox.CreateMock(power.Power)
-    board.power.GetInfoDict().AndReturn({})
-    board.fan = self.mox.CreateMock(fan.FanControl)
-    board.fan.GetFanRPM().AndReturn([2000])
-    board.thermal = self.mox.CreateMock(thermal.Thermal)
-    board.thermal.GetTemperature().AndReturn(37)
-    netifaces.interfaces().AndReturn(['lo', 'eth0', 'wlan0'])
-    netifaces.ifaddresses('eth0').AndReturn(
-        {netifaces.AF_INET6: [{'addr': 'aa:aa:aa:aa:aa:aa'}],
-         netifaces.AF_INET: [{'broadcast': '192.168.1.255',
-                              'addr': '192.168.1.100'}]})
-    netifaces.ifaddresses('wlan0').AndReturn(
-        {netifaces.AF_INET: [{'addr': '192.168.16.100'},
-                             {'addr': '192.168.16.101'}]})
-    self.mox.ReplayAll()
+    netifaces.interfaces.return_value = ['lo', 'eth0', 'wlan0']
+    netifaces.ifaddresses.side_effect = IfaddressesSideEffect
 
     status = status_module.SystemStatus(board).Snapshot()
 
@@ -62,8 +67,6 @@ class SystemStatusTest(unittest.TestCase):
     self.assertEqual(
         'eth0=192.168.1.100, wlan0=192.168.16.100+192.168.16.101',
         status.ips)
-
-    self.mox.VerifyAll()
 
 
 if __name__ == '__main__':
