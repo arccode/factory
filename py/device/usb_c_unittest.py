5b053f9666fc4b20be538d621e54887b54aaa821
Yilin Yang <kerker@google.com>
2020-02-12 11:44:37 +0800

    usb_c_unittest: Migrate mox to mock
    `mox` is not supported in python3, we need to migrate to module `mock`.
    
    BUG=b:147990582
    TEST=usb_c_unittest pass
    
    Change-Id: Ibeae12a8a0c97ad0c87d6814636bdde442b08c77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/2050149
    Reviewed-by: Yilin Yang (kerker) <kerker@chromium.org>
    Tested-by: Yilin Yang (kerker) <kerker@chromium.org>
    Commit-Queue: Yilin Yang (kerker) <kerker@chromium.org>

--- py/device/usb_c_unittest.py
+++ py/device/usb_c_unittest.py
@@ -10,7 +10,7 @@ from __future__ import print_function
 
 import unittest
 
-import mox
+import mock
 
 from cros.factory.device import device_types
 from cros.factory.device import usb_c
@@ -24,60 +24,41 @@ class USBTypeCTest(unittest.TestCase):
   """Unittest for USBTypeC."""
 
   def setUp(self):
-    self.mox = mox.Mox()
-    self.board = self.mox.CreateMock(device_types.DeviceBoard)
+    self.board = mock.Mock(device_types.DeviceBoard)
     self.usb_c = MockUSBTypeC(self.board)
 
-  def tearDown(self):
-    self.mox.UnsetStubs()
-
   def testGetPDVersion(self):
-    self.board.CallOutput(
-        ['mosys', 'pd', 'info', '-s', 'fw_version']).AndReturn(
-            'samus_pd_v1.1.2122-e1ff1a3')
-    self.mox.ReplayAll()
+    self.board.CallOutput.return_value = 'samus_pd_v1.1.2122-e1ff1a3'
     self.assertEqual('samus_pd_v1.1.2122-e1ff1a3', self.usb_c.GetPDVersion())
-    self.mox.VerifyAll()
+    self.board.CallOutput.assert_called_once_with(
+        ['mosys', 'pd', 'info', '-s', 'fw_version'])
 
   def testGetPDStatusV0(self):
-    self.board.CheckOutput(
-        ['ectool', '--interface=dev', '--dev=1', 'usbpd', '0']).AndReturn(
-            'Port C0 is enabled, Role:SRC Polarity:CC1 State:8')
-    self.board.CheckOutput(
-        ['ectool', '--interface=dev', '--dev=1', 'usbpd', '1']).AndReturn(
-            'Port C1 is disabled, Role:SNK Polarity:CC2 State:11')
-
-    self.mox.ReplayAll()
+    self.board.CheckOutput.side_effect = [
+        'Port C0 is enabled, Role:SRC Polarity:CC1 State:8',
+        'Port C1 is disabled, Role:SNK Polarity:CC2 State:11']
 
     status = self.usb_c.GetPDStatus(0)
     self.assertTrue(status['enabled'])
     self.assertEqual('SRC', status['role'])
     self.assertEqual('CC1', status['polarity'])
     self.assertEqual(8, status['state'])
+    self.board.CheckOutput.assert_called_with(
+        ['ectool', '--interface=dev', '--dev=1', 'usbpd', '0'])
 
     status = self.usb_c.GetPDStatus(1)
     self.assertFalse(status['enabled'])
     self.assertEqual('SNK', status['role'])
     self.assertEqual('CC2', status['polarity'])
     self.assertEqual(11, status['state'])
-
-    self.mox.VerifyAll()
+    self.board.CheckOutput.assert_called_with(
+        ['ectool', '--interface=dev', '--dev=1', 'usbpd', '1'])
 
   def testGetPDStatusV1(self):
-    self.board.CheckOutput(
-        ['ectool', '--interface=dev', '--dev=1', 'usbpd', '0']).AndReturn(
-            'Port C0 is enabled, Role:SRC UFP Polarity:CC1 State:SRC_READY')
-    self.board.CheckOutput(
-        ['ectool', '--interface=dev', '--dev=1', 'usbpd', '1']
-    ).AndReturn(
-        'Port C1 is disabled, Role:SNK DFP Polarity:CC2 State:SNK_DISCOVERY')
-    # Empty return value of State
-    self.board.CheckOutput(
-        ['ectool', '--interface=dev', '--dev=1', 'usbpd', '1']
-    ).AndReturn(
-        'Port C1 is disabled, Role:SNK DFP Polarity:CC2 State:')
-
-    self.mox.ReplayAll()
+    self.board.CheckOutput.side_effect = [
+        'Port C0 is enabled, Role:SRC UFP Polarity:CC1 State:SRC_READY',
+        'Port C1 is disabled, Role:SNK DFP Polarity:CC2 State:SNK_DISCOVERY',
+        'Port C1 is disabled, Role:SNK DFP Polarity:CC2 State:']
 
     status = self.usb_c.GetPDStatus(0)
     self.assertTrue(status['enabled'])
@@ -85,6 +66,8 @@ class USBTypeCTest(unittest.TestCase):
     self.assertEqual('UFP', status['datarole'])
     self.assertEqual('CC1', status['polarity'])
     self.assertEqual('SRC_READY', status['state'])
+    self.board.CheckOutput.assert_called_with(
+        ['ectool', '--interface=dev', '--dev=1', 'usbpd', '0'])
 
     status = self.usb_c.GetPDStatus(1)
     self.assertFalse(status['enabled'])
@@ -92,6 +75,9 @@ class USBTypeCTest(unittest.TestCase):
     self.assertEqual('DFP', status['datarole'])
     self.assertEqual('CC2', status['polarity'])
     self.assertEqual('SNK_DISCOVERY', status['state'])
+    self.board.CheckOutput.assert_called_with(
+        ['ectool', '--interface=dev', '--dev=1', 'usbpd', '1'])
+    self.board.CheckOutput.reset_mock()
 
     status = self.usb_c.GetPDStatus(1)
     self.assertFalse(status['enabled'])
@@ -99,25 +85,16 @@ class USBTypeCTest(unittest.TestCase):
     self.assertEqual('DFP', status['datarole'])
     self.assertEqual('CC2', status['polarity'])
     self.assertEqual('', status['state'])
-
-    self.mox.VerifyAll()
+    self.board.CheckOutput.assert_called_with(
+        ['ectool', '--interface=dev', '--dev=1', 'usbpd', '1'])
 
   def testGetPDStatusV1_1(self):
-    self.board.CheckOutput(
-        ['ectool', '--interface=dev', '--dev=1', 'usbpd', '0']).AndReturn(
-            'Port C0 is enabled,connected, Role:SRC UFP Polarity:CC1 '
-            'State:SRC_READY')
-    self.board.CheckOutput(
-        ['ectool', '--interface=dev', '--dev=1', 'usbpd', '1']).AndReturn(
-            'Port C1 is disabled,disconnected, Role:SNK DFP Polarity:CC2 '
-            'State:SNK_DISCOVERY')
-    # Empty return value of State
-    self.board.CheckOutput(
-        ['ectool', '--interface=dev', '--dev=1', 'usbpd', '1']).AndReturn(
-            'Port C1 is disabled,disconnected, Role:SNK DFP Polarity:CC2 '
-            'State:')
-
-    self.mox.ReplayAll()
+    self.board.CheckOutput.side_effect = [
+        'Port C0 is enabled,connected, Role:SRC UFP Polarity:CC1 '
+        'State:SRC_READY',
+        'Port C1 is disabled,disconnected, Role:SNK DFP Polarity:CC2 '
+        'State:SNK_DISCOVERY',
+        'Port C1 is disabled,disconnected, Role:SNK DFP Polarity:CC2 State:']
 
     status = self.usb_c.GetPDStatus(0)
     self.assertTrue(status['enabled'])
@@ -126,6 +103,8 @@ class USBTypeCTest(unittest.TestCase):
     self.assertEqual('UFP', status['datarole'])
     self.assertEqual('CC1', status['polarity'])
     self.assertEqual('SRC_READY', status['state'])
+    self.board.CheckOutput.assert_called_with(
+        ['ectool', '--interface=dev', '--dev=1', 'usbpd', '0'])
 
     status = self.usb_c.GetPDStatus(1)
     self.assertFalse(status['enabled'])
@@ -134,6 +113,9 @@ class USBTypeCTest(unittest.TestCase):
     self.assertEqual('DFP', status['datarole'])
     self.assertEqual('CC2', status['polarity'])
     self.assertEqual('SNK_DISCOVERY', status['state'])
+    self.board.CheckOutput.assert_called_with(
+        ['ectool', '--interface=dev', '--dev=1', 'usbpd', '1'])
+    self.board.CheckOutput.reset_mock()
 
     status = self.usb_c.GetPDStatus(1)
     self.assertFalse(status['enabled'])
@@ -142,30 +124,19 @@ class USBTypeCTest(unittest.TestCase):
     self.assertEqual('DFP', status['datarole'])
     self.assertEqual('CC2', status['polarity'])
     self.assertEqual('', status['state'])
-
-    self.mox.VerifyAll()
+    self.board.CheckOutput.assert_called_with(
+        ['ectool', '--interface=dev', '--dev=1', 'usbpd', '1'])
 
   def testGetPDStatusV1_2(self):
-    self.board.CheckOutput(
-        ['ectool', '--interface=dev', '--dev=1', 'usbpd', '0']).AndReturn(
-            'Port C0: enabled, connected  State:SRC_READY\n'
-            'Role:SRC UFP, '
-            'Polarity:CC1')
-    self.board.CheckOutput(
-        ['ectool', '--interface=dev', '--dev=1', 'usbpd', '1']).AndReturn(
-            'Port C1: enabled, connected  State:SNK_DISCOVERY\n'
-            'Role:SRC DFP VCONN, Polarity:CC1')
-    self.board.CheckOutput(
-        ['ectool', '--interface=dev', '--dev=1', 'usbpd', '1']).AndReturn(
-            'Port C1: disabled, disconnected  State:SNK_DISCONNECTED\n'
-            'Role:SNK DFP, Polarity:CC2')
-    # Empty return value of State
-    self.board.CheckOutput(
-        ['ectool', '--interface=dev', '--dev=1', 'usbpd', '1']).AndReturn(
-            'Port C1: disabled, disconnected  State:\n'
-            'Role:SNK DFP, Polarity:CC2')
-
-    self.mox.ReplayAll()
+    self.board.CheckOutput.side_effect = [
+        'Port C0: enabled, connected  State:SRC_READY\n'
+        'Role:SRC UFP, Polarity:CC1',
+        'Port C1: enabled, connected  State:SNK_DISCOVERY\n'
+        'Role:SRC DFP VCONN, Polarity:CC1',
+        'Port C1: disabled, disconnected  State:SNK_DISCONNECTED\n'
+        'Role:SNK DFP, Polarity:CC2',
+        'Port C1: disabled, disconnected  State:\n'
+        'Role:SNK DFP, Polarity:CC2']
 
     status = self.usb_c.GetPDStatus(0)
     self.assertTrue(status['enabled'])
@@ -175,6 +146,8 @@ class USBTypeCTest(unittest.TestCase):
     self.assertEqual('', status['vconn'])
     self.assertEqual('CC1', status['polarity'])
     self.assertEqual('SRC_READY', status['state'])
+    self.board.CheckOutput.assert_called_with(
+        ['ectool', '--interface=dev', '--dev=1', 'usbpd', '0'])
 
     status = self.usb_c.GetPDStatus(1)
     self.assertTrue(status['enabled'])
@@ -184,6 +157,9 @@ class USBTypeCTest(unittest.TestCase):
     self.assertEqual('VCONN', status['vconn'])
     self.assertEqual('CC1', status['polarity'])
     self.assertEqual('SNK_DISCOVERY', status['state'])
+    self.board.CheckOutput.assert_called_with(
+        ['ectool', '--interface=dev', '--dev=1', 'usbpd', '1'])
+    self.board.CheckOutput.reset_mock()
 
     status = self.usb_c.GetPDStatus(1)
     self.assertFalse(status['enabled'])
@@ -193,6 +169,9 @@ class USBTypeCTest(unittest.TestCase):
     self.assertEqual('', status['vconn'])
     self.assertEqual('CC2', status['polarity'])
     self.assertEqual('SNK_DISCONNECTED', status['state'])
+    self.board.CheckOutput.assert_called_with(
+        ['ectool', '--interface=dev', '--dev=1', 'usbpd', '1'])
+    self.board.CheckOutput.reset_mock()
 
     status = self.usb_c.GetPDStatus(1)
     self.assertFalse(status['enabled'])
@@ -202,8 +181,8 @@ class USBTypeCTest(unittest.TestCase):
     self.assertEqual('', status['vconn'])
     self.assertEqual('CC2', status['polarity'])
     self.assertEqual('', status['state'])
-
-    self.mox.VerifyAll()
+    self.board.CheckOutput.assert_called_with(
+        ['ectool', '--interface=dev', '--dev=1', 'usbpd', '1'])
 
   def testGetPDStatusV2(self):
     self.board.CheckOutput(
@@ -276,12 +255,9 @@ class USBTypeCTest(unittest.TestCase):
     self.mox.VerifyAll()
 
   def testGetPDPowerStatus(self):
-    self.board.CheckOutput(
-        ['ectool', '--interface=dev', '--dev=1', 'usbpdpower']).AndReturn(
-            'Port 0: SNK Charger PD 14384mV / 2999mA, max 15000mV / 3000mA / '
-            '45000mW\nPort 1: Disconnected')
-
-    self.mox.ReplayAll()
+    self.board.CheckOutput.side_effect = [
+        'Port 0: SNK Charger PD 14384mV / 2999mA, max 15000mV / 3000mA / '
+        '45000mW\nPort 1: Disconnected']
 
     status = self.usb_c.GetPDPowerStatus()
     self.assertTrue(status[0])
@@ -294,44 +270,39 @@ class USBTypeCTest(unittest.TestCase):
     self.assertEqual(status[0]['max_millivolt'], 15000)
     self.assertEqual(status[0]['max_milliampere'], 3000)
     self.assertEqual(status[0]['max_milliwatt'], 45000)
-
-    self.mox.VerifyAll()
+    self.board.CheckOutput.assert_called_once_with(
+        ['ectool', '--interface=dev', '--dev=1', 'usbpdpower'])
 
   def testSetHPD(self):
-    self.board.CheckOutput(
+    self.usb_c.SetHPD(0)
+    self.board.CheckOutput.assert_called_once_with(
         ['ectool', '--interface=dev', '--dev=1', 'gpioset', 'USB_C0_DP_HPD',
          '1'])
-    self.mox.ReplayAll()
-    self.usb_c.SetHPD(0)
-    self.mox.VerifyAll()
 
   def testResetHPD(self):
-    self.board.CheckOutput(
+    self.usb_c.ResetHPD(1)
+    self.board.CheckOutput.assert_called_once_with(
         ['ectool', '--interface=dev', '--dev=1', 'gpioset', 'USB_C1_DP_HPD',
          '0'])
-    self.mox.ReplayAll()
-    self.usb_c.ResetHPD(1)
-    self.mox.VerifyAll()
 
   def testSetPortFunction(self):
-    self.board.CheckOutput(
-        ['ectool', '--interface=dev', '--dev=1', 'usbpd', '0', 'dp'])
-    self.mox.ReplayAll()
     self.usb_c.SetPortFunction(0, 'dp')
-    self.mox.VerifyAll()
+    self.board.CheckOutput.assert_called_once_with(
+        ['ectool', '--interface=dev', '--dev=1', 'usbpd', '0', 'dp'])
 
   def testSetPortFunctionFail(self):
     self.assertRaises(device_types.DeviceException, self.usb_c.SetPortFunction,
                       0, 'display')
 
   def testResetPortFunction(self):
-    self.board.CheckOutput(
-        ['ectool', '--interface=dev', '--dev=1', 'usbpd', '1', 'toggle'])
-    self.board.CheckOutput(
-        ['ectool', '--interface=dev', '--dev=1', 'usbpd', '1', 'usb'])
-    self.mox.ReplayAll()
+    check_output_calls = [
+        mock.call(['ectool', '--interface=dev', '--dev=1', 'usbpd', '1',
+                   'toggle']),
+        mock.call(['ectool', '--interface=dev', '--dev=1', 'usbpd', '1',
+                   'usb'])]
+
     self.usb_c.ResetPortFunction(1)
-    self.mox.VerifyAll()
+    self.assertEqual(self.board.CheckOutput.call_args_list, check_output_calls)
 
 if __name__ == '__main__':
   unittest.main()
