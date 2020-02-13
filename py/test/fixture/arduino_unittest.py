b790c466149e077cad8373e8b56e15c1b3de6213
Yilin Yang <kerker@google.com>
2020-02-13 15:53:15 +0800

    arduino_unittest: Migrate mox to mock
    `mox` is not supported in python3, we need to migrate to module `mock`.
    
    BUG=b:147990582
    TEST=arduino_unittest pass
    
    Change-Id: Ib1bb69d94173c7676773631d687ed6a1715b299d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/2053662
    Commit-Queue: Yilin Yang (kerker) <kerker@chromium.org>
    Tested-by: Yilin Yang (kerker) <kerker@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Auto-Submit: Yilin Yang (kerker) <kerker@chromium.org>

--- py/test/fixture/arduino_unittest.py
+++ py/test/fixture/arduino_unittest.py
@@ -6,15 +6,13 @@
 
 """Unittest for serial_utils."""
 
-import time
 import unittest
 
-import mox
+import mock
 import serial
 
 import factory_common  # pylint: disable=unused-import
 from cros.factory.test.fixture import arduino
-from cros.factory.test.utils import serial_utils
 
 _DEFAULT_DRIVER = 'cdc_acm'
 _DEFAULT_READY_DELAY_SECS = 2.0
@@ -25,99 +23,103 @@ _DEFAULT_READY_DELAY_SECS = 2.0
 class ArduinoControllerTest(unittest.TestCase):
 
   def setUp(self):
-    self.mox = mox.Mox()
     self.device = arduino.ArduinoController()
 
   def tearDown(self):
     del self.device
-    self.mox.UnsetStubs()
-    self.mox.VerifyAll()
 
-  def StubOutForConnect(self):
-    self.mox.StubOutWithMock(serial_utils.SerialDevice, 'Connect')
-    self.mox.StubOutWithMock(time, 'sleep')
-    self.mox.StubOutWithMock(self.device, 'Ping')
+  @mock.patch('time.sleep')
+  @mock.patch('cros.factory.test.utils.serial_utils.SerialDevice.Connect')
+  def testConnectDefault(self, connect_mock, sleep_mock):
+    self.device.Ping = mock.Mock(return_value=True)
 
-  def testConnectDefault(self):
-    self.StubOutForConnect()
-    serial_utils.SerialDevice.Connect(driver=_DEFAULT_DRIVER)
-    time.sleep(_DEFAULT_READY_DELAY_SECS)
-    self.device.Ping().AndReturn(True)
-
-    self.mox.ReplayAll()
     self.device.Connect()
 
-  def testCustomReadyDelay(self):
+    connect_mock.assert_called_once_with(driver=_DEFAULT_DRIVER)
+    sleep_mock.assert_called_once_with(_DEFAULT_READY_DELAY_SECS)
+    self.device.Ping.assert_called_once_with()
+
+  @mock.patch('time.sleep')
+  @mock.patch('cros.factory.test.utils.serial_utils.SerialDevice.Connect')
+  def testCustomReadyDelay(self, connect_mock, sleep_mock):
     ready_delay_secs = 0.5
     self.device = arduino.ArduinoController(ready_delay_secs=ready_delay_secs)
-    self.StubOutForConnect()
-    serial_utils.SerialDevice.Connect(driver=_DEFAULT_DRIVER)
-    time.sleep(ready_delay_secs)
-    self.device.Ping().AndReturn(True)
+    self.device.Ping = mock.Mock(return_value=True)
 
-    self.mox.ReplayAll()
     self.device.Connect()
 
-  def testCustomDriver(self):
-    custom_driver = 'CustomDriver'
-    self.StubOutForConnect()
-    serial_utils.SerialDevice.Connect(driver=custom_driver)
-    time.sleep(_DEFAULT_READY_DELAY_SECS)
-    self.device.Ping().AndReturn(True)
+    connect_mock.assert_called_once_with(driver=_DEFAULT_DRIVER)
+    sleep_mock.assert_called_once_with(ready_delay_secs)
+    self.device.Ping.assert_called_once_with()
 
-    self.mox.ReplayAll()
+  @mock.patch('time.sleep')
+  @mock.patch('cros.factory.test.utils.serial_utils.SerialDevice.Connect')
+  def testCustomDriver(self, connect_mock, sleep_mock):
+    custom_driver = 'CustomDriver'
+    self.device.Ping = mock.Mock(return_value=True)
+
     self.device.Connect(driver=custom_driver)
 
-  def testConnectPingFailed(self):
-    self.StubOutForConnect()
-    serial_utils.SerialDevice.Connect(driver=_DEFAULT_DRIVER)
-    time.sleep(_DEFAULT_READY_DELAY_SECS)
-    self.device.Ping().AndReturn(False)
+    connect_mock.assert_called_once_with(driver=custom_driver)
+    sleep_mock.assert_called_once_with(_DEFAULT_READY_DELAY_SECS)
+    self.device.Ping.assert_called_once_with()
 
-    self.mox.ReplayAll()
+  @mock.patch('time.sleep')
+  @mock.patch('cros.factory.test.utils.serial_utils.SerialDevice.Connect')
+  def testConnectPingFailed(self, connect_mock, sleep_mock):
+    self.device.Ping = mock.Mock(return_value=False)
+
     self.assertRaises(serial.SerialException, self.device.Connect)
 
-  def testPing(self):
-    self.mox.StubOutWithMock(self.device, 'SendExpectReceive')
-    self.device.SendExpectReceive(chr(1), chr(1), retry=0).AndReturn(True)
-    self.device.SendExpectReceive(chr(2), chr(2)).AndReturn(True)
-    self.device.SendExpectReceive(chr(3), chr(3)).AndReturn(True)
+    connect_mock.assert_called_once_with(driver=_DEFAULT_DRIVER)
+    sleep_mock.assert_called_once_with(_DEFAULT_READY_DELAY_SECS)
+    self.device.Ping.assert_called_once_with()
 
-    self.mox.ReplayAll()
+  def testPing(self):
+    self.device.SendExpectReceive = mock.Mock(side_effect=[True, True, True])
+    send_expect_receive_calls = [
+        mock.call(chr(1), chr(1), retry=0),
+        mock.call(chr(2), chr(2)),
+        mock.call(chr(3), chr(3))]
+
     self.assertTrue(self.device.Ping())
 
-  def testPingFail(self):
-    self.mox.StubOutWithMock(self.device, 'SendExpectReceive')
-    self.device.SendExpectReceive(chr(1), chr(1), retry=0).AndReturn(True)
-    self.device.SendExpectReceive(chr(2), chr(2)).AndReturn(False)
+    self.assertEqual(self.device.SendExpectReceive.call_args_list,
+                     send_expect_receive_calls)
 
-    self.mox.ReplayAll()
+  def testPingFail(self):
+    self.device.SendExpectReceive = mock.Mock(side_effect=[True, False])
+    send_expect_receive_calls = [
+        mock.call(chr(1), chr(1), retry=0),
+        mock.call(chr(2), chr(2))]
+
     self.assertFalse(self.device.Ping())
 
-  def testReset(self):
-    mock_serial = self.mox.CreateMock(serial.Serial)
-    self.mox.StubOutWithMock(time, 'sleep')
-    self.device._serial = mock_serial  # pylint: disable=protected-access
-    mock_serial.setDTR(False)
-    time.sleep(0.05)
-    mock_serial.setDTR(True)
-    time.sleep(_DEFAULT_READY_DELAY_SECS)
-    mock_serial.close()
+    self.assertEqual(self.device.SendExpectReceive.call_args_list,
+                     send_expect_receive_calls)
 
-    self.mox.ReplayAll()
+  @mock.patch('time.sleep')
+  def testReset(self, sleep_mock):
+    serial_mock = mock.Mock(serial.Serial)
+    self.device._serial = serial_mock  # pylint: disable=protected-access
+    serial_setDTR_calls = [mock.call(False), mock.call(True)]
+    sleep_calls = [mock.call(0.05), mock.call(_DEFAULT_READY_DELAY_SECS)]
+
     self.device.Reset()
 
-  def testResetNoWait(self):
-    mock_serial = self.mox.CreateMock(serial.Serial)
-    self.mox.StubOutWithMock(time, 'sleep')
-    self.device._serial = mock_serial  # pylint: disable=protected-access
-    mock_serial.setDTR(False)
-    time.sleep(0.05)
-    mock_serial.setDTR(True)
-    mock_serial.close()
+    self.assertEqual(serial_mock.setDTR.call_args_list, serial_setDTR_calls)
+    self.assertEqual(sleep_mock.call_args_list, sleep_calls)
 
-    self.mox.ReplayAll()
+  @mock.patch('time.sleep')
+  def testResetNoWait(self, sleep_mock):
+    serial_mock = mock.Mock(serial.Serial)
+    self.device._serial = serial_mock  # pylint: disable=protected-access
+    serial_setDTR_calls = [mock.call(False), mock.call(True)]
+
     self.device.Reset(wait_ready=False)
+
+    self.assertEqual(serial_mock.setDTR.call_args_list, serial_setDTR_calls)
+    sleep_mock.assert_called_once_with(0.05)
 
 
 if __name__ == '__main__':
