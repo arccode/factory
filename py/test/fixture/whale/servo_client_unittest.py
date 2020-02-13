524d4cb5d5f60be01138a60b09d4babfa370145c
Yilin Yang <kerker@google.com>
2020-02-13 15:32:16 +0800

    servo_client_unittest: Migrate mox to mock
    `mox` is not supported in python3, we need to migrate to module `mock`.
    
    BUG=b:147990582
    TEST=servo_client_unittest pass
    
    Change-Id: If7876df2e61ba569124fcbe5c15b0b5965472413
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/2053661
    Tested-by: Yilin Yang (kerker) <kerker@chromium.org>
    Commit-Queue: Yilin Yang (kerker) <kerker@chromium.org>
    Reviewed-by: Kevin Lin <kevinptt@chromium.org>

--- py/test/fixture/whale/servo_client_unittest.py
+++ py/test/fixture/whale/servo_client_unittest.py
@@ -8,7 +8,7 @@
 
 import unittest
 
-import mox
+import mock
 
 from cros.factory.test.fixture.whale import servo_client
 
@@ -16,106 +16,108 @@ from cros.factory.test.fixture.whale import servo_client
 class ServoClientTest(unittest.TestCase):
 
   def setUp(self):
-    self.mox = mox.Mox()
-    self.server_mock = self.mox.CreateMockAnything()
+    self.server_mock = mock.MagicMock()
     self.client = servo_client.ServoClient(host='127.0.0.1', port='9999')
     self.client.__setattr__('_server', self.server_mock)
 
-  def tearDown(self):
-    self.mox.UnsetStubs()
-    self.mox.VerifyAll()
-
   def testGet(self):
-    self.server_mock.get('dev1').AndReturn('on')
-    self.server_mock.get('dev2').AndReturn('1')
-    self.server_mock.get('dev_unknown').AndRaise(Exception('no such dev'))
-    self.mox.ReplayAll()
-
+    self.server_mock.get.return_value = 'on'
     self.assertEqual('on', self.client.Get('dev1'))
+    self.server_mock.get.assert_called_with('dev1')
+
+    self.server_mock.get.return_value = '1'
     self.assertEqual('1', self.client.Get('dev2'))
+    self.server_mock.get.assert_called_with('dev2')
+
+    self.server_mock.get.side_effect = Exception('no such dev')
     self.assertRaises(servo_client.ServoClientError, self.client.Get,
                       'dev_unknown')
+    self.server_mock.get.assert_called_with('dev_unknown')
 
   def testMultipleGet(self):
-    self.server_mock.set_get_all(['dev1', 'dev2']).AndReturn(['on', 'off'])
-    self.server_mock.set_get_all(['dev1', 'dev_unknown']).AndRaise(
-        Exception('no such dev'))
-    self.mox.ReplayAll()
-
+    self.server_mock.set_get_all.return_value = ['on', 'off']
     self.assertDictEqual({'dev1': 'on', 'dev2': 'off'},
                          self.client.MultipleGet(['dev1', 'dev2']))
+    self.server_mock.set_get_all.assert_called_with(['dev1', 'dev2'])
+
+    self.server_mock.set_get_all.side_effect = Exception('no such dev')
     self.assertRaises(servo_client.ServoClientError,
                       self.client.MultipleGet, ['dev1', 'dev_unknown'])
+    self.server_mock.set_get_all.assert_called_with(['dev1', 'dev_unknown'])
 
   def testIsOn(self):
-    self.server_mock.get('dev1').AndReturn('on')
-    self.server_mock.get('dev2').AndReturn('off')
-    self.server_mock.get('dev3').AndReturn('1')
-    self.mox.ReplayAll()
-
+    self.server_mock.get.return_value = 'on'
     self.assertEqual(True, self.client.IsOn('dev1'))
+    self.server_mock.get.assert_called_with('dev1')
+
+    self.server_mock.get.return_value = 'off'
     self.assertEqual(False, self.client.IsOn('dev2'))
+    self.server_mock.get.assert_called_with('dev2')
+
+    self.server_mock.get.return_value = '1'
     self.assertRaises(servo_client.ServoClientError, self.client.IsOn, 'dev3')
+    self.server_mock.get.assert_called_with('dev3')
 
   def testMultipleIsOn(self):
-    self.server_mock.set_get_all(['dev1', 'dev2']).AndReturn(['on', 'off'])
-    self.server_mock.set_get_all(['dev1', 'dev_unknown']).AndRaise(
-        Exception('no such dev'))
-    self.mox.ReplayAll()
-
+    self.server_mock.set_get_all.return_value = ['on', 'off']
     self.assertDictEqual({'dev1': True, 'dev2': False},
                          self.client.MultipleIsOn(['dev1', 'dev2']))
+    self.server_mock.set_get_all.assert_called_with(['dev1', 'dev2'])
+
+    self.server_mock.set_get_all.side_effect = Exception('no such dev')
     self.assertRaises(servo_client.ServoClientError, self.client.MultipleIsOn,
                       ['dev1', 'dev_unknown'])
+    self.server_mock.set_get_all.assert_called_with(['dev1', 'dev_unknown'])
 
   def testSet(self):
-    self.server_mock.set('dev1', 'on')
-    self.server_mock.set('dev2', 'off')
-    self.server_mock.set('dev_unknown', 'on').AndRaise(Exception())
-    self.mox.ReplayAll()
-
     self.client.Set('dev1', 'on')
+    self.server_mock.set.assert_called_with('dev1', 'on')
+
     self.client.Set('dev2', 'off')
+    self.server_mock.set.assert_called_with('dev2', 'off')
+
+    self.server_mock.set.side_effect = Exception()
     self.assertRaises(servo_client.ServoClientError, self.client.Set,
                       'dev_unknown', 'on')
+    self.server_mock.set.assert_called_with('dev_unknown', 'on')
 
   def testMultipleSet(self):
-    self.server_mock.set_get_all(['dev1:on', 'dev2:off'])
-    self.server_mock.set_get_all(['dev_unknown:on', 'dev2:off']).AndRaise(
-        Exception())
-    self.mox.ReplayAll()
-
     self.client.MultipleSet([('dev1', 'on'), ('dev2', 'off')])
+    self.server_mock.set_get_all.assert_called_with(['dev1:on', 'dev2:off'])
+
+    self.server_mock.set_get_all.side_effect = Exception()
     self.assertRaises(servo_client.ServoClientError, self.client.MultipleSet,
                       [('dev_unknown', 'on'), ('dev2', 'off')])
+    self.server_mock.set_get_all.assert_called_with(
+        ['dev_unknown:on', 'dev2:off'])
 
   def testEnable(self):
-    self.server_mock.set('dev1', 'on')
-    self.server_mock.set('dev_unknown', 'on').AndRaise(Exception())
-    self.mox.ReplayAll()
-
     self.client.Enable('dev1')
+    self.server_mock.set.assert_called_with('dev1', 'on')
+
+    self.server_mock.set.side_effect = Exception()
     self.assertRaises(servo_client.ServoClientError, self.client.Enable,
                       'dev_unknown')
+    self.server_mock.set.assert_called_with('dev_unknown', 'on')
 
   def testDisable(self):
-    self.server_mock.set('dev1', 'off')
-    self.server_mock.set('dev_unknown', 'off').AndRaise(Exception())
-    self.mox.ReplayAll()
-
     self.client.Disable('dev1')
+    self.server_mock.set.assert_called_with('dev1', 'off')
+
+    self.server_mock.set.side_effect = Exception()
     self.assertRaises(servo_client.ServoClientError, self.client.Disable,
                       'dev_unknown')
+    self.server_mock.set.assert_called_with('dev_unknown', 'off')
 
   def testClick(self):
-    self.server_mock.set_get_all(['dev1:on', 'dev1:off'])
-    self.server_mock.set_get_all(
-        ['dev_unknown:on', 'dev_unknown:off']).AndRaise(Exception())
-    self.mox.ReplayAll()
-
     self.client.Click('dev1')
+    self.server_mock.set_get_all.assert_called_with(['dev1:on', 'dev1:off'])
+
+    self.server_mock.set_get_all.side_effect = Exception()
     self.assertRaises(servo_client.ServoClientError, self.client.Click,
                       'dev_unknown')
+    self.server_mock.set_get_all.assert_called_with(
+        ['dev_unknown:on', 'dev_unknown:off'])
 
 
 if __name__ == '__main__':
