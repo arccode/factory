a017efb9b3a23a9abd0c861c3406764671182258
Yilin Yang <kerker@google.com>
2020-02-14 14:34:52 +0800

    updater_unittest: Migrate mox to mock
    `mox` is not supported in python3, we need to migrate to module `mock`.
    
    BUG=b:147990582
    TEST=updater_unittest pass
    
    Change-Id: Iefe60bb462449be580da4c4636f0d566407ba749
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/2054707
    Reviewed-by: Chun-Tsen Kuo <chuntsen@chromium.org>
    Tested-by: Yilin Yang (kerker) <kerker@chromium.org>
    Commit-Queue: Yilin Yang (kerker) <kerker@chromium.org>

--- py/goofy/updater_unittest.py
+++ py/goofy/updater_unittest.py
@@ -6,42 +6,36 @@
 import threading
 import unittest
 
-import mox
+import mock
 
 from cros.factory.goofy import updater
-from cros.factory.test import server_proxy
-from cros.factory.test import session
-from cros.factory.test.utils import update_utils
 
 
 class CheckForUpdateTest(unittest.TestCase):
 
-  def setUp(self):
-    self.mox = mox.Mox()
+  @mock.patch('cros.factory.test.utils.update_utils.Updater')
+  @mock.patch('cros.factory.test.server_proxy.GetServerProxy')
+  @mock.patch('cros.factory.test.session.GetToolkitVersion')
+  def _testUpdate(self, local_version, get_toolkit_version_mock=None,
+                  get_server_proxy_mock=None, updater_mock=None):
+    get_toolkit_version_mock.return_value = local_version
 
-  def tearDown(self):
-    self.mox.UnsetStubs()
+    fake_proxy = mock.MagicMock()
+    get_server_proxy_mock.return_value = fake_proxy
 
-  def _testUpdate(self, local_version):
-    self.mox.StubOutWithMock(session, 'GetToolkitVersion')
-    session.GetToolkitVersion().AndReturn(local_version)
+    fake_updater = mock.MagicMock()
+    fake_updater.GetUpdateVersion.return_value = '11111'
+    fake_updater.IsUpdateAvailable.return_value = local_version != '11111'
+    updater_mock.return_value = fake_updater
 
-    self.mox.StubOutWithMock(server_proxy, 'GetServerProxy')
-    fake_proxy = self.mox.CreateMockAnything()
-    server_proxy.GetServerProxy(timeout=3).AndReturn(fake_proxy)
-
-    self.mox.StubOutWithMock(update_utils, 'Updater')
-    fake_updater = self.mox.CreateMockAnything()
-    update_utils.Updater(
-        'toolkit', proxy=fake_proxy).AndReturn(fake_updater)
-    fake_updater.GetUpdateVersion().AndReturn('11111')
-    fake_updater.IsUpdateAvailable(local_version).AndReturn(
-        local_version != '11111')
-
-    self.mox.ReplayAll()
     self.assertEqual(
         updater.CheckForUpdate(3), ('11111', local_version != '11111'))
-    self.mox.VerifyAll()
+
+    get_toolkit_version_mock.assert_called_once_with()
+    get_server_proxy_mock.assert_called_once_with(timeout=3)
+    updater_mock.assert_called_once_with('toolkit', proxy=fake_proxy)
+    fake_updater.GetUpdateVersion.assert_called_once_with()
+    fake_updater.IsUpdateAvailable.assert_called_once_with(local_version)
 
   def testMustUpdate(self):
     self._testUpdate('00000')
@@ -58,21 +52,16 @@ class CheckForUpdateAsyncTest(unittest.TestCase):
     self.event.set()
 
   def setUp(self):
-    self.mox = mox.Mox()
     self.event = threading.Event()
     self.event.clear()
 
-  def tearDown(self):
-    self.mox.UnsetStubs()
-
-  def _testUpdate(self, available):
+  @mock.patch('cros.factory.goofy.updater.CheckForUpdate')
+  def _testUpdate(self, available, check_for_update_mock=None):
     """Provides basic testing flow for testMustUpdate and testNotUpdate."""
-    self.mox.StubOutWithMock(updater, 'CheckForUpdate')
-    updater.CheckForUpdate(timeout=1).AndReturn(('11111', available))
-    callback = self.mox.CreateMockAnything()
-    # pylint: disable=not-callable
-    callback(True, '11111', available).WithSideEffects(self.CallbackCalled)
-    self.mox.ReplayAll()
+    check_for_update_mock.return_value = ('11111', available)
+
+    callback = mock.MagicMock()
+    callback.side_effect = self.CallbackCalled
     updater.CheckForUpdateAsync(callback, 1)
 
     # In the real use case, CheckForUpdate requires a timeout. However it has
@@ -81,7 +70,9 @@ class CheckForUpdateAsyncTest(unittest.TestCase):
     # at most 1 second and verify its correctness.
 
     self.event.wait(1)
-    self.mox.VerifyAll()
+
+    check_for_update_mock.assert_called_once_with(timeout=1)
+    callback.assert_called_once_with(True, '11111', available)
 
   def testMustUpdate(self):
     self._testUpdate(True)
@@ -89,20 +80,21 @@ class CheckForUpdateAsyncTest(unittest.TestCase):
   def testNotUpdate(self):
     self._testUpdate(False)
 
-  def testCanNotReach(self):
-    self.mox.StubOutWithMock(updater, 'CheckForUpdate')
-    updater.CheckForUpdate(timeout=1).AndRaise(
-        Exception('Can not contact factory server'))
-    callback = self.mox.CreateMockAnything()
-    # pylint: disable=not-callable
-    callback(False, None, False).WithSideEffects(self.CallbackCalled)
-    self.mox.ReplayAll()
+  @mock.patch('cros.factory.goofy.updater.CheckForUpdate')
+  def testCanNotReach(self, check_for_update_mock):
+    check_for_update_mock.side_effect = Exception(
+        'Can not contact factory server')
+    callback = mock.MagicMock()
+    callback.side_effect = self.CallbackCalled
+
     updater.CheckForUpdateAsync(callback, 1)
 
     # See comments in _testUpdate for the explanation of event waiting.
 
     self.event.wait(1)
-    self.mox.VerifyAll()
+
+    check_for_update_mock.assert_called_once_with(timeout=1)
+    callback.assert_called_once_with(False, None, False)
 
 
 if __name__ == '__main__':
