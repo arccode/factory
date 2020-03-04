67f20b5b0620024539606aba3ee17c37888cda78
Yilin Yang <kerker@google.com>
2020-03-04 16:04:31 +0800

    connection_manager_unittest: Migrate mox to mock
    `mox` is not supported in python3, we need to migrate to module `mock`.
    
    BUG=b:147990582
    TEST=connection_manager_unittest pass
    
    Change-Id: I815d40a81e035ad07ba59e28d3ff7d1a6a027da5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/2087251
    Reviewed-by: Meng-Huan Yu <menghuan@chromium.org>
    Tested-by: Yilin Yang (kerker) <kerker@chromium.org>
    Commit-Queue: Yilin Yang (kerker) <kerker@chromium.org>

--- py/test/utils/connection_manager_unittest.py
+++ py/test/utils/connection_manager_unittest.py
@@ -4,13 +4,10 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import glob
-import os
-import subprocess
 import unittest
 
 import dbus
-import mox
+import mock
 
 from cros.factory.test.utils import connection_manager
 from cros.factory.utils.net_utils import WLAN
@@ -37,20 +34,11 @@ _FAKE_DATA = {
 
 class WLANTest(unittest.TestCase):
 
-  def setUp(self):
-    self.mox = mox.Mox()
-
-  def tearDown(self):
-    self.mox.VerifyAll()
-    self.mox.UnsetStubs()
-
   def testWLANFailWPASecurity(self):
-    self.mox.ReplayAll()
     self.assertRaises(ValueError, WLAN,
                       ssid='fake_server1', security='wpa', passphrase='1')
 
   def testWLANFailInvalidSecurity(self):
-    self.mox.ReplayAll()
     self.assertRaises(ValueError, WLAN,
                       ssid='fake_server1', security='ABC', passphrase='1')
 
@@ -58,44 +46,51 @@ class WLANTest(unittest.TestCase):
 class ConnectionManagerTest(unittest.TestCase):
 
   def setUp(self):
-    self.mox = mox.Mox()
-    self.mox.StubOutWithMock(connection_manager, 'GetBaseNetworkManager')
-    self.mox.StubOutWithMock(glob, 'glob')
-    self.mox.StubOutWithMock(subprocess, 'call')
-    self.fakeBaseNetworkManager = self.mox.CreateMockAnything()
+    self.fakeBaseNetworkManager = mock.MagicMock()
     self.fakeData = _FAKE_DATA.copy()
     self.fakeData['wlans'] = [WLAN(ssid='fake_server',
                                    security='psk',
                                    passphrase='test0000')]
 
-  def tearDown(self):
-    self.mox.UnsetStubs()
-    self.mox.VerifyAll()
-
-  def MockDisableNetworking(self):
+  def GetDisableNetworkingSubprocessCalls(self):
+    subprocess_call_calls = []
     for service in (_FAKE_SUBSERVICE_LIST + [_FAKE_MANAGER] +
                     _FAKE_DEPSERVICE_LIST):
-      subprocess.call('stop %s' % service, shell=True,
-                      stdout=mox.IgnoreArg(), stderr=mox.IgnoreArg())
-    interfaces = list(_FAKE_INTERFACES)
-    interfaces.remove('lo')
-    glob.glob('/sys/class/net/*').AndReturn(_FAKE_INTERFACES)
-    for dev in interfaces:
-      subprocess.call('ifconfig %s down' % dev, shell=True,
-                      stdout=mox.IgnoreArg(), stderr=mox.IgnoreArg())
-
-  def MockEnableNetworking(self, reset=True):
-    if reset:
-      self.MockDisableNetworking()
-      self.mox.StubOutWithMock(os, 'remove')
-      os.remove(_FAKE_PROFILE_LOCATION % _FAKE_PROC_NAME)
+      subprocess_call_calls.append(
+          mock.call('stop %s' % service, shell=True, stdout=mock.ANY,
+                    stderr=mock.ANY))
 
     interfaces = list(_FAKE_INTERFACES)
     interfaces.remove('lo')
-    glob.glob('/sys/class/net/*').AndReturn(_FAKE_INTERFACES)
     for dev in interfaces:
-      subprocess.call('ifconfig %s up' % dev, shell=True,
-                      stdout=mox.IgnoreArg(), stderr=mox.IgnoreArg())
+      subprocess_call_calls.append(
+          mock.call('ifconfig %s down' % dev, shell=True, stdout=mock.ANY,
+                    stderr=mock.ANY))
+
+    return subprocess_call_calls
+
+  def VerifyDisableNetworking(self, glob_mock, call_mock):
+    glob_mock.assert_called_once_with('/sys/class/net/*')
+    self.assertEqual(call_mock.call_args_list,
+                     self.GetDisableNetworkingSubprocessCalls())
+
+  def MockEnableNetworking(self):
+    fakeDevice = mock.MagicMock()
+    fakeDevice.ScanInterval = dbus.UInt16(_FAKE_SCAN_INTERVAL_SECS)
+    self.fakeBaseNetworkManager.FindElementByNameSubstring = mock.Mock(
+        return_value=fakeDevice)
+
+    self.fakeBaseNetworkManager.manager = mock.MagicMock()
+
+  def GetEnableNetworkingSubprocessCalls(self):
+    subprocess_call_calls = []
+    interfaces = list(_FAKE_INTERFACES)
+    interfaces.remove('lo')
+
+    for dev in interfaces:
+      subprocess_call_calls.append(
+          mock.call('ifconfig %s up' % dev, shell=True, stdout=mock.ANY,
+                    stderr=mock.ANY))
 
     for service in (_FAKE_DEPSERVICE_LIST + [_FAKE_MANAGER] +
                     _FAKE_SUBSERVICE_LIST):
@@ -104,93 +99,126 @@ class ConnectionManagerTest(unittest.TestCase):
           self.fakeData['override_blacklisted_devices'] is not None):
         cmd += ' BLACKLISTED_DEVICES="%s"' % (
             ','.join(self.fakeData['override_blacklisted_devices']))
-      subprocess.call(cmd, shell=True,
-                      stdout=mox.IgnoreArg(), stderr=mox.IgnoreArg())
+      subprocess_call_calls.append(
+          mock.call(cmd, shell=True, stdout=mock.ANY, stderr=mock.ANY))
 
-    connection_manager.GetBaseNetworkManager().AndReturn(
-        self.fakeBaseNetworkManager)
-    glob.glob('/sys/class/net/*').AndReturn(_FAKE_INTERFACES)
-    fakeDevice = self.mox.CreateMockAnything()
-    self.fakeBaseNetworkManager.FindElementByNameSubstring(
-        'Device', 'wlan0').AndReturn(fakeDevice)
-    fakeDevice.SetProperty('ScanInterval',
-                           dbus.UInt16(_FAKE_SCAN_INTERVAL_SECS))
+    return subprocess_call_calls
 
-    fakeMgr = self.mox.CreateMockAnything()
-    self.fakeBaseNetworkManager.manager = fakeMgr
-    self.fakeBaseNetworkManager.manager.ConfigureService({
+  def VerifyEnableNetworking(self, glob_mock, call_mock, remove_mock=None,
+                             reset=True):
+    subprocess_call_calls = []
+    glob_call_count = 2
+
+    if reset:
+      remove_mock.assert_called_once_with(_FAKE_PROFILE_LOCATION %
+                                          _FAKE_PROC_NAME)
+      subprocess_call_calls.extend(self.GetDisableNetworkingSubprocessCalls())
+      glob_call_count += 1
+    subprocess_call_calls.extend(self.GetEnableNetworkingSubprocessCalls())
+
+    self.assertEqual(call_mock.call_args_list, subprocess_call_calls)
+    self.fakeBaseNetworkManager.FindElementByNameSubstring.assert_called_with(
+        'Device', 'wlan0')
+    self.fakeBaseNetworkManager.manager.ConfigureService.assert_called_with({
         'Type': 'wifi',
         'Mode': 'managed',
         'AutoConnect': True,
         'SSID': 'fake_server',
         'Security': 'psk',
         'Passphrase': 'test0000'
-    }, signature=mox.IgnoreArg())
+    }, signature=mock.ANY)
+    connection_manager.GetBaseNetworkManager.assert_called_once_with()
+    glob_mock.assert_called_with('/sys/class/net/*')
+    self.assertEqual(glob_call_count, glob_mock.call_count)
 
-  def testInitWithEnableNetworking(self):
-    self.MockEnableNetworking(reset=False)
+  @mock.patch('cros.factory.test.utils.connection_manager.GetBaseNetworkManager')
+  @mock.patch('subprocess.call')
+  @mock.patch('glob.glob')
+  def testInitWithEnableNetworking(self, glob_mock, call_mock,
+                                   get_base_network_manager_mock):
+    glob_mock.return_value = _FAKE_INTERFACES
+    get_base_network_manager_mock.return_value = self.fakeBaseNetworkManager
 
-    self.mox.ReplayAll()
+    self.MockEnableNetworking()
+
     connection_manager.ConnectionManager(start_enabled=True,
                                          **self.fakeData)
+    self.VerifyEnableNetworking(glob_mock, call_mock, reset=False)
 
-  def testInitWithDisableNetworking(self):
-    self.MockDisableNetworking()
+  @mock.patch('subprocess.call')
+  @mock.patch('glob.glob')
+  def testInitWithDisableNetworking(self, glob_mock, call_mock):
+    glob_mock.return_value = _FAKE_INTERFACES
 
-    self.mox.ReplayAll()
     connection_manager.ConnectionManager(start_enabled=False,
                                          **self.fakeData)
+    self.VerifyDisableNetworking(glob_mock, call_mock)
 
-  def testOverrideBlacklistedDevices(self):
+  @mock.patch('cros.factory.test.utils.connection_manager.GetBaseNetworkManager')
+  @mock.patch('os.remove')
+  @mock.patch('subprocess.call')
+  @mock.patch('glob.glob')
+  def testOverrideBlacklistedDevices(self, glob_mock, call_mock, remove_mock,
+                                     get_base_network_manager_mock):
+    glob_mock.return_value = _FAKE_INTERFACES
+    get_base_network_manager_mock.return_value = self.fakeBaseNetworkManager
     self.fakeData['override_blacklisted_devices'] = (
         _FAKE_OVERRIDE_BLACKLISTED_DEVICES)
-    self.MockEnableNetworking(reset=True)
+    self.MockEnableNetworking()
 
-    self.mox.ReplayAll()
     connection_manager.ConnectionManager(start_enabled=True,
                                          **self.fakeData)
+    self.VerifyEnableNetworking(glob_mock, call_mock, remove_mock, reset=True)
 
   def testInitFailInvalidNetworkManager(self):
-    self.mox.ReplayAll()
     self.assertRaises(AssertionError, connection_manager.ConnectionManager,
                       network_manager='ABC')
 
   def testInitFailInvalidProcessName(self):
-    self.mox.ReplayAll()
     self.assertRaises(AssertionError, connection_manager.ConnectionManager,
                       process_name='XYZ')
 
-  def testIsConnectedOK(self):
-    self.MockDisableNetworking()
-    connection_manager.GetBaseNetworkManager().AndReturn(
-        self.fakeBaseNetworkManager)
-    self.fakeBaseNetworkManager.GetSystemState().AndReturn('online')
+  @mock.patch('cros.factory.test.utils.connection_manager.GetBaseNetworkManager')
+  @mock.patch('subprocess.call')
+  @mock.patch('glob.glob')
+  def testIsConnectedOK(self, glob_mock, call_mock,
+                        get_base_network_manager_mock):
+    glob_mock.return_value = _FAKE_INTERFACES
+    get_base_network_manager_mock.return_value = self.fakeBaseNetworkManager
+    self.fakeBaseNetworkManager.GetSystemState.return_value = 'online'
 
-    self.mox.ReplayAll()
     x = connection_manager.ConnectionManager(start_enabled=False,
                                              **self.fakeData)
     self.assertEqual(x.IsConnected(), True)
+    self.VerifyDisableNetworking(glob_mock, call_mock)
 
-  def testIsConnectedFailNotConnected(self):
-    self.MockDisableNetworking()
-    connection_manager.GetBaseNetworkManager().AndReturn(
-        self.fakeBaseNetworkManager)
-    self.fakeBaseNetworkManager.GetSystemState().AndReturn('offline')
+  @mock.patch('cros.factory.test.utils.connection_manager.GetBaseNetworkManager')
+  @mock.patch('subprocess.call')
+  @mock.patch('glob.glob')
+  def testIsConnectedFailNotConnected(self, glob_mock, call_mock,
+                                      get_base_network_manager_mock):
+    glob_mock.return_value = _FAKE_INTERFACES
+    get_base_network_manager_mock.return_value = self.fakeBaseNetworkManager
+    self.fakeBaseNetworkManager.GetSystemState.return_value = 'offline'
 
-    self.mox.ReplayAll()
     x = connection_manager.ConnectionManager(start_enabled=False,
                                              **self.fakeData)
     self.assertEqual(x.IsConnected(), False)
+    self.VerifyDisableNetworking(glob_mock, call_mock)
 
-  def testIsConnectedFailNetworkManagerNotRunning(self):
-    self.MockDisableNetworking()
-    connection_manager.GetBaseNetworkManager().AndRaise(
-        dbus.exceptions.DBusException('YAYA'))
+  @mock.patch('cros.factory.test.utils.connection_manager.GetBaseNetworkManager')
+  @mock.patch('subprocess.call')
+  @mock.patch('glob.glob')
+  def testIsConnectedFailNetworkManagerNotRunning(
+      self, glob_mock, call_mock, get_base_network_manager_mock):
+    glob_mock.return_value = _FAKE_INTERFACES
+    get_base_network_manager_mock.side_effect = dbus.exceptions.DBusException(
+        'YAYA')
 
-    self.mox.ReplayAll()
     x = connection_manager.ConnectionManager(start_enabled=False,
                                              **self.fakeData)
     self.assertEqual(x.IsConnected(), False)
+    self.VerifyDisableNetworking(glob_mock, call_mock)
 
 
 if __name__ == '__main__':
