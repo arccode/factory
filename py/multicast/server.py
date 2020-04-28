57d659f493954f36b3831ff5f2a7907af1db765e
Pin-Yen Lin <treapking@google.com>
2020-04-28 15:59:28 +0800

    multicast: Add multicast server
    This multicast server read the multicast config from argument and
    spawn a uftp server for each component.
    
    BUG=b:155147650
    TEST=manually run multicast service on Dome
    
    Change-Id: I56876c81bffb9093c393e64eb432de03e5b76181
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/2174087
    Reviewed-by: Stimim Chen <stimim@chromium.org>
    Commit-Queue: Pin-yen Lin <treapking@chromium.org>
    Tested-by: Pin-yen Lin <treapking@chromium.org>

--- py/multicast/server.py
+++ py/multicast/server.py
@@ -5,11 +5,50 @@
 
 """Multicast service to spawn uftp server"""
 
-import time
+from __future__ import print_function
+
+from multiprocessing import Process
+import os
+import sys
+
+from cros.factory.utils import json_utils
+from cros.factory.utils.process_utils import Spawn
+
+
+UFTP_PATH = '/usr/bin/uftp'
+
+CC_TYPE = 'tfmcc' # TCP friendly multicast congestion control
+LOG_LEVEL = '0'
+ROBUST_FACTOR = '50'
+TTL = '10'
+
+def SpawnUFTP(file_name, multicast_addr):
+  addr, port = multicast_addr.split(':')
+
+  cmd = [UFTP_PATH, '-M', addr, '-t', TTL, '-u', port, '-p', port,
+         '-x', LOG_LEVEL, '-C', CC_TYPE, '-s', ROBUST_FACTOR, file_name]
+
+  while True:
+    Spawn(cmd, call=True)
+
 
 def Main():
-  # Dummy server. Not implemented.
-  time.sleep(1e9)
+  assert len(sys.argv) == 3
+
+  resource_dir = os.path.dirname(sys.argv[1])
+  payloads = json_utils.LoadFile(sys.argv[1])
+  multicast_dict = json_utils.LoadFile(sys.argv[2])['multicast']
+
+  for component in multicast_dict:
+    for part in multicast_dict[component]:
+      file_name = payloads[component][part]
+      file_path = os.path.join(resource_dir, file_name)
+      multicast_addr = multicast_dict[component][part]
+
+      p = Process(name=file_name, target=SpawnUFTP,
+                  args=(file_path, multicast_addr))
+      p.start()
+
 
 if __name__ == '__main__':
   Main()
