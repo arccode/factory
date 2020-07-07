e2caee7955c53719141dee5b1de240a3fcc618f6
Clark Chung <clarkchung@google.com>
2020-07-07 15:01:18 +0800

    hwid/service: migrate to cloud logging
    In App Engine Python 3 environment we need to attach AppEngineHandler
    from google-cloud-logging to support filtering by severity and
    correlating the app's entries with request log entries.
    
    BUG=b:154786060
    TEST=manual test by checking if the logs in the console are properly
         formatted.
    
    Change-Id: Ifeb80c0936961867bdccdbe1ed0fe545208bdf76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/2299613
    Reviewed-by: Yilin Yang (kerker) <kerker@chromium.org>
    Commit-Queue: Clark Chung <ckclark@chromium.org>
    Tested-by: Clark Chung <ckclark@chromium.org>

--- py/hwid/service/appengine/app.py
+++ py/hwid/service/appengine/app.py
@@ -4,8 +4,13 @@
 
 """The service handler for APIs."""
 
-import endpoints  # pylint: disable=import-error
-import webapp2  # pylint: disable=import-error
+import os
+
+# pylint: disable=no-name-in-module, import-error, wrong-import-order
+import endpoints
+import google.cloud.logging as gc_logging
+import webapp2
+# pylint: enable=no-name-in-module, import-error, wrong-import-order
 
 from cros.factory.hwid.service.appengine import goldeneye_ingestion
 from cros.factory.hwid.service.appengine import hwid_api
@@ -22,3 +27,13 @@ ingestion_app = webapp2.WSGIApplication(
          goldeneye_ingestion.AllDevicesRefreshHandler),
     ],
     debug=False)
+
+
+def _InitLogging():
+  if os.environ.get('IS_APPENGINE') == 'true':
+    client = gc_logging.Client()
+    client.get_default_handler()
+    client.setup_logging()
+
+
+_InitLogging()
