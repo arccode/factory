7a4358e2422bcd005a97ff8dcfed6274864e0318
Clark Chung <clarkchung@google.com>
2020-07-14 19:43:32 +0800

    hwid/service: remove google appengine sdk reference
    Since bundled services of App Engine are not supported in App Engine
    Python 3 Runtime, this CL removes the references accordingly.
    
    BUG=b:154786060
    TEST=${FACTORY_REPO}/deploy/cros_hwid_service.sh test
    
    Change-Id: I5aec1758254bf7ccecf66f6cbea3bc79a7aac60f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/2301319
    Commit-Queue: Clark Chung <ckclark@chromium.org>
    Tested-by: Clark Chung <ckclark@chromium.org>
    Reviewed-by: Yilin Yang (kerker) <kerker@chromium.org>

--- py/hwid/service/appengine/__init__.py
+++ py/hwid/service/appengine/__init__.py
@@ -17,16 +17,9 @@ _APPENGINE_SRC_ROOT = os.path.join(
 
 
 def _PatchImports():
-  google = 'google'
   site_dir = os.environ.get('CUSTOMIZE_SITE_DIR')
-  appengine_sdk_dir = os.environ.get('APPENGINE_SDK_DIR')
   if site_dir:
     site.addsitedir(site_dir)
-    # Referenced from
-    # https://github.com/GoogleCloudPlatform/python-repo-tools/blob/87422ba/gcp_devrel/testing/appengine.py#L39-L41
-    if google in sys.modules and appengine_sdk_dir:
-      sys.modules[google].__path__.append(
-          os.path.join(appengine_sdk_dir, google))
 
 
 @functools.lru_cache(maxsize=1)
