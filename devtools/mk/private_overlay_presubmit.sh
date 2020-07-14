db15cfd4be1fe4d7ac2db6cfdd33df23542e804a
Yilin Yang <kerker@google.com>
2020-07-14 15:56:30 +0800

    sh: Add private_overlay_presubmit_check tool
    This provides the presubmit check for private overlay to check the
    health of test lists.
    
    "test_list_checker" needs to be run in chroot when checking the private
    overlay. This CL refers to go/croscli/2991495. It tries to run
    `cros_sdk` if it's not in chroot.
    
    BUG=b:161196535
    TEST=test "repo upload" inside/outside chroot on jacuzzi board
    
    Change-Id: I85c0670b8512dcc4aabb8934ea4e31eb23761d29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/2297162
    Commit-Queue: Yilin Yang (kerker) <kerker@chromium.org>
    Tested-by: Yilin Yang (kerker) <kerker@chromium.org>
    Reviewed-by: Pin-yen Lin <treapking@chromium.org>

---
+++ devtools/mk/private_overlay_presubmit.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+# Copyright 2020 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# This is presubmit check for private overlay to check the health of test lists.
+# Example CL : go/croscli/3164743.
+
+if [ -f /etc/debian_chroot ]; then
+  if [[ ${PRESUBMIT_PROJECT} =~ chromeos/overlays/overlay-(.+)-private ]]; then
+    board=${BASH_REMATCH[1]}
+  else
+    exit 0
+  fi
+
+  test_list_ids=""
+  for file in ${PRESUBMIT_FILES}
+  do
+    filename="$(basename "${file}")"
+    if [[ ${filename} =~ (.+).test_list.json ]]; then
+      test_list_ids="${test_list_ids} ${BASH_REMATCH[1]}"
+    fi
+  done
+
+  if [ -z "$test_list_ids" ]; then
+    exit 0
+  fi
+
+  factory_dir="$(dirname "$(dirname " $(dirname "$(readlink -f "$0")")")")"
+  test_list_checker_path="${factory_dir}/bin/test_list_checker"
+
+  # Clean up these env variables. Without doing this, we will get additional
+  # errors when test_list_checker report errors. The reason is that
+  # test_list_checker uses the Makefile, and in the Makefile, we try to read the
+  # path from PRESUBMIT_FILES.
+  PRESUBMIT_FILES="" PRESUBMIT_COMMIT="" PRESUBMIT_PROJECT="" \
+      ${test_list_checker_path} --board "${board}" ${test_list_ids}
+else
+  exec cros_sdk --working-dir=. PRESUBMIT_FILES="${PRESUBMIT_FILES}" \
+      PRESUBMIT_COMMIT="${PRESUBMIT_COMMIT}" \
+      PRESUBMIT_PROJECT="${PRESUBMIT_PROJECT}" -- "$0"
+fi
