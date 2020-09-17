5c9b6b7c997fb6922cb7d3b6e4eaae423f216dd5
Stimim Chen <stimim@google.com>
2020-09-17 17:44:09 +0800

    probe: audio_codec: stop probing /proc/asound/*
    The content of this folder is not useful (not directly related to real
    hardware components).  We should stop probing it.
    
    BUG=b:168778048
    TEST=make test
    
    Change-Id: I97bde7b8d8b657f7e14afa77b97560fa5050be4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/2415922
    Reviewed-by: Yong Hong <yhong@chromium.org>
    Commit-Queue: Stimim Chen <stimim@chromium.org>
    Tested-by: Stimim Chen <stimim@chromium.org>

--- py/probe/functions/generic_audio_codec.py
+++ py/probe/functions/generic_audio_codec.py
@@ -1,12 +1,10 @@
-# Copyright 2017 The Chromium OS Authors. All rights reserved.
+# Copyright 2020 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
 import os
-import re
 
 from cros.factory.probe.lib import cached_probe_function
-from cros.factory.utils import process_utils
 
 
 RESULT_KEY = 'name'
@@ -30,7 +28,6 @@ class GenericAudioCodecFunction(cached_probe_function.CachedProbeFunction):
     """
     KNOWN_INVALID_CODEC_NAMES = set([
         'snd-soc-dummy',
-        'ts3a227e.4-003b',  # autonomous audiojack switch, not an audio codec
         'dw-hdmi-audio'  # this is a virtual audio codec driver
     ])
 
@@ -45,18 +42,4 @@ class GenericAudioCodecFunction(cached_probe_function.CachedProbeFunction):
           results.extend([codec.strip()
                           for codec in f.read().splitlines()
                           if codec not in KNOWN_INVALID_CODEC_NAMES])
-
-    grep_result = process_utils.SpawnOutput(
-        'grep -R "Codec:" /proc/asound/*', shell=True, log=True)
-    match_set = set()
-    for line in grep_result.splitlines():
-      match_set |= set(re.findall(r'.*Codec:(.*)', line))
-    results += [match.strip() for match in sorted(match_set) if match]
-
-    if not results:
-      # Formatted '00-00: WM??? PCM wm???-hifi-0: ...'
-      with open('/proc/asound/pcm', 'r') as f:
-        pcm_data = f.read().strip().split(' ')
-      if len(pcm_data) > 2:
-        results.append(pcm_data[1])
     return [{RESULT_KEY: result} for result in results]
