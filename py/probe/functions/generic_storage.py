1a04f9720a83987ce88394eff805feef3ab1193c
Stimim Chen <stimim@google.com>
2020-10-07 11:33:57 +0800

    generic_storage: only print fwver as ascii if it's letters or digits
    The previous implementation would print fwver as ascii string if all
    bytes are "printable", which mgiht contain whitespaces (including tab,
    LF, punctuation), and would be escaped when printing out (e.g. "\t").
    This would break the following HWID generation.
    
    BUG=b:169216142
    TEST=None
    
    Change-Id: I520237f57ccb0e072268dbcdccdc93d34eefa1fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/2465354
    Reviewed-by: Yong Hong <yhong@chromium.org>
    Commit-Queue: Stimim Chen <stimim@chromium.org>
    Tested-by: Stimim Chen <stimim@chromium.org>

--- py/probe/functions/generic_storage.py
+++ py/probe/functions/generic_storage.py
@@ -69,7 +69,8 @@ def CookVersion(raw_version):
   # Note vendor may choose SPACE (0x20) or NUL (0x00) to pad version string,
   # so we want to strip both in the human readable part.
   ascii_string = ''.join(map(chr, raw_version)).strip(' \0')
-  if ascii_string and all(c in string.printable for c in ascii_string):
+  if ascii_string and all(
+      (c in string.ascii_letters or c in string.digits) for c in ascii_string):
     version += ' (%s)' % ascii_string
   else:
     # Try to decode it as a 64-bit little-endian integer.
