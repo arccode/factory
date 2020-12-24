a6870458e3717e444437e580cd807b9bbffd2c59
Chung-Sheng Wu <chungsheng@google.com>
2020-12-24 18:45:45 +0800

    hwid_extractor: Add RLZData for managing rlz.json
    A local mapping from rlz code to board name is saved to rlz.json. Users
    can upload all_device.json to update rlz.json.
    
    BUG=b:174726309
    TEST=None
    
    Change-Id: Ie33f37c2db9ca84f05ade25f3886545973cf711b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/2596954
    Tested-by: Chung-Sheng Wu <chungsheng@chromium.org>
    Commit-Queue: Chung-Sheng Wu <chungsheng@chromium.org>
    Reviewed-by: Clark Chung <ckclark@chromium.org>

---
+++ py/hwid_extractor/rlz.py
@@ -0,0 +1,56 @@
+# Copyright 2021 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import json
+import os
+
+
+RLZ_JSON = os.path.join(os.path.dirname(__file__), 'rlz.json')
+
+
+class RLZData:
+  """RLZ data stores the mapping from rlz codes to name of reference boards."""
+
+  def __init__(self):
+    self._rlz_data = {}
+    if os.path.isfile(RLZ_JSON):
+      with open(RLZ_JSON, 'r') as f:
+        self._rlz_data = json.load(f)
+
+  def Get(self, *args, **kargs):
+    return self._rlz_data.get(*args, **kargs)
+
+  @staticmethod
+  def _ParseAllDevicesJSON(all_device):
+    res = {}
+    for device in all_device.get('devices', []):
+      cr50_board_id = device.get('cr50_board_id')
+      if not cr50_board_id:
+        continue
+      reference_board = device.get('reference_board')
+      if not reference_board:
+        continue
+      res[cr50_board_id] = reference_board['public_codename']
+    return res
+
+  def UpdateFromAllDevicesJSON(self, all_device):
+    """Update rlz.json with all_device.json.
+
+    all_devices.json: gs://chromeos-build-release-console/all_devices.json
+
+    Ask user to upload all_device.json, parse it and store the results to
+    rlz.json.
+
+    Args:
+      all_device: The parsed json object of all_devices.json.
+    Returns:
+      True if update successfully.
+    """
+    rlz_data = self._ParseAllDevicesJSON(all_device)
+    if not rlz_data:
+      return False
+    with open(RLZ_JSON, 'w') as f:
+      json.dump(rlz_data, f)
+    self._rlz_data = rlz_data
+    return True
