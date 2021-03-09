fdfa694873750039a23356a434402c25c2d6c17b
Yi Chou <yich@google.com>
2021-03-09 15:56:20 +0800

    pytest: Use hwsec daemons to verify EK
    Because we are going to deprecate these commands:
    
    "tpm-manager"
    "cryptohome --action=tpm_status"
    "cryptohome --action=tpm_take_ownership"
    "cryptohome --action=tpm_wait_ownership"
    "cryptohome --action=tpm_verify_ek"
    
    BUG=b:182236467
    TEST=Manually test on DUT.
    
    Change-Id: If69cd92d0b28aa9724f9e5daca8a7123a3c483ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/2744522
    Tested-by: Yi Chou <yich@google.com>
    Reviewed-by: Pin-yen Lin <treapking@chromium.org>
    Reviewed-by: Leo Lai <cylai@google.com>
    Reviewed-by: Stimim Chen <stimim@chromium.org>
    Commit-Queue: Yi Chou <yich@google.com>

--- py/test/pytests/tpm_verify_ek.py
+++ py/test/pytests/tpm_verify_ek.py
@@ -4,22 +4,43 @@
 
 """Verifies the TPM endorsement key.
 
+Description
+-----------
 This test (whether it succeeds or fails) always requests a TPM clear
 on reboot.  It works even if run multiple times without rebooting.
 
 If the TPM is somehow owned but no password is available, the test
 will fail but emit a reasonable error message (and it will pass on the
 next boot).
+
+Test Procedure
+--------------
+This is an automated test without user interaction.
+
+Dependency
+----------
+A workable TPM with endorsement key on it.
+And hardware security daemons & clients.
+
+Examples
+--------
+Examples of how to use this test::
+
+  {
+    "pytest_name": "tpm_verify_ek",
+    "args": {
+      "is_cros_core": false,
+    }
+  }
+
 """
 
 import logging
-import tempfile
 import unittest
 
 from cros.factory.device import device_utils
 from cros.factory.utils.arg_utils import Arg
 from cros.factory.utils import string_utils
-from cros.factory.utils import sync_utils
 
 
 class TPMVerifyEK(unittest.TestCase):
@@ -35,79 +56,86 @@ class TPMVerifyEK(unittest.TestCase):
   def _TPMStatus(self):
     """Returns TPM status as a dictionary.
 
-    e.g., {'TPM Being Owned': 'false',
-           'TPM Ready': 'true',
-           'TPM Password': 'd641b63ce6ff',
-           'TPM Enabled': 'true',
-           'TPM Owned': 'true'}
+    e.g., {'status': 'STATUS_SUCCESS',
+           'is_enabled': 'true',
+           'is_owned': 'true',
+           'is_owner_password_present': 'true',
+           'has_reset_lock_permissions': 'true'}
     """
-    status_txt = self.dut.CheckOutput(['cryptohome', '--action=tpm_status'],
-                                      log=True)
-    status = string_utils.ParseDict(status_txt.splitlines())
+
+    status_txt = self.dut.CheckOutput(
+        ['tpm_manager_client', 'status', '--nonsensitive'], log=True)
+
+    # The status_txt would look like this:
+    #
+    # Message Reply: [tpm_manager.GetTpmNonsensitiveStatusReply] {
+    #   status: STATUS_SUCCESS
+    #   is_enabled: true
+    #   is_owned: true
+    #   is_owner_password_present: true
+    #   has_reset_lock_permissions: true
+    # }
+
+    status_lines = status_txt.splitlines()
+    self.assertTrue(
+        len(status_lines) > 2, 'Failed to get TPM status. Reboot and re-run.')
+
+    # Trim the first and last lines.
+    status = string_utils.ParseDict(status_lines[1:-1])
     logging.info('TPM status: %r', status)
     return status
 
-  def VerifyByCryptoHome(self):
-    """Verifies TPM endorsement by CryptoHome service."""
+  def _AttestationVerifyEK(self):
+    """Returns TPM EK status."""
+
+    status_txt = self.dut.CheckOutput(
+        ['attestation_client', 'verify_attestation', '--ek-only'] +
+        (['--cros_core'] if self.args.is_cros_core else []), log=True)
+
+    # The status_txt would look like this:
+    #
+    # [attestation.VerifyReply] {
+    #   verified: true
+    # }
+
+    status_lines = status_txt.splitlines()
+    self.assertTrue(
+        len(status_lines) > 2,
+        'Failed to get TPM EK status. Reboot and re-run.')
+
+    # Trim the first and last lines.
+    status = string_utils.ParseDict(status_lines[1:-1])
+    logging.info('TPM EK status: %r', status)
+    return status['verified'] == 'true'
+
+  def VerifyByHwsecDaemons(self):
+    """Verifies TPM endorsement by hardware security daemons."""
 
     # Make sure TPM is enabled.
     status = self._TPMStatus()
-    self.assertEqual('true', status['TPM Enabled'])
+    self.assertEqual('true', status['is_enabled'])
 
     # Check explicitly for the case where TPM is owned but password is
     # unavailable.  This shouldn't really ever happen, but in any case
     # the TPM will become un-owned on reboot (thanks to the crossystem
     # command above).
     self.assertFalse(
-        status['TPM Owned'] == 'true' and not status['TPM Password'],
+        status['is_owned'] == 'true' and
+        status['is_owner_password_present'] == 'false',
         'TPM is owned but password is not available. Reboot and re-run.')
 
     # Take ownership of the TPM (if not already taken).
-    self.dut.CheckCall(['cryptohome', '--action=tpm_take_ownership'],
-                       log=True)
-    # Wait for TPM ownership to complete.  No check_call=True since this
-    # may fail if the TPM is already owned.
-    self.dut.Call(['cryptohome', '--action=tpm_wait_ownership'],
-                  log=True)
-    # Sync, to make sure TPM password was written to disk.
-    self.dut.CheckCall(['sync'], log=True)
+    self.dut.CheckCall(['tpm_manager_client', 'take_ownership'], log=True)
 
-    self.assertEqual('true', self._TPMStatus()['TPM Owned'])
-
-    # Verify the endorsement key.
-    with tempfile.TemporaryFile('w+') as stderr:
-      self.dut.CheckCall(
-          ['cryptohome', '--action=tpm_verify_ek'] + (
-              ['--cros_core'] if self.args.is_cros_core else []),
-          log=True, stderr=stderr)
-      # Make sure there's no stderr from tpm_verify_ek (since that, plus
-      # check_call=True, is the only reliable way to make sure it
-      # worked).
-      stderr.seek(0)
-      self.assertEqual('', stderr.read())
-
-  def VerifyByTpmManager(self):
-    """Verifies TPM endorsement by tpm-manager (from CryptoHome package)."""
-
-    # Take ownership of the TPM (if not already taken).
-    self.dut.CheckCall(['tpm-manager', 'initialize'], log=True)
-
-    # Wait until TPM password is ready.
-    sync_utils.PollForCondition(
-        poll_method=self._TPMStatus,
-        condition_method=lambda status: status['TPM Password'],
-        condition_name='TPM password readiness')
+    # Make sure TPM is owned.
+    self.assertEqual('true', self._TPMStatus()['is_owned'])
 
     # Verify TPM endorsement.
-    self.dut.CheckCall(['tpm-manager', 'verify_endorsement'] + (
-        ['--cros_core'] if self.args.is_cros_core else []), log=True)
+    self.assertTrue(self._AttestationVerifyEK())
 
   def runTest(self):
     # Always clear TPM on next boot, in case any problems arise.
     self.dut.CheckCall(['crossystem', 'clear_tpm_owner_request=1'], log=True)
 
-    # Check if we have tpm-manager in system.
-    if self.dut.Call(['which', 'tpm-manager']) == 0:
-      self.VerifyByTpmManager()
-    else:
-      self.VerifyByCryptoHome()
+    # Run the verify process.
+    self.VerifyByHwsecDaemons()
