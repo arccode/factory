324688f8654c763582bda58288789d9b780b38ec
Cheng Yueh <cyueh@chromium.org>
2021-07-21 18:38:54 +0800

    thunderbolt_loopback: Accepts different routes
    Previously, we assume that there is only route 0. We may have different
    routes.
    
    BUG=b:191428023
    TEST=Run test on devices
    
    Change-Id: I25b84f3e47a9bfe7ada83db32c358bb88b401a16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/3041493
    Reviewed-by: Eric Herrmann <eherrmann@chromium.org>
    Reviewed-by: Ting-Hsuan Wang <phoebewang@chromium.org>
    Tested-by: Cheng Yueh <cyueh@chromium.org>
    Commit-Queue: Cheng Yueh <cyueh@chromium.org>

--- py/test/pytests/thunderbolt_loopback.py
+++ py/test/pytests/thunderbolt_loopback.py
@@ -86,7 +86,7 @@ from cros.factory.utils import type_utils
 
 _LOOPBACK_TEST_PATH = '/sys/kernel/debug/thunderbolt'
 _CONTROLLER_PORTS = ('0-1.*', '0-3.*', '1-1.*', '1-3.*')
-_RE_ADP = re.compile(r'^.*\d+-(\d+)\.\d+$')
+_RE_ADP_ROUTE = re.compile(r'^.*(?P<route>\d+)-(?P<adapter>\d+)\.\d+$')
 _RE_MARGIN_LOOPBACK = re.compile(
     r'(RT\d+ L\d+ )(BOTTOM|LEFT),(TOP|RIGHT) = (\d+),(\d+)')
 _DMA_TEST = 'dma_test'
@@ -110,6 +110,7 @@ class ThunderboltLoopbackTest(test_case.TestCase):
   """Thunderbolt loopback card factory test."""
   LOG_GROUP_NAME = 'usb4_lane_margining_log'
   LOG_KEYS = [
+      'ROUTE',
       'ADP',
       'RT1 L0 BOTTOM',
       'RT1 L0 TOP',
@@ -174,6 +175,7 @@ class ThunderboltLoopbackTest(test_case.TestCase):
       self._group_checker = testlog.GroupParam(self.LOG_GROUP_NAME,
                                                self.LOG_KEYS)
       testlog.UpdateParam('ADP', param_type=testlog.PARAM_TYPE.argument)
+      testlog.UpdateParam('ROUTE', param_type=testlog.PARAM_TYPE.argument)
 
   def tearDown(self):
     if self._remove_module:
@@ -286,18 +288,23 @@ class ThunderboltLoopbackTest(test_case.TestCase):
     logging.info('echo %s > %s', content, filename)
     self._dut.WriteFile(filename, content)
 
-  def _TestLaneMargining(self, ADP: str, log_result: dict):
+  def _TestLaneMargining(self, route: str, adapter: str, log_result: dict):
     """Uses tdtl tool to collect lane margining data.
 
     Args:
-      ADP: A string we pass to tdtl tool.
+      route: A string we pass to tdtl tool.
+      adapter: A string we pass to tdtl tool.
       log_result: A dict to save the result.
     """
-    log_result.update({'ADP': int(ADP)})
+    log_result.update({'ADP': int(adapter)})
+    log_result.update({'ROUTE': int(route)})
     # self._dut.CheckOutput do not support env and timeout
     # process_utils.Spawn do not support timeout
-    result = subprocess.run(['cli.py', 'margin_loopback', '-a', ADP], env={
-        'ADP': ADP,
+    result = subprocess.run([
+        'cli.py', 'margin_loopback', '-r', route, '-a', adapter, '-d', '0',
+        '-i', '1'
+    ], env={
+        'ADP': adapter,
         'LC_ALL': 'en_US.utf-8'
     }, cwd=_TDTL_PATH, timeout=self.args.lane_margining_timeout_secs,
                             encoding='utf-8', stdout=subprocess.PIPE,
@@ -374,11 +381,12 @@ class ThunderboltLoopbackTest(test_case.TestCase):
     self.ui.SetState(_('Insert the loopback card.'))
     device_path = sync_utils.WaitFor(self._FindLoopbackPath,
                                      self.args.timeout_secs, poll_interval=0.5)
-    match = _RE_ADP.match(device_path)
+    match = _RE_ADP_ROUTE.match(device_path)
     if not match:
       raise Exception('device_path is not in expected format.')
-    ADP = match.group(1)
-    session.console.info('The ADP is at %r.', ADP)
+    adapter = match.group('adapter')
+    route = match.group('route')
+    session.console.info('The ADP is at %r, route is %r.', adapter, route)
 
     self.ui.SetState(_('Test is in progress, please do not move the device.'))
     session.console.info('The loopback card path is at %r.', device_path)
@@ -417,13 +425,14 @@ class ThunderboltLoopbackTest(test_case.TestCase):
     else:
       errors.append('Unknown result: %r' % result)
     if self.args.lane_margining:
+      session.console.info('Start collecting lane margining data.')
       # Log 0 when failed.
       # Log -1 when timeout.
       log_result = dict.fromkeys(self.LOG_KEYS, None)
       try:
         stop_lane_margining_timer = self.ui.StartCountdownTimer(
             self.args.lane_margining_timeout_secs)
-        self._TestLaneMargining(ADP, log_result)
+        self._TestLaneMargining(route, adapter, log_result)
         stop_lane_margining_timer.set()
       except subprocess.TimeoutExpired:
         logging.exception('_TestLaneMargining timeout')
