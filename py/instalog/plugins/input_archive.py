3c7b25c47b8f536693d0557f7e36125b8c90e9da
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-13 08:49:59 +0000

    Adding tarfile member sanitization to extractall()

--- py/instalog/plugins/input_archive.py
+++ py/instalog/plugins/input_archive.py
@@ -50,7 +50,26 @@ class InputArchive(plugin_base.InputPlugin):
       json_path: The path of log file in tmp_path.
     """
     with tarfile.open(archive_path, 'r:gz') as tar:
-      tar.extractall(tmp_path)
+      def is_within_directory(directory, target):
+          
+          abs_directory = os.path.abspath(directory)
+          abs_target = os.path.abspath(target)
+      
+          prefix = os.path.commonprefix([abs_directory, abs_target])
+          
+          return prefix == abs_directory
+      
+      def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+      
+          for member in tar.getmembers():
+              member_path = os.path.join(path, member.name)
+              if not is_within_directory(path, member_path):
+                  raise Exception("Attempted Path Traversal in Tar File")
+      
+          tar.extractall(path, members, numeric_owner=numeric_owner) 
+          
+      
+      safe_extract(tar, tmp_path)
     archive_name = os.path.basename(archive_path)
     # Remove '.tar.gz'
     dir_name = archive_name.split(os.extsep)[0]
